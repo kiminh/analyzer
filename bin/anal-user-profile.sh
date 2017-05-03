288944c88fc30c462ee179d344422d4895d87f39
Roy Dong <dongwei@innotechx.com>
2017-05-03 17:08:46 +0800

    bash

--- bin/anal-user-profile.sh
+++ bin/anal-user-profile.sh
@@ -4,7 +4,7 @@ cur=/home/cpc/$1analyzer
 SPARK_HOME=/home/spark/spark-2.1.0
 
 $SPARK_HOME/bin/spark-submit --master yarn \
-	--executor-memory 5G --executor-cores 2 --total-executor-cores 10 \
+	--executor-memory 4G --executor-cores 2 --total-executor-cores 10 \
 	--jars "$cur/lib/hadoop-lzo-0.4.20.jar, \
 	        $cur/lib/cpc-protocol_2.11-1.0.jar" \
 	--class com.cpc.spark.qukan.anal.AnalUserProfile \
--- bin/get-popular-pkgs.sh
+++ bin/get-popular-pkgs.sh
@@ -2,16 +2,19 @@
 
 cur=/home/cpc/$1analyzer
 SPARK_HOME=/home/spark/spark-2.1.0
+jars=(
+    "$cur/lib/scala-redis_2.11-1.0.jar",
+    "$cur/lib/hadoop-lzo-0.4.20.jar",
+    "$cur/lib/config-1.2.1.jar",
+    "$cur/lib/cpc-protocol_2.11-1.0.jar",
+    "$cur/lib/json4s-native_2.11-3.5.1.jar",
+    "$cur/lib/json4s-core_2.11-3.5.1.jar",
+    "$cur/lib/json4s-ast_2.11-3.5.1.jar"
+)
 
 $SPARK_HOME/bin/spark-submit --master yarn \
     --executor-memory 4G --executor-cores 2 --total-executor-cores 10 \
-    --jars "$cur/lib/scala-redis_2.11-1.0.jar, \
-            $cur/lib/hadoop-lzo-0.4.20.jar, \
-            $cur/lib/config-1.2.1.jar, \
-            $cur/lib/cpc-protocol_2.11-1.0.jar, \
-            $cur/lib/json4s-native_2.11-3.5.1.jar, \
-            $cur/lib/json4s-core_2.11-3.5.1.jar, \
-            $cur/lib/json4s-ast_2.11-3.5.1.jar" \
+    --jars ${jars[*]// /,} \
     --class com.cpc.spark.qukan.userprofile.GetPopularPkgs \
     $cur/lib/cpc-anal_2.11-1.0.jar 500
 
