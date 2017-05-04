a4f553e3d552faf81e41d246006f7a13a8e5a8d8
Roy Dong <dongwei@innotechx.com>
2017-05-04 15:56:38 +0800

    fix bug

--- src/main/scala/com/cpc/spark/qukan/anal/AnalUserProfile.scala
+++ src/main/scala/com/cpc/spark/qukan/anal/AnalUserProfile.scala
@@ -11,6 +11,14 @@ import org.apache.spark.sql.{SaveMode, SparkSession}
 object AnalUserProfile {
 
   def main(args: Array[String]): Unit = {
+    if (args.length < 1) {
+      System.err.println(
+        s"""
+           |Usage: AnalUserProfile <table>
+           |
+        """.stripMargin)
+      System.exit(1)
+    }
     val cal = Calendar.getInstance()
     cal.add(Calendar.DATE, -1)
     val day = new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime)
@@ -47,10 +55,28 @@ object AnalUserProfile {
           total = total + 1
       }
 
+    val ages = new Array[String](7)
+    val sexs = new Array[String](3)
+    val coins = new Array[String](15)
+
+    for (i <- 0 to age.length - 1) {
+      ages(i) = "%d(%.4f)".format(age(i), age(i).toFloat / total.toFloat)
+    }
+    for (i <- 0 to sex.length - 1) {
+      sexs(i) = "%d(%.4f)".format(sex(i), sex(i).toFloat / total.toFloat)
+    }
+    for (i <- 0 to coin.length - 1) {
+      if (coin(i) > 0) {
+        coins(i) = "%d(%.4f)".format(coin(i), coin(i).toFloat / total.toFloat)
+      } else {
+        coins(i) = "%d(0)".format(coin(i))
+      }
+    }
+
     val stats = UserProfileStats(
-      sex = sex.mkString(","),
-      age = age.mkString(","),
-      coin = coin.mkString(","),
+      sex = sexs.mkString(" "),
+      age = ages.mkString(" "),
+      coin = coins.mkString(" "),
       total = total,
       date = day
     )
@@ -58,27 +84,8 @@ object AnalUserProfile {
     Seq(stats)
       .toDF()
       .write
-      .mode(SaveMode.Overwrite)
+      .mode(SaveMode.Append)
       .saveAsTable("dl_cpc." + args(0))
-
-    println("年龄 0: 未知 1: 小于18 2:18-23 3:24-30 4:31-40 5:41-50 6: >50")
-    for (i <- 0 to age.length - 1) {
-      println("age %d: %d %.4f".format(i, age(i), age(i).toFloat / total.toFloat))
-    }
-
-    println("\n性别 0: 未知 1: 男 2: 女")
-    for (i <- 0 to sex.length - 1) {
-      println("sex %d: %d %.4f".format(i, sex(i), sex(i).toFloat / total.toFloat))
-    }
-
-    println("\n积分, 不显示的分段为0个")
-    for (i <- 0 to coin.length - 1) {
-      if (coin(i) > 0) {
-        println("分段 %d: %d %.4f".format(i + 1, coin(i), coin(i).toFloat / total.toFloat))
-      }
-    }
-
-    println("\ntotal %d".format(total))
 
     ctx.stop()
   }
