6fe4b89f51ce68faf6fd95c7ff553881328e87d6
Roy Dong <dongwei@innotechx.com>
2017-05-08 19:24:46 +0800

    fb

---
+++ src/main/scala/com/cpc/spark/log/parser/TraceLog.scala
@@ -0,0 +1,12 @@
+package com.cpc.spark.log.parser
+
+/**
+  * Created by Roy on 2017/5/8.
+  */
+case class TraceLog(
+                   searchid: String = "",
+                   duration: Int = 0
+
+                   ) {
+
+}
