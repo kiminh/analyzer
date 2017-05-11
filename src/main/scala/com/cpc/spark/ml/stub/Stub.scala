066000fc65dda54ce253553795814168056fd571
Roy Dong <i@roydong.com>
2017-05-12 02:36:47 +0800

    fix bug

--- src/main/scala/com/cpc/spark/ml/stub/Stub.scala
+++ src/main/scala/com/cpc/spark/ml/stub/Stub.scala
@@ -16,7 +16,7 @@ object Stub {
       .usePlaintext(true)
       .build
 
-    val request = Request(ads = Seq(AdInfo(adslotid = 12), AdInfo(adslotid = 33)))
+    val request = Request(ads = Seq(AdInfo(ideaid = 1, adslotid = 12), AdInfo(ideaid = 3, adslotid = 33)))
     val blockingStub = PredictorGrpc.blockingStub(channel)
     val reply = blockingStub.predict(request)
     println(reply.toString)
