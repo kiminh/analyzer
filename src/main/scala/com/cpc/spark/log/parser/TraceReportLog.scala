f26c365f8ac1d30b45881145c490d62b7720ac99
赵刚 <zhaogang@innotechx.com>
2017-05-15 18:14:52 +0800

    trace log

--- src/main/scala/com/cpc/spark/log/parser/TraceReportLog.scala
+++ src/main/scala/com/cpc/spark/log/parser/TraceReportLog.scala
@@ -4,6 +4,7 @@ package com.cpc.spark.log.parser
   * Created by Roy on 2017/5/8.
   */
 case class TraceReportLog(
+                           searchid:String = "",
                            user_id: Int = 0,
                            plan_id: Int = 0,
                            unit_id: Int = 0,
