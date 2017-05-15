33adf5411cac1b5a134bec69f9d6004b8b82dad0
Roy Dong <dongwei@innotechx.com>
2017-05-15 20:32:51 +0800

    svm

---
+++ bin/save-svm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cur=/home/cpc/$1
+SPARK_HOME=/home/spark/spark-2.1.0
+
+jars=(
+    "$cur/lib/mysql-connector-java-5.1.41-bin.jar"
+)
+
+$SPARK_HOME/bin/spark-submit --master yarn \
+    --executor-memory 2G --executor-cores 2 --total-executor-cores 6 \
+    --jars $( IFS=$','; echo "${jars[*]}" ) \
+    --class com.cpc.spark.ml.train.AdvSvm \
+    $cur/lib/cpc-anal-assembly-1.0.jar 3 3 4 19
