fe50107f59cdd2fd2fb00d17c78c03172b6f8509
Roy Dong <dongwei@innotechx.com>
2017-05-18 11:10:06 +0800

    bin

--- bin/get-hour-report.sh
+++ bin/get-hour-report.sh
@@ -7,6 +7,6 @@ $SPARK_HOME/bin/spark-submit --master yarn \
     --executor-memory 2G --executor-cores 2 --total-executor-cores 6 \
     --jars "$cur/lib/mysql-connector-java-5.1.41-bin.jar,$cur/lib/mariadb-java-client-1.5.9.jar" \
     --class com.cpc.spark.log.report.GetHourReport \
-    $cur/lib/cpc-anal_2.11-0.1.jar "cpc_union_log" 3
-
+    $cur/lib/cpc-anal_2.11-0.1.jar "cpc_union_log" 2 && \
+    echo "`date +%Y-%m-%d:%H:%M:%S` done" >> $cur/log/get-hour-report.log
 
--- bin/merge-log.sh
+++ bin/merge-log.sh
@@ -7,10 +7,10 @@ jars=(
     "$cur/lib/mysql-connector-java-5.1.41-bin.jar"
 )
 
-$SPARK_HOME/bin/spark-submit --master yarn \
+$SPARK_HOME/bin/spark-submit --master "spark://10.9.125.57:7077" \
     --executor-memory 2G --executor-cores 2 --total-executor-cores 6 \
     --jars $( IFS=$','; echo "${jars[*]}" ) \
     --class com.cpc.spark.log.anal.AnalUnionLog \
-    $cur/lib/cpc-anal_2.11-0.1.jar "/gobblin/source/cpc" "cpc_union_log_tmp"  "cpc_union_trace_log_tmp" 5 && \
+    $cur/lib/cpc-anal_2.11-0.1.jar "/gobblin/source/cpc" "cpc_union_log" "cpc_union_trace_log" 1 && \
     echo "`date +%Y-%m-%d:%H:%M:%S` done" >> $cur/log/merge-log.log
 
