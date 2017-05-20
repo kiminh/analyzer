8bcb3e3b17e78bdbd162ad52b847a43ae370a0f4
Roy Dong <i@roydong.com>
2017-05-21 01:18:38 +0800

    ml

--- src/main/scala/com/cpc/spark/ml/train/LogisticTrain.scala
+++ src/main/scala/com/cpc/spark/ml/train/LogisticTrain.scala
@@ -2,7 +2,7 @@ package com.cpc.spark.ml.train
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.mllib.feature.Normalizer
-import org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS
+import org.apache.spark.mllib.classification.{LogisticRegressionModel, LogisticRegressionWithLBFGS}
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.sql.{SaveMode, SparkSession}
@@ -15,42 +15,56 @@ import scala.util.Random
 object LogisticTrain {
 
   def main(args: Array[String]): Unit = {
+    if (args.length < 5) {
+      System.err.println(s"""
+                            |Usage: Train <mode=train/test> <path> <model path> <sample rate> <p/n rate>
+        """.stripMargin)
+      System.exit(1)
+    }
     Logger.getRootLogger().setLevel(Level.WARN)
     val ctx = SparkSession.builder()
       .appName("cpc training logistic model")
       .getOrCreate()
     val sc = ctx.sparkContext
-    val parsedData = MLUtils.loadLibSVMFile(sc, args(0))
+    val mode = args(0).trim
+    val inpath = args(1).trim
+    val modelPath = args(2).trim
+    val sampleRate = args(3).toFloat
+    val pnRate = args(4).toInt
+    val parsedData = MLUtils.loadLibSVMFile(sc, inpath)
     val nor = new Normalizer()
     val splits = parsedData
       //random pick 1/20 negative sample
-      .filter(x => x.label > 0.01 || Random.nextInt(20) == 1)
+      .filter(x => x.label > 0.01 || Random.nextInt(pnRate) == 1)
       .map(x => new LabeledPoint(x.label, nor.transform(x.features)))
-      .randomSplit(Array(0.99,0.01), seed = 1314159L)
+      .randomSplit(Array(sampleRate, 1 - sampleRate), seed = 1314159L)
 
-    val training = splits(0).cache()
     val test = splits(1)
-
-    println("sample count", training.count())
-    training
-      .map {
-        x =>
-          var label = 0
-          if (x.label > 0.01) {
-            label = 1
-          }
-          (label, 1)
-      }
-      .reduceByKey((x, y) => x + y)
-      .toLocalIterator
-      .foreach(println)
-
-    println("training ...")
-    val model = new LogisticRegressionWithLBFGS()
-      .setNumClasses(2)
-      .run(training)
-    training.unpersist()
-    println("done")
+    var model: LogisticRegressionModel = null
+    if (mode == "test") {
+      model = LogisticRegressionModel.load(sc, modelPath)
+    } else {
+      val training = splits(0).cache()
+      println("sample count", training.count())
+      training
+        .map {
+          x =>
+            var label = 0
+            if (x.label > 0.01) {
+              label = 1
+            }
+            (label, 1)
+        }
+        .reduceByKey((x, y) => x + y)
+        .toLocalIterator
+        .foreach(println)
+      println("training ...")
+      model = new LogisticRegressionWithLBFGS()
+        .setNumClasses(2)
+        .run(training)
+      println("done")
+      training.unpersist()
+    }
 
     println("testing...")
     model.clearThreshold()
@@ -61,27 +75,42 @@ object LogisticTrain {
     }.cache()
     println("done")
 
-    println("predict distribution", predictionAndLabels.count())
+    println("predict distribution total %s".format(predictionAndLabels.count()))
     predictionAndLabels
       .map {
         x =>
-          ("%.1f-%.0f".format(x._1, x._2), 1)
+          (("%.1f".format(x._1), x._2.toInt), 1)
       }
       .reduceByKey((x, y) => x + y)
+      .map {
+        x =>
+          val key = x._1._1
+          val label = x._1._2
+          if (label == 0) {
+            (key, (x._2, 0))
+          } else {
+            (key, (0, x._2))
+          }
+      }
+      .reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2))
+      .sortByKey(false)
       .toLocalIterator
       .foreach {
         x =>
-          println(x)
+          val sum = x._2
+          println("%s %d %d %.4f".format(x._1, sum._2, sum._1, sum._2.toDouble / (sum._1 + sum._2).toDouble))
       }
 
-    ctx.createDataFrame(predictionAndLabels)
-      .write
-      .mode(SaveMode.Overwrite)
-      .text("/user/cpc/test_result/v1")
+    if (modelPath.length == 0) {
+      ctx.createDataFrame(predictionAndLabels)
+        .write
+        .mode(SaveMode.Overwrite)
+        .text("/user/cpc/test_result/v1")
 
-    predictionAndLabels.unpersist()
-    println("save model")
-    model.save(sc, "/user/cpc/model/v1/")
-    sc.stop()
+      predictionAndLabels.unpersist()
+      println("save model")
+      model.save(sc, modelPath)
+      sc.stop()
+    }
   }
 }
