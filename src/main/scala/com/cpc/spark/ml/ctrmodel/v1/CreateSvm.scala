182152adb859d1ee6ba172e58b48aaa2d89f892a
Roy Dong <i@roydong.com>
2017-08-11 13:45:29 +0800

    fix bug

--- src/main/scala/com/cpc/spark/ml/ctrmodel/v1/CreateSvm.scala
+++ src/main/scala/com/cpc/spark/ml/ctrmodel/v1/CreateSvm.scala
@@ -8,7 +8,9 @@ import com.cpc.spark.ml.common.FeatureDict
 import com.typesafe.config.ConfigFactory
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.luaj.vm2.lib.jse.JsePlatform
 
+import scala.io.Source
 import scala.util.Random
 
 /*
@@ -17,12 +19,13 @@ import scala.util.Random
 object CreateSvm {
 
   def main(args: Array[String]): Unit = {
+
     if (args.length < 7) {
       System.err.println(
         s"""
            |Usage: create svm <version:string> <daybefore:int> <days:int>
            | <rate:int> <ttRate:float> <saveFull:int>
-           | <hour:string>
+           | <hour:string> <updatedict>
            |
         """.stripMargin)
       System.exit(1)
@@ -31,10 +34,11 @@ object CreateSvm {
     val version = args(0)
     val dayBefore = args(1).toInt
     val days = args(2).toInt
-    val rate = args(3).toInt
+    val rate = args(3).split("/").map(_.toInt)
     val ttRate = args(4).toFloat  //train/test rate
     val saveFull = args(5).toInt
-    val hour = args(6)
+    val updateDict = args(6).toBoolean
+    val hour = args(7)
     val ctx = SparkSession.builder()
       .appName("create svm data code:v1 data:" + version)
       .enableHiveSupport()
@@ -55,11 +59,14 @@ object CreateSvm {
       val ulog = ctx.sql(
         s"""
            |select * from dl_cpc.cpc_union_log where `date` = "%s" %s and isfill = 1 and adslotid > 0
+           |and media_appsid in ("80000001", "80000002")
         """.stripMargin.format(date, hourSql))
         .as[UnionLog].rdd
         .randomSplit(Array(ttRate, 1 - ttRate), seed = new Date().getTime)
 
-      FeatureDict.updateDict(ulog(0).cache())
+      if (updateDict) {
+        FeatureDict.updateDict(ulog(0).cache())
+      }
       FeatureDict.loadData()
       FeatureDict.updateServerData(ConfigFactory.load())
       val bdict = ctx.sparkContext.broadcast(FeatureDict.dict)
@@ -67,7 +74,7 @@ object CreateSvm {
       val train = ulog(0).filter {
           u =>
             var ret = false
-            if (u.isclick == 1 || Random.nextInt(rate) == 0) {
+            if (u.isclick == 1 || Random.nextInt(rate(1)) < rate(0)) {
               ret = true
             }
             ret
@@ -88,7 +95,7 @@ object CreateSvm {
         .write
         .mode(SaveMode.Overwrite)
         .text("/user/cpc/svmdata/" + version + "/" + date)
-      println("done", train.count())
+      println("done", train.filter(_.startsWith("1")).count(), train.count())
 
       if (saveFull > 0) {
         println("save full data")
