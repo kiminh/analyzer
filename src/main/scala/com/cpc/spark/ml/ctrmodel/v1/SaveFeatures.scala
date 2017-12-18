f87225e43ccd88e2e6facc0a8c8125046432d238
Roy Dong <i@roydong.com>
2017-12-18 17:45:53 +0800

    features

--- src/main/scala/com/cpc/spark/ml/ctrmodel/v1/SaveFeatures.scala
+++ src/main/scala/com/cpc/spark/ml/ctrmodel/v1/SaveFeatures.scala
@@ -2,7 +2,9 @@ package com.cpc.spark.ml.ctrmodel.v1
 
 import java.util.Date
 
-import org.apache.spark.sql.{SaveMode, SparkSession}
+import com.cpc.spark.log.parser.{TraceLog, UnionLog}
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{Row, SaveMode, SparkSession}
 
 /**
   * Created by roydong on 15/12/2017.
@@ -16,7 +18,9 @@ object SaveFeatures {
       .appName("save feature ids" + date)
       .enableHiveSupport()
       .getOrCreate()
-    saveDataFromLog(spark, date)
+
+    //saveDataFromLog(spark, date)
+    saveCvrData(spark, date)
   }
 
 
@@ -45,6 +49,81 @@ object SaveFeatures {
     ulog.randomSplit(Array(rate, 1 - rate), new Date().getTime)(0)
       .write
       .mode(SaveMode.Overwrite)
-      .parquet("/user/cpc/lrmodel/features/%s".format(date))
+      .parquet("/user/cpc/lrmodel/ctrdata/%s".format(date))
+  }
+
+  def saveCvrData(spark: SparkSession, date: String): Unit = {
+    val sqlStmt =
+      """
+        |select searchid,sex,age,os,isp,network,
+        |       city,media_appsid,ext['phone_level'].int_value,`timestamp`,adtype,
+        |       planid,unitid,ideaid,ext['adclass'].int_value,adslotid,
+        |       adslot_type,ext['pagenum'].int_value,ext['bookid'].string_value
+        |
+        |from dl_cpc.cpc_union_log where `date` = "%s" and isclick = 1
+        |and ext['antispam'].int_value = 0
+        |
+      """.stripMargin.format(date)
+    import spark.implicits._
+
+
+    println(sqlStmt)
+    val clicklog = spark.sql(sqlStmt)
+    println("click log", clicklog.count())
+
+    val cvrlog = spark.sql(
+      s"""
+         |select * from dl_cpc.cpc_union_trace_log where `date` = "%s"
+        """.stripMargin.format(date))
+      .as[TraceLog].rdd
+      .map {
+        x =>
+          (x.searchid, Seq(x))
+      }
+      .reduceByKey(_ ++ _)
+      .map {
+        x =>
+          val convert = cvrPositive(x._2:_*)
+          Cvr(searchid = x._1, convert = convert)
+      }
+
+    clicklog.join(cvrlog.toDS(), Seq("searchid"))
+      .write
+      .mode(SaveMode.Overwrite)
+      .parquet("/user/cpc/lrmodel/cvrdata/%s".format(date))
+  }
+
+  private case class Cvr(searchid: String = "", convert: Int = 0)
+
+  def cvrPositive(traces: TraceLog*): Int = {
+    var stay = 0
+    var click = 0
+    var active = 0
+    var mclick = 0
+    traces.foreach {
+      t =>
+        t.trace_type match {
+          case s if s.startsWith("active") => active += 1
+
+          case "buttonClick" => click += 1
+
+          case "clickMonitor" => mclick += 1
+
+          case "inputFocus" => click += 1
+
+          case "press" => click += 1
+
+          case "stay" =>
+            if (t.duration > stay) {
+              stay = t.duration
+            }
+        }
+    }
+
+    if ((stay >= 30 && click > 0) || active > 0 || (stay >= 60 && mclick > 0)) {
+      1
+    } else {
+      0
+    }
   }
 }
