8b02ada052fa1e2328010ab001f9881766d0865e
Roy Dong <i@roydong.com>
2017-12-21 10:45:05 +0800

    add ulog

--- src/main/scala/com/cpc/spark/log/parser/LogParser.scala
+++ src/main/scala/com/cpc/spark/log/parser/LogParser.scala
@@ -109,6 +109,7 @@ object LogParser {
       )
       ext.update("phone_price", ExtValue(int_value = device.getPhoneprice))
       ext.update("phone_level", ExtValue(int_value = device.getPhonelevel))
+      ext.update("brand_title", ExtValue(string_value = device.getBrandTitle))
       val osv = device.getOsversion
       ext.update("os_version", ExtValue(string_value = "%d.%d.%d.%d"
         .format(osv.getMajor, osv.getMinor, osv.getMicro, osv.getBuild)))
