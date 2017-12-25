bac3e6c3743c79f2863df8c9d16732c2509a21be
zhaolei <zhaolei@aiclk.com>
2017-12-25 16:18:05 +0800

    add uid

--- src/main/scala/com/cpc/spark/ml/ctrmodel/v1/SaveFeatures.scala
+++ src/main/scala/com/cpc/spark/ml/ctrmodel/v1/SaveFeatures.scala
@@ -32,7 +32,7 @@ object SaveFeatures {
         |       adslot_type,ext['pagenum'].int_value as pagenum,ext['bookid'].string_value as bookid,
         |       ext['brand_title'].string_value as brand_title,
         |       ext['user_req_ad_num'].int_value as user_req_ad_num,
-        |       ext['user_req_num'].int_value as user_req_num
+        |       ext['user_req_num'].int_value as user_req_num,uid
         |from dl_cpc.cpc_union_log where `date` = "%s" and isshow = 1
         |and ext['antispam'].int_value = 0
         |
@@ -81,7 +81,7 @@ object SaveFeatures {
         |       adslot_type,ext['pagenum'].int_value as pagenum,ext['bookid'].string_value as bookid,
         |       ext['brand_title'].string_value as brand_title,
         |       ext['user_req_ad_num'].int_value as user_req_ad_num,
-        |       ext['user_req_num'].int_value as user_req_num
+        |       ext['user_req_num'].int_value as user_req_num,uid
         |from dl_cpc.cpc_union_log where `date` = "%s" and isclick = 1
         |and ext['antispam'].int_value = 0
         |
