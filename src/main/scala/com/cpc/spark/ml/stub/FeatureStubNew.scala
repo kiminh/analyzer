e517dd347bd427e03f46bb5d2be8a55aaccc652f
Roy Dong <i@roydong.com>
2017-12-28 18:36:02 +0800

    fix bug

--- src/main/scala/com/cpc/spark/ml/stub/FeatureStubNew.scala
+++ src/main/scala/com/cpc/spark/ml/stub/FeatureStubNew.scala
@@ -13,12 +13,13 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.mllib.regression.IsotonicRegressionModel
 import org.apache.spark.mllib.classification.LogisticRegressionModel
 import org.apache.spark.mllib.util.MLUtils
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Row, SparkSession}
 import com.redis.RedisClient
 import com.redis.serialization.Parse.Implicits._
 import userprofile.Userprofile.{APPPackage, UserProfile}
 import lrmodel.lrmodel._
 import mlserver.mlserver._
+
 import scala.collection.mutable
 
 /**
@@ -52,11 +53,20 @@ object FeatureStubNew {
     val date = "2017-12-26"
     val hour = "13"
 
-    val sql = "select searchid,isclick as label,sex,age,os,isp,network,city,media_appsid,ext['phone_level'].int_value as phone_level,`timestamp`,adtype,planid,unitid,ideaid,ext['adclass'].int_value as adclass,adslotid,adslot_type,ext['pagenum'].int_value as pagenum,ext['bookid'].string_value as bookid,ext['user_req_ad_num'].int_value as user_req_ad_num,ext[ 'user_req_num'].int_value as user_req_num, uid from dl_cpc.cpc_union_log where `date` = \"%s\" and media_appsid in (\"80000001\", \"80000002\") and adslot_type in (1,2)  and interests != \"\" and isclick = 1 and hour = \"%s\" and ext['antispam'].int_value = 0 limit %s".format(date, hour, args(4).toInt)
+    val sql =
+      """
+        |select searchid,isclick as label,sex,age,os,isp,network,city,media_appsid,
+        |ext['phone_level'].int_value as phone_level,`timestamp`,
+        |adtype,planid,unitid,ideaid,ext['adclass'].int_value as adclass,
+        |adslotid,adslot_type,ext['pagenum'].int_value as pagenum,ext['bookid'].string_value as bookid,
+        |ext['user_req_ad_num'].int_value as user_req_ad_num,ext[ 'user_req_num'].int_value as user_req_num, uid
+        |from dl_cpc.cpc_union_log
+        |where `date` = "%s" and media_appsid in ("80000001", "80000002") and adslot_type in (1,2)
+        | and isclick = 1 and hour = "%s" and ext['antispam'].int_value = 0 limit %s"""
+        .stripMargin.format(date, hour, args(4).toInt)
 
     println(sql)
-
-    val logs = ctx.sql(sql)
+    val logs = ctx.sql(sql).cache()
 
 
     //按分区取数据
@@ -75,8 +85,8 @@ object FeatureStubNew {
     }
 
     var dictStr = mutable.Map[String, Map[String, Int]]()
-    val inpath = "/gobblin/source/lechuan/qukan/extend_report/{%s}".format(pathSep.mkString(","))
-    val uidApp = LRTrain.getUserAppInstalled(ctx, inpath)
+    //val inpath = "/gobblin/source/lechuan/qukan/extend_report/{%s}".format(pathSep.mkString(","))
+    //val uidApp = LRTrain.getUserAppInstalled(ctx, inpath)
 
     val pack = Pack.parseFrom(new FileInputStream(args(5)))
     val ids =  pack.appid
@@ -84,7 +94,7 @@ object FeatureStubNew {
 
     //val ids = LRTrain.getTopApp(uidApp, 1000)
     dictStr.update("appid",ids)
-    val userAppIdx = LRTrain.getUserAppIdx(ctx, uidApp, ids)
+    //val userAppIdx = LRTrain.getUserAppIdx(ctx, uidApp, ids)
 
     val modelPath = args(2).trim
     val irPath = args(3).trim
@@ -97,54 +107,91 @@ object FeatureStubNew {
     import ctx.implicits._
 
 
-    logs.rdd.toLocalIterator.foreach{
+    val idxdata = logs.rdd.toLocalIterator.map {
       x =>
-
-        var seq = Seq[String]()
-        val uid = x.getAs[String]("uid") + "_UPDATA"
+        var idseq = Seq[Int]()
+        val uid = x.getAs[String]("uid")
+        val key = uid + "_UPDATA"
 
         val conf = ConfigFactory.load()
         val redis = new RedisClient(conf.getString("redis.host"), conf.getInt("redis.port"))
-        val buffer = redis.get[Array[Byte]](uid).getOrElse(null)
+        val buffer = redis.get[Array[Byte]](key).getOrElse(null)
         if (buffer != null) {
           val installpkg = UserProfile.parseFrom(buffer).getInstallpkgList
-          for( i <- 0 until installpkg.size){
-            seq = seq :+ installpkg.get(i).getPackagename
+          for (i <- 0 until installpkg.size) {
+            val name = installpkg.get(i).getPackagename
+            val id = ids.getOrElse(name, 0)
+            if (id > 0) {
+              idseq = idseq :+ id
+            }
           }
         }
-
-
-
-        val v = LRTrain.getVectorParser3(x)
-        println(v)
-
-        val p = model.predict(v)
-        val p1 = irmodel.predict(p)
-
-        val (ad, m, slot, u, loc, n, d, t) = LRTrain.unionLogToObject(x,seq)
-
-        val req = Request(
-          version = "v3",
-          ads = Seq(ad),
-          media = Option(m),
-          user = Option(u),
-          loc = Option(loc),
-          network = Option(n),
-          device = Option(d),
-          time = x.getAs[Int]("timestamp"),
-          cvrVersion = "cvr_v1",
-          adSlot = Option(slot)
-        )
-
-        val blockingStub = PredictorGrpc.blockingStub(channel)
-        val reply = blockingStub.predict(req)
-
-        //val ctr = x.ext.getOrElse("exp_ctr", ExtValue()).int_value / 1e6
-
-        println(ad.ideaid, p1, reply.results(0).value)
-        println(ad.ideaid, p1)
-        println("")
+        (uid, idseq)
     }
+    .toSeq.toDF("uid", "appIdx")
+
+    println("idx data")
+    idxdata.rdd
+      .toLocalIterator
+      .foreach {
+        x =>
+          println(x.getAs[String]("uid"), x.getAs[Seq[Int]]("appIdx").mkString(" "))
+      }
+
+    println("merge data")
+    logs.join(idxdata, Seq("uid"), "leftouter")
+      .rdd
+      .toLocalIterator
+      .foreach {
+        x =>
+          var seq = Seq[String]()
+          val uid = x.getAs[String]("uid") + "_UPDATA"
+          println(uid)
+
+          val conf = ConfigFactory.load()
+          val redis = new RedisClient(conf.getString("redis.host"), conf.getInt("redis.port"))
+          val buffer = redis.get[Array[Byte]](uid).getOrElse(null)
+          if (buffer != null) {
+            val installpkg = UserProfile.parseFrom(buffer).getInstallpkgList
+            for( i <- 0 until installpkg.size){
+              val name = installpkg.get(i).getPackagename
+              seq = seq :+ name
+            }
+          }
+
+          val appIdx = x.getAs[Seq[Int]]("appIdx")
+          if (appIdx != null) {
+            println(uid, appIdx.mkString(" "))
+          }
+
+          println("------")
+
+          val v = LRTrain.getVectorParser3(x)
+          println(v)
+
+          val p = model.predict(v)
+          val p1 = irmodel.predict(p)
+          val (ad, m, slot, u, loc, n, d, t) = LRTrain.unionLogToObject(x,seq)
+
+          val req = Request(
+            version = "v3",
+            ads = Seq(ad),
+            media = Option(m),
+            user = Option(u),
+            loc = Option(loc),
+            network = Option(n),
+            device = Option(d),
+            time = x.getAs[Int]("timestamp"),
+            cvrVersion = "cvr_v1",
+            adSlot = Option(slot)
+          )
+
+          val blockingStub = PredictorGrpc.blockingStub(channel)
+          val reply = blockingStub.predict(req)
+
+          println("ideaid=%d scala=%.8f ml=%.8f".format(ad.ideaid, p1, reply.results(0).value))
+          println("")
+      }
 
   }
 
