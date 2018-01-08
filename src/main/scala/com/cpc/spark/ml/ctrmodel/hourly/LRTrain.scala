5006d16b2c4d03bd2564f14bdd3be3c6b44eef4a
zhaolei <zhaolei@aiclk.com>
2018-01-08 14:40:28 +0800

    rename destfile name

--- src/main/scala/com/cpc/spark/ml/ctrmodel/hourly/LRTrain.scala
+++ src/main/scala/com/cpc/spark/ml/ctrmodel/hourly/LRTrain.scala
@@ -88,19 +88,19 @@ object LRTrain {
     model.clearResult()
     val qttAllPre = ulogData.filter(x => (x.getAs[String]("media_appsid") == "80000001" || x.getAs[String]("media_appsid") == "80000002") && (x.getAs[Int]("adslot_type") == 1 || x.getAs[Int]("adslot_type") == 2))
     val qttAll = getLimitedData(4e8, qttAllPre).cache()
-    train(spark, "parser1", "qtt-all-parser1", qttAll, "qtt-all-parser1.lrm")
+    train(spark, "parser1", "qtt-all-parser1-hourly", qttAll, "qtt-all-parser1-hourly.lrm")
 
     //qtt-list-parser3
     model.clearResult()
     val qttListPre = ulogData.filter(x => (x.getAs[String]("media_appsid") == "80000001" || x.getAs[String]("media_appsid") == "80000002") && x.getAs[Int]("adslot_type") == 1)
     val qttList = getLimitedData(4e8, qttListPre).cache()
-    train(spark, "parser3", "qtt-list-parser3", qttList, "qtt-list-parser3.lrm")
+    train(spark, "parser3", "qtt-list-parser3-hourly", qttList, "qtt-list-parser3-hourly.lrm")
 
     //qtt-content-parser3
     model.clearResult()
     val qttContentPre = ulogData.filter(x => (x.getAs[String]("media_appsid") == "80000001" || x.getAs[String]("media_appsid") == "80000002") && x.getAs[Int]("adslot_type") == 2)
     val qttContent = getLimitedData(4e8, qttContentPre).cache()
-    train(spark, "parser3", "qtt-content-parser3", qttContent, "qtt-content-parser3.lrm")
+    train(spark, "parser3", "qtt-content-parser3-hourly", qttContent, "qtt-content-parser3-hourly.lrm")
 
     Utils.sendMail(trainLog.mkString("\n"), "TrainLog", Seq("rd@aiclk.com"))
 
