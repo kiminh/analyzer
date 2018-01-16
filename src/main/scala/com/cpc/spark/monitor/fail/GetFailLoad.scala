83005fba2f0d2bed2c672cf23fcfac79f1dc7cd7
谈凯德 <tankaide@aiclicash.com>
2018-01-16 11:47:15 +0800

    MOD 使用日期和小时数来获取数据

--- src/main/scala/com/cpc/spark/monitor/fail/GetFailLoad.scala
+++ src/main/scala/com/cpc/spark/monitor/fail/GetFailLoad.scala
@@ -1,6 +1,7 @@
 package com.cpc.spark.monitor.fail
 
 import java.io.{File, PrintWriter}
+import java.sql.DriverManager
 import java.text.SimpleDateFormat
 import java.util.{Calendar, Properties}
 
@@ -21,99 +22,78 @@ object GetFailLoad {
 
         val day = args(0).toString
         val hour = args(1).toInt
-        val path = args(3).toString
-        if (path.length == 0) {
-            System.err.println(
-                s"""
-                   |Usage: CtrModel <Last timestramp configure file:string>
-        """.stripMargin)
-            System.exit(1)
-        }
-        println("last timestamp configure file path:", path)
-
-        val file = Source.fromFile(path)
-        var lastTimestamp = ""
-        try {
-            lastTimestamp = file.getLines.next()
-        } finally {
-            file.close
-        }
-        println("last timestamp:", lastTimestamp)
-        if (lastTimestamp.length < 13) {
-            println("Last timestramp configure file is valid, content:" + lastTimestamp)
-            System.exit(1)
-        }
 
         val ctx = SparkSession
             .builder()
-            .appName("GetFailLoad run ....lastTimestamp %s".format(lastTimestamp))
+            .appName("GetFailLoad run ....time %s %s".format(day, hour))
             .enableHiveSupport()
             .getOrCreate()
 
-        println("GetFailLoad run ....lastTimestamp %s".format(lastTimestamp))
+        println("GetFailLoad run ....time %s %s".format(day, hour))
 
         val reg = "iclicashsid=[^&]*".r
         val logData = ctx
             .sql(
                 """
-                  |SELECT log_timestamp, field['url'].string_type
+                  |SELECT field['url'].string_type
                   |FROM gobblin.qukan_report_log_five_minutes
                   |WHERE field['cmd'].string_type = "9027"
                   |AND field['url'].string_type like "%s"
                   |AND field['error_code'].string_type != "0"
-                  |AND log_timestamp>%s
-                """.stripMargin.format("%iclica%", lastTimestamp))
+                  |AND thedate = "%s"
+                  |AND thehour = "%s"
+                """.stripMargin.format("%iclica%", day, hour))
             .rdd
             .map {
                 x =>
-                    val timestamp = x.getLong(0)
-                    val url = reg.replaceAllIn(x.getString(1), "")
-                    val count = 1
-                    (url, (timestamp, count))
+                    val url = reg.replaceAllIn(x.getString(0), "")
+                    (url, 1)
             }
             .reduceByKey {
                 (a, b) =>
-                    var timestamp = a._1
-                    if (a._1 < b._1) {
-                        timestamp = b._1
-                    }
-                    (timestamp, a._2 + b._2)
+                    a + b
             }
             .cache()
 
         println("userData count", logData.count())
 
-        var newestTimestramp = 0L
+        var allCount = 0
         val data = logData.collect()
         for (d <- data) {
-            if (newestTimestramp < d._2._1) {
-                newestTimestramp = d._2._1
-            }
             println(d.toString())
+            allCount += d._2
         }
-
-        println("newestTimestramp:", newestTimestramp)
-
-        if (lastTimestamp.toLong < newestTimestramp) {
-            val writer = new PrintWriter(new File(path))
-            try {
-                writer.print(newestTimestramp)
-            } finally {
-                writer.close()
-            }
-        }
+        println("allCount:", allCount)
 
         logData
             .map {
                 x =>
-                    (x._1, x._2._2, day, hour)
+                    (x._1, x._2, day, hour)
             }
-
+        clearReportHourData(day, hour)
         val userCvrDataFrame = ctx.createDataFrame(logData).toDF("url", "count", "date", "hour")
 
         userCvrDataFrame
             .write
             .mode(SaveMode.Append)
             .jdbc(mariadbUrl, "report.monitor_fail_load", mariadbProp)
+    }
+
+    def clearReportHourData(date: String, hour: Int): Unit = {
+        try {
+            Class.forName(mariadbProp.getProperty("driver"))
+            val conn = DriverManager.getConnection(
+                mariadbUrl,
+                mariadbProp.getProperty("user"),
+                mariadbProp.getProperty("password"))
+            val stmt = conn.createStatement()
+            val sql =
+                """
+                  |delete from report.monitor_fail_load where `date` = "%s" and `hour` = %d
+                """.stripMargin.format(date, hour)
+            stmt.executeUpdate(sql);
+        } catch {
+            case e: Exception => println("exception caught: " + e);
+        }
     }
 }
