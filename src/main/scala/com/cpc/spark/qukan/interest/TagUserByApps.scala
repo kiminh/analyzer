9464386be2df29b896c024ca5c38861c103918cb
zhaoyichen <zhaoyichen@aiclk.com>
2018-03-22 11:52:01 +0800

    '.'

--- src/main/scala/com/cpc/spark/qukan/interest/TagUserByApps.scala
+++ src/main/scala/com/cpc/spark/qukan/interest/TagUserByApps.scala
@@ -41,7 +41,7 @@ object TagUserByApps {
       .appName("tag user by installed apps [%s]".format(day))
       .getOrCreate()
 
-    val conf = ConfigFactory.load("interest")
+    var conf = ConfigFactory.load("interest")
 
     println("-----pkg usage-----", day)
     //user app install info
@@ -56,55 +56,58 @@ object TagUserByApps {
 
     println("users", userPkgs.count())
 
+
+
+
     //借贷
-    val loanUids = uidHasApps(userPkgs, "user_tag_by_apps.loans.contains")
+    val loanUids = uidHasApps(userPkgs, "interest", "user_tag_by_apps.loans.contains")
     var sum = tagUser(loanUids, conf.getInt("user_tag_by_apps.loans.tag_id"))
     println("has loans app users", sum)
     loanUids.take(10).foreach(println)
 
-    val finUids = uidHasApps(userPkgs, "user_tag_by_apps.invest.contains")
+    val finUids = uidHasApps(userPkgs, "interest", "user_tag_by_apps.invest.contains")
     sum = tagUser(finUids, conf.getInt("user_tag_by_apps.invest.tag_id"))
     println("has invest app users", sum)
     finUids.take(10).foreach(println)
 
     //二类电商
-    val eshopUids = uidHasNoApps(userPkgs, "user_tag_by_apps.sec_eshops.excludes")
+    val eshopUids = uidHasNoApps(userPkgs, "interest", "user_tag_by_apps.sec_eshops.excludes")
     sum = tagUser(eshopUids, conf.getInt("user_tag_by_apps.sec_eshops.tag_id"))
     println("no eshop users", sum)
     eshopUids.take(10).foreach(println)
 
     //非支付宝用户
-    val alipayUids = uidHasNoApps(userPkgs, "user_tag_by_apps.no_alipay.excludes")
+    val alipayUids = uidHasNoApps(userPkgs, "interest", "user_tag_by_apps.no_alipay.excludes")
     sum = tagUser(alipayUids, conf.getInt("user_tag_by_apps.no_alipay.tag_id"))
     println("no alipay users", sum)
     alipayUids.take(10).foreach(println)
 
     //棋牌
-    val qipaiUids = uidHasApps(userPkgs, "user_tag_by_apps.qipai.contains")
+    val qipaiUids = uidHasApps(userPkgs, "interest", "user_tag_by_apps.qipai.contains")
     sum = tagUser(qipaiUids, conf.getInt("user_tag_by_apps.qipai.tag_id"))
     println("has qipai app users", sum)
     qipaiUids.take(10).foreach(println)
 
     //保健品
-    val baojianpinUids = uidHasApps(userPkgs, "user_tag_by_apps.baojianpin.contains")
+    val baojianpinUids = uidHasApps(userPkgs, "interest", "user_tag_by_apps.baojianpin.contains")
     sum = tagUser(baojianpinUids, conf.getInt("user_tag_by_apps.baojianpin.tag_id"))
     println("has baojianpin app users", sum)
     baojianpinUids.take(10).foreach(println)
 
     //医疗
-    val yiliaoUids = uidHasApps(userPkgs, "user_tag_by_apps.yiliao.contains")
+    val yiliaoUids = uidHasApps(userPkgs, "interest", "user_tag_by_apps.yiliao.contains")
     sum = tagUser(yiliaoUids, conf.getInt("user_tag_by_apps.yiliao.tag_id"))
     println("has yiliao app users", sum)
     yiliaoUids.take(10).foreach(println)
 
     //腾讯QQ
-    val qqUids = uidHasApps(userPkgs, "user_tag_by_apps.mobileqq.contains")
+    val qqUids = uidHasApps(userPkgs, "interest", "user_tag_by_apps.mobileqq.contains")
     sum = tagUser(qqUids, conf.getInt("user_tag_by_apps.mobileqq.tag_id"))
     println("has mobileqq app users", sum)
     qqUids.take(10).foreach(println)
 
     //唯品会
-    val vipUids = uidHasApps(userPkgs, "user_tag_by_apps.vipshop.contains")
+    val vipUids = uidHasApps(userPkgs, "interest", "user_tag_by_apps.vipshop.contains")
     sum = tagUser(vipUids, conf.getInt("user_tag_by_apps.vipshop.tag_id"))
     println("has vipshop app users", sum)
     vipUids.take(10).foreach(println)
@@ -123,7 +126,7 @@ object TagUserByApps {
     game_qipai.take(10).foreach(println)
     */
     //益智游戏
-    val game_yizhi = uidHasApps(userPkgs, "user_tag_by_apps.game_yizhi.contains")
+    val game_yizhi = uidHasApps(userPkgs, "interest", "user_tag_by_apps.game_yizhi.contains")
     sum = tagUser(game_yizhi, conf.getInt("user_tag_by_apps.game_yizhi.tag_id"))
     println("has game_yizhi app users", sum)
     game_yizhi.take(10).foreach(println)
@@ -147,7 +150,7 @@ object TagUserByApps {
     game_xiuxian.take(10).foreach(println)
 */
     //策略游戏
-    val game_celue = uidHasApps(userPkgs, "user_tag_by_apps.game_celue.contains")
+    val game_celue = uidHasApps(userPkgs, "interest", "user_tag_by_apps.game_celue.contains")
     sum = tagUser(game_celue, conf.getInt("user_tag_by_apps.game_celue.tag_id"))
     println("has game_celue app users", sum)
     game_celue.take(10).foreach(println)
@@ -159,27 +162,69 @@ object TagUserByApps {
     game_mmorpg.take(10).foreach(println)
 */
     //团队合作游戏
-    val game_tuandui = uidHasApps(userPkgs, "user_tag_by_apps.game_tuandui.contains")
+    val game_tuandui = uidHasApps(userPkgs, "interest", "user_tag_by_apps.game_tuandui.contains")
     sum = tagUser(game_tuandui, conf.getInt("user_tag_by_apps.game_tuandui.tag_id"))
     println("has game_tuandui app users", sum)
     game_tuandui.take(10).foreach(println)
 
 
+    //手机银行
+    conf = ConfigFactory.load("people/shoujiyinhang")
+    val shoujiyinhangUids = uidHasApps(userPkgs,"people/shoujiyinhang","user_tag_by_apps.shoujiyinhang.contains")
+    sum = tagUser(shoujiyinhangUids, conf.getInt("user_tag_by_apps.shoujiyinhang.tag_id"))
+    println("has shoujiyinhang app users", sum)
+    shoujiyinhangUids.take(10).foreach(println)
 
+    //信用卡
+    conf = ConfigFactory.load("people/xinyongka")
+    val xinyongkaUids = uidHasApps(userPkgs, "people/xinyongka", "user_tag_by_apps.xinyongka.contains")
+    sum = tagUser(xinyongkaUids, conf.getInt("user_tag_by_apps.xinyongka.tag_id"))
+    println("has xinyongka app users", sum)
+    xinyongkaUids.take(10).foreach(println)
+
+    //    val shoujiyinhangUids = getDistribution(userPkgs, "user_tag_by_apps.shoujiyinhang.contains")
+    //    println("手机银行")
+    //    println("只安装了1个app的用户数:%d".format(shoujiyinhangUids.filter(x=>x._2==1).count))
+    //    println("只安装了2个app的用户数:%d".format(shoujiyinhangUids.filter(x=>x._2==2).count))
+    //    println("只安装了3个app的用户数:%d".format(shoujiyinhangUids.filter(x=>x._2==3).count))
+    //    println("只安装了4个app的用户数:%d".format(shoujiyinhangUids.filter(x=>x._2==4).count))
+    //    println("安装超过4个app的用户数:%d".format(shoujiyinhangUids.filter(x=>x._2> 4).count))
+    //    println("---------")
+    //    println("信用卡")
+    //    val xinyongka = getDistribution(userPkgs, "user_tag_by_apps.xinyongka.contains")
+    //    println("只安装了1个app的用户数:%d".format(xinyongka.filter(x=>x._2==1).count))
+    //    println("只安装了2个app的用户数:%d".format(xinyongka.filter(x=>x._2==2).count))
+    //    println("只安装了3个app的用户数:%d".format(xinyongka.filter(x=>x._2==3).count))
+    //    println("只安装了4个app的用户数:%d".format(xinyongka.filter(x=>x._2==4).count))
+    //    println("安装超过4个app的用户数:%d".format(xinyongka.filter(x=>x._2> 4).count))
 
     userPkgs.unpersist()
   }
 
-  def uidHasNoApps(userPkgs: RDD[(String, List[String])], pkg: String): RDD[String] = {
-    checkHasApps(userPkgs, pkg).filter(_._2 == false).map(_._1)
+  def uidHasNoApps(userPkgs: RDD[(String, List[String])],confPath: String, pkg: String): RDD[String] = {
+    checkHasApps(userPkgs, confPath, pkg).filter(_._2 == false).map(_._1)
   }
 
-  def uidHasApps(userPkgs: RDD[(String, List[String])], pkg: String): RDD[String] = {
-    checkHasApps(userPkgs, pkg).filter(_._2).map(_._1)
+  def getDistribution(userPkgs: RDD[(String, List[String])], confPath: String, pkg: String): RDD[(String, Int)] = {
+    val conf = ConfigFactory.load(confPath)
+    val pconf = conf.getConfigList(pkg)
+    var pkgs = Seq[String]()
+    for (i <- 0 until pconf.size()) {
+      val c = pconf.get(i)
+      pkgs = pkgs :+ c.getString("pkg")
+    }
+    //println("has pkg names", pkgs)
+    userPkgs.flatMap(x => x._2.map(v => (x._1, v)))
+      .map(x => (x._1, pkgs.contains(x._2))).filter(x=>x._2).map(x=>(x._1,1))
+      .reduceByKey(_ + _)
   }
 
-  def checkHasApps(userPkgs: RDD[(String, List[String])], pkg: String): RDD[(String, Boolean)] = {
-    val conf = ConfigFactory.load("interest")
+  def uidHasApps(userPkgs: RDD[(String, List[String])], confPath: String, pkg: String): RDD[String] = {
+    checkHasApps(userPkgs, confPath, pkg).filter(_._2).map(_._1)
+  }
+
+  def checkHasApps(userPkgs: RDD[(String, List[String])], confPath: String, pkg: String): RDD[(String, Boolean)] = {
+    val conf = ConfigFactory.load(confPath)
     val pconf = conf.getConfigList(pkg)
     var pkgs = Seq[String]()
     for (i <- 0 until pconf.size()) {
