166ac12bfb0e3204868456b3785ded43b9824b2b
zhuhaijian <zhuhaijian@aiclk.com>
2018-06-09 11:36:31 +0800

    test partitioner

--- src/main/scala/com/cpc/spark/test_partitioner.scala
+++ src/main/scala/com/cpc/spark/test_partitioner.scala
@@ -1,8 +1,6 @@
 package com.cpc.spark
-
 import org.apache.spark.Partitioner
 
-import scala.util.hashing.MurmurHash3
 
 
 class test_partitioner(val num: Int) extends Partitioner {
@@ -10,6 +8,6 @@ class test_partitioner(val num: Int) extends Partitioner {
 
   override def getPartition(key: Any): Int = {
     val s = key.asInstanceOf[(String, Int)]
-    (MurmurHash3.stringHash(s._1) + s._2) % num
+    s._2 % num
   }
 }
