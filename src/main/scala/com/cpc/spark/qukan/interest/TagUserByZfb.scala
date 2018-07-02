35b8d4d1209a41fbbef4233350afbeaa96a34781
mayuntao <mayuntao@aiclk.com>
2018-07-02 19:14:47 +0800

    zfb update user tag

--- src/main/scala/com/cpc/spark/qukan/interest/TagUserByZfb.scala
+++ src/main/scala/com/cpc/spark/qukan/interest/TagUserByZfb.scala
@@ -1,11 +1,32 @@
 package main.scala.com.cpc.spark.qukan.interest
 
+import java.io.{FileWriter, PrintWriter}
+import java.sql.{DriverManager, ResultSet}
 import java.text.SimpleDateFormat
-import java.util.Calendar
-import scala.util.control._
-import com.redis.RedisClient
+import java.util.{Calendar, Properties}
+
+import com.cpc.spark.common.Utils
+import com.cpc.spark.log.parser.{ExtValue, TraceLog, UnionLog}
+import com.cpc.spark.ml.train.LRIRModel
+import com.cpc.spark.qukan.parser.HdfsParser
+import com.hankcs.hanlp.HanLP
+import com.hankcs.hanlp.corpus.tag.Nature
 import com.typesafe.config.ConfigFactory
-import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
+import org.apache.spark.mllib.feature.{Word2Vec, Word2VecModel}
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession, Row}
+
+import scala.io.Source
+import scala.collection.JavaConversions._
+import scala.collection.mutable
+import scala.util.Random
+import com.redis.serialization.Parse.Implicits._
+import com.redis.RedisClient
+import com.cpc.spark.qukan.parser.HdfsParser
+import userprofile.Userprofile.{InterestItem, UserProfile}
 
 object TagUserByZfb {
   def main(args: Array[String]): Unit = {
