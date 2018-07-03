fc74c2584c97f76b64e58236b538ad1280a4ad6c
mayuntao <mayuntao@aiclk.com>
2018-07-03 21:14:00 +0800

    predictage

--- src/main/scala/com/cpc/spark/qukan/interest/PredictAge.scala
+++ src/main/scala/com/cpc/spark/qukan/interest/PredictAge.scala
@@ -8,6 +8,7 @@ import org.apache.spark.sql.{Row, SparkSession}
 import com.redis.RedisClient
 import com.redis.serialization.Parse.Implicits._
 import userprofile.Userprofile.{InterestItem, UserProfile}
+import scala.util.control._
 
 /**
   * Created by roydong on 11/06/2018.
@@ -33,7 +34,6 @@ object PredictAge {
           val brand = r.getAs[Int]("brand")
           val apps = r.getAs[Seq[Row]]("apps")
 
-
           val size = 1002
           var els = Seq[(Int, Double)]()
           if (apps != null && apps.length > 0) {
@@ -52,7 +52,7 @@ object PredictAge {
           val vec = Vectors.sparse(size.toInt, els)
           (did, vec)
       }
-        .toDF("did", "features")
+      .toDF("did", "features")
 
     println(sample.count())
     sample.rdd.take(10).foreach(println)
@@ -63,22 +63,23 @@ object PredictAge {
     val m = args(2).toDouble
     val conf = ConfigFactory.load()
     val sum = result.rdd.map {
-        r =>
-          val did = r.getAs[String]("did")
-          val score = r.getAs[Vector]("probability").toArray
-          (did, score(1))
-      }
+      r =>
+        val did = r.getAs[String]("did")
+        val score = r.getAs[Vector]("probability").toArray
+        (did, score(1))
+    }
       .filter(x => x._2 > m)
       .mapPartitions {
         p =>
-          var n = 0
-          var n1 = 0
-          var n2 = 0
+          var insert = 0
+          var revert = 0
+          var both_taged = 0
+          var total = 0
           val redis = new RedisClient(conf.getString("redis.host"), conf.getInt("redis.port"))
-
+          val loop = new Breaks
           p.foreach {
             r =>
-              n += 1
+              total += 1
               val key = r._1 + "_UPDATA"
               val buffer = redis.get[Array[Byte]](key).getOrElse(null)
               if (buffer != null) {
@@ -93,44 +94,60 @@ object PredictAge {
                   .setTag(age)
                   .setScore(100)
                 var has = false
-                for (i <- 0 to user.getInterestedWordsCount - 1) {
-                  val w = user.getInterestedWords(i)
-                  if (w.getTag == in.getTag) {
-                    if (!has) {
-                      user.setInterestedWords(i, in)
-                      n1 = n1 + 1
-                      has = true
-                    } else {
-                      //user.removeInterestedWords(i)
+                var conflict = false
+                var age_224 = false
+                var age_225 = false
+                while(idx < user.getInterestedWordsCount) {
+                  var idx = 0
+                  val w = user.getInterestedWords(idx)
+                  if (w.getTag == 224 || w.getTag == 225) {
+                    if (w.getTag == 224) age_224 = true
+                    if (w.getTag == 225) age_225 = true
+                    has = true
+                    if (w.getTag != in.getTag) {
+                      conflict = true
                     }
+                    user.removeInterestedWords(idx)
+                  } else {
+                    idx += 1
+                  }
+                  if (idx == user.getInterestedWordsCount) {
+                    loop.break()
                   }
                 }
+                user.addInterestedWords(in)
                 if (!has) {
-                  user.addInterestedWords(in)
-                  n2 = n2 + 1
+                  insert += 1
+                } else if (age_224 && age_225) {
+                  both_taged += 1
+                } else if (conflict) {
+                  revert += 1
                 }
-                redis.setex(key, 3600 * 24 * 7, user.build().toByteArray)
+                //redis.setex(key, 3600 * 24 * 7, user.build().toByteArray)
               }
           }
-          Seq((0, n), (1, n1), (2, n2)).iterator
+          Seq((0, insert), (1, revert), (2, both_taged), (3, total)).iterator
       }
 
     //统计数据
     var n = 0
     var n1 = 0
     var n2 = 0
+    var n3 = 0
     sum.reduceByKey((x, y) => x + y)
-      .take(3)
+      .take(4)
       .foreach {
         x =>
           if (x._1 == 0) {
             n = x._2
           } else if (x._1 == 1) {
             n1 = x._2
-          } else {
+          } else if (x._1 == 2) {
             n2 = x._2
+          } else {
+            n3 = x._2
           }
       }
-    println("total: %s, hit: %s, update %s".format(n, n1, n2))
+    println("total: %s, insert: %s, revert %s, both_taged %s".format(n3, n, n1, n2))
   }
 }
