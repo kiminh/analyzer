1137ef1d231b6e6cc8fef0acc0b36e68121eccbd
mayuntao <mayuntao@aiclk.com>
2018-07-04 11:58:31 +0800

    redis userprofile

--- src/main/scala/com/cpc/spark/qukan/interest/CheckStudentTag.scala
+++ src/main/scala/com/cpc/spark/qukan/interest/CheckStudentTag.scala
@@ -16,7 +16,7 @@ import org.apache.spark.mllib.feature.{Word2Vec, Word2VecModel}
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession, Row}
+import org.apache.spark.sql.{DataFrame, Row, SaveMode, SparkSession}
 
 import scala.io.Source
 import scala.collection.JavaConversions._
@@ -25,11 +25,11 @@ import scala.util.Random
 import com.redis.serialization.Parse.Implicits._
 import com.redis.RedisClient
 import com.cpc.spark.qukan.parser.HdfsParser
-import userprofile.Userprofile.{InterestItem, UserProfile}
+
 import scala.util.control._
 object CheckStudentTag {
   def main(args: Array[String]): Unit = {
-    val days  = args(0).toInt
+    val uid = args(0).toString
     val spark = SparkSession.builder()
       .appName("Tag user by zfb")
       .enableHiveSupport()
@@ -39,40 +39,13 @@ object CheckStudentTag {
     //cal.add(Calendar.DATE, -1)
     val date = new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime)
     println(date)
-    var stmt =
-      """
-        |SELECT DISTINCT uid,interests
-        |FROM dl_cpc.cpc_union_log
-        |WHERE `date` = "%s" AND isshow = 1 AND (media_appsid = "80000001" OR media_appsid = "80000002")
-        |LIMIT 10000000
-      """.stripMargin.format(date)
-
-    var rs1 = spark.sql(stmt).rdd
-      .map {
-        row =>
-          val uid = row.getString(0)
-          val interests = row.getString(1).split(",")
-          var age_224 = false
-          var age_225 = false
-          interests.foreach {
-            r =>
-              if (r != ""){
-                val tag = r.split("=")(0)
-                if (tag.toInt == 224) {
-                  age_224 = true
-                } else if (tag.toInt == 225) {
-                  age_225 = true
-                }
-              }
-          }
-          if (age_224 && age_225) {
-            ("both_taged", 1)
-          } else {
-            ("both_taged", 0)
-          }
-      }
-        .reduceByKey(_+_)
-    rs1.toLocalIterator.foreach(println)
-
+    val conf = ConfigFactory.load()
+    val redis = new RedisClient(conf.getString("redis.host"), conf.getInt("redis.port"))
+    var key = uid + "_UPDATA"
+    val buffer = redis.get[Array[Byte]](key).orNull
+    if (buffer != null) {
+      val user = UserProfile.parseFrom(buffer).toBuilder
+      println(user)
+    }
   }
 }
