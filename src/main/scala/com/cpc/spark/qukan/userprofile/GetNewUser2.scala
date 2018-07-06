2cc6f7012a56f92c1e1cd2de1c5a6a78b588cfb4
mayuntao <mayuntao@aiclk.com>
2018-07-06 18:00:15 +0800

    7day new user

--- src/main/scala/com/cpc/spark/qukan/userprofile/GetNewUser2.scala
+++ src/main/scala/com/cpc/spark/qukan/userprofile/GetNewUser2.scala
@@ -9,6 +9,7 @@ import com.redis.serialization.Parse.Implicits._
 import com.typesafe.config.ConfigFactory
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
+import userprofile.Userprofile.{Userprofile, InterestItem}
 
 import scala.util.control.Breaks
 
