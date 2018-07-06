66813569b3152fa0b8108fc39c1de9c0fdcf5d7c
Roy Dong <i@roydong.com>
2018-07-06 18:52:55 +0800

    fix bug

--- src/main/scala/com/cpc/spark/qukan/interest/NoClickUser.scala
+++ src/main/scala/com/cpc/spark/qukan/interest/NoClickUser.scala
@@ -1,5 +1,8 @@
 package com.cpc.spark.qukan.interest
 
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
 import com.redis.RedisClient
 import com.redis.serialization.Parse.Implicits._
 import com.typesafe.config.ConfigFactory
@@ -13,21 +16,45 @@ object NoClickUser {
 
   def main(args: Array[String]): Unit = {
     Logger.getRootLogger.setLevel(Level.WARN)
-    val date = args(0)
+    val days = args(0)
     val spark = SparkSession.builder()
-      .appName("no click user >= %s".format(date))
+      .appName("no click user >= %s".format(days))
       .enableHiveSupport()
       .getOrCreate()
     import spark.implicits._
 
-    val stmt =
+    val cal = Calendar.getInstance()
+    cal.add(Calendar.DATE, -3)
+    var date = new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime)
+    var stmt =
       """
-        |select uid, max(isclick) c from dl_cpc.cpc_union_log where `date` >= "%s"
+        |select uid, max(isclick) c from dl_cpc.cpc_union_log where `date` = "%s"
         |and media_appsid in ("80000001", "80000002") group by uid having c = 0
       """.stripMargin.format(date)
-
     println(stmt)
-    val uidRDD = spark.sql(stmt)
+    val uidRDD1 = spark.sql(stmt)
+
+    cal.add(Calendar.DATE, 1)
+    date = new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime)
+    stmt =
+      """
+        |select uid, max(isclick) c from dl_cpc.cpc_union_log where `date` = "%s"
+        |and media_appsid in ("80000001", "80000002") group by uid having c = 0
+      """.stripMargin.format(date)
+    println(stmt)
+    val uidRDD2 = spark.sql(stmt)
+
+    cal.add(Calendar.DATE, 1)
+    date = new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime)
+    stmt =
+      """
+        |select uid, max(isclick) c from dl_cpc.cpc_union_log where `date` = "%s"
+        |and media_appsid in ("80000001", "80000002") group by uid having c = 0
+      """.stripMargin.format(date)
+    println(stmt)
+    val uidRDD3 = spark.sql(stmt)
+
+    val uidRDD = uidRDD1.join(uidRDD2, Seq("uid")).join(uidRDD3, Seq("uid"))
 
     println("num", uidRDD.count())
     println(uidRDD.take(10).mkString(" "))
