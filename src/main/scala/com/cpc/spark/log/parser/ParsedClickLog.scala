262ae8442280303cb59be9c63a44c16a75ca3152
zhanghy <zhanghongyang@aiclk.com>
2018-07-07 19:01:37 +0800

    add show click case class

---
+++ src/main/scala/com/cpc/spark/log/parser/ParsedClickLog.scala
@@ -0,0 +1,41 @@
+package com.cpc.spark.log.parser
+
+/**
+  * click日志
+  *
+  * @param searchid
+  * @param isclick
+  * @param ideaid
+  * @param click_timestamp
+  * @param click_ip
+  * @param ext
+  */
+case class ParsedClickLog(
+                           var searchid: String = "",
+                           var isclick: Int = 0,
+                           var ideaid: Int = 0,
+                           var click_timestamp: Int = 0,
+                           var antispam_score: Int = 0,
+                           var antispam_rules: String = "",
+                           var click_ip: String = "",
+                           var ext: collection.Map[String, ExtValue] = null
+                         ) {
+  def spamClick(): Int = {
+    if (ext != null) {
+      ext.getOrElse("spam_click", ExtValue()).int_value
+    } else {
+      0
+    }
+  }
+
+  def isSpamClick(): Int = {
+    if (antispam_score < 10000 && isclick > 0) {
+      1
+    } else {
+      0
+    }
+  }
+
+}
+
+case class ExtValue(int_value: Int = 0, long_value: Long = 0, float_value: Float = 0, string_value: String = "")
---
+++ src/main/scala/com/cpc/spark/log/parser/ParsedShowLog.scala
@@ -0,0 +1,24 @@
+package com.cpc.spark.log.parser
+
+/**
+  * click日志
+  *
+  * @param searchid
+  * @param isshow
+  * @param ideaid
+  * @param show_timestamp
+  * @param show_ip
+  * @param ext
+  */
+case class ParsedShowLog(
+                         var searchid: String="" ,
+                         var isshow: Int=0 ,
+                         var ideaid: Int = 0,
+                         var show_timestamp: Int = 0 ,
+                         var show_ip: String = "" ,
+                         var ext: collection.Map[String, ExtValue] = null
+                         ){
+
+}
+
+case class ExtValue(int_value: Int = 0, long_value: Long = 0, float_value: Float = 0, string_value: String = "")
