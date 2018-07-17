b5a3ed2494788bf352be0d3978ff336b5771679d
mayuntao <mayuntao@aiclk.com>
2018-07-17 16:32:22 +0800

    daily report

--- src/main/scala/com/cpc/spark/qukan/interest/DailyReport.scala
+++ src/main/scala/com/cpc/spark/qukan/interest/DailyReport.scala
@@ -46,6 +46,26 @@ object DailyReport {
       .getOrCreate()
     import spark.implicits._
 
+    checkUVTag(spark, args)
+
+
+//    val stmt2 =
+//      """
+//        |select uid, ext_int["lx_package"] from dl_cpc.cpc_union_log where `date` = "%s"
+//      """.stripMargin.format(date)
+//
+//    val rs2 = spark.sql(stmt2).rdd.map {
+//      r =>
+//        val did = r.getAs[String](0)
+//        val lx = r.getAs[Long](1)
+//        (did, lx)
+//    }.reduceByKey((x, y) => x)
+//
+//    println(rs.map(x => (x, 1)).join(rs2).filter(x => x._2._2 == 0).count())
+  }
+
+  def qukan_new_user(spark : SparkSession, args : Array[String]): Unit = {
+    val days  = args(0).toInt
     val cal = Calendar.getInstance()
     cal.add(Calendar.DATE, -days)
     val date = new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime)
@@ -113,20 +133,50 @@ object DailyReport {
       }
       .reduceByKey(_+_)
     sum.toLocalIterator.foreach(println)
-
-//    val stmt2 =
-//      """
-//        |select uid, ext_int["lx_package"] from dl_cpc.cpc_union_log where `date` = "%s"
-//      """.stripMargin.format(date)
-//
-//    val rs2 = spark.sql(stmt2).rdd.map {
-//      r =>
-//        val did = r.getAs[String](0)
-//        val lx = r.getAs[Long](1)
-//        (did, lx)
-//    }.reduceByKey((x, y) => x)
-//
-//    println(rs.map(x => (x, 1)).join(rs2).filter(x => x._2._2 == 0).count())
   }
+  def checkUVTag(spark : SparkSession, args : Array[String]): Unit = {
+    val days  = args(0).toInt
+    val cal = Calendar.getInstance()
+    cal.add(Calendar.DATE, -days)
+    val sdate = new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime)
+    cal.add(Calendar.DATE, days)
+    val edate = new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime)
+    val stmt =
+      """
+        |select uid from dl_cpc.cpc_union_log where `date` >= "%s" and `date` < "%s"
+      """.stripMargin.format(sdate,edate)
 
+    val rs = spark.sql(stmt).rdd.map {
+      r =>
+        val did = r.getAs[String](0)
+        (did)
+    }
+    val conf = ConfigFactory.load()
+    val sum = rs.repartition(500)
+      .mapPartitions{
+        p =>
+          var young = 0
+          var notYoung = 0
+          val redis = new RedisClient(conf.getString("redis.host"), conf.getInt("redis.port"))
+          p.foreach {
+            r =>
+                val key = r + "_UPDATA"
+                val buffer = redis.get[Array[Byte]](key).orNull
+                if (buffer != null) {
+                  val user = UserProfile.parseFrom(buffer).toBuilder
+                  for (i <- 0 until user.getInterestedWordsCount) {
+                    val w = user.getInterestedWords(i)
+                    if (w.getTag == 224) {
+                      young += 1
+                    } else if (w.getTag == 225) {
+                      notYoung += 1
+                    }
+                  }
+                }
+          }
+          Seq((0, young), (1, notYoung)).iterator
+      }
+      .reduceByKey(_+_)
+    sum.toLocalIterator.foreach(println)
+  }
 }
