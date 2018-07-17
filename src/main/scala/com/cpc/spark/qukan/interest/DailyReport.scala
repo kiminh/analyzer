52535cef024ae3eff3dc53c60ca3b6521e9fa2a0
mayuntao <mayuntao@aiclk.com>
2018-07-17 10:35:17 +0800

    fix bug

--- src/main/scala/com/cpc/spark/qukan/interest/DailyReport.scala
+++ src/main/scala/com/cpc/spark/qukan/interest/DailyReport.scala
@@ -57,47 +57,70 @@ object DailyReport {
         (did)
     }
     println(rs.count())
+    val zfb = spark.read.parquet("user/cpc/qtt-zfb/10").rdd.map {
+      r =>
+        val did = r.getAs[String]("did")
+        val birth = r.getAs[String]("birth")
+        val age = 2018 - birth.toInt / 10000
+        if (age < 22) {
+          (did, 0)
+        } else {
+          (did, 1)
+        }
+    }
     val conf = ConfigFactory.load()
-    val sum = rs.repartition(500)
+    val sum = rs.map(x => (x, 1)).leftOuterJoin(zfb).repartition(500)
       .mapPartitions{
         p =>
           var young = 0
           var notYoung = 0
+          var zfb_num = 0
+          var pre_num = 0
           val redis = new RedisClient(conf.getString("redis.host"), conf.getInt("redis.port"))
           p.foreach {
             r =>
-              val key = r + "_UPDATA"
-              val buffer = redis.get[Array[Byte]](key).orNull
-              if (buffer != null) {
-                val user = UserProfile.parseFrom(buffer).toBuilder
-                for (i <- 0 until user.getInterestedWordsCount) {
-                  val w = user.getInterestedWords(i)
-                  if (w.getTag == 224) {
-                    young += 1
-                  } else if (w.getTag == 225) {
-                    notYoung += 1
+              if (r._2._2 != None) {
+                if (r._2._2 == 0) {
+                  young += 1
+                } else {
+                  notYoung += 1
+                }
+                zfb_num += 1
+              } else {
+                pre_num += 1
+                val key = r._1 + "_UPDATA"
+                val buffer = redis.get[Array[Byte]](key).orNull
+                if (buffer != null) {
+                  val user = UserProfile.parseFrom(buffer).toBuilder
+                  for (i <- 0 until user.getInterestedWordsCount) {
+                    val w = user.getInterestedWords(i)
+                    if (w.getTag == 224) {
+                      young += 1
+                    } else if (w.getTag == 225) {
+                      notYoung += 1
+                    }
                   }
                 }
               }
           }
-          Seq((0, young), (1, notYoung)).iterator
+          Seq((0, young), (1, notYoung), (2, zfb_num), (3, pre_num)).iterator
       }
       .reduceByKey(_+_)
     sum.toLocalIterator.foreach(println)
 
-    val stmt2 =
-      """
-        |select uid, ext_int["lx_package"] from dl_cpc.cpc_union_log where `date` = "%s"
-      """.stripMargin.format(date)
-
-    val rs2 = spark.sql(stmt2).rdd.map {
-      r =>
-        val did = r.getAs[String](0)
-        val lx = r.getAs[Long](1)
-        (did, lx)
-    }.reduceByKey((x, y) => x)
-
-    println(rs.map(x => (x, 1)).join(rs2).filter(x => x._2._2 == 0).count())
+//    val stmt2 =
+//      """
+//        |select uid, ext_int["lx_package"] from dl_cpc.cpc_union_log where `date` = "%s"
+//      """.stripMargin.format(date)
+//
+//    val rs2 = spark.sql(stmt2).rdd.map {
+//      r =>
+//        val did = r.getAs[String](0)
+//        val lx = r.getAs[Long](1)
+//        (did, lx)
+//    }.reduceByKey((x, y) => x)
+//
+//    println(rs.map(x => (x, 1)).join(rs2).filter(x => x._2._2 == 0).count())
   }
 
 }
