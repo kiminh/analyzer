902d8f425c4091b88c0ef4ce2a1978f8613c0517
mayuntao <mayuntao@aiclk.com>
2018-07-18 17:10:45 +0800

    predict sex

--- src/main/scala/com/cpc/spark/qukan/userprofile/SetUserProfileTag.scala
+++ src/main/scala/com/cpc/spark/qukan/userprofile/SetUserProfileTag.scala
@@ -1,10 +1,13 @@
 package com.cpc.spark.qukan.userprofile
 
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
 import org.apache.spark.ml.classification.LogisticRegressionModel
 import org.apache.spark.ml.linalg.Vectors
 import com.typesafe.config.ConfigFactory
 import org.apache.spark.ml.linalg.{Vector, Vectors}
-import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.{Row, SaveMode, SparkSession}
 import com.redis.RedisClient
 import com.redis.serialization.Parse.Implicits._
 
@@ -12,7 +15,6 @@ import scala.util.control._
 import ml.dmlc.xgboost4j.scala.spark._
 import org.apache.spark.rdd.RDD
 import userprofile.Userprofile.{InterestItem, UserProfile}
-
 import scala.collection.mutable
 
 /**
@@ -31,6 +33,8 @@ import scala.collection.mutable
 object SetUserProfileTag {
   def setUserProfileTag (spark : SparkSession, in : RDD[(String, Int, Boolean)]) : Array[(String, Int)] = {
     import spark.implicits._
+    val cal = Calendar.getInstance()
+    val date = new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime)
     val conf = ConfigFactory.load()
     val sum = in.repartition(500)
       .mapPartitions{
@@ -41,7 +45,7 @@ object SetUserProfileTag {
           var tot = 0
           val redis = new RedisClient(conf.getString("redis.host"), conf.getInt("redis.port"))
           val loop = new Breaks
-          p.foreach{
+          val cnt = p.map{
             x =>
               tot += 1
               val key = x._1 + "_UPDATA"
@@ -74,6 +78,7 @@ object SetUserProfileTag {
                     ins += 1
                   }
                   user.addInterestedWords(interest)
+                  isIns = true
                 } else {
                   if (isDel) {
                     del += 1
@@ -81,15 +86,23 @@ object SetUserProfileTag {
                 }
                 redis.setex(key, 3600 * 24 * 7, user.build().toByteArray)
               }
-          }
-          Seq(("total", tot), ("hit", hit), ("insert", ins), ("delete", del)).iterator
+              if (isIns) {
+                ("uid_num_by_tag_%s".format(x._2), 1)
+              }else if (isDel) {
+                ("uid_num_by_tag_%s".format(x._2), -1)
+              } else {
+                ("uid_num_by_tag_%s".format(x._2), 0)
+              }
+          }.toSeq()
+          (Seq(("total", tot), ("hit", hit), ("insert", ins), ("delete", del)) :+ cnt).iterator
       }.reduceByKey(_+_)
       .sortBy(_._1)
-
+    sum.toDF("name", "sum").write.mode(SaveMode.Append).parquet("/user/cpc/uid-tag-number/%s".format(date))
     sum.toLocalIterator.toArray[(String, Int)]
+
   }
 
-  def TestsetUserProfileTag (spark : SparkSession, in : RDD[(String, Int, Boolean)]) : Array[(String, Int)] = {
+  def testSetUserProfileTag (spark : SparkSession, in : RDD[(String, Int, Boolean)]) : Array[(String, Int)] = {
     import spark.implicits._
     val conf = ConfigFactory.load()
     val sum = in.repartition(500)
