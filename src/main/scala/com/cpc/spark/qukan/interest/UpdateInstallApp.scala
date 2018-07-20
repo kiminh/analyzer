ae691894ee9a6864409e014cc88c04e91fd2233e
mayuntao <mayuntao@aiclk.com>
2018-07-20 19:57:46 +0800

    cpc app

--- src/main/scala/com/cpc/spark/qukan/interest/UpdateInstallApp.scala
+++ src/main/scala/com/cpc/spark/qukan/interest/UpdateInstallApp.scala
@@ -7,7 +7,7 @@ import org.apache.spark.sql.{Row, SaveMode, SparkSession}
 import org.json4s._
 import org.json4s.native.JsonMethods._
 import com.cpc.spark.streaming.tools.{Encoding, Gzip}
-
+import com.cpc.spark.common.Utils
 
 
 /**
@@ -24,8 +24,20 @@ object UpdateInstallApp {
     import spark.implicits._
 
     val cal = Calendar.getInstance()
+    val today = new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime)
     cal.add(Calendar.DATE, -days)
     val date = new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime)
+
+    val qukanApps = spark.read.parquet("/user/cpc/userInstalledApp/%s".format(today)).rdd.map {
+      r =>
+        val uid = r.getAs[String]("uid")
+        val pkgs = r.getAs[Seq[Row]]("pkgs").map(x => x.getAs[String](0))
+        (uid, (Seq(), Seq(), Seq(), pkgs))
+    }
+    println("origin statistic")
+    println(qukanApps.count())
+    println(qukanApps.filter(_._2._3.length > 10).count())
+
     val stmt =
       """
         |select trace_op1, trace_op2, trace_op3 from dl_cpc.cpc_all_trace_log where `date` >= "%s" and trace_type = "%s"
@@ -37,7 +49,7 @@ object UpdateInstallApp {
         val did = r.getAs[String](1)
         val in_b64 = r.getAs[String](2)
         var in : String = ""
-        var apps = Seq[(String, String)]()
+        var apps = Seq[String]()
         if (in_b64 != null) {
           val in_gzip = com.cpc.spark.streaming.tools.Encoding.base64Decoder(in_b64).toArray
           in = Gzip.decompress(in_gzip) match {
@@ -50,29 +62,33 @@ object UpdateInstallApp {
               JObject(pkg) <- pkgs
               JField("name", JString(name)) <- pkg
               JField("package_name", JString(package_name)) <- pkg
-              p = (name, package_name)
+              p = (package_name)
             } yield p
           }
         }
         if (op_type == "APP_LIST_ADD") {
-          (did, (apps, Seq(), Seq()))
+          (did, (apps, Seq(), Seq(), Seq()))
         } else if (op_type == "APP_LIST_REMOVE") {
-          (did, (Seq(), apps, Seq()))
+          (did, (Seq(), apps, Seq(), Seq()))
         } else if (op_type == "APP_LIST_USE"){
-          (did, (Seq(), Seq(), apps))
+          (did, (Seq(), Seq(), apps, Seq()))
+        } else if (op_type == "APP_LIST_INSTALLED") {
+          (did, (Seq(), Seq(), Seq(), apps))
         } else {
           null
         }
     }.filter(_ != null)
-      .reduceByKey((x, y) => (x._1 ++ y._1, x._2 ++ y._2, x._3 ++ y._3))
-      .map(x => (x._1, x._2._3.map(p => p._2).distinct))
-        //.map(x => (x._1, x._2._1.distinct, x._2._2.distinct, x._2._3.distinct))
+      .union(qukanApps)
+      .reduceByKey((x, y) => ((x._1 ++ y._1).distinct, (x._2 ++ y._2).distinct, (x._3 ++ y._3).distinct, (x._4 ++ y._4).distinct))
 
+
+
+        //.map(x => (x._1, x._2._1.distinct, x._2._2.distinct, x._2._3.distinct))
     all_list.take(10).foreach(println)
-//    println(all_list.count())
-//    println(all_list.filter(x => x._4.length > 5).count())
-//    println(all_list.filter(x => x._4.length > 10).count())
-    all_list.toDF("uid", "pkgs").write.mode(SaveMode.Overwrite).parquet("/user/cpc/traceInstalledApp/%s".format(days))
+    println(all_list.count())
+    println(all_list.filter(x => x._2._4.length > 5).count())
+    println(all_list.filter(x => x._2._4.length > 10).count())
+    all_list.map(x => (x._1, x._2._1, x._2._2, x._2._3, x._2._4)).toDF("uid", "add_pkgs", "remove_pkgs", "used_pkgs", "pkgs").write.mode(SaveMode.Overwrite).parquet("/user/cpc/traceInstalledApp/%s".format(days))
 
 
   }
