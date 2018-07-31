3d5a20e44bba8562db1ab0de978067580d6e3fda
mayuntao <mayuntao@aiclk.com>
2018-07-31 19:57:06 +0800

    fix bug

--- src/main/scala/com/cpc/spark/qukan/interest/DailyReport.scala
+++ src/main/scala/com/cpc/spark/qukan/interest/DailyReport.scala
@@ -238,7 +238,7 @@ object DailyReport {
     val sample = spark.read.parquet("/user/cpc/qtt-age-sample/p1").rdd.map{
       x =>
         if (x(1) != null && x(2) != null) {
-          (x.getAs[Int](1), x.getAs[Seq[String]](2).size)
+          (x.getAs[Int](1), x.getAs[Seq[String]](2).size, x.getAs[Seq[String]](2))
         } else {
           null
         }
@@ -247,6 +247,21 @@ object DailyReport {
     println(sample.filter(x =>x._1 >= 22  && x._2 > 10).count())
     println(sample.filter(x =>x._1 < 22).count())
     println(sample.filter(x =>x._1 < 22  && x._2 > 10).count())
+    sample.filter(_._1 < 22).flatMap{
+      x =>
+        x._3.map{
+          x =>
+            (x, 1)
+        }
+    }.reduceByKey(_+_).sortBy(_._2, false).take(100).foreach(println)
+    println("===============================================")
+    sample.filter(_._1 >= 22).flatMap{
+      x =>
+        x._3.map{
+          x =>
+            (x, 1)
+        }
+    }.reduceByKey(_+_).sortBy(_._2, false).take(100).foreach(println)
   }
 
 }
