e14a96b35bf5aedcd95b7c0a1e367850dca4149c
mayuntao <mayuntao@aiclk.com>
2018-08-17 17:22:12 +0800

    fix bug

--- src/main/scala/com/cpc/spark/qukan/userprofile/SetUserProfileTag.scala
+++ src/main/scala/com/cpc/spark/qukan/userprofile/SetUserProfileTag.scala
@@ -236,7 +236,7 @@ object SetUserProfileTag {
     val rs = ft.map{
       tag =>
         in.filter(_._2 == tag).map{x => (x._1, x._3)}.toDF("uid", "operation").coalesce(20)
-          .write.mode(SaveMode.Overwrite).parquet("/warehouse/dl_cpc.db/cpc_userprofile_tag_daily/%s/%s".format(date, tag))
+          .write.mode(SaveMode.Append).parquet("/warehouse/dl_cpc.db/cpc_userprofile_tag_daily/%s/%s".format(date, tag))
         val sql =
           """
             |ALTER TABLE dl_cpc.cpc_userprofile_tag_daily add if not exists PARTITION (`date` = "%s" , `tag` = "%s")  LOCATION
