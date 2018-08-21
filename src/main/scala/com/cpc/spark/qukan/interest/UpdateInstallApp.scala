30c0471b8678b07c875121701090e1fcf4029a00
mayuntao <mayuntao@aiclk.com>
2018-08-21 20:38:39 +0800

    exception

--- src/main/scala/com/cpc/spark/qukan/interest/UpdateInstallApp.scala
+++ src/main/scala/com/cpc/spark/qukan/interest/UpdateInstallApp.scala
@@ -100,118 +100,118 @@ object UpdateInstallApp {
           null
         }
     }.filter(_ != null)
-      .union(qukanApps)
-      .reduceByKey((x, y) => ((x._1 ++ y._1).distinct, (x._2 ++ y._2).distinct, (x._3 ++ y._3).distinct, (x._4 ++ y._4).distinct))
-
-
-    var old: RDD[(String, (List[String], Int))] = null
-    try {
-      old = spark.read
-        .parquet(outpath)
-        .rdd
-        .map {
-          x =>
-            (x.getAs[String]("uid"), (x.getAs[Seq[String]]("pkgs").toList, 0))
-        }
-      println("old", old.count())
-    } catch {
-      case e: Exception =>
-        println("old", 0)
-    }
-
-    //标记出老数据
-    var pkgs = all_list.map(x => (x._1, (x._2._4, 1)))
-    if (old != null) {
-      pkgs = pkgs.union(old)
-        .reduceByKey {
-          (x, y) =>
-            if (x._2 == 0) {
-              x
-            } else {
-              y
-            }
-        }
-    }
-    val added = pkgs.filter(_._2._2 == 1)
-    println("new", added.count())
-
-    //保存新增数据 redis
-    val sum = added.map(x => (x._1, x._2._1))
-      .repartition(100)
-      .mapPartitions {
-        p =>
-          var n = 0
-          var n1 = 0
-          var n2 = 0
-          var n3 = 0
-          val conf = ConfigFactory.load()
-          val redis = new RedisClient(conf.getString("redis.host"), conf.getInt("redis.port"))
-          val sec = new Date().getTime / 1000
-          p.foreach {
-            x =>
-              n += 1
-              val key = x._1 + "_UPDATA"
-              val buffer = redis.get[Array[Byte]](key).getOrElse(null)
-
-              var user: UserProfile.Builder = null
-              if (buffer == null) {
-                user = UserProfile.newBuilder()
-                n3 = n3 + 1
-              } else {
-                user = UserProfile.parseFrom(buffer).toBuilder
-              }
-
-              //判断老数据
-              if (user.getInstallpkgCount > 0) {
-                val pkg = user.getInstallpkg(0)
-                //更新时间大于一天
-                if (sec > pkg.getLastUpdateTime + 60 * 60 * 24) {
-                  user.clearInstallpkg()
-                } else {
-                  n1 += 1
-                }
-              }
-
-              if (user.getInstallpkgCount == 0) {
-                x._2.foreach {
-                  n =>
-                    val pkg = APPPackage.newBuilder().setPackagename(n).setLastUpdateTime(sec)
-                    user.addInstallpkg(pkg)
-                }
-                redis.setex(key, 3600 * 24 * 7, user.build().toByteArray)
-                n2 += 1
-              }
-          }
-          Seq(("pass", n1), ("update", n2), ("new", n3)).iterator
-      }
-      .reduceByKey(_ + _)
-      .take(10)
-    println("update redis")
-    sum.foreach(println)
-
-
-
-    println(all_list.map(x => (x._2._1.length, x._2._2.length, x._2._3.length, x._2._4.length))
-        .reduce((x, y) => (x._1 + y._1, x._2 + y._2, x._3 + y._3, x._4 + y._4)))
-
-    all_list.flatMap(x => x._2._3).map{x => (x, 1)}.reduceByKey(_+_).sortBy(_._2, false)
-        .take(50).foreach(println)
-
-
-    all_list.take(10).foreach(println)
-    println(all_list.count())
-    println(all_list.filter(x => x._2._4.length > 5).count())
-    println(all_list.filter(x => x._2._4.length > 10).count())
-    println(all_list.filter(x => x._2._3.size > 0).count())
-    all_list.map(x => (x._1, x._2._4, x._2._1, x._2._2, x._2._3, date)).toDF("uid", "pkgs", "add_pkgs", "remove_pkgs", "used_pkgs", "load_date").coalesce(100).write.mode(SaveMode.Overwrite).parquet("/user/cpc/userInstalledApp/%s".format(date))
-
-    val sql =
-      """
-        |ALTER TABLE dl_cpc.cpc_user_installed_apps add if not exists PARTITION (load_date = "%s" )  LOCATION
-        |       '/user/cpc/userInstalledApp/%s'
-        |
-                """.stripMargin.format(date, date)
-    spark.sql(sql)
+//      .union(qukanApps)
+//      .reduceByKey((x, y) => ((x._1 ++ y._1).distinct, (x._2 ++ y._2).distinct, (x._3 ++ y._3).distinct, (x._4 ++ y._4).distinct))
+//
+//
+//    var old: RDD[(String, (List[String], Int))] = null
+//    try {
+//      old = spark.read
+//        .parquet(outpath)
+//        .rdd
+//        .map {
+//          x =>
+//            (x.getAs[String]("uid"), (x.getAs[Seq[String]]("pkgs").toList, 0))
+//        }
+//      println("old", old.count())
+//    } catch {
+//      case e: Exception =>
+//        println("old", 0)
+//    }
+//
+//    //标记出老数据
+//    var pkgs = all_list.map(x => (x._1, (x._2._4, 1)))
+//    if (old != null) {
+//      pkgs = pkgs.union(old)
+//        .reduceByKey {
+//          (x, y) =>
+//            if (x._2 == 0) {
+//              x
+//            } else {
+//              y
+//            }
+//        }
+//    }
+//    val added = pkgs.filter(_._2._2 == 1)
+//    println("new", added.count())
+//
+//    //保存新增数据 redis
+//    val sum = added.map(x => (x._1, x._2._1))
+//      .repartition(100)
+//      .mapPartitions {
+//        p =>
+//          var n = 0
+//          var n1 = 0
+//          var n2 = 0
+//          var n3 = 0
+//          val conf = ConfigFactory.load()
+//          val redis = new RedisClient(conf.getString("redis.host"), conf.getInt("redis.port"))
+//          val sec = new Date().getTime / 1000
+//          p.foreach {
+//            x =>
+//              n += 1
+//              val key = x._1 + "_UPDATA"
+//              val buffer = redis.get[Array[Byte]](key).getOrElse(null)
+//
+//              var user: UserProfile.Builder = null
+//              if (buffer == null) {
+//                user = UserProfile.newBuilder()
+//                n3 = n3 + 1
+//              } else {
+//                user = UserProfile.parseFrom(buffer).toBuilder
+//              }
+//
+//              //判断老数据
+//              if (user.getInstallpkgCount > 0) {
+//                val pkg = user.getInstallpkg(0)
+//                //更新时间大于一天
+//                if (sec > pkg.getLastUpdateTime + 60 * 60 * 24) {
+//                  user.clearInstallpkg()
+//                } else {
+//                  n1 += 1
+//                }
+//              }
+//
+//              if (user.getInstallpkgCount == 0) {
+//                x._2.foreach {
+//                  n =>
+//                    val pkg = APPPackage.newBuilder().setPackagename(n).setLastUpdateTime(sec)
+//                    user.addInstallpkg(pkg)
+//                }
+//                redis.setex(key, 3600 * 24 * 7, user.build().toByteArray)
+//                n2 += 1
+//              }
+//          }
+//          Seq(("pass", n1), ("update", n2), ("new", n3)).iterator
+//      }
+//      .reduceByKey(_ + _)
+//      .take(10)
+//    println("update redis")
+//    sum.foreach(println)
+//
+//
+//
+//    println(all_list.map(x => (x._2._1.length, x._2._2.length, x._2._3.length, x._2._4.length))
+//        .reduce((x, y) => (x._1 + y._1, x._2 + y._2, x._3 + y._3, x._4 + y._4)))
+//
+//    all_list.flatMap(x => x._2._3).map{x => (x, 1)}.reduceByKey(_+_).sortBy(_._2, false)
+//        .take(50).foreach(println)
+//
+//
+//    all_list.take(10).foreach(println)
+//    println(all_list.count())
+//    println(all_list.filter(x => x._2._4.length > 5).count())
+//    println(all_list.filter(x => x._2._4.length > 10).count())
+//    println(all_list.filter(x => x._2._3.size > 0).count())
+//    all_list.map(x => (x._1, x._2._4, x._2._1, x._2._2, x._2._3, date)).toDF("uid", "pkgs", "add_pkgs", "remove_pkgs", "used_pkgs", "load_date").coalesce(100).write.mode(SaveMode.Overwrite).parquet("/user/cpc/userInstalledApp/%s".format(date))
+//
+//    val sql =
+//      """
+//        |ALTER TABLE dl_cpc.cpc_user_installed_apps add if not exists PARTITION (load_date = "%s" )  LOCATION
+//        |       '/user/cpc/userInstalledApp/%s'
+//        |
+//                """.stripMargin.format(date, date)
+//    spark.sql(sql)
 
 //    val yest = spark.read.parquet("/user/cpc/traceInstalledApp/%s".format(yesterday)).rdd.map {
 //      r =>
