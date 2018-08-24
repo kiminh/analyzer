5220369238f8092d2b9f0925c777864dc4d39a88
zhanghy <zhanghongyang@aiclk.com>
2018-08-24 19:07:01 +0800

    TopCtrIdeaV2

--- src/main/scala/com/cpc/spark/log/anal/TopCtrIdeaV2.scala
+++ src/main/scala/com/cpc/spark/log/anal/TopCtrIdeaV2.scala
@@ -207,7 +207,7 @@ object TopCtrIdeaV2 {
     var topIdeaData = mutable.Seq[TopIdea]()
     var topIdeaRDD2: Seq[TopIdea] = Seq()
 
-    for (i <- 0 until adslot_type.length) {
+    for (i <- adslot_type) {
       val size = (80000 * (rate_map.getOrElse[Double](i, 0.0))).toInt //要取元素个数
       val size2 = adslot_type_map.getOrElse[Int](i, 0) //每个adslot_type总元素个数
 
@@ -215,15 +215,15 @@ object TopCtrIdeaV2 {
 
       //如果要取元素个数小于总元素个数，或总元素个数小于500，取所有
       if (size > size2 && size2 < 500) {
-        topIdeaRDD2 = topIdeaRDD.filter(x => x.adslot_type == adslot_type(i))
+        topIdeaRDD2 = topIdeaRDD.filter(x => x.adslot_type == i)
           .sortWith(_.ctr_score > _.ctr_score).take(size2)
       } else {
-        topIdeaRDD2 = topIdeaRDD.filter(x => x.adslot_type == adslot_type(i))
+        topIdeaRDD2 = topIdeaRDD.filter(x => x.adslot_type == i)
           .sortWith(_.ctr_score > _.ctr_score).take(size)
       }
 
 
-      println("adslot_type=" + adslot_type(i) + "已取出 " + topIdeaRDD2.length + " 条")
+      println("adslot_type=" + i + "已取出 " + topIdeaRDD2.length + " 条")
       topIdeaData = topIdeaData ++ topIdeaRDD2
     }
 
