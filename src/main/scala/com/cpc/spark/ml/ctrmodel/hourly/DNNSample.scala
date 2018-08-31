f6768c98409962c11960a47e08d31f9e3f376032
dongjiaqi@aiclk.com <dongjiaqi@aiclk.com>
2018-08-31 12:32:16 +0800

    - test

--- src/main/scala/com/cpc/spark/ml/ctrmodel/hourly/DNNSample.scala
+++ src/main/scala/com/cpc/spark/ml/ctrmodel/hourly/DNNSample.scala
@@ -72,7 +72,8 @@ object DNNSample {
 
     println(sql)
 
-    val sample0 = spark.sql(sql).limit(100000000)
+    val sample0 = spark.sql(sql)
+      //.limit(100000000)
     getStrMapByDataset(spark, uidMap, "uid", sample0)
 
     println(s"max index = $currentMaxIdx")
