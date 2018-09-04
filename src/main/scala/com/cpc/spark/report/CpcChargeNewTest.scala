8bc9dc8ec0914561bea528f2507c10364e8fc89b
zhanghy <zhanghongyang@aiclk.com>
2018-09-04 19:44:05 +0800

    test

---
+++ src/main/scala/com/cpc/spark/report/CpcChargeNewTest.scala
@@ -0,0 +1,127 @@
+package com.cpc.spark.report
+
+import java.text.SimpleDateFormat
+
+import com.cpc.spark.common.LogData
+import kafka.common.TopicAndPartition
+import kafka.message.MessageAndMetadata
+import kafka.serializer.{DefaultDecoder, StringDecoder}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.dstream.InputDStream
+import org.apache.spark.streaming.kafka.KafkaCluster.LeaderOffset
+import org.apache.spark.streaming.kafka.{KafkaCluster, KafkaUtils}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+/**
+  * Created on ${Date} ${Time}
+  */
+object CpcChargeNewTest {
+
+  def main(args: Array[String]): Unit = {
+    Logger.getRootLogger.setLevel(Level.WARN)
+    val Array(brokers, topics, seconds) = args
+
+    val sparkConf = new SparkConf().setAppName("srcLog: topics = " + topics)
+    val ssc = new StreamingContext(sparkConf, Seconds(seconds.toInt))
+
+    val topicsSet = topics.split(",").toSet
+    val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
+
+
+    var kafkaoffset: Map[TopicAndPartition, Long] = null
+    kafkaoffset = getKafkaTopicAndPartitionOffset(topicsSet, kafkaParams)
+    println("***************from kafka offsets*******************")
+    println(kafkaoffset)
+
+    val messageHandler = (mmd: MessageAndMetadata[String, Array[Byte]]) => (mmd.topic, mmd.message())
+    var messages: InputDStream[(String, Array[Byte])] = null
+    if (kafkaoffset == null || kafkaoffset.isEmpty) {
+      messages = KafkaUtils.createDirectStream[String, Array[Byte], StringDecoder, DefaultDecoder](ssc, kafkaParams, topicsSet)
+      println("no offset")
+    } else {
+      messages = KafkaUtils.createDirectStream[String, Array[Byte], StringDecoder, DefaultDecoder, (String, Array[Byte])](ssc, kafkaParams, kafkaoffset, messageHandler)
+      println("from offset")
+    }
+
+
+    val base_data = messages.map {
+      case (k, v) =>
+        try {
+          val logdata = LogData.parseData(v)
+          val log_timestamp = logdata.log.getLogTimestamp
+          val date = new SimpleDateFormat("yyyy-MM-dd").format(log_timestamp)
+          val hour = new SimpleDateFormat("HH").format(log_timestamp)
+          val minute = new SimpleDateFormat("mm").format(log_timestamp).charAt(0) + "0"
+
+          val field2 = scala.collection.mutable.Map[String, ExtValue]()
+          val fieldCount = logdata.log.getField.getMapCount
+          for (i <- 0 until fieldCount) {
+            val field = logdata.log.getField.getMap(i)
+            val extValue = ExtValue(field.getValue.getIntType, field.getValue.getLongType, field.getValue.getFloatType, field.getValue.getStringType)
+            field2 += (field.getKey -> extValue)
+
+          }
+
+          val field: collection.Map[String, ExtValue] = field2.toMap
+          SrcLog(logdata.log.getLogTimestamp, logdata.log.getIp, field, date, hour, minute)
+        } catch {
+          case t: Throwable =>
+            t.printStackTrace() // TODO: handle error
+            null
+        }
+    }
+      .filter(_ != null)
+    base_data.print()
+
+
+    base_data.foreachRDD {
+      rs =>
+        println("################################")
+        rs.take(10).foreach(x => println(x))
+    }
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+
+  def getKafkaTopicAndPartitionOffset(topicsSet: Set[String], kafkaParams: Map[String, String]): Map[TopicAndPartition, Long] = {
+    var kafkaCluster = new KafkaCluster(kafkaParams)
+    var topicAndPartitions = Set[TopicAndPartition]()
+    var fromOffsets: Map[TopicAndPartition, Long] = Map()
+    val partitions: Either[KafkaCluster.Err, Set[TopicAndPartition]] = kafkaCluster.getPartitions(topicsSet)
+    partitions match {
+      case Left(x) => System.err.println("kafka getPartitions error" + x); System.exit(1)
+      case Right(x) =>
+        for (partition <- x) {
+          topicAndPartitions += partition
+        }
+    }
+    println("***************from kafka TopicAndPartition*******************")
+    println(topicAndPartitions)
+    val consumerOffset: Either[KafkaCluster.Err, Map[TopicAndPartition, LeaderOffset]] = kafkaCluster.getLatestLeaderOffsets(topicAndPartitions)
+    consumerOffset match {
+      case Left(x) => System.err.println("kafka getConsumerOffsets error" + x); System.exit(1)
+      case Right(x) =>
+        x.foreach(
+          tp => {
+            fromOffsets += (tp._1 -> tp._2.offset)
+          }
+        )
+    }
+    fromOffsets
+  }
+
+  case class SrcLog(
+                     log_timestamp: Long = 0,
+                     ip: String = "",
+                     field: collection.Map[String, ExtValue] = null,
+                     thedate: String = "",
+                     thehour: String = "",
+                     theminute: String = ""
+                   )
+
+  case class ExtValue(int_type: Int = 0, long_type: Long = 0, float_type: Float = 0, string_type: String = "")
+
+
+}
