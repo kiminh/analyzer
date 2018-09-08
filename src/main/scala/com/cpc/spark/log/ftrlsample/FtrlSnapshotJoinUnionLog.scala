164e69076cee8925d92fb1aebf4d425257fc3c60
dongjiaqi@aiclk.com <dongjiaqi@aiclk.com>
2018-09-08 18:34:01 +0800

    - ftrl snapshot

---
+++ src/main/scala/com/cpc/spark/log/ftrlsample/FtrlSnapshotJoinUnionLog.scala
@@ -0,0 +1,32 @@
+package com.cpc.spark.log.ftrlsample
+
+import org.apache.spark.sql.SparkSession
+
+object FtrlSnapshotJoinUnionLog {
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder()
+      .enableHiveSupport()
+      .getOrCreate()
+
+    val dt = args(0)
+    val hour = args(1)
+
+
+    val snapshot1 = spark.table("dl_cpc.ml_snapshot_from_show")
+      .filter(s"dt = '$dt' and hour = '$hour'")
+
+    val unionlog1 = spark.table("dl_cpc.cpc_union_log")
+      .filter(s"dt = '$dt' and hour = '$hour'")
+
+    val join = unionlog1.join(snapshot1, Seq("searchid"), "left")
+        .filter("feature_vector is not null")
+
+    println(s"snapshot1 count = ${snapshot1.count()}")
+    println(s"unionlog1 count = ${unionlog1.count()}")
+    println(s"join count = ${join.count()}")
+
+  }
+
+}
