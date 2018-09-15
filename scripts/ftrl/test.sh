97020e0e86588ccffa436db3bc5a8922d85f0e91
dongjiaqi@aiclk.com <dongjiaqi@aiclk.com>
2018-09-15 21:38:17 +0800

    - new

--- scripts/ftrl/test.sh
+++ scripts/ftrl/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sh testFtrlNew.sh 2018-09-15 10 list-ctr 9 19 1 0
-sh testFtrlNew.sh 2018-09-15 10 content-ctr 9 19 1 0
-sh testFtrlNew.sh 2018-09-15 10 interact-ctr 9 19 1 0
-sh testFtrlNew.sh 2018-09-15 10 all-cvr 9 19 1 0
+sh testFtrlNew.sh 2018-09-15 10 list-ctr 9 19 1 1
+sh testFtrlNew.sh 2018-09-15 10 content-ctr 9 19 1 1
+sh testFtrlNew.sh 2018-09-15 10 interact-ctr 9 19 1 1
+sh testFtrlNew.sh 2018-09-15 10 all-cvr 9 19 1 1
