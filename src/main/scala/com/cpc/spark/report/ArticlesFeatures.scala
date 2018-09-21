cf04cb3755cc17db2daf6517eb7ca3c7af77b515
zhanghy <zhanghongyang@aiclk.com>
2018-09-21 21:11:12 +0800

    article features

--- src/main/scala/com/cpc/spark/report/ArticlesFeatures.scala
+++ src/main/scala/com/cpc/spark/report/ArticlesFeatures.scala
@@ -50,7 +50,7 @@ object ArticlesFeatures {
       """.stripMargin
     println("sql" + sql)
 
-    val rdd1=spark.sql(sql)
+    spark.sql(sql)
       .repartition(20)
       .rdd
       .map {
@@ -60,14 +60,10 @@ object ArticlesFeatures {
           val detail = x.getAs[String]("detail")
           (doc_id, title, detail, getDocFeature(doc_id))
       }
-    println(rdd1.take(3).foreach(x=>println("##: "+x)))
-      val rdd2=rdd1
       .filter(_._4 != null)
       .map { x =>
         (x._1, x._2, x._3, (for (i <- x._4) yield i.featureName -> i.featureValue).toMap)
       }
-      println(rdd2.take(3).foreach(x=>println("~~: "+x)))
-      rdd2
       .toDF("doc_id", "title", "detail", "features")
       .write.mode("overwrite")
       .orc(s"/warehouse/dl_cpc.db/cpc_doc_features_from_algo/load_date=$date")
