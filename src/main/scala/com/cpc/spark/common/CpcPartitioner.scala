dfde6e066f1fd666df7b6e6cfdec0b6a40b19778
Roy Dong <i@roydong.com>
2018-09-25 18:36:13 +0800

    fix bug

--- src/main/scala/com/cpc/spark/common/CpcPartitioner.scala
+++ src/main/scala/com/cpc/spark/common/CpcPartitioner.scala
@@ -7,8 +7,8 @@ import scala.util.hashing.MurmurHash3
 class CpcPartitioner(num: Int) extends Partitioner {
   override def numPartitions: Long = num.toLong
 
-  override def getPartition(key: Any): Long = {
+  override def getPartition(key: Any): Int = {
     val k = key.asInstanceOf[(String, Int)]
-    (Murmur3Hash.stringHash(k._1) % numPartitions + numPartitions + k._2) % numPartitions
+    ((Murmur3Hash.stringHash(k._1) % numPartitions + numPartitions + k._2) % numPartitions).toInt
   }
 }
