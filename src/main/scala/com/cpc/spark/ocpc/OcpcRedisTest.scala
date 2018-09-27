0847da78a73baa72bde23274268a1e9013d23419
wangjun02 <wangjun@qutoutiao.net>
2018-09-27 21:48:28 +0800

    -

---
+++ src/main/scala/com/cpc/spark/ocpc/OcpcRedisTest.scala
@@ -0,0 +1,53 @@
+package com.cpc.spark.ocpc
+
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.zip
+
+import com.redis.RedisClient
+import com.redis.serialization.Parse.Implicits._
+import com.typesafe.config.ConfigFactory
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.functions._
+import userprofile.Userprofile.UserProfile
+
+import scala.collection.mutable.ListBuffer
+//import UseridDataOcpc._
+//import userprofile.Userprofile.SingleUser2
+//import userprofile.Userprofile.{SingleUser, UserOcpc}
+import userocpc.userocpc._
+//import userocpc.User
+import java.io.FileOutputStream
+
+import org.apache.spark.TaskContext
+
+
+object OcpcRedisTest {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().enableHiveSupport().getOrCreate()
+
+    // calculate time period for historical data
+    val key = args(0)
+
+    val kValue = key + "_UPDATA"
+    testPbRedis(kValue)
+  }
+
+
+  def testPbRedis(key: String): Unit = {
+    println("testPbRedis function: " + key)
+    val conf = ConfigFactory.load()
+    val redis = new RedisClient(conf.getString("redis.host"), conf.getInt("redis.port"))
+    val buffer = redis.get[Array[Byte]](key).orNull
+    if (buffer != null) {
+      var user = UserProfile.parseFrom(buffer)
+      println(user.getAge)
+      println(user.getCtrcnt)
+      println(user.getCvrcnt)
+      //      redis.setex(key, 3600 * 24 * 7, user.build().toByteArray)
+    }
+    redis.disconnect
+  }
+}
+
+
