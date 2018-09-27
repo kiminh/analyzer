09619025a388b8632e579d908fe6caaee4175396
wangjun02 <wangjun@qutoutiao.net>
2018-09-27 22:33:50 +0800

    -

--- src/main/scala/com/cpc/spark/ocpc/OcpcSampleToRedis.scala
+++ src/main/scala/com/cpc/spark/ocpc/OcpcSampleToRedis.scala
@@ -68,6 +68,7 @@ object OcpcSampleToRedis {
     val uidData = base
       .groupBy("uid")
       .agg(sum("ctr_cnt").alias("ctr_cnt"), sum("cvr_cnt").alias("cvr_cnt"))
+      .withColumn("data", concat_ws(",", col("ctr_cnt"), col("cvr_cnt")))
 //      .filter("ctr_cnt>0")
 //      .limit(20)
 
@@ -117,30 +118,34 @@ object OcpcSampleToRedis {
 //    val returnValue = resultList.mkString(",")
 //    println(returnValue)
 
-
     dataset.write.mode("overwrite").saveAsTable("test.test_redis_table_20180927")
-    dataset.foreachPartition(iterator => {
-        val conf = ConfigFactory.load()
+    val conf = ConfigFactory.load()
+    val data = dataset.select("uid", "data").rdd.map(row => (row.get(0), row.get(1)))
+    data.foreachPartition(iterator => {
+
         val redis = new RedisClient(conf.getString("redis.host"), conf.getInt("redis.port"))
 
-        iterator.foreach(record => {
-          val uid = record.get(0).toString
-          var key = uid + "_UPDATA"
-//          cnt.add(1)
-          val ctrCnt = record.get(1).toString
-          val cvrCnt = record.get(2).toString
-          val buffer = redis.get[Array[Byte]](key).orNull
-          var user: UserProfile.Builder = null
-          if (buffer != null) {
-            user = UserProfile.parseFrom(buffer).toBuilder
-            val u = user.build()
-            if (u.getCtrcnt != ctrCnt)
-              user.setCtrcnt(ctrCnt)
-            if (u.getCvrcnt != cvrCnt)
-              user.setCvrcnt(cvrCnt)
-            redis.setex(key, 3600 * 24 * 7, user.build().toByteArray)
+        iterator.foreach{
+          case(uid: String, data: String) => {
+            //          val uid = record.get(0).toString
+            var key = uid + "_UPDATA"
+            //          cnt.add(1)
+            val cnts = data.split(",")
+            val ctrCnt = cnts(0)
+            val cvrCnt = cnts(1)
+            val buffer = redis.get[Array[Byte]](key).orNull
+            var user: UserProfile.Builder = null
+            if (buffer != null) {
+              user = UserProfile.parseFrom(buffer).toBuilder
+              val u = user.build()
+              if (u.getCtrcnt != ctrCnt)
+                user.setCtrcnt(ctrCnt)
+              if (u.getCvrcnt != cvrCnt)
+                user.setCvrcnt(cvrCnt)
+              redis.setex(key, 3600 * 24 * 7, user.build().toByteArray)
+            }
           }
-        })
+        }
         redis.disconnect
       })
 //    println("####################2")
