b1216668be426de5ea33137903c16252e6fafcb4
wangjun02 <wangjun@qutoutiao.net>
2018-09-28 12:54:37 +0800

    -

--- src/main/scala/com/cpc/spark/ocpc/OcpcRedisTest.scala
+++ src/main/scala/com/cpc/spark/ocpc/OcpcRedisTest.scala
@@ -16,16 +16,19 @@ object OcpcRedisTest {
 
     // calculate time period for historical data
     val randSeed = args(0).toInt
-    val dataset = spark.table("test.uid_userporfile_ctr_cvr").orderBy(rand(randSeed)).limit(20)
-    for (row <- dataset.collect()) {
-      val key = row.get(0).toString
-      val ctrCnt = row.getLong(1)
-      val cvrCnt = row.getLong(2)
-//      val data = row.get(3).toString
-      val kValue = key + "_UPDATA"
-      println(s"$key, $ctrCnt, $cvrCnt")
-      testPbRedis(kValue)
-    }
+    val totalData = spark.table("test.uid_userporfile_ctr_cvr")
+
+//    val dataset = totalData.orderBy(rand(randSeed)).limit(20)
+//    for (row <- dataset.collect()) {
+//      val key = row.get(0).toString
+//      val ctrCnt = row.getLong(1)
+//      val cvrCnt = row.getLong(2)
+//      val kValue = key + "_UPDATA"
+//      println(s"$key, $ctrCnt, $cvrCnt")
+//      testPbRedis(kValue)
+//    }
+
+    testPbRedisTotal(totalData, spark)
 
   }
 
@@ -45,6 +48,57 @@ object OcpcRedisTest {
       //      redis.setex(key, 3600 * 24 * 7, user.build().toByteArray)
     }
     redis.disconnect
+  }
+
+
+
+  def testPbRedisTotal(dataset: Dataset[Row], spark: SparkSession): Unit = {
+    var cnt = spark.sparkContext.longAccumulator
+    var cvrResultAcc = spark.sparkContext.longAccumulator
+    var ctrResultAcc = spark.sparkContext.longAccumulator
+    println("###############1")
+    println(s"accumulator before partition loop")
+    println(cnt)
+    println(ctrResultAcc)
+    println(cvrResultAcc)
+    val conf = ConfigFactory.load()
+
+    dataset.foreachPartition(iterator => {
+
+      val redis = new RedisClient(conf.getString("redis.host"), conf.getInt("redis.port"))
+
+      iterator.foreach{
+        record => {
+          val uid = record.get(0).toString
+          var key = uid + "_UPDATA"
+          val ctrCnt = record.getLong(1)
+          val cvrCnt = record.getLong(2)
+
+          val buffer = redis.get[Array[Byte]](key).orNull
+          var user: UserProfile.Builder = null
+          if (buffer != null) {
+            cnt.add(1)
+            user = UserProfile.parseFrom(buffer).toBuilder
+            val currentCtr = user.getCtrcnt
+            val currentCvr = user.getCvrcnt
+            if (currentCtr == ctrCnt) {
+              ctrResultAcc.add(1)
+            }
+            if (currentCvr == cvrCnt) {
+              cvrResultAcc.add(1)
+            }
+          }
+        }
+      }
+      redis.disconnect
+    })
+
+
+    println("####################2")
+    println(s"accumulator before partition loop")
+    println(cnt)
+    println(ctrResultAcc)
+    println(cvrResultAcc)
   }
 }
 
