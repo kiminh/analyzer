21b8e1bdff0989f1b9574253a4ee835681c68615
wangjun02 <wangjun@qutoutiao.net>
2018-09-28 10:30:12 +0800

    -

--- src/main/scala/com/cpc/spark/ocpc/OcpcRedisTest.scala
+++ src/main/scala/com/cpc/spark/ocpc/OcpcRedisTest.scala
@@ -49,10 +49,10 @@ object OcpcRedisTest {
     var user: UserProfile.Builder = null
     if (buffer != null) {
       user = UserProfile.parseFrom(buffer).toBuilder
-      val u = user.build()
-      println(u.getAge)
-      println(u.getCtrcnt)
-      println(u.getCvrcnt)
+//      val u = user.build()
+      println(user.getAge)
+      println(user.getCtrcnt)
+      println(user.getCvrcnt)
       //      redis.setex(key, 3600 * 24 * 7, user.build().toByteArray)
     }
     redis.disconnect
