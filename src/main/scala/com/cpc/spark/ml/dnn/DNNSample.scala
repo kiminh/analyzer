c6b9b4cd4a35c1de33ba53591faafb6f6bed5514
Roy Dong <i@roydong.com>
2018-10-03 13:57:45 +0800

    dnn

--- src/main/scala/com/cpc/spark/ml/dnn/DNNSample.scala
+++ src/main/scala/com/cpc/spark/ml/dnn/DNNSample.scala
@@ -120,7 +120,7 @@ object DNNSample {
 
         hashSeq("f30", "string")($"pkgs").alias("f30"))
 
-      .select(array($"f1", $"f2", $"f3", $"f4", $"f5", $"f6", $"f7", $"f8", $"f9",
+      .select(array($"f1", $"f2", $"f3", $"f4", $"f5", $"f6", $"f9",
         $"f10", $"f11", $"f12", $"f13", $"f14", $"f15", $"f16", $"f17", $"f18", $"f19",
         $"f20", $"f21", $"f22", $"f23", $"f24", $"f25", $"f26", $"f27", $"f28", $"f29" ).alias("dense"),
         //mkSparseFeature($"apps", $"ideaids").alias("sparse"), $"label"
