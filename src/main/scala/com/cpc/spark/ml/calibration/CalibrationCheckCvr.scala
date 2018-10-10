0154f664a74424befda7c53060b0b8d93bc4ac4a
troyhua <troyhua0426@gmail.com>
2018-10-10 11:01:13 +0800

    update

--- src/main/scala/com/cpc/spark/ml/calibration/CalibrationCheckCvr.scala
+++ src/main/scala/com/cpc/spark/ml/calibration/CalibrationCheckCvr.scala
@@ -31,13 +31,14 @@ object CalibrationCheckCvr {
 
     // get union log
    val sql = s"""
-                 |select if(iscvr is null, 0, 1), ecvr from
-                 |( select searchid, ext_int['raw_cvr'] as ecvr
+                 |select if(iscvr is null, 0, 1), ecvr, raw_cvr from
+                 |( select searchid, ext_int['raw_cvr'] as raw_cvr, ext['exp_cvr'].int_value as ecvr
                  | from dl_cpc.cpc_union_log
                  | where $timeRangeSql
                  | and media_appsid in ('80000001', '80000002') and isclick = 1 and ext['antispam'].int_value = 0
                  | and ideaid > 0 and adsrc = 1 and adslot_type in (1, 2, 3) AND userid > 0
                  | and ext_string['ctr_model_name'] = '$modelName'
+                 | and and exptags like "%calibration=on%"
                  | )
                  | a left join
                  |(select searchid, label as iscvr from dl_cpc.ml_cvr_feature_v1
@@ -47,21 +48,24 @@ object CalibrationCheckCvr {
 
     println(s"sql:\n$sql")
     val log = session.sql(sql)
-
     val result = log.rdd.map( x => {
-      val isClick = x.getInt(0).toDouble
-      val ectr = x.getLong(1).toDouble / 1e6d
-      val calibrated = HourlyCalibration.computeCalibration(ectr, irModel.ir.get)
-      (isClick, ectr, calibrated, 1.0)
-    }).reduce((x, y) => (x._1 + y._1, x._2 + y._2, x._3 + y._3, x._4 + y._4))
-    val ctr = result._1 / result._4
-    val ectr = result._2 / result._4
-    val calibrated_ctr = result._3 / result._4
-    println(s"click: ${result._4}")
-    println(s"cvr: $ctr")
-    println(s"ecvr: $ectr")
-    println(s"calibrated_cvr: $calibrated_ctr")
-    println(s"no calibration: ${ectr / ctr}")
-    println(s"new calibration: ${calibrated_ctr / ctr}")
+      val isCvr = x.getInt(0).toDouble
+      val ecvr = x.getLong(1).toDouble / 1e6d
+      val rawCvr = x.getInt(2).toDouble / 1e6d
+      val calibrated = HourlyCalibration.computeCalibration(rawCvr, irModel.ir.get)
+      (isCvr, ecvr, rawCvr, calibrated, 1.0)
+    }).reduce((x, y) => (x._1 + y._1, x._2 + y._2, x._3 + y._3, x._4 + y._4, x._5 + y._5))
+    val cvr = result._1 / result._5
+    val ecvr = result._2 / result._5
+    val rawCvr = result._3 / result._5
+    val calibrated_cvr = result._4 / result._5
+    println(s"conversion: ${result._5}")
+    println(s"cvr: $cvr")
+    println(s"ecvr: $ecvr")
+    println(s"raw_cvr: $rawCvr")
+    println(s"calibrated_cvr: $calibrated_cvr")
+    println(s"no calibration: ${rawCvr / cvr}")
+    println(s"online calibration: ${ecvr / cvr}")
+    println(s"new calibration: ${calibrated_cvr / cvr}")
   }
 }
