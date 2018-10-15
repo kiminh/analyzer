ad0cf1805f62157dfdd3a9daf8f3630bd1a5a1c7
troyhua <troyhua0426@gmail.com>
2018-10-15 15:59:04 +0800

    add empty model writer

---
+++ src/main/scala/com/cpc/spark/ml/ftrl/FtrlEmptyModel.scala
@@ -0,0 +1,30 @@
+package com.cpc.spark.ml.ftrl
+
+/**
+  * author: huazhenhao
+  * date: 9/18/18
+  */
+
+import com.cpc.spark.common.{Murmur3Hash, Utils}
+import com.cpc.spark.ml.common.{Utils => MUtils}
+import com.cpc.spark.ml.train.Ftrl
+import com.typesafe.config.ConfigFactory
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+object FtrlEmptyModel {
+
+  val LOCAL_DIR = "/home/cpc/ftrl/"
+  val DEST_DIR = "/home/work/mlcpp/model/"
+
+  val DOWN_SAMPLE_RATE = 0.2
+
+
+  def main(args: Array[String]): Unit = {
+    val ftrl = Ftrl(1)
+    Ftrl.saveLrPbPackWithDict(ftrl, "/home/cpc/ftrl/ctr-protrait21-ftrl-id-qtt-list-redis.mlm", "ctr-ftrl-v1", "ctr-protrait21-ftrl-id-qtt-list-redis")
+  }
+}
