d0a310a73e24f3f273e544732a048987879f4f22
zhanghy <zhanghongyang@aiclk.com>
2018-10-16 23:46:12 +0800

    fix bug

--- src/main/scala/com/cpc/spark/log/report/GetHourReport.scala
+++ src/main/scala/com/cpc/spark/log/report/GetHourReport.scala
@@ -584,7 +584,7 @@ object GetHourReport {
           var realCost = 0
           if (isclick > 0 && antispam_score == 10000) {
             realCost = x.getAs[Int]("price")
-            if (realCost >= Int.MaxValue) {
+            if (realCost >= Int.MaxValue/100) {
               realCost = 1
             }
           } else {
