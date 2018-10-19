c05c4060c2ea5175d5b27ef6eaefe8bd24a310d8
wangjun02 <wangjun@qutoutiao.net>
2018-10-19 17:56:10 +0800

    -

--- src/main/scala/com/cpc/spark/ocpc/OcpcPIDwithCPA.scala
+++ src/main/scala/com/cpc/spark/ocpc/OcpcPIDwithCPA.scala
@@ -2,11 +2,7 @@ package com.cpc.spark.ocpc
 
 import java.text.SimpleDateFormat
 import java.util.Calendar
-
-import com.cpc.spark.udfs.Udfs_wj.udfOcpcLogExtractCPA
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.sql.functions.{col, max}
-import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{Column, Dataset, Row, SparkSession}
 
 
@@ -14,31 +10,19 @@ object OcpcPIDwithCPA {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("OcpcPIDwithCPA").enableHiveSupport().getOrCreate()
 
-//    import spark.implicits._
-
     val date = args(0).toString
     val hour = args(1).toString
     val filename = "/user/cpc/wangjun/cpa_given.txt"
 
-//    val data = spark.sparkContext.textFile(filename)
-//
-//    val resultRDD = data.map(x => (x.split(",")(0).toInt, x.split(",")(1).toInt))
-//    resultRDD.foreach(println)
-//
-//    val resultDF = resultRDD.toDF("ideaid", "cpa_given")
-//    resultDF.show(10)
-
-//    genCPAgiven(date, hour, spark)
     val dataset = testGenCPAgiven(filename, spark)
     dataset.show(10)
-    genCPAhistory(dataset, date, hour, spark)
+    genCPAratio(dataset, date, hour, spark)
     calculateK(spark)
 
   }
 
   def testGenCPAgiven(filename: String, spark: SparkSession) = {
     import spark.implicits._
-//    filename=" /user/cpc/wangjun/cpa_given.txt"
     val data = spark.sparkContext.textFile(filename)
 
     val resultRDD = data.map(x => (x.split(",")(0).toInt, x.split(",")(1).toInt))
@@ -49,7 +33,8 @@ object OcpcPIDwithCPA {
   }
 
 
-  def genCPAhistory(CPAgiven: DataFrame, date: String, hour: String, spark:SparkSession) = {
+  def genCPAratio(CPAgiven: DataFrame, date: String, hour: String, spark:SparkSession) = {
+    // 取历史数据
     val dateConverter = new SimpleDateFormat("yyyy-MM-dd")
     val today = dateConverter.parse(date)
     val calendar = Calendar.getInstance
@@ -80,6 +65,7 @@ object OcpcPIDwithCPA {
     CPAhistory.show(10)
     CPAhistory.printSchema()
 
+    // 计算cpa_given和cpa_history的比值
     CPAgiven.createOrReplaceTempView("cpa_given_table")
     CPAhistory.createOrReplaceTempView("cpa_history_table")
 
@@ -101,19 +87,59 @@ object OcpcPIDwithCPA {
     val resultDF = spark.sql(sqlRequest2)
     resultDF.show(10)
 
+    // 将结果输出到临时表
     resultDF.write.mode("overwrite").saveAsTable("test.ocpc_cpa_given_history_ratio")
 
   }
 
+//  def testCalculateK(spark:SparkSession): Unit = {
+//    import spark.implicits._
+//    val filename1="/user/cpc/wangjun/ocpc_k.txt"
+//    val data = spark.sparkContext.textFile(filename1)
+//
+//    val dataRDD = data.map(x => (x.split(",")(0).toInt, x.split(",")(1).toInt))
+//    dataRDD.foreach(println)
+//
+//    val dataDF = dataRDD.toDF("ideaid", "k_value")
+//    dataDF.show(10)
+//
+//    val ratioDF = spark.table("test.ocpc_cpa_given_history_ratio")
+//
+//    dataDF.createOrReplaceTempView("k_table")
+//    ratioDF.createOrReplaceTempView("ratio_table")
+//
+//    val sqlRequest =
+//      s"""
+//         |SELECT
+//         |  a.ideaid,
+//         |  (case when a.ratio>1.0 then b.k_value * 1.2
+//         |        when a.ratio<1.0 then b.k_value / 1.2
+//         |        else b.k_value end) as k_value
+//         |FROM
+//         |  ratio_table as a
+//         |INNER JOIN
+//         |  k_table b
+//         |ON
+//         |  a.ideaid=b.ideaid
+//       """.stripMargin
+//
+//    println(sqlRequest)
+//
+//    val resultDF = spark.sql(sqlRequest)
+//
+//    println("final table of the k-value for ocpc:")
+//    resultDF.show(10)
+//
+//    resultDF.write.mode("overwrite").saveAsTable("test.ocpc_k_value_table")
+//
+//  }
+
+
   def calculateK(spark:SparkSession): Unit = {
     import spark.implicits._
-    val filename1="/user/cpc/wangjun/ocpc_k.txt"
-    val data = spark.sparkContext.textFile(filename1)
 
-    val dataRDD = data.map(x => (x.split(",")(0).toInt, x.split(",")(1).toInt))
-    dataRDD.foreach(println)
 
-    val dataDF = dataRDD.toDF("ideaid", "k_value")
+    val dataDF = spark.table("test.test_new_pb_ocpc").select("ideaid", "k_value")
     dataDF.show(10)
 
     val ratioDF = spark.table("test.ocpc_cpa_given_history_ratio")
---
+++ src/main/scala/com/cpc/spark/ocpc/OcpcSampleHourlyV1.scala
@@ -0,0 +1,78 @@
+package com.cpc.spark.ocpc
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.lit
+
+object OcpcSampleHourlyV1 {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().enableHiveSupport().getOrCreate()
+
+    // extract data from hive
+    val dt = args(0)
+    val hour = args(1)
+    var selectWhere = s"`date`='$dt' and hour = '$hour'"
+    var sqlRequest =
+      s"""
+         | select
+         |  a.searchid,
+         |  a.uid,
+         |  a.ideaid,
+         |  a.price,
+         |  a.userid,
+         |  a.ext['adclass'].int_value as adclass,
+         |  a.isclick,
+         |  a.isshow,
+         |  b.label2 as iscvr
+         | from
+         |      (
+         |        select *
+         |        from dl_cpc.cpc_union_log
+         |        where $selectWhere
+         |        and isclick is not null
+         |        and media_appsid  in ("80000001", "80000002")
+         |        and isshow = 1
+         |        and ext['antispam'].int_value = 0
+         |        and ideaid > 0
+         |        and adsrc = 1
+         |        and adslot_type in (1,2,3)
+         |      ) a
+         |inner join
+         |      (
+         |        select searchid, label2
+         |        from dl_cpc.ml_cvr_feature_v1
+         |        where $selectWhere
+         |      ) b on a.searchid = b.searchid
+      """.stripMargin
+    println(sqlRequest)
+    val base = spark.sql(sqlRequest)
+
+    // recalculation with groupby of userid and uid
+    base.createOrReplaceTempView("tmpTable")
+    val groupByRequesst =
+      s"""
+         |Select
+         |  userid,
+         |  uid,
+         |  ideaid,
+         |  adclass,
+         |  SUM(CASE WHEN isclick == 1 then price else 0 end) as cost,
+         |  SUM(CASE WHEN isclick == 1 then 1 else 0 end) as ctr_cnt,
+         |  SUM(CASE WHEN iscvr == 1 then 1 else 0 end) as cvr_cnt,
+         |  SUM(CASE WHEN isshow == 1 then 1 else 0 end) as total_cnt
+         |FROM
+         |  tmpTable
+         |GROUP BY userid, uid, ideaid, adclass
+       """.stripMargin
+
+    val groupBy = spark.sql(groupByRequesst)
+    val result = groupBy.withColumn("date", lit(dt))
+      .withColumn("hour", lit(hour))
+
+    // save data
+    result.write.mode("overwrite").insertInto("dl_cpc.ocpc_uid_userid_track")
+
+
+
+    println("successfully save data into table dl_cpc.ocpc_uid_userid_track")
+  }
+}
