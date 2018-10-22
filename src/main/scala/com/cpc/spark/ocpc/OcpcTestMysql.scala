e343042c34d3c7763664e1f933894585ab04885e
wangjun02 <wangjun@qutoutiao.net>
2018-10-22 16:01:03 +0800

    -

--- src/main/scala/com/cpc/spark/ocpc/OcpcTestMysql.scala
+++ src/main/scala/com/cpc/spark/ocpc/OcpcTestMysql.scala
@@ -9,30 +9,30 @@ object OcpcTestMysql {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().enableHiveSupport().getOrCreate()
 
-    val url = "jdbc:mysql://192.168.66.11:3306/adv?useUnicode=true&characterEncoding=utf-8"
+    val url = "jdbc:mysql://rm-2zef52mz0p6mv5007.mysql.rds.aliyuncs.com:3306/adv_test?useUnicode=true&characterEncoding=utf-8"
 //    val url = "jdbc:mysql://rr-2zehhy0xn8833n2u5.mysql.rds.aliyuncs.com:3306/adv?useUnicode=true&characterEncoding=utf-8"
 //    val url = "jdbc:mysql://192.168.66.11:3306/union?useUnicode=true&characterEncoding=utf-8"
-    val user = "root"
-    val passwd = "cpcv587"
+    val user = "cpcrw"
+    val passwd = "zZdlz9qUB51awT8b"
     val driver = "com.mysql.jdbc.Driver"
     val table = "(select bid, ocpc_bid, ocpc_bid_update_time from adv.unit where is_ocpc=1 limit 100) as tmp"
 
 
-    val mariadbProp = new Properties()
-    mariadbProp.put("user", "root")
-    mariadbProp.put("password", "cpcv587")
-    mariadbProp.put("driver", "com.mysql.jdbc.Driver")
+//    val mariadbProp = new Properties()
+//    mariadbProp.put("user", "root")
+//    mariadbProp.put("password", "cpcv587")
+//    mariadbProp.put("driver", "com.mysql.jdbc.Driver")
 
 //    val dataframe_mysql = spark.read.format("jdbc").option("url", "jdbc:mysql://192.168.66.11:3306/adv").option("driver", "com.mysql.jdbc.Driver").option("dbtable", "unit").option("user", "root").option("password", "cpcv587").load()
 
-//    val data = spark.read.format("jdbc")
-//      .option("url", url)
-//      .option("driver", driver)
-//      .option("user", user)
-//      .option("password", passwd)
-//      .option("dbtable", table)
-//      .load()
-    val data = spark.read.jdbc(url, "adv.unit", mariadbProp)
+    val data = spark.read.format("jdbc")
+      .option("url", url)
+      .option("driver", driver)
+      .option("user", user)
+      .option("password", passwd)
+      .option("dbtable", table)
+      .load()
+//    val data = spark.read.jdbc(url, "unit", mariadbProp)
 
 //    val data = dataframe_mysql.select("bid", "ocpc_bid", "ocpc_bid_update_time")
 
