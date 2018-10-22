dc6ec57fb24378b3117d9ac390e4298d2d8c9a60
wangjun02 <wangjun@qutoutiao.net>
2018-10-22 10:41:27 +0800

    -

--- src/main/scala/com/cpc/spark/ocpc/OcpcTestMysql.scala
+++ src/main/scala/com/cpc/spark/ocpc/OcpcTestMysql.scala
@@ -6,9 +6,24 @@ object OcpcTestMysql {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().enableHiveSupport().getOrCreate()
 
-    val dataframe_mysql = spark.read.format("jdbc").option("url", "jdbc:mysql://192.168.66.11:3306/adv").option("driver", "com.mysql.jdbc.Driver").option("dbtable", "unit").option("user", "root").option("password", "cpcv587").load()
+    val url = "jdbc:mysql://192.168.66.11:3306/adv?useUnicode=true&characterEncoding=utf-8"
+    val user = "root"
+    val passwd = "cpcv587"
+    val driver = "com.mysql.jdbc.Driver"
+    val table = "(select bid, ocpc_bid, ocpc_bid_update_time from unit where date='2018-10-18') as tmp"
 
-    val data = dataframe_mysql.select("bid", "ocpc_bid", "ocpc_bid_update_time")
+//    val dataframe_mysql = spark.read.format("jdbc").option("url", "jdbc:mysql://192.168.66.11:3306/adv").option("driver", "com.mysql.jdbc.Driver").option("dbtable", "unit").option("user", "root").option("password", "cpcv587").load()
+
+    val data = spark.read.format("jdbc")
+      .option("url", url)
+      .option("driver", driver)
+      .option("user", user)
+      .option("password", passwd)
+      .option("dbtable", table)
+      .load()
+      .repartition(10)
+
+//    val data = dataframe_mysql.select("bid", "ocpc_bid", "ocpc_bid_update_time")
 
     data.show(10)
   }
