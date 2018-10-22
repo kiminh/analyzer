3f341f9caf0d7c636a90ffbf6df4929364b55bdd
wangjun02 <wangjun@qutoutiao.net>
2018-10-22 16:06:31 +0800

    -

--- src/main/scala/com/cpc/spark/ocpc/OcpcTestMysql.scala
+++ src/main/scala/com/cpc/spark/ocpc/OcpcTestMysql.scala
@@ -15,7 +15,7 @@ object OcpcTestMysql {
     val user = "cpcrw"
     val passwd = "zZdlz9qUB51awT8b"
     val driver = "com.mysql.jdbc.Driver"
-    val table = "(select bid, ocpc_bid, ocpc_bid_update_time from adv.unit where is_ocpc=1 limit 100) as tmp"
+    val table = "(select * from adv_test.unit where is_ocpc=1 limit 100) as tmp"
 
 
 //    val mariadbProp = new Properties()
@@ -35,6 +35,8 @@ object OcpcTestMysql {
 //    val data = spark.read.jdbc(url, "unit", mariadbProp)
 
 //    val data = dataframe_mysql.select("bid", "ocpc_bid", "ocpc_bid_update_time")
+
+    data.printSchema()
 
     data.show(10)
   }
