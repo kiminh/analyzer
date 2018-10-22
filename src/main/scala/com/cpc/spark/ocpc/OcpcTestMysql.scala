d2177425d6a7df963285ef6c4466aeaeeacf414a
wangjun02 <wangjun@qutoutiao.net>
2018-10-22 13:04:34 +0800

    -

--- src/main/scala/com/cpc/spark/ocpc/OcpcTestMysql.scala
+++ src/main/scala/com/cpc/spark/ocpc/OcpcTestMysql.scala
@@ -6,7 +6,7 @@ object OcpcTestMysql {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().enableHiveSupport().getOrCreate()
 
-    val url = "192.168.66.11:3306/adv?characterEncoding=utf-8"
+    val url = "192.168.66.11:3306/adv?"
     val user = "root"
     val passwd = "cpcv587"
     val driver = "com.mysql.jdbc.Driver"
