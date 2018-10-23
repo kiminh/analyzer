41d3784305eacf8c6c9933cd602d93fd96c733f8
wangjun02 <wangjun@qutoutiao.net>
2018-10-23 10:22:19 +0800

    -

--- src/main/scala/com/cpc/spark/ocpc/OcpcTestSamplePb.scala
+++ src/main/scala/com/cpc/spark/ocpc/OcpcTestSamplePb.scala
@@ -4,7 +4,17 @@ package com.cpc.spark.ocpc
 
 import java.text.SimpleDateFormat
 import java.util.Calendar
-import org.apache.spark.sql.{Column, Dataset, Row, SparkSession}
+
+import com.redis.RedisClient
+import com.redis.serialization.Parse.Implicits._
+import com.typesafe.config.ConfigFactory
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.functions._
+import userprofile.Userprofile.UserProfile
+
+import scala.collection.mutable.ListBuffer
+import userocpc.userocpc._
+import java.io.FileOutputStream
 
 
 
@@ -12,8 +22,6 @@ import org.apache.spark.sql.{Column, Dataset, Row, SparkSession}
 object OcpcTestSamplePb {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().enableHiveSupport().getOrCreate()
-
-    import spark.implicits._
 
     // calculate time period for historical data
     val end_date = args(0)
@@ -23,10 +31,7 @@ object OcpcTestSamplePb {
     val date = sdf.parse(end_date)
     val calendar = Calendar.getInstance
     calendar.setTime(date)
-    calendar.add(Calendar.DATE, -3)
-    val dt3 = calendar.getTime
-    val date3 = sdf.format(dt3)
-    calendar.add(Calendar.DATE, -4)
+    calendar.add(Calendar.DATE, -7)
     val dt = calendar.getTime
     val start_date = sdf.format(dt)
     val selectCondition1 = s"`date`='$start_date' and hour > '$hour'"
@@ -34,47 +39,74 @@ object OcpcTestSamplePb {
     val selectCondition3 = s"`date`='$end_date' and hour <= '$hour'"
 
     // read data and set redis configuration
-    val filename = "/user/cpc/wangjun/ocpc_demo.txt"
-    val dataRDD = spark.sparkContext.textFile(filename)
-    val dataDF = dataRDD.map(x=>(x.split(",")(0), x.split(",")(1), x.split(",")(2), x.split(",")(3))).toDF("ideaid", "cpa_given", "date", "hour")
-
-    dataDF.show(10)
-    dataDF.createOrReplaceTempView("test_ocpc_demo")
-
-    println("##############")
-    println(selectCondition1)
-    println("##############")
-    println(selectCondition2)
-    println("##############")
-    println(selectCondition3)
-
-
     val sqlRequest =
       s"""
          |SELECT
-         |    ideaid,
-         |    update_date
+         |  ideaid,
+         |  adclass,
+         |  date,
+         |  hour,
+         |  SUM(cost) as cost,
+         |  SUM(ctr_cnt) as ctr_cnt,
+         |  SUM(cvr_cnt) as cvr_cnt,
+         |  SUM(total_cnt) as total_cnt
          |FROM
-         |    test.ocpc_idea_update_time
-         |WHERE
-         |    `update_date` > '$date3'
-         |or
-         |    (`update_date` = '$date3' and `update_hour` > '$hour')
+         |  dl_cpc.ocpc_uid_userid_track_label2
+         |WHERE ($selectCondition1) OR
+         |($selectCondition2) OR
+         |($selectCondition3)
+         |GROUP BY ideaid, adclass, date, hour
        """.stripMargin
     println(sqlRequest)
 
-    val ocpcUpdate = spark.sql(sqlRequest)
+    val base = spark.sql(sqlRequest)
 
-    ocpcUpdate.show(10)
-
-    val userDF = dataDF.join(ocpcUpdate, Seq("ideaid"), "left_outer").filter("update_date is null")
-
-    println("###########################")
-    userDF.show(10)
+    base.show(10)
 
   }
 
 
+  def savePbPack(dataset: Dataset[Row]): Unit = {
+    var list = new ListBuffer[SingleUser]
+    val filename = s"UseridDataOcpc.pb"
+    println("size of the dataframe")
+    println(dataset.count)
+    dataset.show(10)
+    for (record <- dataset.collect()) {
 
+      val ideaid = record.get(0).toString
+      val userId = record.get(1).toString
+      val adclassId = record.get(2).toString
+      val costValue = record.get(3).toString
+      val ctrValue = record.getLong(4).toString
+      val cvrValue = record.getLong(5).toString
+      val adclassCost = record.get(6).toString
+      val adclassCtr = record.getLong(7).toString
+      val adclassCvr = record.getLong(8).toString
+      val k = record.get(9).toString
+
+      val currentItem = SingleUser(
+        ideaid = ideaid,
+        userid = userId,
+        cost = costValue,
+        ctrcnt = ctrValue,
+        cvrcnt = cvrValue,
+        adclass = adclassId,
+        adclassCost = adclassCost,
+        adclassCtrcnt = adclassCtr,
+        adclassCvrcnt = adclassCvr,
+        kvalue = k
+      )
+      list += currentItem
+    }
+    val result = list.toArray[SingleUser]
+    val useridData = UserOcpc(
+      user = result
+    )
+    println("length of the array")
+    println(result.length)
+    useridData.writeTo(new FileOutputStream(filename))
+    println("complete save data into protobuffer")
+
+  }
 }
-
