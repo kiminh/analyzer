595f3bf1b8a042f4076d46303cf582d0721f9de6
wangjun02 <wangjun@qutoutiao.net>
2018-10-23 13:42:48 +0800

    -

--- src/main/scala/com/cpc/spark/ocpc/OcpcSampleToPb.scala
+++ src/main/scala/com/cpc/spark/ocpc/OcpcSampleToPb.scala
@@ -26,15 +26,13 @@ object OcpcSampleToPb {
     // calculate time period for historical data
     val end_date = args(0)
     val hour = args(1)
+    //    val threshold = args(2).toInt  //default: 20
     val threshold = 20
     val sdf = new SimpleDateFormat("yyyy-MM-dd")
     val date = sdf.parse(end_date)
     val calendar = Calendar.getInstance
     calendar.setTime(date)
-    calendar.add(Calendar.DATE, -3)
-    val dt3 = calendar.getTime
-    val date3 = sdf.format(dt3)
-    calendar.add(Calendar.DATE, -4)
+    calendar.add(Calendar.DATE, -7)
     val dt = calendar.getTime
     val start_date = sdf.format(dt)
     val selectCondition1 = s"`date`='$start_date' and hour > '$hour'"
@@ -46,9 +44,10 @@ object OcpcSampleToPb {
       s"""
          |SELECT
          |  userid,
-         |  uid,
          |  ideaid,
          |  adclass,
+         |  date,
+         |  hour,
          |  SUM(cost) as cost,
          |  SUM(ctr_cnt) as ctr_cnt,
          |  SUM(cvr_cnt) as cvr_cnt,
@@ -56,20 +55,47 @@ object OcpcSampleToPb {
          |FROM
          |  dl_cpc.ocpc_uid_userid_track_label2
          |WHERE ($selectCondition1) OR
-         |
          |($selectCondition2) OR
          |($selectCondition3)
-         |GROUP BY userid, uid, ideaid, adclass
+         |GROUP BY userid, ideaid, adclass, date, hour
        """.stripMargin
     println(sqlRequest)
 
-    val base = spark.sql(sqlRequest)
+    val rawBase = spark.sql(sqlRequest)
 
-    // calculation by uid
-    val uidData = base
-      .groupBy("uid")
-      .agg(sum("ctr_cnt").alias("ctr_cnt"), sum("cvr_cnt").alias("cvr_cnt"))
-      .withColumn("data", concat_ws(",", col("ctr_cnt"), col("cvr_cnt")))
+    rawBase.createOrReplaceTempView("base_table")
+
+    val sqlRequestNew1 =
+      s"""
+         |SELECT
+         |  a.userid,
+         |  a.ideaid,
+         |  a.adclass,
+         |  a.cost,
+         |  a.ctr_cnt,
+         |  a.cvr_cnt,
+         |  a.total_cnt,
+         |  a.date,
+         |  a.hour,
+         |  (case when b.update_date is null then '$start_date' else b.update_date end) as update_date,
+         |  (case when b.update_hour is null then '$hour' else b.update_hour end) as update_hour,
+         |  (case when b.update_date is null or b.update_hour is null then 1
+         |        when b.update_date < date then 1
+         |        when b.update_date = date and b.update_hour <= hour then 1
+         |        else 0 end) as flag
+         |FROM
+         |  base_table as a
+         |LEFT JOIN
+         |  test.ocpc_idea_update_time as b
+         |ON
+         |  a.ideaid=b.ideaid
+       """.stripMargin
+
+    println(sqlRequestNew1)
+
+    val rawData = spark.sql(sqlRequestNew1)
+
+    val base = rawData.filter("flag=1").select("userid", "ideaid", "adclass", "cost", "ctr_cnt", "cvr_cnt", "total_cnt")
 
 
     // calculation by userid
@@ -78,6 +104,8 @@ object OcpcSampleToPb {
       .agg(sum("cost").alias("cost"), sum("ctr_cnt").alias("user_ctr_cnt"), sum("cvr_cnt").alias("user_cvr_cnt"))
       .select("ideaid", "userid", "adclass", "cost", "user_ctr_cnt", "user_cvr_cnt")
 
+    userData.write.mode("overwrite").saveAsTable("test.ocpc_data_userdata")
+
 
     // calculate by adclass
     val adclassData = userData
@@ -85,6 +113,7 @@ object OcpcSampleToPb {
       .agg(sum("cost").alias("adclass_cost"), sum("user_ctr_cnt").alias("adclass_ctr_cnt"), sum("user_cvr_cnt").alias("adclass_cvr_cnt"))
       .select("adclass", "adclass_cost", "adclass_ctr_cnt", "adclass_cvr_cnt")
 
+    adclassData.write.mode("overwrite").saveAsTable("test.ocpc_data_adclassdata")
 
 
     // connect adclass and userid
@@ -95,8 +124,8 @@ object OcpcSampleToPb {
          |    a.userid,
          |    a.adclass,
          |    a.cost,
-         |    (case when a.user_cvr_cnt<$threshold then b.adclass_ctr_cnt else a.user_ctr_cnt end) as ctr_cnt,
-         |    (case when a.user_cvr_cnt<$threshold then b.adclass_cvr_cnt else a.user_cvr_cnt end) as cvr_cnt,
+         |    a.user_ctr_cnt,
+         |    a.user_cvr_cnt,
          |    b.adclass_cost,
          |    b.adclass_ctr_cnt,
          |    b.adclass_cvr_cnt
@@ -117,15 +146,17 @@ object OcpcSampleToPb {
          |    userid,
          |    adclass,
          |    cost,
-         |    (case when cvr_cnt=0 then ctr_cnt+1 else ctr_cnt end) as ctr_cnt,
-         |    (case when cvr_cnt=0 then 1 else cvr_cnt end) as cvr_cnt,
+         |    user_ctr_cnt as ctr_cnt,
+         |    user_cvr_cnt as cvr_cnt,
          |    adclass_cost,
-         |    (case when adclass_cvr_cnt=0 then adclass_ctr_cnt+1 else adclass_ctr_cnt end) as adclass_ctr_cnt,
-         |    (case when adclass_cvr_cnt=0 then 1 else adclass_cvr_cnt end) as adclass_cvr_cnt,
+         |    adclass_ctr_cnt as adclass_ctr_cnt,
+         |    adclass_cvr_cnt as adclass_cvr_cnt,
          |    '$end_date' as date,
          |    '$hour' as hour
          |FROM
          |    useridTable
+         |WHERE
+         |    user_cvr_cnt>=20
        """.stripMargin
 
 
@@ -147,7 +178,9 @@ object OcpcSampleToPb {
          |  (case when b.k_value is null then 1.0
          |        when b.k_value > 2.0 then 2.0
          |        when b.k_value < 0.2 then 0.2
-         |        else b.k_value end) as k_value
+         |        else b.k_value end) as k_value,
+         |   '$end_date' as date,
+         |   '$hour' as hour
          |FROM
          |  (SELECT
          |    *
@@ -161,27 +194,17 @@ object OcpcSampleToPb {
          |   test.ocpc_k_value_table b
          |ON
          |   a.ideaid=b.ideaid
-         |LEFT JOIN
-         |   (SELECT
-         |      ideaid
-         |    FROM
-         |      test.ocpc_idea_update_time
-         |    WHERE
-         |      `date` < '$date3'
-         |    or (`date`='$date3' and `hour`<'$hour')) c
-         |ON
-         |    a.ideaid=c.ideaid
-         |WHERE
-         |    c.ideaid is not null
        """.stripMargin
 
     val userFinalData2 = spark.sql(sqlRequest3)
 
     userFinalData2.show(10)
 
+    userFinalData2.write.mode("overwrite").saveAsTable("test.test_new_pb_ocpc")
 
+    userFinalData2.write.mode("overwrite").insertInto("dl_cpc.ocpc_pb_result_table_v1")
 
-    //     save data into pb file
+    // save data into pb file
     savePbPack(userFinalData2)
   }
 
