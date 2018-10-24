b12b5015a76828e14f28a2507d8e17b685f03571
wangjun02 <wangjun@qutoutiao.net>
2018-10-24 14:54:32 +0800

    -

--- src/main/scala/com/cpc/spark/ocpc/OcpcPIDwithCPA.scala
+++ src/main/scala/com/cpc/spark/ocpc/OcpcPIDwithCPA.scala
@@ -258,7 +258,7 @@ object OcpcPIDwithCPA {
     val filename1="/user/cpc/wangjun/ocpc_k.txt"
     val data = spark.sparkContext.textFile(filename1)
 
-    val dataRDD = data.map(x => (x.split(",")(0).toInt, x.split(",")(1).toString))
+    val dataRDD = data.map(x => (x.split(",")(0).toInt, x.split(",")(1).toString.slice(0,5)))
 //    dataRDD.foreach(println)
 
     val dataDF = dataRDD.toDF("ideaid", "k_value")
