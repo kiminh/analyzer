a4e8037996f8cce3ec4c68a65166c09f02f07037
wangyao <wangyao@qutoutiao.net>
2018-10-25 18:02:11 +0800

    hh

--- scripts/adcategory/test.sh
+++ scripts/adcategory/test.sh
@@ -17,5 +17,5 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.yarn.executor.memoryOverhead=4g'\
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
-    --class com.cpc.spark.ocpc.OcpcSpecificAd \
+    --class com.cpc.spark.adcategory.AdCategoryShow \
     /home/cpc/wangyao/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar
