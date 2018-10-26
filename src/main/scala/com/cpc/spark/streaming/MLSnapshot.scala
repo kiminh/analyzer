7e8bcc3fd7a2f0094ea2200ca9b67710805f18f5
dongjinbao <dongjinbao@qutoutiao.net>
2018-10-26 17:15:09 +0800

    add action

--- src/main/scala/com/cpc/spark/streaming/MLSnapshot.scala
+++ src/main/scala/com/cpc/spark/streaming/MLSnapshot.scala
@@ -202,7 +202,7 @@ object MLSnapshot {
                     )
                 }
             }.cache()
-
+            System.out.println("snap num is ",snap.count())
             val spark = SparkSession.builder().config(ssc.sparkContext.getConf).getOrCreate()
             val keys = snap.mapPartitions(f => {
                 val result = scala.collection.mutable.ListBuffer[((String,String),Int)]()
@@ -224,6 +224,9 @@ object MLSnapshot {
             //val keys = snap.map(f => ((f.date,f.hour),1)).reduceByKey((x,y) => x).map(f => f._1).toLocalIterator
             System.out.println("******keys 's num is " + keys.length + " ******")
             //val keys = snap.map { x => (x.date, x.hour) }.distinct.toLocalIterator
+            for (k <- keys){
+                System.out.println(k._1,k._2)
+            }
             keys.foreach { key =>
                 System.out.println(key._1,key._2)
                 val part = snap.filter(r => r.date == key._1 && r.hour == key._2)
