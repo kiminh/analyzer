ffd42360f39d4e09e73a6a138ed0173c96b44aeb
wangjun <wangjun02@qutoutiao.net>
2018-10-30 14:26:20 +0800

    -

---
+++ src/main/scala/com/cpc/spark/ocpc/OcpcLogParserByID.scala
@@ -0,0 +1,45 @@
+package com.cpc.spark.ocpc
+
+import com.cpc.spark.udfs.Udfs_wj.udfStringToMap
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.col
+
+
+object OcpcLogParserByID {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().appName("OcpcMonitor").enableHiveSupport().getOrCreate()
+
+    val day = args(0).toString
+    val hour = args(1).toString
+    val ideaid = args(2).toString
+
+    val sqlRequest =
+      s"""
+         |SELECT
+         |  ideaid,
+         |  ocpc_log,
+         |  isshow,
+         |  isclick,
+         |  iscvr
+         |FROM
+         |  dl_cpc.ocpc_result_unionlog_table_bak
+         |WHERE
+         |  `date`='$day'
+         |AND
+         |  `hour`='$hour'
+         |AND
+         |  ideaid='$ideaid'
+       """.stripMargin
+
+    println(sqlRequest)
+    val data = spark.sql(sqlRequest)
+
+    logParser(data)
+  }
+
+  def logParser(dataset: DataFrame): Unit = {
+    val newData = dataset.withColumn("ocpc_log_dict", udfStringToMap()(col("ocpc_log")))
+
+    newData.write.mode("overwrite").saveAsTable("test.ocpc_log_parser_20181030")
+  }
+}
