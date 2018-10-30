13317e4013e4ab3a9a0e2baab583feffadf0367b
zhuhaijian <zhuhaijian@qutoutiao.net>
2018-10-30 10:59:47 +0800

    -

--- src/main/scala/com/cpc/spark/ml/dnn/DNNSampleV2_tmp.scala
+++ src/main/scala/com/cpc/spark/ml/dnn/DNNSampleV2_tmp.scala
@@ -116,7 +116,7 @@ object DNNSampleV2_tmp {
          |  hour
          |
          |from dl_cpc.cpc_union_log where `date` = '$date'
-         |  and isshow = 1 and ideaid > 0 and adslot_type = 1
+         |  and isshow = 1 and ideaid > 0 and adslot_type in (1,2)
          |  and media_appsid in ("80000001", "80000002")
          |  and uid not like "%.%"
          |  and uid not like "%000000%"
