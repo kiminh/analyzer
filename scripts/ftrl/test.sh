f93a9b54bce40c765da84872c41cbc4e7514d465
wangyao <wangyao@qutoutiao.net>
2018-10-31 11:13:30 +0800

    hh

--- scripts/ftrl/test.sh
+++ scripts/ftrl/test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sh testFtrlNew.sh 2018-09-15 11 list-ctr 9 19 1 0
-sh testFtrlNew.sh 2018-09-15 11 content-ctr 9 19 1 0
-sh testFtrlNew.sh 2018-09-15 11 interact-ctr 9 19 1 0
-sh testFtrlNew.sh 2018-09-15 11 all-cvr 9 19 1 0
+#sh testFtrlNew.sh 2018-09-15 11 list-ctr 9 19 1 0
+#sh testFtrlNew.sh 2018-09-15 11 content-ctr 9 19 1 0
+#sh testFtrlNew.sh 2018-09-15 11 interact-ctr 9 19 1 0
+#sh testFtrlNew.sh 2018-09-15 11 all-cvr 9 19 1 0
+sh testFtrlNew.sh 2018-10-31 07 list-cvr2 19 19 1 0
+#sh testFtrlNew.sh 2018-10-30 12 content-cvr2 19 19 1 0
+#sh testFtrlNew.sh 2018-10-30 12 interact-cvr2 19 19 1 0
