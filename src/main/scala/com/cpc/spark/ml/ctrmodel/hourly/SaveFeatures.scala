cf8ec367504c8febd77b9aeacc8801a83a9cb44b
zhanghongyang <zhanghongyang@qutoutiao.net>
2018-11-02 16:31:30 +0800

    fix 3

--- src/main/scala/com/cpc/spark/ml/ctrmodel/hourly/SaveFeatures.scala
+++ src/main/scala/com/cpc/spark/ml/ctrmodel/hourly/SaveFeatures.scala
@@ -237,7 +237,7 @@ object SaveFeatures {
          |        ,b.trace_type
          |        ,b.trace_op1
          |from (select * from dl_cpc.cpc_motivation_log
-         |        where `date` = "%s" and `hour` = "%s" and searchid is not null and searchid != "" and isclick > 0) a
+         |        where `date` = "%s" and `hour` = "%s" and searchid is not null and searchid != "") a
          |    left join (select id from bdm.cpc_userid_test_dim where day='%s') t2
          |        on a.userid = t2.id
          |    left join
@@ -282,7 +282,7 @@ object SaveFeatures {
          |        ,a.ideaid
          |        ,b.trace_type
          |from (select * from dl_cpc.cpc_union_log
-         |        where `date` = "%s" and `hour` = "%s" and searchid is not null and searchid != "" and isclick > 0) a
+         |        where `date` = "%s" and `hour` = "%s" and searchid is not null and searchid != "") a
          |    left join (select id from bdm.cpc_userid_test_dim where day='%s') t2
          |        on a.userid = t2.id
          |    left join
@@ -355,7 +355,7 @@ object SaveFeatures {
          |       ,a.ideaid
          |       ,b.*
          |from (select * from dl_cpc.cpc_union_log
-         |        where `date` = "%s" and `hour` = "%s" and searchid is not null and searchid != "" and isclick > 0) a
+         |        where `date` = "%s" and `hour` = "%s" and searchid is not null and searchid != "") a
          |    left join (select id from bdm.cpc_userid_test_dim where day='%s') t2
          |        on a.userid = t2.id
          |    left join
