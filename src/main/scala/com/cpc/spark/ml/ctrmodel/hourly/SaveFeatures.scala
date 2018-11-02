4167ce07306446286fcc632e2dddac2efe833c7b
zhanghongyang <zhanghongyang@qutoutiao.net>
2018-11-02 19:09:15 +0800

    fix 3

--- src/main/scala/com/cpc/spark/ml/ctrmodel/hourly/SaveFeatures.scala
+++ src/main/scala/com/cpc/spark/ml/ctrmodel/hourly/SaveFeatures.scala
@@ -360,8 +360,8 @@ object SaveFeatures {
          |        on a.userid = t2.id
          |    left join
          |        (select *
-         |            from dl_cpc.cpc_union_trace_log
-         |            where `date` = "%s" and `hour` = "%s"
+         |            from dl_cpc.logparsed_cpc_trace_minute
+         |            where `thedate` = "%s" and `thehour` = "%s"
          |         ) b
          |    on a.searchid=b.searchid
          | where t2.id is null
