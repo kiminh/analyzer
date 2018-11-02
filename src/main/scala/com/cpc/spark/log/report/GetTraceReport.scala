64d02ccf32b7dd7eb1823eef0d1007aaf006abe7
zhanghongyang <zhanghongyang@qutoutiao.net>
2018-11-02 18:54:50 +0800

    fix 3

--- src/main/scala/com/cpc/spark/log/report/GetTraceReport.scala
+++ src/main/scala/com/cpc/spark/log/report/GetTraceReport.scala
@@ -60,18 +60,6 @@ object GetTraceReport {
       .getOrCreate()
 
 
-    //    val traceReport = ctx.sql(
-    //      s"""
-    //         |select tr.searchid, un.userid as user_id
-    //         |,un.planid as plan_id ,un.unitid as unit_id ,
-    //         |un.ideaid as idea_id, tr.date as date,tr.hour,
-    //         |tr.trace_type as trace_type,tr.trace_op1 as trace_op1 ,tr.duration as duration, tr.auto
-    //         |from dl_cpc.cpc_union_trace_log as tr left join dl_cpc.cpc_union_log as un on tr.searchid = un.searchid
-    //         |where  tr.`date` = "%s" and tr.`hour` = "%s"  and un.`date` = "%s" and un.`hour` = "%s" and un.isclick = 1
-    //       """.stripMargin.format(date, hour, date, hour))
-    //      //      .as[TraceReportLog]
-    //      .rdd.cache()
-
     val traceReport = ctx.sql(
       s"""
          |select tr.searchid, un.userid as user_id
@@ -79,11 +67,10 @@ object GetTraceReport {
          |un.ideaid as idea_id, tr.date as date,tr.hour,
          |tr.trace_type as trace_type,tr.trace_op1 as trace_op1 ,tr.duration as duration, tr.auto
          |from dl_cpc.logparsed_cpc_trace_minute as tr left join dl_cpc.cpc_union_log as un on tr.searchid = un.searchid
-         |where  tr.`thedate` = "%s" and tr.`thehour` = "%s"  and un.`date` = "%s" and un.`hour` = "%s"
+         |where  tr.`thedate` = "%s" and tr.`thehour` = "%s"  and un.`date` = "%s" and un.`hour` = "%s" and un.isclick = 1
        """.stripMargin.format(date, hour, date, hour))
+      //      .as[TraceReportLog]
       .rdd.cache()
-
-
     val sql1 = "select ideaid , sum(isshow) as show, sum(isclick) as click from dl_cpc.cpc_union_log where `date` = \"%s\" and `hour` =\"%s\" group by ideaid ".format(date, hour)
     val unionRdd = ctx.sql(sql1).rdd.map {
       x =>
@@ -96,7 +83,6 @@ object GetTraceReport {
 
     val traceData = traceReport.filter {
       trace =>
-        //trace.getAs[Int]("plan_id") > 0 && trace.getAs[String]("trace_type").length < 100 && trace.getAs[String]("trace_type").length > 1
         trace.getAs[Int]("plan_id") > 0 && trace.getAs[String]("trace_type").length < 100 && trace.getAs[String]("trace_type").length > 1
     }.map {
       trace =>
