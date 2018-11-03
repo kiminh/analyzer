028e5c0ecbaae714ef1c4f1b3fcc8a0a65c232c2
wangjun <wangjun02@qutoutiao.net>
2018-11-03 15:52:31 +0800

    fix bug

--- src/main/scala/com/cpc/spark/ocpc/OcpcPcvrCalibration.scala
+++ src/main/scala/com/cpc/spark/ocpc/OcpcPcvrCalibration.scala
@@ -13,23 +13,17 @@ object OcpcPcvrCalibration {
     val date = args(0).toString
     val hour = args(1).toString
 
-    val historyData = getHistoryData(date, hour, 24 * 7, spark)
+    val historyData = getCompleteData(date, hour, 24 * 7, spark)
     val result = calibrationV1(historyData, spark)
     result.write.mode("overwrite").saveAsTable("test.ocpc_new_calibration_value")
   }
 
-  def calibrationV1(historyData: DataFrame,spark: SparkSession) = {
+  def calibrationV1(historyData: DataFrame,spark: SparkSession) :DataFrame = {
     val resultDF = historyData
-      .groupBy("ideaid", "adclass", "hour")
-      .agg(
-        sum(col("isshow")).alias("show_cnt"),
-        sum(col("isclick")).alias("ctr_cnt"),
-        sum(col("iscvr")).alias("cvr_cnt"))
       .withColumn("timespan", udfHourToTimespan()(col("hour")))
-      .select("ideaid", "adclass", "timespan", "show_cnt", "ctr_cnt", "cvr_cnt")
+      .select("ideaid", "adclass", "timespan", "ctr_cnt", "cvr_cnt")
       .groupBy("ideaid", "adclass", "timespan")
       .agg(
-        sum(col("show_cnt")).alias("show_cnt"),
         sum(col("ctr_cnt")).alias("ctr_cnt"),
         sum(col("cvr_cnt")).alias("cvr_cnt"))
       .withColumn("weight", udfTimespanToWeight()(col("timespan")))
--- src/main/scala/com/cpc/spark/ocpc/OcpcUtils.scala
+++ src/main/scala/com/cpc/spark/ocpc/OcpcUtils.scala
@@ -55,4 +55,42 @@ object OcpcUtils {
     val resultDF = spark.sql(sqlRequest)
     resultDF
   }
+
+
+  def getCompleteData(date: String, hour: String, hourCnt: Int, spark: SparkSession) :DataFrame ={
+    /**
+      * 按照给定的时间区间获取从OcpcMonitor程序的结果表获取历史数据
+      */
+
+    // 取历史数据
+    val dateConverter = new SimpleDateFormat("yyyy-MM-dd HH")
+    val newDate = date + " " + hour
+    val today = dateConverter.parse(newDate)
+    val calendar = Calendar.getInstance
+    calendar.setTime(today)
+    calendar.add(Calendar.HOUR, -hourCnt)
+    val yesterday = calendar.getTime
+    val tmpDate = dateConverter.format(yesterday)
+    val tmpDateValue = tmpDate.split(" ")
+    val date1 = tmpDateValue(0)
+    val hour1 = tmpDateValue(1)
+    val selectCondition = getTimeRangeSql2(date1, hour1, date, hour)
+
+    val sqlRequest =
+      s"""
+         |SELECT
+         |  ideaid,
+         |  adclass,
+         |  ctr_cnt,
+         |  cvr_cnt,
+         |  hour
+         |FROM
+         |  dl_cpc.ocpc_uid_userid_track_label2
+         |WHERE
+         |  $selectCondition
+       """.stripMargin
+    println(sqlRequest)
+    val resultDF = spark.sql(sqlRequest)
+    resultDF
+  }
 }
