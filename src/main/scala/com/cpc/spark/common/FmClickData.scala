815eb9b90910fc19a768b1b60797873ee5d13947
zhanghongyang <zhanghongyang@qutoutiao.net>
2018-11-05 11:24:32 +0800

    fm_click

---
+++ src/main/scala/com/cpc/spark/common/FmClickData.scala
@@ -0,0 +1,16 @@
+package com.cpc.spark.common
+
+import data.Mlevent
+
+/**
+  * Created on ${Date} ${Time}
+  */
+object FmClickData {
+  def parseData(line:Array[Byte]):LogData = {
+    new LogData(line)
+  }
+}
+
+class LogData(data :Array[Byte] ){
+  val log =
+}
---
+++ src/main/scala/com/cpc/spark/streaming/anal/CpcStreamingFmClickParser.scala
@@ -0,0 +1,176 @@
+package com.cpc.spark.streaming.anal
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import com.cpc.spark.common.LogData
+import com.cpc.spark.streaming.tools.OffsetRedis
+import kafka.common.TopicAndPartition
+import kafka.message.MessageAndMetadata
+import kafka.serializer.{DefaultDecoder, StringDecoder}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, TaskContext}
+import org.apache.spark.streaming.dstream.InputDStream
+import org.apache.spark.streaming.kafka.KafkaCluster.LeaderOffset
+import org.apache.spark.streaming.kafka.{HasOffsetRanges, KafkaCluster, KafkaUtils, OffsetRange}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+
+/**
+  * Created on ${Date} ${Time}
+  */
+object CpcStreamingFmClickParser {
+
+  val offsetRedis = new OffsetRedis()
+  offsetRedis.setRedisKey("FM_CLICK_LOG_KAFKA_OFFSET")
+
+  def main(args: Array[String]): Unit = {
+    if (args.length < 4) {
+      System.err.println(
+        s"""
+           |Usage: CpcStreamingAnal <brokers> <topics> <out_topics>
+           |  <brokers> is a list of one or more Kafka brokers
+           |  <topics> is a list of one or more kafka topics to consume from
+           |  <outTable> is the out table
+        """.stripMargin)
+      System.exit(1)
+    }
+
+    Logger.getRootLogger.setLevel(Level.WARN)
+    val Array(brokers, topics, seconds, outTable) = args
+
+    val sparkConf = new SparkConf().setAppName("topic:fm_click = " + topics)
+    val ssc = new StreamingContext(sparkConf, Seconds(seconds.toInt))
+
+    val topicsSet = topics.split(",").toSet
+    val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
+
+
+    var fromOffsets: Map[TopicAndPartition, Long] = Map()
+    val messageHandler = (mmd: MessageAndMetadata[String, Array[Byte]]) => (mmd.topic, mmd.message())
+    try {
+      for (topic <- topicsSet) {
+        val partitions = offsetRedis.getPartitionByTopic(topic)
+        for (partition <- partitions) {
+          println("topic:" + topic + ";partition:" + partition)
+          val tp = TopicAndPartition(topic, partition.toInt)
+          fromOffsets += (tp -> offsetRedis.getTopicAndPartitionOffSet(topic, partition.toInt))
+        }
+      }
+    } catch {
+      case t: Exception => System.err.println("connect redis exception" + t.getStackTrace)
+        fromOffsets = Map()
+    }
+    println("***************redis fromOffsets*******************")
+    println(fromOffsets)
+
+
+    var kafkaoffset: Map[TopicAndPartition, Long] = null
+    if (fromOffsets.nonEmpty) {
+      kafkaoffset = getKafkaTopicAndPartitionOffset(topicsSet, kafkaParams)
+      println("***************from kafka offsets*******************")
+      println(kafkaoffset)
+      kafkaoffset.foreach(tp => {
+        try {
+          //可能key不存在
+          if (fromOffsets.contains(tp._1)) {
+            if (fromOffsets(tp._1) < tp._2) {
+              kafkaoffset += (tp._1 -> fromOffsets(tp._1))
+
+            }
+          }
+
+        } catch {
+          case t: Throwable =>
+            t.printStackTrace()
+            println("set kafkaoffset key filled" + t.printStackTrace()) // TODO: handle error
+        }
+      })
+    }
+    println("***************last offsets*******************")
+    println(kafkaoffset)
+
+
+    var messages: InputDStream[(String, Array[Byte])] = null
+
+    if (kafkaoffset == null || kafkaoffset.isEmpty) {
+      messages = KafkaUtils.createDirectStream[String, Array[Byte], StringDecoder, DefaultDecoder](ssc, kafkaParams, topicsSet)
+      println("no offset")
+    } else {
+      messages = KafkaUtils.createDirectStream[String, Array[Byte], StringDecoder, DefaultDecoder, (String, Array[Byte])](ssc, kafkaParams, kafkaoffset, messageHandler)
+      println("from offset")
+    }
+
+    messages.foreachRDD {
+      rdd => {
+        val offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+        rdd.foreachPartition { iter =>
+          try {
+            val o: OffsetRange = offsetRanges(TaskContext.get.partitionId)
+            offsetRedis.setTopicAndPartitionOffSet(o.topic, o.partition, o.fromOffset)
+          } catch {
+            case t: Throwable =>
+              t.printStackTrace()
+              println("connect redis exception in offsetRanges " + t.getStackTrace)
+          }
+
+        }
+      }
+    }
+    getCurrentDate("end-offsetRanges")
+
+    messages.map {
+      case (k, v) =>
+        try {
+          val logData = FmClickData
+
+
+        } catch {
+          case t: Throwable =>
+            t.printStackTrace()
+            null
+        }
+    }
+
+  }
+
+
+  def getKafkaTopicAndPartitionOffset(topicsSet: Set[String], kafkaParams: Map[String, String]): Map[TopicAndPartition, Long] = {
+    var kafkaCluster = new KafkaCluster(kafkaParams)
+    var topicAndPartitions = Set[TopicAndPartition]()
+    var fromOffsets: Map[TopicAndPartition, Long] = Map()
+    val partitions: Either[KafkaCluster.Err, Set[TopicAndPartition]] = kafkaCluster.getPartitions(topicsSet)
+    partitions match {
+      case Left(x) => System.err.println("kafka getPartitions error" + x); System.exit(1)
+      case Right(x) =>
+        for (partition <- x) {
+          topicAndPartitions += partition
+        }
+    }
+    println("***************from kafka TopicAndPartition*******************")
+    println(topicAndPartitions)
+    val consumerOffset: Either[KafkaCluster.Err, Map[TopicAndPartition, LeaderOffset]] = kafkaCluster.getLatestLeaderOffsets(topicAndPartitions)
+    consumerOffset match {
+      case Left(x) => System.err.println("kafka getConsumerOffsets error" + x); System.exit(1)
+      case Right(x) =>
+        x.foreach(
+          tp => {
+            fromOffsets += (tp._1 -> tp._2.offset)
+          }
+        )
+    }
+    fromOffsets
+  }
+
+  def getCurrentDate(message: String) {
+    var now: Date = new Date()
+    var dateFormat: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    var hehe = dateFormat.format(now)
+    if (message.length() > 0) {
+      println("currentDate:" + message + ":" + hehe)
+    } else {
+      print("currentDate:" + hehe)
+    }
+  }
+
+}
