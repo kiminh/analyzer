1f97d5af9c430f1720ba16a0b0557d3e6e29c2f3
zhanghongyang <zhanghongyang@qutoutiao.net>
2018-11-06 17:39:59 +0800

    cvr test

--- src/main/scala/com/cpc/spark/ml/ctrmodel/hourly/SaveFeatures.scala
+++ src/main/scala/com/cpc/spark/ml/ctrmodel/hourly/SaveFeatures.scala
@@ -355,13 +355,13 @@ object SaveFeatures {
          |       ,a.ideaid
          |       ,b.*
          |from (select * from dl_cpc.cpc_union_log
-         |        where `date` = "%s" and `hour` = "%s" and searchid is not null and searchid != "" and isclick > 0) a
+         |        where `date` = "%s" and `hour` = "%s" and searchid is not null and searchid != "") a
          |    left join (select id from bdm.cpc_userid_test_dim where day='%s') t2
          |        on a.userid = t2.id
          |    left join
          |        (select *
-         |            from dl_cpc.logparsed_cpc_trace_minute
-         |            where `thedate` = "%s" and `thehour` = "%s"
+         |            from dl_cpc.cpc_union_trace_log
+         |            where `date` = "%s" and `hour` = "%s"
          |         ) b
          |    on a.searchid=b.searchid
          | where t2.id is null
