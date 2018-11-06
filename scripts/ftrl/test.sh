de3090e8f4f12bbce90d3519092cb827a2632dca
wangyao <wangyao@qutoutiao.net>
2018-11-06 15:51:24 +0800

    hh

--- scripts/ftrl/test.sh
+++ scripts/ftrl/test.sh
@@ -5,5 +5,7 @@
 #sh testFtrlNew.sh 2018-09-15 11 interact-ctr 9 19 1 0
 #sh testFtrlNew.sh 2018-09-15 11 all-cvr 9 19 1 0
 #sh testFtrlNew.sh 2018-10-31 07 list-cvr2 19 19 1 0
-sh testFtrlNew.sh 2018-10-31 07 content-cvr2 19 19 1 0
+#sh testFtrlNew.sh 2018-10-31 07 content-cvr2 19 19 1 0
 #sh testFtrlNew.sh 2018-10-30 12 interact-cvr2 19 19 1 0
+#sh testFtrlNew.sh 22018-11-06 10 novel-ctr 19 20 1 0
+#sh testFtrlNew.sh 2018-11-06 10 novel-cvr2 19 20 1 0
