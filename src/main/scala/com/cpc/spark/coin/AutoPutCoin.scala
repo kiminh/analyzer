da5192b707020dedf5ea9b8e98b9071b2cf6a611
dongjinbao <dongjinbao@qutoutiao.net>
2018-11-07 20:11:24 +0800

    test

--- src/main/scala/com/cpc/spark/coin/AutoPutCoin.scala
+++ src/main/scala/com/cpc/spark/coin/AutoPutCoin.scala
@@ -23,15 +23,19 @@ object AutoPutCoin {
 
         val p = 0.7
 
+        val preDay = 3
+
         val spark = SparkSession.builder()
           .appName(s"AutoPutCoin date = $date, hour = $hour")
           .enableHiveSupport()
           .getOrCreate()
 
+        import spark.implicits._
+
         val datehourlist = scala.collection.mutable.ListBuffer[String]()
         val cal = Calendar.getInstance()
         cal.set(date.substring(0, 4).toInt, date.substring(5, 7).toInt - 1, date.substring(8, 10).toInt, hour, minute)
-        for (t <- 0 to 24*1) {
+        for (t <- 0 to 24 * preDay) {
             if (t > 0) {
                 cal.add(Calendar.HOUR, -1)
             }
@@ -59,40 +63,37 @@ object AutoPutCoin {
         val apiUnionLog = spark.sql(apiUnionLogSql)
         println("apiUnionLog 's count is " + apiUnionLog.rdd.count())
         val apiUnionNth = getNth(apiUnionLog,p)
+
         println("apiUnionNth 's count is " + apiUnionNth.count())
         val mlFeatureSql =
             s"""
-               |select distinct searchid
-               |from dl_cpc.ml_cvr_feature_v1
+               |select ideaid,ext["exp_cvr"].int_value as exp_cvr
+               |from test.ml_cvr_feature_v1
                |where ($datehour)
                |and label2 = 1
+               |and media_appsid in ('80000001','80000002')
+               |and adslot_type in (1, 2, 3)
+               |and ideaid > 0
              """.stripMargin
 
         val mlFeature = spark.sql(mlFeatureSql)
         println("mlFeature 's count is " + mlFeature.rdd.count())
-        val unionLogSql =
-            s"""
-               |select searchid, ideaid,ext["exp_cvr"].int_value as exp_cvr1
-               |from dl_cpc.cpc_union_log
-               |where ($datehour)
-               |and media_appsid in ('80000001','80000002')
-               |and ideaid > 0
-               |and adslot_type in (1, 2, 3)
-             """.stripMargin
 
-        val unionLog = spark.sql(unionLogSql)
-        println("unionLog 's count is " + unionLog.rdd.count())
-        val cols = Array("ideaid", "exp_cvr1")
+        val mlFeatureNth = getNth(mlFeature, p)
 
-        val unionLogFilter = unionLog.join(mlFeature,Seq("searchid"),"inner").select("ideaid", "exp_cvr1")
-        //  .select(cols.map(col):_*)
-        unionLogFilter.show(2)
-        println("unionLogFilter 's count is " + unionLogFilter.rdd.count())
-        val unionNth = getNth(unionLogFilter, p)
-        println("unionNth 's count is " + unionNth.count())
-        val Nth = unionNth.join(apiUnionNth)
+        val Nth = mlFeatureNth.join(apiUnionNth)
+          .map(x => coin(ideaid = x._1,
+              label_exp_cvr = x._2._1,
+              api_exp_cvr = x._2._1,
+              date = date,
+              hour = hour.toString))
+          .toDS()
+
+        Nth.write.mode("overwrite").insertInto("test.coin")
 
         println("Nth 's count is " + Nth.count())
+
+        spark.stop()
     }
     def getNth(df: DataFrame,p:Double): RDD[(Int,Int)] = {
         df.rdd.map(x => (x.getAs[Int]("ideaid"), x.getAs[Int]("exp_cvr")))
@@ -105,4 +106,10 @@ object AutoPutCoin {
                 x(index)
             })
     }
+
 }
+case class coin(var ideaid:Int = 0,
+                var label_exp_cvr: Int = 0,
+                var api_exp_cvr: Int = 0,
+                var date:String = "",
+                var hour:String = "")
