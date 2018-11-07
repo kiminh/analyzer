3c5352ba1913d3327442d34cee1faf0ec6aa3a8a
wangjun <wangjun02@qutoutiao.net>
2018-11-07 13:58:00 +0800

    -

---
+++ src/main/scala/com/cpc/spark/ocpc/OcpcRunOver.scala
@@ -0,0 +1,33 @@
+package com.cpc.spark.ocpc
+
+import com.cpc.spark.ocpc.OcpcSampleToRedis.savePbPack
+import org.apache.spark.sql.SparkSession
+
+
+object OcpcRunOver {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().enableHiveSupport().getOrCreate()
+
+    val date = args(0).toString
+    val hour = args(1).toString
+
+    val sqlRequest =
+      s"""
+         |SELECT
+         |  *
+         |FROM
+         |  dl_cpc.ocpc_pb_result_table_v4
+         |WHERE
+         |  `date` = '$date'
+         |AND
+         |  `hour` = '$hour'
+       """.stripMargin
+
+    println(sqlRequest)
+    val data = spark.sql(sqlRequest)
+
+    // 保存pb文件
+    savePbPack(data)
+  }
+  
+}
