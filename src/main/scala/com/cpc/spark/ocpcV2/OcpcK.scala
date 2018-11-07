5f982091764fe9f9cc1425900234220f07d04363
dongjiaqi@aiclk.com <dongjiaqi@aiclk.com>
2018-11-07 23:13:02 +0800

    - ocpc v2

--- src/main/scala/com/cpc/spark/ocpcV2/OcpcK.scala
+++ src/main/scala/com/cpc/spark/ocpcV2/OcpcK.scala
@@ -10,9 +10,7 @@ object OcpcK {
 
     // val date = args(0).toString
     // val hour = args(1).toString
-    // val onDuty = args(2).toInt // onDuty=1表示部署模型，onDuty!=1表示测试新代码
-
-    // TODO ideaid与userid的名称
+    // val onDuty = args(2).toInt
 
     val dtCondition = "`date` = '2018-11-07' and hour in ('09','10','11')"
 
@@ -22,17 +20,17 @@ object OcpcK {
         |  ideaid,
         |  ocpc_log_dict['kvalue'] k,
         |  ocpc_log_dict['cpagiven'] cpagiven,
-        |  sum(if(isclick=1,price,0))/sum(label2) as cpa2
-        |  sum(if(isclick=1,price,0))/sum(label3) as cpa3
-        |  sum(if(isclick=1,price,0))/sum(label2)/ocpc_log_dict['cpagiven'] as ratio2
+        |  sum(if(isclick=1,price,0))/sum(label2) as cpa2,
+        |  sum(if(isclick=1,price,0))/sum(label3) as cpa3,
+        |  sum(if(isclick=1,price,0))/sum(label2)/ocpc_log_dict['cpagiven'] as ratio2,
         |  sum(if(isclick=1,price,0))/sum(label3)/ocpc_log_dict['cpagiven'] as ratio3
         |from
         |  (select * from dl_cpc.ocpc_unionlog where $dtCondition and ocpc_log_dict['kvalue'] !=null) a
         |  left outer join
-        |  (select searchid, label2 as iscvr from dl_cpc.ml_cvr_feature_v1 where $dtCondition) b on a.searchid = b.searchid
+        |  (select searchid, label2 from dl_cpc.ml_cvr_feature_v1 where $dtCondition) b on a.searchid = b.searchid
         |  left outer join
         |  (select searhcid, iscvr as label3 from dl_cpc.cpc_api_union_log where $dtCondition) c on a.searchid = c.searchid
-        |group by ideaid, ocpc_log_dict['kvalue'], ocpc_log_dict['cpagiven'] cpagiven,
+        |group by ideaid, ocpc_log_dict['kvalue'], ocpc_log_dict['cpagiven']
       """.stripMargin
 
       println(statSql)
