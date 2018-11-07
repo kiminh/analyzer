91afcaa58d505984221e8ad14e1710dad9a6988d
dongjiaqi@aiclk.com <dongjiaqi@aiclk.com>
2018-11-07 23:38:51 +0800

    - ocpc v2

--- src/main/scala/com/cpc/spark/ocpcV2/OcpcK.scala
+++ src/main/scala/com/cpc/spark/ocpcV2/OcpcK.scala
@@ -24,7 +24,10 @@ object OcpcK {
         |  sum(if(isclick=1,price,0))/sum(COALESCE(label2,0)) as cpa2,
         |  sum(if(isclick=1,price,0))/sum(COALESCE(label3,0)) as cpa3,
         |  sum(if(isclick=1,price,0))/sum(COALESCE(label2,0))/ocpc_log_dict['cpagiven'] as ratio2,
-        |  sum(if(isclick=1,price,0))/sum(COALESCE(label3,0))/ocpc_log_dict['cpagiven'] as ratio3
+        |  sum(if(isclick=1,price,0))/sum(COALESCE(label3,0))/ocpc_log_dict['cpagiven'] as ratio3,
+        |  sum(isclick) clickCnt,
+        |  sum(COALESCE(label2,0) cvr2Cnt,
+        |  sum(COALESCE(label3,0) cvr3Cnt
         |from
         |  (select * from dl_cpc.ocpc_unionlog where $dtCondition2 and ocpc_log_dict['kvalue'] is not null and isclick=1) a
         |  left outer join
