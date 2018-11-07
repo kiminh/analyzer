27b04e1606a4530c5cfdcef2a3126fc1eba406de
dongjiaqi@aiclk.com <dongjiaqi@aiclk.com>
2018-11-08 00:42:37 +0800

    - ocpc v2

--- src/main/scala/com/cpc/spark/ocpcV2/OcpcK.scala
+++ src/main/scala/com/cpc/spark/ocpcV2/OcpcK.scala
@@ -19,7 +19,7 @@ object OcpcK {
       s"""
         |select
         |  ideaid,
-        |  ocpc_log_dict['kvalue'] * ocpc_log_dict['cali'] as k,
+        |  round(ocpc_log_dict['kvalue'] * ocpc_log_dict['cali'] * 100.0) as k,
         |  ocpc_log_dict['cpagiven'] as cpagiven,
         |  sum(if(isclick=1,price,0))/sum(COALESCE(label2,0)) as cpa2,
         |  sum(if(isclick=1,price,0))/sum(COALESCE(label3,0)) as cpa3,
@@ -34,7 +34,7 @@ object OcpcK {
         |  (select searchid, label2 from dl_cpc.ml_cvr_feature_v1 where $dtCondition) b on a.searchid = b.searchid
         |  left outer join
         |  (select searchid, iscvr as label3 from dl_cpc.cpc_api_union_log where $dtCondition) c on a.searchid = c.searchid
-        |group by ideaid, ocpc_log_dict['kvalue'] * ocpc_log_dict['cali'] , ocpc_log_dict['cpagiven']
+        |group by ideaid, round(ocpc_log_dict['kvalue'] * ocpc_log_dict['cali'] * 100.0) , ocpc_log_dict['cpagiven']
       """.stripMargin
 
       println(statSql)
