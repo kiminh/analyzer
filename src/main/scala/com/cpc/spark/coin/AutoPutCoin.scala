16a9e3c4f42884b108eefaf84e9bc96a93ad5a79
dongjinbao <dongjinbao@qutoutiao.net>
2018-11-07 20:59:26 +0800

    test

--- src/main/scala/com/cpc/spark/coin/AutoPutCoin.scala
+++ src/main/scala/com/cpc/spark/coin/AutoPutCoin.scala
@@ -82,21 +82,40 @@ object AutoPutCoin {
         val mlFeatureNth = getNth(mlFeature, p)
 
 
-        val Nth = mlFeatureNth.join(apiUnionNth)
-          .map(x => coin(ideaid = x._1,
-              label_exp_cvr = x._2._1,
-              api_exp_cvr = x._2._2,
-              date = date,
-              hour = hour.toString))
+        val Nth = mlFeatureNth.fullOuterJoin(apiUnionNth)
+            .map(x => coin(ideaid = x._1,
+                label_exp_cvr = x._2._1._1,
+                label_min = x._2._1._2,
+                label_max = x._2._1._3,
+                label_num = x._2._1._4,
+                label_5th = x._2._1._5,
+                label_6th = x._2._1._6,
+                label_7th = x._2._1._7,
+                label_8th = x._2._1._8,
+                label_9th = x._2._1._9,
+
+                api_exp_cvr = x._2._2._1,
+                api_min = x._2._2._2,
+                api_max = x._2._2._3,
+                api_num = x._2._2._4,
+                api_5th = x._2._2._5,
+                api_6th = x._2._2._6,
+                api_7th = x._2._2._7,
+                api_8th = x._2._2._8,
+                api_9th = x._2._2._9,
+
+                date = date,
+                hour = hour.toString)
+            )
           .toDS()
 
-        Nth.write.mode("overwrite").insertInto("test.coin")
+        Nth.write.mode("overwrite").insertInto("test.coin2")
 
         println("Nth 's count is " + Nth.count())
 
         spark.stop()
     }
-    def getNth(df: DataFrame,p:Double): RDD[(Int,Int)] = {
+    def getNth(df: DataFrame,p:Double): RDD[(Int,(Int, Int, Int, Int, Int, Int, Int, Int, Int))] = {
         df.rdd.map(x => (x.getAs[Int]("ideaid"), x.getAs[Int]("exp_cvr")))
           .combineByKey(x => List(x),
             (x:List[Int], y:Int) => y::x,
@@ -104,13 +123,36 @@ object AutoPutCoin {
             .mapValues(x => {
                 val sorted = x.sorted
                 val index = (x.length * p).toInt
-                x(index)
+                val i5th = (x.length * 0.5).toInt
+                val i6th = (x.length * 0.6).toInt
+                val i7th = (x.length * 0.7).toInt
+                val i8th = (x.length * 0.8).toInt
+                val i9th = (x.length * 0.9).toInt
+                (x(index), x(0),x(x.length - 1), x.length, x(i5th), x(i6th), x(i7th), x(i8th), x(i9th))
+                //x(index)
             })
     }
 
 }
 case class coin(var ideaid:Int = 0,
                 var label_exp_cvr: Int = 0,
+                var label_min: Int = 0,
+                var label_max: Int = 0,
+                var label_num: Int = 0,
+                var label_5th: Int = 0,
+                var label_6th: Int = 0,
+                var label_7th: Int = 0,
+                var label_8th: Int = 0,
+                var label_9th: Int = 0,
+
                 var api_exp_cvr: Int = 0,
+                var api_min: Int = 0,
+                var api_max: Int = 0,
+                var api_num: Int = 0,
+                var api_5th: Int = 0,
+                var api_6th: Int = 0,
+                var api_7th: Int = 0,
+                var api_8th: Int = 0,
+                var api_9th: Int = 0,
                 var date:String = "",
                 var hour:String = "")
