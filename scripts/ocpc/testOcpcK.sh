184b1cbedf2bc8b9b9b507e92825cc1e52b43b6f
wangjun <wangjun02@qutoutiao.net>
2018-11-08 16:06:36 +0800

    fix bug

--- scripts/ocpc/testOcpcK.sh
+++ scripts/ocpc/testOcpcK.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-hdfsCur=hdfs://emr-cluster/warehouse/azkaban
+cur=/data/cpc/anal
 SPARK_HOME=/usr/lib/spark-current
-localCur=/data/cpc/anal
-set -e
-cur_time=$1
-date=`date -d"${cur_time} 3 hours ago" +"%Y-%m-%d"`
-hour=`date -d"${cur_time} 3 hours ago" +"%H"`
-queue=root.production.algo.cpc
+queue=root.develop.adhoc.cpc
 
 jars=(
-    "$localCur/lib/mysql-connector-java-5.1.41-bin.jar"
-    "$localCur/lib/hadoop-lzo-0.4.20.jar"
-    "$localCur/lib/config-1.2.1.jar"
+    "$cur/lib/mysql-connector-java-5.1.41-bin.jar"
+    "$cur/lib/hadoop-lzo-0.4.20.jar"
+    "$cur/lib/config-1.2.1.jar"
 )
-
-randjar="tmp_"`date +%s%N`".jar"
-hadoop fs -get $hdfsCur/lib/ocpc-data-userprofile.jar $randjar
 
 $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.port.maxRetries=100' \
@@ -26,6 +18,4 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
     --class com.cpc.spark.ocpcV2.OcpcK \
-    $randjar $date $hour
-
-rm $randjar
+    /home/cpc/wangjun/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $1 $2
