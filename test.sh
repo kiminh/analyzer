b9a32dc2756a96db691563bf23cafee04010b380
wangyao <wangyao@qutoutiao.net>
2018-11-08 18:02:40 +0800

    hh

--- test.sh
+++ test.sh
@@ -21,4 +21,4 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
     --class com.cpc.spark.ml.novel.UserBehaviorNovel \
-    /home/cpc/wy/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar 2018-11-06
+    /home/cpc/wy/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar 2018-11-05
