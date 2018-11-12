29905f9107a8472c27ad446d58fd8554b7cc66b6
wangjun <wangjun02@qutoutiao.net>
2018-11-12 11:32:37 +0800

    fix bug

--- src/main/scala/com/cpc/spark/ocpc/OcpcActivationDataV1.scala
+++ src/main/scala/com/cpc/spark/ocpc/OcpcActivationDataV1.scala
@@ -54,4 +54,50 @@ object OcpcActivationDataV1 {
 
 //    resultDF.write.mode("overwrite").saveAsTable("test.ocpc_ideaid_adclass_label3_track_v1")
   }
+
+
+  def getDataHourlyV1(date: String, hour: String, spark: SparkSession) :Unit = {
+    /**
+      * ctr数据：cpc_union_log
+      * cvr数据：ml_cvr_feature_v2
+      */
+    var selectWhere = s"`date`='$date' and hour = '$hour'"
+    var sqlRequest =
+      s"""
+         |SELECT
+         |  ideaid,
+         |  ext['adclass'].int_value as adclass,
+         |  price,
+         |  isclick
+         |FROM
+         |  dl_cpc.cpc_union_log
+         |WHERE
+         |  $selectWhere
+         |AND isclick is not null
+         |and media_appsid  in ("80000001", "80000002")
+         |and isshow = 1
+         |and ext['antispam'].int_value = 0
+         |and ideaid > 0
+         |and adsrc = 1
+         |and adslot_type in (1,2,3)
+      """.stripMargin
+    println(sqlRequest)
+    val base = spark.sql(sqlRequest).filter("isclick=1")
+
+    val resultDF = base
+      .groupBy("ideaid", "adclass")
+      .agg(
+        sum(col("price")).alias("cost"),
+        sum(col("isclick")).alias("ctr_cnt"),
+        sum(col("isact")).alias("cvr_cnt"))
+      .select("ideaid", "adclass", "cost", "ctr_cnt", "cvr_cnt")
+      .withColumn("date", lit(date))
+      .withColumn("hour", lit(hour))
+
+    //    resultDF.write.mode("overwrite").saveAsTable("test.ocpc_ideaid_adclass_label3_track")
+
+    resultDF.write.mode("overwrite").insertInto("dl_cpc.ocpc_ideaid_adclass_label3_track_v1")
+
+    //    resultDF.write.mode("overwrite").saveAsTable("test.ocpc_ideaid_adclass_label3_track_v1")
+  }
 }
