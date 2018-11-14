86beb85a9d34e84b2873f93c57d6ee66db986146
zhanghongyang <zhanghongyang@qutoutiao.net>
2018-11-14 13:51:44 +0800

    topApps top100

--- src/main/scala/com/cpc/spark/qukan/userprofile/TopApps.scala
+++ src/main/scala/com/cpc/spark/qukan/userprofile/TopApps.scala
@@ -2,6 +2,7 @@ package com.cpc.spark.qukan.userprofile
 
 import com.cpc.spark.qukan.parser.HdfsParser
 import org.apache.spark.sql.{SaveMode, SparkSession}
+import com.cpc.spark.common.Utils.sendMail
 
 /**
   * Created by roydong on 12/07/2018.
@@ -48,6 +49,29 @@ object TopApps {
       """.stripMargin.format(date, date))
 
     pkgs.take(100).foreach(println)
+
+    val topApps_top100 = spark.sql(
+      s"""
+         |select pkg, install_user_num
+         |from dl_cpc.top_apps
+         |where \`date\`='${date}'
+         |sort by install_user_num desc
+         |limit 10
+      """.stripMargin)
+      .collect()
+
+    var txt = ""
+    for (i <- 0 until topApps_top100.length) {
+      val row = topApps_top100(i)
+      txt = txt + "%-30s%-15s\n".format(row.getAs[String]("pkg"), row.getAs[Long]("install_user_num").toString)
+
+    }
+
+    val b = sendMail(txt, "'$date' topApps 活跃用户数top100", Seq("zhanghongyang@aiclk.com")
+    if (!b) {
+      println("发送邮件失败")
+    }
+
   }
 }
 
