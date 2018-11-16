6213a952bb044a5df911098e5307398b6ca1386b
wangjun <wangjun02@qutoutiao.net>
2018-11-16 15:29:25 +0800

    save test table

--- src/main/scala/com/cpc/spark/ocpc/report/OcpcDataDetail.scala
+++ src/main/scala/com/cpc/spark/ocpc/report/OcpcDataDetail.scala
@@ -61,7 +61,7 @@ object OcpcDataDetail {
 
 //    data.show(10)
     // TODO 删除临时表
-//    data.write.mode("overwrite").saveAsTable("test.test_ocpc_export_hourly_report")
+    data.write.mode("overwrite").saveAsTable("test.test_ocpc_export_hourly_report")
 
     // 输出结果
     val result = data.select("user_id", "idea_id", "conversion_goal", "step2_click_percent", "is_step2", "cpa_given", "cpa_real", "cpa_ratio", "is_cpa_ok", "impression", "click", "conversion", "ctr", "click_cvr", "show_cvr", "cost", "acp", "avg_k", "recent_k", "date", "hour")
