3633390508c6de4b9fc40e938da0197937ea0661
wangyao <wangyao@qutoutiao.net>
2018-11-19 15:04:20 +0800

    -

--- test.sh
+++ test.sh
@@ -23,4 +23,4 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
     --class com.cpc.spark.ml.novel.Behavior2RedisNovelCvr \
-    /home/cpc/wy/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar 2018-11-15
+    /home/cpc/wy/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar 2018-11-16
