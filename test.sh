a6bb8400c252e786313e37102a6d17be0c2b914e
wangyao <wangyao@qutoutiao.net>
2018-11-22 19:57:15 +0800

    -

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 cur=/data/cpc/anal
 SPARK_HOME=/usr/lib/spark-current
 queue=root.develop.adhoc.cpc
-date=`date +"%Y-%m-%d" -d "-1day"`
+#date=`date +"%Y-%m-%d" -d "-1day"`
 day=$1
 hour=$2
 
@@ -25,4 +25,4 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
     --class com.cpc.spark.ml.novel.NovelDNNSampleHourlyV3 \
-    /home/cpc/wy/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $date $hour
+    /home/cpc/wy/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $day $hour
