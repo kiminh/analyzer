cccf93e4558c0c7c36bb25d88e3c3a3e7b53ad71
wangjun <wangjun02@qutoutiao.net>
2018-11-24 15:09:02 +0800

    add test code

--- src/main/scala/com/cpc/spark/ocpcV3/ocpcNovel/data/OcpcLabelCvr3.scala
+++ src/main/scala/com/cpc/spark/ocpcV3/ocpcNovel/data/OcpcLabelCvr3.scala
@@ -13,7 +13,10 @@ object OcpcLabelCvr3 {
 
     // TODO  测试
     val result1 = getLabelFromAdv(date, hour, spark)
-    result1.write.mode("overwrite").saveAsTable("test.ocpcv3_base_data_part4_bak1")
+    result1.write.mode("overwrite").saveAsTable("test.ocpcv3_base_data_part4")
+    // TODO 测试
+//    val result2 = getLabel(date, hour, spark)
+//    result2.write.mode("overwrite").saveAsTable("test.ocpcv3_base_data_part4")
   }
 
   def getLabelFromAdv(date: String, hour: String, spark: SparkSession) = {
@@ -63,12 +66,72 @@ object OcpcLabelCvr3 {
     val resultDF = rawData
       .join(labelData, Seq("searchid"), "left_outer")
       .groupBy("ideaid", "adclass", "media_appsid")
-      .agg(sum(col("label")).alias("cvr2_cnt"))
-      .select("ideaid", "adclass", "media_appsid", "cvr2_cnt")
+      .agg(sum(col("label")).alias("cvr3_cnt"))
+      .select("ideaid", "adclass", "media_appsid", "cvr3_cnt")
 
     resultDF.show(10)
     resultDF.printSchema()
 
     resultDF
   }
+
+  def getLabel(date: String, hour: String, spark: SparkSession) = {
+    var selectWhere = s"`date`='$date' and hour = '$hour'"
+
+    var sqlRequest1 =
+      s"""
+         |select
+         |    searchid,
+         |    uid,
+         |    ideaid,
+         |    price,
+         |    userid,
+         |    media_appsid,
+         |    ext['adclass'].int_value as adclass,
+         |    ext['exp_cvr'].int_value * 1.0 / 1000000 as exp_cvr,
+         |    isclick,
+         |    isshow
+         |from dl_cpc.cpc_union_log
+         |where $selectWhere
+         |and isclick is not null
+         |and media_appsid in ("80001098","80001292","80000001", "80000002")
+         |and isshow = 1
+         |and ext['antispam'].int_value = 0
+         |and ideaid > 0
+         |and adsrc = 1
+         |and adslot_type in (1,2,3)
+         |and ext_int['is_api_callback']!=1
+      """.stripMargin
+    println(sqlRequest1)
+    val rawData = spark.sql(sqlRequest1)
+    rawData.createOrReplaceTempView("raw_table")
+
+    val sqlRequest2 =
+      s"""
+         |SELECT
+         |  searchid,
+         |  label2 as label
+         |FROM
+         |  dl_cpc.ml_cvr_feature_v1
+         |WHERE
+         |  where $selectWhere
+         |AND
+         |  label2=1
+       """.stripMargin
+    println(sqlRequest2)
+    val labelData = spark.sql(sqlRequest2)
+
+    val resultDF = rawData
+      .join(labelData, Seq("searchid"), "left_outer")
+      .groupBy("ideaid", "adclass", "media_appsid")
+      .agg(sum(col("label")).alias("cvr3_cnt"))
+      .select("ideaid", "adclass", "media_appsid", "cvr3_cnt")
+
+
+    resultDF.show(10)
+    resultDF.printSchema()
+
+    resultDF
+  }
+
 }
