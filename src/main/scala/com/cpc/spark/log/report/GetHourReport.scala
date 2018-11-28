220839a912632c4830517cd783dce0c7c74d90d1
zhanghongyang <zhanghongyang@qutoutiao.net>
2018-11-28 19:15:38 +0800

    fix

--- src/main/scala/com/cpc/spark/log/report/GetHourReport.scala
+++ src/main/scala/com/cpc/spark/log/report/GetHourReport.scala
@@ -454,6 +454,7 @@ object GetHourReport {
          |       ,a.ext_int['siteid'] as siteid
          |       ,a.adsrc
          |       ,a.interaction
+         |       ,1 as flag3
          |       ,b.*
          |from (select * from dl_cpc.cpc_union_log
          |        where `date` = "%s" and `hour` = "%s" ) a
