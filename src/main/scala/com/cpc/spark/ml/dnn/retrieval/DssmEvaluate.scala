29e719867b632eae8840f4b2f0495dc3cdcd40a4
华祯豪 <huazhenhao@qutoutiao.net>
2018-11-28 11:37:15 +0800

    update

---
+++ src/main/scala/com/cpc/spark/ml/dnn/retrieval/DssmEvaluate.scala
@@ -0,0 +1,96 @@
+package com.cpc.spark.ml.dnn.retrieval
+
+import com.cpc.spark.ml.dnn.retrieval.UserEmbeddingToRedis.{hdfsDir => userHDFSDir}
+import com.cpc.spark.ml.dnn.retrieval.AdEmbeddingToFile.{hdfsDir => adHDFSDir}
+import com.redis.RedisClient
+import mlmodel.mlmodel.RetrievalEmbedding
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+import scala.collection.mutable
+
+/**
+  * author: huazhenhao
+  * date: 11/28/18
+  */
+object DssmEvaluate {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().appName("dssm-ad-eval").enableHiveSupport().getOrCreate()
+    import spark.implicits._
+
+    val date = args(0)
+
+    val labels = getLabels(date, spark)
+    val userEmbedding = getUserEmbedding(date, spark)
+    val adEmbeding = getAdEmbedding(date, spark)
+    labels.join(userEmbedding, Seq("uid"), "left")
+      .join(adEmbeding, Seq("ideaid"), "left")
+      .rdd.map(x => {
+      val userEmbedding = x.getAs[mutable.WrappedArray[Double]]("user_embedding")
+      val adEmbedding = x.getAs[mutable.WrappedArray[Double]]("ad_embedding")
+      val userisNull = if (userEmbedding == null) 1 else 0
+      val adIsNull = if (adEmbedding == null) 1 else 0
+      val clickCount = x.getAs[Long]("click")
+      val score = if (userEmbedding == null || adEmbedding == null) 0 else {
+        var sum = 0.0
+        for (i <- userEmbedding.indices) {
+          sum = sum + userEmbedding(i) * adEmbedding(i)
+        }
+        sum
+      }
+      (clickCount, score, userisNull, adIsNull)
+    }).toDF("clickCount", "score", "userNull", "adNull").write.mode("overwrite")
+      .insertInto("dl_cpc.dssm_eval")
+  }
+
+  def getLabels(date: String, spark: SparkSession): DataFrame = {
+    val sql =
+      s"""
+         |select
+         |  uid as uid,
+         |  ideaid as ideaid,
+         |  sum(isclick) as click
+         |from dl_cpc.cpc_union_log where `date` = '$date' and hour = '20'
+         |  and isshow = 1 and ideaid > 0 and adslot_type = 1
+         |  and media_appsid in ("80000001", "80000002")
+         |  and uid not like "%.%"
+         |  and uid not like "%000000%"
+         |  and uid > 0
+         |group by uid, ideaid
+      """.stripMargin
+    println("--------------------------------")
+    println(sql)
+    println("--------------------------------")
+    spark.sql(sql)
+  }
+
+  def getUserEmbedding(date: String, spark: SparkSession): DataFrame = {
+    import spark.implicits._
+    val data = spark.read.parquet(userHDFSDir + date)
+    data.rdd.map(
+      x => {
+        val uid = x.getAs[Array[Byte]](64).map(_.toChar).mkString
+        val embedding = new Array[Double](64)
+        for (i <- 0 to 63) {
+           embedding(i) = x.getAs[Number](i).doubleValue()
+        }
+        (uid, embedding)
+      }
+    ).toDF("uid", "user_embedding")
+  }
+
+  def getAdEmbedding(date: String, spark: SparkSession): DataFrame = {
+    import spark.implicits._
+    val data = spark.read.parquet(adHDFSDir + date)
+    data.rdd.map(
+      row => {
+        val ideaid = row.getAs[Array[Byte]](64).map(_.toChar).mkString.toInt
+        val embedding = new Array[Double](64)
+
+        for (i <- 0 to 63) {
+          embedding(i) = row.getAs[Number](i).doubleValue()
+        }
+        (ideaid, embedding)
+      }
+    ).toDF("ideaid", "ad_embedding")
+  }
+}
