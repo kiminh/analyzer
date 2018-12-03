6f52d6a40f098def0747abeded5e8f19a1fc3295
wangjun <wangjun02@qutoutiao.net>
2018-12-03 10:26:28 +0800

    fix bug

--- src/main/scala/com/cpc/spark/ocpcV3/ocpcNovel/model/OcpcRegression.scala
+++ src/main/scala/com/cpc/spark/ocpcV3/ocpcNovel/model/OcpcRegression.scala
@@ -106,7 +106,7 @@ object OcpcRegression {
       .agg(collect_set("str").as("liststr"))
       .select("unitid", "liststr").collect()
 
-    val targetK = 1.0
+    val targetK = 1.5
     var resList = new mutable.ListBuffer[(String, Double, String, String)]()
     for (row <- res) {
       val unitid = row(0).toString
