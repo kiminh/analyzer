1ae37f3b537bfbafbe73d75754fb27f523f40330
dongjinbao <dongjinbao@qutoutiao.net>
2018-12-04 17:08:25 +0800

    test

--- src/main/scala/com/cpc/spark/cvr/OcpcCost.scala
+++ src/main/scala/com/cpc/spark/cvr/OcpcCost.scala
@@ -35,24 +35,59 @@ object OcpcCost {
                |) b
                |on a.searchid = b.searchid
              """.stripMargin
-
+        println(sql)
         val all = spark.sql(sql)
-
+        println("all 's count is " + all.count())
         all.createOrReplaceTempView("all")
 
         val qttSql =
             s"""
                |select unitid,
-               |    sum(price) as total_price,
-               |    sum(case when label2 = 1 then 1 else 0) as convert_num,
+               |    sum(price) / sum(case when label2 = 1 then 1 else 0 end) as cost,
                |    "qtt" as tag
                |from all
                |where media_appsid in ('80000001', '80000002')
                |group by unitid
              """.stripMargin
-
+        println(qttSql)
         val qtt = spark.sql(qttSql)
-
+        println("qtt 's count is " + qtt.count())
         qtt.show(10)
+
+        val miduSql =
+            s"""
+               |select unitid,
+               |    sum(price) / sum(case when label2 = 1 then 1 else 0 end) as cost,
+               |    "qtt" as tag
+               |from all
+               |where media_appsid in ('80001098', '80001292')
+               |group by unitid
+             """.stripMargin
+        println(miduSql)
+
+        val midu = spark.sql(miduSql)
+        midu.show(10)
+        println("midu 's count is " + midu.count())
+        val midu_ocpcSql =
+            s"""
+               |select unitid,
+               |    sum(price) / sum(case when label2 = 1 then 1 else 0 end) as cost,
+               |    "qtt" as tag
+               |from all
+               |where media_appsid in ('80001098', '80001292')
+               |and is_ocpc = 1
+               |group by unitid
+             """.stripMargin
+        println(midu_ocpcSql)
+        val midu_ocpc = spark.sql(midu_ocpcSql)
+
+        midu_ocpc.show(10)
+        println("midu_ocpc 's count is " + midu_ocpc.count())
+        val result = qtt.union(midu).union(midu_ocpc)
+
+        result.show(10)
+
+        println("result 's count is " + result.count())
     }
+
 }
