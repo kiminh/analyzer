a968f34c764ad7e94821a00f8d4e53e7d68f2097
wangyao <wangyao@qutoutiao.net>
2018-12-04 14:30:42 +0800

    -

--- src/main/scala/com/cpc/spark/ml/novel/Behavior2RedisNovelV2.scala
+++ src/main/scala/com/cpc/spark/ml/novel/Behavior2RedisNovelV2.scala
@@ -78,7 +78,7 @@ object Behavior2RedisNovelV2 {
         hashSeq("m11", "int")($"c_adclass_1").alias("m11"),
         hashSeq("m12", "int")($"c_adclass_2").alias("m12"),
         hashSeq("m13", "int")($"c_adclass_3").alias("m13"),
-        hashSeq("m14", "int")($"c_adclass_4_7").alias("m14"),
+        hashSeq("m14", "int")($"c_ideaid_4_7").alias("m14"),
         hashSeq("m15", "int")($"c_adclass_4_7").alias("m15"),
         hashSeq("m16", "int")($"book_id").alias("m16"),
         hashSeq("m17", "int")($"first_category_id").alias("m17"),
