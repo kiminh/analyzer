c57b63819c622ffd81716232a6381c575bd2c13f
wangyao <wangyao@qutoutiao.net>
2018-12-06 16:23:12 +0800

    -

--- src/main/scala/com/cpc/spark/ml/novel/Behavior2RedisV3.scala
+++ src/main/scala/com/cpc/spark/ml/novel/Behavior2RedisV3.scala
@@ -131,7 +131,7 @@ object Behavior2RedisV3 {
       ).persist()
 
     ud_features.coalesce(50).write.mode("overwrite")
-      .parquet(s"/user/cpc/wy/novel/features/ud-$date")
+      .parquet(s"/user/cpc/wy/novel/features/ud")
 
     ud_features.show()
 
