57b3e5ee581b101866c3407fe574f5f9a30d38bf
wangyao <wangyao@qutoutiao.net>
2018-12-06 22:28:57 +0800

    -

--- test2.sh
+++ test2.sh
@@ -24,5 +24,5 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.yarn.executor.memoryOverhead=4g'\
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
-    --class com.cpc.spark.ml.novel.DNNSampleV3 \
+    --class com.cpc.spark.ml.novel.DNNSampleCvr \
     /home/cpc/wy/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar 2018-11-25 /user/cpc/wy/novel/cvr_v2 2018-11-25 /user/cpc/wy/dnn/cvr_v2
