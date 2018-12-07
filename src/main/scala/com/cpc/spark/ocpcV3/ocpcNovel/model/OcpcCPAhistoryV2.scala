763c24216b93b9899d0f9e3d7b41283685aea512
wangjun <wangjun02@qutoutiao.net>
2018-12-07 21:59:37 +0800

    fix bug

--- src/main/scala/com/cpc/spark/ocpcV3/ocpcNovel/model/OcpcCPAhistoryV2.scala
+++ src/main/scala/com/cpc/spark/ocpcV3/ocpcNovel/model/OcpcCPAhistoryV2.scala
@@ -31,26 +31,26 @@ object OcpcCPAhistoryV2 {
     // 按照要求生成相关基础数据表
     val baseData = getBaseData(date, hour, spark)
     val qttData = getQttCPA(baseData, date, hour, spark)
-//    val novelData = getNovelCPA(baseData, date, hour, spark)
-//    val adclassData = getAdclassCPA(baseData, date, hour, spark).select("new_adclass", "cpa1", "cpa2")
+    val novelData = getNovelCPA(baseData, date, hour, spark)
+    val adclassData = getAdclassCPA(baseData, date, hour, spark).select("new_adclass", "cpa1", "cpa2")
     val qttAlpha = checkCPAhistory(qttData, 0.8, "qtt", date, hour, spark)
-//    val novelAlpha = checkCPAhistory(novelData, 0.8, "novel", date, hour, spark)
-//
-//    // 数据表关联
-//    val data = baseData
-//      .select("unitid", "new_adclass")
-//      .distinct()
-//      .join(qttAlpha, Seq("unitid", "new_adclass"), "left_outer")
-//      .join(novelAlpha, Seq("unitid", "new_adclass"), "left_outer")
-//      .join(adclassData, Seq("new_adclass"), "left_outer")
-//      .select("unitid", "new_adclass", "cpa1_history_qtt", "cpa2_history_qtt", "cpa1_history_novel", "cpa2_history_novel", "cpa1", "cpa2")
-//
-//    // 按照策略挑选合适的cpa以及确定对应的conversion_goal
-//    val result = getResult(data, date, hour, spark)
-//    // TODO 删除临时表
-//    val tableName = "test.ocpcv3_novel_cpa_history_hourly_v2"
-//    result.write.mode("overwrite").saveAsTable(tableName)
-//    println(s"save data into table: $tableName")
+    val novelAlpha = checkCPAhistory(novelData, 0.8, "novel", date, hour, spark)
+
+    // 数据表关联
+    val data = baseData
+      .select("unitid", "new_adclass")
+      .distinct()
+      .join(qttAlpha, Seq("unitid", "new_adclass"), "left_outer")
+      .join(novelAlpha, Seq("unitid", "new_adclass"), "left_outer")
+      .join(adclassData, Seq("new_adclass"), "left_outer")
+      .select("unitid", "new_adclass", "cpa1_history_qtt", "cpa2_history_qtt", "cpa1_history_novel", "cpa2_history_novel", "cpa1", "cpa2")
+
+    // 按照策略挑选合适的cpa以及确定对应的conversion_goal
+    val result = getResult(data, date, hour, spark)
+    // TODO 删除临时表
+    val tableName = "test.ocpcv3_novel_cpa_history_hourly_v2"
+    result.write.mode("overwrite").saveAsTable(tableName)
+    println(s"save data into table: $tableName")
 
   }
 
@@ -285,16 +285,9 @@ object OcpcCPAhistoryV2 {
       .select("unitid", "new_adclass", "cpa2_history_" + media)
 
     // 关联数据表
-    println("cvr1Final ########################")
-    cvr1Final.printSchema()
-    cvr1Final.show(10)
-    println("cvr2Final ########################")
-    cvr2Final.printSchema()
-    cvr2Final.show(10)
     val resultDF = cvr1Final
       .join(cvr2Final, Seq("unitid", "new_adclass"), "outer")
-//      .select("unitid", "new_adclass", "cpa1_history_" + media, "cpa2_history_" + media)
-    resultDF.printSchema()
+      .select("unitid", "new_adclass", "cpa1_history_" + media, "cpa2_history_" + media)
 
     // TODO 删除临时表
     resultDF.write.mode("overwrite").saveAsTable("test.ocpcv3_cpa_history_v2_final_" + media)
