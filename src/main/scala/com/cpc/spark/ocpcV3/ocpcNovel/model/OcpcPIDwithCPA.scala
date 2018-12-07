1c46d56cc82f0086aa38862c67ba6380be7de0a8
wangjun <wangjun02@qutoutiao.net>
2018-12-07 16:45:22 +0800

    ocpcv3: pidwithCPA remove duplicates in pb table

--- src/main/scala/com/cpc/spark/ocpcV3/ocpcNovel/model/OcpcPIDwithCPA.scala
+++ src/main/scala/com/cpc/spark/ocpcV3/ocpcNovel/model/OcpcPIDwithCPA.scala
@@ -163,6 +163,8 @@ object OcpcPIDwithCPA {
     val case2 = spark
       .table("test.ocpcv3_novel_pb_v1_hourly")
       .withColumn("kvalue2", col("kvalue"))
+      .groupBy("unitid")
+      .agg(avg(col("kvalue2")).alias("kvalue2"))
       .select("unitid", "kvalue2")
       .distinct()
 
