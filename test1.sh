b80460812e434064fe02bfe2b4319d68c0dc7965
wangyao <wangyao@qutoutiao.net>
2018-12-08 21:50:52 +0800

    -

--- test1.sh
+++ test1.sh
@@ -1,10 +1,3 @@
-sh test2.sh 2018-12-01
-sh test2.sh 2018-12-02
-sh test2.sh 2018-12-03
-sh test2.sh 2018-12-04
-sh test2.sh 2018-12-05
-sh test2.sh 2018-12-06
-sh test2.sh 2018-12-07
 sh test2.sh 2018-12-08
 #sh test2.sh 2018-11-25
 #sh test2.sh 2018-11-26
