27917149def13fcad546c8ae45413a3d86d7dbc8
wangyao <wangyao@qutoutiao.net>
2018-12-08 22:46:32 +0800

    -

--- src/main/scala/com/cpc/spark/ml/novel/DNNSampleV3.scala
+++ src/main/scala/com/cpc/spark/ml/novel/DNNSampleV3.scala
@@ -238,7 +238,7 @@ class DNNSampleV3(spark: SparkSession, trdate: String = "", trpath: String = "",
          |                  and load_date<='${getDay(date, 4)}',click_ideaid,null)) as c_ideaid_4_7,
          |       collect_list(if(load_date>='${getDay(date, 7)}'
          |                  and load_date<='${getDay(date, 4)}',click_adclass,null)) as c_adclass_4_7
-         |from dl_cpc.cpc_user_behaviors
+         |from dl_cpc.cpc_user_behaviors_novel
          |where load_date in ('${getDays(date, 1, 7)}')
          |group by uid
       """.stripMargin
