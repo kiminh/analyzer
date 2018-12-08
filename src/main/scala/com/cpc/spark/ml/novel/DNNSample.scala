5b44d4adefe87e8057bbadc10d10e970c3c7fb8b
wangyao <wangyao@qutoutiao.net>
2018-12-08 14:28:52 +0800

    -

--- src/main/scala/com/cpc/spark/ml/novel/DNNSample.scala
+++ src/main/scala/com/cpc/spark/ml/novel/DNNSample.scala
@@ -37,6 +37,10 @@ class DNNSample(spark: SparkSession, trDate: String, trPath: String,
       .option("recordType", "Example")
       .save(s"$p/dnntrain-$trDate")
 
+    //确认样本数
+    val n = data.count()
+    println("训练数据：total = %d, 正比例 = %.4f".format(n, data.where("label=array(1,0)").count.toDouble / n))
+
     if (trDate.length == 13) {
       val count = data.count()
       s"echo $count" #> new File("count") !
