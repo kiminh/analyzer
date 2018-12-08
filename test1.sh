f3cda8310a205047aedabb704d610d162a0dee56
wangyao <wangyao@qutoutiao.net>
2018-12-08 23:15:47 +0800

    -

--- test1.sh
+++ test1.sh
@@ -1,10 +1,9 @@
-sh test2.sh 2018-12-08
-#sh test2.sh 2018-11-25
-#sh test2.sh 2018-11-26
-#sh test2.sh 2018-11-27
-#sh test2.sh 2018-11-28
-#sh test2.sh 2018-11-29
-#sh test2.sh 2018-11-30
+sh test2.sh 2018-11-25
+sh test2.sh 2018-11-26
+sh test2.sh 2018-11-27
+sh test2.sh 2018-11-28
+sh test2.sh 2018-11-29
+sh test2.sh 2018-11-30
 
 
 
