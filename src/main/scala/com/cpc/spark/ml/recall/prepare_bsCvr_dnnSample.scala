e7262135661937d29f73a798b7a83305481cdac9
段广东 <duanguangdong@qutoutiao.net>
2018-12-10 19:55:29 +0800

    dnn 年龄模型

--- src/main/scala/com/cpc/spark/ml/recall/prepare_bsCvr_dnnSample.scala
+++ src/main/scala/com/cpc/spark/ml/recall/prepare_bsCvr_dnnSample.scala
@@ -27,7 +27,7 @@ object prepare_bsCvr_dnnSample {
     val n = train.count()
     println("训练数据：total = %d, 正比例 = %.4f".format(n, train.where("label=array(1,0)").count.toDouble / n))
 
-    train.repartition(100)
+    train.repartition(1000)
       .write
       .mode("overwrite")
       .format("tfrecords")
