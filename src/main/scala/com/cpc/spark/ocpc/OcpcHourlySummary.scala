04eed94b1c41e624fda2da0f414f25c0c125e819
wangjun <wangjun02@qutoutiao.net>
2018-12-12 15:43:10 +0800

    fix bug

--- src/main/scala/com/cpc/spark/ocpc/OcpcHourlySummary.scala
+++ src/main/scala/com/cpc/spark/ocpc/OcpcHourlySummary.scala
@@ -21,46 +21,45 @@ object OcpcHourlySummary {
   }
 
   def joinData(date: String, hour: String, spark: SparkSession) = {
-    val tableNameNoAPI = "dl_cpc.ocpc_check_hourly_report_noapi"
-    val tableNameAPI = "dl_cpc.ocpc_check_hourly_report_api"
-    val selectCondition = s"`date`='$date' and `hour`='$hour'"
+//    val tableNameNoAPI = "dl_cpc.ocpc_check_hourly_report_noapi"
+//    val tableNameAPI = "dl_cpc.ocpc_check_hourly_report_api"
+//    val selectCondition = s"`date`='$date' and `hour`='$hour'"
+//
+//    val noAPI = spark
+//      .table(tableNameNoAPI)
+//      .where(selectCondition)
+//      .withColumn("conversion_goal", lit(1))
+//      .select("ideaid", "userid", "conversion_goal", "step2_percent", "cpa_given", "cpa_real", "show_cnt", "ctr_cnt", "cvr_cnt", "price", "avg_k", "recent_k")
+//      .withColumn("cvr_cnt", when(col("cvr_cnt").isNull, 0).otherwise(col("cvr_cnt")))
+//
+//    val api = spark
+//      .table(tableNameAPI)
+//      .where(selectCondition)
+//      .withColumn("conversion_goal", lit(2))
+//      .select("ideaid", "userid", "conversion_goal", "step2_percent", "cpa_given", "cpa_real", "show_cnt", "ctr_cnt", "cvr_cnt", "price", "avg_k", "recent_k")
+//      .withColumn("cvr_cnt", when(col("cvr_cnt").isNull, 0).otherwise(col("cvr_cnt")))
 
-    val noAPI = spark
-      .table(tableNameNoAPI)
-      .where(selectCondition)
-      .withColumn("conversion_goal", lit(1))
-      .select("ideaid", "userid", "conversion_goal", "step2_percent", "cpa_given", "cpa_real", "show_cnt", "ctr_cnt", "cvr_cnt", "price", "avg_k", "recent_k")
-      .withColumn("cvr_cnt", when(col("cvr_cnt").isNull, 0).otherwise(col("cvr_cnt")))
 
-    val api = spark
-      .table(tableNameAPI)
-      .where(selectCondition)
-      .withColumn("conversion_goal", lit(2))
-      .select("ideaid", "userid", "conversion_goal", "step2_percent", "cpa_given", "cpa_real", "show_cnt", "ctr_cnt", "cvr_cnt", "price", "avg_k", "recent_k")
-      .withColumn("cvr_cnt", when(col("cvr_cnt").isNull, 0).otherwise(col("cvr_cnt")))
+    val rawData = spark
+      .table("test.ocpc_detail_report_hourly")
+      .where(s"`date`='$date' and `hour`='$hour'")
+      .select("user_id", "idea_id", "conversion_goal", "step2_click_percent", "is_step2", "cpa_given", "cpa_real", "cpa_ratio", "is_cpa_ok", "impression", "click", "conversion", "ctr", "click_cvr", "show_cvr", "cost", "acp", "avg_k", "recent_k")
 
     val ideaidList = spark
       .table("dl_cpc.ocpcv3_ctr_data_hourly")
       .where(s"`date`='$date' and `hour` <= '$hour' and media_appsid in ('80000001', '80000002')")
-      .select("ideaid", "unitid")
+      .withColumn("idea_id", col("ideaid"))
+      .withColumn("unit_id", col("unitid"))
+      .select("idea_id", "unit_id")
       .distinct()
 
-    val resultDF = noAPI
-      .union(api)
-      .join(ideaidList, Seq("ideaid"), "left_outer")
-      .select("ideaid", "unitid", "userid", "conversion_goal", "step2_percent", "cpa_given", "cpa_real", "show_cnt", "ctr_cnt", "cvr_cnt", "price", "avg_k", "recent_k")
+    val resultDF = rawData
+      .join(ideaidList, Seq("idea_id"), "left_outer")
+      .select("user_id", "idea_id", "unit_id", "conversion_goal", "step2_click_percent", "is_step2", "cpa_given", "cpa_real", "cpa_ratio", "is_cpa_ok", "impression", "click", "conversion", "ctr", "click_cvr", "show_cvr", "cost", "acp", "avg_k", "recent_k")
 
     resultDF.show(10)
 
     resultDF
   }
-
-  def calculateData(rawData: DataFrame, date: String, hour: String, spark: SparkSession) = {
-    val data = rawData
-      .withColumn("is_step2", when(col("step2_percent")===1, 1).otherwise(0))
-      .withColumn("cpa_ratio", col("cpa_given") * 1.0 / col("cpa_real"))
-      .withColumn("is_high_cpa", when(col("is_step2")===1 && (col("cpa_ratio")<0.8 || col("cpa_real").isNull), 1).otherwise(0))
-      .withColumn("is_low_cpa", when(col("is_step2")===1 && col("cpa_ratio")>=0.8, 1).otherwise(0))
-      .withColumn("cost", col("price") * col("ctr_cnt"))
-  }
+  
 }
