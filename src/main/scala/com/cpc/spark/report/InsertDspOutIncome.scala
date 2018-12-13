ccc346c860f16c3a879a1ac75a95a97308884af1
zhanghongyang <zhanghongyang@qutoutiao.net>
2018-12-13 20:25:01 +0800

    -

--- src/main/scala/com/cpc/spark/report/InsertDspOutIncome.scala
+++ src/main/scala/com/cpc/spark/report/InsertDspOutIncome.scala
@@ -73,16 +73,17 @@ object InsertDspOutIncome {
        """.stripMargin
     println("sql: " + sql)
 
-    var dspLog = spark.sql(sql).collect()
-    for (log <- dspLog) {
-      val dsp_adslot_id = log.getAs[String]("dsp_adslot_id")
-      val dsp_income = log.getAs[Double]("dsp_income")
-      val dsp_click = log.getAs[Long]("dsp_click")
-      val dsp_impression = log.getAs[Long]("dsp_impression")
-      val n = updateData(table, day, dsp_adslot_id, dsp_income, dsp_click, dsp_impression)
-    }
+    var dspLog = spark.sql(sql)
 
-    //    dspLog.foreachPartition(updateData)
+//    for (log <- dspLog) {
+//      val dsp_adslot_id = log.getAs[String]("dsp_adslot_id")
+//      val dsp_income = log.getAs[Double]("dsp_income")
+//      val dsp_click = log.getAs[Long]("dsp_click")
+//      val dsp_impression = log.getAs[Long]("dsp_impression")
+//      val n = updateData(table, day, dsp_adslot_id, dsp_income, dsp_click, dsp_impression)
+//    }
+
+        dspLog.rdd.foreachPartition(updateData)
     //    val s = Seq()
 
     //    dspLog.foreach { r =>
@@ -98,7 +99,7 @@ object InsertDspOutIncome {
     spark.stop()
   }
 
-  def updateData(table: String, day: String, dsp_adslot_id: String, dsp_income: Double, dsp_click: Long, dsp_impression: Long): Int = {
+  /*def updateData(table: String, day: String, dsp_adslot_id: String, dsp_income: Double, dsp_click: Long, dsp_impression: Long): Int = {
     println("#####: " + table + ", " + day + ", " + dsp_adslot_id + ", " + dsp_income + ", " + dsp_click + ", " + dsp_impression)
     try {
       Class.forName(mariadbProp.getProperty("driver"))
@@ -122,42 +123,42 @@ object InsertDspOutIncome {
       case e: Exception => println("exception caught: " + e)
         -1
     }
-  }
+  }*/
 
-  //  def updateData(iter: Iterator[Row]): Unit = {
-  //    var conn: Connection = null;
-  //
-  //    try {
-  //      Class.forName(mariadbProp.getProperty("driver"))
-  //      val conn = DriverManager.getConnection(
-  //        mariadbUrl,
-  //        mariadbProp.getProperty("user"),
-  //        mariadbProp.getProperty("password"))
-  //      iter.foreach { r =>
-  //        val dsp_adslot_id = r.getAs[String]("dsp_adslot_id")
-  //        val dsp_income = r.getAs[Double]("dsp_income")
-  //        val dsp_click = r.getAs[Long]("dsp_click")
-  //        val dsp_impression = r.getAs[Long]("dsp_impression")
-  //        val sql =
-  //          s"""
-  //             |update union_test.%s
-  //             |set dsp_income = %s,
-  //             |dsp_click = %s,
-  //             |dsp_impression = %s
-  //             |where `date` = "%s" and dsp_adslot_id = "%s" and ad_src = 22
-  //      """.stripMargin.format(table, dsp_income, dsp_click, dsp_impression, day, dsp_adslot_id)
-  //        println("sql" + sql);
-  //
-  //        val stmt = conn.createStatement()
-  //        val num = stmt.executeUpdate(sql);
-  //      }
-  //    } catch {
-  //      case e: Exception => println("exception caught: " + e)
-  //    } finally {
-  //      if (conn != null)
-  //        conn.close()
-  //    }
-  //  }
+    def updateData(iter: Iterator[Row]): Unit = {
+      var conn: Connection = null;
+
+      try {
+        Class.forName(mariadbProp.getProperty("driver"))
+        val conn = DriverManager.getConnection(
+          mariadbUrl,
+          mariadbProp.getProperty("user"),
+          mariadbProp.getProperty("password"))
+        iter.foreach { r =>
+          val dsp_adslot_id = r.getAs[String]("dsp_adslot_id")
+          val dsp_income = r.getAs[Double]("dsp_income")
+          val dsp_click = r.getAs[Long]("dsp_click")
+          val dsp_impression = r.getAs[Long]("dsp_impression")
+          val sql =
+            s"""
+               |update union_test.%s
+               |set dsp_income = %s,
+               |dsp_click = %s,
+               |dsp_impression = %s
+               |where `date` = "%s" and dsp_adslot_id = "%s" and ad_src = 22
+        """.stripMargin.format(table, dsp_income, dsp_click, dsp_impression, day, dsp_adslot_id)
+          println("sql" + sql);
+
+          val stmt = conn.createStatement()
+          val num = stmt.executeUpdate(sql);
+        }
+      } catch {
+        case e: Exception => println("exception caught: " + e)
+      } finally {
+        if (conn != null)
+          conn.close()
+      }
+    }
 
 
 }
