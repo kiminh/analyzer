52aad731884004fab05c7c5cd7913dee98f710e3
wangyao <wangyao@qutoutiao.net>
2018-12-17 16:10:07 +0800

    -

--- test2.sh
+++ test2.sh
@@ -25,7 +25,7 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
     --class com.cpc.spark.ml.novel.DNNSampleCvrV4 \
-    /home/cpc/wy/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $day /user/cpc/wy/novel/cvr_v3_tail $day /user/cpc/wy/novel/cvr_v3_tail 0
+    /home/cpc/wy/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $day /user/cpc/wy/novel/cvr_v3_tail $day /user/cpc/wy/novel/cvr_v3_tail
     #2018-12-04 /user/cpc/wy/novel/cvr_v2 2018-12-04 /user/cpc/wy/novel/cvr_v2
     #2018-12-05 /user/cpc/wy/novel/ctr_v3 2018-12-05 /user/cpc/wy/novel/ctr_v3
     #2018-12-06 /user/cpc/wy/novel/ctr_v3 2018-12-06 /user/cpc/wy/novel/ctr_v3
