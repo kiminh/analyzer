ffbaaece7f97f327fe7f542dce5b89b9b2349db1
wangyao <wangyao@qutoutiao.net>
2018-12-17 16:13:46 +0800

    -

--- test1.sh
+++ test1.sh
@@ -1,9 +1,13 @@
 sh test2.sh 2018-12-10
-#sh test2.sh 2018-12-11
-#sh test2.sh 2018-12-09
-#sh test2.sh 2018-12-07
-#sh test2.sh 2018-12-08
-
+sh test2.sh 2018-12-11
+sh test2.sh 2018-12-09
+sh test2.sh 2018-12-07
+sh test2.sh 2018-12-08
+sh test2.sh 2018-12-12
+sh test2.sh 2018-12-13
+sh test2.sh 2018-12-14
+sh test2.sh 2018-12-15
+sh test2.sh 2018-12-16
 
 
 
