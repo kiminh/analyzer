bf9ad05a41f7ed40dd51599f91e0af80d1e36205
dongjinbao <dongjinbao@qutoutiao.net>
2018-12-17 10:58:01 +0800

    test

---
+++ src/main/scala/com/cpc/spark/novel/OperateMySQL.scala
@@ -0,0 +1,36 @@
+package com.cpc.spark.novel
+
+import com.typesafe.config.ConfigFactory
+import java.sql.{ Connection, DriverManager }
+/**
+  * @author Jinbao
+  * @date 2018/12/17 10:46
+  */
+object OperateMySQL {
+    def del(sql:String): Unit = {
+        val conf = ConfigFactory.load()
+        val url = conf.getString("mariadb.report2_write.url")
+        val driver = conf.getString("mariadb.report2_write.driver")
+        val username = conf.getString("mariadb.report2_write.user")
+        val password = conf.getString("mariadb.report2_write.password")
+        var connection: Connection = null
+        try {
+            Class.forName(driver)
+            connection = DriverManager.getConnection(url, username, password)
+            val statement = connection.createStatement
+            val rs = statement.executeUpdate(sql)
+            println("delete success!")
+        }
+        catch {
+            case e: Exception => e.printStackTrace
+        }
+        //关闭连接，释放资源
+        connection.close
+    }
+
+    def main(args: Array[String]): Unit = {
+        val sql = "delete from report2.report_novel_evaluation where `date` = '2018-12-12'"
+
+        del(sql)
+    }
+}
