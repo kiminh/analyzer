093ea82af8a3d63129e43731d826f66ca0889cc9
wangjun <wangjun02@qutoutiao.net>
2018-12-20 20:55:23 +0800

    fix bug

--- src/main/scala/com/cpc/spark/ocpc/OcpcCheckData.scala
+++ src/main/scala/com/cpc/spark/ocpc/OcpcCheckData.scala
@@ -1,6 +1,7 @@
 package com.cpc.spark.ocpc
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
 object OcpcCheckData {
   def main(args: Array[String]): Unit = {
@@ -24,8 +25,7 @@ object OcpcCheckData {
          |    price,
          |    ext_int['bid_ocpc'] as bid_ocpc,
          |    ext_int['is_ocpc'] as is_ocpc,
-         |    ext_string['ocpc_log'] as ocpc_log,
-         |    hour
+         |    ext_string['ocpc_log'] as ocpc_log
          |from
          |    dl_cpc.cpc_union_log
          |WHERE
@@ -43,7 +43,11 @@ object OcpcCheckData {
          |and ideaid in (2450708,2450542,2450447,2432700,2374508,2442830);
        """.stripMargin
     println(sqlRequest)
-    val data = spark.sql(sqlRequest)
+    val data = spark
+      .sql(sqlRequest)
+      .withColumn("date", lit(date))
+      .withColumn("hour", lit(hour))
+
     data.write.mode("overwrite").saveAsTable("test.ocpc_check_ideaid_ctr20181220_hourly")
   }
 }
