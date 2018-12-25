611cb167258f9a5a3f2ace8916514b6197bb7e5e
段广东 <duanguangdong@qutoutiao.net>
2018-12-25 20:30:43 +0800

    dnn 年龄模型

--- src/main/scala/com/cpc/spark/ml/recall/report_userprofile_effect.scala
+++ src/main/scala/com/cpc/spark/ml/recall/report_userprofile_effect.scala
@@ -72,11 +72,11 @@ object report_userprofile_effect {
          |on
          |  a.searchid=b.searchid and a.ideaid=b.ideaid
          |left join
-         |  (select searchid,ideaid, label as label3 from dl_cpc.ml_cvr_feature_v2 where date='$date') as c
+         |  (select searchid,ideaid, label as label3 from dl_cpc.ml_cvr_feature_v2 where date='$date' group by searchid,ideaid, label) as c
          |on
          |  a.searchid=c.searchid and a.ideaid=c.ideaid
          |left join
-         |  (select searchid,ideaid, label as label4 from dl_cpc.ml_cvr_feature_motivate where date='$date') as d
+         |  (select searchid,ideaid, label as label4 from dl_cpc.ml_cvr_feature_motivate where date='$date' group by searchid,ideaid, label) as d
          |on a.searchid=d.searchid and a.ideaid=d.ideaid
        """.stripMargin
 
