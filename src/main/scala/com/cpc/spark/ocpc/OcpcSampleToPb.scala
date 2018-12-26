191c1c7c2a49b86bceddc5bb82b9d3dbe7beb21c
wangjun <wangjun02@qutoutiao.net>
2018-12-26 13:48:13 +0800

    fix bug

--- src/main/scala/com/cpc/spark/ocpc/OcpcSampleToPb.scala
+++ src/main/scala/com/cpc/spark/ocpc/OcpcSampleToPb.scala
@@ -472,6 +472,7 @@ object OcpcSampleToPb {
       .na.fill(0.0, Seq("kvalue1", "kvalue2"))
       .withColumn("k_value", when(col("conversion_goal") === 1 || col("conversion_goal") === 3, col("kvalue1")).otherwise(col("kvalue2")))
       .filter(s"kvalue1 != 0 or kvalue2 != 0 or conversion_goal is not null")
+      .filter("k_value > 0")
 
 
     val resultDF = result
