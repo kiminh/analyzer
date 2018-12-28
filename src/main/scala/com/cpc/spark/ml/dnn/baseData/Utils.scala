04de50a6f79c22f38bd399e79f7587071956b171
zhuhaijian <zhuhaijian@qutoutiao.net>
2018-12-28 12:10:35 +0800

    fix

--- src/main/scala/com/cpc/spark/ml/dnn/baseData/Utils.scala
+++ src/main/scala/com/cpc/spark/ml/dnn/baseData/Utils.scala
@@ -88,9 +88,9 @@ object Utils {
 
     val sql = generateSql(str, "redis")
     spark.sql(sql).sample(withReplacement = false, 0.01)
-      .rdd.map(x => (prefix + x.getString(0), Base64.decodeBase64(x.getString(1))))
+      .rdd.map(x => prefix + x.getString(0) + Base64.decodeBase64(x.getString(1)))
       .coalesce(10)
-      .toDF("key", "value")
+      .toDF("value")
       .write
       .text("/user/cpc/dnn/eval/redis/")
 
