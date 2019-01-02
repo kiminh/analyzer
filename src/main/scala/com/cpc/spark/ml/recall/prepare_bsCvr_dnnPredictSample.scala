b75f333b2a6051e88a30ad6d13245c9d46ec882d
段广东 <duanguangdong@qutoutiao.net>
2019-01-02 19:49:02 +0800

    dnn 年龄模型

--- src/main/scala/com/cpc/spark/ml/recall/prepare_bsCvr_dnnPredictSample.scala
+++ src/main/scala/com/cpc/spark/ml/recall/prepare_bsCvr_dnnPredictSample.scala
@@ -163,7 +163,7 @@ object prepare_bsCvr_dnnPredictSample {
       $"sparse".getField("_3").alias("idx2"),
       $"sparse".getField("_4").alias("id_arr"),
       $"uid"
-    ).repartition(40000).persist(StorageLevel.DISK_ONLY)
+    ).repartition(8000).persist(StorageLevel.DISK_ONLY)
 
     result_temp.show(10)
 
