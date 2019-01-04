5444be3b3d490da223bdb44bb359322d4881cf0b
wangjun <wangjun02@qutoutiao.net>
2019-01-04 11:00:12 +0800

    change code

--- src/main/scala/com/cpc/spark/ocpcV3/ocpc/filter/OcpcCalculateAUC.scala
+++ src/main/scala/com/cpc/spark/ocpcV3/ocpc/filter/OcpcCalculateAUC.scala
@@ -16,36 +16,28 @@ object OcpcCalculateAUC {
     // 计算日期周期
     val date = args(0).toString
     val hour = args(1).toString
+    val conversionGoal = args(2).toString
     val spark = SparkSession
       .builder()
       .appName(s"ocpc ideaid auc: $date, $hour")
       .enableHiveSupport().getOrCreate()
 
     // 抽取数据
-//    val data = getData(date, hour, spark)
-    val tableName1 = "test.ocpc_calc_auc20190103"
-//    data.write.mode("overwrite").saveAsTable(tableName1)
+    val data = getData(conversionGoal, date, hour, spark)
+    val tableName1 = "test.ocpc_auc_raw_conversiongoal_" + conversionGoal
+    data.write.mode("overwrite").saveAsTable(tableName1)
 
-    // 过滤当天cvrcntt<15的ideaid
+    // 过滤去除当天cvrcntt<30的ideaid
     val processedData = filterData(tableName1, date, hour, spark)
-    val tableName2 = "test.ocpc_calc_auc_filtered20190103"
+    val tableName2 = "test.ocpc_auc_filter_conversiongoal_" + conversionGoal
     processedData.write.mode("overwrite").saveAsTable(tableName2)
     // 计算auc
-    // cvr1
-    val auc1Data = getAuc(tableName2, 1, date, hour, spark)
-//    // cvr2
-//    val auc2Data = getAuc(tableName, 2, date, hour, spark)
-//    // cvr3
-//    val auc3Data = getAuc(tableName, 3, date, hour, spark)
-//
-//    // 合并数据
-//    val aucData = auc1Data.union(auc2Data).union(auc3Data)
-    val aucData = auc1Data
-    aucData.show(10)
-    aucData.write.mode("overwrite").saveAsTable("test.ocpc_qtt_auc_ideaid20190103")
+    val aucData = getAuc(tableName2, date, hour, spark)
+    val resultDF = aucData.withColumn("conversion_goal", lit(conversionGoal))
+    resultDF.write.mode("overwrite").saveAsTable("test.ocpc_check_auc_data20190104")
   }
 
-  def getData(date: String, hour: String, spark: SparkSession) = {
+  def getData(conversionGoal: String, date: String, hour: String, spark: SparkSession) = {
     // 取历史区间: score数据
     val dateConverter = new SimpleDateFormat("yyyy-MM-dd")
     val today = dateConverter.parse(date)
@@ -82,77 +74,62 @@ object OcpcCalculateAUC {
     val yesterday1 = calendar.getTime
     val date2 = dateConverter.format(yesterday1)
     val selectCondition2 = s"`date` between '$date1' and '$date2'"
-    // cvr1数据
-    val sqlRequest1 =
-      s"""
-         |SELECT
-         |  searchid,
-         |  label2 as label
-         |FROM
-         |  dl_cpc.ml_cvr_feature_v1
-         |WHERE
-         |  $selectCondition2
-         |AND
-         |  label2=1
-         |AND
-         |  label_type!=12
-         |GROUP BY searchid, label2
+    // 根据conversionGoal选择cv的sql脚本
+    if (conversionGoal == "1") {
+      // cvr1数据
+      val sqlRequest =
+        s"""
+           |SELECT
+           |  searchid,
+           |  label2 as label
+           |FROM
+           |  dl_cpc.ml_cvr_feature_v1
+           |WHERE
+           |  $selectCondition2
+           |AND
+           |  label2=1
+           |AND
+           |  label_type!=12
+           |GROUP BY searchid, label2
        """.stripMargin
-    println(sqlRequest1)
-    val cvr1Data = spark.sql(sqlRequest1)
-
-    // cvr2数据
-    val sqlRequest2 =
-      s"""
-         |SELECT
-         |  searchid,
-         |  label
-         |FROM
-         |  dl_cpc.ml_cvr_feature_v2
-         |WHERE
-         |  $selectCondition2
-         |AND
-         |  label=1
-         |GROUP BY searchid, label
+    } else if (conversionGoal == "2") {
+      // cvr2数据
+      val sqlRequest =
+        s"""
+           |SELECT
+           |  searchid,
+           |  label
+           |FROM
+           |  dl_cpc.ml_cvr_feature_v2
+           |WHERE
+           |  $selectCondition2
+           |AND
+           |  label=1
+           |GROUP BY searchid, label
        """.stripMargin
-    println(sqlRequest2)
-    val cvr2Data = spark.sql(sqlRequest2)
-
-    // cvr3数据
-    val sqlRequest3 =
-      s"""
-         |SELECT
-         |  searchid,
-         |  1 as label
-         |FROM
-         |  dl_cpc.site_form_unionlog
-         |WHERE
-         |  $selectCondition2
-         |AND
-         |  ideaid>0
+    } else {
+      // cvr3数据
+      val sqlRequest =
+        s"""
+           |SELECT
+           |  searchid,
+           |  1 as label
+           |FROM
+           |  dl_cpc.site_form_unionlog
+           |WHERE
+           |  $selectCondition2
+           |AND
+           |  ideaid>0
        """.stripMargin
-    println(sqlRequest3)
-    val cvr3Data = spark.sql(sqlRequest3).distinct()
+    }
+    println(sqlRequest)
+    val cvrData = spark.sql(sqlRequest)
 
 
     // 关联数据
-    val result1 = scoreData
-      .join(cvr1Data, Seq("searchid"), "left_outer")
-      .withColumn("conversion_goal", lit(1))
-      .select("searchid", "ideaid", "score", "label", "conversion_goal")
-    val result2 = scoreData
-      .join(cvr2Data, Seq("searchid"), "left_outer")
-      .withColumn("conversion_goal", lit(2))
-      .select("searchid", "ideaid", "score", "label", "conversion_goal")
-    val result3 = scoreData
-      .join(cvr3Data, Seq("searchid"), "left_outer")
-      .withColumn("conversion_goal", lit(3))
-      .select("searchid", "ideaid", "score", "label", "conversion_goal")
-
-    // 合并数据
-    val resultDF = result1
-      .union(result2)
-      .union(result3)
+    val resultDF = scoreData
+      .join(cvrData, Seq("searchid"), "left_outer")
+      .select("searchid", "ideaid", "score", "label")
       .na.fill(0, Seq("label"))
 //    resultDF.show(10)
     resultDF
@@ -161,24 +138,24 @@ object OcpcCalculateAUC {
   def filterData(tableName: String, date: String, hour: String, spark: SparkSession) = {
     val rawData = spark.table(tableName)
     val dataIdea = rawData
-      .groupBy("ideaid", "conversion_goal")
+      .groupBy("ideaid")
       .agg(sum(col("label")).alias("cvrcnt"))
-      .select("ideaid", "conversion_goal", "cvrcnt")
-      .filter(s"cvrcnt >= 15")
+      .select("ideaid", "cvrcnt")
+      .filter(s"cvrcnt >= 30")
 
     val resultDF = rawData
-      .join(dataIdea, Seq("ideaid", "conversion_goal"), "inner")
-      .select("searchid", "ideaid", "score", "label", "conversion_goal")
+      .join(dataIdea, Seq("ideaid"), "inner")
+      .select("searchid", "ideaid", "score", "label")
 
     resultDF
   }
 
-  def getAuc(tableName: String, conversionGoal: Int, date: String, hour: String, spark: SparkSession) = {
+  def getAuc(tableName: String, date: String, hour: String, spark: SparkSession) = {
     import spark.implicits._
     //获取模型标签
 
 //    val aucGaucBuffer = ListBuffer[AucGauc.AucGauc]()
-    val data = spark.table(tableName).where(s"conversion_goal=$conversionGoal")
+    val data = spark.table(tableName)
 
 
     val aucList = new mutable.ListBuffer[(String, Double)]()
@@ -212,7 +189,6 @@ object OcpcCalculateAUC {
     val resultDF = spark
       .createDataFrame(aucList)
       .toDF("ideaid", "auc")
-      .withColumn("conversion_goal", lit(conversionGoal))
 
     resultDF
   }
---
+++ src/main/scala/com/cpc/spark/ocpcV3/ocpc/filter/OcpcPreprocessAUC.scala
@@ -0,0 +1,163 @@
+package com.cpc.spark.ocpcV3.ocpc.filter
+
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+import com.cpc.spark.ocpcV3.ocpc.OcpcUtils.getTimeRangeSql2
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+object OcpcPreprocessAUC {
+  def main(args: Array[String]): Unit = {
+    // 计算日期周期
+    val date = args(0).toString
+    val hour = args(1).toString
+    val spark = SparkSession
+      .builder()
+      .appName(s"ocpc calculate auc filter ideaid: $date, $hour")
+      .enableHiveSupport().getOrCreate()
+
+    // 抽取数据
+    val data = getData(date, hour, spark)
+    val tableName1 = "test.ocpc_calc_auc20190104"
+    data.write.mode("overwrite").saveAsTable(tableName1)
+
+    // 过滤当天cvrcntt<15的ideaid
+    val processedData = filterData(tableName1, date, hour, spark)
+    val tableName2 = "test.ocpc_calc_auc_filtered20190104"
+    processedData.write.mode("overwrite").saveAsTable(tableName2)
+  }
+
+  def getData(date: String, hour: String, spark: SparkSession) = {
+    // 取历史区间: score数据
+    val dateConverter = new SimpleDateFormat("yyyy-MM-dd")
+    val today = dateConverter.parse(date)
+    val calendar = Calendar.getInstance
+    calendar.setTime(today)
+    calendar.add(Calendar.DATE, -1)
+    val yesterday = calendar.getTime
+    val date1 = dateConverter.format(yesterday)
+    val selectCondition1 = s"`date`='$date1'"
+
+    // 取数据: score数据
+    val sqlRequest =
+      s"""
+         |select
+         |    searchid,
+         |    ideaid,
+         |    ext['exp_cvr'].int_value as score
+         |from dl_cpc.cpc_union_log
+         |where $selectCondition1
+         |and isclick = 1
+         |and ext['exp_ctr'].int_value is not null
+         |and media_appsid  in ("80000001", "80000002")
+         |and ext['antispam'].int_value = 0
+         |and ideaid > 0 and adsrc = 1
+         |and ext_int['dsp_adnum_by_src_1'] > 1
+         |and userid > 0
+         |and (ext['charge_type'] IS NULL OR ext['charge_type'].int_value = 1)
+       """.stripMargin
+    println(sqlRequest)
+    val scoreData = spark.sql(sqlRequest)
+
+    // 取历史区间: cvr数据
+    calendar.add(Calendar.DATE, 2)
+    val yesterday1 = calendar.getTime
+    val date2 = dateConverter.format(yesterday1)
+    val selectCondition2 = s"`date` between '$date1' and '$date2'"
+    // cvr1数据
+    val sqlRequest1 =
+      s"""
+         |SELECT
+         |  searchid,
+         |  label2 as label
+         |FROM
+         |  dl_cpc.ml_cvr_feature_v1
+         |WHERE
+         |  $selectCondition2
+         |AND
+         |  label2=1
+         |AND
+         |  label_type!=12
+         |GROUP BY searchid, label2
+       """.stripMargin
+    println(sqlRequest1)
+    val cvr1Data = spark.sql(sqlRequest1)
+
+    // cvr2数据
+    val sqlRequest2 =
+      s"""
+         |SELECT
+         |  searchid,
+         |  label
+         |FROM
+         |  dl_cpc.ml_cvr_feature_v2
+         |WHERE
+         |  $selectCondition2
+         |AND
+         |  label=1
+         |GROUP BY searchid, label
+       """.stripMargin
+    println(sqlRequest2)
+    val cvr2Data = spark.sql(sqlRequest2)
+
+    // cvr3数据
+    val sqlRequest3 =
+      s"""
+         |SELECT
+         |  searchid,
+         |  1 as label
+         |FROM
+         |  dl_cpc.site_form_unionlog
+         |WHERE
+         |  $selectCondition2
+         |AND
+         |  ideaid>0
+       """.stripMargin
+    println(sqlRequest3)
+    val cvr3Data = spark.sql(sqlRequest3).distinct()
+
+
+    // 关联数据
+    val result1 = scoreData
+      .join(cvr1Data, Seq("searchid"), "left_outer")
+      .withColumn("conversion_goal", lit(1))
+      .select("searchid", "ideaid", "score", "label", "conversion_goal")
+    val result2 = scoreData
+      .join(cvr2Data, Seq("searchid"), "left_outer")
+      .withColumn("conversion_goal", lit(2))
+      .select("searchid", "ideaid", "score", "label", "conversion_goal")
+    val result3 = scoreData
+      .join(cvr3Data, Seq("searchid"), "left_outer")
+      .withColumn("conversion_goal", lit(3))
+      .select("searchid", "ideaid", "score", "label", "conversion_goal")
+
+    // 合并数据
+    val resultDF = result1
+      .union(result2)
+      .union(result3)
+      .na.fill(0, Seq("label"))
+    //    resultDF.show(10)
+    resultDF
+  }
+
+  def filterData(tableName: String, date: String, hour: String, spark: SparkSession) = {
+    val rawData = spark.table(tableName)
+    val dataIdea = rawData
+      .groupBy("ideaid", "conversion_goal")
+      .agg(sum(col("label")).alias("cvrcnt"))
+      .select("ideaid", "conversion_goal", "cvrcnt")
+      .filter(s"cvrcnt >= 15")
+
+    val resultDF = rawData
+      .join(dataIdea, Seq("ideaid", "conversion_goal"), "inner")
+      .select("searchid", "ideaid", "score", "label", "conversion_goal")
+
+    resultDF
+  }
+
+}
