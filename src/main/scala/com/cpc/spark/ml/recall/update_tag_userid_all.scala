fd592fe4d6fe02aedb400df520a2705996924c02
段广东 <duanguangdong@qutoutiao.net>
2019-01-04 17:03:21 +0800

    dnn 年龄模型

--- src/main/scala/com/cpc/spark/ml/recall/update_tag_userid_all.scala
+++ src/main/scala/com/cpc/spark/ml/recall/update_tag_userid_all.scala
@@ -14,12 +14,21 @@ object update_tag_userid_all {
 
     val sqlRequest1 =
       s"""
-         | insert into dl_cpc.cpc_tag_userid_all
-         | select ta.profile_tag,ta.userid,'$date' from dl_cpc.cpc_userid_tag ta left join dl_cpc.cpc_tag_userid_all tb
-         |  on ta.userid=tb.userid and ta.profile_tag=tb.tag where tb.tag is null
+         | insert into dl_cpc.cpc_tag_all
+         | select ta.profile_tag,'$date' from dl_cpc.cpc_userid_tag ta left join dl_cpc.cpc_tag_all tb
+         |  on ta.profile_tag=tb.tag where tb.tag is null group by ta.profile_tag
       """.stripMargin
     println(sqlRequest1)
     spark.sql(sqlRequest1)
+
+    val sqlRequest2 =
+      s"""
+         | insert into dl_cpc.cpc_userid_all
+         | select ta.userid,'$date' from dl_cpc.cpc_userid_tag ta left join dl_cpc.cpc_userid_all tb
+         |  on ta.userid=tb.userid where tb.userid is null group by ta.userid
+      """.stripMargin
+    println(sqlRequest2)
+    spark.sql(sqlRequest2)
 
     //    连接adv
     val jdbcProp = new Properties()
@@ -32,14 +41,33 @@ object update_tag_userid_all {
     val table=s"(select look_like_id as tag,user_id as userid from adv.look_like where status = 0 and modified_time>'$date') as tmp"
     spark.read.jdbc(jdbcUrl, table, jdbcProp).createTempView("ftp_tag_userid")
 
-    val sqlRequest2 =
+    val sqlRequest3 =
+      s"""
+         | insert into dl_cpc.cpc_tag_all
+         | select ta.tag,'$date' from ftp_tag_userid ta left join dl_cpc.cpc_tag_all tb
+         |  on ta.profile_tag=tb.tag where tb.tag is null group by ta.tag
+      """.stripMargin
+    println(sqlRequest3)
+    spark.sql(sqlRequest3)
+
+    val sqlRequest4 =
+      s"""
+         | insert into dl_cpc.cpc_userid_all
+         | select ta.userid,'$date' from ftp_tag_userid ta left join dl_cpc.cpc_userid_all tb
+         |  on ta.userid=tb.userid where tb.userid is null group by ta.userid
+      """.stripMargin
+    println(sqlRequest4)
+    spark.sql(sqlRequest4)
+
+    val sqlRequest5 =
       s"""
          | insert into dl_cpc.cpc_tag_userid_all
-         | select ta.tag,ta.userid,'$date' from ftp_tag_userid ta left join dl_cpc.cpc_tag_userid_all tb
+         | select ta.tag,ta.userid,'$date' from (select tag, userid from dl_cpc.cpc_tag_all join dl_cpc.cpc_userid_all) ta left join dl_cpc.cpc_tag_userid_all tb
          |  on ta.userid=tb.userid and ta.tag=tb.tag where tb.tag is null
       """.stripMargin
-    println(sqlRequest2)
-    spark.sql(sqlRequest2)
+
+    println(sqlRequest5)
+    spark.sql(sqlRequest5)
   }
 
 }
