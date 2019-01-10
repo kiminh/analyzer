be36caa06634028bc81e6e8adb10fedcc4412ef1
段广东 <duanguangdong@qutoutiao.net>
2019-01-10 17:12:59 +0800

    dnn 年龄模型

--- src/main/scala/com/cpc/spark/ml/recall/report_userprofile_effect.scala
+++ src/main/scala/com/cpc/spark/ml/recall/report_userprofile_effect.scala
@@ -62,13 +62,15 @@ val sqlRequest1 =
      |  a.uid,
      |  a.ideaid,
      |  a.userid,
+     |  adslot_type,
+     |  a.unitid,
      |  a.isclick,
      |  a.isshow,
      |  case when charge_type=2 then a.price*1.0/1000 else a.price end as price,
      |  a.interests
      | from
      |      (
-     |        select userid,ideaid, isshow, isclick, searchid, uid, interests, case when isclick=1 or ext['charge_type'].int_value=2 then price else 0 end as price, ext['charge_type'].int_value as charge_type, ext['adclass'].int_value as adclass
+     |        select userid,adslot_type,ideaid,unitid, isshow, isclick, searchid, uid, interests, case when isclick=1 or ext['charge_type'].int_value=2 then price else 0 end as price, ext['charge_type'].int_value as charge_type, ext['adclass'].int_value as adclass
      |        from dl_cpc.cpc_union_log
      |        where date='$date'
      |        and media_appsid  in ("80000001", "80000002", "80000006", "800000062", "80000064", "80000066","80000141")
@@ -77,16 +79,7 @@ val sqlRequest1 =
      |        and ideaid > 0
      |        and adsrc = 1
      |        and userid is not null
-     |        and adslot_type!=7
-     |        union
-     |        select info.userid,info.ideaid, info.isshow, info.isclick, searchid, uid, interests, case when info.isclick=1 or ext['charge_type'].int_value=2 then info.price else 0 end as price, ext['charge_type'].int_value as charge_type, ext['adclass'].int_value as adclass
-     |        from dl_cpc.cpc_union_log
-     |        lateral view explode(motivation) b AS info
-     |        where date='$date'
-     |        and media_appsid  in ("80000001", "80000002", "80000006", "800000062", "80000064", "80000066","80000141")
-     |        and ext['antispam'].int_value = 0
-     |        and info.isshow=1
-     |        and adslot_type=7
+     |        and adslot_type in (1,2,3)
      |      ) a
       """.stripMargin
     //charge_type 1 cpc, 2 cpm, 3 cpa, 0 free
@@ -125,11 +118,15 @@ val sqlRequest2 =
      |  a.searchid,
      |  a.uid,
      |  a.userid,
+     |  a.adslot_type,
+     |  a.ideaid,
+     |  a.unitid,
      |  COALESCE(a.isclick, 0) as isclick,
      |  a.isshow,
      |  COALESCE(a.price, 0) price,
      |  a.interests,
-     |  case when b.label2=1 or c.label3=1 then 1 else 0 end as iscvr
+     |  case when b.label2=1 then 1 else 0 end as iscvr,
+     |  case when c.label3=1 then 1 else 0 end as iscvr1
      |from
      |  unionlog_table as a
      |left join
@@ -153,18 +150,24 @@ val sqlRequest2 =
       s"""
          |Select
          |  userid,
+         |  unitid,
+         |  ideaid,
+         |  adslot_type,
          |  SUM(price) as cost,
          |  SUM(isclick) as ctr,
          |  SUM(iscvr) as cvr,
+         |  SUM(iscvr1) as apiCvr,
          |  SUM(isshow) as show
-         |FROM tmpTable GROUP BY userid
+         |FROM tmpTable GROUP BY userid,unitid,ideaid,adslot_type
        """.stripMargin
 
     spark.sql(result).repartition(100).createOrReplaceTempView("total")
 
     val result1 =
       s"""
-         |select userid, tag, SUM(price) as costWithTag, SUM(isclick) as ctrWithTag, SUM(iscvr) as cvrWithTag, SUM(isshow) as showWithTag
+         |select userid, tag,unitid,ideaid,adslot_type,
+         |SUM(price) as costWithTag, SUM(isclick) as ctrWithTag, SUM(iscvr) as cvrWithTag,
+         |SUM(iscvr1) as apicvrWithTag, SUM(isshow) as showWithTag
          |from (Select
          |  searchid,
          |  uid,
@@ -175,11 +178,29 @@ val sqlRequest2 =
          |  iscvr,
          |  split(interest, '=')[0] as tag
          |FROM tmpTable lateral view explode(split(interests, ',')) a as interest
-         |where interest like '%=100') ta group by userid, tag
+         |where interest like '%=100') ta group by userid, tag,unitid,ideaid,adslot_type
        """.stripMargin
 
     spark.sql(result1).repartition(100).createOrReplaceTempView("withtag")
+    val result2 =
+      s"""
+         |insert into dl_cpc.cpc_profileTag_report_daily_v1 partition (`date`='$date')
+         |Select ta.userid,ta.unitid, ta.ideaid, ta.adslot_type, ta.tag,
+         | ta.showWithTag, show-showWithTag,
+         | ta.ctrWithTag, ctr-ctrWithTag, ctrWithTag*1.0/ctr,
+         | ta.costWithTag, cost-ta.costWithTag,
+         | ta.apicvrWithTag, ta.cvrWithTag,
+         | ta.costWithTag*1.0/ta.apicvrWithTag, ta.costWithTag*1.0/ta.cvrWithTag,
+         | apiCvr-ta.apicvrWithTag, cvr-ta.cvrWithTag,
+         | (cost-ta.costWithTag)*1.0/(apiCvr-ta.apicvrWithTag), (cost-ta.costWithTag)*1.0/(cvr-ta.cvrWithTag),
+         | (ta.costWithTag*1.0/ta.apicvrWithTag)/((cost-ta.costWithTag)*1.0/(apiCvr-ta.apicvrWithTag)),
+         | (ta.costWithTag*1.0/ta.cvrWithTag)/((cost-ta.costWithTag)*1.0/(cvr-ta.cvrWithTag))
+         |from withtag ta left join total tb on ta.userid=tb.userid and ta.unitid=tb.unitid and ta.ideaid=tb.ideaid and ta.adslot_type=tb.adslot_type
+       """.stripMargin
 
+    spark.sql(result2)
+
+/**
     val result2 =
       s"""
          |insert into dl_cpc.cpc_profileTag_report_daily partition (`date`='$date')
@@ -189,7 +210,7 @@ val sqlRequest2 =
        """.stripMargin
 
     spark.sql(result2)
-
+*/
     unionlog.unpersist()
     base.unpersist()
 
