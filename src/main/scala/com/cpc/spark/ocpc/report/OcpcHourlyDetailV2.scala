899c9b431e0f6c423f74b826fcace42936ddd3f4
wangjun <wangjun02@qutoutiao.net>
2019-01-10 21:22:59 +0800

    fix bug

--- src/main/scala/com/cpc/spark/ocpc/report/OcpcHourlyDetailV2.scala
+++ src/main/scala/com/cpc/spark/ocpc/report/OcpcHourlyDetailV2.scala
@@ -24,16 +24,16 @@ object OcpcHourlyDetailV2 {
       .join(ideaidData2, Seq("ideaid", "userid", "conversion_goal"), "left_outer")
       .select("ideaid", "userid", "conversion_goal", "pre_cvr", "post_cvr", "q_factor", "cpagiven", "cpareal", "acp", "acb", "auc")
 
+    ideaidData.write.mode("overwrite").saveAsTable("test.ocpc_detail_report_hourly20190109")
+
     // 汇总表数据
-    val conversionData1 = calculateByConversionGoal(rawData, date, hour, spark)
+//    val conversionData1 = calculateByConversionGoal(rawData, date, hour, spark)
 //    val conversionData2 = calculateAUCbyConversionGoal(rawData, date, hour, spark)
 //    val conversionData = conversionData1
 //      .join(conversionData2, Seq("conversion_goal"), "left_outer")
 //      .select("conversion_goal", "pre_cvr", "post_cvr", "q_factor", "cpagiven", "cpareal", "acp", "acb", "auc")
-
-    // 存储数据
-//    ideaidData.write.mode("overwrite").saveAsTable("test.ocpc_detail_report_hourly20190109")
-    conversionData1.write.mode("overwrite").saveAsTable("test.ocpc_summary_report_hourly20190109")
+    
+//    conversionData.write.mode("overwrite").saveAsTable("test.ocpc_summary_report_hourly20190109")
 
 
   }
