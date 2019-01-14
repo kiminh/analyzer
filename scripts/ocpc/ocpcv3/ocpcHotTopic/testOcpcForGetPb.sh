0b960f6dd8422a0162f8c7aa353755c1c456d5d4
sunjianqiang <sunjianqiang@qutoutiao.net>
2019-01-14 19:27:12 +0800

    change get pb

--- scripts/ocpc/ocpcv3/ocpcHotTopic/testOcpcForGetPb.sh
+++ scripts/ocpc/ocpcv3/ocpcHotTopic/testOcpcForGetPb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#sh testOcpcForGetPb.sh model.OcpcGetPb 2019-01-05 12
+#sh testOcpcForGetPb.sh model.OcpcGetPb 2019-01-02 12 hottopic_v1 hottopic 1
 
 cur=/data/cpc/anal
 SPARK_HOME=/usr/lib/spark-current
@@ -8,6 +8,9 @@ queue=root.cpc.develop
 
 date=$1
 hour=$2
+version=$3
+media=$4
+isKnown=$5
 
 jars=(
     "$cur/lib/mysql-connector-java-5.1.41-bin.jar"
@@ -24,4 +27,5 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
     --class com.cpc.spark.ocpcV3.HotTopicOcpc.model.OcpcGetPb \
-    /home/cpc/sunjianqiang/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $date $hour
+    /home/cpc/sunjianqiang/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $date $hour $version $ media $isKnown
+
