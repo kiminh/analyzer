d73c30815b4a85aa0be0e7264ce5b115b9218338
dongjinbao <dongjinbao@qutoutiao.net>
2019-01-16 18:57:47 +0800

    test

--- src/main/scala/com/cpc/spark/hottopic/HotTopicCtrAuc.scala
+++ src/main/scala/com/cpc/spark/hottopic/HotTopicCtrAuc.scala
@@ -46,48 +46,49 @@ object HotTopicCtrAuc {
         for (ctrModelName <- ctrModelNames) {
             val ctrModelUnion = union.filter(s"ctr_model_name = '$ctrModelName'")
             val ctrModelAuc = CalcMetrics.getAuc(spark,ctrModelUnion)
-            DetailAucListBuffer += DetailAuc(tag = "ctr_model_name",
+            DetailAucListBuffer += DetailAuc(
                 name = ctrModelName,
                 auc = ctrModelAuc,
                 sum = ctrModelUnion.count().toDouble,
+                day = date,
                 media = media,
-                day = date)
+                tag = "ctr_model_name")
         }
         val time2=System.currentTimeMillis()
         println("ctrModelNames cost is " + (time2-time1))
         //分行业
-        val adclassList = union.select("adclass")
-          .distinct()
-          .collect()
-          .map(x => x.getAs[String]("adclass"))
-        println("adclassList 's num is " + adclassList.length)
-        for (adclass <- adclassList) {
-            val adclassUnion = union.filter(s"adclass = '$adclass'")
-            val adclassAuc = CalcMetrics.getAuc(spark,adclassUnion)
-
-            DetailAucListBuffer += DetailAuc(tag = "ctr_model_name",
-                name = adclass,
-                auc = adclassAuc,
-                sum = adclassUnion.count().toDouble,
-                media = media,
-                day = date)
-        }
-//        val adclassAucList = CalcMetrics.getGauc(spark,union,"adclass").rdd
-//          .map(x => {
-//              val adclass = x.getAs[String]("name")
-//              val auc = x.getAs[Double]("auc")
-//              val sum = x.getAs[Double]("sum")
-//              (adclass,auc,sum)
-//          })
+//        val adclassList = union.select("adclass")
+//          .distinct()
 //          .collect()
-//        for(adclassAuc <- adclassAucList) {
-//            DetailAucListBuffer += DetailAuc(tag = "adclass",
-//                name = adclassAuc._1,
-//                auc = adclassAuc._2,
-//                sum = adclassAuc._3,
+//          .map(x => x.getAs[String]("adclass"))
+//        println("adclassList 's num is " + adclassList.length)
+//        for (adclass <- adclassList) {
+//            val adclassUnion = union.filter(s"adclass = '$adclass'")
+//            val adclassAuc = CalcMetrics.getAuc(spark,adclassUnion)
+//
+//            DetailAucListBuffer += DetailAuc(tag = "ctr_model_name",
+//                name = adclass,
+//                auc = adclassAuc,
+//                sum = adclassUnion.count().toDouble,
 //                media = media,
 //                day = date)
 //        }
+        val adclassAucList = CalcMetrics.getGauc(spark,union,"adclass").rdd
+          .map(x => {
+              val adclass = x.getAs[String]("name")
+              val auc = x.getAs[Double]("auc")
+              val sum = x.getAs[Double]("sum")
+              (adclass,auc,sum)
+          })
+          .collect()
+        for(adclassAuc <- adclassAucList) {
+            DetailAucListBuffer += DetailAuc(tag = "adclass",
+                name = adclassAuc._1,
+                auc = adclassAuc._2,
+                sum = adclassAuc._3,
+                media = media,
+                day = date)
+        }
         val time3=System.currentTimeMillis()
         println("adclassAucList cost is " + (time3-time2))
         //分栏位
@@ -99,12 +100,13 @@ object HotTopicCtrAuc {
         for (adslotId <- adslotIds) {
             val adslotIdUnion = union.filter(s"adslot_id = '$adslotId'")
             val adslotIdAuc = CalcMetrics.getAuc(spark,adslotIdUnion)
-            DetailAucListBuffer += DetailAuc(tag = "adslot_id",
+            DetailAucListBuffer += DetailAuc(
                 name = adslotId,
                 auc = adslotIdAuc,
                 sum = adslotIdUnion.count().toDouble,
+                day = date,
                 media = media,
-                day = date)
+                tag = "adslot_id")
         }
         val time4=System.currentTimeMillis()
         println("adslotIds cost is " + (time4-time3))
@@ -118,12 +120,13 @@ object HotTopicCtrAuc {
           })
           .collect()
         for(userIdAuc <- userIdAucList) {
-            DetailAucListBuffer += DetailAuc(tag = "userid",
+            DetailAucListBuffer += DetailAuc(
                 name = userIdAuc._1,
                 auc = userIdAuc._2,
                 sum = userIdAuc._3,
+                day = date,
                 media = media,
-                day = date)
+                tag = "userid")
         }
         val time5=System.currentTimeMillis()
         println("userIdAucList cost is " + (time5-time4))
@@ -136,12 +139,13 @@ object HotTopicCtrAuc {
         for (adslotType <- adslotTypes) {
             val adslotIdUnion = union.filter(s"adslot_type = '$adslotType'")
             val adslotIdAuc = CalcMetrics.getAuc(spark,adslotIdUnion)
-            DetailAucListBuffer += DetailAuc(tag = "adslot_type",
+            DetailAucListBuffer += DetailAuc(
                 name = adslotType,
                 auc = adslotIdAuc,
                 sum = adslotIdUnion.count().toDouble,
+                day = date,
                 media = media,
-                day = date)
+                tag = "adslot_type")
         }
         val time6=System.currentTimeMillis()
         println("adslotTypes cost is " + (time6-time5))
@@ -154,12 +158,12 @@ object HotTopicCtrAuc {
           .insertInto("dl_cpc.cpc_detail_auc")
 
     }
-    case class DetailAuc(var tag:String = "",
-                         var name:String = "",
+    case class DetailAuc(var name:String = "",
                          var auc:Double = 0,
                          var sum:Double = 0,
+                         var day:String = "",
                          var media:String = "",
-                         var day:String = "")
+                         var tag:String = "")
 }
 
 /**
