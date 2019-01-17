28c764660577d84204670675321833d194d11e8e
wangjun <wangjun02@qutoutiao.net>
2019-01-17 16:09:20 +0800

    fix bug

--- src/main/scala/com/cpc/spark/ocpcV3/ocpc/filter/OcpcCalculateAUC.scala
+++ src/main/scala/com/cpc/spark/ocpcV3/ocpc/filter/OcpcCalculateAUC.scala
@@ -33,24 +33,24 @@ object OcpcCalculateAUC {
 //    data
 //      .repartition(10).write.mode("overwrite").saveAsTable(tableName1)
 
-    // 设置cv门槛
-    var cvThreshold = 100
-    if (conversionGoal == "3") {
-      cvThreshold = 30
-    } else {
-      cvThreshold = 100
-    }
-
-    val processedData = filterData(tableName1, cvThreshold, conversionGoal, version, date, hour, spark)
-    val tableName2 = "dl_cpc.ocpc_auc_filter_conversiongoal"
-    processedData
-      .repartition(10).write.mode("overwrite").insertInto(tableName2)
+//    // 设置cv门槛
+//    var cvThreshold = 100
+//    if (conversionGoal == "3") {
+//      cvThreshold = 30
+//    } else {
+//      cvThreshold = 100
+//    }
+//
+//    val processedData = filterData(tableName1, cvThreshold, conversionGoal, version, date, hour, spark)
+//    val tableName2 = "dl_cpc.ocpc_auc_filter_conversiongoal"
+//    processedData
+//      .repartition(10).write.mode("overwrite").insertInto(tableName2)
 //    val tableName2 = "test.ocpc_auc_filter_conversiongoal"
 //    processedData
 //      .repartition(10).write.mode("overwrite").saveAsTable(tableName2)
 
     // 计算auc
-    val aucData = getAuc(tableName2, conversionGoal, version, date, hour, spark)
+    val aucData = getAuc(tableName1, conversionGoal, version, date, hour, spark)
     val resultDF = aucData
       .withColumn("conversion_goal", lit(conversionGoal))
       .withColumn("date", lit(date))
