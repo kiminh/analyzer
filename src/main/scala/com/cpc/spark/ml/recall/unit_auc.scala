0ad4c0642231eccd9d6afb6bda450a712cb1d839
段广东 <duanguangdong@qutoutiao.net>
2019-01-19 17:15:06 +0800

    dnn 年龄模型

--- src/main/scala/com/cpc/spark/ml/recall/unit_auc.scala
+++ src/main/scala/com/cpc/spark/ml/recall/unit_auc.scala
@@ -12,11 +12,13 @@ object unit_auc {
       .appName("unit auc calculation")
       .enableHiveSupport()
       .getOrCreate()
+    import spark.implicits._
     val cal1 = Calendar.getInstance()
     cal1.add(Calendar.DATE, -1)
     val tardate = new SimpleDateFormat("yyyy-MM-dd").format(cal1.getTime)
     val testset_result = spark.read.parquet("hdfs://emr-cluster/user/cpc/result/cvrTrain")
     val DetailAucListBuffer = scala.collection.mutable.ListBuffer[DetailAuc]()
+    val id_tag = "unitid"
 
     //分广告主
     val unitIdAucList = CalcMetrics.getGauc(spark,testset_result,"unitid").rdd
@@ -25,21 +27,28 @@ object unit_auc {
         val auc = x.getAs[Double]("auc")
         val sum = x.getAs[Double]("sum")
         (unitid,auc,sum)
-      })
-      .collect()
+      }).collect()
     for(unitIdAuc <- unitIdAucList) {
       DetailAucListBuffer += DetailAuc(
-        name = unitIdAuc._1,
+        id = unitIdAuc._1,
         auc = unitIdAuc._2,
-        sum = unitIdAuc._3,
+        count = unitIdAuc._3,
         day = tardate,
-        tag = "unitid")
+        tag = id_tag)
     }
-    val time5=System.currentTimeMillis()
+
+    val detailAuc = DetailAucListBuffer.toList.toDF()
+
+    detailAuc.createOrReplaceTempView("unitid_table")
+    spark.sql(
+      s"""
+        |insert overwrite table dl_cpc.cpc_id_bscvr_auc partition (day='$tardate', tag='$id_tag')
+        |select id, auc, count from unitid_table
+      """.stripMargin)
   }
-  case class DetailAuc(var name:String = "",
+  case class DetailAuc(var id:String = "",
                        var auc:Double = 0,
-                       var sum:Double = 0,
+                       var count:Double = 0,
                        var day:String = "",
                        var tag:String = "")
 }
