87e8495d94607b8de8ea5f56590a2afa398c18c1
dongjinbao <dongjinbao@qutoutiao.net>
2019-01-19 17:03:46 +0800

    test

--- src/main/scala/com/cpc/spark/coin/ReportCoinMetrics.scala
+++ src/main/scala/com/cpc/spark/coin/ReportCoinMetrics.scala
@@ -57,8 +57,10 @@ object ReportCoinMetrics {
              """.stripMargin
 
         val union = spark.sql(unionSql)
+        //保存到临时表里
+        union.write.mode("overwrite").saveAsTable("test.union")
 
-        union.createOrReplaceTempView("union")
+//        union.createOrReplaceTempView("union")
 
 //        val ideaidSql =
 //            s"""
@@ -125,6 +127,64 @@ object ReportCoinMetrics {
 //        println("insert into report2.report_coin_ideaid_metrics success!")
 //        ideaidMetrics.unpersist()
 
+//        val useridSql =
+//            s"""
+//               |select
+//               |    userid,
+//               |    show_num,
+//               |    coin_show_num,
+//               |    if (show_num!=0,round(coin_show_num/show_num, 6),0) as coin_show_rate,
+//               |    click_num,
+//               |    coin_click_num,
+//               |    if (click_num!=0,round(coin_click_num/click_num, 6),0) as coin_click_rate,
+//               |    if (show_num!=0,round(click_num/show_num, 6),0) as ctr,
+//               |    if (coin_show_num!=0,round(coin_click_num/coin_show_num, 6),0) as coin_ctr,
+//               |    convert_num,
+//               |    coin_convert_num,
+//               |    if (convert_num!=0,round(coin_convert_num/convert_num,6),0) as coin_convert_rate,
+//               |    if (click_num!=0,round(convert_num/click_num, 6),0) as cvr,
+//               |    if (coin_click_num!=0,round(coin_convert_num/coin_click_num, 6),0) as coin_cvr,
+//               |    click_total_price,
+//               |    coin_click_total_price,
+//               |    uid_num,
+//               |    if (show_num!=0,round(click_total_price*10/show_num,6),0) as cpm,
+//               |    if (click_num!=0,round(click_total_price*10/click_num,6),0) as acp,
+//               |    if (uid_num!=0,round(click_total_price*10/uid_num,6),0) as arpu,
+//               |    if (uid_num!=0,round(show_num/uid_num,6),0) as aspu,
+//               |    if (uid_num!=0,round(convert_num*100/uid_num,6),0) as acpu,
+//               |    '$date' as `date`
+//               |from
+//               |(
+//               |    select userid,
+//               |        sum(isshow) as show_num, --展示数
+//               |        sum(if (isshow=1 and ext_int['is_auto_coin'] = 1, 1, 0)) as coin_show_num, --金币展示数
+//               |        sum(isclick) as click_num, --点击数
+//               |        sum(if (isclick=1 and ext_int['is_auto_coin'] = 1, 1, 0)) as coin_click_num, --金币点击数
+//               |        sum(case when label2 = 1 then 1 else 0 end) as convert_num, --转化数
+//               |        sum(case when label2 = 1 and ext_int['is_auto_coin'] = 1 then 1 else 0 end) as coin_convert_num, --金币样式转化数
+//               |        sum(case WHEN isclick = 1 then price else 0 end) as click_total_price, --点击总价
+//               |        sum(case WHEN isclick = 1 and ext_int['is_auto_coin'] = 1 then price else 0 end) as coin_click_total_price, --金币点击总价
+//               |        count(distinct uid) as uid_num --用户数
+//               |    from test.union
+//               |    group by userid
+//               |) c
+//             """.stripMargin
+
+        val useridAucListSql =
+            s"""
+               |select cast(userid as string) as userid,ext['exp_cvr'].int_value as score,label2 as label
+               |from test.union
+             """.stripMargin
+
+        val useridAucList = spark.sql(useridAucListSql)
+
+        val uAuc = CalcMetrics.getGauc(spark,useridAucList,"userid")
+          .select("name","auc")
+          .withColumn("userid",string2Int(col("name")))
+          .drop("name")
+          .select("userid","auc")
+        uAuc.write.mode("overwrite").saveAsTable("test.uauc")
+
         val useridSql =
             s"""
                |select
@@ -150,6 +210,7 @@ object ReportCoinMetrics {
                |    if (uid_num!=0,round(click_total_price*10/uid_num,6),0) as arpu,
                |    if (uid_num!=0,round(show_num/uid_num,6),0) as aspu,
                |    if (uid_num!=0,round(convert_num*100/uid_num,6),0) as acpu,
+               |    auc,
                |    '$date' as `date`
                |from
                |(
@@ -163,50 +224,48 @@ object ReportCoinMetrics {
                |        sum(case WHEN isclick = 1 then price else 0 end) as click_total_price, --点击总价
                |        sum(case WHEN isclick = 1 and ext_int['is_auto_coin'] = 1 then price else 0 end) as coin_click_total_price, --金币点击总价
                |        count(distinct uid) as uid_num --用户数
-               |    from union
+               |    from test.union
                |    group by userid
                |) c
+               |left outer join test.uauc d
+               |on c.userid = d.userid
              """.stripMargin
 
-        val useridAucListSql =
-            s"""
-               |select cast(userid as string) as userid,ext['exp_cvr'].int_value as score,label2 as label
-               |from union
-             """.stripMargin
 
-        val useridAucList = spark.sql(useridAucListSql)
-
-        val uAuc = CalcMetrics.getGauc(spark,useridAucList,"userid")
-          .select("name","auc")
-          .withColumn("userid",string2Int(col("name")))
-          .drop("name")
 
         uAuc.show(20)
         println("uAuc 's count is " + uAuc.count())
 
-        val useridOtherMetrics = spark.sql(useridSql)
-        useridOtherMetrics.show(10)
-        println("useridOtherMetrics 's count is " + useridOtherMetrics.count())
-        val useridMetrics = useridOtherMetrics
-          .join(uAuc,Seq("userid"),"left_outer")
-          .select("userid","show_num","coin_show_num","coin_show_rate","click_num","coin_click_num","coin_click_rate"
-          ,"ctr","coin_ctr","convert_num","coin_convert_num","coin_convert_rate","cvr","coin_cvr","click_total_price",
-          "coin_click_total_price","uid_num","cpm","acp","arpu","aspu","acpu","auc","date")
-          .cache()
+        val useridMetrics = spark.sql(useridSql).cache()
+//        useridOtherMetrics.show(10)
+//        println("useridOtherMetrics 's count is " + useridOtherMetrics.count())
+//        val useridMetrics = useridOtherMetrics
+//          .join(uAuc,Seq("userid"),"left_outer")
+//          .select("userid","show_num","coin_show_num","coin_show_rate","click_num","coin_click_num","coin_click_rate"
+//          ,"ctr","coin_ctr","convert_num","coin_convert_num","coin_convert_rate","cvr","coin_cvr","click_total_price",
+//          "coin_click_total_price","uid_num","cpm","acp","arpu","aspu","acpu","auc","date")
+//          .cache()
 
         useridMetrics.show(20)
 
-//        useridMetrics.repartition(1)
-//          .write
-//          .mode("overwrite")
-//          .insertInto("dl_cpc.cpc_report_coin_userid_metrics")
-//
-//        val useridMetricsDelSql = s"delete from report2.report_coin_userid_metrics where `date` = '$date'"
-//        OperateMySQL.del(useridMetricsDelSql)
-//        useridMetrics.write.mode(SaveMode.Append)
-//          .jdbc(mariadb_write_url, "report2.report_coin_userid_metrics", mariadb_write_prop)
-//        println("insert into report2.report_coin_userid_metrics success!")
+        useridMetrics.repartition(1)
+          .write
+          .mode("overwrite")
+          .insertInto("dl_cpc.cpc_report_coin_userid_metrics")
+
+        val useridMetricsDelSql = s"delete from report2.report_coin_userid_metrics where `date` = '$date'"
+        OperateMySQL.del(useridMetricsDelSql)
+        useridMetrics.write.mode(SaveMode.Append)
+          .jdbc(mariadb_write_url, "report2.report_coin_userid_metrics", mariadb_write_prop)
+        println("insert into report2.report_coin_userid_metrics success!")
         useridMetrics.unpersist()
+
+        val deluAucSql = "delete from test.uauc"
+
+        spark.sql(deluAucSql)
+
+        val deleteUnionSql = "delete from test.union"
+        spark.sql(deleteUnionSql)
     }
     def string2Int = udf((name:String) => {
         name.toInt
