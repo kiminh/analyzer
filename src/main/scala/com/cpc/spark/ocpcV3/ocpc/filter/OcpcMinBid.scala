dc2c45b8f097ac66c374c9e856b5f6c41d5a5c74
wangjun <wangjun02@qutoutiao.net>
2019-01-21 22:02:41 +0800

    fix bug

--- src/main/scala/com/cpc/spark/ocpcV3/ocpc/filter/OcpcMinBid.scala
+++ src/main/scala/com/cpc/spark/ocpcV3/ocpc/filter/OcpcMinBid.scala
@@ -38,7 +38,7 @@ object OcpcMinBid {
       .repartition(10).write.mode("overwrite").insertInto("dl_cpc.ocpc_check_min_bid")
 //      .repartition(10).write.mode("overwrite").saveAsTable("test.ocpc_check_min_bid")
 
-    val data = resultDF.orderBy(col("cnt")).limit(3000)
+    val data = resultDF.orderBy(desc("cnt")).limit(3000)
 
     savePbPack(data, "ocpc_minbidv2.pb")
   }
