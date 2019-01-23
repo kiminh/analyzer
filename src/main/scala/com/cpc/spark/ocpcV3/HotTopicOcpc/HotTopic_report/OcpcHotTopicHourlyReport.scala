6a1def7dc44019def314de9f4f1b9a7389046336
sunjianqiang <sunjianqiang@qutoutiao.net>
2019-01-23 19:32:11 +0800

    add columns

--- src/main/scala/com/cpc/spark/ocpcV3/HotTopicOcpc/HotTopic_report/OcpcHotTopicHourlyReport.scala
+++ src/main/scala/com/cpc/spark/ocpcV3/HotTopicOcpc/HotTopic_report/OcpcHotTopicHourlyReport.scala
@@ -26,8 +26,10 @@ object OcpcHotTopicHourlyReport {
     println(s"successfully save table into $tableName")
 
     val result1 = result
-      .withColumnRenamed("userid","user_id" )
-      .withColumnRenamed("unitid","idea_id")
+//      .withColumnRenamed("userid","user_id" )
+//      .withColumnRenamed("unitid","idea_id")
+        .withColumn("user_id",col("userid"))
+      .withColumn("ideaid", col("unitid"))
       .withColumn("pre_cvr", lit(0) )
       .withColumn("post_cvr",lit(0) )
       .withColumn("q_factor", lit(0))
