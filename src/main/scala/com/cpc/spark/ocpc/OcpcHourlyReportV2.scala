3088173f47306d08df38da914ac10099417c5fa7
wangjun <wangjun02@qutoutiao.net>
2019-01-25 16:11:42 +0800

    fix bug

--- src/main/scala/com/cpc/spark/ocpc/OcpcHourlyReportV2.scala
+++ src/main/scala/com/cpc/spark/ocpc/OcpcHourlyReportV2.scala
@@ -155,7 +155,7 @@ object OcpcHourlyReportV2 {
       .join(cvr2Data, Seq("searchid"), "left_outer")
       .join(cvr3Data, Seq("searchid"), "left_outer")
       .withColumn("iscvr", when(col("conversion_goal") === 1, col("iscvr1")).otherwise(when(col("conversion_goal") === 2, col("iscvr2")).otherwise(col("iscvr3"))))
-      .select("searchid", "ideaid", "userid", "isclick", "isshow", "price", "cpagiven", "bid", "kvalue", "conversion_goal", "ocpc_step", "iscvr1", "iscvr2", "iscvr3", "iscvr")
+      .select("searchid", "ideaid", "userid", "isclick", "isshow", "price", "exp_ctr", "exp_cvr", "cpagiven", "bid", "kvalue", "conversion_goal", "ocpc_step", "iscvr1", "iscvr2", "iscvr3", "iscvr")
 
     resultDF.show(10)
 
