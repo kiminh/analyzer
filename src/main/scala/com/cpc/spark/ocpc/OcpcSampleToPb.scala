ddaecd06ebfcf4499d193f1558db3c310642812f
wangjun <wangjun02@qutoutiao.net>
2019-01-29 15:15:50 +0800

    fix bug

--- src/main/scala/com/cpc/spark/ocpc/OcpcSampleToPb.scala
+++ src/main/scala/com/cpc/spark/ocpc/OcpcSampleToPb.scala
@@ -741,13 +741,14 @@ object OcpcSampleToPb {
           sum(col("isclick")).alias("click"),
           sum(col("iscvr")).alias("conversion")
         )
-        .withColumn("post_cvr", col("conversion") * 5.0 / col("click"))
-        .select("ideaid", "adclass", "post_cvr")
+        .withColumn("post_cvr", col("conversion") * 1.0 / col("click"))
+        .withColumn("post_cvr_cali", col("post_cvr") * 5.0)
+        .select("ideaid", "adclass", "post_cvr", "post_cvr_cali")
 
     val caliData1 = data1
         .join(cvrData1, Seq("ideaid", "adclass"), "left_outer")
-        .select("searchid", "ideaid", "adclass", "exp_cvr", "isclick", "iscvr", "post_cvr")
-        .withColumn("pre_cvr", when(col("exp_cvr")> col("post_cvr"), col("post_cvr")).otherwise(col("exp_cvr")))
+        .select("searchid", "ideaid", "adclass", "exp_cvr", "isclick", "iscvr", "post_cvr", "post_cvr_cali")
+        .withColumn("pre_cvr", when(col("exp_cvr")> col("post_cvr_cali"), col("post_cvr_cali")).otherwise(col("exp_cvr")))
         .select("searchid", "ideaid", "adclass", "exp_cvr", "isclick", "iscvr", "post_cvr", "pre_cvr")
 
     val finalData1 = caliData1
@@ -814,13 +815,14 @@ object OcpcSampleToPb {
           sum(col("isclick")).alias("click"),
           sum(col("iscvr")).alias("conversion")
         )
-        .withColumn("post_cvr", col("conversion") * 5.0 / col("click"))
-        .select("ideaid", "adclass", "post_cvr")
+        .withColumn("post_cvr", col("conversion") * 1.0 / col("click"))
+        .withColumn("post_cvr_cali", col("post_cvr") * 5.0)
+        .select("ideaid", "adclass", "post_cvr", "post_cvr_cali")
 
     val caliData2 = data2
         .join(cvrData2, Seq("ideaid", "adclass"), "left_outer")
-        .select("searchid", "ideaid", "adclass", "exp_cvr", "isclick", "iscvr", "post_cvr")
-        .withColumn("pre_cvr", when(col("exp_cvr")>col("post_cvr"), col("post_cvr")).otherwise(col("exp_cvr")))
+        .select("searchid", "ideaid", "adclass", "exp_cvr", "isclick", "iscvr", "post_cvr", "post_cvr_cali")
+        .withColumn("pre_cvr", when(col("exp_cvr")>col("post_cvr_cali"), col("post_cvr_cali")).otherwise(col("exp_cvr")))
         .select("searchid", "ideaid", "adclass", "exp_cvr", "isclick", "iscvr", "post_cvr", "pre_cvr")
 
     val finalData2 = caliData2
