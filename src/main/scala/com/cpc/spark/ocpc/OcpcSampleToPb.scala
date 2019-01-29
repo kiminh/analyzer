9304acc4a281a535cad1a0fed881987f0f14b275
wangjun <wangjun02@qutoutiao.net>
2019-01-29 15:55:17 +0800

    fix bug

--- src/main/scala/com/cpc/spark/ocpc/OcpcSampleToPb.scala
+++ src/main/scala/com/cpc/spark/ocpc/OcpcSampleToPb.scala
@@ -61,7 +61,7 @@ object OcpcSampleToPb {
     val resultDF = getCPCbid(result3, date, hour, spark)
 
 
-//    resultDF.write.mode("overwrite").saveAsTable("dl_cpc.ocpc_qtt_prev_pb")
+    resultDF.write.mode("overwrite").saveAsTable("dl_cpc.ocpc_qtt_prev_pb")
     resultDF
       .repartition(10).write.mode("overwrite").insertInto("dl_cpc.ocpc_pb_result_table_v7")
 //    resultDF.write.mode("overwrite").saveAsTable("test.ocpc_qtt_prev_pb20190129")
