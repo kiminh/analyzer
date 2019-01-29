3ac026b07bf0c458ce9da526a65e123dbb3033b1
wangjun <wangjun02@qutoutiao.net>
2019-01-29 15:44:24 +0800

    fix bug

--- src/main/scala/com/cpc/spark/ocpc/OcpcSampleToPb.scala
+++ src/main/scala/com/cpc/spark/ocpc/OcpcSampleToPb.scala
@@ -64,9 +64,9 @@ object OcpcSampleToPb {
 //    resultDF.write.mode("overwrite").saveAsTable("dl_cpc.ocpc_qtt_prev_pb")
     resultDF
       .repartition(10).write.mode("overwrite").insertInto("dl_cpc.ocpc_pb_result_table_v7")
-    resultDF.write.mode("overwrite").saveAsTable("test.ocpc_qtt_prev_pb20190129")
+//    resultDF.write.mode("overwrite").saveAsTable("test.ocpc_qtt_prev_pb20190129")
 
-//    savePbPack(resultDF)
+    savePbPack(resultDF)
 
   }
 
