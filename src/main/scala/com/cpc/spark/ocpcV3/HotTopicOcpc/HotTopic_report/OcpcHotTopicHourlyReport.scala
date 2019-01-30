f53ab5216e5b9868ef599c834519071d15092de8
sunjianqiang <sunjianqiang@qutoutiao.net>
2019-01-30 13:21:40 +0800

    change report parameter

--- src/main/scala/com/cpc/spark/ocpcV3/HotTopicOcpc/HotTopic_report/OcpcHotTopicHourlyReport.scala
+++ src/main/scala/com/cpc/spark/ocpcV3/HotTopicOcpc/HotTopic_report/OcpcHotTopicHourlyReport.scala
@@ -154,7 +154,7 @@ object OcpcHotTopicHourlyReport {
       .withColumn("is_step2", when(col("step2_percent")===1, 1).otherwise(0))
       .withColumn("cpa_ratio", col("cpa_given") * 1.0 / col("cpa_real"))
       .withColumn("cpa_ratio", when(col("cpa_real")===0, 9999999).otherwise(col("cpa_ratio")))
-      .withColumn("is_cpa_ok", when(col("cpa_ratio")>=0.5, 1).otherwise(0))
+      .withColumn("is_cpa_ok", when(col("cpa_ratio")>=0.95, 1).otherwise(0))
       .withColumn("impression", col("show_cnt"))
       .withColumn("click", col("ctr_cnt"))
       .withColumn("conversion", col("cvr_cnt"))
