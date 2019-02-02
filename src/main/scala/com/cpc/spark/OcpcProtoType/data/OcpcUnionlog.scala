3393b5fdde2711cda7d721ca050c9dc0370c1b72
wangjun <wangjun02@qutoutiao.net>
2019-02-02 20:31:11 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/data/OcpcUnionlog.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/data/OcpcUnionlog.scala
@@ -14,9 +14,8 @@ object OcpcUnionlog {
 
     val data = getBaseUnionlog(date, hour, spark)
 
-    // todo 删除临时措施
-//    data
-//      .repartition(100).write.mode("overwrite").insertInto("dl_cpc.ocpc_base_unionlog")
+    data
+      .repartition(100).write.mode("overwrite").insertInto("dl_cpc.ocpc_base_unionlog")
 //      .repartition(100).write.mode("overwrite").saveAsTable("test.ocpc_base_unionlog")
 
     println("successfully save data into table: dl_cpc.ocpc_base_unionlog")
