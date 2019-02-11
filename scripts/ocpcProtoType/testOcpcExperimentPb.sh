699b08a33e68b85f242dc14e8927cddb581931f7
wangjun <wangjun02@qutoutiao.net>
2019-02-11 15:31:04 +0800

    fix bug

--- scripts/ocpcProtoType/testOcpcExperimentPb.sh
+++ scripts/ocpcProtoType/testOcpcExperimentPb.sh
@@ -22,4 +22,4 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
     --class com.cpc.spark.OcpcProtoType.experiment.OcpcExperimentPb \
-    /home/cpc/wangjun/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $date $hour qtt_demo ab_test|cpa_suggest
+    /home/cpc/wangjun/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $date $hour qtt_demo "ab_test|cpa_suggest"
