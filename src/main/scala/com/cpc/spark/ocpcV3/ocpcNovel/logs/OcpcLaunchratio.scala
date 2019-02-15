686fa98a7a1d9052a96b51435f74ada941dc3bad
wangyao <wangyao@qutoutiao.net>
2019-02-15 11:52:35 +0800

    -

--- src/main/scala/com/cpc/spark/ocpcV3/ocpcNovel/logs/OcpcLaunchratio.scala
+++ src/main/scala/com/cpc/spark/ocpcV3/ocpcNovel/logs/OcpcLaunchratio.scala
@@ -158,9 +158,9 @@ object OcpcLaunchratio {
        """.stripMargin
 
     println(sql4)
-    spark.sql(sql4).withColumn("sum_money_ratio",round(col("money")/money_overall,3))
+    val data4result=spark.sql(sql4).withColumn("sum_money_ratio",round(col("money")/money_overall,3))
       .select("choose","mode","money","sum_money_ratio","cpm","acp","ctr","`date`")
-      .repartition(1).write.mode("overwrite").insertInto("dl_cpc.midu_ocpc_launch_ocpc_cpc")
+    data4result.repartition(1).write.mode("overwrite").insertInto("dl_cpc.midu_ocpc_launch_ocpc_cpc")
 
     val table2 = "report2.midu_ocpc_launch_ocpc_cpc"
     val deleteSql2 = s"delete from $table2 where `date` = '$date'"
@@ -203,9 +203,9 @@ object OcpcLaunchratio {
          """.stripMargin
     println(sql5)
 
-        spark.sql(sql5).withColumn("sum_money_ratio",round(col("money")/money_overall,3))
+    val data5result=spark.sql(sql5).withColumn("sum_money_ratio",round(col("money")/money_overall,3))
           .select("choose","usertype","money","sum_money_ratio","cpm","acp","ctr","`date`")
-          .repartition(1).write.mode("overwrite").insertInto("dl_cpc.midu_ocpc_launch_usertype")
+    data5result.repartition(1).write.mode("overwrite").insertInto("dl_cpc.midu_ocpc_launch_usertype")
 
     val table3 = "report2.midu_ocpc_launch_usertype"
     val deleteSql3 = s"delete from $table3 where `date` = '$date'"
@@ -252,9 +252,9 @@ object OcpcLaunchratio {
 
     println(sql6)
 
-    spark.sql(sql6).withColumn("sum_money_ratio",round(col("money")/money_overall,3))
+    val data6result=spark.sql(sql6).withColumn("sum_money_ratio",round(col("money")/money_overall,3))
       .select("choose","adclass","money","sum_money_ratio","cpm","acp","ctr","`date`")
-      .repartition(1).write.mode("overwrite").insertInto("dl_cpc.midu_ocpc_launch_adclass")
+    data6result.repartition(1).write.mode("overwrite").insertInto("dl_cpc.midu_ocpc_launch_adclass")
 
     val table4 = "report2.midu_ocpc_launch_adclass"
     val deleteSql4 = s"delete from $table4 where `date` = '$date'"
@@ -300,10 +300,10 @@ object OcpcLaunchratio {
                """.stripMargin
 
     println(sql7)
-    spark.sql(sql7)
+    val data7result=spark.sql(sql7)
       .select("usertype","sum_qtt_money_1","sum_qtt_money_2","sum_novel_money","avg_ratio_1","avg_ratio_2"
         ,"gt200","gt100","gt50","lt50","eq0","`date`")
-      .repartition(1).write.mode("overwrite").insertInto("dl_cpc.midu_ocpc_launch_usertype_ratio")
+    data7result.repartition(1).write.mode("overwrite").insertInto("dl_cpc.midu_ocpc_launch_usertype_ratio")
 
     val table5 = "report2.midu_ocpc_launch_usertype_ratio"
     val deleteSql5 = s"delete from $table5 where `date` = '$date'"
@@ -350,10 +350,10 @@ object OcpcLaunchratio {
          |  else 'notag' end
          """.stripMargin
     println(sql8)
-    spark.sql(sql8)
+    val data8result=spark.sql(sql8)
       .select("adclass","sum_qtt_money_1","sum_qtt_money_2","sum_novel_money","avg_ratio_1","avg_ratio_2"
         ,"gt200","gt100","gt50","lt50","eq0","`date`")
-      .repartition(1).write.mode("overwrite").insertInto("dl_cpc.midu_ocpc_launch_adclass_ratio")
+    data8result.repartition(1).write.mode("overwrite").insertInto("dl_cpc.midu_ocpc_launch_adclass_ratio")
 
     val table6 = "report2.midu_ocpc_launch_usertype_ratio"
     val deleteSql6 = s"delete from $table6 where `date` = '$date'"
