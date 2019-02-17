7c15a6b06cb6d3b18d6e2adeedb2a4dd687f4efc
zhanghongyang <zhanghongyang@qutoutiao.net>
2019-02-17 22:11:46 +0800

    fix

--- src/main/scala/com/cpc/spark/adindex/AdvertisingIndex.scala
+++ src/main/scala/com/cpc/spark/adindex/AdvertisingIndex.scala
@@ -3,9 +3,8 @@ package com.cpc.spark.adindex
 import java.text.SimpleDateFormat
 import java.util.Calendar
 
-import org.apache.commons.httpclient.HttpClient
-import org.apache.commons.httpclient.methods.GetMethod
 import org.apache.spark.sql.SparkSession
+import scalaj.http.Http
 
 object AdvertisingIndex {
   def main(args: Array[String]): Unit = {
@@ -25,15 +24,17 @@ object AdvertisingIndex {
     val hour = cal.get(Calendar.HOUR_OF_DAY)
     val minute = cal.get(Calendar.MINUTE)
 
-    val client = new HttpClient
-    val method = new GetMethod(url)
-    client.executeMethod(method)
-
-    println("status:" + method.getStatusLine)
-
-    val body = method.getResponseBodyAsString
-    method.releaseConnection()
-    val data = body.substring(17)
+    //    val client = new HttpClient
+    //    val method = new GetMethod(url)
+    //    val response=client.executeMethod(method)
+    //    println("status:" + method.getStatusLine.getStatusCode)
+    //
+    //    val body = method.getResponseBodyAsString
+    //    method.releaseConnection()
+    //    val data = body.substring(15)
+    val reponse = Http(url).asString
+    val data = reponse.body.substring(16)
+    println(data.length)
 
     val idxItems = idxinterface.Idx.IdxItems.parseFrom(data.getBytes)
 
@@ -41,70 +42,70 @@ object AdvertisingIndex {
     val ditemsCount = idxItems.getDitemsCount
     println("count: " + gitemsCount + ", ditemsCount")
 
-/*
-    var ideaItemMap = Map[Int, Idea]()
-    var unitItemMap = Map[Int, Group]()
-    var idx = Seq[Group]()
+    /*
+        var ideaItemMap = Map[Int, Idea]()
+        var unitItemMap = Map[Int, Group]()
+        var idx = Seq[Group]()
 
-    for (i <- 0 until gitemsCount) {
-      val dItem = idxItems.getDitems(i) //ideaItem
+        for (i <- 0 until gitemsCount) {
+          val dItem = idxItems.getDitems(i) //ideaItem
 
-      val ideaid = dItem.getIdeaid
-      val idea = GetItem.getIdea(dItem)
-      ideaItemMap += (ideaid -> idea)
-    }
-
-    for (i <- 0 until ditemsCount) {
-      val gItems = idxItems.getGitems(i) //groupItem
-
-      val unitid = GetItem.getGroup(gItems)
-      unitid.foreach { u =>
-        val ideaid = u.ideaid
-        unitItemMap += (ideaid -> u)
-      }
-    }
-    println("unitItemMap count:  " + unitItemMap.size, "head:" + unitItemMap.head)
-    println("ideaItemMap count:  " + ideaItemMap.size, "head:" + ideaItemMap.head)
-
-
-    unitItemMap.foreach { u =>
-      val uIdeaid = u._1
-      val unitItem = u._2
-      ideaItemMap.foreach { i =>
-        val iIdeaid = i._1
-        val ideaItem = i._2
-        if (uIdeaid == iIdeaid) {
-          unitItem.copy(mtype = ideaItem.mtype,
-            width = ideaItem.width,
-            height = ideaItem.height,
-            interaction = ideaItem.interaction,
-            `class` = ideaItem.`class`,
-            material_level = ideaItem.material_level,
-            siteid = ideaItem.siteid,
-            white_user_ad_corner = ideaItem.white_user_ad_corner,
-            date = date,
-            hour = hour.toString,
-            minute = minute.toString,
-            timestamp = timestamp)
+          val ideaid = dItem.getIdeaid
+          val idea = GetItem.getIdea(dItem)
+          ideaItemMap += (ideaid -> idea)
         }
-        idx :+= unitItem
-      }
-    }
-    println("idx count:  " + idx.size, "head:" + idx.head)
 
-    val idxRDD = spark.sparkContext.parallelize(idx)
-    spark.createDataFrame(idx)
-      .repartition(1)
-      .write
-      .mode("overwrite")
-      .parquet(s"hdfs://emr-cluster2/warehouse/dl_cpc.db/cpc_ad_index/date=$date/hour=$hour/minute=$minute")
+        for (i <- 0 until ditemsCount) {
+          val gItems = idxItems.getGitems(i) //groupItem
 
-    spark.sql(
-      s"""
-         |alter table dl_cpc.xx if not exists add partitions(date = "$date",hour="$hour",minute="$minute")
-         |location 'hdfs://emr-cluster2/warehouse/dl_cpc.db/cpc_ad_index/date=$date/hour=$hour/minute=$minute'
-       """.stripMargin)
-*/
+          val unitid = GetItem.getGroup(gItems)
+          unitid.foreach { u =>
+            val ideaid = u.ideaid
+            unitItemMap += (ideaid -> u)
+          }
+        }
+        println("unitItemMap count:  " + unitItemMap.size, "head:" + unitItemMap.head)
+        println("ideaItemMap count:  " + ideaItemMap.size, "head:" + ideaItemMap.head)
+
+
+        unitItemMap.foreach { u =>
+          val uIdeaid = u._1
+          val unitItem = u._2
+          ideaItemMap.foreach { i =>
+            val iIdeaid = i._1
+            val ideaItem = i._2
+            if (uIdeaid == iIdeaid) {
+              unitItem.copy(mtype = ideaItem.mtype,
+                width = ideaItem.width,
+                height = ideaItem.height,
+                interaction = ideaItem.interaction,
+                `class` = ideaItem.`class`,
+                material_level = ideaItem.material_level,
+                siteid = ideaItem.siteid,
+                white_user_ad_corner = ideaItem.white_user_ad_corner,
+                date = date,
+                hour = hour.toString,
+                minute = minute.toString,
+                timestamp = timestamp)
+            }
+            idx :+= unitItem
+          }
+        }
+        println("idx count:  " + idx.size, "head:" + idx.head)
+
+        val idxRDD = spark.sparkContext.parallelize(idx)
+        spark.createDataFrame(idx)
+          .repartition(1)
+          .write
+          .mode("overwrite")
+          .parquet(s"hdfs://emr-cluster2/warehouse/dl_cpc.db/cpc_ad_index/date=$date/hour=$hour/minute=$minute")
+
+        spark.sql(
+          s"""
+             |alter table dl_cpc.xx if not exists add partitions(date = "$date",hour="$hour",minute="$minute")
+             |location 'hdfs://emr-cluster2/warehouse/dl_cpc.db/cpc_ad_index/date=$date/hour=$hour/minute=$minute'
+           """.stripMargin)
+    */
     println("done.")
 
   }
