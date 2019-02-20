1c222638cd10be595260ea42ce24f14cc7b0f247
段广东 <duanguangdong@qutoutiao.net>
2019-02-20 19:43:34 +0800

    dnn 年龄模型

--- src/main/scala/com/cpc/spark/ml/recall/prepare_bsCvr_dnnSample.scala
+++ src/main/scala/com/cpc/spark/ml/recall/prepare_bsCvr_dnnSample.scala
@@ -30,7 +30,7 @@ object prepare_bsCvr_dnnSample {
 
     val sampleDay = getDay(date, 1)
 
-    val Array(traindata, testdata) = train.randomSplit(Array(0.95, 0.05), 1)
+    val Array(traindata, testdata) = train.randomSplit(Array(0.8, 0.2), 1)
 
     traindata.repartition(1000)
       .write
