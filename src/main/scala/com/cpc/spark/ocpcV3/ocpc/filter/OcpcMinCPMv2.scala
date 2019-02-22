436c7acb746fc2da16f0582b95ee1decbc92a09e
wangjun <wangjun02@qutoutiao.net>
2019-02-22 16:12:21 +0800

    fix bug

--- src/main/scala/com/cpc/spark/ocpcV3/ocpc/filter/OcpcMinCPMv2.scala
+++ src/main/scala/com/cpc/spark/ocpcV3/ocpc/filter/OcpcMinCPMv2.scala
@@ -203,7 +203,7 @@ object OcpcMinCPMv2 {
        |  percentile(bid, 0.015) as min_bid,
        |  percentile(cpm, 0.15) as min_cpm,
        |  count(1) as cnt,
-       |  percentile(exp_cvr, 0.98) * 1.5 as pcvr
+       |  percentile(exp_cvr, 0.98) * 1.2 as pcvr
        |FROM
        |  base_data
        |GROUP BY hr, adslot_type, city_level, floor(adclass/1000), ocpc_flag
