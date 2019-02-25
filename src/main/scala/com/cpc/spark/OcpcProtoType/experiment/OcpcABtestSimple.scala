b3c24d85b7b67eb7c41424b16162039f5b7c5f52
wangjun <wangjun02@qutoutiao.net>
2019-02-25 22:22:48 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/experiment/OcpcABtestSimple.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/experiment/OcpcABtestSimple.scala
@@ -1,12 +1,17 @@
 package com.cpc.spark.OcpcProtoType.experiment
 
+import java.io.FileOutputStream
 import java.text.SimpleDateFormat
 import java.util.Calendar
 
 import com.cpc.spark.ocpc.OcpcUtils.getTimeRangeSql2
 import com.typesafe.config.ConfigFactory
+import ocpc.Ocpc
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
+import ocpcabtest.ocpcabtest.{OcpcList, SingleRecord}
+
+import scala.collection.mutable.ListBuffer
 
 object OcpcABtestSimple {
   def main(args: Array[String]): Unit = {
@@ -18,20 +23,61 @@ object OcpcABtestSimple {
     // 计算日期周期
     val date = args(0).toString
     val hour = args(1).toString
-    val media = args(2).toString
-    val version = args(3).toString
-    val expTag = "ab_test"
 
+    val conf = ConfigFactory.load("ocpc")
+    val expDataPath = conf.getString("ocpc_all.ocpc_abtest.path")
+    val fileName = conf.getString("ocpc_all.ocpc_abtest.pbfile")
     println("parameters:")
-    println(s"date=$date, hour=$hour, version=$version, media=$media, expTag=$expTag")
-    val data = readExpSet(date, hour, spark)
+    println(s"date=$date, hour=$hour")
+    println(s"path is: $expDataPath")
+    println(s"fileName is: $fileName")
 
+    val data = readExpSet(expDataPath, date, hour, spark)
+
+    savePbPack(data, fileName)
+  }
+
+  def savePbPack(dataset: DataFrame, filename: String): Unit = {
+    var list = new ListBuffer[SingleRecord]
+    println("size of the dataframe")
+    val resultData = dataset.select("identifier", "cpc_bid")
+    println(resultData.count)
+    resultData.show(10)
+    resultData.printSchema()
+    var cnt = 0
+
+    for (record <- resultData.collect()) {
+      val id = record.getAs[String]("identifier")
+      val cpc_bid = record.getAs[Double]("cpc_bid")
+
+
+      println(s"identifier:$id, cpc_bid:$cpc_bid")
+
+      cnt += 1
+      val currentItem = SingleRecord(
+        identifier = id,
+        cpcBid = cpc_bid
+      )
+      list += currentItem
+
+    }
+
+    val result = list.toArray[SingleRecord]
+    val adRecordList = OcpcList(
+      adrecord = result
+    )
+
+    println("length of the array")
+    println(result.length)
+    adRecordList.writeTo(new FileOutputStream(filename))
+
+    println("complete save data into protobuffer")
 
   }
 
 
-  def readExpSet(date: String, hour: String, spark: SparkSession) = {
-    val path = s"/user/cpc/wangjun/ocpc_exp/ocpc_ab.json"
+  def readExpSet(expDataPath: String, date: String, hour: String, spark: SparkSession) = {
+    val path = expDataPath
 
     val data = spark.read.format("json").json(path)
 
@@ -40,7 +86,7 @@ object OcpcABtestSimple {
       .agg(
         min(col("cpc_bid")).alias("cpc_bid")
       )
-      .select("identifier", "cpc_bid")
+      .selectExpr("cast(identifier as string) identifier", "cast(cpc_bid as double) cpc_bid")
 
     resultDF
   }
