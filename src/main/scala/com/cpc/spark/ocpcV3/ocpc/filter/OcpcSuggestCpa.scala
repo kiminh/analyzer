ea301c62eae1d9da754b54e6397449a5af5b18a8
wangjun <wangjun02@qutoutiao.net>
2019-02-25 19:26:01 +0800

    fix bug

--- src/main/scala/com/cpc/spark/ocpcV3/ocpc/filter/OcpcSuggestCpa.scala
+++ src/main/scala/com/cpc/spark/ocpcV3/ocpc/filter/OcpcSuggestCpa.scala
@@ -89,7 +89,7 @@ object OcpcSuggestCpa{
       .select("unitid", "userid", "adclass", "conversion_goal", "show", "click", "cvrcnt", "cost", "post_ctr", "acp", "acb", "jfb", "cpa", "pcvr", "post_cvr", "pcoc", "cal_bid", "auc")
       .withColumn("original_conversion", col("conversion_goal"))
       .withColumn("conversion_goal", when(col("conversion_goal") === 3, 1).otherwise(col("conversion_goal")))
-      .join(kvalue, Seq("unitid", "conversion_goal"), "left_outer")
+      .join(kvalue, Seq("unitid", "original_conversion"), "left_outer")
       .withColumn("cal_bid", col("cpa") * col("pcvr") * col("kvalue") / col("jfb"))
       .select("unitid", "userid", "adclass", "original_conversion", "conversion_goal", "show", "click", "cvrcnt", "cost", "post_ctr", "acp", "acb", "jfb", "cpa", "pcvr", "post_cvr", "pcoc", "cal_bid", "auc", "kvalue")
       .withColumn("is_recommend", when(col("auc").isNotNull && col("auc")>0.65, 1).otherwise(0))
