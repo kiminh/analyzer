a8f3f7d756f5026e315d499741333418d82cb122
xiongyao <xiongyao@qutoutiao.net>
2019-03-07 13:15:00 +0800

    installedApp to new redis

--- src/main/scala/com/cpc/spark/qukan/interest/UpdateInstallApp.scala
+++ src/main/scala/com/cpc/spark/qukan/interest/UpdateInstallApp.scala
@@ -143,55 +143,55 @@ object UpdateInstallApp {
     added.map(x => (x._1, x._2._1)).take(10).foreach(println)
 
 //    保存新增数据 redis
-    val sum = added.map(x => (x._1, x._2._1))
-      .repartition(100)
-      .mapPartitions {
-        p =>
-          var n = 0
-          var n1 = 0
-          var n2 = 0
-          var n3 = 0
-          val conf = ConfigFactory.load()
-          val redis = new RedisClient(conf.getString("redis.host"), conf.getInt("redis.port"))
-          val sec = new Date().getTime / 1000
-          p.foreach {
-            x =>
-              n += 1
-              val key = x._1 + "_UPDATA"
-              val buffer = redis.get[Array[Byte]](key).getOrElse(null)
-              var user: UserProfile.Builder = null
-              if (buffer == null) {
-                user = UserProfile.newBuilder()
-                n3 = n3 + 1
-              } else {
-                user = UserProfile.parseFrom(buffer).toBuilder
-              }
-              //判断老数据
-              if (user.getInstallpkgCount > 0) {
-                val pkg = user.getInstallpkg(0)
-                //更新时间大于一天
-                if (sec > pkg.getLastUpdateTime + 60 * 60 * 24) {
-                  user.clearInstallpkg()
-                } else {
-                  n1 += 1
-                }
-              }
-              if (user.getInstallpkgCount == 0) {
-                x._2.foreach {
-                  n =>
-                    val pkg = APPPackage.newBuilder().setPackagename(n).setLastUpdateTime(sec)
-                    user.addInstallpkg(pkg)
-                }
-                redis.setex(key, 3600 * 24 * 7, user.build().toByteArray)
-                n2 += 1
-              }
-          }
-          Seq(("pass", n1), ("update", n2), ("new", n3)).iterator
-      }
-      .reduceByKey(_ + _)
-      .take(10)
-    println("update redis")
-    sum.foreach(println)
+//    val sum = added.map(x => (x._1, x._2._1))
+//      .repartition(100)
+//      .mapPartitions {
+//        p =>
+//          var n = 0
+//          var n1 = 0
+//          var n2 = 0
+//          var n3 = 0
+//          val conf = ConfigFactory.load()
+//          val redis = new RedisClient(conf.getString("redis.host"), conf.getInt("redis.port"))
+//          val sec = new Date().getTime / 1000
+//          p.foreach {
+//            x =>
+//              n += 1
+//              val key = x._1 + "_UPDATA"
+//              val buffer = redis.get[Array[Byte]](key).getOrElse(null)
+//              var user: UserProfile.Builder = null
+//              if (buffer == null) {
+//                user = UserProfile.newBuilder()
+//                n3 = n3 + 1
+//              } else {
+//                user = UserProfile.parseFrom(buffer).toBuilder
+//              }
+//              //判断老数据
+//              if (user.getInstallpkgCount > 0) {
+//                val pkg = user.getInstallpkg(0)
+//                //更新时间大于一天
+//                if (sec > pkg.getLastUpdateTime + 60 * 60 * 24) {
+//                  user.clearInstallpkg()
+//                } else {
+//                  n1 += 1
+//                }
+//              }
+//              if (user.getInstallpkgCount == 0) {
+//                x._2.foreach {
+//                  n =>
+//                    val pkg = APPPackage.newBuilder().setPackagename(n).setLastUpdateTime(sec)
+//                    user.addInstallpkg(pkg)
+//                }
+//                redis.setex(key, 3600 * 24 * 7, user.build().toByteArray)
+//                n2 += 1
+//              }
+//          }
+//          Seq(("pass", n1), ("update", n2), ("new", n3)).iterator
+//      }
+//      .reduceByKey(_ + _)
+//      .take(10)
+//    println("update redis")
+//    sum.foreach(println)
 
     //新增数据迁移至新的redis集群
     val result = added.map(x => (x._1, x._2._1))
