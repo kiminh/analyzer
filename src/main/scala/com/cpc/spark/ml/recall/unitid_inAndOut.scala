503544febca9c12995a3b2c98c3cb1517b6f562f
段广东 <duanguangdong@qutoutiao.net>
2019-03-07 10:12:12 +0800

    dnn 年龄模型

--- src/main/scala/com/cpc/spark/ml/recall/unitid_inAndOut.scala
+++ src/main/scala/com/cpc/spark/ml/recall/unitid_inAndOut.scala
@@ -12,7 +12,6 @@ object unitid_inAndOut {
       .enableHiveSupport()
       .getOrCreate()
     val cal1 = Calendar.getInstance()
-    cal1.add(Calendar.DATE, -1)
     val today = new SimpleDateFormat("yyyy-MM-dd").format(cal1.getTime)
     cal1.add(Calendar.DATE, -1)
     val tardate = new SimpleDateFormat("yyyy-MM-dd").format(cal1.getTime)
