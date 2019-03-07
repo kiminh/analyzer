a217adb76c43b68113cbf268885f47b83f93b1d0
dongjinbao <dongjinbao@qutoutiao.net>
2019-03-07 19:25:41 +0800

    test

--- src/main/scala/com/cpc/spark/cvr/test.scala
+++ src/main/scala/com/cpc/spark/cvr/test.scala
@@ -49,7 +49,7 @@ object test {
 
         val data = spark.sql(sql).cache()
 
-        val ideaid = data.select("ideaid").rdd.map(_.getAs[String]("ideaid")).collect().toList
+        val ideaid = data.select("ideaid").rdd.map(_.getAs[String]("ideaid")).distinct().collect().toList
 
         for (i <- ideaid) {
             val data1 = data.filter(s"ideaid = $i").cache()
