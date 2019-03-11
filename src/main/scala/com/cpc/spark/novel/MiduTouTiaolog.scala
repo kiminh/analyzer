7eeccbf2d39d95f368cb3ab49da860abd7eef6e2
wangyao <wangyao@qutoutiao.net>
2019-03-11 20:49:50 +0800

    -

--- src/main/scala/com/cpc/spark/novel/MiduTouTiaolog.scala
+++ src/main/scala/com/cpc/spark/novel/MiduTouTiaolog.scala
@@ -43,7 +43,7 @@ object MiduTouTiaolog {
         data2.printSchema()
        data2.write.mode("overwrite").saveAsTable("test.wy02")
 
-        val data3 =data2.withColumn("data2",unzip2(col("data1")))
+        val data3 =data2.withColumn("data2",unzip(col("data1")))
         data3.show(1)
         data3.write.mode("overwrite").saveAsTable("test.wy03")
 //        spark.sql(sql).write.mode("overwrite").insertInto("dl_cpc.cpc_midu_toutiao_log")
