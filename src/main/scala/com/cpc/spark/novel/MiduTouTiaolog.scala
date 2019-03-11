90680e4213835b6fad24bcc6af36ad7ed4137520
wangyao <wangyao@qutoutiao.net>
2019-03-11 19:46:55 +0800

    -

--- src/main/scala/com/cpc/spark/novel/MiduTouTiaolog.scala
+++ src/main/scala/com/cpc/spark/novel/MiduTouTiaolog.scala
@@ -43,7 +43,7 @@ object MiduTouTiaolog {
         data2.printSchema()
        data2.write.mode("overwrite").saveAsTable("test.wy02")
 
-        val data3 =data2.withColumn("data2",unzip(col("data1")))
+        val data3 =data2.withColumn("data2",unzip2(col("data1")))
         data3.show(1)
         data3.write.mode("overwrite").saveAsTable("test.wy03")
 //        spark.sql(sql).write.mode("overwrite").insertInto("dl_cpc.cpc_midu_toutiao_log")
@@ -64,6 +64,15 @@ object MiduTouTiaolog {
         (x:Array[Byte])=>
         {
             val inputStream = new GZIPInputStream(new ByteArrayInputStream(x))
+            val output = scala.io.Source.fromInputStream(inputStream).mkString
+            output
+        }
+    }
+
+    def unzip2 = udf {
+        (x:String)=>
+        {
+            val inputStream = new GZIPInputStream(this.getClass.getClassLoader.getResourceAsStream(x))
             val output = scala.io.Source.fromInputStream(inputStream).mkString
             output
         }
