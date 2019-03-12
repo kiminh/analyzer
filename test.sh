cafae1434e09a911825c770a24e0560f5c0b5cd3
wangyao <wangyao@qutoutiao.net>
2019-03-12 16:51:32 +0800

    -

--- test.sh
+++ test.sh
@@ -26,4 +26,4 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --jars $( IFS=$','; echo "${jars[*]}" ) \
     --conf "spark.sql.shuffle.partitions=1000" \
     --class com.cpc.spark.novel.MiduTouTiaolog \
-    /home/cpc/wy/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar 2019-03-12 00
+    /home/cpc/wy/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $day $hour
