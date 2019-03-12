d505f08fbad50e7a41fd75eebec9019eb2b08e55
wangyao <wangyao@qutoutiao.net>
2019-03-12 10:47:19 +0800

    -

--- src/main/scala/com/cpc/spark/novel/MiduTouTiaolog.scala
+++ src/main/scala/com/cpc/spark/novel/MiduTouTiaolog.scala
@@ -23,34 +23,23 @@ object MiduTouTiaolog {
           .enableHiveSupport()
           .getOrCreate()
 
-//        val sql =
-//            s"""
-//               |select opt["data"] as data
-//               |from dl_cpc.cpc_union_trace_log
-//               |where `date`='2019-03-10' and trace_type ='inform' and opt["chan"] = "midunov"
-//             """.stripMargin
-//
-//        println(sql)
-            val sql =
-                    s"""
-                       |select 'E8xHlllllllllWaJBeYWRTBM/eYa_4+z3GR-E30TQnDinLc5FXV9rtd1STET-rDwRZdusqupZHH1aGDRMLbre7PG/VkITEqQIJb79KNagzHlBZG4fABPr1+QxvYTO_DJA3_NBmpZ181trAO4W8WkErQdC_rwlqVTcRENc1LVlN_bpK4Pbq5O-RTywZO9DTWIFN+l4ykgs95rQk8JbrTpkOrgim1Gkxgce7u-Zz1HwVQf/RV9sABHxwH99i8/2xFV1JIz9/Q2nNRXg-LDmqFB9_LEFCZvXkyr3-qyD7q/ETPb+/s07OF/hzOU2/dZXB52G8pZZSdBbKwmbhyPpMWAA5flzQiB_VeX_Ha1e9K7udWQcz_gIehBkCRGldByY09SJpi/-K4_RedVAsFM5OXG1Ii7oskIxFNplRll'
-                       | as data
-                       |from test.wy00 limit 1
-                     """.stripMargin
+        val sql =
+            s"""
+               |select opt["data"] as data
+               |from dl_cpc.cpc_union_trace_log
+               |where `date`='2019-03-12' and trace_type ='inform' and opt["chan"] = "midunov"
+             """.stripMargin
 
-      val data1 = spark.sql(sql)
-        data1.write.mode("overwrite").saveAsTable("test.wy01")
-        data1.show(1)
+        println(sql)
 
+      val data = spark.sql(sql)
 
-        val data2 =data1.withColumn("data1",decode(col("data")))
+        data.show(5)
+      val data2=data
+        .withColumn("data1",decode(col("data")))
+        .withColumn("data1",unzip(col("data1")))
         data2.show(1)
-        data2.printSchema()
-       data2.write.mode("overwrite").saveAsTable("test.wy02")
-
-        val data3 =data2.withColumn("data2",unzip1(col("data1")))
-        data3.show(1)
-        data3.write.mode("overwrite").saveAsTable("test.wy03")
+        data2.write.mode("overwrite").saveAsTable("test.wy03")
 //        spark.sql(sql).write.mode("overwrite").insertInto("dl_cpc.cpc_midu_toutiao_log")
 
     }
@@ -65,24 +54,17 @@ object MiduTouTiaolog {
         }
     }
 
-    def unzip1 = udf {
+    def unzip = udf {
         (x:Array[Byte]) =>
             if (x != null) decompress(x) else null
     }
 
-    def unzip = udf {
-        (x:String)=>
-        {
-            val inputStream = new GZIPInputStream(this.getClass.getClassLoader.getResourceAsStream(x))
-            val output = scala.io.Source.fromInputStream(inputStream).mkString
-            output
-        }
-    }
-
-    def unzip3 = udf {
-      (x:Array[Byte])=>
-      {
-      Utils.uncompressToString(x,"UTF-8")
-    }
-  }
+//    def unzip = udf {
+//        (x:String)=>
+//        {
+//            val inputStream = new GZIPInputStream(this.getClass.getClassLoader.getResourceAsStream(x))
+//            val output = scala.io.Source.fromInputStream(inputStream).mkString
+//            output
+//        }
+//    }
 }
