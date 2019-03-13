8dc6459ceb2486827588a3ca405f8b69e348a1e0
wangjun <wangjun02@qutoutiao.net>
2019-03-13 14:41:58 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/data/OcpcUnionlog.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/data/OcpcUnionlog.scala
@@ -15,15 +15,15 @@ object OcpcUnionlog {
     val data = getBaseUnionlog(date, hour, spark)
 
     data
-      .repartition(100).write.mode("overwrite").insertInto("dl_cpc.ocpc_base_unionlog")
-//      .repartition(100).write.mode("overwrite").saveAsTable("test.ocpc_base_unionlog")
+//      .repartition(100).write.mode("overwrite").insertInto("dl_cpc.ocpc_base_unionlog")
+      .repartition(100).write.mode("overwrite").saveAsTable("test.ocpc_base_unionlog")
 
     println("successfully save data into table: dl_cpc.ocpc_base_unionlog")
 
     val ocpcData = getOcpcUnionlog(data, date, hour, spark)
     ocpcData
-      .repartition(10).write.mode("overwrite").insertInto("dl_cpc.ocpc_filter_unionlog")
-//        .repartition(10).write.mode("overwrite").saveAsTable("test.ocpc_filter_unionlog")
+//      .repartition(10).write.mode("overwrite").insertInto("dl_cpc.ocpc_filter_unionlog")
+        .repartition(10).write.mode("overwrite").saveAsTable("test.ocpc_filter_unionlog")
     println("successfully save data into table: dl_cpc.ocpc_filter_unionlog")
   }
 
@@ -72,7 +72,8 @@ object OcpcUnionlog {
          |    ocpc_log_dict,
          |    exp_ctr,
          |    exp_cvr,
-         |    antispam
+         |    antispam,
+         |    conversion_goal
          |from
          |    base_data
        """.stripMargin
@@ -90,9 +91,60 @@ object OcpcUnionlog {
   }
 
   def getBaseUnionlog(date: String, hour: String, spark: SparkSession) = {
-    var selectWhere = s"(`date`='$date' and hour = '$hour')"
+    var selectWhere = s"(`day`='$date' and hour = '$hour')"
 
-    // 拿到基础数据
+//    // 旧版基础数据抽取逻辑
+//    // 拿到基础数据
+//    var sqlRequest =
+//      s"""
+//         |select
+//         |    searchid,
+//         |    timestamp,
+//         |    network,
+//         |    exptags,
+//         |    media_type,
+//         |    media_appsid,
+//         |    adslotid,
+//         |    adslot_type,
+//         |    adtype,
+//         |    adsrc,
+//         |    interaction,
+//         |    bid,
+//         |    price,
+//         |    ideaid,
+//         |    unitid,
+//         |    planid,
+//         |    country,
+//         |    province,
+//         |    city,
+//         |    uid,
+//         |    ua,
+//         |    os,
+//         |    sex,
+//         |    age,
+//         |    isshow,
+//         |    isclick,
+//         |    duration,
+//         |    userid,
+//         |    ext_int['is_ocpc'] as is_ocpc,
+//         |    ext_string['ocpc_log'] as ocpc_log,
+//         |    ext_string['user_city'] as user_city,
+//         |    ext['city_level'].int_value as city_level,
+//         |    ext['adclass'].int_value as adclass,
+//         |    cast(ext['exp_ctr'].int_value * 1.0 / 1000000 as double) as exp_ctr,
+//         |    cast(ext['exp_cvr'].int_value * 1.0 / 1000000 as double) as exp_cvr,
+//         |    cast(ext["charge_type"].int_value as int) as charge_type,
+//         |    ext['antispam'].int_value as antispam,
+//         |    cast(ext['usertype'].int_value as bigint) as usertype
+//         |from dl_cpc.cpc_union_log
+//         |where $selectWhere
+//         |and (isshow>0 or isclick>0)
+//      """.stripMargin
+//    println(sqlRequest)
+//    val rawData = spark
+//      .sql(sqlRequest)
+
+    // 新版基础数据抽取逻辑
     var sqlRequest =
       s"""
          |select
@@ -102,7 +154,7 @@ object OcpcUnionlog {
          |    exptags,
          |    media_type,
          |    media_appsid,
-         |    adslotid,
+         |    adslot_id as adslotid,
          |    adslot_type,
          |    adtype,
          |    adsrc,
@@ -122,21 +174,23 @@ object OcpcUnionlog {
          |    age,
          |    isshow,
          |    isclick,
-         |    duration,
+         |    0 as duration,
          |    userid,
-         |    ext_int['is_ocpc'] as is_ocpc,
-         |    ext_string['ocpc_log'] as ocpc_log,
-         |    ext_string['user_city'] as user_city,
-         |    ext['city_level'].int_value as city_level,
-         |    ext['adclass'].int_value as adclass,
-         |    cast(ext['exp_ctr'].int_value * 1.0 / 1000000 as double) as exp_ctr,
-         |    cast(ext['exp_cvr'].int_value * 1.0 / 1000000 as double) as exp_cvr,
-         |    cast(ext["charge_type"].int_value as int) as charge_type,
-         |    ext['antispam'].int_value as antispam,
-         |    cast(ext['usertype'].int_value as bigint) as usertype
-         |from dl_cpc.cpc_union_log
+         |    cast(is_ocpc as int) as is_ocpc,
+         |    ocpc_log,
+         |    user_city,
+         |    city_level,
+         |    adclass,
+         |    cast(exp_ctr * 1.0 / 1000000 as double) as exp_ctr,
+         |    cast(exp_cvr * 1.0 / 1000000 as double) as exp_cvr,
+         |    charge_type,
+         |    (case when antispam_score=10000 then 0 else 1 end) as antispam,
+         |    usertype,
+         |    conversion_goal
+         |from dl_cpc.cpc_basedata_union_events
          |where $selectWhere
          |and (isshow>0 or isclick>0)
+         |and adslot_type != 7
       """.stripMargin
     println(sqlRequest)
     val rawData = spark
