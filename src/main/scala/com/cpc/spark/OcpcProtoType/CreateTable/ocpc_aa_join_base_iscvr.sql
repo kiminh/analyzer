a900e89f7f60ef1d031d2bbeae6ff0ffef10308c
wentao <wentao@qutoutiao.net>
2019-03-13 11:24:03 +0800

    ocpc ratio in market

--- src/main/scala/com/cpc/spark/OcpcProtoType/CreateTable/ocpc_aa_join_base_iscvr.sql
+++ src/main/scala/com/cpc/spark/OcpcProtoType/CreateTable/ocpc_aa_join_base_iscvr.sql
@@ -7,7 +7,6 @@ create table if not exists dl_cpc.ocpc_aa_join_base_iscvr(
     price               int,
     uid                 string,
     bid                 int,
-    is_ocpc             int,
     iscvr1              int,
     iscvr2              int,
     iscvr3              int,
