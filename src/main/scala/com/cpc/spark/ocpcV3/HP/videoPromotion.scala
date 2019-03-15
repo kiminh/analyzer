41a721d9dad3e0290b845c8b266f6c16c130bb37
sunjianqiang <sunjianqiang@qutoutiao.net>
2019-03-15 17:35:53 +0800

    change

--- src/main/scala/com/cpc/spark/ocpcV3/HP/videoPromotion.scala
+++ src/main/scala/com/cpc/spark/ocpcV3/HP/videoPromotion.scala
@@ -53,9 +53,19 @@ object videoPromotion {
          |      --and hour in ('11', '12')
          |      and adsrc = 1
          |      and isclick = 1
+         |      and isshow = 1
          |      and media_appsid in ("80000001")
          |      and adtype in (2, 8, 10) --and    userid>0
          |      and usertype in (0, 1, 2)
+         |      and adslot_type = 1
+         |
+         |        and ideaid > 0
+         |        and  interaction=2
+         |        and userid > 0
+         |        and uid not like "%.%"
+         |        and uid not like "%000000%"
+         |        and length(uid) in (14, 15, 36)
+         |
          |  ) t1
          |  left join (
          |    select
