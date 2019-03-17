53014e595e2c472e6e769353a839a52b1fd12b3f
wangjun <wangjun02@qutoutiao.net>
2019-03-17 15:56:08 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/suggest_cpa_qtt/OcpcSuggestCPA.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/suggest_cpa_qtt/OcpcSuggestCPA.scala
@@ -225,6 +225,8 @@ object OcpcSuggestCPA {
          |FROM
          |  dl_cpc.ocpc_unitid_auc_daily
          |WHERE
+         |  `date` = '$date'
+         |AND
          |  version = '$version'
          |AND
          |  conversion_goal = $conversionGoal
