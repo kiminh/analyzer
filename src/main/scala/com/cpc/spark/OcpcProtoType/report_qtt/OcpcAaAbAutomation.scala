3044f03c11dd57ebadcff220dca24d2cedaa9475
wentao <wentao@qutoutiao.net>
2019-03-18 13:50:24 +0800

    aa ab automation

--- src/main/scala/com/cpc/spark/OcpcProtoType/report_qtt/OcpcAaAbAutomation.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/report_qtt/OcpcAaAbAutomation.scala
@@ -1,19 +1,58 @@
 package com.cpc.spark.OcpcProtoType.report_qtt
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
 
 object OcpcAaAbAutomation {
 
   def main(args: Array[String]): Unit = {
-
+    val date = args(0).toString
+    val spark = SparkSession.builder().appName("OcpcAaAbAutomation").enableHiveSupport().getOrCreate()
+    val abTestDataDF = getAbTestData(date, spark)
+    compareAbTestData(abTestDataDF)
   }
 
-  // 抽取Ab实验结果
-  def getAbTestData(date: String, spark: SparkSession): Unit ={
+  // 抽取Ab实验数据
+  def getAbTestData(date: String, spark: SparkSession): DataFrame ={
     val sql =
       s"""
-        |
+        |select
+        |	dt,
+        |	unitid,
+        |	userid,
+        |	ab_group,
+        |	acp,
+        |	acb,
+        |	acb_max,
+        |	cpm,
+        |	cpagiven,
+        |	cpareal,
+        |	pre_cvr,
+        |	post_cvr,
+        |	kvalue,
+        |	cost,
+        |	show,
+        |	click,
+        |	cv
+        |from
+        |	dl_cpc.ocpc_ab_test_data
+        |where
+        |	`date` = '$date'
+        |and
+        |	dt = '$date'
+        |and
+        |	tag = 'yesterday'
+        |order by
+        |	unitid, userid, ab_group
       """.stripMargin
+    val abTestDataDF = spark.sql(sql)
+    abTestDataDF
+  }
+
+  // 得到ab实验数据比较结果
+  def compareAbTestData(abTestDataDF: DataFrame): Unit ={
+    val abTestData = abTestDataDF.collectAsList()
+    println(abTestData)
   }
 
 }
