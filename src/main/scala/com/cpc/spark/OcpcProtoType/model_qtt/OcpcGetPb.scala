334401c400d95d8252185bbdb28e3f696cc927c6
wangjun <wangjun02@qutoutiao.net>
2019-03-20 19:03:43 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/model_qtt/OcpcGetPb.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/model_qtt/OcpcGetPb.scala
@@ -239,11 +239,11 @@ object OcpcGetPb {
       .join(kRegion, Seq("identifier"), "left_outer")
       .withColumn("kvalue", when(col("flag") === 1 && col("kvalue") < col("bottom_k"), col("bottom_k")).otherwise(when(col("flag") === 1 && col("kvalue") > col("top_k"), col("top_k")).otherwise(col("kvalue"))))
 
-//    result
-//        .withColumn("date", lit(date))
-//        .withColumn("hour", lit(hour))
-//        .withColumn("version", lit(version))
-//        .repartition(10).write.mode("overwrite").insertInto("dl_cpc.ocpc_check_smooth_k")
+    result
+        .withColumn("date", lit(date))
+        .withColumn("hour", lit(hour))
+        .withColumn("version", lit(version))
+        .repartition(10).write.mode("overwrite").insertInto("dl_cpc.ocpc_check_smooth_k")
 
     println("k smooth strat1:")
     result.show(10)
