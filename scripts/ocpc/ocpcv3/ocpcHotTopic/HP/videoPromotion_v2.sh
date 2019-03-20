0a97a7966cdb08d39a9815cd2f76a35e80972814
sunjianqiang <sunjianqiang@qutoutiao.net>
2019-03-20 20:03:55 +0800

    change

--- scripts/ocpc/ocpcv3/ocpcHotTopic/HP/videoPromotion_v2.sh
+++ scripts/ocpc/ocpcv3/ocpcHotTopic/HP/videoPromotion_v2.sh
@@ -24,6 +24,6 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.yarn.executor.memoryOverhead=4g'\
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
-    --class com.cpc.spark.ocpcV3.HP.videoPromotion \
+    --class com.cpc.spark.ocpcV3.HP.videoPromotion_v2 \
     /home/cpc/sunjianqiang/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $date $hour $hr
 
