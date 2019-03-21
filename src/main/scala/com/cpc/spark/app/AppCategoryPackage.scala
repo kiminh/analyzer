dcdfd72d3b2b2cc699ce162db11cde29bda79f95
langjingxiang <langjingxiang@qutoutiao.net>
2019-03-21 12:07:30 +0800

    抓包hive

--- src/main/scala/com/cpc/spark/app/AppCategoryPackage.scala
+++ src/main/scala/com/cpc/spark/app/AppCategoryPackage.scala
@@ -32,13 +32,11 @@ object AppCategoryPackage {
   }
 
   def readAppList() = {
-    val file = Source.fromFile(AppCategoryPackage.getClass.getResource("/src/main/resources/appList").getFile)
-
-    val list = new mutable.ArrayBuffer[String]()
-    for (line <- file.getLines()) {
-      list.append(line)
-    }
-    list
+    val listapp = List("休闲游戏", "宝石消除", "动作射击", "儿童益智", "体育格斗", "经营策略", "跑酷竞速", "网络游戏", "扑克棋牌",
+      "塔防守卫", "角色扮演", "波动游戏", "捕鱼游戏", "得力游戏", "模拟人生", "通信聊天", "美化手机", "生活服务", "常用工具",
+      "新闻资讯", "金融理财", "育儿母婴", "影音图像", "网上购物", "阅读学习", "旅游出行", "性能优化", "社交网络", "办公软件",
+      "资讯qtt", "娱乐消遣", "彩票APP", "短视频", "网贷")
+    listapp
   }
 
   case class AppCategory(name: String, pak: String)
