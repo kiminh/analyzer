27a49683ce9e79d3716a01ffd144d55fc2c20ee6
sunjianqiang <sunjianqiang@qutoutiao.net>
2019-03-21 13:56:51 +0800

    add lab

--- scripts/ocpc/ocpcv3/ocpcHotTopic/HP/Lab/lab.sh
+++ scripts/ocpc/ocpcv3/ocpcHotTopic/HP/Lab/lab.sh
@@ -20,5 +20,5 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.yarn.executor.memoryOverhead=4g'\
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
-    --class com.cpc.spark.ocpcV3.HP.lab \
+    --class com.cpc.spark.ocpcV3.HP.Lab \
     /home/cpc/sunjianqiang/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar
