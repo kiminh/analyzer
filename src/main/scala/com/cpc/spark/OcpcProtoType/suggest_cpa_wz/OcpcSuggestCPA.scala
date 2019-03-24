35b839b82b9f5a7f8c4ce69324399f95d4054193
wangjun <wangjun02@qutoutiao.net>
2019-03-24 15:19:24 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/suggest_cpa_wz/OcpcSuggestCPA.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/suggest_cpa_wz/OcpcSuggestCPA.scala
@@ -68,9 +68,9 @@ object OcpcSuggestCPA {
       .withColumn("hour", lit(hour))
       .withColumn("version", lit(version))
 
-    resultDF.write.mode("overwrite").saveAsTable("test.check_suggest_data20190311")
-//    resultDF
-//      .repartition(10).write.mode("overwrite").insertInto("dl_cpc.ocpc_suggest_cpa_recommend_hourly_v2")
+//    resultDF.write.mode("overwrite").saveAsTable("test.check_suggest_data20190311")
+    resultDF
+      .repartition(10).write.mode("overwrite").insertInto("dl_cpc.ocpc_suggest_cpa_recommend_hourly_v2")
     println("successfully save data into table: dl_cpc.ocpc_suggest_cpa_recommend_hourly_v2")
   }
 
