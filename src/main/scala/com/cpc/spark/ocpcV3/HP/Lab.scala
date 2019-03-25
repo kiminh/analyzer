85428b5d0162dcbc0820f4623b25c7fb6617eaf3
sunjianqiang <sunjianqiang@qutoutiao.net>
2019-03-25 21:42:25 +0800

    change

--- src/main/scala/com/cpc/spark/ocpcV3/HP/Lab.scala
+++ src/main/scala/com/cpc/spark/ocpcV3/HP/Lab.scala
@@ -9,6 +9,12 @@ object Lab {
     val date = args(0).toString
     import spark.implicits._
 
+    val app_cat = spark.read.csv("/home/cpc/sunjianqiang/app_cat.csv")
+    app_cat.show(10)
+
+
+
+
     val sql1 =
       s"""
          |select
@@ -23,7 +29,7 @@ object Lab {
     val appFreq = pkgs.rdd
       .map(x =>  x.getAs[String]("pkgs1") )
       .flatMap(x => x.split(","))
-      .map(x => (x,1)).reduceByKey((x, y) => x+y).map(x => (x._1.split("-"), x._2)).map( x => if(x._1.length > 1) AppCount2(x._1(0), x._1(1), x._2) else AppCount2(x._1(0), "" , x._2)).toDF()
+      .map(x => (x,1)).reduceByKey((x, y) => x+y).map(x => (x._1.split("-"), x._2)).map( x => if(x._1.length > 1) AppCount(x._1(0), x._1(1), x._2) else AppCount(x._1(0), "" , x._2)).toDF()
 
     appFreq.show(10)
 
@@ -32,6 +38,7 @@ object Lab {
 //    appFreq.orderBy( appFreq("count").desc ).show(50, false)
 
   }
-  case class AppCount(  var appName: String, var count: Int)
-  case class AppCount2( var appName1: String, appName2: String, var count: Int)
+
+//  case class AppCat( var appName: String, )
+  case class AppCount( var appName1: String, appName2: String, var count: Int)
 }
