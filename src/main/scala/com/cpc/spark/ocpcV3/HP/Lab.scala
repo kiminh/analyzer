887bb58a35053a0d86d615114ab29beba7920ca0
sunjianqiang <sunjianqiang@qutoutiao.net>
2019-03-25 20:57:46 +0800

    change

--- src/main/scala/com/cpc/spark/ocpcV3/HP/Lab.scala
+++ src/main/scala/com/cpc/spark/ocpcV3/HP/Lab.scala
@@ -25,6 +25,8 @@ object Lab {
       .flatMap(x => x.split(","))
       .map(x => (x,1)).reduceByKey((x, y) => x+y).map(x => (x._1.split("-"), x._2)).map( x => AppCount2(x._1(0), x._1(1), x._2)).toDF()
 
+    appFreq.show(10)
+
     appFreq.write.mode("overwrite").saveAsTable("test.appInstalledCount_sjq")
 
 //    appFreq.orderBy( appFreq("count").desc ).show(50, false)
