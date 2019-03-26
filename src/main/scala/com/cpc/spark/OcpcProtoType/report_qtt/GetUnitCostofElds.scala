5203b9542b19709f60d88cdf9ac58219a3719bea
wentao <wentao@qutoutiao.net>
2019-03-26 08:29:57 +0800

    unit cost of elds

---
+++ src/main/scala/com/cpc/spark/OcpcProtoType/report_qtt/GetUnitCostofElds.scala
@@ -0,0 +1,62 @@
+package com.cpc.spark.OcpcProtoType.report_qtt
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+import scala.collection.mutable.ListBuffer
+
+object GetUnitCostofElds {
+  def main(args: Array[String]): Unit = {
+    val dateBuffer = getDate("2019-03-01", "2019-03-25")
+    for(date <- dateBuffer) println(date)
+  }
+
+  def getDate(startDate: String, endDate: String): ListBuffer[String] = {
+    val sdf = new SimpleDateFormat("yyyy-MM-dd")
+    var dateBuffer = new ListBuffer[String]
+    val startTimeStamp = sdf.parse(startDate).getTime
+    val endTimeStamp = sdf.parse(endDate).getTime
+    val n = ((endTimeStamp - startTimeStamp) / (24 * 60 * 60 * 1000)).toInt
+    for(i <- 0 to n){
+      val tempDate = sdf.format(new Date(startTimeStamp + i * 24 * 60 * 60 * 1000))
+      dateBuffer.append(tempDate.toString)
+    }
+    dateBuffer
+  }
+
+  def getUnitCost(dateBuffer: String, spark: SparkSession): Unit ={
+    for(date <- dateBuffer){
+      val sql =
+        s"""
+          |select
+          |    unitid,
+          |    sum(case when isclick = 1 then price else 0 end) * 0.01 as cost
+          |from
+          |    dl_cpc.ocpc_base_unionlog
+          |where
+          |    `date` = '$date'
+          |and
+          |    (cast(adclass as string) like "134%" or cast(adclass as string) like "107%")
+          |and
+          |    media_appsid  in ("80000001", "80000002")
+          |and
+          |    isshow = 1
+          |and
+          |    antispam = 0
+          |and
+          |    adsrc = 1
+          |group by
+          |    unitid
+        """.stripMargin
+      val dataDF = spark.sql(sql)
+      dataDF
+        .withColumn("date", lit(date))
+        .repartition(50)
+        .write.mode("overwrite").insertInto("test.wt_unit_cost_every_day_elds")
+      println(date)
+    }
+  }
+}
