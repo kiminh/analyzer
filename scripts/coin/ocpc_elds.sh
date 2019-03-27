8417f35c7fc9ef7e724fca9ab05d9997aa660c12
liuyulin <liuyulin@qutoutiao.net>
2019-03-27 22:24:47 +0800

    modify

--- scripts/coin/ocpc_elds.sh
+++ scripts/coin/ocpc_elds.sh
@@ -24,5 +24,5 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.yarn.executor.memoryOverhead=4g'\
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
-    --class com.cpc.spark.hottopic.ocpc_elds \
+    --class com.cpc.spark.coin.ocpc_elds \
     $indin $date
