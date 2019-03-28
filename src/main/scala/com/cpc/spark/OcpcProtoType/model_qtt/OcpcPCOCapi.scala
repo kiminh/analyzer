1085afe41bdd75d62b27afbfc83f6605985d1760
wangjun <wangjun02@qutoutiao.net>
2019-03-29 01:06:07 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/model_qtt/OcpcPCOCapi.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/model_qtt/OcpcPCOCapi.scala
@@ -31,14 +31,21 @@ object OcpcPCOCapi {
     println("parameters:")
     println(s"date=$date, hour=$hour, hourInt=$hourInt, version=$version, media=$media")
 
+    // 抽取基础数据表
+    val baseData = getBaseData(media, hourInt, date, hour, spark)
+
     // 抽取click的基础数据
-    val clickData = getClickData(media, hourInt, date, hour, spark)
+    val clickData = getClickData(baseData, date, hour, spark)
+    val jfbData = clickData.select("untitid", "jfb")
 
     // 从ml_cvr_feature_v2表中抽取iscvr的转化数据
     val cvData = getCvData(media, hourInt, date, hour, spark)
 
+    // cvr平滑后的pcoc
+    val pcocData = calculatePCOC(baseData, clickData, cvData, spark)
+
     // 计算pcoc与jfb
-    val result = calculateK(clickData, cvData, spark)
+    val result = calculateK(jfbData, pcocData, spark)
 
     val resultDF = result
       .select("identifier", "kvalue")
@@ -48,21 +55,105 @@ object OcpcPCOCapi {
       .withColumn("version", lit(version))
       .withColumn("method", lit("api_pcoc"))
 
-//    resultDF.write.mode("overwrite").saveAsTable("test.ocpc_k_api_pcoc_hourly")
-    resultDF
-      .repartition(10).write.mode("overwrite").insertInto("dl_cpc.ocpc_k_model_hourly")
+    resultDF.write.mode("overwrite").saveAsTable("test.ocpc_k_api_pcoc_hourly")
+//    resultDF
+//      .repartition(10).write.mode("overwrite").insertInto("dl_cpc.ocpc_k_model_hourly")
 
 
   }
 
-  def calculateK(clickData: DataFrame, cvData: DataFrame, spark: SparkSession) = {
-    val data = clickData
-        .join(cvData, Seq("unitid"), "outer")
-        .withColumn("jfb", col("total_price") * 1.0 / col("total_bid"))
-        .select("unitid", "total_price", "click", "total_bid", "total_pcvr", "jfb", "cv")
-        .withColumn("post_cvr", col("cv") * 1.0 / col("click"))
-        .withColumn("pre_cvr", col("total_pcvr") * 1.0 / col("click"))
-        .withColumn("pcoc", col("pre_cvr") / col("post_cvr"))
+  def calculatePCOC(baseData: DataFrame, clickData: DataFrame, cvData: DataFrame, spark: SparkSession) = {
+    val cvrData = clickData
+      .join(cvData, Seq("unitid"), "left_outer")
+      .withColumn("post_cvr", col("cv") * 1.0 / col("click"))
+      .select("unitid", "post_cvr")
+
+    val pcvrData = baseData
+      .join(cvrData, Seq("unitid"), "left_outer")
+      .withColumn("pre_cvr", col("exp_cvr") * 0.5 + col("post_cvr") * 0.5)
+      .filter(s"isclick = 1")
+      .select("searchid", "unitid", "pre_cvr")
+      .groupBy("unitid")
+      .agg(avg(col("pre_cvr")).alias("pre_cvr"))
+      .select("unitid", "pre_cvr")
+
+    val pcocData = pcvrData
+      .join(cvrData, Seq("unitid"), "left_outer")
+      .withColumn("pcoc", col("pre_cvr") * 1.0 / col("post_cvr"))
+
+    pcocData
+  }
+
+  def getBaseData(media: String, hourInt: Int, date: String, hour: String, spark: SparkSession) = {
+    // 抽取基础数据
+    // 媒体选择
+    val conf = ConfigFactory.load("ocpc")
+    val conf_key = "medias." + media + ".media_selection"
+    val mediaSelection = conf.getString(conf_key)
+
+    // 取历史数据
+    val dateConverter = new SimpleDateFormat("yyyy-MM-dd HH")
+    val newDate = date + " " + hour
+    val today = dateConverter.parse(newDate)
+    val calendar = Calendar.getInstance
+    calendar.setTime(today)
+    calendar.add(Calendar.HOUR, -hourInt)
+    val yesterday = calendar.getTime
+    val tmpDate = dateConverter.format(yesterday)
+    val tmpDateValue = tmpDate.split(" ")
+    val date1 = tmpDateValue(0)
+    val hour1 = tmpDateValue(1)
+    val selectCondition = getTimeRangeSql2(date1, hour1, date, hour)
+
+    val sqlRequest =
+      s"""
+         |SELECT
+         |  searchid,
+         |  unitid,
+         |  isshow,
+         |  isclick,
+         |  bid as original_bid,
+         |  price,
+         |  exp_cvr,
+         |  ocpc_log
+         |FROM
+         |  dl_cpc.ocpc_base_unionlog
+         |WHERE
+         |  $selectCondition
+         |AND
+         |  $mediaSelection
+       """.stripMargin
+    println(sqlRequest)
+    val base = spark
+      .sql(sqlRequest)
+      .withColumn("ocpc_log_dict", udfStringToMap()(col("ocpc_log")))
+
+    base.createOrReplaceTempView("base_table")
+    val sqlRequestBase =
+      s"""
+         |select
+         |    searchid,
+         |    unitid,
+         |    price,
+         |    original_bid,
+         |    cast(exp_cvr as double) as exp_cvr,
+         |    isclick,
+         |    isshow,
+         |    ocpc_log,
+         |    ocpc_log_dict,
+         |    (case when length(ocpc_log)>0 then cast(ocpc_log_dict['dynamicbid'] as int) else original_bid end) as bid
+         |from base_table
+       """.stripMargin
+    println(sqlRequestBase)
+    val rawData = spark.sql(sqlRequestBase)
+
+    val resultDF = rawData.select("searchid", "unitid", "price", "exp_cvr", "isclick", "isshow", "bid")
+    resultDF
+  }
+
+  def calculateK(jfbData: DataFrame, pcocData: DataFrame, spark: SparkSession) = {
+    val data = jfbData
+        .join(pcocData, Seq("unitid"), "outer")
 
     data.show(10)
     data.write.mode("overwrite").saveAsTable("test.check_api_unitid_pcoc201909328")
@@ -129,72 +220,12 @@ object OcpcPCOCapi {
     resultDF
   }
 
-  def getClickData(media: String, hourInt: Int, date: String, hour: String, spark: SparkSession) = {
+  def getClickData(baseData: DataFrame, date: String, hour: String, spark: SparkSession) = {
     /*
     抽取click基础数据：isclick, exp_cvr, price, bid
      */
-    // 媒体选择
-    val conf = ConfigFactory.load("ocpc")
-    val conf_key = "medias." + media + ".media_selection"
-    val mediaSelection = conf.getString(conf_key)
 
-    // 取历史数据
-    val dateConverter = new SimpleDateFormat("yyyy-MM-dd HH")
-    val newDate = date + " " + hour
-    val today = dateConverter.parse(newDate)
-    val calendar = Calendar.getInstance
-    calendar.setTime(today)
-    calendar.add(Calendar.HOUR, -hourInt)
-    val yesterday = calendar.getTime
-    val tmpDate = dateConverter.format(yesterday)
-    val tmpDateValue = tmpDate.split(" ")
-    val date1 = tmpDateValue(0)
-    val hour1 = tmpDateValue(1)
-    val selectCondition = getTimeRangeSql2(date1, hour1, date, hour)
-
-    val sqlRequest =
-      s"""
-         |SELECT
-         |  searchid,
-         |  unitid,
-         |  isshow,
-         |  isclick,
-         |  bid as original_bid,
-         |  price,
-         |  exp_cvr,
-         |  ocpc_log
-         |FROM
-         |  dl_cpc.ocpc_base_unionlog
-         |WHERE
-         |  $selectCondition
-         |AND
-         |  $mediaSelection
-       """.stripMargin
-    println(sqlRequest)
-    val base = spark
-      .sql(sqlRequest)
-      .withColumn("ocpc_log_dict", udfStringToMap()(col("ocpc_log")))
-
-    base.createOrReplaceTempView("base_table")
-    val sqlRequestBase =
-      s"""
-         |select
-         |    searchid,
-         |    unitid,
-         |    price,
-         |    original_bid,
-         |    cast(exp_cvr as double) as exp_cvr,
-         |    isclick,
-         |    isshow,
-         |    ocpc_log,
-         |    ocpc_log_dict,
-         |    (case when length(ocpc_log)>0 then cast(ocpc_log_dict['dynamicbid'] as int) else original_bid end) as bid
-         |from base_table
-       """.stripMargin
-    println(sqlRequestBase)
-    val rawData = spark.sql(sqlRequestBase)
-    rawData.createOrReplaceTempView("raw_table")
-
+    baseData.createOrReplaceTempView("raw_table")
     // 展现数、点击数、花费
     val sqlRequest1 =
       s"""
@@ -213,6 +244,7 @@ object OcpcPCOCapi {
 
     val resultDF = data
       .select("unitid", "total_price", "click", "total_bid", "total_pcvr")
+      .withColumn("jfb", col("total_price") * 1.0 / col("total_bid"))
     resultDF.show(10)
 
     resultDF
