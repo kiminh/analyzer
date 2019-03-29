9addb628ac686d374de363bc65dd06d9dec7b1d4
wangjun <wangjun02@qutoutiao.net>
2019-03-30 00:53:14 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/model_qtt/OcpcGetPb.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/model_qtt/OcpcGetPb.scala
@@ -284,7 +284,7 @@ object OcpcGetPb {
          |  identifier,
          |  regression_k as k1,
          |  api_pcoc_k as k2,
-         |  (case when identifier in ('270') and api_pcoc_k is not null then api_pcoc_k
+         |  (case when identifier in ('1888967') and api_pcoc_k is not null then api_pcoc_k
          |        else regression_k end) as regression_k
          |FROM
          |  middle_table
