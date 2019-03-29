642ae0ac555528f4f7320fee01c060c987a08c1e
wangjun <wangjun02@qutoutiao.net>
2019-03-29 16:07:31 +0800

    fix bug

---
+++ src/main/scala/com/cpc/spark/OcpcProtoType/model_qtt/OcpcSmoothFactor.scala
@@ -0,0 +1,104 @@
+package com.cpc.spark.OcpcProtoType.model_qtt
+
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+import com.cpc.spark.common.Utils.getTimeRangeSql
+import com.cpc.spark.ocpc.OcpcUtils.{getTimeRangeSql2, getTimeRangeSql3}
+import com.typesafe.config.ConfigFactory
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import ocpc.ocpc.{OcpcList, SingleRecord}
+
+import scala.collection.mutable.ListBuffer
+import org.apache.log4j.{Level, Logger}
+
+
+object OcpcSmoothFactor{
+  def main(args: Array[String]): Unit = {
+    /*
+    动态计算alpha平滑系数
+    1. 基于原始pcoc，计算预测cvr的量纲系数
+    2. 二分搜索查找到合适的平滑系数
+     */
+    val spark = SparkSession.builder().enableHiveSupport().getOrCreate()
+    Logger.getRootLogger.setLevel(Level.WARN)
+
+    val date = args(0).toString
+    val hour = args(1).toString
+    val media = args(2).toString
+    val hourInt = args(3).toInt
+    val cvrType = args(4).toString
+    println("parameters:")
+    println(s"date=$date, hour=$hour, media:$media, hourInt:$hourInt, cvrType:$cvrType")
+
+    val baseData = getBaseData(media, cvrType, hourInt, date, hour, spark)
+
+  }
+
+  def getBaseData(media: String, cvrType: String, hourInt: Int, date: String, hour: String, spark: SparkSession) = {
+    // 抽取媒体类型
+    val conf = ConfigFactory.load("ocpc")
+    val conf_key1 = "medias." + media + ".media_selection"
+    val mediaSelection = conf.getString(conf_key1)
+
+    // 时间分区
+    val dateConverter = new SimpleDateFormat("yyyy-MM-dd HH")
+    val newDate = date + " " + hour
+    val today = dateConverter.parse(newDate)
+    val calendar = Calendar.getInstance
+    calendar.setTime(today)
+    calendar.add(Calendar.HOUR, -hourInt)
+    val yesterday = calendar.getTime
+    val tmpDate = dateConverter.format(yesterday)
+    val tmpDateValue = tmpDate.split(" ")
+    val date1 = tmpDateValue(0)
+    val hour1 = tmpDateValue(1)
+    val selectCondition = getTimeRangeSql2(date1, hour1, date, hour)
+
+    // 抽取click数据
+    val sqlRequest1 =
+      s"""
+         |SELECT
+         |  searchid,
+         |  unitid,
+         |  isclick,
+         |  exp_cvr
+         |FROM
+         |  dl_cpc.ocpc_base_unionlog
+         |WHERE
+         |  $selectCondition
+         |AND
+         |  $mediaSelection
+         |AND
+         |  isclick = 1
+       """.stripMargin
+    println(sqlRequest1)
+    val clickData = spark.sql(sqlRequest1)
+
+    // 抽取cv数据
+    val sqlRequest2 =
+      s"""
+         |SELECT
+         |  searchid,
+         |  label as iscvr
+         |FROM
+         |  dl_cpc.ocpc_label_cvr_hourly
+         |WHERE
+         |  `date` >= '$date1'
+       """.stripMargin
+    println(sqlRequest2)
+    val cvData = spark.sql(sqlRequest2)
+
+
+    // 数据关联
+    val resultDF = clickData
+      .join(cvData, Seq("searchid"), "left_outer")
+      .select("searchid", "unitid", "isclick", "exp_cvr", "iscvr")
+
+    resultDF
+  }
+
+
+}
