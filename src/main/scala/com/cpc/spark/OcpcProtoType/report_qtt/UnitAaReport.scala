41f544f39c4dfe3d5c75a3bc1df0a21a4eef9590
wentao <wentao@qutoutiao.net>
2019-03-30 13:32:18 +0800

    unit aa report

--- src/main/scala/com/cpc/spark/OcpcProtoType/report_qtt/UnitAaReport.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/report_qtt/UnitAaReport.scala
@@ -1,19 +1,25 @@
 package com.cpc.spark.OcpcProtoType.report_qtt
 
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
 import com.cpc.spark.udfs.Udfs_wj.udfStringToMap
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
 
 object UnitAaReport {
   def main(args: Array[String]): Unit = {
+    val date = args(0).toString
+    val hour = args(1).toString
     val spark = SparkSession.builder().appName("UnitAaReport").enableHiveSupport().getOrCreate()
-    val df = getBaseData("2019-03-27", "-1", spark)
-    df.createOrReplaceTempView("temp_table")
-    val sql = "select unitid, (case when length(ocpc_log) > 0 then ocpc_log_dict['cpagiven'] else -1 end) from temp_table limit 10"
-    val list = spark.sql(sql).collectAsList()
-    println("-----------")
-    println(list)
+    val baseDataDF = getBaseData(date, hour, spark)
+    println("-----has got base data from base unionlog-----")
+    val suggestCpaDF = getSuggestCpa(date, hour, spark)
+    println("-----has got suggest cpa-----")
+    val aucDF = getAuc(date, hour, spark)
+    println("-----has got auc-----")
+    calculateIndexValue(date, hour, baseDataDF, suggestCpaDF, aucDF, spark)
+    println("-----successful------")
   }
 
   // 首先从base表里获取基础字段
@@ -51,9 +57,10 @@ object UnitAaReport {
         |    `date` = '$date'
       """.stripMargin
     // 当hour等于-1时，表示取全天的数据
-    if("-1".equals(hour.toString) == false) sql +=  s" and hour = '$hour'"
+    if("all".equals(hour) == false) sql +=  s" and hour = '$hour'"
     val dataDF = spark.sql(sql)
-    dataDF.withColumn("ocpc_log_dict", udfStringToMap()(col("ocpc_log")))
+    val baseDataDF = dataDF.withColumn("ocpc_log_dict", udfStringToMap()(col("ocpc_log")))
+    baseDataDF
   }
 
   // 获取suggest_cpa
@@ -72,9 +79,8 @@ object UnitAaReport {
          |and
          |   `date` = '$date'
        """.stripMargin
-
     // 判断当前是否是查询全天的数据
-    if("-1".equals(hour.toString) == false) subQuery +=  s" and hour = '$hour'"
+    if("all".equals(hour) == false) subQuery +=  s" and hour = '$hour'"
 
     val sql =
       s"""
@@ -88,8 +94,8 @@ object UnitAaReport {
         |    a.row_num = 1
       """.stripMargin
 
-    val dataDF = spark.sql(sql)
-    dataDF
+    val suggestCpaDF = spark.sql(sql)
+    suggestCpaDF
    }
 
   // 获取auc
@@ -135,8 +141,224 @@ object UnitAaReport {
         |and
         |    (a.charge_type is null or a.charge_type = 1)
       """.stripMargin
-    if("-1".equals(hour.toString) == false) sql1 += s" and hour = '$hour'"
+    if("all".equals(hour) == false) sql1 += s" and hour = '$hour'"
     val dataFrame1 = spark.sql(sql1)
-    dataFrame1
+
+    var sql2 =
+      s"""
+        |select
+        |    a.unitid,
+        |    a.userid,
+        |    2 as conversion_goal,
+        |    a.exp_cvr,
+        |    c.iscvr2 as iscvr
+        |from
+        |    dl_cpc.ocpc_base_unionlog a
+        |left join
+        |    (select
+        |        searchid,
+        |        label as iscvr2
+        |    from
+        |        dl_cpc.ml_cvr_feature_v2
+        |    where
+        |        `date` = '$date'
+        |    and
+        |        label = 1
+        |    ) as c
+        |on
+        |    a.searchid = c.searchid
+        |where
+        |    `date` = '$date'
+        |and
+        |    a.media_appsid  in ("80000001", "80000002")
+        |and
+        |    a.isshow = 1
+        |and
+        |    a.antispam = 0
+        |and
+        |    a.adslot_type in (1, 2, 3)
+        |and
+        |    a.adsrc = 1
+        |and
+        |    (a.charge_type is null or a.charge_type = 1)
+      """.stripMargin
+    if("all".equals(hour) == false) sql2 += s" and hour = '$hour'"
+    val dataFrame2 = spark.sql(sql2)
+
+    var sql3 =
+      s"""
+        |select
+        |    a.unitid,
+        |    a.userid,
+        |    3 as conversion_goal,
+        |    a.exp_cvr,
+        |    d.iscvr3 as iscvr
+        |from
+        |    dl_cpc.ocpc_base_unionlog a
+        |left join
+        |    (select
+        |        searchid,
+        |        1 as iscvr3
+        |    from
+        |        dl_cpc.site_form_unionlog
+        |    where
+        |        `date` = '$date'
+        |    and
+        |        ideaid > 0
+        |    and
+        |        searchid is not null
+        |    ) as d
+        |on
+        |    a.searchid = d.searchid
+        |where
+        |    `date` = '$date'
+        |and
+        |    a.media_appsid  in ("80000001", "80000002")
+        |and
+        |    a.isshow = 1
+        |and
+        |    a.antispam = 0
+        |and
+        |    a.adslot_type in (1, 2, 3)
+        |and
+        |    a.adsrc = 1
+        |and
+      """.stripMargin
+    if("all".equals(hour))
+    if("all".equals(hour) == false) sql3 += s" and hour = '$hour'"
+    val dataFrame3 = spark.sql(sql3)
+
+    val dataFrame = dataFrame1.union(dataFrame2).union(dataFrame3)
+    val aucDF = OcpcHourlyAucReport.calculateAUCbyUnitid(dataFrame, date, hour, spark)
+    aucDF
+  }
+
+  // 计算基本统计值
+  def calculateIndexValue(date: String, hour: String, baseDataDF: DataFrame, suggestCpaDF: DataFrame,
+                          aucDF: DataFrame, spark: SparkSession): Unit ={
+    baseDataDF.createOrReplaceTempView("base_data_table")
+    suggestCpaDF.createOrReplaceTempView("suggest_cpa_table")
+    aucDF.createOrReplaceTempView("auc_table")
+
+    val sql =
+      s"""
+        |select
+        |    unitid,
+        |    userid,
+        |    industry,
+        |    round(sum(case when isclick = 1 then is_ocpc else 0 end) / sum(isclick), 4) as put_type,
+        |    adslot_type,
+        |    conversion_goal,
+        |    sum(iscvr) as cv,
+        |    sum(isclick) as click,
+        |    sum(isshow) as show,
+        |    sum(case when isclick = 1 then price else 0 end) * 0.01 as cost,
+        |    round(sum(case when isclick = 1 then price else 0 end) * 10.0 / sum(isshow), 4) as cpm,
+        |    sum(case when isclick = 1 then exp_cvr else 0 end) * 1.0 / sum(isclick) as exp_cvr,
+        |    round(sum(case when isclick = 1 then cast(ocpc_log_dict['pcvr'] as double) else 0 end) * 1.0 / sum(isclick), 4) as pre_cvr,
+        |    round(sum(iscvr) * 1.0 / sum(isclick), 4) as post_cvr,
+        |    round(sum(case when isclick = 1 then price else 0 end) / sum(isclick), 4) as cost_of_every_click,
+        |    round(sum(case when isclick = 1 and length(ocpc_log) = 0 then bid
+        |                   when isclick = 1 and length(ocpc_log) > 0 then cast(ocpc_log_dict['bid'] as double)
+        |                   else 0 end) / sum(isclick), 4) as bid_of_every_click,
+        |    round(sum(case when isclick = 1 then price else 0 end) / sum(iscvr), 4) as cpa_real,
+        |    round(sum(case when isclick = 1 and length(ocpc_log) > 0 then cast(ocpc_log_dict['capgiven'] as double)
+        |                   else 0 end) / sum(isclick), 4) as cpa_given,
+        |    round(sum(case when isclick = 1 and length(ocpc_log) > 0 and ocpc_log_dict['IsHiddenOcpc'] = '1' then price
+        |                   else 0 end) / sum(case when isclick = 1 then price else 0 end), 4) as hidden_cost_ratio,
+        |    round(sum(case when isclick = 1 and length(ocpc_log) > 0 then cast(ocpc_log_dict['kvalue'] as double)
+        |                   else 0 end) / sum(isclick), 4) as kvalue
+        |    max(case when isclick = 1 and length(ocpc_log) > 0 then cast(ocpc_log_dict['budget'] as double) else 0 end) as budget,
+        |    round(case when max(case when isclick = 1 and length(ocpc_log) > 0 then cast(ocpc_log_dict['budget'] as double) else 0 end) = 0 then 0
+        |               else sum(case when isclick = 1 then price else 0 end)
+        |                    / max(case when isclick = 1 and length(ocpc_log) > 0 then cast(ocpc_log_dict['budget'] as double) end, 4) as cost_budget_ratio
+        |from
+        |    base_data_table
+        |group by
+        |    unitid,
+        |    userid,
+        |    industry,
+        |    adslot_type,
+        |    conversion_goal
+      """.stripMargin
+    val dataFrame = spark.sql(sql)
+    val indexValueDF = dataFrame
+      .join(suggestCpaDF, Seq("unitid", "usrid"), "left")
+      .join(aucDF, Seq("unitid", "usrid", "conversion_goal"), "left")
+      .select("unitid", "userid", "industry", "put_type", "adslot_type", "conversion_goal",
+                                      "cv", "click", "show", "cost", "cpm", "exp_cvr", "pre_cvr",
+                                      "post_cvr", "cost_of_every_click", "bid_of_every_click",
+                                      "cpa_real", "sugget_cpa", "cpa_given", "hidden_cost_ratio", "kvalue",
+                                      "auc", "budget", "cost_budget_ratio")
+    val allIndexValueDF = joinIndexValueRingRatio(date, hour, indexValueDF, spark)
+    allIndexValueDF
+      .withColumn("date", lit(date))
+      .withColumn("hour", lit(hour))
+      .withColumn("version", lit("qtt_demo"))
+      .repartition(50)
+      .write.mode("overwrite").insertInto("test.wt_unit_aa_report")
+  }
+
+  // 将统计字段值与环比数据进行join
+  def joinIndexValueRingRatio(date: String, hour: String, indexValueDF: DataFrame, spark: SparkSession): DataFrame ={
+    indexValueDF.createOrReplaceTempView("index_value_table")
+    if("all".equals(hour) == false){
+      val sql =
+        s"""
+          |select
+          |    *,
+          |    0.0 as cv_ring_ratio,
+          |    0.0 as cost_ring_ratio,
+          |    0.0 as post_cvr_ring_ratio
+          |from
+          |    index_value_table
+        """.stripMargin
+      val allIndexValueDF = spark.sql(sql)
+      allIndexValueDF
+    }else{
+      // 首先获取前一天的数据
+      val sdf = new SimpleDateFormat("yyyy-MM-dd")
+      val today = sdf.parse(date)
+      val calendar = Calendar.getInstance
+      calendar.setTime(today)
+      calendar.add(Calendar.DATE, -1)
+      val yesterday = calendar.getTime
+      val preDate = sdf.format(yesterday)
+      val sql1 =
+        s"""
+           |select
+           |    unitid,
+           |    userid,
+           |    cv,
+           |    cost,
+           |    post_cvr
+           |from
+           |    test.wt_unit_aa_report
+           |where
+           |    `date` = "$preDate"
+           |and
+           |    hour = 'all'
+      """.stripMargin
+      spark.sql(sql1).createOrReplaceTempView("yesterday_data_table")
+
+      val sql2 =
+        s"""
+          |select
+          |    a.*,
+          |    round(a.cv / b.cv, 4) as cv_ring_ratio,
+          |    round(a.cost / b.cost, 4) as cost_ring_ratio,
+          |    round(a.post_cvr / b.post_cvr) as post_cvr_ring_ratio
+          |from
+          |    index_value_table a
+          |left join
+          |    yesterday_data_table b
+          |on
+          |    a.unitid = b.unitid
+          |and
+          |    a.userid = b.userid
+        """.stripMargin
+      val allIndexValueDF = spark.sql(sql2)
+      allIndexValueDF
+    }
   }
 }
