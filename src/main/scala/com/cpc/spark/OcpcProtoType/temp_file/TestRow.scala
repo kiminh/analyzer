5ebef1abb6ada0cc0909e19698e498a1a3f5c7db
wentao <wentao@qutoutiao.net>
2019-03-31 22:00:59 +0800

    aa report

--- src/main/scala/com/cpc/spark/OcpcProtoType/temp_file/TestRow.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/temp_file/TestRow.scala
@@ -1,6 +1,7 @@
 package com.cpc.spark.OcpcProtoType.temp_file
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.Row
 
 object TestRow {
   def main(args: Array[String]): Unit = {
@@ -12,9 +13,10 @@ object TestRow {
         |where `date` = '2019-03-27'
         |limit 5
       """.stripMargin
+    val row = Row("1", 2, "3")
     val dataList = spark.sql(sql).collect()
     for(list <- dataList){
-      for(item <- list) print(item + "\t")
+      for(i <- 0 until list.length) print(list.get(i) + "\t")
       println()
     }
   }
