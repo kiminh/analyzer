931a4b08f44b1a596f52c7e8c498955dc20d0745
sunjianqiang <sunjianqiang@qutoutiao.net>
2019-04-01 16:44:50 +0800

    cg

--- src/main/scala/com/cpc/spark/ocpcV3/HP/Lab.scala
+++ src/main/scala/com/cpc/spark/ocpcV3/HP/Lab.scala
@@ -1,128 +1,221 @@
 package com.cpc.spark.ocpcV3.HP
 
+import com.cpc.spark.qukan.userprofile.SetUserProfileTag.SetUserProfileTagInHiveDaily
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import org.apache.spark.sql.{SparkSession, DataFrame}
-import org.apache.spark.sql.SaveMode
-import java.util.Properties
-import java.sql.{Connection, DriverManager}
-import com.typesafe.config.ConfigFactory
+import org.apache.spark.sql.functions._
 
 object Lab {
-  def main(args: Array[String]): Unit ={
-    val spark = SparkSession.builder().appName("Lab").enableHiveSupport().getOrCreate()
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().appName("appInstallation").enableHiveSupport().getOrCreate()
+    val date = args(0).toString
+    val days = args(1).toInt
+
+    val appCat = getAppCat(spark) // appName, cat
+    println("============================== appCat ===================================")
+    appCat.show(10)
+
+    val appFreq = getAppMap(spark, date) // appName0, appName1, appName, count
+    println("============================== appFreq  ======================================")
+    appFreq.show(10)
+
+    val appCat0 = appCat
+      .join(appFreq, Seq("appName"), "left").filter("appName0 is not NULL")
+      .select("cat", "appName", "appName1").distinct()
+      .toDF() // cat, appName, appName1
+
+    appCat0.write.mode("overwrite").saveAsTable("test.appCat0_sjq")
+
+    println("appCat0 has " + appCat0.count() + " lines. ")
+    println("==============================  appCat0  ==========================================")
+    appCat0.show(10)
+
+    val uidUsingSocialApp = getUidUsingSocialApp(spark, date, days - 1, appCat0).cache() //"uid", "cat", "date"
+
+    println("uidApp has " + uidUsingSocialApp.count() + " lines. ")
+    println("=============================== uidApp =============================================")
+    uidUsingSocialApp.show(10)
+    //
+    //    //    create table if not exists dl_cpc.hottopic_uid_bag
+    //    //    (
+    //    //      uid STRING COMMENT '用户id',
+    //    //      cat STRING COMMENT 'app类别'
+    //    //    )
+    //    //    comment '段子社交、短视频、直播人群包'
+    //    //    partitioned by (`date` string);
+    //
+    uidUsingSocialApp.write.mode("overwrite").insertInto("dl_cpc.hottopic_uid_bag")
+    //
+    //    upDate(spark, date)
+
+
+  }
+
+  def getAppCat(spark: SparkSession) = {
+    import spark.implicits._
+    val live = List("火山小视频", "映客", "花椒直播", "石榴直播", "斗鱼直播", "水多直播", "丁香直播", "盒子直播", "深入直播", "一直播", "番茄直播", "快手美女秀", "香蕉直播", "妖娆直播", "小宝贝直播", "易直播", "猫咪视频直播", "快猫直播", "蜜秀直播",
+      "快狐直播", "么么直播", "直播吧", "辣舞直播", "大秀直播", "樱桃直播", "浴火直播", "诱火", "嗨秀秀场", "哇塞直播", "小蛮腰直播", "蜜聊直播", "蜜疯直播", "棉花糖", "陌秀直播", "NOW直播", "夜嗨直播", "蜜兔直播", "花间娱乐美女视频直播交友", "水滴直播",
+      "要播直播", "伊人直播", "NN直播", "红人直播", "Z直播", "比心直播", "来疯直播", "酷咪直播", "九秀美女直播")
+    val lb = scala.collection.mutable.ListBuffer[AppCat]()
+    for (app <- live) {
+      lb += AppCat(app, "直播")
+    }
+    lb.toDF()
+  }
+
+  def getAppMap(spark: SparkSession, date: String) = {
+    import spark.implicits._
 
     val sql1 =
       s"""
-         | select
-         |  case when (`date` = '2019-03-18' and hour >= '21') or (`date` = '2019-03-19' and hour < '21') then '2019-03-19'
-         |       when (`date` = '2019-03-19' and hour >= '21') or (`date` = '2019-03-20' and hour < '21') then '2019-03-20'
-         |       when (`date` = '2019-03-20' and hour >= '21') or (`date` = '2019-03-21' and hour < '21') then '2019-03-21'
-         |       when (`date` = '2019-03-21' and hour >= '21') or (`date` = '2019-03-22' and hour < '21') then '2019-03-22'
-         |       when (`date` = '2019-03-22' and hour >= '21') or (`date` = '2019-03-23' and hour < '21') then '2019-03-23'
-         |       when (`date` = '2019-03-23' and hour >= '21') or (`date` = '2019-03-24' and hour < '21') then '2019-03-24'
-         |       when (`date` = '2019-03-24' and hour >= '21') or (`date` = '2019-03-25' and hour < '21') then '2019-03-25'
-         |       when (`date` = '2019-03-25' and hour >= '21') or (`date` = '2019-03-26' and hour < '21') then '2019-03-26'
-         |       when (`date` = '2019-03-26' and hour >= '21') or (`date` = '2019-03-27' and hour < '21') then '2019-03-27'
-         |       when (`date` = '2019-03-27' and hour >= '21') or (`date` = '2019-03-28' and hour < '21') then '2019-03-28'
-         |  end as dt,
-         |  identifier
-         | from dl_cpc.ocpc_pb_result_hourly  --该表中的identifier已经去直投了
-         |where `date` between '2019-03-18' and '2019-03-28'
-         |  and version = 'hottopicv1'
+         |select
+         | concat_ws(',', app_name) as pkgs1
+         |from dl_cpc.cpc_user_installed_apps a
+         |where load_date = '$date'
        """.stripMargin
+    val pkgs = spark.sql(sql1)
+    pkgs.show(3)
 
-    val date_identifier = spark.sql(sql1).distinct().toDF()
-    date_identifier.createOrReplaceTempView("date_identifier")
-
-    val sql2 =
-      s"""
-         |  select
-         |   a.userid,
-         |   a.`date`,
-         |   a.identifier,
-         |   case
-         |     when c.identifier is not NULL and exptags like "%hot_topic%" and length(ext_string['ocpc_log']) > 0  then '暗投ocpc'
-         |     when c.identifier is not NULL and                                length(ext_string['ocpc_log']) = 0  then '暗投cpc'
-         |     else '直投'
-         |    end as label, --这里的ocpc是指付费方式为cpc下面的ocpc
-         |   sum(isshow)                                                                    as show_n,
-         |         sum(isclick)                                                             as click_n,
-         |   round(sum(case WHEN isclick = 1 then price else 0 end)/100, 3)                 as money
-         |FROM
-         |     (  select
-         |         ext_string['ocpc_log'] as ocpc_log,
-         |         cast(unitid as string) as identifier,
-         |         *
-         |        from dl_cpc.cpc_hot_topic_union_log
-         |       WHERE `date` between '2019-03-19' and '2019-03-28'
-         |         and isshow = 1 --已经展示
-         |         and media_appsid = '80002819'
-         |         and ext['antispam'].int_value = 0  --反作弊标记：1作弊，0未作弊
-         |         AND userid > 0 -- 广告主id
-         |         and adsrc = 1  -- cpc广告（我们这边的广告，非外部广告）
-         |         AND ( ext["charge_type"] IS NULL OR ext["charge_type"].int_value = 1 ) --charge_type: 计费类型
-         |     ) a
-         |left join
-         |     (
-         |        select
-         |         dt,
-         |         identifier
-         |        from date_identifier
-         |      ) c on a.`date` = c.dt and a.identifier = c.identifier
-         |GROUP BY
-         |   a.userid,
-         |   a.`date`,
-         |   a.identifier,
-         |   case
-         |     when c.identifier is not NULL and exptags like "%hot_topic%" and length(ext_string['ocpc_log']) > 0  then '暗投ocpc'
-         |     when c.identifier is not NULL and                                length(ext_string['ocpc_log']) = 0  then '暗投cpc'
-         |     else '直投'
-         |    end
-       """.stripMargin
-
-    val result = spark.sql(sql2)
-    result.write.mode("overwrite").saveAsTable("test.detail_sjq")
-    val usertype = getUsertype(spark)
-    usertype.write.mode("overwrite").saveAsTable("test.usertype_sjq")
-
+    val result = pkgs.rdd
+      .map(x => x.getAs[String]("pkgs1"))
+      .flatMap(x => x.split(","))
+      .map(x => (x, 1))
+      .reduceByKey((x, y) => x + y)
+      .map(x => (x._1, x._1.split("-"), x._2))
+      .map(x =>
+        if (x._2.length > 1) {
+          AppCount(x._1, x._2(0), x._2(1), x._3)
+        } else {
+          AppCount(x._1, x._2(0), "", x._3)
+        }
+      ).toDF()
+    result
   }
 
-  def getUsertype(spark: SparkSession) ={
+
+  def getUidUsingSocialApp(spark: SparkSession, date: String, days: Int, appCat: DataFrame) = {
+    /** appCat字段： cat, appName, appName0 */
+    import spark.implicits._
+
+    val sdf = new SimpleDateFormat("yyyy-MM-dd")
+    val calendar = Calendar.getInstance
+    val yesterday = sdf.parse(date)
+    calendar.setTime(yesterday)
+    calendar.add(Calendar.DATE, -days)
+    val firstDay = calendar.getTime
+    val date0 = sdf.format(firstDay)
+
     val sql =
       s"""
-         |select
-         | userid,
-         | usertype,
-         | sum(isshow) as shown
-         |from dl_cpc.slim_union_log
-         |where dt between '2019-03-19' and '2019-03-28'
-         |  and userid in (1581037,    1581406,    1581836,    1588390,    1576960,    1583276,    1588283,    1589298,    1588107,    1576675,    1588403,    1543127,    1552449,    1585017,    1589171,    1541932,    1570426,    1583384,    1590326,    1590035,
-         |1589637,    1566487,    1567729,    1535264,    1554537,    1590076,    1591531,    1590084,    1569058,    1587059,    1590254,    1587366,    1585591,    1533758,    1563600,    1587946,    1584567,    1585232,    1590776,    1589474,
-         |1547207,    1567482,    1579766,    1519484,    1590104,    1564079,    1588890,    1539251,    1571994,    1569167,    1539923,    1587914,    1565696,    1589520,    1563171,    1579543,    1589530,    1564584,    1588901,    1588641,
-         |1573052,    1590158,    1588598,    1566925,    1591022,    1566761,    1563596,    1571664,    1574862,    1588897,    1572222,    1574148,    1589825,    1588710,    1567871,    1584144,    1567453,    1588590,    1583860,    1583947,
-         |1587614,    1565596,    1550052,    1581581,    1543199,    1537508,    1576831,    1520202,    1565843,    1587888,    1518228,    1590212,    1583193,    1592227,    1527654,    1590783,    1587861,    1588109,    1592173,    1588773,
-         |1519843,    1591654,    1514929,    1588684,    1583822,    1571996,    1589738,    1521579,    1589739,    1565306,    1591985,    1584841,    1590709,    1587632,    1590655,    1590727,    1592575,    1592856,    1592796,    1584052,
-         |1589982,    1586491,    1556107,    1586707,    1570905,    1591503,    1589905,    1575449,    1591356,    1589439,    1589095,    1592654,    1592747,    1574096,    1579684,    1586905,    1542903,    1588937,    1588472,    1579388,
-         |1589533,    1580490,    1571705,    1557971,    1566783,    1584173,    1587514,    1588551,    1579644,    1534362,    1590730,    1589570,    1587519,    1540520,    1592877,    1508354,    1588947,    1588928,    1589056,    1581027,
-         |1533606,    1549068,    1592435,    1581345,    1523417,    1589270,    1592821,    1587980,    1588363,    1572665,    1592114,    1592790,    1555582,    1592545,    1588688,    1544535,    1591860,    1583546,    1592531,    1592655,
-         |1587177,    1533463,    1549785,    1589119,    1573904,    1572956,    1555580,    1526997,    1532869,    1542405,    1523536,    1589217,    1566601,    1589108,    1578363,    1572744,    1586487,    1565280,    1592152,    1525122,
-         |1521839,    1582164,    1560379,    1533524,    1585056,    1581644,    1533329,    1585565,    1587619,    1556856,    1559638,    1574891,    1553638,    1586494,    1581157,    1581331,    1564438,    1591498,    1592797,    1546477,
-         |1586181,    1587402,    1593632,    1565311,    1591511,    1566085,    1579727,    1586483,    1590529,    1593155,    1593705,    1587353,    1591070,    1592774,    1588085,    1539305,    1593367,    1535812,    1591527,    1588506,
-         |1593673,    1592751,    1564929,    1566936,    1518336,    1592535,    1593061,    1584142,    1593752,    1587926,    1570866,    1593924,    1592661,    1591421,    1591888,    1592714,    1594167,    1587662,    1577298,    1578300,
-         |1591516,    1590009,    1592393,    1590023,    1589987,    1590425,    1584147,    1591917,    1576478,    1587935,    1589675,    1591721,    1594151,    1587583,    1566499,    1586026,    1588993,    1589067,    1567080,    1586029,
-         |1528033,    1587574,    1564480,    1576517,    1586060,    1580871,    1589979,    1589951,    1532173,    1589403,    1580059,    1587319,    1587309,    1587235,    1593477,    1572326,    1589646,    1574464,    1582759,    1581362,
-         |1527038,    1568798,    1574147,    1595371,    1591529,    1594038,    1589647,    1589801,    1589891,    1589993,    1518117,    1569538,    1593110,    1560733,    1589362,    1517329,    1594017 )
-         |  and isshow = 1 --已经展示
-         |  and antispam = 0  --反作弊标记：1作弊，0未作弊
-         |  and adsrc = 1  -- cpc广告（我们这边的广告，非外部广告）
-         |  AND ( charge_type IS NULL OR charge_type = 1 )
-         |group by userid,
-         | usertype
+         | select
+         |    t1.uid,
+         |    t2.pkgs1
+         |from (
+         |       select
+         |        dt, uid
+         |       from dl_cpc.slim_union_log
+         |      where dt between '$date0' and '$date'
+         |        and adsrc = 1
+         |        and userid >0
+         |        and isshow = 1
+         |        and antispam = 0
+         |        and media_appsid = '80002819'
+         |       group by dt, uid ) t1
+         | join (
+         |       select
+         |        load_date,
+         |        uid,
+         |        concat_ws(',', used_pkgs) as pkgs1
+         |       from dl_cpc.cpc_user_installed_apps
+         |      where load_date between '$date0' and '$date'
+         |      group by load_date, uid, used_pkgs
+         |     ) t2
+         | on t1.dt = t2.load_date and t1.uid = t2.uid
+         | group by t1.uid, t2.pkgs1
        """.stripMargin
+    println(sql)
     val df = spark.sql(sql)
-    df
+
+    val result = df.rdd
+      .map(x => (x.getAs[String]("uid"), x.getAs[String]("pkgs1").split(",")))
+      .flatMap(x => {
+        val uid = x._1
+        val pkgs = x._2
+        val lb = scala.collection.mutable.ListBuffer[UidApp]()
+        for (app <- pkgs) {
+          lb += UidApp(uid, app)
+        }
+        lb
+      }
+      ).toDF() // uid, appName1
+      .join(appCat, Seq("appName1"))
+      .select("uid", "cat")
+      .distinct().toDF()
+      .withColumn("date", lit(date))
+
+    result
   }
+
+  def upDate(spark: SparkSession, date: String): Unit = {
+    val sdf = new SimpleDateFormat("yyyy-MM-dd")
+    val calendar = Calendar.getInstance
+    val today = sdf.parse(date)
+    calendar.setTime(today)
+    calendar.add(Calendar.DATE, -1)
+    val yesterday = calendar.getTime()
+    val date0 = sdf.format(yesterday)
+
+    val sqlRequest =
+      s"""
+         |select
+         |  coalesce(t1.cat, t2.cat) as cat,
+         |  coalesce(t1.uid, t2.uid) as uid,
+         |  tag0,
+         |  tag1
+         |from
+         |  (
+         |    select
+         |      cat,
+         |      uid,
+         |      1 as tag1
+         |    from
+         |      dl_cpc.hottopic_uid_bag
+         |    where
+         |      `date` = '$date'
+         |  ) t1 full
+         |  outer join (
+         |    select
+         |      cat,
+         |      uid,
+         |      1 as tag0
+         |    from
+         |      dl_cpc.hottopic_uid_bag
+         |    where
+         |      `date` = '$date0'
+         |  ) t2 on t1.cat = t2.cat
+         |  and t1.uid = t2.uid
+       """.stripMargin
+
+    println(sqlRequest)
+    val df = spark.sql(sqlRequest)
+      .withColumn("id", when(col("cat") === "社交", lit(317)).otherwise(when(col("cat") === "短视频", lit(318)).otherwise(lit(319))))
+      .withColumn("io", when(col("tag1").isNotNull, lit(true)).otherwise(lit(false)))
+      .select("cat", "uid", "tag0", "tag1", "id", "io")
+
+    //    df.write.mode("overwrite").saveAsTable("test.putOrDrop_sjq")
+
+    val rdd1 = df.select("uid", "id", "io").rdd.map(x => (x.getAs[String]("uid"), x.getAs[Int]("id"), x.getAs[Boolean]("io")))
+
+    val result = SetUserProfileTagInHiveDaily(rdd1)
+
+  }
+
+  case class AppCat(var appName: String, var cat: String)
+
+  case class AppCount(var appName0: String, var appName1: String, appName: String, var count: Int)
+
+  case class UidApp(var uid: String, var appName1: String)
 
 }
 
