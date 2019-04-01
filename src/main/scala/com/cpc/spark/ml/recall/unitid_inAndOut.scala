4497bf6974b4933471e2a7486968c030648ccc84
段广东 <duanguangdong@qutoutiao.net>
2019-04-01 21:02:56 +0800

    dnn 年龄模型

--- src/main/scala/com/cpc/spark/ml/recall/unitid_inAndOut.scala
+++ src/main/scala/com/cpc/spark/ml/recall/unitid_inAndOut.scala
@@ -18,13 +18,37 @@ object unitid_inAndOut {
     cal1.add(Calendar.DATE, -2)
     val startdate = new SimpleDateFormat("yyyy-MM-dd").format(cal1.getTime)
 
+//    spark.sql(
+//      s"""
+//         |select ta.unitid,'bscvr' as experiment,0 as performance from
+//         |(select unitid,cost/0.099 as cpm,cvr from dl_cpc.cpc_recall_bscvr_report where date='$tardate' and exp='control'
+//         |and unitid not in ('exp_unitid', 'all')) ta
+//         |join
+//         |(select unitid,cost/0.901 as cpm,cvr from dl_cpc.cpc_recall_bscvr_report where date='$tardate' and exp='enabled0.3'
+//         |and unitid not in ('exp_unitid', 'all')) tb
+//         |on ta.unitid=tb.unitid
+//         |where ta.cpm>tb.cpm or ta.cvr>tb.cvr group by ta.unitid
+//      """.stripMargin).createOrReplaceTempView("bscvr")
+//
+//    spark.sql(
+//      s"""
+//         |select ta.unitid,'bscvrExp' as experiment,1 as performance from
+//         |(select unitid,cost/0.899 as cpm,cvr from dl_cpc.cpc_recall_bsExp_report where date='$tardate' and exp='control'
+//         |and unitid not in ('exp_unitid', 'all')) ta
+//         |join
+//         |(select unitid,cost/0.101 as cpm,cvr from dl_cpc.cpc_recall_bsExp_report where date='$tardate' and exp='enabled0.3'
+//         |and unitid not in ('exp_unitid', 'all')) tb
+//         |on ta.unitid=tb.unitid
+//         |where ta.cpm<tb.cpm and ta.cvr<=tb.cvr group by ta.unitid
+//      """.stripMargin).createOrReplaceTempView("bscvrExp")
+
     spark.sql(
       s"""
          |select ta.unitid,'bscvr' as experiment,0 as performance from
-         |(select unitid,cost/0.099 as cpm,cvr from dl_cpc.cpc_recall_bscvr_report where date='$tardate' and exp='control'
+         |(select unitid,cpm,cvr from dl_cpc.cpc_recall_bscvr_report where date='$tardate' and exp='control'
          |and unitid not in ('exp_unitid', 'all')) ta
          |join
-         |(select unitid,cost/0.901 as cpm,cvr from dl_cpc.cpc_recall_bscvr_report where date='$tardate' and exp='enabled0.3'
+         |(select unitid,cpm,cvr from dl_cpc.cpc_recall_bscvr_report where date='$tardate' and exp='enabled0.3'
          |and unitid not in ('exp_unitid', 'all')) tb
          |on ta.unitid=tb.unitid
          |where ta.cpm>tb.cpm or ta.cvr>tb.cvr group by ta.unitid
@@ -33,10 +57,10 @@ object unitid_inAndOut {
     spark.sql(
       s"""
          |select ta.unitid,'bscvrExp' as experiment,1 as performance from
-         |(select unitid,cost/0.899 as cpm,cvr from dl_cpc.cpc_recall_bsExp_report where date='$tardate' and exp='control'
+         |(select unitid,cpm,cvr from dl_cpc.cpc_recall_bsExp_report where date='$tardate' and exp='control'
          |and unitid not in ('exp_unitid', 'all')) ta
          |join
-         |(select unitid,cost/0.101 as cpm,cvr from dl_cpc.cpc_recall_bsExp_report where date='$tardate' and exp='enabled0.3'
+         |(select unitid,cpm,cvr from dl_cpc.cpc_recall_bsExp_report where date='$tardate' and exp='enabled0.3'
          |and unitid not in ('exp_unitid', 'all')) tb
          |on ta.unitid=tb.unitid
          |where ta.cpm<tb.cpm and ta.cvr<=tb.cvr group by ta.unitid
