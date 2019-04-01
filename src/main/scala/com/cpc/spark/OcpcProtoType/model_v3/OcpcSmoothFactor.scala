27ca6ca7fd9dccb6e605104f830774812717e78e
wangjun <wangjun02@qutoutiao.net>
2019-04-01 15:12:16 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/model_v3/OcpcSmoothFactor.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/model_v3/OcpcSmoothFactor.scala
@@ -52,8 +52,8 @@ object OcpcSmoothFactor{
     resultDF.show()
 
     resultDF
-//      .repartition(5).write.mode("overwrite").insertInto("dl_cpc.ocpc_pcoc_jfb_hourly")
-      .repartition(5).write.mode("overwrite").saveAsTable("test.check_cvr_smooth_data20190329")
+      .repartition(5).write.mode("overwrite").insertInto("dl_cpc.ocpc_pcoc_jfb_hourly")
+//      .repartition(5).write.mode("overwrite").saveAsTable("test.check_cvr_smooth_data20190329")
   }
 
 //  def getConfData(spark: SparkSession) = {
