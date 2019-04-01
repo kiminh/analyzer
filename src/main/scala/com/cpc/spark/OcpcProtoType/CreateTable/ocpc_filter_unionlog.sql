03eb9c7bec4f644318b152a2b6cd0d39cd114f97
wangjun <wangjun02@qutoutiao.net>
2019-04-01 13:32:50 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/CreateTable/ocpc_filter_unionlog.sql
+++ src/main/scala/com/cpc/spark/OcpcProtoType/CreateTable/ocpc_filter_unionlog.sql
@@ -44,3 +44,4 @@ STORED as PARQUET;
 --alter table dl_cpc.ocpc_filter_unionlog add columns (charge_type int);
 --alter table dl_cpc.ocpc_filter_unionlog add columns (conversion_from int);
 --alter table dl_cpc.ocpc_filter_unionlog add columns (is_api_callback int);
+--alter table dl_cpc.ocpc_filter_unionlog add columns (siteid int);
