eab364921e3b6b5de6a7a38135c1de7a93523e3c
sunjianqiang <sunjianqiang@qutoutiao.net>
2019-04-04 15:50:39 +0800

    cg

--- src/main/scala/com/cpc/spark/ocpcV3/HP/Lab.scala
+++ src/main/scala/com/cpc/spark/ocpcV3/HP/Lab.scala
@@ -6,16 +6,16 @@ import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.mllib.fpm.FPGrowth
 
 object Lab {
-  def main(args: Array[String]): Unit ={
+  def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("fpg")
-    val sc   = new SparkContext(conf)
+    val sc = new SparkContext(conf)
     Logger.getRootLogger.setLevel(Level.WARN)
 
     //1. 读取样本数据
     val filename = "sample_fpgrowth.txt"
     val data_path = s"hdfs://emr-cluster/warehouse/sunjianqiang/${filename}"
     val data = sc.textFile(data_path)
-    val examples = data.map(_.split(" ")).cache( )
+    val examples = data.map(_.split(" ")).cache()
 
     //2. 建立模型
     //设置最小支持度
@@ -23,15 +23,14 @@ object Lab {
     //设置并行分区数
     val numPartition = 10
     val model = new FPGrowth()
-      .setMinSupport( minSupport )
+      .setMinSupport(minSupport)
       .setNumPartitions(numPartition)
       .run(examples)
 
     //3. 查看所有的频繁项集，并且列出它们出现的次数
     println(s"Number of frequent itemsets: ${model.freqItemsets.count()}")
-    val f2 = model.freqItemsets.take(5)
-    println( f2 )
-    model.freqItemsets.collect().foreach{ itemset => println( itemset.items.mkString("[", ",", "]") + "," + itemset.freq ) }
+    model.freqItemsets.foreach { itemset => println(itemset.items.getClass.getSimpleName ) }
+    //    model.freqItemsets.collect().foreach{ itemset => println( itemset.items.mkString("[", ",", "]") + "," + itemset.freq ) }
 
     //4. 通过置信度筛选出推荐规则
     //antecedent：前项
@@ -39,12 +38,12 @@ object Lab {
     //confidence：置信度
     val minConfidence = 0.8
     model.generateAssociationRules(minConfidence).collect()
-      .foreach( rule => {
-        println( rule.antecedent.mkString(",") + "-->" + rule.consequent.mkString(",") + "-->" + rule.confidence )
+      .foreach(rule => {
+        println(rule.antecedent.mkString(",") + "-->" + rule.consequent.mkString(",") + "-->" + rule.confidence)
       })
 
     //查看规则生成的数量
-    println(model.generateAssociationRules(minConfidence).getClass().getSimpleName() )
+    println(model.generateAssociationRules(minConfidence).getClass().getSimpleName())
   }
 }
 
