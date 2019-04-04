a483ca22eb4a5cd89adcad7addc4f7c77bbe6c76
段广东 <duanguangdong@qutoutiao.net>
2019-04-04 18:55:46 +0800

    dnn 年龄模型

--- src/main/scala/com/cpc/spark/ml/recall/unitid_inAndOut.scala
+++ src/main/scala/com/cpc/spark/ml/recall/unitid_inAndOut.scala
@@ -116,7 +116,7 @@ object unitid_inAndOut {
       s"""
          |select * from dl_cpc.cpc_recall_high_confidence_unitid where unitid not in (select unitid from undesired)
          |and unitid in (select unit_id from cost_unitid) and date='$tardate' and unitid not in
-         |(select unitid from dl_cpc.cpc_recall_unitid_ctr_dif where dt='$tardate' and ratio>1.5 group by unitid)
+         |(select unitid from dl_cpc.cpc_recall_unitid_ctr_dif where dt>='$startdate' group by unitid having avg(ratio)>1.5)
          |and unitid not in ('2013592')
       """.stripMargin).repartition(1).cache()
     data.show(10)
