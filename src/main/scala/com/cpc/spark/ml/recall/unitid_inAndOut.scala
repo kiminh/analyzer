9885a944a3ec86d1b476a2a44a8aef42e4b3aa97
段广东 <duanguangdong@qutoutiao.net>
2019-04-04 16:31:13 +0800

    dnn 年龄模型

--- src/main/scala/com/cpc/spark/ml/recall/unitid_inAndOut.scala
+++ src/main/scala/com/cpc/spark/ml/recall/unitid_inAndOut.scala
@@ -63,7 +63,7 @@ object unitid_inAndOut {
          |(select unitid,cpm,cvr from dl_cpc.cpc_recall_bsExp_report where date='$tardate' and exp='enabled0.3'
          |and unitid not in ('exp_unitid', 'all')) tb
          |on ta.unitid=tb.unitid
-         |where ta.cpm>8 and ta.cvr<=tb.cvr group by ta.unitid
+         |where ta.cpm>8 and ta.cvr<tb.cvr group by ta.unitid
       """.stripMargin).createOrReplaceTempView("bscvrExp")
 
     spark.sql(
