faac1959bec80457b8dd7a4df508d00deef08113
wentao <wentao@qutoutiao.net>
2019-04-04 17:56:50 +0800

    industry ab report

--- src/main/scala/com/cpc/spark/OcpcProtoType/aa_ab_report/GetBaseData.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/aa_ab_report/GetBaseData.scala
@@ -112,7 +112,7 @@ object GetBaseData {
          |    where
          |        `date` = '$date'
          |    and
-         |        cv_goal = 'cv1') as b
+         |        cvr_goal = 'cv1') as b
          |on
          |    a.searchid = b.searchid
          |left join
@@ -124,7 +124,7 @@ object GetBaseData {
          |    where
          |        `date` = '$date'
          |    and
-         |        cv_goal = 'cv2') as c
+         |        cvr_goal = 'cv2') as c
          |on
          |    a.searchid = c.searchid
          |left join
@@ -136,7 +136,7 @@ object GetBaseData {
          |    WHERE
          |        `date` = '$date'
          |    and
-         |        cv_goal = 'cv3') as d
+         |        cvr_goal = 'cv3') as d
          |on
          |    a.searchid = d.searchid
       """.stripMargin
