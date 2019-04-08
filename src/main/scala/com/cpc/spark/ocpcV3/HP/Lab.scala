5b9b66d29a8b11150d81709875f30da07d25df9b
sunjianqiang <sunjianqiang@qutoutiao.net>
2019-04-08 11:59:26 +0800

    cg

--- src/main/scala/com/cpc/spark/ocpcV3/HP/Lab.scala
+++ src/main/scala/com/cpc/spark/ocpcV3/HP/Lab.scala
@@ -12,11 +12,17 @@ object Lab {
     import spark.implicits._
 
     val baseData = getBaseData(spark, date).map(x => x._2.distinct)
+    val t6 = new Date()
+    println("T6 is " + t6)
     print("baseData has " + baseData.count() + " elements" )
+    val t7 = new Date()
+    println("T7 is " + t7)
 
     val minSupport = 0.4
     val numPartition = 10
     val model = new FPGrowth().setMinSupport(minSupport).setNumPartitions(numPartition).run(baseData)
+    val t8 = new Date()
+    println("T8 is " + t8)
 
     val freqItemsets = model.freqItemsets.persist()
     val numFreqItemsets = freqItemsets.count()
@@ -27,7 +33,8 @@ object Lab {
     val associationRules = model.generateAssociationRules(minConfidence)
     val numAssociationRules = associationRules.count()
     println("Number of association rules: "+ numAssociationRules )
-    associationRules.take(30).foreach{ rule => println(rule.antecedent.mkString( "{", ",", "}") + " => " + rule.consequent.mkString(",") + " : " + rule.confidence ) }
+    associationRules.take(30)
+      .foreach{ rule => println(rule.antecedent.mkString( "{", ",", "}") + " => " + rule.consequent.mkString(",") + " : 1.confidence" + rule.confidence + "; 2.lift" + rule.lift) }
 
 
 
@@ -46,7 +53,7 @@ object Lab {
 
     println(sqlRequest)
     val t1 = new Date()
-    println("Now is " + t1)
+    println("T1 is " + t1)
     val df1 = spark.sql(sqlRequest).rdd
       .map(x => (x.getAs[String]("uid"), x.getAs[String]("pkgs").split(",") ))
       .flatMap(x => {
@@ -66,7 +73,7 @@ object Lab {
       }).toDF("uid", "appName").persist()
 
     val t2 = new Date()
-    println("Now is " + t2)
+    println("T2 is " + t2)
 
     val apps_exception = Array("", "趣头条").mkString("('", "','", "')")
     val countLimit = 100
@@ -78,20 +85,20 @@ object Lab {
       .filter(s"appName not in ${apps_exception} and  count >= ${countLimit}")
 
     val t3 = new Date()
-    println("Now is " + t3)
+    println("T3 is " + t3)
 
     val df3 = df1.join(df2, Seq("appName"), "left").select("uid", "appName", "count").filter("count is not NULL")
 
 //    df3.write.mode("overwrite").saveAsTable("test.app_count_sjq")
 
     val t4 = new Date()
-    println("Now is " + t4)
+    println("T4 is " + t4)
 
     val result = df3.rdd
       .map(x => (x.getAs[String]("uid"), Array(x.getAs[String]("appName"))))
       .reduceByKey((appArr1, appArr2) => appArr1 ++ appArr2)
     val t5 = new Date()
-    println("Now is " + t5)
+    println("T5 is " + t5)
     result
   }
 
