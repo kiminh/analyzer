921841ed024b6b6de3d40b7b2b63865b45c9e263
wangyao <wangyao@qutoutiao.net>
2019-04-08 15:28:23 +0800

    -

--- src/main/scala/com/cpc/spark/ocpcV3/ocpcNovel/OcpcGetPbV2.scala
+++ src/main/scala/com/cpc/spark/ocpcV3/ocpcNovel/OcpcGetPbV2.scala
@@ -488,9 +488,10 @@ object OcpcGetPbV2 {
     jdbcProp.put("driver", "com.mysql.jdbc.Driver")
 
     //从adv后台mysql获取人群包的url
-    val table=s"(select cast(id as int) as unitid FROM adv.unit WHERE target_medias ='80001098,80001292,80001539,80002480,80001011' and status=0) as tmp"
+    val table=s"(select id as unitid FROM adv.unit WHERE target_medias ='80001098,80001292,80001539,80002480,80001011' and status=0) as tmp"
       val resultDF = spark.read.jdbc(jdbcUrl, table, jdbcProp)
           .withColumn("target",lit(1))
+          .selectExpr("cast(unitid as int)unitid","target")
 
     resultDF
   }
