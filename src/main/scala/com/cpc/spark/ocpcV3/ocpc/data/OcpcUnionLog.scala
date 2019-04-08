350df7407eff562b3cd6a3a9309eaec113bad6a4
wentao <wentao@qutoutiao.net>
2019-04-08 21:10:39 +0800

    ocpc union aa report

--- src/main/scala/com/cpc/spark/ocpcV3/ocpc/data/OcpcUnionLog.scala
+++ src/main/scala/com/cpc/spark/ocpcV3/ocpc/data/OcpcUnionLog.scala
@@ -13,9 +13,9 @@ object OcpcUnionLog {
     val hour = args(1).toString
 
     val result = getOcpcUnionlog(date, hour, spark)
-//    result.write.mode("overwrite").saveAsTable("test.ocpc_union_log_hourly")
-    result
-      .repartition(10).write.mode("overwrite").insertInto("dl_cpc.ocpc_union_log_hourly")
+    result.write.mode("overwrite").saveAsTable("test.ocpc_union_log_hourly")
+//    result
+//      .repartition(10).write.mode("overwrite").insertInto("dl_cpc.ocpc_union_log_hourly")
     println("successfully save data into table: dl_cpc.ocpc_union_log_hourly")
   }
 
