af0a986614789371aad346666c9c282a61581b26
wangjun <wangjun02@qutoutiao.net>
2019-04-09 16:16:01 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/model_v3/OcpcCvrSmooth.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/model_v3/OcpcCvrSmooth.scala
@@ -222,7 +222,7 @@ object OcpcCvrSmooth {
       .na.fill(0, Seq("min_bid", "min_cpm", "cpc_bid", "cpa_suggest", "param_t"))
       .na.fill(0.0, Seq("cvr1", "cvr2", "cvr3"))
       .na.fill(0.2, Seq("factor1"))
-      .na.fill(0.5, Seq("factor2", "factor2"))
+      .na.fill(0.5, Seq("factor2", "factor3"))
       .na.fill(1.0, Seq("cali_value"))
       .selectExpr("identifier", "cast(min_bid as double) min_bid", "cvr1", "cvr2", "cvr3", "cast(min_cpm as double) as min_cpm", "cast(factor1 as double) factor1", "cast(factor2 as double) as factor2", "cast(factor3 as double) factor3", "cast(cpc_bid as double) cpc_bid", "cpa_suggest", "param_t", "cali_value")
 
