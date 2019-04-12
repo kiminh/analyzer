97db9f5f0f7f31f1c79ad3ad648c565ed5b86974
wangyao <wangyao@qutoutiao.net>
2019-04-12 20:32:04 +0800

    -

---
+++ src/main/scala/com/cpc/spark/novel/toutiao/toutiaosample2.scala
@@ -0,0 +1,34 @@
+package com.cpc.spark.novel.toutiao
+
+import java.io.StringReader
+import au.com.bytecode.opencsv.CSVReader
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object toutiaosample2 {
+  def main(args: Array[String]): Unit = {
+    val date = args(0)
+    val spark = SparkSession.builder()
+      .appName(s"midu_sample")
+      .enableHiveSupport()
+      .getOrCreate()
+
+    import spark.implicits._
+
+    val input = spark.sparkContext.textFile("home/cpc/wy/prediction.csv")
+    val result = input.map{ line =>
+      val reader = new CSVReader(new StringReader(line));
+      reader.readNext()
+    }
+    println("alldata:"+result.count())
+
+
+    val resultDF= result.map(x => {
+      (x(0).toInt,x(1).toString())
+    }).toDF("id", "title")
+
+    resultDF.show(10)
+
+//      .repartition(20).write.mode("overwrite").
+
+  }
+}
