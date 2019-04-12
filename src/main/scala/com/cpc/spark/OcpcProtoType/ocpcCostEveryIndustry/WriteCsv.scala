1c2b5f54b3b86cddeb588f409551a4c263219b82
wentao <wentao@qutoutiao.net>
2019-04-12 13:54:52 +0800

    recreate ocpc cost report

--- src/main/scala/com/cpc/spark/OcpcProtoType/ocpcCostEveryIndustry/WriteCsv.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/ocpcCostEveryIndustry/WriteCsv.scala
@@ -23,9 +23,8 @@ object WriteCsv {
       x.getAs[Int]("ocpc_unit_today").toString, x.getAs[Int]("new_ocpc_unit").toString,
       x.getAs[Int]("recommend_unit").toString, x.getAs[String]("date").toString).mkString(","))
       .map(x => (x, 2)))
-      .sortBy(_._2)
+      .sortBy(x => (x._2, x._1))
       .map(x => x._1)
-      .sortBy(x => x)
     val list = data.collect()
     for(item <- list) println(item)
 //    data
