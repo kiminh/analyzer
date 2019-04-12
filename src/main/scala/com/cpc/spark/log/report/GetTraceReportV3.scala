35bd17b66c7c39cbb6a38368eb1afc1694b9ad01
zhanghongyang <zhanghongyang@qutoutiao.net>
2019-04-12 12:45:46 +0800

    fix

--- src/main/scala/com/cpc/spark/log/report/GetTraceReportV3.scala
+++ src/main/scala/com/cpc/spark/log/report/GetTraceReportV3.scala
@@ -381,7 +381,7 @@ object GetTraceReportV3 {
       s"""
          |select
          |   a.searchid
-         |  , a.userid as user_id
+         |  , cast(a.userid as int) as user_id
          |  , a.planid as plan_id
          |  , a.unitid as unit_id
          |  , a.ideaid as idea_id
