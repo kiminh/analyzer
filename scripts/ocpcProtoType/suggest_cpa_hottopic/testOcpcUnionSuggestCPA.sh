49a60e41e37b6477b88740d2313dc442fd8b6d6c
wangjun <wangjun02@qutoutiao.net>
2019-04-13 21:03:41 +0800

    fix bug

--- scripts/ocpcProtoType/suggest_cpa_hottopic/testOcpcUnionSuggestCPA.sh
+++ scripts/ocpcProtoType/suggest_cpa_hottopic/testOcpcUnionSuggestCPA.sh
@@ -17,5 +17,5 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.yarn.executor.memoryOverhead=4g'\
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
-    --class com.cpc.spark.OcpcProtoType.suggest_cpa_v1.OcpcUnionSuggestCPA\
-    /home/cpc/wangjun/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $1 $2 $3
+    --class com.cpc.spark.OcpcProtoType.suggest_cpa_hottopic.OcpcUnionSuggestCPA\
+    /home/cpc/wangjun/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $1 $2 $3 hottopic
