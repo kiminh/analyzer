e042483ea2614b578c89d4e629be861b71c59697
wangyao <wangyao@qutoutiao.net>
2019-04-13 16:04:59 +0800

    -

--- src/main/scala/com/cpc/spark/novel/toutiao/toutiaosample2.scala
+++ src/main/scala/com/cpc/spark/novel/toutiao/toutiaosample2.scala
@@ -1,9 +1,12 @@
 package com.cpc.spark.novel.toutiao
 
 import java.io.StringReader
+
 import au.com.bytecode.opencsv.CSVReader
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
+import scala.sys.process._
 
 object toutiaosample2 {
   def main(args: Array[String]): Unit = {
@@ -15,16 +18,27 @@ object toutiaosample2 {
 
     import spark.implicits._
 
-    val input = spark.sparkContext.textFile("/user/cpc/wy/prediction.csv")
-    val result = input.map{ line =>
-      val reader = new CSVReader(new StringReader(line))
-      reader.readNext()
-    }
-    println("alldata:"+result.count())
+//    val input = spark.sparkContext.textFile("/user/cpc/wy/prediction.csv")
+//    val result = input.map{ line =>
+//      val reader = new CSVReader(new StringReader(line))
+//      reader.readNext()
+//    }
 
-    val resultDF= result.map(x => {
-      (x(0).toString,x(1).toString)
-    }).toDF("id", "title")
+    val filename = "/home/cpc/wy/prediction.txt"
+    val path = s"/user/cpc/wy/prediction.txt"
+    val movefiletohdfs = s"hadoop fs -put -f ${filename} ${path}"
+    movefiletohdfs !
+    val title= spark.sparkContext.textFile(path)
+      .map(x=>x.split("$")).map(x=> Row(x(0),x(1),"GBK"))
+
+    val schema: StructType = (new StructType)
+      .add("id", StringType)
+      .add("title", StringType)
+
+    //根据rdd和schema信息创建DataFrame
+    val titleDF: DataFrame = spark.createDataFrame(title, schema)
+
+    val resultDF= titleDF
       .withColumn("category",convert(col("id")))
       .select("title","category")
 
@@ -36,14 +50,14 @@ object toutiaosample2 {
     (x:String)=> {
       var y = ""
       x match {
-        case "0.0" =>  y = "美容化妆"
-        case "1.0" =>  y = "游戏类"
-        case "2.0" =>  y = "社交网络"
-        case "3.0" =>  y = "网上购物"
-        case "4.0" =>  y = "二类电商"
-        case "5.0" =>  y = "成人用品"
-        case "6.0" =>  y = "生活服务"
-        case "7.0" =>  y = "无"
+        case "0" =>  y = "美容化妆"
+        case "1" =>  y = "游戏类"
+        case "2" =>  y = "社交网络"
+        case "3" =>  y = "网上购物"
+        case "4" =>  y = "二类电商"
+        case "5" =>  y = "成人用品"
+        case "6" =>  y = "生活服务"
+        case "7" =>  y = "无"
 
       }
       y
