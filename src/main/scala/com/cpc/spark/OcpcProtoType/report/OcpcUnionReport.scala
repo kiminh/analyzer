3e25eef318bd827a522137e0317d571ef914dfa8
wangjun <wangjun02@qutoutiao.net>
2019-04-13 20:25:48 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/report/OcpcUnionReport.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/report/OcpcUnionReport.scala
@@ -13,12 +13,11 @@ object OcpcUnionReport {
     val dataUnitRaw = unionDetailReport(date, hour, spark)
     // get the suggest cpa
     val dataUnit = addSuggestCPA(dataUnitRaw, date, hour, spark)
-    dataUnit.write.mode("overwrite").saveAsTable("test.check_ocpc_data20190413a")
-//    println("------union detail report success---------")
-//    val dataConversion = unionSummaryReport(date, hour, spark)
-//    println("------union summary report success---------")
-//    saveDataToMysql(dataUnit, dataConversion, date, hour, spark)
-//    println("------insert into mysql success----------")
+    println("------union detail report success---------")
+    val dataConversion = unionSummaryReport(date, hour, spark)
+    println("------union summary report success---------")
+    saveDataToMysql(dataUnit, dataConversion, date, hour, spark)
+    println("------insert into mysql success----------")
   }
 
   def addSuggestCPA(rawData: DataFrame, date: String, hour: String, spark: SparkSession) = {
