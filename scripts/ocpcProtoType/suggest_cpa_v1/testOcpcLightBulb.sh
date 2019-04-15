39bac3bea68fa60a61208c1e17222654a8a9d4f6
wangjun <wangjun02@qutoutiao.net>
2019-04-15 14:09:25 +0800

    fix bug

--- scripts/ocpcProtoType/suggest_cpa_v1/testOcpcLightBulb.sh
+++ scripts/ocpcProtoType/suggest_cpa_v1/testOcpcLightBulb.sh
@@ -22,5 +22,5 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.yarn.executor.memoryOverhead=4g'\
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
-    --class com.cpc.spark.OcpcPrototype.suggest_cpa_v1.OcpcLightBulb \
+    --class com.cpc.spark.OcpcProtoType.suggest_cpa_v1.OcpcLightBulb \
     /home/cpc/wangjun/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $1 $2 $3 $4
