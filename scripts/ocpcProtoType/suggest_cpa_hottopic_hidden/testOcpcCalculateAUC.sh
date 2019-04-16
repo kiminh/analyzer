5b1e81e3c87700091680bd8a0b2af13e0f5f9dc0
wangjun <wangjun02@qutoutiao.net>
2019-04-16 11:21:25 +0800

    fix bug

---
+++ scripts/ocpcProtoType/suggest_cpa_hottopic_hidden/testOcpcCalculateAUC.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cur=/data/cpc/anal
+SPARK_HOME=/usr/lib/spark-current
+queue=root.cpc.develop
+
+jars=(
+    "$cur/lib/mysql-connector-java-5.1.41-bin.jar"
+    "$cur/lib/hadoop-lzo-0.4.20.jar"
+    "$cur/lib/config-1.2.1.jar"
+)
+
+$SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
+    --conf 'spark.port.maxRetries=100' \
+    --executor-memory 20g --driver-memory 20g \
+    --executor-cores 10 --num-executors 20  \
+    --conf 'spark.yarn.executor.memoryOverhead=4g'\
+    --conf 'spark.dynamicAllocation.maxExecutors=50'\
+    --jars $( IFS=$','; echo "${jars[*]}" ) \
+    --class com.cpc.spark.OcpcProtoType.suggest_cpa_hottopic_hidden.OcpcCalculateAUC \
+    /home/cpc/wangjun/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $1 $2 $3 $4 hottopic
