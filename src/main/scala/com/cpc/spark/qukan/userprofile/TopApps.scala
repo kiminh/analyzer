51f7340351381d1637708e728f112e075b6eb5fc
zhanghongyang <zhanghongyang@qutoutiao.net>
2019-04-16 20:11:07 +0800

    -

--- src/main/scala/com/cpc/spark/qukan/userprofile/TopApps.scala
+++ src/main/scala/com/cpc/spark/qukan/userprofile/TopApps.scala
@@ -66,7 +66,8 @@ object TopApps {
 
     val b = sendMail(txt, "%s topApps 活跃用户DAU大于2w".format(date), Seq("zhanghongyang@aiclk.com", "dongwei@aiclk.com",
       "zhangting@qutoutiao.net", "huxinjie@aiclk.com", "sujiaqi@qutoutiao.net", "weijinxian@qutoutiao.net",
-      "yishaobin@qutoutiao.net", "mayinbo@qutoutiao.net", "yuxiaoyang@qutoutiao.net", "heting@qutoutiao.net"))
+      "yishaobin@qutoutiao.net", "mayinbo@qutoutiao.net", "yuxiaoyang@qutoutiao.net", "heting@qutoutiao.net",
+      "yuyao02@qutoutiao.net"))
     //val b = sendMail(txt, "%s topApps 活跃用户数top100".format(date), Seq("zhanghongyang@aiclk.com"))
     if (!b) {
       println("发送邮件失败")
