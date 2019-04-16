7d5627eb7868db9614f2af6732df7d7df30b58a1
wangjun <wangjun02@qutoutiao.net>
2019-04-16 15:30:33 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/data/OcpcUnitList.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/data/OcpcUnitList.scala
@@ -16,18 +16,18 @@ object OcpcUnitList {
     val hour = args(1).toString
 
     // 链接adv数据库
-    val cpaGiven = getCPAgiven(date, hour, spark)
-//    cpaGiven.write.mode("overwrite").saveAsTable("test.ocpc_cpa_given_hourly")
-    cpaGiven
-      .repartition(10).write.mode("overwrite").insertInto("dl_cpc.ocpc_cpa_given_hourly")
+    val unitList = getOcpcUnit(date, hour, spark)
+    unitList.write.mode("overwrite").saveAsTable("test.ocpc_unit_list_hourly")
+//    unitList
+//      .repartition(10).write.mode("overwrite").insertInto("dl_cpc.ocpc_cpa_given_hourly")
   }
 
-  def getCPAgiven(date: String, hour: String, spark: SparkSession) = {
+  def getOcpcUnit(date: String, hour: String, spark: SparkSession) = {
     val url = "jdbc:mysql://rr-2zehhy0xn8833n2u5.mysql.rds.aliyuncs.com:3306/adv?useUnicode=true&characterEncoding=utf-8"
     val user = "adv_live_read"
     val passwd = "seJzIPUc7xU"
     val driver = "com.mysql.jdbc.Driver"
-    val table = "(select id, user_id, ideas, bid, ocpc_bid, ocpc_bid_update_time, cast(conversion_goal as char) as conversion_goal, status from adv.unit where is_ocpc=1 and ideas is not null) as tmp"
+    val table = "(select id, user_id, ideas, bid, ocpc_bid, ocpc_bid_update_time, cast(conversion_goal as char) as conversion_goal, DATE(last_ocpc_opentime) as ocpc_last_open_date, HOUR(last_ocpc_opentime) as ocpc_last_open_hour, status from adv.unit where is_ocpc=1 and ideas is not null) as tmp"
 
     val data = spark.read.format("jdbc")
       .option("url", url)
@@ -40,30 +40,26 @@ object OcpcUnitList {
     val base = data
       .withColumn("unitid", col("id"))
       .withColumn("userid", col("user_id"))
-      .select("unitid", "userid", "ideas", "bid", "ocpc_bid", "ocpc_bid_update_time", "conversion_goal", "status")
+      .select("unitid", "userid", "bid", "ocpc_bid", "ocpc_bid_update_time", "conversion_goal", "ocpc_last_open_date", "ocpc_last_open_hour", "status")
 
-
-    val ideaTable = base
-      .withColumn("ideaid", explode(split(col("ideas"), "[,]")))
-      .select("unitid", "userid", "ideaid", "ocpc_bid", "ocpc_bid_update_time", "conversion_goal", "status")
-
-    ideaTable.createOrReplaceTempView("ideaid_update_time")
+    base.createOrReplaceTempView("base_data")
 
     val sqlRequest =
       s"""
          |SELECT
          |    unitid,
          |    userid,
-         |    ideaid,
          |    ocpc_bid as cpa_given,
          |    cast(conversion_goal as int) as conversion_goal,
          |    ocpc_bid_update_time as update_timestamp,
          |    from_unixtime(ocpc_bid_update_time) as update_time,
          |    from_unixtime(ocpc_bid_update_time, 'yyyy-MM-dd') as update_date,
          |    from_unixtime(ocpc_bid_update_time, 'HH') as update_hour,
+         |    ocpc_last_open_date,
+         |    ocpc_last_open_hour,
          |    status
          |FROM
-         |    ideaid_update_time
+         |    base_data
        """.stripMargin
 
     println(sqlRequest)
