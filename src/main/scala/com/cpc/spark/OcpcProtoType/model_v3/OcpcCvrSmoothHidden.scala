f867d982237986c1770dd5708b988c3500463f83
wangjun <wangjun02@qutoutiao.net>
2019-04-18 00:17:17 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/model_v3/OcpcCvrSmoothHidden.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/model_v3/OcpcCvrSmoothHidden.scala
@@ -65,8 +65,8 @@ object OcpcCvrSmoothHidden {
       .withColumn("version", lit(version))
 
     resultDF
-//      .repartition(10).write.mode("overwrite").saveAsTable("test.ocpc_post_cvr_unitid_hourly20190304")
-      .repartition(10).write.mode("overwrite").insertInto("dl_cpc.ocpc_post_cvr_unitid_hourly")
+      .repartition(10).write.mode("overwrite").saveAsTable("test.ocpc_post_cvr_unitid_hourly20190304")
+//      .repartition(10).write.mode("overwrite").insertInto("dl_cpc.ocpc_post_cvr_unitid_hourly")
 
 
     savePbPack(resultDF, fileName)
