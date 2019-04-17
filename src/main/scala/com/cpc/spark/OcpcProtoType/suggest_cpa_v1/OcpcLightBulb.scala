06478a58dc5dc438408bcbe134c09398f67d402c
wangjun <wangjun02@qutoutiao.net>
2019-04-17 15:00:42 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/suggest_cpa_v1/OcpcLightBulb.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/suggest_cpa_v1/OcpcLightBulb.scala
@@ -188,40 +188,40 @@ object OcpcLightBulb{
     println(s"host: $host")
     println(s"port: $port")
 
-    for (record <- data.collect()) {
-      val identifier = record.getAs[Int]("unitid").toString
-      val valueDouble = record.getAs[Double]("cpa")
-      var key = "new_algorithm_unit_ocpc_" + identifier
-      if (valueDouble >= 0) {
-        var valueString = valueDouble.toString
-        if (valueString == "0.0") {
-          valueString = "0"
-        }
-        println(s"key:$key, value:$valueString")
-      }
-    }
-
-
-//    data.foreachPartition(iterator => {
-//      val redis = new RedisClient(host, port)
-//      redis.auth(auth)
-//      iterator.foreach{
-//        record => {
-//          val identifier = record.getAs[Int]("unitid").toString
-//          val valueDouble = record.getAs[Double]("cpa")
-//          var key = "new_algorithm_unit_ocpc_" + identifier
-//          if (valueDouble >= 0) {
-//            var valueString = valueDouble.toString
-//            if (valueString == "0.0") {
-//              valueString = "0"
-//            }
-//            println(s"key:$key, value:$valueString")
-//            redis.setex(key, 7 * 24 * 60 * 60, valueString)
-//          }
+//    for (record <- data.collect()) {
+//      val identifier = record.getAs[Int]("unitid").toString
+//      val valueDouble = record.getAs[Double]("cpa")
+//      var key = "new_algorithm_unit_ocpc_" + identifier
+//      if (valueDouble >= 0) {
+//        var valueString = valueDouble.toString
+//        if (valueString == "0.0") {
+//          valueString = "0"
 //        }
+//        println(s"key:$key, value:$valueString")
 //      }
-//      redis.disconnect
-//    })
+//    }
+
+
+    data.foreachPartition(iterator => {
+      val redis = new RedisClient(host, port)
+      redis.auth(auth)
+      iterator.foreach{
+        record => {
+          val identifier = record.getAs[Int]("unitid").toString
+          val valueDouble = record.getAs[Double]("cpa")
+          var key = "new_algorithm_unit_ocpc_" + identifier
+          if (valueDouble >= 0) {
+            var valueString = valueDouble.toString
+            if (valueString == "0.0") {
+              valueString = "0"
+            }
+            println(s"key:$key, value:$valueString")
+            redis.setex(key, 7 * 24 * 60 * 60, valueString)
+          }
+        }
+      }
+      redis.disconnect
+    })
   }
 
   def getRecommendationAd(version: String, date: String, hour: String, spark: SparkSession) = {
