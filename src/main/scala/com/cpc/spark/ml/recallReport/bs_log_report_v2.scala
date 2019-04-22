78d260a128ba15dbc86487af0caab1b330e7e8aa
wangyao <wangyao@qutoutiao.net>
2019-04-22 18:53:50 +0800

    -

--- src/main/scala/com/cpc/spark/ml/recallReport/bs_log_report_v2.scala
+++ src/main/scala/com/cpc/spark/ml/recallReport/bs_log_report_v2.scala
@@ -144,13 +144,12 @@ object bs_log_report_v2 {
       "groups_hit_ad_slot_type_ids","groups_hit_media_class_ids", "groups_hit_regional_ids","groups_hit_user_level_ids",
       "groups_hit_phone_level_ids","groups_hit_os_type_ids", "groups_hit_black_install_pkg_ids","groups_hit_white_install_pkg_ids",
       "groups_hit_content_category_ids", "groups_hit_new_user_ids","groups_hit_acc_user_type_ids")
-      .filter("exptags like '%%bsfilterdetail%%'")
     pbData.createOrReplaceTempView("temp_table")
     println(excp.value)
     val insertIntoTable =
       s"""
          |insert overwrite table dl_cpc.recall_filter_number_report_v2 partition (`date`='$tardate')
-         |select * from temp_table
+         |select * from temp_table where exptags like '%%bsfilterdetail%%'
       """.stripMargin
     spark.sql(insertIntoTable)
 //    pbData.repartition(1000).write.mode("overwrite").insertInto("dl_cpc.recall_filter_number_report_v2")
