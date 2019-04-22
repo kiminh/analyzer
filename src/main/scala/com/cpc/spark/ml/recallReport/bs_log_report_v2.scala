2e3b75ae98083c4af50d80b761af6436cf38437a
wangyao <wangyao@qutoutiao.net>
2019-04-22 20:32:18 +0800

    -

--- src/main/scala/com/cpc/spark/ml/recallReport/bs_log_report_v2.scala
+++ src/main/scala/com/cpc/spark/ml/recallReport/bs_log_report_v2.scala
@@ -126,7 +126,7 @@ object bs_log_report_v2 {
       "groups_hit_ad_slot_type_ids","groups_hit_media_class_ids", "groups_hit_regional_ids","groups_hit_user_level_ids",
       "groups_hit_phone_level_ids","groups_hit_os_type_ids", "groups_hit_black_install_pkg_ids","groups_hit_white_install_pkg_ids",
       "groups_hit_content_category_ids", "groups_hit_new_user_ids","groups_hit_acc_user_type_ids")
-      .filter("exptags like '%%bsfilterdetail%%'")
+      .where("exptags like '%%bsfilterdetail%%'")
       .withColumn("`date`",lit(s"$tardate"))
     pbData.write.mode("overwrite").insertInto("dl_cpc.recall_filter_number_report_v2")
   }
