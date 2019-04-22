b6f86b93198044b60b503c74b0af85b84c3f22df
wangyao <wangyao@qutoutiao.net>
2019-04-22 14:52:49 +0800

    -

--- src/main/scala/com/cpc/spark/ml/recallReport/bs_log_report_v2.scala
+++ src/main/scala/com/cpc/spark/ml/recallReport/bs_log_report_v2.scala
@@ -6,7 +6,7 @@ import bslog.Bslog.NoticeLogBody
 import com.cpc.spark.streaming.tools.Encoding
 import org.apache.spark.sql.SparkSession
 import scala.collection.JavaConverters._
-import collection.mutable._
+import org.apache.spark.sql.functions._
 
 object bs_log_report_v2 {
   def main(args: Array[String]): Unit = {
@@ -147,6 +147,7 @@ object bs_log_report_v2 {
       "groups_hit_ad_slot_type_ids","groups_hit_media_class_ids", "groups_hit_regional_ids","groups_hit_user_level_ids",
       "groups_hit_phone_level_ids","groups_hit_os_type_ids", "groups_hit_black_install_pkg_ids","groups_hit_white_install_pkg_ids",
       "groups_hit_content_category_ids", "groups_hit_new_user_ids","groups_hit_acc_user_type_ids")
+        .withColumn("`date`",lit(s"$tardate"))
 //    pbData.createOrReplaceTempView("temp_table")
 //    println(excp.value)
 //    val insertIntoTable =
@@ -155,6 +156,7 @@ object bs_log_report_v2 {
 //         |select * from temp_table
 //      """.stripMargin
 //    spark.sql(insertIntoTable)
+//    pbData.repartition(1000).write.mode("overwrite").saveAsTable("dl_cpc.recall_filter_number_report_v2")
     pbData.repartition(1000).write.mode("overwrite").saveAsTable("test.bslog")
   }
   case class BsLog1(
