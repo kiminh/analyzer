1f66b790d42eeae9985ea36d1a8ff81a75d15cfa
wangjun <wangjun02@qutoutiao.net>
2019-04-22 20:32:18 +0800

    fix bug

---
+++ src/main/scala/com/cpc/spark/OcpcProtoType/report/OcpcHourlyGeneralData.scala
@@ -0,0 +1,42 @@
+package com.cpc.spark.OcpcProtoType.report
+
+import com.cpc.spark.tools.OperateMySQL
+import com.typesafe.config.ConfigFactory
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+
+object OcpcHourlyGeneralData {
+  def main(args: Array[String]): Unit = {
+    /*
+    新版报表程序
+    1. 从ocpc_unionlog拉取ocpc广告记录
+    2. 采用数据关联方式获取转化数据
+    3. 统计分ideaid级别相关数据
+    4. 统计分conversion_goal级别相关数据
+    5. 存储到hdfs
+    6. 存储到mysql
+     */
+    val spark = SparkSession
+      .builder()
+      .appName("OcpcHourlyReport")
+      .enableHiveSupport()
+      .getOrCreate()
+
+    val date = args(0).toString
+    val hour = args(1).toString
+    val version = args(2).toString
+    val media = args(3).toString
+    println("parameters:")
+    println(s"date=$date, hour=$hour, version=$version, media=$media")
+
+
+
+
+
+
+  }
+
+
+
+}
