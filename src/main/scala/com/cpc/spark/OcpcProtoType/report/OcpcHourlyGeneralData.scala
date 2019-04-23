f097a756d65b366ac844ae1d613ed99912455a98
wangjun <wangjun02@qutoutiao.net>
2019-04-23 22:22:39 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/report/OcpcHourlyGeneralData.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/report/OcpcHourlyGeneralData.scala
@@ -86,7 +86,7 @@ object OcpcHourlyGeneralData {
         .withColumn("cost_cmp", when(col("cost_yesterday") === 0.0, 1.0).otherwise((col("cost") * 100.0 - col("cost_yesterday")) / col("cost_yesterday")))
     val result = result2
 
-//    result.show(10)
+    result.show(10)
 
     val resultDF = result
       .withColumn("cost", col("ocpc_cost") * 0.01)
