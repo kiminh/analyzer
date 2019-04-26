75343e52157a08ef5b5366dce5f86c43bb509c33
wangyao <wangyao@qutoutiao.net>
2019-04-26 17:45:02 +0800

    -

--- src/main/scala/com/cpc/spark/novel/toutiao/DspTitleSample.scala
+++ src/main/scala/com/cpc/spark/novel/toutiao/DspTitleSample.scala
@@ -35,50 +35,60 @@ object DspTitleSample {
       p => avgs.foreach(p.println) // avgs.foreach(p.println)
     }
 
-//    //穿山甲dsp
-//    val sql2 =
-//      s"""
-//         |SELECT
-//         |distinct
-//         |adid, title
-//         |FROM dl_cpc.slim_union_log
-//         |WHERE dt> date_sub('$date', 7) and adid != '' and adsrc = 22
-//         |and media_appsid in ("80001098", "80001292")
-//         |and title != ''
-//             """.stripMargin
-//    println(sql2)
-//    val data2 = spark.sql(sql2)
-//    data2.repartition(1).write.mode("overwrite").format("com.databricks.spark.csv")
-//      .option("delimiter","\001").save("hdfs://emr-cluster/home/cpc/dsp_title/dsp_toutiao_sample.csv")
-//    data2.repartition(1).write.mode("overwrite").saveAsTable("dl_cpc.midu_toutiao_sample2")
-//
-//    //title label
-//    val sql3 =
-//      s"""
-//         |select
-//         |  title,
-//         |  case when cate_1='美容化妆' then '0'
-//         |  when cate_2='游戏类'  then '1'
-//         |  when cate_2='社交网络' then '2'
-//         |  when cate_2='网上购物' then '3'
-//         |  when cate_1='二类电商' then '4'
-//         |  when cate_1='成人用品' then '5'
-//         |  when cate_2='生活服务' then '6'
-//         |  when cate_2='短视频' then '7'
-//         |  else '8' end as label
-//         |from dl_cpc.midu_toutiao_title
-//             """.stripMargin
-//    println(sql3)
-//    val data3 = spark.sql(sql3)
-//    data3.repartition(1).write.mode("overwrite").format("com.databricks.spark.csv")
-//      .option("delimiter","\001").save("hdfs://emr-cluster/home/cpc/dsp_title/title_label.csv")
+    //穿山甲dsp
+    val sql2 =
+      s"""
+         |SELECT
+         |distinct
+         |adid, title
+         |FROM dl_cpc.slim_union_log
+         |WHERE dt> date_sub('$date', 7) and adid != '' and adsrc = 22
+         |and media_appsid in ("80001098", "80001292")
+         |and title != ''
+             """.stripMargin
+    println(sql2)
+    val data2 = spark.sql(sql2)
+    val avgs2 = data2.rdd
+      .map( t=>
+        t(0).toString()+"\001"+t(1).toString())
+      .collect()
+
+    printToFile(new File("/home/cpc/dsp_title/dsp_toutiao_sample.csv")) {
+      p => avgs2.foreach(p.println) // avgs.foreach(p.println)
+    }
+    //title label
+    val sql3 =
+      s"""
+         |select
+         |  title,
+         |  case when cate_1='美容化妆' then '0'
+         |  when cate_2='游戏类'  then '1'
+         |  when cate_2='社交网络' then '2'
+         |  when cate_2='网上购物' then '3'
+         |  when cate_1='二类电商' then '4'
+         |  when cate_1='成人用品' then '5'
+         |  when cate_2='生活服务' then '6'
+         |  when cate_2='短视频' then '7'
+         |  else '8' end as label
+         |from dl_cpc.midu_toutiao_title
+             """.stripMargin
+    println(sql3)
+    val data3 = spark.sql(sql3)
+    val avgs3 = data3.rdd
+      .map( t=>
+        t(0).toString()+"\001"+t(1).toString())
+      .collect()
+
+    printToFile(new File("/home/cpc/dsp_title/title_label.csv")) {
+      p => avgs.foreach(p.println) // avgs.foreach(p.println)
+    }
   }
 
   def printToFile(f: java.io.File)(op: java.io.PrintWriter => Unit)
   {
     val p = new java.io.PrintWriter(f);
-    p.write("asin,")
-    p.write("rating_avg\n")
+//    p.write("asin,")
+//    p.write("rating_avg\n")
     try { op(p) }
     finally { p.close() }
   }
