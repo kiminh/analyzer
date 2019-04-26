018a7ab6cecba04388083d8593d62fd290f2f031
wangyao <wangyao@qutoutiao.net>
2019-04-26 15:44:50 +0800

    -

--- src/main/scala/com/cpc/spark/novel/toutiao/DspTitleSample.scala
+++ src/main/scala/com/cpc/spark/novel/toutiao/DspTitleSample.scala
@@ -1,6 +1,7 @@
 package com.cpc.spark.novel.toutiao
 
 import org.apache.spark.sql.SparkSession
+import java.io._
 
 object DspTitleSample {
   def main(args: Array[String]): Unit = {
@@ -25,8 +26,10 @@ object DspTitleSample {
              """.stripMargin
     println(sql)
     val data = spark.sql(sql)
-    data.repartition(1).write.mode("overwrite").format("com.databricks.spark.csv")
-      .option("delimiter","\001").save("hdfs://emr-cluster/home/cpc/dsp_title/midu_toutiao_sample.csv")
+    val avgs = data.rdd.map(x=>(x(0),x(1))).collect()
+    printToFile(new File("/home/cpc/dsp_title/midu_toutiao_sample.csv")) {
+      p => avgs.foreach(p.println)
+    }
     data.repartition(1).write.mode("overwrite").saveAsTable("dl_cpc.midu_toutiao_sample")
 
     //穿山甲dsp
@@ -42,8 +45,42 @@ object DspTitleSample {
              """.stripMargin
     println(sql2)
     val data2 = spark.sql(sql2)
-    data2.repartition(1).write.mode("overwrite").format("com.databricks.spark.csv")
-      .option("delimiter","\001").save("hdfs://emr-cluster/home/cpc/dsp_title/dsp_toutiao_sample.csv")
+    val avgs2 = data.rdd.map(x=>(x(0),x(1))).collect()
+    printToFile(new File("/home/cpc/dsp_title/midu_toutiao_sample.csv")) {
+      p => avgs2.foreach(p.println)
+    }
     data2.repartition(1).write.mode("overwrite").saveAsTable("dl_cpc.midu_toutiao_sample2")
+
+    //title label
+    val sql3 =
+      s"""
+         |select
+         |  title,
+         |  case when cate_1='美容化妆' then '0'
+         |  when cate_2='游戏类'  then '1'
+         |  when cate_2='社交网络' then '2'
+         |  when cate_2='网上购物' then '3'
+         |  when cate_1='二类电商' then '4'
+         |  when cate_1='成人用品' then '5'
+         |  when cate_2='生活服务' then '6'
+         |  when cate_2='短视频' then '7'
+         |  else '8' end as label
+         |from dl_cpc.midu_toutiao_title
+             """.stripMargin
+    println(sql3)
+    val data3 = spark.sql(sql3)
+    val avgs3 = data.rdd.map(x=>(x(0),x(1))).collect()
+    printToFile(new File("/home/cpc/dsp_title/midu_toutiao_sample.csv")) {
+      p => avgs3.foreach(p.println)
+    }
+  }
+
+  def printToFile(f: java.io.File)(op: java.io.PrintWriter => Unit)
+  {
+    val p = new java.io.PrintWriter(f);
+    p.write("asin,")
+    p.write("rating_avg\n")
+    try { op(p) }
+    finally { p.close() }
   }
 }
