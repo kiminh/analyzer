241f6b89026b1b914ff6f613f4419303c68e9504
dongjinbao <dongjinbao@qutoutiao.net>
2019-04-27 22:33:31 +0800

    recover

--- src/main/scala/com/cpc/spark/OcpcProtoType/suggest_cpa_qtt/OcpcCalculateAUC.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/suggest_cpa_qtt/OcpcCalculateAUC.scala
@@ -48,7 +48,7 @@ object OcpcCalculateAUC {
     val finalTableName = "test.ocpc_unitid_auc_daily_" + conversionGoal
     resultDF
       .repartition(10).write.mode("overwrite").insertInto("dl_cpc.ocpc_unitid_auc_daily")
-    //        .write.mode("overwrite").saveAsTable(finalTableName)
+//        .write.mode("overwrite").saveAsTable(finalTableName)
   }
 
   def getIndustry(date: String, hour: String, spark: SparkSession) = {
