f8cce6f90d625537dc9680e4933a5933cbed25e4
wangjun <wangjun02@qutoutiao.net>
2019-04-29 21:44:29 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/report/OcpcDailyFunnelIndustry.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/report/OcpcDailyFunnelIndustry.scala
@@ -188,7 +188,7 @@ object OcpcDailyFunnelIndustry {
       .sql(sqlRequestCtr)
       .filter(s"conversion_goal in (2, 3)")
 
-    ctrBaseData.repartition(50).write.mode("overwrite").saveAsTable("test.check_data_ocpc20190429")
+//    ctrBaseData.repartition(50).write.mode("overwrite").saveAsTable("test.check_data_ocpc20190429")
 
     // cvr2Data
     val sqlRequest2 =
