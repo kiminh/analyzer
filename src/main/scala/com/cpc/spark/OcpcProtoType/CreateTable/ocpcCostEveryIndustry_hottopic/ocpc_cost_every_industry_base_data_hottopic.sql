272f7afea409f718e93f2c29b935dcb80420713e
wentao <wentao@qutoutiao.net>
2019-04-30 16:32:14 +0800

    ocpc cost of hottopic

--- src/main/scala/com/cpc/spark/OcpcProtoType/CreateTable/ocpcCostEveryIndustry_hottopic/ocpc_cost_every_industry_base_data_hottopic.sql
+++ src/main/scala/com/cpc/spark/OcpcProtoType/CreateTable/ocpcCostEveryIndustry_hottopic/ocpc_cost_every_industry_base_data_hottopic.sql
@@ -2,7 +2,7 @@ drop table if exists dl_cpc.ocpc_cost_every_industry_base_data_hottopic;
 create table if not exists dl_cpc.ocpc_cost_every_industry_base_data_hottopic(
     dt                  string,
     unitid              int,
-    is_ocpc             int,
+    ocpc_log            string,
     industry            string,
     siteid              int,
     is_api_callback     int,
