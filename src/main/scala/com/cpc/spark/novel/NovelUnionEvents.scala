a4ee44f08ab73e31aa72b8324a8531ec45743a93
wangyao <wangyao@qutoutiao.net>
2019-04-30 11:00:42 +0800

    -

--- src/main/scala/com/cpc/spark/novel/NovelUnionEvents.scala
+++ src/main/scala/com/cpc/spark/novel/NovelUnionEvents.scala
@@ -43,9 +43,9 @@ object NovelUnionEvents {
             println(minute)
             spark.sql(
                 s"""
-                   |ALTER TABLE dl_cpc.cpc_basedata_union_events
+                   |ALTER TABLE dl_cpc.cpc_novel_union_events
                    | add if not exists PARTITION(`day` = "$date", `hour` = "$hour",`minute`="$minute")
-                   | LOCATION 'hdfs://emr-cluster/warehouse/dl_cpc.db/cpc_basedata_union_events/day=$date/hour=$hour/minute=$minute'
+                   | LOCATION 'hdfs://emr-cluster/warehouse/dl_cpc.db/cpc_novel_union_events/day=$date/hour=$hour/minute=$minute'
           """.stripMargin.trim)
             i=i+1
         }
