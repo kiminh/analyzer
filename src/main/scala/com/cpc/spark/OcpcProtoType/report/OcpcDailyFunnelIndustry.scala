df34cfb3efb2577864d8ae71f695fc8896229ef3
wangjun <wangjun02@qutoutiao.net>
2019-04-30 13:38:24 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/report/OcpcDailyFunnelIndustry.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/report/OcpcDailyFunnelIndustry.scala
@@ -44,7 +44,8 @@ object OcpcDailyFunnelIndustry {
 
     result2
       .repartition(1)
-      .write.mode("overwrite").saveAsTable("test.ocpc_funnel_ideaid_cnt_daily")
+      .write.mode("overwrite").saveAsTable("dl_cpc.ocpc_funnel_ideaid_cnt_daily")
+//      .write.mode("overwrite").saveAsTable("test.ocpc_funnel_ideaid_cnt_daily")
 
 
   }
