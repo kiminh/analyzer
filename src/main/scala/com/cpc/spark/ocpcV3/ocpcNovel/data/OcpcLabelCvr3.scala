6fb8c56c95eee79e1a378c3a15a8cf0cf342729f
wangyao <wangyao@qutoutiao.net>
2019-05-07 17:22:09 +0800

    -

--- src/main/scala/com/cpc/spark/ocpcV3/ocpcNovel/data/OcpcLabelCvr3.scala
+++ src/main/scala/com/cpc/spark/ocpcV3/ocpcNovel/data/OcpcLabelCvr3.scala
@@ -16,9 +16,10 @@ object OcpcLabelCvr3 {
     val hour = args(1).toString
 
     val result1 = getLabelFromAdv(date, hour, spark)
-    result1
-      .repartition(10).write.mode("overwrite").insertInto("dl_cpc.ocpcv3_cvr3_data_hourly")
-    println("successfully save data into table: dl_cpc.ocpcv3_cvr3_data_hourly")
+    result1.show(10)
+    result1.write.mode("overwrite").saveAsTable("test.wy00")
+//      .repartition(10).write.mode("overwrite").insertInto("dl_cpc.ocpcv3_cvr2_data_hourly")
+//    println("successfully save data into table: dl_cpc.ocpcv3_cvr2_data_hourly")
 
   }
 
@@ -66,65 +67,6 @@ object OcpcLabelCvr3 {
     val tmpDateValue = tmpDate.split(" ")
     val date1 = tmpDateValue(0)
     val hour1 = tmpDateValue(1)
-    val selectCondition = getTimeRangeSql4(date, hour, date1, hour1)
-
-    val sqlRequest2 =
-      s"""
-         |SELECT
-         |  searchid,
-         |  1 as label
-         |FROM
-         |  dl_cpc.site_form_unionlog
-         |WHERE
-         |  $selectCondition
-       """.stripMargin
-    println(sqlRequest2)
-    val labelData = spark.sql(sqlRequest2).distinct()
-
-    val resultDF = rawData
-      .join(labelData, Seq("searchid"), "left_outer")
-      .groupBy("ideaid", "unitid", "adclass", "media_appsid")
-      .agg(sum(col("label")).alias("cvr3_cnt"))
-      .select("ideaid", "unitid", "adclass", "media_appsid", "cvr3_cnt")
-      .withColumn("date", lit(date))
-      .withColumn("hour", lit(hour))
-
-    resultDF.show(10)
-    resultDF.printSchema()
-
-    resultDF
-  }
-
-  def getLabel(date: String, hour: String, spark: SparkSession) = {
-    var selectWhere = s"`date`='$date' and hour = '$hour'"
-
-    var sqlRequest1 =
-      s"""
-         |select
-         |    searchid,
-         |    uid,
-         |    ideaid,
-         |    unitid,
-         |    price,
-         |    bid,
-         |    userid,
-         |    media_appsid,
-         |    adclass,
-         |    isclick,
-         |    isshow
-         |from dl_cpc.cpc_basedata_union_events
-         |where day='$date' and hour = '$hour'
-         |and isclick is not null
-         |and media_appsid in ("80001098","80001292","80000001", "80000002")
-         |and isshow = 1
-         |and ideaid > 0
-         |and adsrc = 1
-         |and adslot_type in (1,2,3)
-         |and is_api_callback!=1
-      """.stripMargin
-    println(sqlRequest1)
-    val rawData = spark.sql(sqlRequest1)
-    rawData.createOrReplaceTempView("raw_table")
 
     val sqlRequest2 =
       s"""
@@ -151,11 +93,9 @@ object OcpcLabelCvr3 {
       .withColumn("date", lit(date))
       .withColumn("hour", lit(hour))
 
-
     resultDF.show(10)
     resultDF.printSchema()
 
     resultDF
   }
-
 }
