6770c21c60a2b717a41813124d600937821b4c84
wangjun <wangjun02@qutoutiao.net>
2019-05-13 21:21:31 +0800

    fix bug

--- src/main/scala/com/cpc/spark/ecpc/v1/eCPCforElds.scala
+++ src/main/scala/com/cpc/spark/ecpc/v1/eCPCforElds.scala
@@ -29,7 +29,7 @@ object eCPCforElds {
     val media = args(3).toString
     val hourInt = args(4).toInt
     val highBidFactor = args(5).toDouble
-    val fileName = "test.pb"
+    val fileName = "adclass_ecpc.pb"
 
     println("parameters:")
     println(s"date=$date, hour=$hour, version:$version, media:$media, hourInt:$hourInt")
