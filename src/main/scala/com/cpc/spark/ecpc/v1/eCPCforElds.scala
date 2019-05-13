7dfb9a4418e567d73fefa82f4665f6042b592666
wangjun <wangjun02@qutoutiao.net>
2019-05-13 17:56:20 +0800

    fix bug

--- src/main/scala/com/cpc/spark/ecpc/v1/eCPCforElds.scala
+++ src/main/scala/com/cpc/spark/ecpc/v1/eCPCforElds.scala
@@ -12,7 +12,6 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 object eCPCforElds {
   def main(args: Array[String]): Unit = {
     /*
-
     计算新版的cvr平滑策略：
     1. 抽取基础数据
     2. 计算该维度下pcoc与计费比、后验cvr等等指标
@@ -23,22 +22,66 @@ object eCPCforElds {
 
     val date = args(0).toString
     val hour = args(1).toString
-    val media = args(2).toString
-    val version = args(3).toString
-    val expTag = args(4).toString
-    val highBidFactor = args(5).toDouble
-    val hourInt = args(6).toInt
-    val conversionGoal = args(7).toInt
+    val version = args(2).toString
+    val media = args(3).toString
+    val hourInt = args(4).toInt
 
     println("parameters:")
-    println(s"date=$date, hour=$hour, media:$media, version:$version, expTag:$expTag, highBidFactor:$highBidFactor, hourInt:$hourInt")
+    println(s"date=$date, hour=$hour, version:$version, media:$media, hourInt:$hourInt")
 
     // 抽取基础数据
-    val cvrType = "cvr" + conversionGoal.toString
-
+    val baseData = getBaseData(media, hourInt, date, hour, spark)
 
   }
 
+  def getBaseData(media: String, hourInt: Int, date: String, hour: String, spark: SparkSession) = {
+    // 抽取媒体id
+    val conf = ConfigFactory.load("ocpc")
+    val conf_key = "medias." + media + ".media_selection"
+    val mediaSelection = conf.getString(conf_key)
+
+    // 取历史数据
+    val dateConverter = new SimpleDateFormat("yyyy-MM-dd HH")
+    val newDate = date + " " + hour
+    val today = dateConverter.parse(newDate)
+    val calendar = Calendar.getInstance
+    calendar.setTime(today)
+    calendar.add(Calendar.HOUR, -hourInt)
+    val yesterday = calendar.getTime
+    val tmpDate = dateConverter.format(yesterday)
+    val tmpDateValue = tmpDate.split(" ")
+    val date1 = tmpDateValue(0)
+    val hour1 = tmpDateValue(1)
+    val selectCondition = getTimeRangeSql2(date1, hour1, date, hour)
+
+    val sqlRequest =
+      s"""
+         |SELECT
+         |  searchid,
+         |  ideaid,
+         |  unitid,
+         |  adslotid as slotid,
+         |  adslot_type as slottype,
+         |  adtype,
+         |  bid_discounted_by_ad_slot as bid,
+         |  price,
+         |  exp_cvr,
+         |  isclick,
+         |  isshow
+         |FROM
+         |  dl_cpc.ocpc_base_unionlog
+         |WHERE
+         |  $selectCondition
+         |AND
+         |  $mediaSelection
+         |AND
+         |  price <= bid_discounted_by_ad_slot
+       """.stripMargin
+    println(sqlRequest)
+    val data = spark.sql(sqlRequest)
+
+    data
+  }
 
 
 }
