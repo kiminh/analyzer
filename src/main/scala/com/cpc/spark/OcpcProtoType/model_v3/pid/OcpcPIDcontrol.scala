1da673b056aeca0cb0385769db2d7bcf794ae12a
wangjun <wangjun02@qutoutiao.net>
2019-05-14 16:21:24 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/model_v3/pid/OcpcPIDcontrol.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/model_v3/pid/OcpcPIDcontrol.scala
@@ -18,16 +18,190 @@ object OcpcPIDcontrol {
     val hour = args(1).toString
     val media = args(2).toString
     val version = args(3).toString
-    val sampleHour = args(4).toInt
-    val kp = args(5).toDouble
-    val ki = args(6).toDouble
-    val kd = args(7).toDouble
+    val conversionGoal = args(4).toInt
+    val sampleHour = args(5).toInt
+    val minCV = args(6).toInt
+    val kp = args(7).toDouble
+    val ki = args(8).toDouble
+    val kd = args(9).toDouble
 
-    val baseData = getBaseData(media, sampleHour, date, hour, spark)
+    val baseData = getBaseData(media, sampleHour, conversionGoal, date, hour, spark)
+    val errorData = calculateError(baseData, minCV, date, hour, spark)
+    val prevError = getPrevData(1, conversionGoal, version, date, hour, spark)
+
+    val data = errorData
+      .join(prevError, Seq("unitid"), "left_outer")
+      .select("unitid", "current_error", "prev_error", "last_error")
+
+    val result = calculatePID(data, kp, ki, kd, date, hour, spark)
+
+    val resultDF = result
+      .select("unitid", "current_error", "prev_error", "last_error", "kp", "ki", "kd", "increment")
+      .withColumn("conversion_goal", lit(conversionGoal))
+      .withColumn("date", lit(date))
+      .withColumn("hour", lit(hour))
+      .withColumn("version", lit(version))
+
+    resultDF
+//      .repartition(5).write.mode("overwrite").saveAsTable("test.ocpc_pid_error_data_hourly")
+      .repartition(5).write.mode("overwrite").insertInto("dl_cpc.ocpc_pid_error_data_hourly")
   }
 
-  def getBaseData(media: String, sampleHour: Int, date: String, hour: String, spark: SparkSession) = {
-    
+  def calculatePID(baseData: DataFrame, kp: Double, ki: Double, kd: Double, date: String, hour: String, spark: SparkSession) = {
+    val result = baseData
+      .filter(s"current_error is not null and prev_error is not null and last_error is not null")
+      .withColumn("increment", udfCalculatePID(kp, ki, kd)(col("current_error"), col("prev_error"), col("last_error")))
+      .withColumn("kp", lit(kp))
+      .withColumn("ki", lit(ki))
+      .withColumn("kd", lit(kd))
+      .select("unitid", "current_error", "prev_error", "last_error", "kp", "ki", "kd", "increment")
+
+    result
+  }
+
+  def udfCalculatePID(kp: Double, ki: Double, kd: Double) = udf((currentError: Double, prevError: Double, lastError: Double) => {
+    val result = kp * currentError - ki * prevError + kd * lastError
+    result
+  })
+
+  def getPrevData(hourInt: Int, conversionGoal: Int, version: String, date: String, hour: String, spark: SparkSession) = {
+    // 取历史数据
+    val dateConverter = new SimpleDateFormat("yyyy-MM-dd HH")
+    val newDate = date + " " + hour
+    val today = dateConverter.parse(newDate)
+    val calendar = Calendar.getInstance
+    calendar.setTime(today)
+    calendar.add(Calendar.HOUR, -hourInt)
+    val yesterday = calendar.getTime
+    val tmpDate = dateConverter.format(yesterday)
+    val tmpDateValue = tmpDate.split(" ")
+    val date1 = tmpDateValue(0)
+    val hour1 = tmpDateValue(1)
+
+    // 抽取数据
+    val sqlRequest1 =
+      s"""
+         |SELECT
+         |  unitid,
+         |  current_error as error1,
+         |  prev_error as error2
+         |FROM
+         |  dl_cpc.ocpc_pid_error_data_hourly
+         |WHERE
+         |  `date` = '$date1'
+         |AND
+         |  `hour` = '$hour1'
+         |AND
+         |  version = '$version'
+         |AND
+         |  conversion_goal = $conversionGoal
+       """.stripMargin
+    println(sqlRequest1)
+    val data = spark
+        .sql(sqlRequest1)
+        .withColumn("prev_error", col("error1"))
+        .withColumn("last_error", col("error2"))
+
+    data
+  }
+
+  def calculateError(baseData: DataFrame, minCV: Int, date: String, hour: String, spark: SparkSession) = {
+    baseData.createOrReplaceTempView("base_data")
+    val sqlRequest1 =
+      s"""
+         |SELECT
+         |  unitid,
+         |  sum(case when isclick=1 then price else 0 end) * 1.0 / sum(iscvr) as cpareal,
+         |  sum(case when isclick=1 then cpagiven else 0 end) * 1.0 / sum(isclick) as cpagiven,
+         |  sum(iscvr) as cv
+         |FROM
+         |  base_data
+         |GROUP BY unitid
+       """.stripMargin
+    val data = spark.sql(sqlRequest1)
+
+    val currentError = data
+      .filter(s"cv >= $minCV")
+      .select("unitid", "cpareal", "cpagiven", "cv")
+      .withColumn("current_error", col("cpagiven") - col("cpareal"))
+
+    currentError
+  }
+
+
+  def getBaseData(media: String, sampleHour: Int, conversionGoal: Int, date: String, hour: String, spark: SparkSession) = {
+    // 抽取媒体id
+    val conf = ConfigFactory.load("ocpc")
+    val conf_key = "medias." + media + ".media_selection"
+    val mediaSelection = conf.getString(conf_key)
+
+    // 取历史数据
+    val dateConverter = new SimpleDateFormat("yyyy-MM-dd HH")
+    val newDate = date + " " + hour
+    val today = dateConverter.parse(newDate)
+    val calendar = Calendar.getInstance
+    calendar.setTime(today)
+    calendar.add(Calendar.HOUR, -sampleHour)
+    val yesterday = calendar.getTime
+    val tmpDate = dateConverter.format(yesterday)
+    val tmpDateValue = tmpDate.split(" ")
+    val date1 = tmpDateValue(0)
+    val hour1 = tmpDateValue(1)
+    val selectCondition = getTimeRangeSql2(date1, hour1, date, hour)
+
+    // 点击数据
+    val sqlRequest1 =
+      s"""
+         |SELECT
+         |  searchid,
+         |  unitid,
+         |  adclass,
+         |  isshow,
+         |  isclick,
+         |  price,
+         |  cast(ocpc_log_dict['cpagiven'] as double) as cpagiven,
+         |  cast(ocpc_log_dict['IsHiddenOcpc'] as int) as is_hidden,
+         |  conversion_goal,
+         |  hour
+         |FROM
+         |  dl_cpc.ocpc_filter_unionlog
+         |WHERE
+         |  $selectCondition
+         |AND
+         |  $mediaSelection
+         |AND
+         |  is_ocpc = 1
+         |AND
+         |  conversion_goal = $conversionGoal
+       """.stripMargin
+    println(sqlRequest1)
+    val clickData = spark
+      .sql(sqlRequest1)
+      .filter("is_hidden = 0")
+
+    // 转化数据
+    val cvrType = "cvr" + conversionGoal.toString
+    val sqlRequest2 =
+      s"""
+         |SELECT
+         |  searchid,
+         |  label as iscvr
+         |FROM
+         |  dl_cpc.ocpc_label_cvr_hourly
+         |WHERE
+         |  `date` >= '$date1'
+         |AND
+         |  cvr_goal = '$cvrType'
+       """.stripMargin
+    println(sqlRequest2)
+    val cvData = spark.sql(sqlRequest2)
+
+    // 数据关联
+    val data = clickData
+      .join(cvData, Seq("searchid"), "left_outer")
+      .na.fill(0, Seq("iscvr"))
+
+    data
   }
 }
 
