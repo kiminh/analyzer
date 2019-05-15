5b87620c44e0b6cb3817950efed4b99c004d8df1
wangyao <wangyao@qutoutiao.net>
2019-05-15 16:10:02 +0800

    -

--- src/main/scala/com/cpc/spark/ml/calibration/CalibrationCheckOnMidu.scala
+++ src/main/scala/com/cpc/spark/ml/calibration/CalibrationCheckOnMidu.scala
@@ -70,7 +70,7 @@ object CalibrationCheckOnMidu {
     log.write.mode("overwrite").saveAsTable("test.wy00")
 
     val data = log.filter("length(group)>0")
-        .filter("len=40")
+        .filter("len=38")
     println("calibration data:%d".format(data.count()))
     var uncalibrated = 0
     data.rdd.toLocalIterator.foreach( x => {
