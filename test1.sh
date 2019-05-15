a2fc369da47cf3b6d18b2e81a238053d62a2b396
wangyao <wangyao@qutoutiao.net>
2019-05-15 18:50:58 +0800

    -

--- test1.sh
+++ test1.sh
@@ -25,5 +25,5 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.sql.shuffle.partitions=2000'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
     --conf "spark.sql.shuffle.partitions=1000" \
-    --class com.cpc.spark.novel.UnitDeliveryType\
-    /home/cpc/wy/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar 2019-05-14 10
+    --class com.cpc.spark.ml.calibration.MultiDimensionCalibOnQttV2\
+    /home/cpc/wy/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar 2019-05-15 15 1 1
