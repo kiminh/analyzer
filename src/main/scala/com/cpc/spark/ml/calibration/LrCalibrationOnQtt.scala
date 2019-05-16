9de66c4efcbd7716d49bab938c38383a150d90af
wangyao <wangyao@qutoutiao.net>
2019-05-16 20:22:09 +0800

    -

--- src/main/scala/com/cpc/spark/ml/calibration/LrCalibrationOnQtt.scala
+++ src/main/scala/com/cpc/spark/ml/calibration/LrCalibrationOnQtt.scala
@@ -81,7 +81,7 @@ object LrCalibrationOnQtt {
 
     val adslotid_sum = adslotidID.size
     val ideaid_sum = ideaidID.size
-    val profile_num = adslotid_sum + ideaid_sum + 1
+    val profile_num = adslotid_sum + ideaid_sum
 
     val sample = log.rdd.map {
       r =>
@@ -131,6 +131,6 @@ object LrCalibrationOnQtt {
       val new_els: Seq[(Int, Double)] = els.map(x => {
         (x.getInt(0), x.getDouble(1))
       })
-      Vectors.sparse(profile_num, new_els)
+      Vectors.sparse(profile_num - 1, new_els)
   }
 }
