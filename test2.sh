8795f12939d12fd12a58ad8e196ab7d5c96b15b9
wangyao <wangyao@qutoutiao.net>
2019-05-21 16:24:25 +0800

    -

--- test2.sh
+++ test2.sh
@@ -27,4 +27,4 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --jars $( IFS=$','; echo "${jars[*]}" ) \
     --conf "spark.sql.shuffle.partitions=1000" \
     --class com.cpc.spark.ml.calibration.HourlyCalibrationOnMiduCvr \
-    /home/cpc/wy/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar 2019-05-21 10 24 0
+    /home/cpc/wy/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar 2019-05-21 10 6 0
