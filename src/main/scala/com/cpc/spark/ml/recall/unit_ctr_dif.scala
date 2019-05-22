5bc7e0706e8a1a130d41276daeda084d476224a5
段广东 <duanguangdong@qutoutiao.net>
2019-05-22 18:14:10 +0800

    update

--- src/main/scala/com/cpc/spark/ml/recall/unit_ctr_dif.scala
+++ src/main/scala/com/cpc/spark/ml/recall/unit_ctr_dif.scala
@@ -3,9 +3,11 @@ package com.cpc.spark.ml.recall
 import java.text.SimpleDateFormat
 import java.util.Calendar
 
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 
 object unit_ctr_dif {
+  Logger.getRootLogger.setLevel(Level.WARN)
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
       .appName("unit ctr dif")
