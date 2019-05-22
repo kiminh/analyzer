5314243457a9eedb98f0a4a71d2aecf4af6ae860
wangyao <wangyao@qutoutiao.net>
2019-05-22 19:43:05 +0800

    -

--- src/main/scala/com/cpc/spark/ml/calibration/LrCalibrationOnQtt.scala
+++ src/main/scala/com/cpc/spark/ml/calibration/LrCalibrationOnQtt.scala
@@ -119,7 +119,7 @@ object LrCalibrationOnQtt {
                  |select isclick, raw_ctr, adslotid, ideaid,user_req_ad_num,exp_ctr
                  | from dl_cpc.slim_union_log
                  | where dt = '2019-05-19'
-                 | and media_appsid in ('80001098', '80001292') and isshow = 1
+                 | and media_appsid in ('80000001', '80000002') and isshow = 1 and adslot_type = 1
                  | and ctr_model_name in ('$calimodel') and hour = '22'
                  | and ideaid > 0 and adsrc = 1 AND userid > 0
                  | AND (charge_type IS NULL OR charge_type = 1)
