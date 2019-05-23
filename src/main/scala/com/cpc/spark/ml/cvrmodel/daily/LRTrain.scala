e0f2cabcc906eeaa4ede2c17b941ec8cb7fe707c
樊一鸣 <fanyiming@qutoutiao.net>
2019-05-23 16:44:56 +0800

    cvr parser5.

--- src/main/scala/com/cpc/spark/ml/cvrmodel/daily/LRTrain.scala
+++ src/main/scala/com/cpc/spark/ml/cvrmodel/daily/LRTrain.scala
@@ -3,6 +3,7 @@ package com.cpc.spark.ml.cvrmodel.daily
 import java.text.SimpleDateFormat
 import java.util.{Calendar, Date}
 
+import com.cpc.spark.ml.ctrmodel.hourly.LRTrain.dict
 import org.apache.spark.sql.SaveMode
 
 import scala.collection.immutable
@@ -22,9 +23,9 @@ import scala.util.Random
 
 /**
   * Created by zhaolei on 22/12/2017.
-  * new owner: fym (190511).
+  * new owner: fym (190520).
   */
-object TrainLRCVR {
+object LRTrain {
 
   private var trainLog = Seq[String]()
   private val model = new LRIRModel
@@ -46,7 +47,7 @@ object TrainLRCVR {
     Logger.getRootLogger.setLevel(Level.WARN)
 
     val spark: SparkSession = model
-      .initSpark("[cpc-model] linear regression cvr v4")
+      .initSpark("[cpc-model] qtt-bs-cvrparser4-daily")
     // 按分区取数据
     val ctrPathSep = getPathSeq(args(0).toInt)
     //    val cvrPathSep = getPathSeq(args(1).toInt)
@@ -154,7 +155,7 @@ object TrainLRCVR {
     println("write complete")
     model.clearResult()
     val allData = spark.sqlContext.read.parquet(dfPath)
-    train(spark, "ctrparser4", "qtt-bs-cvrparser4-daily", allData, "qtt-bs-cvrparser4-daily.lrm", 1e8, args(1).toString)
+    train(spark, "ctrparser5", "qtt-bs-cvrparser4-daily", allData, "qtt-bs-cvrparser4-daily.lrm", 1e8, args(1).toString)
     allData.unpersist()
     println(trainLog.mkString("\n"))
     println("train complete")
@@ -390,16 +391,8 @@ object TrainLRCVR {
           p.map {
             u =>
               val vec = parser match {
-                case "parser1" =>
-                  getVectorParser1(u)
-                case "parser2" =>
-                  getVectorParser2(u)
-                case "parser3" =>
-                  getVectorParser3(u)
-                case "ctrparser2" =>
-                  getCtrVectorParser2(u)
-                case "ctrparser3" =>
-                  getCtrVectorParser3(u)
+                case "ctrparser5" =>
+                  getCtrVectorParser5(u)
                 case "ctrparser4" =>
                   getCtrVectorParser4(u)
               }
@@ -1115,6 +1108,167 @@ object TrainLRCVR {
   }
 
   def getCtrVectorParser4(x: Row): Vector = {
+
+    val cal = Calendar.getInstance()
+    cal.setTimeInMillis(x.getAs[Int]("timestamp") * 1000L)
+    val week = cal.get(Calendar.DAY_OF_WEEK) //1 to 7
+    val hour = cal.get(Calendar.HOUR_OF_DAY)
+    var els = Seq[(Int, Double)]()
+    var i = 0
+
+    els = els :+ (week + i - 1, 1d)
+    i += 7
+
+    //(24)
+    els = els :+ (hour + i, 1d)
+    i += 24
+
+    //sex
+    els = els :+ (x.getAs[Int]("sex") + i, 1d)
+    i += 9
+
+    //age
+    els = els :+ (x.getAs[Int]("age") + i, 1d)
+    i += 100
+
+    //os 96 - 97 (2)
+    els = els :+ (x.getAs[Int]("os") + i, 1d)
+    i += 10
+
+    //isp
+    els = els :+ (x.getAs[Int]("isp") + i, 1d)
+    i += 20
+
+    //net
+    els = els :+ (x.getAs[Int]("network") + i, 1d)
+    i += 10
+
+    els = els :+ (dict("cityid").getOrElse(x.getAs[Int]("city"), 0) + i, 1d)
+    i += dict("cityid").size + 1
+
+    //media id
+    els = els :+ (dict("mediaid").getOrElse(x.getAs[String]("media_appsid").toInt, 0) + i, 1d)
+    i += dict("mediaid").size + 1
+
+    //ad slot id
+    els = els :+ (dict("slotid").getOrElse(x.getAs[String]("adslotid").toInt, 0) + i, 1d)
+    i += dict("slotid").size + 1
+
+    //0 to 4
+    els = els :+ (x.getAs[Int]("phone_level") + i, 1d)
+    i += 10
+
+    //pagenum
+    var pnum = x.getAs[Int]("pagenum")
+    if (pnum < 0 || pnum > 50) {
+      pnum = 0
+    }
+    els = els :+ (pnum + i, 1d)
+    i += 100
+
+    //bookid
+    var bid = 0
+    try {
+      bid = x.getAs[String]("bookid").toInt
+    } catch {
+      case e: Exception =>
+    }
+    if (bid < 0 || bid > 50) {
+      bid = 0
+    }
+    els = els :+ (bid + i, 1d)
+    i += 100
+
+    //ad class
+    val adcls = dict("adclass").getOrElse(x.getAs[Int]("adclass"), 0)
+    els = els :+ (adcls + i, 1d)
+    i += dict("adclass").size + 1
+
+    //adtype
+    els = els :+ (x.getAs[Int]("adtype") + i, 1d)
+    i += 10
+
+    //adslot_type
+    els = els :+ (x.getAs[Int]("adslot_type") + i, 1d)
+    i += 10
+
+    //planid
+    els = els :+ (dict("planid").getOrElse(x.getAs[Int]("planid"), 0) + i, 1d)
+    i += dict("planid").size + 1
+
+    //unitid
+    els = els :+ (dict("unitid").getOrElse(x.getAs[Int]("unitid"), 0) + i, 1d)
+    i += dict("unitid").size + 1
+
+    //ideaid
+    els = els :+ (dict("ideaid").getOrElse(x.getAs[Int]("ideaid"), 0) + i, 1d)
+    i += dict("ideaid").size + 1
+
+    //user_req_ad_num
+    /*var uran_idx = 0
+    val uran = x.getAs[Int]("user_req_ad_num")
+    if (uran >= 1 && uran <= 10) {
+      uran_idx = uran
+    }
+    if (uran > 10) {
+      uran_idx = 11
+    }
+    els = els :+ (uran_idx + i, 1d)
+    i += 12 + 1
+
+    //user_req_num
+    var urn_idx = 0
+    val urn = x.getAs[Int]("user_req_num")
+    if (urn >= 1 && urn <= 10) {
+      urn_idx = 1
+    } else if (urn > 10 && urn <= 100) {
+      urn_idx = 2
+    } else if (urn > 100 && urn <= 1000) {
+      urn_idx = 3
+    } else if (urn > 1000) {
+      urn_idx = 4
+    }
+    els = els :+ (urn_idx + i, 1d)
+    i += 5 + 1
+
+    var user_click = x.getAs[Int]("user_click_num")
+    if (user_click >= 5) {
+      user_click = 5
+    }
+    els = els :+ (user_click + i, 1d)
+    i += 6
+
+    var user_click_unit = x.getAs[Int]("user_click_unit_num")
+    if (user_click_unit >= 5) {
+      user_click_unit = 5
+    }
+    els = els :+ (user_click + i, 1d)
+    i += 6
+
+    //sex - age
+    if (x.getAs[Int]("sex") > 0 && x.getAs[Int]("age") > 0) {
+      els = els :+ (6 * (x.getAs[Int]("sex") - 1) + x.getAs[Int]("age") + i, 1d)
+    }
+    i += 2 * 6 + 1*/
+
+    //user installed app
+    /*val appIdx = x.getAs[WrappedArray[Int]]("appIdx")
+    if (appIdx != null) {
+      val inxList = appIdx.map(p => (p + i, 1d))
+      els = els ++ inxList
+    }
+    i += 1000 + 1*/
+
+    try {
+      Vectors.sparse(i, els)
+    } catch {
+      case e: Exception =>
+        throw new Exception(els.toString + " " + i.toString + " " + e.getMessage)
+        null
+    }
+  }
+
+  def getCtrVectorParser5(x: Row): Vector = {
 
     val cal = Calendar.getInstance()
     cal.setTimeInMillis(x.getAs[Int]("timestamp") * 1000L)
