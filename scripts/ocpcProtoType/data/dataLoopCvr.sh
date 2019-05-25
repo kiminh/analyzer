dfd1e25dc4beb64ee01fa25c7fd8d030b5ba3129
wangjun <wangjun02@qutoutiao.net>
2019-05-25 12:08:01 +0800

    fix bug

---
+++ scripts/ocpcProtoType/data/dataLoopCvr.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# sh dataLoop.sh testOcpcSampleHourlyV1.sh 2018-10-27 0 23
+
+date=$1
+h=$2
+while [ ${h} -le $3 ]; do
+    if ((${h}<10))
+    then
+        hour="0${h}"
+    else
+        hour="${h}"
+    fi
+
+    echo $date
+    echo $hour
+    sh testOcpcConversion.sh $date $hour
+#    sh $1 $2 $hour
+
+    let h=h+1
+done
