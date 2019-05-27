1a2a1da9d7d1d8f5419aff1967e5e455b5d9a849
wangyao <wangyao@qutoutiao.net>
2019-05-27 15:49:59 +0800

    -

--- src/main/scala/com/cpc/spark/OcpcProtoType/suggest_cpa_novel/OcpcLightBulbV2.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/suggest_cpa_novel/OcpcLightBulbV2.scala
@@ -158,7 +158,7 @@ object OcpcLightBulbV2{
          |AND
          |    $mediaSelection
          |AND
-         |    is_ocpc = 1
+         |    ocpc_status = 1
          |AND
          |    isclick = 1
        """.stripMargin
