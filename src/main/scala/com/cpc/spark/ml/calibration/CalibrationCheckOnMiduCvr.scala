96c761ef40f4be6187aeca2f12bd4b19bef617c1
wangyao <wangyao@qutoutiao.net>
2019-05-28 13:29:24 +0800

    -

--- src/main/scala/com/cpc/spark/ml/calibration/CalibrationCheckOnMiduCvr.scala
+++ src/main/scala/com/cpc/spark/ml/calibration/CalibrationCheckOnMiduCvr.scala
@@ -38,7 +38,7 @@ object CalibrationCheckOnMiduCvr {
 
     // get union log
     val sql = s"""
-                 |select iscvr as isclick, cast(raw_cvr as bigint) as ectr, cvr_model_name, adslotid as adslot_id, cast(ideaid as string) ideaid,
+                 |select iscvr as isclick, raw_ctr, cast(exp_ctr as bigint) as ectr, cvr_model_name, adslotid as adslot_id, cast(ideaid as string) ideaid,
                  |case when user_req_ad_num = 1 then '1'
                  |  when user_req_ad_num = 2 then '2'
                  |  when user_req_ad_num in (3,4) then '4'
