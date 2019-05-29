a83619add52a66742b569728fb7c3ae19ed80067
wangjun <wangjun02@qutoutiao.net>
2019-05-29 22:28:54 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/model_qtt/OcpcGetPbV2.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/model_qtt/OcpcGetPbV2.scala
@@ -58,82 +58,10 @@ object OcpcGetPbV2 {
         .withColumn("version", lit(finalVersion))
 
     resultDF
-      .repartition(10).write.mode("overwrite").saveAsTable("test.ocpc_pcoc_jfb_hourly")
-//      .repartition(10).write.mode("overwrite").insertInto("dl_cpc.ocpc_pcoc_jfb_hourly")
+//      .repartition(10).write.mode("overwrite").saveAsTable("test.ocpc_pcoc_jfb_hourly")
+      .repartition(10).write.mode("overwrite").insertInto("dl_cpc.ocpc_pcoc_jfb_hourly")
 
   }
-
-//  def getPbByConversion(conversionGoal: Int, version: String, hourInt1: Int, hourInt2: Int, date: String, hour: String, spark: SparkSession) = {
-//    /*
-//    计算步骤
-//    1. 获取主校准的数据
-//    2. 获取备用校准的数据
-//    3. 更新数据：主校准优先级更高
-//     */
-//    // 主校准模型
-//    val dataRaw1 = getCalibrationData(conversionGoal, version, hourInt1, date, hour, spark)
-//    val data1 = dataRaw1
-//      .withColumn("pcoc1", col("pcoc"))
-//      .withColumn("jfb1", col("jfb"))
-//      .withColumn("post_cvr1", col("post_cvr"))
-//      .withColumn("flag", lit(1))
-//      .select("identifier", "pcoc1", "jfb1", "post_cvr1", "flag")
-//
-//    // 备用校准模型
-//    val dataRaw2 = getCalibrationData(conversionGoal, version, hourInt2, date, hour, spark)
-//    val data2 = dataRaw2
-//      .withColumn("pcoc2", col("pcoc"))
-//      .withColumn("jfb2", col("jfb"))
-//      .withColumn("post_cvr2", col("post_cvr"))
-//      .select("identifier", "pcoc2", "jfb2", "post_cvr2")
-//
-//    // 数据表关联
-//    val data = data2
-//      .join(data1, Seq("identifier"), "left_outer")
-//      .na.fill(0, Seq("flag"))
-//      .withColumn("pcoc", when(col("flag") === 1, col("pcoc1")).otherwise(col("pcoc2")))
-//      .withColumn("jfb", when(col("flag") === 1, col("jfb1")).otherwise(col("jfb2")))
-//      .withColumn("post_cvr", when(col("flag") === 1, col("post_cvr1")).otherwise(col("post_cvr2")))
-//
-//    data.show()
-////    data
-////        .write.mode("overwrite").saveAsTable("test.check_ocpc_pb_data20190529")
-//
-//    data
-//
-//  }
-//
-//  def udfSelectData() = udf((flag: Int, value1: Double, value2: Double) => {
-//    var result = value1
-//  })
-//
-//  def getCalibrationData(conversionGoal: Int, version: String, hourInt: Int, date: String, hour: String, spark: SparkSession) = {
-//    val finalVersion = version + hourInt.toString
-//
-//    val sqlRequest =
-//      s"""
-//         |SELECT
-//         |  identifier,
-//         |  pcoc,
-//         |  jfb,
-//         |  post_cvr
-//         |FROM
-//         |  dl_cpc.ocpc_pcoc_jfb_hourly
-//         |WHERE
-//         |  `date` = '$date'
-//         |AND
-//         |  `hour` = '$hour'
-//         |AND
-//         |  conversion_goal = $conversionGoal
-//         |AND
-//         |  version = '$finalVersion'
-//       """.stripMargin
-//    println(sqlRequest)
-//    val data = spark.sql(sqlRequest)
-//
-//    data
-//
-//  }
 
 }
 
