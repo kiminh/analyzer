3851e3c81b45e70357bf31bd6bcb6e17d763d09b
xiongyao <xiongyao@qutoutiao.net>
2019-05-29 21:52:24 +0800

    poi feature

--- src/main/scala/com/cpc/spark/ml/recall/ReporTagByIndustry.scala
+++ src/main/scala/com/cpc/spark/ml/recall/ReporTagByIndustry.scala
@@ -79,9 +79,8 @@ object ReporTagByIndustry {
         |	b.category,
         | b.name,
         |	a.tag
-      """.stripMargin).repartition(100).write.mode(SaveMode.Append).jdbc(tagReport2dbUrl, "report2.cpc_profiletag_report_v1", tagReport2dbProp)
-
-
+      """.stripMargin).repartition(100).filter{x=> { x.getAs("class_id")==132101100}}.show(100,false)
+//      write.mode(SaveMode.Append).jdbc(tagReport2dbUrl, "report2.cpc_profiletag_report_v1", tagReport2dbProp)
 
   }
 
