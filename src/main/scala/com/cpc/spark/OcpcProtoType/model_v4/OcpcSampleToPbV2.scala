24794c7d8cb48f1b7dd43e1fec4e4ac943e74c2a
wangjun <wangjun02@qutoutiao.net>
2019-05-30 12:26:25 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/model_v4/OcpcSampleToPbV2.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/model_v4/OcpcSampleToPbV2.scala
@@ -51,8 +51,8 @@ object OcpcSampleToPbV2 {
       .withColumn("date", lit(date))
       .withColumn("hour", lit(hour))
       .withColumn("version", lit(version))
-      .repartition(5).write.mode("overwrite").saveAsTable("test.ocpc_kvalue_smooth_strat")
-//      .repartition(5).write.mode("overwrite").insertInto("dl_cpc.ocpc_kvalue_smooth_strat")
+//      .repartition(5).write.mode("overwrite").saveAsTable("test.ocpc_kvalue_smooth_strat")
+      .repartition(5).write.mode("overwrite").insertInto("dl_cpc.ocpc_kvalue_smooth_strat")
 //
 //    val resultDF = result
 //      .select("identifier", "conversion_goal", "cpagiven", "cvrcnt", "kvalue")
