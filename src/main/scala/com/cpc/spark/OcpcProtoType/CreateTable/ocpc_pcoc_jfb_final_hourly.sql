9db072d4e5ac4a36194a460d8977d76edc149ab0
wangjun <wangjun02@qutoutiao.net>
2019-05-30 11:42:39 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/CreateTable/ocpc_pcoc_jfb_final_hourly.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists dl_cpc.ocpc_pcoc_jfb_final_hourly(
-    identifier          string,
-    pcoc                double,
-    jfb                 double,
-    post_cvr            double,
-    conversion_goal     int
-)
-partitioned by (`date` string, `hour` string, version String)
-stored as parquet;
