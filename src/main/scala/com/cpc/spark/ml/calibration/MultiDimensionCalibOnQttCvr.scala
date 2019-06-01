53c25b9a296783f94c45c02facea6ddbf3492520
wangyao <wangyao@qutoutiao.net>
2019-06-01 17:20:14 +0800

    -

--- src/main/scala/com/cpc/spark/ml/calibration/MultiDimensionCalibOnQttCvr.scala
+++ src/main/scala/com/cpc/spark/ml/calibration/MultiDimensionCalibOnQttCvr.scala
@@ -3,12 +3,16 @@ package com.cpc.spark.ml.calibration
 import java.io.{File, FileOutputStream, PrintWriter}
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+
 import com.cpc.spark.OcpcProtoType.model_novel_v3.OcpcSuggestCPAV3.matchcvr
 import com.cpc.spark.common.Utils
+import com.cpc.spark.ml.calibration.HourlyCalibration._
+import com.cpc.spark.ml.common.{Utils => MUtils}
 import com.cpc.spark.ocpc.OcpcUtils.getTimeRangeSql4
 import com.typesafe.config.ConfigFactory
 import com.cpc.spark.ml.calibration.MultiDimensionCalibOnQttV2._
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 
 object MultiDimensionCalibOnQttCvr {
--- src/main/scala/com/cpc/spark/ml/calibration/MultiDimensionCalibOnQttV2.scala
+++ src/main/scala/com/cpc/spark/ml/calibration/MultiDimensionCalibOnQttV2.scala
@@ -111,21 +111,21 @@ object MultiDimensionCalibOnQttV2 {
     }
   }
 
-  def GroupToConfig(data:DataFrame, session: SparkSession, calimodel: String, minBinSize: Int = MIN_BIN_SIZE,
+  def GroupToConfig(data:DataFrame, session: SparkSession, model: String, minBinSize: Int = MIN_BIN_SIZE,
                     maxBinCount : Int = MAX_BIN_COUNT, minBinCount: Int = 2): scala.collection.mutable.Map[String,CalibrationConfig] = {
     val irTrainer = new IsotonicRegression()
     val sc = session.sparkContext
     var calimap = scala.collection.mutable.Map[String,CalibrationConfig]()
-    val result = data.select("user_req_ad_num","adslot_id","ideaid","isclick","ectr","ctr_model_name","group")
+    val result = data.select("isclick","ectr","model","group")
       .rdd.map( x => {
       var isClick = 0d
-      if (x.get(3) != null) {
-        isClick = x.getLong(3).toDouble
+      if (x.get(0) != null) {
+        isClick = x.getInt(0).toDouble
       }
-      val ectr = x.getLong(4).toDouble / 1e6d
-      val model = x.getString(5)
-      val group = x.getString(6)
-      val key = calimodel + "_" + group
+      val ectr = x.getLong(1).toDouble / 1e6d
+      val model = x.getString(2)
+      val group = x.getString(3)
+      val key = model + "_" + group
       (key, (ectr, isClick))
     }).groupByKey()
       .mapValues(
