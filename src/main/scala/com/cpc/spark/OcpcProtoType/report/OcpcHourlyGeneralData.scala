ffc35dff68867b8940d4f4298468d3afeffcdd53
wangjun <wangjun02@qutoutiao.net>
2019-06-01 14:30:29 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/report/OcpcHourlyGeneralData.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/report/OcpcHourlyGeneralData.scala
@@ -198,7 +198,7 @@ object OcpcHourlyGeneralData {
       .withColumn("high_cost", col("ocpc_cost") -  col("pred_cost") * 1.2)
       .withColumn("high_cost", when(col("high_cost") <= 0, 0.0).otherwise(col("high_cost")))
 
-//    baseData.write.mode("overwrite").saveAsTable("test.ocpc_general_data_industry20190423a")
+    baseData.write.mode("overwrite").saveAsTable("test.ocpc_general_data_industry20190423a")
 
     baseData.createOrReplaceTempView("base_data")
     val sqlRequest2 =
