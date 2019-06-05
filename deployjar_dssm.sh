ae8ed5a6d67c5e64e9752c5120334fcd38d520c6
齐智 <qizhi@qutoutiao.net>
2019-06-05 12:13:11 +0800

    train count add

--- deployjar_dssm.sh
+++ deployjar_dssm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git checkout master
+#git checkout master
 git pull
 sbt assembly
 echo "hadoop fs -put -f target/scala-2.11/cpc-anal_2.11-0.1.jar hdfs://emr-cluster/warehouse/azkaban/lib/dssm.jar"
