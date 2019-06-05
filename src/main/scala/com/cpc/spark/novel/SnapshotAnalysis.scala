9e189b90e5a090795faa9eecc6189ef5c48f8c5a
wangyao <wangyao@qutoutiao.net>
2019-06-05 15:22:05 +0800

    -

--- src/main/scala/com/cpc/spark/novel/SnapshotAnalysis.scala
+++ src/main/scala/com/cpc/spark/novel/SnapshotAnalysis.scala
@@ -63,6 +63,7 @@ object SnapshotAnalysis {
           }
           (searchid,postcali_value,key,md5)
         }).toDF("searchid","postcali_value","key","md5")
+            .filter("postcali_value=0")
 
         data.show(10)
       data.write.mode("overwrite").saveAsTable("test.wy00")
