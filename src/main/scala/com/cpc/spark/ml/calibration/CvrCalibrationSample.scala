3de7d941dca87075c59dd29da6fd63f766d33483
wangyao <wangyao@qutoutiao.net>
2019-06-11 23:43:23 +0800

    -

--- src/main/scala/com/cpc/spark/ml/calibration/CvrCalibrationSample.scala
+++ src/main/scala/com/cpc/spark/ml/calibration/CvrCalibrationSample.scala
@@ -27,9 +27,15 @@ object CvrCalibrationSample {
                  |select a.*,b.conversion_target[0] as unit_target, if(c.searchid is null,0,1) iscvr
                  |from
                  |(
-                 |  select searchid, raw_cvr, exp_cvr, cvr_model_name, adslotid, ideaid, user_req_ad_num,dt,hour,unitid,adclass
-                 | from dl_cpc.slim_union_log
-                 | where dt = '$date'
+                 |  select searchid, raw_cvr, exp_cvr, cvr_model_name, adslotid, ideaid, user_req_ad_num,dt,hour,unitid,adclass,
+                 |  case
+                 |  when is_ocpc = 1 then 'ocpc'
+                 |  when user_cvr_threshold = 200 then "cvr2"
+                 |  when user_cvr_threshold >0 then "cvr1"
+                 |  else "other"
+                 |  end as exp_cvr_type
+                 | from dl_cpc.cpc_basedata_union_events
+                 | where day = '$date'
                  | and media_appsid in ('80000001', '80000002') and isclick = 1
                  | and cvr_model_name in ('qtt-cvr-dnn-rawid-v1-180','qtt-cvr-dnn-rawid-v1-180-newcali')
                  | and ideaid > 0 and adsrc = 1 AND userid > 0
@@ -42,7 +48,7 @@ object CvrCalibrationSample {
        """.stripMargin
     println(s"sql:\n$sql")
     val sample = spark.sql(sql).filter("unit_target is not null and unit_target not in ('none','site_uncertain')")
-      .select("searchid","raw_cvr","cvr_model_name","adslotid","ideaid","user_req_ad_num","iscvr","exp_cvr","adclass","dt","hour")
+      .select("searchid","raw_cvr","cvr_model_name","adslotid","ideaid","user_req_ad_num","iscvr","exp_cvr","adclass","exp_cvr_type","dt","hour")
 
     sample.show(10)
     sample.repartition(1).write.mode("overwrite").insertInto("dl_cpc.qtt_cvr_calibration_sample")
--- test2.sh
+++ test2.sh
@@ -1,3 +1,4 @@
+sh test1.sh 2019-06-10
 sh test1.sh 2019-06-09
 sh test1.sh 2019-06-08
 sh test1.sh 2019-06-07
