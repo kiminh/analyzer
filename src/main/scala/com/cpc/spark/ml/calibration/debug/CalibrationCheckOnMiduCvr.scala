7ea3f180eec7905dc99d710318905d094b1d09ee
wangyao <wangyao@qutoutiao.net>
2019-06-11 15:38:01 +0800

    -

--- src/main/scala/com/cpc/spark/ml/calibration/debug/CalibrationCheckOnMiduCvr.scala
+++ src/main/scala/com/cpc/spark/ml/calibration/debug/CalibrationCheckOnMiduCvr.scala
@@ -43,25 +43,25 @@ object CalibrationCheckOnMiduCvr {
 
     // get union log
     val sql = s"""
-                 |select iscvr as isclick,searchid, raw_cvr, cast(exp_cvr as bigint) as ecvr, substring(adclass,1,6) as adclass, cvr_model_name, adslotid as adslot_id, cast(ideaid as string) ideaid,
+                 |select postcali_value as ecvr,searchid, raw_cvr, key, adclass, adslotid as adslot_id, ideaid,
                  |case when user_req_ad_num = 1 then '1'
                  |  when user_req_ad_num = 2 then '2'
                  |  when user_req_ad_num in (3,4) then '4'
                  |  when user_req_ad_num in (5,6,7) then '7'
                  |  else '8' end as user_req_ad_num
-                 |  from dl_cpc.qtt_cvr_calibration_sample where dt = '2019-05-30'
+                 |  from test.wy00
        """.stripMargin
     println(s"sql:\n$sql")
-    val log = session.sql(sql).withColumn("group1",concat_ws("_",col("cvr_model_name"),col("adclass"),col("ideaid"),col("user_req_ad_num"),col("adslot_id")))
-      .withColumn("group2",concat_ws("_",col("cvr_model_name"),col("adclass"),col("ideaid"),col("user_req_ad_num")))
-      .withColumn("group3",concat_ws("_",col("cvr_model_name"),col("adclass"),col("ideaid")))
-      .withColumn("group4",concat_ws("_",col("cvr_model_name"),col("adclass")))
-      .withColumn("group",when(searchMap(modelset)(col("group4")),col("group4")).otherwise(concat_ws("_",col("cvr_model_name"),lit("0"))))
+    val log = session.sql(sql).withColumn("group1",concat_ws("_",col("adclass"),col("ideaid"),col("user_req_ad_num"),col("adslot_id")))
+      .withColumn("group2",concat_ws("_",col("adclass"),col("ideaid"),col("user_req_ad_num")))
+      .withColumn("group3",concat_ws("_",col("adclass"),col("ideaid")))
+      .withColumn("group4",concat_ws("_",col("adclass")))
+      .withColumn("group",when(searchMap(modelset)(col("group4")),col("group4")).otherwise(lit("0")))
       .withColumn("group",when(searchMap(modelset)(col("group3")),col("group3")).otherwise(col("group")))
       .withColumn("group",when(searchMap(modelset)(col("group2")),col("group2")).otherwise(col("group")))
       .withColumn("group",when(searchMap(modelset)(col("group1")),col("group1")).otherwise(col("group")))
       .withColumn("len",length(col("group")))
-      .select("isclick","raw_cvr","ecvr","searchid","group","group1","group2","group3","cvr_model_name","adslot_id","ideaid","user_req_ad_num","len")
+      .select("ideaid","raw_cvr","ecvr","searchid","group","group1","group2","group3","adslot_id","user_req_ad_num")
 
     log.show(50)
     println("total data:%d".format(log.count()))
@@ -70,7 +70,6 @@ object CalibrationCheckOnMiduCvr {
     val data = log.filter("length(group)>0")
     println("calibration data:%d".format(data.count()))
     val result = data.rdd.map( x => {
-      val isClick = x.getInt(0).toDouble
       val ectr = x.getLong(1).toDouble / 1e6d
       val onlineCtr = x.getLong(2).toDouble / 1e6d
       val group = x.getString(4)
@@ -80,38 +79,16 @@ object CalibrationCheckOnMiduCvr {
       if (Math.abs(onlineCtr - calibrated) / calibrated > 0.2) {
         mistake = 1
       }
-      (isClick, ectr, calibrated, 1.0, onlineCtr, mistake)
+      (1.0, ectr, calibrated, 1.0, onlineCtr, mistake)
     }).reduce((x, y) => (x._1 + y._1, x._2 + y._2, x._3 + y._3, x._4 + y._4, x._5 + y._5, x._6 + y._6))
-    val ctr = result._1 / result._4
     val ectr = result._2 / result._4
     val calibrated_ctr = result._3 / result._4
     val onlineCtr = result._5 / result._4
     println(s"impression: ${result._4}")
-    println(s"ctr: $ctr")
     println(s"ectr: $ectr")
     println(s"online ctr: $onlineCtr")
     println(s"calibrated_ctr: $calibrated_ctr")
-    println(s"no calibration: ${ectr / ctr}")
-    println(s"online calibration: ${onlineCtr / ctr}")
-    println(s"new calibration: ${calibrated_ctr / ctr}")
 
-    val result2 = data.rdd.map( x => {
-      val isClick = x.getInt(0).toDouble
-      val ectr = x.getLong(1).toDouble / 1e6d
-      val onlineCtr = x.getLong(2).toDouble / 1e6d
-      val group = x.getString(4)
-      val irModel = calimap.get(group).get
-      val calibrated = computeCalibration(ectr, irModel.ir.get)
-      val ideaid = x.getString(10)
-      (ectr,calibrated,ideaid, isClick,onlineCtr)
-    }).toDF("ectr","calibrated","ideaid","isclick","onlinectr")
-
-     val original=result2.selectExpr("cast(isclick as Int) label","cast(ectr* 1e6d as Int) prediction","ideaid")
-    calculateAuc(original,"original",spark)
-    val online=result2.selectExpr("cast(isclick as Int) label","cast(onlinectr* 1e6d as Int) prediction","ideaid")
-    calculateAuc(online,"online",spark)
-    val cali=result2.selectExpr("cast(isclick as Int) label","cast(calibrated * 1e6d as Int) prediction","ideaid")
-    calculateAuc(cali,"postcali",spark)
   }
 
   def searchMap(modelset:Set[String])= udf{
