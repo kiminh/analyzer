e96af04b383fe0a21ad58bac1226ad5ac72d100c
wangyao <wangyao@qutoutiao.net>
2019-06-13 16:51:45 +0800

    -

--- test1.sh
+++ test1.sh
@@ -27,4 +27,4 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --jars $( IFS=$','; echo "${jars[*]}" ) \
     --conf "spark.sql.shuffle.partitions=1000" \
     --class com.cpc.spark.ml.calibration.MultiDimensionCalibOnQttCvr \
-    /home/cpc/wy/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar 2019-06-12 1 1 qtt qtt-cvr-dnn-rawid-v1-180-newcali
+    /home/cpc/wy/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar 2019-06-12 01 1 qtt qtt-cvr-dnn-rawid-v1-180-newcali
