7e2c742e937112b8a0a31e5970f967cb13cd3905
wangjun <wangjun02@qutoutiao.net>
2019-06-13 12:18:59 +0800

    fix bug

--- scripts/ocpcProtoType/model_v5/testOcpcSampleToPbV2.sh
+++ scripts/ocpcProtoType/model_v5/testOcpcSampleToPbV2.sh
@@ -17,7 +17,7 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.yarn.executor.memoryOverhead=4g'\
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
-    --class com.cpc.spark.OcpcProtoType.model_v4.OcpcSampleToPbV2 \
+    --class com.cpc.spark.OcpcProtoType.model_v5.OcpcSampleToPbV2 \
     /home/cpc/wangjun/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $1 $2 $3 1
 
 
