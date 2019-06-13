c2290b280b399ba2d98a80d104acb21f2118da4b
wangjun <wangjun02@qutoutiao.net>
2019-06-13 13:48:31 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/model_v5/OcpcSampleToPbV2.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/model_v5/OcpcSampleToPbV2.scala
@@ -33,18 +33,18 @@ object OcpcSampleToPbV2 {
     val version = args(2).toString
     val fileName = args(3).toString
 
-//    val fileName = "ocpc_params_qtt.pb"
+    //    val fileName = "ocpc_params_qtt.pb"
 
     val data = getCalibrationData(date, hour, version, spark)
 
     data
-        .select("identifier", "conversion_goal", "is_hidden", "exp_tag", "cali_value", "jfb_factor", "post_cvr", "high_bid_factor", "low_bid_factor", "cpa_suggest", "smooth_factor", "cpagiven")
-        .withColumn("date", lit(date))
-        .withColumn("hour", lit(hour))
-        .withColumn("version", lit(version))
-        .repartition(5)
-//        .write.mode("overwrite").saveAsTable("test.ocpc_param_pb_data_hourly")
-        .write.mode("overwrite").insertInto("dl_cpc.ocpc_param_pb_data_hourly_v2")
+      .select("identifier", "conversion_goal", "is_hidden", "exp_tag", "cali_value", "jfb_factor", "post_cvr", "high_bid_factor", "low_bid_factor", "cpa_suggest", "smooth_factor", "cpagiven")
+      .withColumn("date", lit(date))
+      .withColumn("hour", lit(hour))
+      .withColumn("version", lit(version))
+      .repartition(5)
+      //        .write.mode("overwrite").saveAsTable("test.ocpc_param_pb_data_hourly_v2")
+      .write.mode("overwrite").insertInto("dl_cpc.ocpc_param_pb_data_hourly_v2")
 
     savePbPack(data, fileName, spark)
   }
@@ -61,7 +61,7 @@ object OcpcSampleToPbV2 {
          |  1.0 / jfb as jfb_factor,
          |  post_cvr,
          |  high_bid_factor,
-         |  low_bid_factor,
+         |  low_bid_factor
          |  cpagiven
          |FROM
          |  dl_cpc.ocpc_param_calibration_hourly_v2
@@ -81,7 +81,7 @@ object OcpcSampleToPbV2 {
          |SELECT
          |  cast(unitid as string) identifier,
          |  conversion_goal,
-         |  cpa as cpa_suggest
+         |  cpa * 100.0 as cpa_suggest
          |FROM
          |  test.ocpc_qtt_light_control_v2
        """.stripMargin
@@ -101,7 +101,6 @@ object OcpcSampleToPbV2 {
 
     data
   }
-
 
   def udfSelectSmoothFactor() = udf((conversionGoal: Int) => {
     var factor = conversionGoal match {
@@ -200,6 +199,106 @@ object OcpcSampleToPbV2 {
     println("complete save data into protobuffer")
 
   }
+
+  //  def savePbPack(data: DataFrame, fileName: String, spark: SparkSession): Unit = {
+  //    /*
+  //    oCPCQTT&unitid&isHiddenOcpc
+  //    string   key = 1;
+  //    int32    conversionGoal = 2;
+  //    double   cvrCalFactor = 3;
+  //    double   jfbFactor = 4;
+  //    double   smoothFactor = 5;
+  //    double   postCvr = 6;
+  //    double   cpaGiven = 7;
+  //    double   cpaSuggest = 8;
+  //    double   paramT = 9;
+  //    double   highBidFactor = 10;
+  //    double   lowBidFactor = 11;
+  //    int64    ocpcMincpm = 12;
+  //    int64    ocpcMinbid = 13;
+  //    int64    cpcbid = 14;
+  //    int64    maxbid = 15;
+  //     */
+  //    //    val key = "oCPCQtt&270&0"
+  //    //    val conversionGoal = 3
+  //    //    val cvrCalFactor = 0.5
+  //    //    val jfbFactor = 1.1
+  //    //    val smoothFactor = 0.5
+  //    //    val postCvr = 0.032
+  //    //    val cpaGiven = 1000.0
+  //    //    val cpaSuggest = 1200.0
+  //    //    val paramT = 2.0
+  //    //    val highBidFactor = 1.1
+  //    //    val lowBidFactor = 1.0
+  //    //    val minCPM = 0
+  //    //    val minBid = 0
+  //    //    val cpcbid = 12
+  //    //    val maxbid = 0
+  //    var list = new ListBuffer[SingleItem]
+  //    var cnt = 0
+  //
+  //    for (record <- data.collect()) {
+  //      val identifier = record.getAs[String]("identifier")
+  //      val key = "oCPCQtt&" + identifier + "&0"
+  //      val conversionGoal = 3
+  //      val cvrCalFactor = record.getAs[Double]("cali_value")
+  //      val jfbFactor = 1.1
+  //      val smoothFactor = 0.5
+  //      val postCvr = record.getAs[Double]("post_cvr")
+  //      val cpaGiven = 1000.0
+  //      val cpaSuggest = record.getAs[Double]("cpa_suggest")
+  //      val paramT = 2.0
+  //      val highBidFactor = 1.1
+  //      val lowBidFactor = 1.0
+  //      val minCPM = 0
+  //      val minBid = 0
+  //      val cpcbid = 12
+  //      val maxbid = 0
+  //
+  //      if (cnt % 100 == 0) {
+  //        println(s"key:$key, conversionGoal:$conversionGoal, cvrCalFactor:$cvrCalFactor, jfbFactor:$jfbFactor, smoothFactor:$smoothFactor, postCvr:$postCvr, cpaGiven:$cpaGiven, cpaSuggest:$cpaSuggest, paramT:$paramT, highBidFactor:$highBidFactor, lowBidFactor:$lowBidFactor, minCPM:$minCPM, minBid:$minBid, cpcbid:$cpcbid, maxbid:$maxbid")
+  //      }
+  //      cnt += 1
+  //
+  //      //      string identifier = 1;
+  //      //      int32 conversiongoal = 2;
+  //      //      double kvalue = 3;
+  //      //      double cpagiven = 4;
+  //      //      int64 cvrcnt = 5;
+  //
+  //      val currentItem = SingleItem(
+  //        key = key,
+  //        conversionGoal = conversionGoal,
+  //        cvrCalFactor = cvrCalFactor,
+  //        jfbFactor = jfbFactor,
+  //        smoothFactor = smoothFactor,
+  //        postCvr = postCvr,
+  //        cpaGiven = cpaGiven,
+  //        cpaSuggest = cpaSuggest,
+  //        paramT = paramT,
+  //        highBidFactor = highBidFactor,
+  //        lowBidFactor = lowBidFactor,
+  //        ocpcMincpm = minCPM,
+  //        ocpcMinbid = minBid,
+  //        cpcbid = cpcbid,
+  //        maxbid = maxbid
+  //
+  //      )
+  //      list += currentItem
+  //
+  //    }
+  //    val result = list.toArray[SingleItem]
+  //    val adRecordList = OcpcParamsList(
+  //      records = result
+  //    )
+  //
+  //    println("length of the array")
+  //    println(result.length)
+  //    adRecordList.writeTo(new FileOutputStream(fileName))
+  //
+  //    println("complete save data into protobuffer")
+  //
+  //  }
 
 }
 
