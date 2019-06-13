62bf93995f93d11b37263cc4acfe409ec74e5b36
wangyao <wangyao@qutoutiao.net>
2019-06-13 16:47:27 +0800

    -

--- src/main/scala/com/cpc/spark/ml/calibration/MultiDimensionCalibOnQttCvr.scala
+++ src/main/scala/com/cpc/spark/ml/calibration/MultiDimensionCalibOnQttCvr.scala
@@ -107,22 +107,22 @@ object MultiDimensionCalibOnQttCvr {
     log.show(10)
     log.write.mode("overwrite").saveAsTable("test.wy11")
     LogToPb(log, session, calimodel)
-//    val k = log.filter("exp_cvr_type='cvr1'").groupBy().agg(
-//      sum("ectr").alias("ctrnum"),
-//      sum("isclick").alias("clicknum"))
-//      .withColumn("k",col("ctrnum")/col("clicknum")/1e6d)
-//      .first().getAs[Double]("k")
-//    val irModel = IRModel(
-//      boundaries = Seq(0.0,1.0),
-//      predictions = Seq(0.0,k)
-//    )
-//    println(s"k is: $k")
-//    val caliconfig = CalibrationConfig(
-//      name = calimodel,
-//      ir = Option(irModel)
-//    )
-//    val localPath = saveProtoToLocal(calimodel, caliconfig)
-//    saveFlatTextFileForDebug(calimodel, caliconfig)
+    val k = log.filter("exp_cvr_type='cvr1'").groupBy().agg(
+      sum("ectr").alias("ctrnum"),
+      sum("isclick").alias("clicknum"))
+      .withColumn("k",col("ctrnum")/col("clicknum")/1e6d)
+      .first().getAs[Double]("k")
+    val irModel = IRModel(
+      boundaries = Seq(0.0,1.0),
+      predictions = Seq(0.0,1.8)
+    )
+    println(s"k is: $k")
+    val caliconfig = CalibrationConfig(
+      name = calimodel,
+      ir = Option(irModel)
+    )
+    val localPath = saveProtoToLocal(calimodel, caliconfig)
+    saveFlatTextFileForDebug(calimodel, caliconfig)
   }
 
   def LogToPb(log:DataFrame, session: SparkSession, model: String)={
