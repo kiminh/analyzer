e5e522e5661ec74d7cde9b9782fa66e44d553024
wangjun <wangjun02@qutoutiao.net>
2019-06-18 13:58:12 +0800

    fix bug

--- scripts/ocpcProtoType/report_v2/testOcpcHourlyAucReport.sh
+++ scripts/ocpcProtoType/report_v2/testOcpcHourlyAucReport.sh
@@ -17,7 +17,7 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.yarn.executor.memoryOverhead=4g'\
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
-    --class com.cpc.spark.OcpcProtoType.report.OcpcHourlyAucReport \
+    --class com.cpc.spark.OcpcProtoType.report_v2.OcpcHourlyAucReport \
     /home/cpc/wangjun/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $1 $2 $3 $4 $5
 
 
