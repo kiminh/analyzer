1bb2704a79dfe6e283eb75e1298291540f25ecee
wangjun <wangjun02@qutoutiao.net>
2019-06-24 12:04:42 +0800

    fix bug

--- scripts/ocpcProtoType/model_qtt_v2/testOcpcSampleToPbV2.sh
+++ scripts/ocpcProtoType/model_qtt_v2/testOcpcSampleToPbV2.sh
@@ -18,10 +18,10 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
     --class com.cpc.spark.OcpcProtoType.model_qtt_v2.OcpcSampleToPbV2 \
-    /home/cpc/wangjun/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $1 $2 $3 1
+    /home/cpc/wangjun/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $1 $2 $3 $4
 
 
 #val date = args(0).toString
 #val hour = args(1).toString
 #val version = args(2).toString
-#val isKnown = args(3).toInt
+#val fileName = args(3).toString
