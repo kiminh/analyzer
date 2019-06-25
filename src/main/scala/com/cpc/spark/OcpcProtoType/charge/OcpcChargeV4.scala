dd8d25df324272f2156dc17bebcbe790dbc2a4a0
wangjun <wangjun02@qutoutiao.net>
2019-06-25 20:35:14 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/charge/OcpcChargeV4.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/charge/OcpcChargeV4.scala
@@ -185,6 +185,7 @@ object OcpcChargeV4 {
          |  searchid,
          |  unitid,
          |  timestamp,
+         |  from_unixtime(timestamp,'YYYY-MM-dd HH:mm:ss') as unix_time,
          |  date,
          |  hour,
          |  row_number() over(partition by unitid order by timestamp desc) as seq
@@ -195,9 +196,8 @@ object OcpcChargeV4 {
     val costUnits = spark
       .sql(sqlRequest2)
       .filter(s"seq = 1")
-      .select("unitid", "date", "hour", "timestamp")
+      .select("unitid", "date", "hour", "timestamp", "unix_time")
       .withColumn("ocpc_charge_time", concat_ws(" ", col("date"), col("hour")))
-      .withColumn("unix_time", col("timestamp").cast(DateType))
       .select("unitid", "ocpc_charge_time", "unix_time", "date", "hour")
       .cache()
 
