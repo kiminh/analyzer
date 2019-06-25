c2af27d3fb3a479d8647cbfbb5301c036dc485c6
wangjun <wangjun02@qutoutiao.net>
2019-06-25 13:30:27 +0800

    fix bug

--- scripts/ocpcProtoType/data/testOcpcConversionSiteformTemp.sh
+++ scripts/ocpcProtoType/data/testOcpcConversionSiteformTemp.sh
@@ -21,8 +21,9 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
     --class com.cpc.spark.OcpcProtoType.data.OcpcConversionSiteformTemp \
-    /home/cpc/wangjun/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $1 $2
+    /home/cpc/wangjun/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $1 $2 $3
 
 
 #val date = args(0).toString
 #val hour = args(1).toString
+#val cvrType = args(2).toString
