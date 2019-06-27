3489d0a7d73677ccecc4cfeb1564431edd56cf39
wangjun <wangjun02@qutoutiao.net>
2019-06-27 15:59:59 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/model_v5/OcpcPIDerror.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/model_v5/OcpcPIDerror.scala
@@ -30,37 +30,88 @@ object OcpcPIDerror {
 
     val baseData = getBaseData(media, sampleHour, conversionGoal, date, hour, spark)
     val cvrData = calculateCvr(baseData, date, hour, spark)
-//    val errorData = calculateError(baseData, minCV, date, hour, spark)
-//    val prevError = getPrevData(sampleHour, conversionGoal, version, date, hour, spark)
-//
-//    val data = errorData
-//      .join(prevError, Seq("unitid"), "left_outer")
-//      .select("unitid", "current_error", "prev_error", "last_error")
-//
-//    val result = calculatePID(data, kp, ki, kd, date, hour, spark)
-//
-//    val resultDF = result
-//      .select("unitid", "current_error", "prev_error", "last_error", "kp", "ki", "kd", "increment")
-//      .withColumn("conversion_goal", lit(conversionGoal))
-//      .withColumn("date", lit(date))
-//      .withColumn("hour", lit(hour))
-//      .withColumn("version", lit(version))
-//
-//    resultDF
-////      .repartition(5).write.mode("overwrite").saveAsTable("test.ocpc_pid_error_data_hourly")
+    val errorData = calculateError(cvrData, minCV, date, hour, spark)
+    val prevError = getPrevData(sampleHour, conversionGoal, version, date, hour, spark)
+
+    val data = errorData
+      .join(prevError, Seq("unitid"), "left_outer")
+      .select("unitid", "current_error", "prev_error", "last_error")
+
+    val result = calculatePID(data, kp, ki, kd, date, hour, spark)
+
+    val resultDF = result
+      .select("unitid", "current_error", "prev_error", "last_error", "kp", "ki", "kd", "increment")
+      .withColumn("conversion_goal", lit(conversionGoal))
+      .withColumn("date", lit(date))
+      .withColumn("hour", lit(hour))
+      .withColumn("version", lit(version))
+
+    resultDF
+      .repartition(5).write.mode("overwrite").saveAsTable("test.ocpc_pid_error_data_hourly")
 //      .repartition(5).write.mode("overwrite").insertInto("dl_cpc.ocpc_pid_error_data_hourly")
-//  }
-//
-//  def calculatePID(baseData: DataFrame, kp: Double, ki: Double, kd: Double, date: String, hour: String, spark: SparkSession) = {
-//    val result = baseData
-//      .na.fill(0, Seq("current_error", "prev_error", "last_error"))
-//      .withColumn("increment", udfCalculatePID(kp, ki, kd)(col("current_error"), col("prev_error"), col("last_error")))
-//      .withColumn("kp", lit(kp))
-//      .withColumn("ki", lit(ki))
-//      .withColumn("kd", lit(kd))
-//      .select("unitid", "current_error", "prev_error", "last_error", "kp", "ki", "kd", "increment")
-//
-//    result
+  }
+
+  def calculatePID(baseData: DataFrame, kp: Double, ki: Double, kd: Double, date: String, hour: String, spark: SparkSession) = {
+    val result = baseData
+      .na.fill(0, Seq("current_error", "prev_error", "last_error"))
+      .withColumn("increment", udfCalculatePID(kp, ki, kd)(col("current_error"), col("prev_error"), col("last_error")))
+      .withColumn("kp", lit(kp))
+      .withColumn("ki", lit(ki))
+      .withColumn("kd", lit(kd))
+      .select("unitid", "current_error", "prev_error", "last_error", "kp", "ki", "kd", "increment")
+
+    result
+  }
+
+  def udfCalculatePID(kp: Double, ki: Double, kd: Double) = udf((currentError: Double, prevError: Double, lastError: Double) => {
+    var result = 0.0
+    if (currentError == 0 || prevError == 0 || lastError == 0) {
+      result = 0.0
+    } else {
+      result = kp * (currentError - prevError) + ki * currentError + kd * (currentError - 2.0 * prevError + lastError)
+    }
+    result
+  })
+
+  def getPrevData(hourInt: Int, conversionGoal: Int, version: String, date: String, hour: String, spark: SparkSession) = {
+    // 取历史数据
+    val dateConverter = new SimpleDateFormat("yyyy-MM-dd HH")
+    val newDate = date + " " + hour
+    val today = dateConverter.parse(newDate)
+    val calendar = Calendar.getInstance
+    calendar.setTime(today)
+    calendar.add(Calendar.HOUR, -hourInt)
+    val yesterday = calendar.getTime
+    val tmpDate = dateConverter.format(yesterday)
+    val tmpDateValue = tmpDate.split(" ")
+    val date1 = tmpDateValue(0)
+    val hour1 = tmpDateValue(1)
+
+    // 抽取数据
+    val sqlRequest1 =
+      s"""
+         |SELECT
+         |  unitid,
+         |  current_error as error1,
+         |  prev_error as error2
+         |FROM
+         |  dl_cpc.ocpc_pid_error_data_hourly
+         |WHERE
+         |  `date` = '$date1'
+         |AND
+         |  `hour` = '$hour1'
+         |AND
+         |  version = '$version'
+         |AND
+         |  conversion_goal = $conversionGoal
+       """.stripMargin
+    println(sqlRequest1)
+    val data = spark
+      .sql(sqlRequest1)
+      .withColumn("prev_error", col("error1"))
+      .withColumn("last_error", col("error2"))
+
+    data
   }
 
   def calculateCvr(baseData: DataFrame, date: String, hour: String, spark: SparkSession) = {
@@ -81,84 +132,33 @@ object OcpcPIDerror {
 
   }
 
-//  def udfCalculatePID(kp: Double, ki: Double, kd: Double) = udf((currentError: Double, prevError: Double, lastError: Double) => {
-//    var result = 0.0
-//    if (currentError == 0 || prevError == 0 || lastError == 0) {
-//      result = 0.0
-//    } else {
-//      result = kp * currentError - ki * prevError + kd * lastError
-//    }
-//    result
-//  })
-//
-//  def getPrevData(hourInt: Int, conversionGoal: Int, version: String, date: String, hour: String, spark: SparkSession) = {
-//    // 取历史数据
-//    val dateConverter = new SimpleDateFormat("yyyy-MM-dd HH")
-//    val newDate = date + " " + hour
-//    val today = dateConverter.parse(newDate)
-//    val calendar = Calendar.getInstance
-//    calendar.setTime(today)
-//    calendar.add(Calendar.HOUR, -hourInt)
-//    val yesterday = calendar.getTime
-//    val tmpDate = dateConverter.format(yesterday)
-//    val tmpDateValue = tmpDate.split(" ")
-//    val date1 = tmpDateValue(0)
-//    val hour1 = tmpDateValue(1)
-//
-//    // 抽取数据
-//    val sqlRequest1 =
-//      s"""
-//         |SELECT
-//         |  unitid,
-//         |  current_error as error1,
-//         |  prev_error as error2
-//         |FROM
-//         |  dl_cpc.ocpc_pid_error_data_hourly
-//         |WHERE
-//         |  `date` = '$date1'
-//         |AND
-//         |  `hour` = '$hour1'
-//         |AND
-//         |  version = '$version'
-//         |AND
-//         |  conversion_goal = $conversionGoal
-//       """.stripMargin
-//    println(sqlRequest1)
-//    val data = spark
-//        .sql(sqlRequest1)
-//        .withColumn("prev_error", col("error1"))
-//        .withColumn("last_error", col("error2"))
-//
-//    data
-//  }
-//
-//  def calculateError(baseData: DataFrame, minCV: Int, date: String, hour: String, spark: SparkSession) = {
-//    baseData.createOrReplaceTempView("base_data")
-//    val sqlRequest1 =
-//      s"""
-//         |SELECT
-//         |  unitid,
-//         |  sum(case when isclick=1 then price else 0 end) * 1.0 / sum(iscvr) as cpareal,
-//         |  sum(case when isclick=1 then cpagiven else 0 end) * 1.0 / sum(isclick) as cpagiven,
-//         |  sum(iscvr) as cv
-//         |FROM
-//         |  base_data
-//         |GROUP BY unitid
-//       """.stripMargin
-//    val data = spark.sql(sqlRequest1)
-//
-//    val currentError = data
-//      .filter(s"cv >= $minCV")
-//      .select("unitid", "cpareal", "cpagiven", "cv")
-//      .withColumn("current_error", udfCalculateError()(col("cpagiven"), col("cpareal")))
-//
-//    currentError
-//  }
-//
-//  def udfCalculateError() = udf((cpagiven: Double, cpareal: Double) => {
-//    val result = 1.0 - cpareal / cpagiven
-//    result
-//  })
+  def calculateError(baseData: DataFrame, minCV: Int, date: String, hour: String, spark: SparkSession) = {
+    baseData.createOrReplaceTempView("base_data")
+    val sqlRequest1 =
+      s"""
+         |SELECT
+         |  unitid,
+         |  pre_cvr,
+         |  post_cvr,
+         |  cv
+         |FROM
+         |  base_data
+         |WHERE
+         |  cv > $minCV
+       """.stripMargin
+    val currentError = spark
+      .sql(sqlRequest1)
+      .withColumn("current_error", udfCalculateError()(col("post_cvr"), col("pre_cvr")))
+      .cache()
+
+    currentError.show(10)
+    currentError
+  }
+
+  def udfCalculateError() = udf((post_cvr: Double, pre_cvr: Double) => {
+    val result = 1.0 - pre_cvr / post_cvr
+    result
+  })
 
 
   def getBaseData(media: String, sampleHour: Int, conversionGoal: Int, date: String, hour: String, spark: SparkSession) = {
