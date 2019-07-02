a8600e832e3f1c2b190ef1d8f07c69ddbf6fbe58
wangjun <wangjun02@qutoutiao.net>
2019-07-02 12:36:05 +0800

    test new code

--- src/main/scala/com/cpc/spark/OcpcProtoType/model_v6/OcpcCalculateCalibration.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/model_v6/OcpcCalculateCalibration.scala
@@ -1,6 +1,6 @@
 package com.cpc.spark.OcpcProtoType.model_v6
 
-import com.cpc.spark.OcpcProtoType.model_v5.OcpcSmoothFactor.OcpcSmoothFactorMain
+import com.cpc.spark.OcpcProtoType.model_v6.OcpcSmoothFactor.OcpcSmoothFactorMain
 import com.typesafe.config.ConfigFactory
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.functions._
@@ -22,38 +22,37 @@ object OcpcCalculateCalibration {
     val hour = args(1).toString
     val version = args(2).toString
     val media = args(3).toString
-    val conversionGoal = args(4).toInt
-    val minCV = args(5).toInt
+    val minCV = args(4).toInt
 
     // 主校准回溯时间长度
-    val hourInt1 = args(6).toInt
+    val hourInt1 = args(5).toInt
     // 备用校准回溯时间长度
-    val hourInt2 = args(7).toInt
+    val hourInt2 = args(6).toInt
     // 兜底校准时长
-    val hourInt3 = args(8).toInt
+    val hourInt3 = args(7).toInt
 
 
     println("parameters:")
-    println(s"date=$date, hour=$hour, conversionGoal=$conversionGoal, version=$version, media=$media, hourInt1=$hourInt1, hourInt2=$hourInt2")
+    println(s"date=$date, hour=$hour, version=$version, media=$media, hourInt1=$hourInt1, hourInt2=$hourInt2")
     // 抽取媒体id
 
-    val resultDF = OcpcCalculateCalibrationMain(date, hour, conversionGoal, version, media, minCV, hourInt1, hourInt2, hourInt3, spark)
-    resultDF.show(10)
+    val result = OcpcCalculateCalibrationMain(date, hour, version, media, minCV, hourInt1, hourInt2, hourInt3, spark)
+    result
+      .repartition(10).write.mode("overwrite").saveAsTable("test.check_ocpc_calibration20190702a")
 
   }
 
-  def OcpcCalculateCalibrationMain(date: String, hour: String, conversionGoal: Int, version: String, media: String, minCV: Int, hourInt1: Int, hourInt2: Int, hourInt3: Int, spark: SparkSession) = {
+  def OcpcCalculateCalibrationMain(date: String, hour: String, version: String, media: String, minCV: Int, hourInt1: Int, hourInt2: Int, hourInt3: Int, spark: SparkSession) = {
     // 抽取媒体id
     val conf = ConfigFactory.load("ocpc")
     val conf_key = "medias." + media + ".media_selection"
     val mediaSelection = conf.getString(conf_key)
-    val cvrType = "cvr" + conversionGoal.toString
 
-    val data1 = OcpcSmoothFactorMain(date, hour, version, media, hourInt1, cvrType, spark).cache()
+    val data1 = OcpcSmoothFactorMain(date, hour, version, media, hourInt1, spark).cache()
     data1.show(10)
-    val data2 = OcpcSmoothFactorMain(date, hour, version, media, hourInt2, cvrType, spark).cache()
+    val data2 = OcpcSmoothFactorMain(date, hour, version, media, hourInt2, spark).cache()
     data2.show(10)
-    val data3 = OcpcSmoothFactorMain(date, hour, version, media, hourInt3, cvrType, spark).cache()
+    val data3 = OcpcSmoothFactorMain(date, hour, version, media, hourInt3, spark).cache()
     data3.show(10)
 
     val calibration1 = calculateCalibrationValue(data1, data2, minCV, spark)
@@ -62,10 +61,10 @@ object OcpcCalculateCalibration {
       .withColumn("pcoc_new", col("pre_cvr") * 1.0 / col("post_cvr"))
       .withColumn("jfb_new", col("total_price") * 1.0 / col("total_bid"))
       .withColumn("post_cvr_new", col("post_cvr"))
-      .select("identifier", "pcoc_new", "jfb_new", "post_cvr_new")
+      .select("identifier", "conversion_goal", "pcoc_new", "jfb_new", "post_cvr_new")
 
     val calibration = calibrationNew
-      .join(calibration1, Seq("identifier"), "left_outer")
+      .join(calibration1, Seq("identifier", "conversion_goal"), "left_outer")
       .select("identifier", "pcoc_new", "jfb_new", "post_cvr_new", "pcoc3", "jfb3", "post_cvr3")
       .withColumn("post_cvr", when(col("post_cvr3").isNotNull, col("post_cvr3")).otherwise(col("post_cvr_new")))
       .withColumn("pcoc", when(col("pcoc3").isNotNull, col("pcoc3")).otherwise(col("pcoc_new")))
@@ -78,7 +77,7 @@ object OcpcCalculateCalibration {
     data3.unpersist()
 
     val resultDF = calibration
-      .select("identifier", "pcoc", "jfb", "post_cvr")
+      .select("identifier", "conversion_goal", "pcoc", "jfb", "post_cvr")
 
     resultDF
 
@@ -97,7 +96,7 @@ object OcpcCalculateCalibration {
       .withColumn("jfb1", col("total_price") * 1.0 / col("total_bid"))
       .withColumn("post_cvr1", col("post_cvr"))
       .withColumn("flag", lit(1))
-      .select("identifier", "pcoc1", "jfb1", "post_cvr1", "flag")
+      .select("identifier", "conversion_goal", "pcoc1", "jfb1", "post_cvr1", "flag")
 
     // 备用校准模型
     val data2 = dataRaw2
@@ -106,11 +105,11 @@ object OcpcCalculateCalibration {
       .withColumn("pcoc2", col("pre_cvr") * 1.0 / col("post_cvr"))
       .withColumn("jfb2", col("total_price") * 1.0 / col("total_bid"))
       .withColumn("post_cvr2", col("post_cvr"))
-      .select("identifier", "pcoc2", "jfb2", "post_cvr2")
+      .select("identifier", "conversion_goal", "pcoc2", "jfb2", "post_cvr2")
 
     // 数据表关联
     val data = data2
-      .join(data1, Seq("identifier"), "left_outer")
+      .join(data1, Seq("identifier", "conversion_goal"), "left_outer")
       .na.fill(0, Seq("flag"))
       .withColumn("pcoc3", when(col("flag") === 1, col("pcoc1")).otherwise(col("pcoc2")))
       .withColumn("jfb3", when(col("flag") === 1, col("jfb1")).otherwise(col("jfb2")))
