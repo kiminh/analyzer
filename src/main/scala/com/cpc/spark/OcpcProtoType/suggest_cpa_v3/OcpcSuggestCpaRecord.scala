79eb973ebc85313eaa7e66e1243aef410a1edc86
wangjun <wangjun02@qutoutiao.net>
2019-07-09 17:32:43 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/suggest_cpa_v3/OcpcSuggestCpaRecord.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/suggest_cpa_v3/OcpcSuggestCpaRecord.scala
@@ -37,6 +37,7 @@ object OcpcSuggestCpaRecord {
 
     // 过滤出最近72小时没有ocpc广告记录的cpa与kvalue
     val newData = getCleanData(suggestCPA, ocpcFlag, date, hour, spark)
+    newData.write.mode("overwrite").saveAsTable("test.check_ocpc_novel_record")
 
     // 读取前一小时的时间分区中的所有cpa与kvalue
     val prevData = getPrevData(version, date, hour, spark)
