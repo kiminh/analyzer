8dfbbe525d1eb3f2a009534057ef0473dc399df5
冯华宾 <fenghuabin@qutoutiao.net>
2019-07-10 21:38:46 +0800

    fhb

---
+++ src/main/scala/com/cpc/spark/ml/dnn/baseData/ReadExampleFromHdfs.scala
@@ -0,0 +1,20 @@
+package com.cpc.spark.ml.dnn.baseData
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object ReadExampleFromHdfs {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().enableHiveSupport().getOrCreate()
+
+    val path = "hdfs://emr-cluster/user/cpc/aiclk_dataflow/daily/adlist-v4/2019-06-11/"
+
+    //Read TFRecords into DataFrame.
+    //The DataFrame schema is inferred from the TFRecords if no custom schema is provided.
+    val importedDf1: DataFrame = spark.read.format("tfrecords").option("recordType", "Example").load(path)
+    importedDf1.show(10)
+
+    //val new_path = "hdfs://emr-cluster/user/cpc/fhb/adlist-v4/2019-06-11"
+    //importedDf1.repartition(100).saveAs
+
+  }
+}
