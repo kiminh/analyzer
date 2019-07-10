7ef9381966f01766c14c7e2662566da6bef253bd
冯华宾 <fenghuabin@qutoutiao.net>
2019-07-10 22:08:07 +0800

    fhb

---
+++ deployjar_fhb.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#git checkout master
+git pull
+sbt assembly
+echo "hadoop fs -put -f target/scala-2.11/cpc-anal_2.11-0.1.jar hdfs://emr-cluster/warehouse/azkaban/lib/fhb_start_v1.jar"
+hadoop fs -put -f target/scala-2.11/cpc-anal_2.11-0.1.jar hdfs://emr-cluster/warehouse/azkaban/lib/fhb_start_v1.jar
