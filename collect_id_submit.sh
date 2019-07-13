083c42a62abb8f17ca70f2d4b80a09552f8992dd
冯华宾 <fenghuabin@qutoutiao.net>
2019-07-13 17:42:35 +0800

    fhb

--- collect_id_submit.sh
+++ collect_id_submit.sh
@@ -10,7 +10,7 @@ cur_date=$1
 
 jarLib=hdfs://emr-cluster/warehouse/azkaban/lib/fhb_start_v1.jar
 
-queue=root.cpc.basedata
+queue=root.cpc.bigdata
 jars=("/home/cpc/anal/lib/spark-tensorflow-connector_2.11-1.10.0.jar" )
 
 randjar="fhb_start"`date +%s%N`".jar"
