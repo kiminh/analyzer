d5b2541da72f646e0d3d8af5ce9a2e0483e34523
fenghuabin <fenghuabin@qutoutiao.net>
2019-07-14 12:46:24 +0800

    fhb

---
+++ src/main/scala/com/cpc/spark/ml/dnn/baseData/MakeTrainExamples.scala
@@ -0,0 +1,199 @@
+package com.cpc.spark.ml.dnn.baseData
+
+import java.io.{File, PrintWriter}
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.types.{StructField, _}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+
+import scala.sys.process._
+import scala.util.Random
+
+/**
+  * 解析tfrecord到hdfs
+  * created time : 2019/07/13 10:38
+  * @author fenghuabin
+  * @version 1.0
+  *
+  */
+
+object MakeTrainExamples {
+
+  def delete_hdfs_path(path: String): Unit = {
+
+    val conf = new org.apache.hadoop.conf.Configuration()
+    val p = new org.apache.hadoop.fs.Path(path)
+    val hdfs = p.getFileSystem(conf)
+    val hdfs_path = new org.apache.hadoop.fs.Path(path.toString)
+
+    //val hdfs_path = new org.apache.hadoop.fs.Path(path.toString)
+    //val hdfs = org.apache.hadoop.fs.FileSystem.get(new org.apache.hadoop.conf.Configuration())
+    if (hdfs.exists(hdfs_path)) {
+      hdfs.delete(hdfs_path, true)
+    }
+  }
+
+  def exists_hdfs_path(path: String): Boolean = {
+
+    val conf = new org.apache.hadoop.conf.Configuration()
+    val p = new org.apache.hadoop.fs.Path(path)
+    val hdfs = p.getFileSystem(conf)
+    val hdfs_path = new org.apache.hadoop.fs.Path(path.toString)
+    //val hdfs = org.apache.hadoop.fs.FileSystem.get(new org.apache.hadoop.conf.Configuration())
+
+    if (hdfs.exists(hdfs_path)) {
+      true
+    } else {
+      false
+    }
+  }
+
+  def writeNum2File(file: String, num: Long): Unit = {
+    val writer = new PrintWriter(new File(file))
+    writer.write(num.toString)
+    writer.close()
+  }
+
+  //def getColAtIndex(id:Int): Column = {
+  //  col(s"column1")(id).as(s"column1_${id+1}")
+  //}
+
+
+  def main(args: Array[String]): Unit = {
+    if (args.length != 5) {
+      System.err.println(
+        """
+          |you have to input 5 parameters !!!
+        """.stripMargin)
+      System.exit(1)
+    }
+    //val Array(src, des_dir, des_date, des_map_prefix, numPartitions) = args
+    val Array(src_dir, src_date_str, des_dir, instances_file, numPartitions) = args
+
+    println(args)
+
+    Logger.getRootLogger.setLevel(Level.WARN)
+    val spark = SparkSession.builder().enableHiveSupport().getOrCreate()
+    val sc = spark.sparkContext
+
+    val src_date_list = src_date_str.split(";")
+    val src_file_list = scala.collection.mutable.ArrayBuffer[String]()
+    for (idx <- 0 until src_date_list.length) {
+      val curr_file = src_dir + "/" + src_date_list(idx) + "-instances"
+      if (!exists_hdfs_path(curr_file)) {
+        println("not exist src file:", curr_file)
+      }
+      src_file_list += curr_file
+    }
+
+    /************collect map instances for id feature************************/
+    for (src_date <- src_date_list) {
+      val instances_path = des_dir + "/instances-" + src_date
+      if (!exists_hdfs_path(instances_path)) {
+        val curr_file_src = src_dir + "/" + src_date + "/part-r-*"
+        val importedDf: DataFrame = spark.read.format("tfrecords").option("recordType", "Example").load(curr_file_src)
+        importedDf.cache()
+        println("DF file count:" + importedDf.count().toString + " of file:" + curr_file_src)
+        if (importedDf.count() < 10000) {
+          importedDf.unpersist()
+          println("un-persist df file:" + curr_file_src)
+        } else {
+          val map_path = des_dir + "/instances-" + src_date + "-collect"
+          if (!exists_hdfs_path(map_path)) {
+            importedDf.createOrReplaceTempView("sql_table_view_name_" + src_date)
+            val tf_decode_df_rows = spark.sql("SELECT sample_idx, label, dense, idx0, idx1, idx2, id_arr FROM " + "sql_table_view_name_" + src_date)
+            tf_decode_df_rows.rdd.map(
+              rs => {
+                val sample_idx = rs.getLong(0).toString
+                val label_arr = rs.getSeq[Long](1)
+                val dense = rs.getSeq[Long](2)
+                val idx0 = rs.getSeq[Long](3)
+                val idx1 = rs.getSeq[Long](4)
+                val idx2 = rs.getSeq[Long](5)
+                val idx_arr = rs.getSeq[Long](6)
+
+                val output: Array[String] = new Array[String](1 + 1 + dense.length + idx_arr.length)
+
+                var label = "0.0"
+                if (label_arr.head == 1) {
+                  label = "1.0"
+                } else {
+                  label = "0.0"
+                }
+                output(0) = sample_idx
+                output(1) = label
+
+                for (idx <- 0 until dense.length) {
+                  output(idx + 2) = dense(idx).toString
+                }
+
+                for (idx <- 0 until idx_arr.length) {
+                  output(idx + 2 + dense.length) = idx_arr(idx).toString
+                }
+
+                output.mkString("\t")
+              }
+            ).saveAsTextFile(map_path)
+          }
+
+          var data = sc.parallelize(Array[(String, Long)]())
+          data = data.union(
+            sc.textFile(map_path).map(
+              rs => {
+                val line = rs.split("\t")
+                val output: Array[String] = new Array[String](line.length - 2)
+                for (idx <- 2 until line.length) {
+                  output(idx - 2) = line(idx)
+                }
+                output.mkString("\t")
+              }
+            ).flatMap(
+              rs => {
+                val line = rs.split("\t")
+                for (elem <- line)
+                  yield (elem, 1L)
+              }
+            ).reduceByKey(_ + _)
+          )
+
+          data.reduceByKey(_ + _).sortByKey().map {
+            case (key, value) =>
+              key + "\t" + value.toString
+          }.repartition(1).saveAsTextFile(instances_path)
+        }
+      }
+    }
+
+    /************************make total instances********************************/
+    val output = scala.collection.mutable.ArrayBuffer[String]()
+    for (src_date <- src_date_list) {
+      val instances_path = des_dir + "/instances-" + src_date
+      if (exists_hdfs_path(instances_path)) {
+        output += instances_path
+      }
+    }
+
+    val src_instances_files = output.mkString(",")
+    //统计每个ID特征的每个取值出现的次数
+    val instances_all = des_dir + "/" + instances_file
+    if (!exists_hdfs_path(instances_all)) {
+      var data = sc.parallelize(Array[(String, Long)]())
+      data = data.union(
+        sc.textFile(src_instances_files).map(
+          rs => {
+            val line = rs.split("\t")
+            val key = line(0)
+            val cnt = line(1).toLong
+            (key, cnt)
+          }
+        ).reduceByKey(_ + _)
+      )
+      data.reduceByKey(_ + _).repartition(1).sortBy(_._2 * -1).map {
+        case (key, value) =>
+          key + "\t" + value.toString
+      }.saveAsTextFile(instances_all)
+    }
+
+    val negativeSampleRatio = 0.19
+  }
+}
---
+++ submit_make_example.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+cur_date="2019-06-11-bak"
+cur_date=$1
+#modelVersion=$2
+#today=`date -d "${cur_time} 1 hours ago" +%Y-%m-%d`
+#hour=`date -d "${cur_time} 1 hours ago" +%H`
+
+jarLib=hdfs://emr-cluster/warehouse/azkaban/lib/fhb_start_v1.jar
+
+queue=root.cpc.bigdata
+jars=("/home/cpc/anal/lib/spark-tensorflow-connector_2.11-1.10.0.jar" )
+
+randjar="fhb_start"`date +%s%N`".jar"
+hadoop fs -get ${jarLib} ${randjar}
+
+#src="hdfs://emr-cluster/user/cpc/aiclk_dataflow/daily/adlist-v4/2019-06-11/part-*"
+src="hdfs://emr-cluster/user/cpc/aiclk_dataflow/daily/adlist-v4/"${cur_date}"/part*"
+des_date=${cur_date}
+des_map="emb-map"
+partitions=1000
+
+src_dir="hdfs://emr-cluster/user/cpc/aiclk_dataflow/daily/adlist-v4"
+src_date="2019-05-26;2019-05-27;2019-05-28;2019-05-29;2019-05-30;2019-05-31;2019-06-01;2019-06-02;2019-06-03;2019-06-04;2019-06-05;2019-06-06;2019-06-07;2019-06-08;2019-06-09;2019-06-10;2019-06-11"
+des_dir="hdfs://emr-cluster/user/cpc/fenghuabin/adlist-v4-base"
+instances_file="instances-all"
+partitions=1000
+
+spark-submit --master yarn --queue ${queue} \
+    --name "cpc-adlist-tf-decode" \
+    --driver-memory 8g --executor-memory 8g \
+    --num-executors 500 --executor-cores 4 \
+    --conf spark.hadoop.fs.defaultFS=hdfs://emr-cluster2 \
+    --conf "spark.yarn.executor.memoryOverhead=4g" \
+    --conf "spark.sql.shuffle.partitions=500" \
+    --jars $( IFS=$','; echo "${jars[*]}" ) \
+    --class com.cpc.spark.ml.dnn.baseData.MakeTrainExamples \
+    ${randjar} ${src_dir} ${src_date} ${des_dir} ${instances_file} ${partitions}
+
+chmod_des="hdfs://emr-cluster/user/cpc/fenghuabin/adlist-v4-info"${des_date}"*"
+hadoop fs -chmod -R 0777 ${chmod_des}
