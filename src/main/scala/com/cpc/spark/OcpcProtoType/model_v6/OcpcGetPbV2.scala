0e88a4bf6bb2d703773d6975d4015b2d358af7ac
wangjun <wangjun02@qutoutiao.net>
2019-07-15 14:59:24 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/model_v6/OcpcGetPbV2.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/model_v6/OcpcGetPbV2.scala
@@ -2,10 +2,14 @@ package com.cpc.spark.OcpcProtoType.model_v6
 
 import java.text.SimpleDateFormat
 import java.util.Calendar
+import com.typesafe.config.ConfigFactory
+
 import com.cpc.spark.OcpcProtoType.model_v6.OcpcCalculateCalibration.OcpcCalculateCalibrationMain
 import com.cpc.spark.OcpcProtoType.model_v6.OcpcRangeCalibration.OcpcRangeCalibrationMain
+import com.cpc.spark.OcpcProtoType.model_v6.OcpcGetPbWeishi.OcpcGetPbWeishiMain
+import com.cpc.spark.OcpcProtoType.OcpcTools.getConversionGoal
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 
 
@@ -52,11 +56,16 @@ object OcpcGetPbV2 {
     val calibraionData = OcpcCalculateCalibrationMain(date, hour, version, media, minCV, hourInt1, hourInt2, hourInt3, spark)
     val factorData = OcpcRangeCalibrationMain(date, hour, version, media, highBidFactor, lowBidFactor, hourInt, 40, spark)
 
-    val resultDF = calibraionData
+    val result1 = calibraionData
       .join(factorData.select("identifier", "conversion_goal", "high_bid_factor", "low_bid_factor"), Seq("identifier", "conversion_goal"), "left_outer")
       .na.fill(1.0, Seq("high_bid_factor", "low_bid_factor"))
       .withColumn("cpagiven", lit(1.0))
       .cache()
+
+    val result2 = OcpcGetPbWeishiMain(date, hour, version, media, highBidFactor, lowBidFactor, hourInt, minCV, hourInt1, hourInt2, hourInt3, spark)
+
+    val resultDF = selectWeishiCali(expTag, result1, result2, date, hour, spark)
+
 
     resultDF.show(10)
     resultDF
@@ -76,6 +85,77 @@ object OcpcGetPbV2 {
 
   }
 
+  def selectWeishiCali(expTag: String, dataRaw1: DataFrame, dataRaw2: DataFrame, date: String, hour: String, spark: SparkSession) = {
+    // 获取oCPC单元中userid与unitid的映射关系
+    val useridToUnitid = getConversionGoal(date, hour, spark)
+    val useridUnitid = useridToUnitid
+      .selectExpr("cast(unitid as string) identifier", "userid")
+      .select("identifier", "userid")
+      .distinct()
+
+    // 从配置文件获取需要特殊化配置的广告主id（微视广告主）
+    val conf = ConfigFactory.load("ocpc")
+    val confPath = conf.getString("exp_tag.weishi")
+    val rawData = spark.read.format("json").json(confPath)
+    val confData = rawData
+      .filter(s"exp_tag = '$expTag'")
+      .select("userid")
+      .distinct()
+
+    val flagData = useridUnitid
+      .join(confData, Seq("userid"), "inner")
+      .select("identifier")
+      .distinct()
+
+//      "identifier", "pcoc", "jfb", "post_cvr", "high_bid_factor", "low_bid_factor", "cpagiven", "conversion_goal"
+    val data2 = dataRaw2
+      .join(flagData, Seq("identifier"), "inner")
+      .filter(s"conversion_goal = 2")
+      .withColumn("pcoc_bak", col("pcoc"))
+      .withColumn("jfb_bak", col("jfb"))
+      .withColumn("post_cvr_bak", col("post_cvr"))
+      .withColumn("high_bid_factor_bak", col("high_bid_factor"))
+      .withColumn("low_bid_factor_bak", col("low_bid_factor"))
+      .withColumn("flag", lit(1))
+      .select("identifier", "pcoc_bak", "jfb_bak", "post_cvr_bak", "high_bid_factor_bak", "low_bid_factor_bak", "flag", "conversion_goal")
+
+    val data1 = dataRaw1
+      .withColumn("pcoc_orig", col("pcoc"))
+      .withColumn("jfb_orig", col("jfb"))
+      .withColumn("post_cvr_orig", col("post_cvr"))
+      .withColumn("high_bid_factor_orig", col("high_bid_factor"))
+      .withColumn("low_bid_factor_orig", col("low_bid_factor"))
+      .select("identifier", "pcoc_orig", "jfb_orig", "post_cvr_orig", "high_bid_factor_orig", "low_bid_factor_orig", "cpagiven", "conversion_goal")
+
+    val data = data1
+      .join(data2, Seq("identifier", "conversion_goal"), "left_outer")
+      .na.fill(0, Seq("pcoc_bak", "jfb_bak", "post_cvr_bak", "high_bid_factor_bak", "low_bid_factor_bak", "flag"))
+      .withColumn("pcoc", udfSelectValue()(col("flag"), col("pcoc_orig"), col("pcoc_bak")))
+      .withColumn("jfb", udfSelectValue()(col("flag"), col("jfb_orig"), col("jfb_bak")))
+      .withColumn("post_cvr", udfSelectValue()(col("flag"), col("post_cvr_orig"), col("post_cvr_bak")))
+      .withColumn("high_bid_factor", udfSelectValue()(col("flag"), col("high_bid_factor_orig"), col("high_bid_factor_bak")))
+      .withColumn("low_bid_factor", udfSelectValue()(col("flag"), col("low_bid_factor_orig"), col("low_bid_factor_bak")))
+
+    data
+      .write.mode("overwrite").saveAsTable("test.check_ocpc_data20190715a")
+
+    val result = data
+      .select("identifier", "pcoc", "jfb", "post_cvr", "high_bid_factor", "low_bid_factor", "cpagiven", "conversion_goal")
+
+    result
+
+
+  }
+
+  def udfSelectValue() = udf((flag: Int, valueOrigin: Double, valueBak: Double) => {
+    var result = valueOrigin
+    if (flag == 1) {
+      result = valueBak
+    } else {
+      result = valueOrigin
+    }
+    result
+  })
 
 
 }
