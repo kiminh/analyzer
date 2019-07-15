646d76439495bda3bc1083e21b1d2c80f898fff5
wangjun <wangjun02@qutoutiao.net>
2019-07-15 13:09:49 +0800

    test new code

--- src/main/scala/com/cpc/spark/OcpcProtoType/model_v6/OcpcSmoothFactor.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/model_v6/OcpcSmoothFactor.scala
@@ -133,7 +133,7 @@ object OcpcSmoothFactor{
          |FROM
          |  dl_cpc.ocpc_label_cvr_hourly
          |WHERE
-         |  $selectCondition
+         |  date >= '$date1'
        """.stripMargin
     println(sqlRequest2)
     val cvData = spark.sql(sqlRequest2)
