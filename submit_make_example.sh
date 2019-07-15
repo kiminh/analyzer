92617afabeabd78dafe7ae76a990969ebc31ae2b
冯华宾 <fenghuabin@qutoutiao.net>
2019-07-15 18:02:50 +0800

    fhb

--- submit_make_example.sh
+++ submit_make_example.sh
@@ -7,9 +7,6 @@ cur_date=$1
 #modelVersion=$2
 #today=`date -d "${cur_time} 1 hours ago" +%Y-%m-%d`
 #hour=`date -d "${cur_time} 1 hours ago" +%H`
-filelist=`cat filelist`
-echo $filelist
-exit
 
 jarLib=hdfs://emr-cluster/warehouse/azkaban/lib/fhb_start_v1.jar
 
@@ -31,7 +28,7 @@ src_date="2019-05-26;2019-05-27;2019-05-28;2019-05-29;2019-05-30;2019-05-31;2019
 src_date="2019-06-10;2019-06-09;2019-06-08;2019-06-07;2019-06-11"
 src_date="2019-06-10;2019-06-09;2019-06-08;2019-06-07;2019-06-11;2019-06-06;2019-06-05;2019-06-04;2019-06-03;2019-06-02;2019-06-01;2019-05-31;2019-05-30;2019-05-29;2019-05-28;2019-05-27;2019-05-26"
 src_date="2019-06-07;2019-06-08;2019-06-09;2019-06-10;2019-06-11;2019-06-06;2019-06-05;2019-06-04;2019-06-03;2019-06-02;2019-06-01;2019-05-31;2019-05-30;2019-05-29;2019-05-28;2019-05-27;2019-05-26"
-filelist=`cat filelist`
+src_date="2019-06-10;2019-06-09;2019-06-08;2019-06-07;2019-06-11"
 des_dir="hdfs://emr-cluster/user/cpc/fenghuabin/adlist-v4-base"
 instances_file="instances-all"
 partitions=1000
@@ -39,7 +36,7 @@ partitions=1000
 spark-submit --master yarn --queue ${queue} \
     --name "adlist-tf-make-example" \
     --driver-memory 16g --executor-memory 16g \
-    --num-executors 10 --executor-cores 4 \
+    --num-executors 500 --executor-cores 4 \
     --conf spark.hadoop.fs.defaultFS=hdfs://emr-cluster2 \
     --conf "spark.yarn.executor.memoryOverhead=4g" \
     --conf "spark.sql.shuffle.partitions=500" \
