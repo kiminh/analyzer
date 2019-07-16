b81291a5a2ecd1ed7ecdfa8a843c8752df863091
齐智 <qizhi@qutoutiao.net>
2019-07-16 11:58:43 +0800

    change ip to 172.16.84.241

--- deployjar_lr.sh
+++ deployjar_lr.sh
@@ -2,5 +2,5 @@
 #git checkout master
 git pull
 sbt assembly
-echo "hadoop fs -put -f target/scala-2.11/cpc-anal_2.11-0.1.jar hdfs://emr-cluster/warehouse/azkaban/lib/dssm_v1.jar"
+echo "cp target/scala-2.11/cpc-anal_2.11-0.1.jar /home/cpc/anal/lib/lr-train.jar"
 cp target/scala-2.11/cpc-anal_2.11-0.1.jar /home/cpc/anal/lib/lr-train.jar
