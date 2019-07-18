7ac661a8beab3d068bdb09ab8cf39013908448f1
wangjun <wangjun02@qutoutiao.net>
2019-07-18 15:51:18 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/report/OcpcDailyStatReport.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/report/OcpcDailyStatReport.scala
@@ -24,49 +24,18 @@ object OcpcDailyStatReport {
 
     val data1 = calculateBase(rawData, date, spark)
 
-    val result1 = data1
-      .select("unitid", "planid", "userid", "click", "show", "cv", "cost", "ocpc_cpagiven", "ocpc_cpareal", "ocpc_click", "ocpc_show", "ocpc_cv", "ocpc_cost", "hidden_cpagiven", "hidden_cpareal", "hidden_click", "hidden_show", "hidden_cv", "hidden_cost", "budget", "adslot_type", "site_type", "media", "is_ocpc", "industry", "date")
+    val result = data1
+      .select("unitid", "planid", "userid", "adclass", "industry", "adslot_type", "media", "click", "show", "cv", "cost", "cpa", "ocpc_click", "ocpc_show", "ocpc_cv", "ocpc_cost", "ocpc_cpagiven", "ocpc_cpareal", "budget", "date")
 
-    result1
+
+    result
       .repartition(5)
-      .write.mode("overwrite").insertInto("test.ocpc_funnel_data_industry_daily")
-//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_funnel_data_industry_daily")
+      .write.mode("overwrite").insertInto("test.ocpc_daily_stat_report")
+//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_daily_stat_report")
 
 
   }
 
-  def calculateCnt(baseData: DataFrame, date: String, spark: SparkSession) = {
-    // 计算日期
-    val dateConverter = new SimpleDateFormat("yyyy-MM-dd")
-    val today = dateConverter.parse(date)
-    val calendar = Calendar.getInstance
-    calendar.setTime(today)
-    calendar.add(Calendar.DATE, -1)
-    val yesterday = calendar.getTime
-    val date1 = dateConverter.format(yesterday)
-    val selectCondition = s"`dt` = '$date1'"
-
-    baseData.createOrReplaceTempView("base_data")
-
-    val sqlRequest =
-      s"""
-         |SELECT
-         |    industry,
-         |    media,
-         |    count(distinct ideaid) as ideaid_cnt,
-         |    count(distinct unitid) as unitid_cnt,
-         |    count(distinct userid) as userid_cnt
-         |FROM
-         |    base_data
-         |GROUP BY industry, media
-       """.stripMargin
-    println(sqlRequest)
-    val data = spark
-        .sql(sqlRequest)
-        .withColumn("date", lit(date1))
-
-    data
-  }
 
   def calculateBase(baseData: DataFrame, date: String, spark: SparkSession) = {
     // 汇总数据
@@ -77,6 +46,7 @@ object OcpcDailyStatReport {
          |    unitid,
          |    planid,
          |    userid,
+         |    adclass,
          |    industry,
          |    adslot_type,
          |    media,
@@ -84,24 +54,39 @@ object OcpcDailyStatReport {
          |    sum(isshow) as show,
          |    sum(iscvr) as cv,
          |    sum(case when isclick=1 then price else 0 end) * 0.01 as cost,
-         |    sum(case when cpc_type = 'ocpc' and isclick=1 then cpagiven else 0 end) * 1.0 / sum(case when cpc_type = 'ocpc' and isclick=1 then 1 else 0 end) as ocpc_cpagiven,
-         |    sum(case when cpc_type = 'ocpc' and isclick=1 then price else 0 end) * 1.0 / sum(case when cpc_type = 'ocpc' and iscvr=1 then 1 else 0 end) as ocpc_cpareal,
-         |    sum(case when cpc_type = 'ocpc' and isclick=1 then 1 else 0 end) as ocpc_click,
-         |    sum(case when cpc_type = 'ocpc' and isshow=1 then 1 else 0 end) as ocpc_show,
-         |    sum(case when cpc_type = 'ocpc' and iscvr=1 then 1 else 0 end) as ocpc_cv,
-         |    sum(case when cpc_type = 'ocpc' and isclick=1 then price else 0 end) * 0.01 as ocpc_cost,
-         |    sum(case when cpc_type = 'hidden_ocpc' and isclick=1 then cpagiven else 0 end) * 1.0 / sum(case when cpc_type = 'hidden_ocpc' and isclick=1 then 1 else 0 end) as hidden_cpagiven,
-         |    sum(case when cpc_type = 'hidden_ocpc' and isclick=1 then price else 0 end) * 1.0 / sum(case when cpc_type = 'hidden_ocpc' and iscvr=1 then 1 else 0 end) as hidden_cpareal,
-         |    sum(case when cpc_type = 'hidden_ocpc' and isclick=1 then 1 else 0 end) as hidden_click,
-         |    sum(case when cpc_type = 'hidden_ocpc' and isshow=1 then 1 else 0 end) as hidden_show,
-         |    sum(case when cpc_type = 'hidden_ocpc' and iscvr=1 then 1 else 0 end) as hidden_cv,
-         |    sum(case when cpc_type = 'hidden_ocpc' and isclick=1 then price else 0 end) * 0.01 as hidden_cost
+         |    sum(case when isclick=1 then price else 0 end) * 0.01 / sum(iscvr) as cpa
          |FROM
          |    base_data
-         |GROUP BY unitid, planid, userid, industry, adslot_type, site_type, media
+         |GROUP BY unitid, planid, userid, adclass, industry, adslot_type, media
        """.stripMargin
     println(sqlRequest1)
     val data1 = spark.sql(sqlRequest1)
+
+    val sqlRequest2 =
+      s"""
+         |SELECT
+         |    unitid,
+         |    planid,
+         |    userid,
+         |    adclass,
+         |    industry,
+         |    adslot_type,
+         |    media,
+         |    sum(isclick) as ocpc_click,
+         |    sum(isshow) as ocpc_show,
+         |    sum(iscvr) as ocpc_cv,
+         |    sum(case when isclick=1 then price else 0 end) * 0.01 as ocpc_cost,
+         |    sum(case when isclick=1 then cpagiven else 0 end) * 0.01 / sum(isclick) as ocpc_cpagiven,
+         |    sum(case when isclick=1 then price else 0 end) * 0.01 / sum(iscvr) as ocpc_cpareal
+         |FROM
+         |    base_data
+         |WHERE
+         |    ocpc_step = 2
+         |GROUP BY unitid, planid, userid, adclass, industry, adslot_type, media
+       """.stripMargin
+    println(sqlRequest2)
+    val data2 = spark.sql(sqlRequest2)
+
 
     // 预算数据
     val dateConverter = new SimpleDateFormat("yyyy-MM-dd")
@@ -113,7 +98,7 @@ object OcpcDailyStatReport {
     val date1 = dateConverter.format(yesterday)
     val selectCondition = s"`dt` = '$date1'"
 
-    val sqlRequest2 =
+    val sqlRequest3 =
       s"""
          |SELECT
          |    plan_id as planid,
@@ -123,29 +108,15 @@ object OcpcDailyStatReport {
          |WHERE
          |    $selectCondition
        """.stripMargin
-    println(sqlRequest2)
-    val data2 = spark.sql(sqlRequest2)
-
-
-    // 是否oCPC单元
-    val sqlRequest3 =
-      s"""
-         |SELECT
-         |  unit_id as unitid,
-         |  is_ocpc
-         |FROM
-         |  qttdw.dim_unit_ds
-         |WHERE
-         |  $selectCondition
-       """.stripMargin
     println(sqlRequest3)
     val data3 = spark.sql(sqlRequest3)
 
     // 数据关联
+//    unitid, planid, userid, adclass, industry, adslot_type, media
     val data = data1
-        .join(data2, Seq("planid"), "left_outer")
-        .join(data3, Seq("unitid"), "left_outer")
-        .select("unitid", "planid", "userid", "industry", "adslot_type", "site_type", "media", "click", "show", "cv", "cost", "ocpc_cpagiven", "ocpc_cpareal", "ocpc_click", "ocpc_show", "ocpc_cv", "ocpc_cost", "hidden_cpagiven", "hidden_cpareal", "hidden_click", "hidden_show", "hidden_cv", "hidden_cost", "budget", "is_ocpc")
+        .join(data2, Seq("unitid", "planid", "userid", "adclass", "industry", "adslot_type", "media"), "left_outer")
+        .join(data3, Seq("planid"), "left_outer")
+        .select("unitid", "planid", "userid", "adclass", "industry", "adslot_type", "media", "click", "show", "cv", "cost", "cpa", "ocpc_click", "ocpc_show", "ocpc_cv", "ocpc_cost", "ocpc_cpagiven", "ocpc_cpareal", "budget")
         .withColumn("date", lit(date1))
 
     data
@@ -175,6 +146,7 @@ object OcpcDailyStatReport {
          |    unitid,
          |    planid,
          |    userid,
+         |    adclass,
          |    adslot_type,
          |    isclick,
          |    isshow,
@@ -221,6 +193,7 @@ object OcpcDailyStatReport {
          |    unitid,
          |    planid,
          |    userid,
+         |    adclass,
          |    adslot_type,
          |    isclick,
          |    isshow,
