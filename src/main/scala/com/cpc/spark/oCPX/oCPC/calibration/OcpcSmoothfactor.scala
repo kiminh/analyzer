1e4427b1fbfda64357cf11f43a88021872dcab0e
wangjun <wangjun02@qutoutiao.net>
2019-07-23 13:54:39 +0800

    test new code

--- src/main/scala/com/cpc/spark/oCPX/oCPC/calibration/OcpcSmoothfactor.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/calibration/OcpcSmoothfactor.scala
@@ -23,30 +23,83 @@ object OcpcSmoothfactor {
     val version = args(2).toString
     val expTag = args(3).toString
     val hourInt = args(4).toInt
-    val minCV = args(5).toInt
     println("parameters:")
-    println(s"date=$date, hour=$hour, version:$version, expTag:$expTag, hourInt:$hourInt, minCV:$minCV")
+    println(s"date=$date, hour=$hour, version:$version, expTag:$expTag, hourInt:$hourInt")
 
-    val result = OcpcSmoothFactorMain(date, hour, version, expTag, hourInt, minCV, spark)
+    val result = OcpcSmoothFactorMain(date, hour, version, expTag, hourInt, spark)
     result
-      .repartition(10).write.mode("overwrite").saveAsTable("test.check_smooth_factor20190702a")
+      .repartition(10).write.mode("overwrite").saveAsTable("test.check_smooth_factor20190723a")
   }
 
-  def OcpcSmoothFactorMain(date: String, hour: String, version: String, expTag: String, hourInt: Int, minCV: Int, spark: SparkSession) = {
+  def OcpcSmoothFactorMain(date: String, hour: String, version: String, expTag: String, hourInt: Int, spark: SparkSession) = {
     val baseData = getBaseData(hourInt, date, hour, spark)
 
     // 计算结果
     val result = calculateCVR(baseData, spark)
 
-    val finalVersion = version + hourInt.toString
+    // 关联配置文件中的mincv和smooth_factor
+    /*
+    min_cv:配置文件中如果为负数或空缺，则用默认值10，其他情况使用设定值
+    smooth_factor：配置文件中如果为负数或空缺，则用默认值0.5，其他情况使用设定值
+     */
+    val minCV = getExpConf(version, spark)
+
     val resultDF = result
-      .select("identifier", "conversion_goal", "click", "cv", "pre_cvr", "total_price", "total_bid", "hour_cnt")
-      .filter(s"cv > 0")
+      .select("unitid", "conversion_goal", "media", "click", "cv", "cvr")
+      .withColumn("exp_tag", lit(expTag))
+      .withColumn("media", udfMediaName()(col("media")))
+      .withColumn("exp_tag", concat(col("exp_tag"), col("media")))
+      .join(minCV, Seq("conversion_goal", "exp_tag"), "left_outer")
+      .na.fill(10, Seq("min_cv"))
+      .na.fill(0.5, Seq("smooth_factor"))
+      .withColumn("min_cv", udfSetMinCV()(col("min_cv")))
+      .withColumn("smooth_factor", udfSetSmoothFactor()(col("smooth_factor")))
+      .filter(s"cv >= min_cv")
 
     resultDF
   }
 
+  def udfSetSmoothFactor() = udf((smoothFactor: Double) => {
+    var result = smoothFactor
+    if (result < 0) {
+      result = 0.5
+    }
+    result
+  })
 
+  def udfSetMinCV() = udf((minCV: Int) => {
+    var result = minCV
+    if (result < 0) {
+      result = 10
+    }
+    result
+  })
+
+  def udfMediaName() = udf((media: String) => {
+    var result = media match {
+      case "qtt" => "Qtt"
+      case "hottopic" => "HT66"
+      case "novel" => "Midu"
+      case _ => "others"
+    }
+    result
+  })
+
+
+  def getExpConf(version: String, spark: SparkSession) ={
+    // 从配置文件读取数据
+    val conf = ConfigFactory.load("ocpc")
+    val confPath = conf.getString("exp_config.jfb_factor")
+    val rawData = spark.read.format("json").json(confPath)
+    val data = rawData
+      .filter(s"version = '$version'")
+      .select("exp_tag", "conversion_goal", "min_cv", "smooth_factor")
+      .distinct()
+
+    data.show(10)
+
+    data
+  }
 
   def calculateCVR(rawData: DataFrame, spark: SparkSession) = {
     val data  =rawData
@@ -60,83 +113,8 @@ object OcpcSmoothfactor {
       .select("unitid", "conversion_goal", "media", "click", "cv", "cvr")
 
     val result = data
-        .selectExpr("cast(unitid as string) identifier", "conversion_goal", "click", "cv", "pre_cvr", "total_price", "total_bid", "hour_cnt")
+        .select("unitid", "conversion_goal", "media", "click", "cv", "cvr")
 
     result
   }
-
-//  def getBaseData(media: String, hourInt: Int, date: String, hour: String, spark: SparkSession) = {
-//    // 抽取媒体id
-//    val conf = ConfigFactory.load("ocpc")
-//    val conf_key = "medias." + media + ".media_selection"
-//    val mediaSelection = conf.getString(conf_key)
-//
-//    // 取历史数据
-//    val dateConverter = new SimpleDateFormat("yyyy-MM-dd HH")
-//    val newDate = date + " " + hour
-//    val today = dateConverter.parse(newDate)
-//    val calendar = Calendar.getInstance
-//    calendar.setTime(today)
-//    calendar.add(Calendar.HOUR, -hourInt)
-//    val yesterday = calendar.getTime
-//    val tmpDate = dateConverter.format(yesterday)
-//    val tmpDateValue = tmpDate.split(" ")
-//    val date1 = tmpDateValue(0)
-//    val hour1 = tmpDateValue(1)
-//    val selectCondition = getTimeRangeSqlDate(date1, hour1, date, hour)
-//
-//    val sqlRequest =
-//      s"""
-//         |SELECT
-//         |  searchid,
-//         |  unitid,
-//         |  isshow,
-//         |  isclick,
-//         |  bid_discounted_by_ad_slot as bid,
-//         |  price,
-//         |  cast(exp_cvr as double) as exp_cvr,
-//         |  conversion_goal,
-//         |  hour
-//         |FROM
-//         |  dl_cpc.ocpc_base_unionlog
-//         |WHERE
-//         |  $selectCondition
-//         |AND
-//         |  $mediaSelection
-//         |AND
-//         |  isclick = 1
-//         |AND
-//         |  is_ocpc = 1
-//         |AND
-//         |  conversion_goal > 0
-//       """.stripMargin
-//    println(sqlRequest)
-//    val clickData = spark
-//      .sql(sqlRequest)
-//      .withColumn("cvr_goal", udfConcatStringInt("cvr")(col("conversion_goal")))
-//
-//    // 抽取cv数据
-//    val sqlRequest2 =
-//      s"""
-//         |SELECT
-//         |  searchid,
-//         |  label as iscvr,
-//         |  cvr_goal
-//         |FROM
-//         |  dl_cpc.ocpc_label_cvr_hourly
-//         |WHERE
-//         |  $selectCondition
-//       """.stripMargin
-//    println(sqlRequest2)
-//    val cvData = spark.sql(sqlRequest2)
-//
-//
-//    // 数据关联
-//    val resultDF = clickData
-//      .join(cvData, Seq("searchid", "cvr_goal"), "left_outer")
-//      .select("searchid", "unitid", "conversion_goal", "isclick", "exp_cvr", "iscvr", "price", "bid", "hour")
-//
-//    resultDF
-//  }
-
 }
