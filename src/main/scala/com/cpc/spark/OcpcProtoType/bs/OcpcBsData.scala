57ae9ea4466cb9d41670b24c32624a17d3807895
wangjun <wangjun02@qutoutiao.net>
2019-07-23 10:24:03 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/bs/OcpcBsData.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/bs/OcpcBsData.scala
@@ -30,9 +30,6 @@ object OcpcBsData {
 
 
     val baseData = getBaseData(hourInt, date, hour, spark)
-    baseData
-        .repartition(10).write.mode("overwrite").saveAsTable("test.check_ocpc_data20190722a")
-    baseData.show(10)
 
     // 计算结果
     val data = calculateData(baseData, expTag, spark)
@@ -44,8 +41,8 @@ object OcpcBsData {
         .withColumn("exp_tag", lit(expTag))
         .withColumn("version", lit(version))
         .repartition(5)
-        .write.mode("overwrite").insertInto("test.ocpc_bs_params_pb_hourly")
-//        .write.mode("overwrite").insertInto("dl_cpc.ocpc_bs_params_pb_hourly")
+//        .write.mode("overwrite").insertInto("test.ocpc_bs_params_pb_hourly")
+        .write.mode("overwrite").insertInto("dl_cpc.ocpc_bs_params_pb_hourly")
 
 
     savePbPack(result, fileName, spark)
