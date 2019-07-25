f02329bb3a1de9578d707def99ca13c7c2ad066d
冯华宾 <fenghuabin@qutoutiao.net>
2019-07-25 14:17:52 +0800

    fhb

--- submit_make_example.sh
+++ submit_make_example.sh
@@ -25,7 +25,7 @@ partitions=1000
 
 spark-submit --master yarn --queue ${queue} \
     --name "adlist-tf-make-example" \
-    --driver-memory 16g --executor-memory 100g \
+    --driver-memory 16g --executor-memory 50g \
     --num-executors 500 --executor-cores 4 \
     --conf spark.hadoop.fs.defaultFS=hdfs://emr-cluster2 \
     --conf "spark.yarn.executor.memoryOverhead=4g" \
