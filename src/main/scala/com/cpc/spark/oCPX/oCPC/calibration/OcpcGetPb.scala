8705865770fcccfa8799708dc721a21d17a4e6d6
wangjun <wangjun02@qutoutiao.net>
2019-07-27 14:02:44 +0800

    fix bug

--- src/main/scala/com/cpc/spark/oCPX/oCPC/calibration/OcpcGetPb.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/calibration/OcpcGetPb.scala
@@ -4,6 +4,9 @@ import com.cpc.spark.oCPX.oCPC.calibration.OcpcJFBfactor._
 import com.cpc.spark.oCPX.oCPC.calibration.OcpcSmoothfactor._
 import com.cpc.spark.oCPX.oCPC.calibration.OcpcCVRfactor._
 import com.cpc.spark.oCPX.oCPC.calibration.OcpcBIDfactor._
+import com.cpc.spark.oCPX.oCPC.calibration.OcpcGetPbDelay._
+import com.cpc.spark.oCPX.OcpcTools._
+import com.typesafe.config.ConfigFactory
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
@@ -52,7 +55,13 @@ object OcpcGetPb {
     val bidFactorData = bidFactorDataRaw
       .select("unitid", "conversion_goal", "exp_tag", "high_bid_factor", "low_bid_factor")
 
-    val data = assemblyData(jfbData, smoothData, pcocData, bidFactorData, spark)
+    val data1 = assemblyData(jfbData, smoothData, pcocData, bidFactorData, spark)
+
+    val data2 = OcpcGetPbDelayMain(date, hour, version, expTag, jfbHourInt, smoothHourInt, bidFactorHourInt, hourInt1, hourInt2, hourInt3, 6, spark)
+
+    val data = selectWeishiCali(expTag, data1, data2, date, hour, spark)
+
+
 
     val resultDF = data
       .withColumn("cpagiven", lit(1.0))
@@ -69,6 +78,79 @@ object OcpcGetPb {
 
 
   }
+
+  def selectWeishiCali(expTag: String, dataRaw1: DataFrame, dataRaw2: DataFrame, date: String, hour: String, spark: SparkSession) = {
+    // 获取oCPC单元中userid与unitid的映射关系
+    val useridToUnitid = getConversionGoal(date, hour, spark)
+    val useridUnitid = useridToUnitid
+      .select("unitid", "userid")
+      .distinct()
+
+    // 从配置文件获取需要特殊化配置的广告主id（微视广告主）
+    val conf = ConfigFactory.load("ocpc")
+    val confPath = conf.getString("exp_tag.weishi")
+    val rawData = spark.read.format("json").json(confPath)
+    rawData.show(10)
+    val confData = rawData
+      .filter(s"exp_tag = '$expTag'")
+      .select("userid")
+      .distinct()
+
+    val flagData = useridUnitid
+      .join(confData, Seq("userid"), "inner")
+      .select("unitid", "userid")
+      .distinct()
+    flagData.show(10)
+
+    val data2 = dataRaw2
+      .join(flagData, Seq("unitid"), "inner")
+      .filter(s"conversion_goal = 2")
+      .withColumn("cvr_factor_bak", col("cvr_factor"))
+      .withColumn("jfb_factor_bak", col("jfb_factor"))
+      .withColumn("post_cvr_bak", col("post_cvr"))
+      .withColumn("high_bid_factor_bak", col("high_bid_factor"))
+      .withColumn("low_bid_factor_bak", col("low_bid_factor"))
+      .withColumn("flag", lit(1))
+      .select("unitid", "cvr_factor_bak", "jfb_factor_bak", "post_cvr_bak", "high_bid_factor_bak", "low_bid_factor_bak", "flag", "conversion_goal", "exp_tag")
+
+    val data1 = dataRaw1
+      .withColumn("cvr_factor_orig", col("cvr_factor"))
+      .withColumn("jfb_factor_orig", col("jfb_factor"))
+      .withColumn("post_cvr_orig", col("post_cvr"))
+      .withColumn("high_bid_factor_orig", col("high_bid_factor"))
+      .withColumn("low_bid_factor_orig", col("low_bid_factor"))
+      .select("unitid", "cvr_factor_orig", "jfb_factor_orig", "post_cvr_orig", "high_bid_factor_orig", "low_bid_factor_orig", "cpagiven", "conversion_goal", "exp_tag", "smooth_factor")
+
+    val data = data1
+      .join(data2, Seq("unitid", "conversion_goal", "exp_tag"), "left_outer")
+      .na.fill(0, Seq("cvr_factor_bak", "jfb_factor_bak", "post_cvr_bak", "high_bid_factor_bak", "low_bid_factor_bak", "flag"))
+      .withColumn("cvr_factor", udfSelectValue()(col("flag"), col("cvr_factor_orig"), col("cvr_factor_bak")))
+      .withColumn("jfb_factor", udfSelectValue()(col("flag"), col("jfb_factor_orig"), col("jfb_factor_bak")))
+      .withColumn("post_cvr", udfSelectValue()(col("flag"), col("post_cvr_orig"), col("post_cvr_bak")))
+      .withColumn("high_bid_factor", udfSelectValue()(col("flag"), col("high_bid_factor_orig"), col("high_bid_factor_bak")))
+      .withColumn("low_bid_factor", udfSelectValue()(col("flag"), col("low_bid_factor_orig"), col("low_bid_factor_bak")))
+      .cache()
+
+    data.show(10)
+
+    val result = data
+      .select("unitid", "cvr_factor", "jfb_factor", "post_cvr", "high_bid_factor", "low_bid_factor", "cpagiven", "conversion_goal", "exp_tag", "smooth_factor")
+
+    result
+
+
+  }
+
+  def udfSelectValue() = udf((flag: Int, valueOrigin: Double, valueBak: Double) => {
+    var result = valueOrigin
+    if (flag == 1) {
+      result = valueBak
+    } else {
+      result = valueOrigin
+    }
+    result
+  })
+
 
   def assemblyData(jfbData: DataFrame, smoothData: DataFrame, pcocData: DataFrame, bidFactorData: DataFrame, spark: SparkSession) = {
     // 组装数据
