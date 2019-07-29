f6febe51e3beb295354bd169b614d8924bf14316
wangjun <wangjun02@qutoutiao.net>
2019-07-29 13:46:21 +0800

    fix bug

--- src/main/scala/com/cpc/spark/oCPX/OcpcTools.scala
+++ src/main/scala/com/cpc/spark/oCPX/OcpcTools.scala
@@ -22,22 +22,9 @@ object OcpcTools {
     val hour = args(1).toString
 
     // 测试实时数据表和离线表
-    val dataRaw1 = getBaseData(24, date, hour, spark)
-    val data1 = dataRaw1
-      .filter(s"isclick=1")
-      .groupBy("unitid", "conversion_goal", "media")
-      .agg(
-        avg(col("exp_cvr")).alias("pre_cvr"),
-        sum(col("isclick")).alias("click"),
-        sum(col("iscvr")).alias("cv")
-      )
-      .withColumn("post_cvr", col("cv") * 1.0 / col("click"))
-    data1
-      .repartition(5)
-      .write.mode("overwrite").saveAsTable("test.check_cv_data20190729a")
-
-//    val dataRaw2 = getRealtimeData(24, date, hour, spark)
-//    val data2 = dataRaw2
+//    val dataRaw1 = getBaseData(24, date, hour, spark)
+//    val data1 = dataRaw1
+//      .filter(s"isclick=1")
 //      .groupBy("unitid", "conversion_goal", "media")
 //      .agg(
 //        avg(col("exp_cvr")).alias("pre_cvr"),
@@ -45,9 +32,22 @@ object OcpcTools {
 //        sum(col("iscvr")).alias("cv")
 //      )
 //      .withColumn("post_cvr", col("cv") * 1.0 / col("click"))
-//    data2
+//    data1
 //      .repartition(5)
-//      .write.mode("overwrite").saveAsTable("test.check_cv_data20190729b")
+//      .write.mode("overwrite").saveAsTable("test.check_cv_data20190729a")
+
+    val dataRaw2 = getRealtimeData(24, date, hour, spark)
+    val data2 = dataRaw2
+      .groupBy("unitid", "conversion_goal", "media")
+      .agg(
+        avg(col("exp_cvr")).alias("pre_cvr"),
+        sum(col("isclick")).alias("click"),
+        sum(col("iscvr")).alias("cv")
+      )
+      .withColumn("post_cvr", col("cv") * 1.0 / col("click"))
+    data2
+      .repartition(5)
+      .write.mode("overwrite").saveAsTable("test.check_cv_data20190729b")
   }
 
   def getConversionGoal(date: String, hour: String, spark: SparkSession) = {
