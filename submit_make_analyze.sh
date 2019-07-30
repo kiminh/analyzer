9a0d81b897efc89cbc00245205dccc911fc76ca2
fenghuabin <fenghuabin@qutoutiao.net>
2019-07-30 15:18:15 +0800

    fhb_start

--- submit_make_analyze.sh
+++ submit_make_analyze.sh
@@ -10,14 +10,12 @@ jars=("/home/cpc/anal/lib/spark-tensorflow-connector_2.11-1.10.0.jar" )
 randjar="fhb_start"`date +%s%N`".jar"
 hadoop fs -get ${jarLib} ${randjar}
 
-
 spark-submit --master yarn --queue ${queue} \
-    --name "adlist-tf-make-example" \
+    --name "adlist-tf-make-analyze" \
     --driver-memory 16g --executor-memory 16g \
     --num-executors 50 --executor-cores 4 \
     --conf spark.hadoop.fs.defaultFS=hdfs://emr-cluster2 \
     --conf "spark.yarn.executor.memoryOverhead=4g" \
     --conf "spark.sql.shuffle.partitions=500" \
     --jars $( IFS=$','; echo "${jars[*]}" ) \
-    --class com.cpc.spark.ml.dnn.baseData.MakeAnalyze \
-    ${randjar}
+    --class com.cpc.spark.ml.dnn.baseData.MakeAnalyze ${randjar}
