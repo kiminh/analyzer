045ae234cfb0bfd8bbfadf8cb1e577f41898c1d2
wangjun <wangjun02@qutoutiao.net>
2019-07-31 21:28:46 +0800

    fix bug

---
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/calibrationv2/OcpcCalibrationBaseDelay.scala
@@ -0,0 +1,68 @@
+package com.cpc.spark.oCPX.oCPC.calibrationv2
+
+import com.cpc.spark.oCPX.OcpcTools._
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+
+object OcpcCalibrationBaseDelay {
+  def main(args: Array[String]): Unit = {
+    /*
+    动态计算alpha平滑系数
+    1. 基于原始pcoc，计算预测cvr的量纲系数
+    2. 二分搜索查找到合适的平滑系数
+     */
+    val spark = SparkSession.builder().enableHiveSupport().getOrCreate()
+    Logger.getRootLogger.setLevel(Level.WARN)
+
+    val date = args(0).toString
+    val hour = args(1).toString
+    val hourInt = args(2).toInt
+    println("parameters:")
+    println(s"date=$date, hour=$hour, hourInt:$hourInt")
+
+    val result = OcpcCalibrationBaseDelayMain(date, hour, hourInt, spark)
+    result
+      .repartition(10).write.mode("overwrite").saveAsTable("test.check_base_factor20190731a")
+  }
+
+  def OcpcCalibrationBaseDelayMain(date: String, hour: String, hourInt: Int, spark: SparkSession) = {
+    /*
+    动态计算alpha平滑系数
+    1. 基于原始pcoc，计算预测cvr的量纲系数
+    2. 二分搜索查找到合适的平滑系数
+     */
+    val baseData = getBaseDataDelay(hourInt, date, hour, spark)
+
+    // 计算结果
+    val result = calculateParameter(baseData, spark)
+
+    val resultDF = result
+      .select("unitid", "conversion_goal", "media", "click", "cv", "pre_cvr", "post_cvr", "pcoc", "acb", "acp")
+
+
+    resultDF
+  }
+
+
+
+  def calculateParameter(rawData: DataFrame, spark: SparkSession) = {
+    val data  =rawData
+      .filter(s"isclick=1")
+      .groupBy("unitid", "conversion_goal", "media")
+      .agg(
+        sum(col("isclick")).alias("click"),
+        sum(col("iscvr")).alias("cv"),
+        avg(col("bid")).alias("acb"),
+        avg(col("price")).alias("acp"),
+        avg(col("exp_cvr")).alias("pre_cvr")
+      )
+      .withColumn("post_cvr", col("cv") * 1.0 / col("click"))
+      .withColumn("pcoc", col("pre_cvr") * 1.0 / col("post_cvr"))
+      .select("unitid", "conversion_goal", "media", "click", "cv", "pre_cvr", "post_cvr", "pcoc", "acb", "acp")
+
+    data
+  }
+
+}
