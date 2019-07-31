f7bba06fcd1934c59bfee1f9bcad8cdb728efc10
wangjun <wangjun02@qutoutiao.net>
2019-07-31 11:38:32 +0800

    fix bug

--- src/main/scala/com/cpc/spark/oCPX/oCPC/calibration/OcpcCVRfactorV2.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/calibration/OcpcCVRfactorV2.scala
@@ -1,6 +1,8 @@
 package com.cpc.spark.oCPX.oCPC.calibration
 
+import com.cpc.spark.oCPX.OcpcTools.{udfMediaName, udfSetExpTag}
 import com.cpc.spark.oCPX.oCPC.calibration.OcpcCalibrationBase._
+import com.typesafe.config.ConfigFactory
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SparkSession}
@@ -41,11 +43,33 @@ object OcpcCVRfactorV2 {
   }
 
   def OcpcCVRfactorV2Main(date: String, hour: String, version: String, expTag: String, hourInt1: Int, hourInt2: Int, hourInt3: Int, spark: SparkSession) = {
-    val data1 = OcpcCalibrationBaseMain(date, hour, version, expTag, hourInt1, spark).cache()
+    // cvr实验配置文件
+    // min_cv:配置文件中如果为负数或空缺，则用默认值40，其他情况使用设定值
+    val expConf = getExpConf(version, spark)
+
+    val dataRaw1 = OcpcCalibrationBaseMain(date, hour, hourInt1, spark)
+    val data1 = dataRaw1
+        .withColumn("media", udfMediaName()(col("media")))
+        .withColumn("exp_tag", udfSetExpTag(expTag)(col("media")))
+        .join(expConf, Seq("conversion_goal", "exp_tag"), "left_outer")
+        .na.fill(40, Seq("min_cv"))
+        .cache()
     data1.show(10)
-    val data2 = OcpcCalibrationBaseMain(date, hour, version, expTag, hourInt2, spark).cache()
+    val dataRaw2 = OcpcCalibrationBaseMain(date, hour, hourInt2, spark)
+    val data2 = dataRaw2
+      .withColumn("media", udfMediaName()(col("media")))
+      .withColumn("exp_tag", udfSetExpTag(expTag)(col("media")))
+      .join(expConf, Seq("conversion_goal", "exp_tag"), "left_outer")
+      .na.fill(40, Seq("min_cv"))
+      .cache()
     data2.show(10)
-    val data3 = OcpcCalibrationBaseMain(date, hour, version, expTag, hourInt3, spark).cache()
+    val dataRaw3 = OcpcCalibrationBaseMain(date, hour, hourInt3, spark)
+    val data3 = dataRaw3
+      .withColumn("media", udfMediaName()(col("media")))
+      .withColumn("exp_tag", udfSetExpTag(expTag)(col("media")))
+      .join(expConf, Seq("conversion_goal", "exp_tag"), "left_outer")
+      .na.fill(40, Seq("min_cv"))
+      .cache()
     data3.show(10)
 
     val calibration1 = calculateCalibrationValue(data1, data2, spark)
@@ -69,6 +93,22 @@ object OcpcCVRfactorV2 {
 
     resultDF
 
+  }
+
+  def getExpConf(version: String, spark: SparkSession) ={
+    // 从配置文件读取数据
+    val conf = ConfigFactory.load("ocpc")
+    val confPath = conf.getString("exp_config.cvr_factor")
+    val rawData = spark.read.format("json").json(confPath)
+    val data = rawData
+      .filter(s"version = '$version'")
+      .select("exp_tag", "conversion_goal", "min_cv")
+      .distinct()
+
+    println("cvr factor config:")
+    data.show(10)
+
+    data
   }
 
   def calculateCalibrationValue(dataRaw1: DataFrame, dataRaw2: DataFrame, spark: SparkSession) = {
