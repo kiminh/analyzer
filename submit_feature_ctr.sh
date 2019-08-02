4c218daaf8d7b6df2961a954bcc87668996e5da0
fenghuabin <fenghuabin@qutoutiao.net>
2019-08-02 10:51:40 +0800

    fhb_start

--- submit_feature_ctr.sh
+++ submit_feature_ctr.sh
@@ -31,9 +31,9 @@ collect_date_begin=`date --date='36 days ago' +%Y-%m-%d`
 collect_date_end=`date --date='9 days ago' +%Y-%m-%d`
 ctr_feature_date=`date --date='8 days ago' +%Y-%m-%d`
 
-#collect_date_begin=2019-06-01
-#collect_date_end=2019-07-31
-#ctr_feature_date=2019-07-31
+collect_date_begin=2019-06-14
+collect_date_end=2019-06-25
+ctr_feature_date=2019-07-23
 
 echo ${ctr_feature_date}
 echo ${collect_date_begin}
