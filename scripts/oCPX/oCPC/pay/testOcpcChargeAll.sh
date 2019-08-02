3f3fed21864f91aaf58c12408077ea6f684e45e3
wangjun <wangjun02@qutoutiao.net>
2019-08-02 16:12:37 +0800

    fix bug

--- scripts/oCPX/oCPC/pay/testOcpcChargeAll.sh
+++ scripts/oCPX/oCPC/pay/testOcpcChargeAll.sh
@@ -20,7 +20,7 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.yarn.executor.memoryOverhead=4g'\
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
-    --class com.cpc.spark.OcpcProtoType.charge.OcpcChargeAll \
+    --class com.cpc.spark.oCPX.oCPC.pay.OcpcChargeAll \
     /home/cpc/wangjun/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $1 $2 $3
 
 
