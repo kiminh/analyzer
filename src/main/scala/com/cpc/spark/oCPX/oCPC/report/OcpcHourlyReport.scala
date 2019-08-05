8b55c1559e2565c4b52acd80fc4f18f47880c085
wangjun <wangjun02@qutoutiao.net>
2019-08-05 13:56:33 +0800

    fix bug

--- src/main/scala/com/cpc/spark/oCPX/oCPC/report/OcpcHourlyReport.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/report/OcpcHourlyReport.scala
@@ -36,40 +36,40 @@ object OcpcHourlyReport {
     // 分ideaid和conversion_goal统计数据
     val baseData = calculateBaseData(rawData, spark)
 
-    // 为邮件准备临时表
-    val ideaData = calculateIdea(baseData, spark)
-    ideaData
-      .withColumn("date", lit(date))
-      .withColumn("hour", lit(hour))
-      .repartition(5)
-      .write.mode("overwrite").saveAsTable("test.ocpc_hourly_idea_report_email20190805")
-
-    val unitData = calculateUnit(baseData, spark)
-    unitData
-      .withColumn("date", lit(date))
-      .withColumn("hour", lit(hour))
-      .repartition(5)
-      .write.mode("overwrite").saveAsTable("test.ocpc_hourly_unit_report_email20190805")
-
-    val userData = calcualteUser(unitData, spark)
-    userData
-      .withColumn("date", lit(date))
-      .withColumn("hour", lit(hour))
-      .repartition(5)
-      .write.mode("overwrite").saveAsTable("test.ocpc_hourly_user_report_email20190805")
-
-    val industry = calculateIndustry(unitData, spark)
-    industry
-      .withColumn("date", lit(date))
-      .withColumn("hour", lit(hour))
-      .repartition(5)
-      .write.mode("overwrite").saveAsTable("test.ocpc_hourly_industry_report_email20190805")
+//    // 为邮件准备临时表
+//    val ideaData = calculateIdea(baseData, spark)
+//    ideaData
+//      .withColumn("date", lit(date))
+//      .withColumn("hour", lit(hour))
+//      .repartition(5)
+//      .write.mode("overwrite").saveAsTable("test.ocpc_hourly_idea_report_email")
+//
+//    val unitData = calculateUnit(baseData, spark)
+//    unitData
+//      .withColumn("date", lit(date))
+//      .withColumn("hour", lit(hour))
+//      .repartition(5)
+//      .write.mode("overwrite").saveAsTable("test.ocpc_hourly_unit_report_email")
+//
+//    val userData = calcualteUser(unitData, spark)
+//    userData
+//      .withColumn("date", lit(date))
+//      .withColumn("hour", lit(hour))
+//      .repartition(5)
+//      .write.mode("overwrite").saveAsTable("test.ocpc_hourly_user_report_email")
+//
+//    val industry = calculateIndustry(unitData, spark)
+//    industry
+//      .withColumn("date", lit(date))
+//      .withColumn("hour", lit(hour))
+//      .repartition(5)
+//      .write.mode("overwrite").saveAsTable("test.ocpc_hourly_industry_report_email")
 
     // 存储数据到hadoop
     saveDataToHDFS(baseData, date, hour, spark)
 
-//    // 存储数据到mysql
-//    saveDataToMysql(baseData, date, hour, spark)
+    // 存储数据到mysql
+    saveDataToMysql(baseData, date, hour, spark)
 
   }
 
