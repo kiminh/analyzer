563ec5cecaba499387d44f0e23929b4c1841972d
wangjun <wangjun02@qutoutiao.net>
2019-08-05 19:23:57 +0800

    fix bug

--- src/main/scala/com/cpc/spark/oCPX/monitor/OcpcConversionDelayMonitor.scala
+++ src/main/scala/com/cpc/spark/oCPX/monitor/OcpcConversionDelayMonitor.scala
@@ -40,8 +40,8 @@ object OcpcConversionDelayMonitor {
       .withColumn("date", lit(date))
       .withColumn("hour", lit(hour))
       .repartition(5)
-//      .write.mode("overwrite").insertInto("test.ocpc_total_delay_cvr_hourly")
-      .write.mode("overwrite").insertInto("dl_cpc.ocpc_total_delay_cvr_hourly")
+      .write.mode("overwrite").insertInto("test.ocpc_total_delay_cvr_hourly")
+//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_total_delay_cvr_hourly")
   }
 
   def calculateLostCvr(baseData: DataFrame, spark: SparkSession) = {
