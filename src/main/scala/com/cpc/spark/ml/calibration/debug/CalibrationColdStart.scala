173ab8b3db868308d90ef47ec2b64453bbf17cfa
wangyao <wangyao@qutoutiao.net>
2019-08-06 20:25:13 +0800

    -

--- src/main/scala/com/cpc/spark/ml/calibration/debug/CalibrationColdStart.scala
+++ src/main/scala/com/cpc/spark/ml/calibration/debug/CalibrationColdStart.scala
@@ -9,6 +9,7 @@ import mlmodel.mlmodel.{IRModel, PostCalibrations}
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, concat_ws, udf, _}
+import com.cpc.spark.ml.calibration.debug.UidGroup.hash
 //import com.cpc.spark.ml.calibration.MultiDimensionCalibOnQtt.computeCalibration
 
 /**
@@ -20,14 +21,12 @@ object CalibrationColdStart{
     Logger.getRootLogger.setLevel(Level.WARN)
 
     val spark = SparkSession.builder().enableHiveSupport().getOrCreate()
-//    val modelPath = args(0)
-//    val dt = args(1)
+    val dt = args(0)
 //    val hour = args(2)
 //    val modelName = args(3)
-//
-//
-//    println(s"modelPath=$modelPath")
-//    println(s"dt=$dt")
+
+
+    println(s"dt=$dt")
 //    println(s"hour=$hour")
 //    println(s"modelName=$modelName")
 
@@ -36,41 +35,46 @@ object CalibrationColdStart{
     import session.implicits._
 
     val dnn_data = spark.read.parquet("hdfs://emr-cluster/user/cpc/wy/dnn_prediction/adcvr-v1wzjf/result-*")
-      .toDF()
+      .toDF("id","prediction","num")
 
     dnn_data.show(10)
     println("sum is %d".format(dnn_data.count()))
     // get union log
 
 
-//    val sql = s"""
-//                 |select a.searchid, substring(a.adclass,1,6) as adclass,
-//                 |a.ideaid,coalesce(c.iscvr,0) as iscvr
-//                 |from dl_cpc.cpc_basedata_union_events a
-//                 |join dl_cpc.dw_unitid_conversion_target b
-//                 |    on a.unitid = b.unitid
-//                 |    and b.day = '$dt'
-//                 |    and b.conversion_target[0] not in ('none','site_uncertain')
-//                 |left join
-//                 |  (
-//                 |    select distinct aa.searchid,aa.ideaid,1 as iscvr,
-//                 |       bb.conversion_target[0] as real_target
-//                 |    from dl_cpc.cpc_conversion aa
-//                 |    join dl_cpc.dw_unitid_conversion_target bb
-//                 |      on aa.unitid=bb.unitid
-//                 |      and bb.day = '$dt'
-//                 |      and bb.conversion_target[0] not in ('none','site_uncertain')
-//                 |      and array_contains(aa.conversion_target,bb.conversion_target[0])
-//                 |      where aa.day = '$dt'
-//                 |      ) c
-//                 |  on a.searchid = c.searchid and a.ideaid=c.ideaid
-//                 |  where a.day ='$dt'
-//                 |  and a.media_appsid in ('80000001','80000002') and a.isclick = 1
-//                 |  and a.adsrc = 1
-//                 |  AND (charge_type IS NULL OR charge_type = 1)
-//       """.stripMargin
-//    println(s"sql:\n$sql")
-//    val basedata = session.sql(sql)
+    val sql = s"""
+                 |select a.searchid, substring(a.adclass,1,6) as adclass,
+                 |a.ideaid,coalesce(c.iscvr,0) as iscvr
+                 |from dl_cpc.cpc_basedata_union_events a
+                 |join dl_cpc.dw_unitid_conversion_target b
+                 |    on a.unitid = b.unitid
+                 |    and b.day = '$dt'
+                 |    and b.conversion_target[0] not in ('none','site_uncertain')
+                 |left join
+                 |  (
+                 |    select distinct aa.searchid,aa.ideaid,1 as iscvr,
+                 |       bb.conversion_target[0] as real_target
+                 |    from dl_cpc.cpc_conversion aa
+                 |    join dl_cpc.dw_unitid_conversion_target bb
+                 |      on aa.unitid=bb.unitid
+                 |      and bb.day = '$dt'
+                 |      and bb.conversion_target[0] not in ('none','site_uncertain')
+                 |      and array_contains(aa.conversion_target,bb.conversion_target[0])
+                 |      where aa.day = '$dt'
+                 |      ) c
+                 |  on a.searchid = c.searchid and a.ideaid=c.ideaid
+                 |  where a.day ='$dt'
+                 |  and a.media_appsid in ('80000001','80000002') and a.isclick = 1
+                 |  and a.adsrc = 1
+                 |  AND (charge_type IS NULL OR charge_type = 1)
+       """.stripMargin
+    println(s"sql:\n$sql")
+    val basedata = session.sql(sql)
+      .withColumn("id",hash(0)(col("searchid")))
+      .join(dnn_data,Seq("id"),"inner")
+
+    basedata.show(10)
+    println("sum is %d".format(basedata.count()))
 
   }
 
