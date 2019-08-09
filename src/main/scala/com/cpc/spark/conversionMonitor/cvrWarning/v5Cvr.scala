ba47f27fddd71a00305ede7c16fae2684de07d1a
wangjun <wangjun02@qutoutiao.net>
2019-08-09 23:54:26 +0800

    fix bug

--- src/main/scala/com/cpc/spark/conversionMonitor/cvrWarning/v5Cvr.scala
+++ src/main/scala/com/cpc/spark/conversionMonitor/cvrWarning/v5Cvr.scala
@@ -24,7 +24,7 @@ object v5Cvr {
       .union(cv4Data)
 
     val dataToday = clickData
-      .join(cvData)
+      .join(cvData, Seq("searchid"), "left_outer")
       .na.fill(0, Seq("iscvr"))
       .groupBy("conversion_goal", "hour")
       .agg(
