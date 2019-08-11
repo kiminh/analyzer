f2b3fa58667d898b60dd761a3efcb0f51e16e021
冯华宾 <fenghuabin@qutoutiao.net>
2019-08-11 21:16:53 +0800

    fhb_start

--- submit_make_sampling.sh
+++ submit_make_sampling.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-cur_date="2019-06-11-bak"
-cur_date=$1
+#cur_date=$1
 #modelVersion=$2
 #today=`date -d "${cur_time} 1 hours ago" +%Y-%m-%d`
 #hour=`date -d "${cur_time} 1 hours ago" +%H`
@@ -20,8 +19,8 @@ src_dir="hdfs://emr-cluster/user/cpc/aiclk_dataflow/daily/adlist-v4"
 date_begin=`date --date='31 days ago' +%Y-%m-%d`
 date_end=`date --date='1 days ago' +%Y-%m-%d`
 des_dir="hdfs://emr-cluster/user/cpc/fenghuabin/adlist-v4-sampled"
-test_data_src="2019-08-03/part-r-000*"
-test_data_des="test-2019-08-03"
+test_data_src="2019-08-10/part-r-000*"
+test_data_des="test-2019-08-10"
 test_data_week="Sat"
 partitions=1000
 
