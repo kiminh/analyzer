530a96f295aa8bf1f3eae1be85c56da7f3fb189e
wangyao <wangyao@qutoutiao.net>
2019-08-14 14:00:43 +0800

    -

--- src/main/scala/com/cpc/spark/ml/calibration/MultiDimensionCalibrationOnQttCtr.scala
+++ src/main/scala/com/cpc/spark/ml/calibration/MultiDimensionCalibrationOnQttCtr.scala
@@ -105,7 +105,8 @@ object MultiDimensionCalibrationOnQttCtr {
     val data4 = log.join(group4,Seq("adclass"),"inner")
     val calimap4 = GroupToConfig(data4, session,model)
 
-    val calimap = calimap1 ++ calimap2 ++ calimap3 ++ calimap4
+    val calimap5 = GroupToConfig(log.withColumn("group",lit("0")), session,model)
+    val calimap = calimap1 ++ calimap2 ++ calimap3 ++ calimap4 ++ calimap5
     val califile = PostCalibrations(calimap.toMap)
     val localPath = saveProtoToLocal2(model, califile)
     saveFlatTextFileForDebug2(model, califile)
