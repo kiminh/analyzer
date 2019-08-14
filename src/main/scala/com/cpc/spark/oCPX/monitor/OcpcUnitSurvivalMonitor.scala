7534234378b74f8dd748f8f923c8faf111039d93
wangjun <wangjun02@qutoutiao.net>
2019-08-14 21:11:11 +0800

    fix bug

--- src/main/scala/com/cpc/spark/oCPX/monitor/OcpcUnitSurvivalMonitor.scala
+++ src/main/scala/com/cpc/spark/oCPX/monitor/OcpcUnitSurvivalMonitor.scala
@@ -32,16 +32,125 @@ object OcpcUnitSurvivalMonitor {
       .repartition(5)
       .write.mode("overwrite").insertInto("dl_cpc.unit_cost_by_media_daily")
 
-//    // 分小时计算cvr
-//    val data = calculateCvr(baseData, date, spark).cache()
-//
-//    data.show(10)
-//    data
-//      .select("unitid", "userid", "conversion_goal", "media", "click", "click_cv", "show_cv", "date", "hour", "hour_int")
-//      .repartition(5)
-////      .write.mode("overwrite").insertInto("test.ocpc_cvr_delay_hourly")
-//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_cvr_delay_hourly")
+    // 统计近各个媒体，近七天的单元数、ocpc消费、非ocpc消费，新单元的消费数，近七天相对八天前有多少单元停止消费
+    val data = calculateBaseData(newUnits, date, spark)
 
+    // 存储基础数据
+    data
+      .withColumn("date", lit(date))
+      .select("media", "is_ocpc", "unitid_cnt", "userid_cnt", "cost", "data_type", "date")
+      .repartition(5)
+      .write.mode("overwrite").insertInto("test.unit_changing_by_media_daily")
+
+
+  }
+
+  def calculateBaseData(newUnits: DataFrame, date: String, spark: SparkSession) = {
+    // 统计近各个媒体，近七天的单元数、ocpc消费、非ocpc消费，新单元的消费数，近七天相对八天前有多少单元停止消费
+    // 取历史数据
+    val dateConverter = new SimpleDateFormat("yyyy-MM-dd")
+    val today = dateConverter.parse(date)
+    val calendar = Calendar.getInstance
+    calendar.setTime(today)
+    calendar.add(Calendar.DATE, -7)
+    val rawDate1 = calendar.getTime
+    val date1 = dateConverter.format(rawDate1)
+    calendar.add(Calendar.DATE, -7)
+    val rawDate2 = calendar.getTime
+    val date2 = dateConverter.format(rawDate2)
+
+    val sqlRequest1 =
+      s"""
+         |SELECT
+         |  unitid,
+         |  userid,
+         |  media,
+         |  is_ocpc,
+         |  sum(show) as show,
+         |  sum(click) as click,
+         |  sum(cost) * 0.01 as cost
+         |FROM
+         |  dl_cpc.unit_cost_by_media_daily
+         |WHERE
+         |  `date` >= '$date1'
+         |GROUP BY unitid, userid, media, is_ocpc
+       """.stripMargin
+    println(sqlRequest1)
+    val baseData = spark
+      .sql(sqlRequest1)
+      .withColumn("flag", lit(1))
+
+    val sqlRequest2 =
+      s"""
+         |SELECT
+         |  unitid,
+         |  userid,
+         |  media,
+         |  is_ocpc,
+         |  sum(show) as show,
+         |  sum(click) as click,
+         |  sum(cost) * 0.01 as cost
+         |FROM
+         |  dl_cpc.unit_cost_by_media_daily
+         |WHERE
+         |  `date` >= '$date2'
+         |and
+         |  `date` < '$date1'
+         |GROUP BY unitid, userid, media, is_ocpc
+       """.stripMargin
+    println(sqlRequest2)
+    val prevData = spark.sql(sqlRequest2)
+
+
+    val newData = newUnits
+      .withColumn("new_flag", col("flag"))
+      .select("unitid",  "userid", "media", "is_ocpc")
+
+
+    // 各个媒体，近七天的ocpc, 非ocpc单元数、ocpc消费、非ocpc消费
+    val unitCnt = baseData
+      .groupBy("media", "is_ocpc")
+      .agg(
+        countDistinct(col("unitid")).alias("unitid_cnt"),
+        countDistinct(col("userid")).alias("userid_cnt"),
+        sum(col("cost")).alias("cost")
+      )
+      .select("media", "is_ocpc", "unitid_cnt", "userid_cnt", "cost")
+      .withColumn("data_type", lit("complete"))
+
+    // 各个媒体，近七天新增的的ocpc, 非ocpc单元数、ocpc消费、非ocpc消费
+    val newCnt = baseData
+      .join(newData, Seq("unitid",  "userid", "media", "is_ocpc"), "inner")
+      .groupBy("media", "is_ocpc")
+      .agg(
+        countDistinct(col("unitid")).alias("unitid_cnt"),
+        countDistinct(col("userid")).alias("userid_cnt"),
+        sum(col("cost")).alias("cost")
+      )
+      .select("media", "is_ocpc", "unitid_cnt", "userid_cnt", "cost")
+      .withColumn("data_type", lit("new"))
+
+    // 各个媒体上一个七天周期中停投单元数和消费
+    val baseList = baseData
+      .select("unitid",  "userid", "media", "is_ocpc", "flag")
+    val lostCnt = prevData
+      .join(baseList, Seq("unitid",  "userid", "media", "is_ocpc"), "left_outer")
+      .na.fill(0, Seq("flag"))
+      .filter(s"flag = 0")
+      .groupBy("media", "is_ocpc")
+      .agg(
+        countDistinct(col("unitid")).alias("unitid_cnt"),
+        countDistinct(col("userid")).alias("userid_cnt"),
+        sum(col("cost")).alias("cost")
+      )
+      .select("media", "is_ocpc", "unitid_cnt", "userid_cnt", "cost")
+      .withColumn("data_type", lit("lost"))
+
+    val result = unitCnt.union(newCnt).union(lostCnt).cache()
+
+    result.show()
+
+    result
   }
 
   def getData(date: String, spark: SparkSession) = {
@@ -120,10 +229,6 @@ object OcpcUnitSurvivalMonitor {
     val data = spark.sql(sqlRequest)
       .select("unitid",  "userid", "media_appsid", "is_ocpc", "create_time", "create_date")
       .withColumn("media", udfDetermineMedia()(col("media_appsid")))
-
-    data
-      .repartition(5)
-      .write.mode("overwrite").saveAsTable("test.check_ocpc_report20190814")
 
     val resultDF = data
         .filter(s"create_date > '$date1'")
