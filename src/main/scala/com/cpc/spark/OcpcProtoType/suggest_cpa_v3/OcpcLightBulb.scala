2d805f46e7dd3d409303a4b885b3d639e39d4995
wangjun <wangjun02@qutoutiao.net>
2019-08-15 14:33:58 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/suggest_cpa_v3/OcpcLightBulb.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/suggest_cpa_v3/OcpcLightBulb.scala
@@ -46,8 +46,8 @@ object OcpcLightBulb{
       .repartition(5)
       .select("unitid", "userid", "adclass", "media", "cpa", "version")
       .repartition(5)
-      .write.mode("overwrite").insertInto("test.ocpc_unit_light_control_version")
-//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_unit_light_control_version")
+//      .write.mode("overwrite").insertInto("test.ocpc_unit_light_control_version")
+      .write.mode("overwrite").insertInto("dl_cpc.ocpc_unit_light_control_version")
 
     // 根据上一个小时的灯泡数据，分别判断需要熄灭和点亮的灯泡
     val result = getUpdateTableV2(currentLight, date, hour, version, spark)
@@ -69,8 +69,8 @@ object OcpcLightBulb{
 
     resultDF
       .repartition(5)
-      .write.mode("overwrite").insertInto("test.ocpc_light_api_control_hourly")
-//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_light_api_control_hourly")
+//      .write.mode("overwrite").insertInto("test.ocpc_light_api_control_hourly")
+      .write.mode("overwrite").insertInto("dl_cpc.ocpc_light_api_control_hourly")
 
 //    // 存入redis
 //    saveDataToRedis(version, date, hour, spark)
