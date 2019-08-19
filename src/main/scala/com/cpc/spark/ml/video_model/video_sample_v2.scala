904bbdad64940e193c7b1249ca1016d6a0a23aac
duanguangdong <duanguangdong@qutoutiao.net>
2019-08-19 16:01:47 +0800

    update

--- src/main/scala/com/cpc/spark/ml/video_model/video_sample_v2.scala
+++ src/main/scala/com/cpc/spark/ml/video_model/video_sample_v2.scala
@@ -6,7 +6,7 @@ import java.util.Calendar
 import com.cpc.spark.common.Murmur3Hash
 import com.cpc.spark.ml.dnn.Utils.CommonUtils
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.functions.{expr, udf}
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object video_sample_v2 {
@@ -33,14 +33,14 @@ object video_sample_v2 {
   def getSample(spark: SparkSession, date: String): DataFrame = {
     import spark.implicits._
     var original_sample: DataFrame = null
-    original_sample = spark.read.format("tfrecords").option("recordType", "Example").load(s"hdfs://emr-cluster/user/cpc/aiclk_dataflow/daily/adlist-v4/$date/part*").
+    original_sample = spark.read.format("tfrecords").option("recordType", "Example").load(s"hdfs://emr-cluster/user/cpc/aiclk_dataflow/daily/adlist-v4/$date/part*9").
       select($"sample_idx",$"idx0",$"idx1",$"idx2",$"id_arr", $"label", $"dense",expr("dense[8]").alias("adtypehash"),expr("dense[11]").alias("ideaidhash"))
     val adtype8_hash = Murmur3Hash.stringHash64("f8" + "#" + "8", 0)
     val adtype10_hash = Murmur3Hash.stringHash64("f8" + "#" + "10", 0)
     val video_sample = original_sample.where(s"adtypehash in ($adtype8_hash, $adtype10_hash)").select($"sample_idx",$"idx0",$"idx1",$"idx2",$"id_arr", $"label", $"dense", $"ideaidhash")
     val ideaid_feature = spark.sql(s"""select ideaid, resource from dl_cpc.resource where model='video'""").select(hash("f11")($"ideaid").alias("ideaidhash"),$"resource")
     val result = video_sample.join(ideaid_feature, Seq("ideaidhash"), "left_outer").
-      select($"sample_idx", $"idx0", $"idx1", $"idx2", $"id_arr", $"label", $"dense", hash("f" + 28)($"resource").alias("resourceidhash")).rdd.map{
+      select($"sample_idx", $"idx0", $"idx1", $"idx2", $"id_arr", $"label", $"dense", array(hash("f" + 28)($"resource")).alias("resourceidhash")).rdd.map{
       r =>
         val sample_idx = r.getAs[Long]("sample_idx")
         val label = r.getAs[Seq[Long]]("label")
