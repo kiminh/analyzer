01756ba19e6755f9406d863182e98ad86acc12ad
冯华宾 <fenghuabin@qutoutiao.net>
2019-08-19 16:47:11 +0800

    fhb_start

---
+++ src/main/scala/com/cpc/spark/ml/dnn/baseData/MakeUserIdStatistics.scala
@@ -0,0 +1,159 @@
+package com.cpc.spark.ml.dnn.baseData
+
+import java.io.{File, PrintWriter}
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import org.apache.commons.lang3.time.{DateFormatUtils, DateUtils}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.types.{StructField, _}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+
+import scala.collection.mutable.ArrayBuffer
+import scala.sys.process._
+import scala.util.Random
+
+/**
+  * 解析tfrecord到hdfs并下采样
+  * created time : 2019/07/13 10:38
+  * @author fenghuabin
+  * @version 1.0
+  *
+  */
+
+object MakeUserIdStatistics {
+
+
+
+  def delete_hdfs_path(path: String): Unit = {
+
+    val conf = new org.apache.hadoop.conf.Configuration()
+    val p = new org.apache.hadoop.fs.Path(path)
+    val hdfs = p.getFileSystem(conf)
+    val hdfs_path = new org.apache.hadoop.fs.Path(path.toString)
+
+    //val hdfs_path = new org.apache.hadoop.fs.Path(path.toString)
+    //val hdfs = org.apache.hadoop.fs.FileSystem.get(new org.apache.hadoop.conf.Configuration())
+    if (hdfs.exists(hdfs_path)) {
+      hdfs.delete(hdfs_path, true)
+    }
+  }
+
+  def exists_hdfs_path(path: String): Boolean = {
+
+    val conf = new org.apache.hadoop.conf.Configuration()
+    val p = new org.apache.hadoop.fs.Path(path)
+    val hdfs = p.getFileSystem(conf)
+    val hdfs_path = new org.apache.hadoop.fs.Path(path.toString)
+    //val hdfs = org.apache.hadoop.fs.FileSystem.get(new org.apache.hadoop.conf.Configuration())
+
+    if (hdfs.exists(hdfs_path)) {
+      true
+    } else {
+      false
+    }
+  }
+
+  def writeNum2File(file: String, num: Long): Unit = {
+    val writer = new PrintWriter(new File(file))
+    writer.write(num.toString)
+    writer.close()
+  }
+
+  def formatDate(date: Date, pattern: String="yyyy-MM-dd"): String = {
+    val formatDate = DateFormatUtils.format(date, pattern)
+    formatDate
+  }
+
+  def GetDataRangeWithWeek(beginStr: String, endStr: String, format : String = "yyyy-MM-dd"): ArrayBuffer[String] = {
+    val ranges = ArrayBuffer[String]()
+    val sdf = new SimpleDateFormat(format)
+    var dateBegin = sdf.parse(beginStr)
+    val dateEnd = sdf.parse(endStr)
+    while (dateBegin.compareTo(dateEnd) <= 0) {
+      ranges += sdf.format(dateBegin) + ";" + DateFormatUtils.format(dateBegin, "E")
+      dateBegin = DateUtils.addDays(dateBegin, 1)
+    }
+    ranges
+  }
+
+  def GetDataRange(beginStr: String, endStr: String, format : String = "yyyy-MM-dd"): ArrayBuffer[String] = {
+    val ranges = ArrayBuffer[String]()
+    val sdf = new SimpleDateFormat(format)
+    var dateBegin = sdf.parse(beginStr)
+    val dateEnd = sdf.parse(endStr)
+    while (dateBegin.compareTo(dateEnd) <= 0) {
+      ranges += sdf.format(dateBegin)
+      dateBegin = DateUtils.addDays(dateBegin, 1)
+    }
+    ranges
+  }
+
+  def main(args: Array[String]): Unit = {
+    if (args.length != 4) {
+      System.err.println(
+        """
+          |you have to input 4 parameters !!!
+        """.stripMargin)
+      System.exit(1)
+    }
+    //val Array(src, des_dir, des_date, des_map_prefix, numPartitions) = args
+    val Array(src_dir, date_begin, date_end, des_dir) = args
+
+    println(args)
+
+    Logger.getRootLogger.setLevel(Level.WARN)
+
+    val sparkConf = new SparkConf()
+    sparkConf.set("spark.driver.maxResultSize", "5g")
+    val spark = SparkSession.builder().config(sparkConf).enableHiveSupport().getOrCreate()
+    val sc = spark.sparkContext
+
+    //val src_date_list = src_date_str.split(";")
+    val src_date_list = ArrayBuffer[String]()
+    val src_date_list_with_week = GetDataRangeWithWeek(date_begin, date_end)
+    for (pair <- src_date_list_with_week) {
+      src_date_list += pair.split(";")(0)
+    }
+    println("src_date_list:" + src_date_list.mkString(";"))
+
+    /** **********make text examples ************************/
+    println("Make text examples")
+    for (date_idx <- src_date_list.indices) {
+      val src_date = src_date_list(date_idx)
+      val curr_file_src = src_dir + "/" + src_date
+      val tf_text = des_dir + "/" + src_date + "-text"
+      if (!exists_hdfs_path(tf_text) && exists_hdfs_path(curr_file_src)) {
+        val curr_file_src_collect = src_dir + "/" + src_date + "/part-r-*"
+        println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        val importedDf: DataFrame = spark.read.format("tfrecords").option("recordType", "Example").load(curr_file_src_collect)
+
+        importedDf.rdd.map(
+          rs => {
+            val idx2 = rs.getSeq[Long](0)
+            val idx1 = rs.getSeq[Long](1)
+            val idx_arr = rs.getSeq[Long](2)
+            val idx0 = rs.getSeq[Long](3)
+            val sample_idx = rs.getLong(4)
+            val label_arr = rs.getSeq[Long](5)
+            val dense = rs.getSeq[Long](6)
+
+            //11,12,13,14
+            //,ideaid,unitid,planid,userid
+            (dense(11).toString, dense(12).toString, dense(13).toString, dense(14).toString)
+
+            val output = scala.collection.mutable.ArrayBuffer[String]()
+            output += dense(11).toString
+            output += dense(12).toString
+            output += dense(13).toString
+            output += dense(14).toString
+            output.mkString("\t")
+          }
+        ).repartition(10).saveAsTextFile(tf_text)
+      }
+    }
+    println("Done.......")
+  }
+}
+
---
+++ submit_make_userid_info.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+jarLib=hdfs://emr-cluster/warehouse/azkaban/lib/fhb_start_v1.jar
+
+queue=root.cpc.bigdata
+jars=("/home/cpc/anal/lib/spark-tensorflow-connector_2.11-1.10.0.jar" )
+
+randjar="fhb_start"`date +%s%N`".jar"
+hadoop fs -get ${jarLib} ${randjar}
+
+src_dir="hdfs://emr-cluster/user/cpc/aiclk_dataflow/daily/adlist-v4"
+date_begin=`date --date='31 days ago' +%Y-%m-%d`
+date_end=`date --date='1 days ago' +%Y-%m-%d`
+date_begin="2019-08-04"
+date_end="2019-08-18"
+des_dir="hdfs://emr-cluster/user/cpc/fenghuabin/adlist-v4-userid-info"
+
+one_hot_feature_list="media_type,mediaid,channel,sdk_type,adslot_type,adslotid,sex,dtu_id,adtype,interaction,bid,ideaid,unitid,planid,userid,is_new_ad,adclass,site_id,os,network,phone_price,brand,province,city,city_level,uid,age,hour"
+
+spark-submit --master yarn --queue ${queue} \
+    --name "adlist-tf-make-userid-statistics" \
+    --driver-memory 4g --executor-memory 4g \
+    --num-executors 200 --executor-cores 4 \
+    --conf spark.hadoop.fs.defaultFS=hdfs://emr-cluster2 \
+    --conf "spark.yarn.executor.memoryOverhead=4g" \
+    --conf "spark.sql.shuffle.partitions=500" \
+    --jars $( IFS=$','; echo "${jars[*]}" ) \
+    --class com.cpc.spark.ml.dnn.baseData.MakeSampling \
+    ${randjar} ${src_dir} ${date_begin} ${date_end} ${des_dir}
+
+chmod_des="hdfs://emr-cluster/user/cpc/fenghuabin/adlist-v4-sampled"
+hadoop fs -chmod -R 0777 ${chmod_des}
+
