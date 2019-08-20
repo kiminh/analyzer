9509b253d97e0ffe1cf2af068323683bae4838b2
wangjun <wangjun02@qutoutiao.net>
2019-08-20 21:47:40 +0800

    fix bug

--- scripts/oCPX/oCPC/calibration_all/testOcpcCVRfactorRealtime.sh
+++ scripts/oCPX/oCPC/calibration_all/testOcpcCVRfactorRealtime.sh
@@ -18,16 +18,9 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
     --class com.cpc.spark.oCPX.oCPC.calibration_alltype.OcpcCVRfactorRealtime \
-    /home/cpc/wangjun/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $1 $2 $3 $4 $5 $6 $7
+    /home/cpc/wangjun/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $1 $2 $3 $4
 
 #val date = args(0).toString
 #val hour = args(1).toString
 #val version = args(2).toString
 #val expTag = args(3).toString
-#
-#// 主校准回溯时间长度
-#val hourInt1 = args(4).toInt
-#// 备用校准回溯时间长度
-#val hourInt2 = args(5).toInt
-#// 兜底校准时长
-#val hourInt3 = args(6).toInt
