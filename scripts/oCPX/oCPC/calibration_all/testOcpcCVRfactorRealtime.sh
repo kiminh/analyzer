73160f0933ed0c06655a090610491f5357ea8bb3
wangjun <wangjun02@qutoutiao.net>
2019-08-20 21:49:51 +0800

    fix bug

--- scripts/oCPX/oCPC/calibration_all/testOcpcCVRfactorRealtime.sh
+++ scripts/oCPX/oCPC/calibration_all/testOcpcCVRfactorRealtime.sh
@@ -17,7 +17,7 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.yarn.executor.memoryOverhead=4g'\
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
-    --class com.cpc.spark.oCPX.oCPC.calibration_alltype.OcpcCVRfactorRealtime \
+    --class com.cpc.spark.oCPX.oCPC.calibration_all.OcpcCVRfactorRealtime \
     /home/cpc/wangjun/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $1 $2 $3 $4 $5 $6 $7
 
 #val date = args(0).toString
