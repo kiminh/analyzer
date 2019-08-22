d9b5c2fd4d456e941993897f8c966541ebf6c079
wangjun <wangjun02@qutoutiao.net>
2019-08-22 10:52:15 +0800

    change code

--- src/main/scala/com/cpc/spark/oCPX/oCPC/pid/OcpcPIDcontrolV1.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/pid/OcpcPIDcontrolV1.scala
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat
 import java.util.Calendar
 
 import com.cpc.spark.oCPX.OcpcTools._
+import com.cpc.spark.oCPX.oCPC.pid.OcpcPIDretrieveData._
 import com.typesafe.config.ConfigFactory
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.functions._
@@ -28,7 +29,7 @@ object OcpcPIDcontrolV1 {
     println("parameters:")
     println(s"date=$date, hour=$hour, version=$version, hourInt=$hourInt, minCV=$minCV, kp=$kp, ki=$ki, kd=$kd, exptag=$expTag")
 
-    val baseData = getBaseData(hourInt, date, hour, spark)
+    val baseData = getBaseDataV1(hourInt, date, hour, spark)
     val errorData = calculateError(baseData, date, hour, spark)
     val prevError = getPrevData(hourInt, expTag, version, date, hour, spark)
     val prevCali = getPrevCali(baseData, date, hour, spark)
@@ -193,86 +194,5 @@ object OcpcPIDcontrolV1 {
     result
   })
 
-
-  def getBaseData(hourInt: Int, date: String, hour: String, spark: SparkSession) = {
-    // 抽取媒体id
-    val conf = ConfigFactory.load("ocpc")
-    val conf_key = "medias.total.media_selection"
-    val mediaSelection = conf.getString(conf_key)
-
-    // 取历史数据
-    val dateConverter = new SimpleDateFormat("yyyy-MM-dd HH")
-    val newDate = date + " " + hour
-    val today = dateConverter.parse(newDate)
-    val calendar = Calendar.getInstance
-    calendar.setTime(today)
-    calendar.add(Calendar.HOUR, -hourInt)
-    val yesterday = calendar.getTime
-    val tmpDate = dateConverter.format(yesterday)
-    val tmpDateValue = tmpDate.split(" ")
-    val date1 = tmpDateValue(0)
-    val hour1 = tmpDateValue(1)
-    val selectCondition = getTimeRangeSqlDate(date1, hour1, date, hour)
-
-    val sqlRequest =
-      s"""
-         |SELECT
-         |  searchid,
-         |  unitid,
-         |  cvr_model_name,
-         |  adslot_type,
-         |  isshow,
-         |  isclick,
-         |  bid_discounted_by_ad_slot as bid,
-         |  price,
-         |  cast(exp_cvr as double) as exp_cvr,
-         |  cast(exp_ctr as double) as exp_ctr,
-         |  media_appsid,
-         |  conversion_goal,
-         |  cast(ocpc_log_dict['cpagiven'] as double) as cpagiven,
-         |  date,
-         |  hour
-         |FROM
-         |  dl_cpc.ocpc_filter_unionlog
-         |WHERE
-         |  $selectCondition
-         |AND
-         |  $mediaSelection
-         |AND
-         |  is_ocpc = 1
-         |AND
-         |  isclick = 1
-       """.stripMargin
-    println(sqlRequest)
-    val clickData = spark
-      .sql(sqlRequest)
-      .withColumn("cvr_goal", udfConcatStringInt("cvr")(col("conversion_goal")))
-      .withColumn("media", udfDetermineMedia()(col("media_appsid")))
-      .withColumn("adslot_type", udfAdslotTypeMapAs()(col("adslot_type")))
-      .withColumn("identifier", concat_ws("&", col("adslot_type"), col("unitid")))
-
-    // 抽取cv数据
-    val sqlRequest2 =
-      s"""
-         |SELECT
-         |  searchid,
-         |  label as iscvr,
-         |  cvr_goal
-         |FROM
-         |  dl_cpc.ocpc_label_cvr_hourly
-         |WHERE
-         |  $selectCondition
-       """.stripMargin
-    println(sqlRequest2)
-    val cvData = spark.sql(sqlRequest2)
-
-
-    // 数据关联
-    val resultDF = clickData
-      .join(cvData, Seq("searchid", "cvr_goal"), "left_outer")
-      .na.fill(0, Seq("iscvr"))
-
-    resultDF
-  }
 }
 
