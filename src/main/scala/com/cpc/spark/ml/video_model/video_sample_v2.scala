8f444b4b9b71a294b7a38b60faab16fd7cb0d0e7
duanguangdong <duanguangdong@qutoutiao.net>
2019-08-25 13:31:17 +0800

    update

--- src/main/scala/com/cpc/spark/ml/video_model/video_sample_v2.scala
+++ src/main/scala/com/cpc/spark/ml/video_model/video_sample_v2.scala
@@ -13,22 +13,35 @@ object video_sample_v2 {
   Logger.getRootLogger.setLevel(Level.WARN)
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("video sample").enableHiveSupport().getOrCreate()
+    import spark.implicits._
     val curday = args(0)
     val cal = Calendar.getInstance()
     cal.setTime(new SimpleDateFormat("yyyy-MM-dd").parse(s"$curday"))
     cal.add(Calendar.DATE, -1)
     val oneday = new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime)
+    val adtype8_hash = Murmur3Hash.stringHash64("f8" + "#" + "8", 0)
+    val adtype10_hash = Murmur3Hash.stringHash64("f8" + "#" + "10", 0)
 
-    val sample = getSample(spark, oneday)
-    sample.repartition(500).write
+    val sample = getSample(spark, oneday).repartition(500).cache()
+    sample.where(s"adtypehash in ($adtype8_hash, $adtype10_hash)").select($"sample_idx",$"idx0",$"idx1",$"idx2",$"id_arr", $"label", $"dense").write
       .mode("overwrite")
       .format("tfrecords")
       .option("recordType", "Example")
       .save(s"hdfs://emr-cluster/user/cpc/aiclk_dataflow/daily/videoctr-v2/$oneday/")
     val CountPathTmpName = s"hdfs://emr-cluster/user/cpc/aiclk_dataflow/daily/videoctr-v2/tmp/"
     val CountPathName = s"hdfs://emr-cluster/user/cpc/aiclk_dataflow/daily/videoctr-v2/$oneday/count"
-    val count = spark.read.format("tfrecords").option("recordType", "Example").load(s"hdfs://emr-cluster/user/cpc/aiclk_dataflow/daily/videoctr-v2/$oneday/part*").count()
+    val count = sample.where(s"adtypehash in ($adtype8_hash, $adtype10_hash)").count()
     CommonUtils.writeCountToFile(spark, count, CountPathTmpName, CountPathName)
+
+    sample.union(sample.where(s"adtypehash in ($adtype8_hash, $adtype10_hash)")).select($"sample_idx",$"idx0",$"idx1",$"idx2",$"id_arr", $"label", $"dense").write
+      .mode("overwrite")
+      .format("tfrecords")
+      .option("recordType", "Example")
+      .save(s"hdfs://emr-cluster/user/cpc/aiclk_dataflow/daily/videoctr-v3/$oneday/")
+    val CountPathTmpNamev3 = s"hdfs://emr-cluster/user/cpc/aiclk_dataflow/daily/videoctr-v3/tmp/"
+    val CountPathNamev3 = s"hdfs://emr-cluster/user/cpc/aiclk_dataflow/daily/videoctr-v3/$oneday/count"
+    val countv3 = sample.union(sample.where(s"adtypehash in ($adtype8_hash, $adtype10_hash)")).count()
+    CommonUtils.writeCountToFile(spark, countv3, CountPathTmpNamev3, CountPathNamev3)
   }
   def getSample(spark: SparkSession, date: String): DataFrame = {
     import spark.implicits._
@@ -37,12 +50,12 @@ object video_sample_v2 {
       select($"sample_idx",$"idx0",$"idx1",$"idx2",$"id_arr", $"label", $"dense",expr("dense[8]").alias("adtypehash"),expr("dense[11]").alias("ideaidhash"))
     val adtype8_hash = Murmur3Hash.stringHash64("f8" + "#" + "8", 0)
     val adtype10_hash = Murmur3Hash.stringHash64("f8" + "#" + "10", 0)
-    val video_sample = original_sample.where(s"adtypehash in ($adtype8_hash, $adtype10_hash)").select($"sample_idx",$"idx0",$"idx1",$"idx2",$"id_arr", $"label", $"dense", $"ideaidhash")
+//    val video_sample = original_sample.where(s"adtypehash in ($adtype8_hash, $adtype10_hash)").select($"sample_idx",$"idx0",$"idx1",$"idx2",$"id_arr", $"label", $"dense", $"adtypehash", $"ideaidhash")
     val ideaid_feature = spark.sql(s"""select ideaid, resource from dl_cpc.resource where model='video'""").select(hash("f11")($"ideaid").alias("ideaidhash"),$"resource")
-    val video_sample_new = video_sample.join(ideaid_feature, Seq("ideaidhash"), "left_outer").select($"sample_idx",$"idx0",$"idx1",$"idx2",$"id_arr", $"label", $"dense", $"resource")
-    val original_sample_new = original_sample.join(ideaid_feature, Seq("ideaidhash"), "left_outer").select($"sample_idx",$"idx0",$"idx1",$"idx2",$"id_arr", $"label", $"dense", $"resource")
-    val result = original_sample_new.union(video_sample_new).
-      select($"sample_idx", $"idx0", $"idx1", $"idx2", $"id_arr", $"label", $"dense", array(hash("f" + 73)($"resource")).alias("resourceidhash")).rdd.map{
+//    val video_sample_new = video_sample.join(ideaid_feature, Seq("ideaidhash"), "left_outer").select($"sample_idx",$"idx0",$"idx1",$"idx2",$"id_arr", $"label", $"dense", $"adtypehash", $"resource")
+    val original_sample_new = original_sample.join(ideaid_feature, Seq("ideaidhash"), "left_outer").select($"sample_idx",$"idx0",$"idx1",$"idx2",$"id_arr", $"label", $"dense", $"adtypehash", $"resource")
+    val result = original_sample_new.
+      select($"sample_idx", $"idx0", $"idx1", $"idx2", $"id_arr", $"label", $"dense", $"adtypehash", array(hash("f" + 73)($"resource")).alias("resourceidhash")).rdd.map{
       r =>
         val sample_idx = r.getAs[Long]("sample_idx")
         val label = r.getAs[Seq[Long]]("label")
@@ -51,9 +64,10 @@ object video_sample_v2 {
         val idx2 = r.getAs[Seq[Long]]("idx2")
         val id_arr = r.getAs[Seq[Long]]("id_arr")
         val dense = r.getAs[Seq[Long]]("dense")
+        val adtype = r.getAs[Seq[Long]]("adtypehash")
         val resourceidhash = r.getAs[Seq[Long]]("resourceidhash")
-        (sample_idx, label, dense ++ resourceidhash, idx0, idx1, idx2, id_arr)
-    }.toDF("sample_idx", "label", "dense", "idx0", "idx1", "idx2", "id_arr")
+        (sample_idx, label, dense ++ resourceidhash, idx0, idx1, idx2, id_arr, adtype)
+    }.toDF("sample_idx", "label", "dense", "idx0", "idx1", "idx2", "id_arr", "adtypehash")
     result
   }
 
