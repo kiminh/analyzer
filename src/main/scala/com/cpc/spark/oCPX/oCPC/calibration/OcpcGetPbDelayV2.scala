01198ff40710fe358daa8fd65c0a90d7b7dbf698
wangjun <wangjun02@qutoutiao.net>
2019-08-29 19:51:54 +0800

    test new code

--- src/main/scala/com/cpc/spark/oCPX/oCPC/calibration/OcpcGetPbDelayV2.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/calibration/OcpcGetPbDelayV2.scala
@@ -110,8 +110,8 @@ object OcpcGetPbDelayV2 {
 
     resultDF
       .repartition(1)
-      //      .write.mode("overwrite").insertInto("test.ocpc_pb_data_hourly_exp")
-      .write.mode("overwrite").insertInto("dl_cpc.ocpc_pb_data_hourly_exp")
+      .write.mode("overwrite").insertInto("test.ocpc_pb_data_hourly_exp")
+//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_pb_data_hourly_exp")
 
 
   }
