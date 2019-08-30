990696abcba40d7615b6efc5d97e093d0a393e80
冯华宾 <fenghuabin@qutoutiao.net>
2019-08-30 11:05:42 +0800

    fhb_start

--- submit_make_userid_info.sh
+++ submit_make_userid_info.sh
@@ -11,11 +11,11 @@ randjar="fhb_start"`date +%s%N`".jar"
 hadoop fs -get ${jarLib} ${randjar}
 
 src_dir="hdfs://emr-cluster/user/cpc/aiclk_dataflow/daily/adlist-v4"
-date_begin=`date --date='20 days ago' +%Y-%m-%d`
-date_end=`date --date='1 days ago' +%Y-%m-%d`
+date_begin=`date --date='22 days ago' +%Y-%m-%d`
+date_end=`date --date='2 days ago' +%Y-%m-%d`
 des_dir="hdfs://emr-cluster/user/cpc/fenghuabin/adlist-v4-userid-info"
-sta_date_begin="2019-08-04"
-sta_date_end="2019-08-18"
+sta_date_begin="2019-08-29"
+sta_date_end="2019-08-29"
 
 one_hot_feature_list="media_type,mediaid,channel,sdk_type,adslot_type,adslotid,sex,dtu_id,adtype,interaction,bid,ideaid,unitid,planid,userid,is_new_ad,adclass,site_id,os,network,phone_price,brand,province,city,city_level,uid,age,hour"
 
