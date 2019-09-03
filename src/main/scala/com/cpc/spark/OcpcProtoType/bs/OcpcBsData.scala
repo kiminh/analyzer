a3461b16b6dc3a0357e2861215fde91951121ef9
wangjun <wangjun02@qutoutiao.net>
2019-09-03 22:19:54 +0800

    fix bug

--- src/main/scala/com/cpc/spark/OcpcProtoType/bs/OcpcBsData.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/bs/OcpcBsData.scala
@@ -41,8 +41,8 @@ object OcpcBsData {
         .withColumn("exp_tag", lit(expTag))
         .withColumn("version", lit(version))
         .repartition(5)
-        .write.mode("overwrite").insertInto("test.ocpc_bs_params_pb_hourly")
-//        .write.mode("overwrite").insertInto("dl_cpc.ocpc_bs_params_pb_hourly")
+//        .write.mode("overwrite").insertInto("test.ocpc_bs_params_pb_hourly")
+        .write.mode("overwrite").insertInto("dl_cpc.ocpc_bs_params_pb_hourly")
 
 
     savePbPack(result, fileName, spark)
@@ -207,7 +207,7 @@ object OcpcBsData {
          |WHERE
          |  $selectCondition
          |AND
-         |  media_appsid in ('80000001', '80000002', '80001098', '80001292', '80001539', '80002480', '80001011', '80004786', '80004787', '80002819')
+         |  $mediaSelection
          |AND
          |  isshow = 1
          |AND
