f8d2840940a15739c40ffad83ccbf286aa386bcd
wangjun <wangjun02@qutoutiao.net>
2019-09-03 11:57:40 +0800

    平滑后验cvr数据调整

--- src/main/scala/com/cpc/spark/oCPX/oCPC/calibration_all/OcpcGetPb.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/calibration_all/OcpcGetPb.scala
@@ -43,7 +43,9 @@ object OcpcGetPb {
       .cache()
     jfbData.show(10)
 
-    val smoothDataRaw = OcpcSmoothfactorMain(date, hour, version, expTag, dataRaw, hourInt1, hourInt2, hourInt3, spark)
+    val dataRawOnlySmooth = OcpcCalibrationBaseMainOnlySmooth(date, hour, hourInt3, spark).cache()
+    dataRawOnlySmooth.show(10)
+    val smoothDataRaw = OcpcSmoothfactorMain(date, hour, version, expTag, dataRawOnlySmooth, hourInt1, hourInt2, hourInt3, spark)
     val smoothData = smoothDataRaw
       .withColumn("post_cvr", col("cvr"))
       .select("identifier", "conversion_goal", "exp_tag", "post_cvr", "smooth_factor")
@@ -68,6 +70,7 @@ object OcpcGetPb {
     data.show(10)
 
     dataRaw.unpersist()
+    dataRawOnlySmooth.unpersist()
     dataRawRealtime.unpersist()
 
     // 明投单元
@@ -84,8 +87,8 @@ object OcpcGetPb {
 
     resultDF
       .repartition(1)
-//      .write.mode("overwrite").insertInto("test.ocpc_pb_data_hourly_exp_alltype")
-      .write.mode("overwrite").insertInto("dl_cpc.ocpc_pb_data_hourly_exp_alltype")
+      .write.mode("overwrite").insertInto("test.ocpc_pb_data_hourly_exp_alltype")
+//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_pb_data_hourly_exp_alltype")
 
 
   }
