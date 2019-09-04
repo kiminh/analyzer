baab1c58b65d615ce3ff473160b6d02ab5fdc0b9
齐智 <qizhi@qutoutiao.net>
2019-09-04 14:29:12 +0800

    push

--- src/main/scala/com/cpc/spark/ml/cvrmodel/daily/LREval.scala
+++ src/main/scala/com/cpc/spark/ml/cvrmodel/daily/LREval.scala
@@ -1,5 +1,6 @@
 package com.cpc.spark.ml.cvrmodel.daily
 
+import com.cpc.spark.ml.dnn.Utils.DateUtils
 import com.cpc.spark.ml.train.LRIRModel
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.mllib.linalg.{Vector, Vectors}
@@ -21,6 +22,7 @@ object LREval {
     import spark.implicits._
 
     val date = args(0)
+    val tomorrow=DateUtils.getPrevDate(date, -1)
 
     val beforeSql=
       """
@@ -30,24 +32,46 @@ object LREval {
 
     val sql =
       s"""
+         |create temporary function auc as 'hivemall.evaluation.AUCUDAF' using jar "/home/cpc/anal/lib/hivemall-all-0.5.2-incubating.jar";
          |select
-         |ctr_model_name,
-         |count(*) as show_cnt,
-         |auc(A.exp_ctr/1000000, coalesce(A.isclick,0)) as auc,
-         |sum(A.exp_ctr)/1000000/sum(A.isclick) as pcoc
+         |cvr_model_name,
+         |auc(raw_cvr, coalesce(label,0)) as auc,
+         |avg(raw_cvr)*sum(isclick)/sum(label) as pcoc
          |from
          |(
-         |  select
-         |  if(array_contains(exptags, 'bslrcvr=bs-v4-cvr') and array_contains(exptags, 'bscvrfactor=0'), 'bs-v4-cvr', 'no-cvr') as ctr_model_name, isshow, isclick, exp_ctr, exptags
+         |select cvr_model_name, isshow, isclick, raw_cvr/1000000 as raw_cvr, exptags,case when cv_types = null then 0
+         |           when conversion_goal = 1 and B.cv_types like '%cvr1%' then 1
+         |           when conversion_goal = 2 and B.cv_types like '%cvr2%' then 1
+         |           when conversion_goal = 3 and B.cv_types like '%cvr3%' then 1
+         |           when conversion_goal = 4 and B.cv_types like '%cvr4%' then 1
+         |           when conversion_goal = 0 and is_api_callback = 1 and B.cv_types like '%cvr2%' then 1
+         |           when conversion_goal = 0 and is_api_callback = 0 and (adclass like '11011%' or adclass like '125%') and B.cv_types like '%cvr4%' then 1
+         |           when conversion_goal = 0 and is_api_callback = 0 and adclass not like '11011%' and adclass not like '125%' and B.cv_types like '%cvr%' then 1
+         |      else 0 end as label from
+         |  (select
+         |  searchid,conversion_goal,is_api_callback,adclass,if(array_contains(exptags, 'bslrcvr=bs-v4-cvr') and array_contains(exptags, 'bscvrfactor=0'), 1, 0) as cvr_model_name, isshow, isclick, raw_cvr*1.0/1000000 as raw_cvr, bsrawcvr*1.0/1000000 as bsrawcvr, exptags
          |  from
          |  dl_cpc.cpc_basedata_union_events
          |  where
          |  day in ('$date')
+         |  and isshow=1
          |  and media_appsid in ('80000001','80000002')
-         |  and array_contains(exptags, 'bslradtypecorrection')
-         |  distribute by if(array_contains(exptags, 'bslrcvr=bs-v4-cvr') and array_contains(exptags, 'bscvrfactor=0'), 'bs-v4-cvr', 'no-cvr') sort by exp_ctr DESC
-         |) A
-         |group by ctr_model_name
+         |  and array_contains(exptags, 'bslradtypecorrection'))A
+         |  left outer join
+         |   (
+         |      select
+         |      searchid, concat_ws(',', collect_set(cvr_goal)) as cv_types
+         |      from
+         |         dl_cpc.ocpc_label_cvr_hourly
+         |      where
+         |         `date`>="$date" and `date`<="$tomorrow"
+         |      and label=1
+         |      group by searchid
+         |   ) B
+         |   on A.searchid=B.searchid
+         |  distribute by cvr_model_name sort by raw_cvr DESC
+         |) t1
+         |group by cvr_model_name;
       """.stripMargin
     val df=spark.sql(sql)
     df.show(5)
