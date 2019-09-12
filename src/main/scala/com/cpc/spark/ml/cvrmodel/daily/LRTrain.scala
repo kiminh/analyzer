4ba69af3f8371d2042581fc0d46f922ad50a6ee1
齐智 <qizhi@qutoutiao.net>
2019-09-12 12:06:47 +0800

    to hive

--- src/main/scala/com/cpc/spark/ml/cvrmodel/daily/LRTrain.scala
+++ src/main/scala/com/cpc/spark/ml/cvrmodel/daily/LRTrain.scala
@@ -245,6 +245,9 @@ object LRTrain {
     }else if("cvrparser5".equals(parser)){
       name="qtt-bs-cvrparser5-daily"
       destfile="qtt-bs-cvrparser5-daily.lrm"
+    }else if("cvrparser8".equals(parser)){
+      name="qtt-bs-cvrparser8-daily"
+      destfile="qtt-bs-cvrparser8-daily.lrm"
     }
 
     println("name = " + name + " , destfile = " + destfile)
@@ -513,6 +516,8 @@ object LRTrain {
                   getCvrVectorParser6(u)
                 case "cvrparser7" =>
                   getCvrVectorParser7(u)
+                case "cvrparser8" =>
+                  getCvrVectorParser8(u)
               }
               LabeledPoint(u.getAs[Int]("label").toDouble, vec)
           }
@@ -723,6 +728,121 @@ object LRTrain {
     //ideaid
     els = els :+ (dict("ideaid").getOrElse(x.getAs[Int]("ideaid"), 0) + i, 1d)
     i += dict("ideaid").size + 1
+
+    println("Vectors size = " + i)
+
+    try {
+      Vectors.sparse(i, els)
+    } catch {
+      case e: Exception =>
+        throw new Exception(els.toString + " " + i.toString + " " + e.getMessage)
+        null
+    }
+  }
+
+
+  // 190511: baseline features.
+  // bs-q项目最初的特征集, 从前同事的项目继承而来.
+  def getCvrVectorParser8(x: Row): Vector = {
+
+    val cal = Calendar.getInstance()
+    cal.setTimeInMillis(x.getAs[Int]("timestamp") * 1000L)
+    val week = cal.get(Calendar.DAY_OF_WEEK) //1 to 7
+    val hour = cal.get(Calendar.HOUR_OF_DAY)
+    var els = Seq[(Int, Double)]()
+    var i = 0
+
+    els = els :+ (week + i - 1, 1d)
+    i += 7
+
+    //(24)
+    els = els :+ (hour + i, 1d)
+    i += 24
+
+    //sex
+    els = els :+ (x.getAs[Int]("sex") + i, 1d)
+    i += 9
+
+    //age
+    els = els :+ (x.getAs[Int]("age") + i, 1d)
+    i += 100
+
+    //os 96 - 97 (2)
+    els = els :+ (x.getAs[Int]("os") + i, 1d)
+    i += 10
+
+    //isp
+    els = els :+ (x.getAs[Int]("isp") + i, 1d)
+    i += 20
+
+    //net
+    els = els :+ (x.getAs[Int]("network") + i, 1d)
+    i += 10
+
+    els = els :+ (dict("cityid").getOrElse(x.getAs[Int]("city"), 0) + i, 1d)
+    i += dict("cityid").size + 1
+
+    //media id
+    els = els :+ (dict("mediaid").getOrElse(x.getAs[String]("media_appsid").toInt, 0) + i, 1d)
+    i += dict("mediaid").size + 1
+
+    //ad slot id
+    els = els :+ (dict("slotid").getOrElse(x.getAs[String]("adslotid").toInt, 0) + i, 1d)
+    i += dict("slotid").size + 1
+
+    //0 to 4
+    els = els :+ (x.getAs[Int]("phone_level") + i, 1d)
+    i += 10
+
+    //pagenum
+    var pnum = x.getAs[Int]("pagenum")
+    if (pnum < 0 || pnum > 50) {
+      pnum = 0
+    }
+    els = els :+ (pnum + i, 1d)
+    i += 100
+
+    //bookid
+    var bid = 0
+    try {
+      bid = x.getAs[String]("bookid").toInt
+    } catch {
+      case e: Exception =>
+    }
+    if (bid < 0 || bid > 50) {
+      bid = 0
+    }
+    els = els :+ (bid + i, 1d)
+    i += 100
+
+    //ad class
+    val adcls = dict("adclass").getOrElse(x.getAs[Int]("adclass"), 0)
+    els = els :+ (adcls + i, 1d)
+    i += dict("adclass").size + 1
+
+    //adtype
+    els = els :+ (x.getAs[Int]("adtype") + i, 1d)
+    i += 16
+
+    //adslot_type
+    els = els :+ (x.getAs[Int]("adslot_type") + i, 1d)
+    i += 10
+
+    //planid
+    els = els :+ (dict("planid").getOrElse(x.getAs[Int]("planid"), 0) + i, 1d)
+    i += dict("planid").size + 1
+
+    //unitid
+    els = els :+ (dict("unitid").getOrElse(x.getAs[Int]("unitid"), 0) + i, 1d)
+    i += dict("unitid").size + 1
+
+    //ideaid
+    els = els :+ (dict("ideaid").getOrElse(x.getAs[Int]("ideaid"), 0) + i, 1d)
+    i += dict("ideaid").size + 1
+
+    //conversion_goal
+    els = els :+ (x.getAs[Int]("conversion_goal") + i, 1d)
+    i += 10
 
     println("Vectors size = " + i)
 
