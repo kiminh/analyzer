98c30255de536c9c23f6b51c2a410d442af84c4c
wangjun <wangjun02@qutoutiao.net>
2019-09-17 13:08:53 +0800

    fix bug

--- src/main/scala/com/cpc/spark/oCPX/oCPC/calibration_all/OcpcCalculateCalibrationValue.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/calibration_all/OcpcCalculateCalibrationValue.scala
@@ -4,8 +4,6 @@ import java.text.SimpleDateFormat
 import java.util.Calendar
 
 import com.cpc.spark.oCPX.OcpcTools._
-//import com.cpc.spark.oCPX.oCPC.calibration_all.OcpcCalibrationBase._
-import com.typesafe.config.ConfigFactory
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SparkSession}
@@ -67,98 +65,6 @@ object OcpcCalculateCalibrationValue {
 
 
     resultDF
-  }
-
-  def selectDataByHourInt(dataRaw: DataFrame, date: String, hour: String, hourInt: Int, expConf: DataFrame, expTag: String, spark: SparkSession) = {
-    val dateConverter = new SimpleDateFormat("yyyy-MM-dd HH")
-    val newDate = date + " " + hour
-    val today = dateConverter.parse(newDate)
-    val calendar = Calendar.getInstance
-    calendar.setTime(today)
-    calendar.add(Calendar.HOUR, -hourInt)
-    val yesterday = calendar.getTime
-    val tmpDate = dateConverter.format(yesterday)
-    val tmpDateValue = tmpDate.split(" ")
-    val date1 = tmpDateValue(0)
-    val hour1 = tmpDateValue(1)
-    val selectCondition = getTimeRangeSqlDate(date1, hour1, date, hour)
-
-    val data = dataRaw
-      .filter(selectCondition)
-      .groupBy("identifier", "conversion_goal", "media")
-      .agg(
-        sum(col("click")).alias("click"),
-        sum(col("cv")).alias("cv"),
-        sum(col("total_bid")).alias("total_bid"),
-        sum(col("total_price")).alias("total_price")
-      )
-      .select("identifier", "conversion_goal", "media", "click", "cv", "total_bid", "total_price")
-      .na.fill(0, Seq("cv"))
-      .withColumn("jfb", col("total_price") * 1.0 / col("total_bid"))
-      .withColumn("media", udfMediaName()(col("media")))
-      .withColumn("exp_tag", udfSetExpTag(expTag)(col("media")))
-      .join(expConf, Seq("conversion_goal", "exp_tag"), "left_outer")
-      .na.fill(0, Seq("min_cv"))
-      .withColumn("min_cv", udfSetMinCV()(col("min_cv")))
-      .filter(s"cv > 0")
-    data.show(10)
-
-    data
-  }
-
-  def udfSetMinCV() = udf((minCV: Int) => {
-    var result = minCV
-    if (result < 0) {
-      result = 0
-    }
-    result
-  })
-
-  def getExpConf(version: String, spark: SparkSession) ={
-    // 从配置文件读取数据
-    val conf = ConfigFactory.load("ocpc")
-    val confPath = conf.getString("exp_config_v2.jfb_factor")
-    val rawData = spark.read.format("json").json(confPath)
-    val data = rawData
-      .select("exp_tag", "conversion_goal", "min_cv")
-      .groupBy("exp_tag", "conversion_goal")
-      .agg(min(col("min_cv")).alias("min_cv"))
-      .distinct()
-
-    println("jfb factor: config")
-    data.show(10)
-
-    data
-  }
-
-
-  def calculateCalibrationValue(dataRaw1: DataFrame, dataRaw2: DataFrame, spark: SparkSession) = {
-    /*
-    "identifier", "click", "cv", "pre_cvr", "total_price", "total_bid"
-     */
-
-    // 主校准模型
-    val data1 = dataRaw1
-      .filter(s"cv >= min_cv")
-      .withColumn("jfb1", col("jfb"))
-      .select("identifier", "conversion_goal", "exp_tag", "jfb1")
-
-    // 备用校准模型
-    val data2 = dataRaw2
-      .filter(s"cv >= min_cv")
-      .withColumn("jfb2", col("jfb"))
-      .select("identifier", "conversion_goal", "exp_tag", "jfb2")
-
-    // 数据表关联
-    val data = data2
-      .join(data1, Seq("identifier", "conversion_goal", "exp_tag"), "left_outer")
-      .na.fill(0, Seq("flag"))
-      .withColumn("jfb3", when(col("jfb1").isNotNull, col("jfb1")).otherwise(col("jfb2")))
-
-    data.show()
-
-    data
-
   }
 
 
