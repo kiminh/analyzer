df4d262e1a4a05aaf774c042f50a65e26ebeb5c6
wangjun <wangjun02@qutoutiao.net>
2019-09-18 13:44:32 +0800

    fix bug

--- src/main/scala/com/cpc/spark/oCPX/basedata/OcpcQuickLog.scala
+++ src/main/scala/com/cpc/spark/oCPX/basedata/OcpcQuickLog.scala
@@ -93,6 +93,7 @@ object OcpcQuickLog {
 
   def getCvLog(date: String, hour: String, spark: SparkSession) = {
     // 抽取cv数据
+    // todo 数据调研
     spark.udf.register("getConversionGoal", (traceType: String, traceOp1: String, traceOp2: String) => {
       var result = 0
       if (traceOp1 == "REPORT_DOWNLOAD_PKGADDED") {
