b1729f2ee603588a8645da468bb48cf0e3c336d3
冯华宾 <fenghuabin@qutoutiao.net>
2019-09-20 00:21:30 +0800

    fhb_start

---
+++ crontab_collect_inc_mom_data.sh
@@ -0,0 +1,241 @@
+#!/usr/bin/env bash
+#fenghuabin@qutoutiao.net
+
+
+source /etc/profile
+
+ml_name=adlist
+ml_ver=v4refult
+
+date_full=`date`
+printf "*****************************${date_full}********************************\n"
+
+inc_epochs=$1
+update_online=$2
+name_space=$3
+total=$4
+worker=$5
+ps_server=$6
+tf_server=$7
+
+curr_date=`date --date='1 days ago' +%Y-%m-%d`
+printf "now curr_date is:${curr_date}\n"
+printf "max_epoch_num is:${inc_epochs}\n"
+
+now_hour=$(date "+%H")
+now_minutes=$(date "+%M")
+now_id="00"${now_hour}
+if [ ${now_minutes} -ge 30 ];then
+    now_id="30"${now_hour}
+fi
+printf "now id is:%s\n" ${now_id}
+
+dir=collect_inc_mom
+if [[ ! -d "${dir}" ]]; then
+    mkdir ${dir}
+fi
+printf "now id is:%s\n" ${now_id}
+
+shell_in_run=${dir}/shell_in_busy
+if [[ -f "$shell_in_run" ]]; then
+    printf "shell are busy now, existing\n"
+    exit 0
+fi
+touch ${shell_in_run}
+
+busy_file=collect_inc_mom_busy_data
+busy=${dir}/${busy_file}
+rm ${busy}
+remote_busy="hdfs://emr-cluster/warehouse/dl_cpc.db/cpc_algo_models/qtt-list-dnn-rawid_v4refult/"${busy_file}
+hadoop fs -get ${remote_busy} ${busy}
+if [[ -f "$busy" ]]; then
+    printf "cluster are busy now, existing\n"
+    rm ${shell_in_run}
+    exit 0
+fi
+
+id_list=( "0000" "3000" "0001" "3001" "0002" "3002" "0003" "3003" "0004" "3004" "0005" "3005" "0006" "3006" "0007" "3007" "0008" "3008" "0009" "3009" "0010" "3010" "0011" "3011" "0012" "3012" "0013" "3013" "0014" "3014" "0015" "3015" "0016" "3016" "0017" "3017" "0018" "3018" "0019" "3019" "0020" "3020" "0021" "3021" "0022" "3022" "0023" "3023" )
+prefix=hdfs://emr-cluster2ns2/user/cpc_tensorflow_example_half/${curr_date}
+end=part-*
+sample_list=(
+    ${prefix}"/00/0/"
+    ${prefix}"/00/1/"
+    ${prefix}"/01/0/"
+    ${prefix}"/01/1/"
+    ${prefix}"/02/0/"
+    ${prefix}"/02/1/"
+    ${prefix}"/03/0/"
+    ${prefix}"/03/1/"
+    ${prefix}"/04/0/"
+    ${prefix}"/04/1/"
+    ${prefix}"/05/0/"
+    ${prefix}"/05/1/"
+    ${prefix}"/06/0/"
+    ${prefix}"/06/1/"
+    ${prefix}"/07/0/"
+    ${prefix}"/07/1/"
+    ${prefix}"/08/0/"
+    ${prefix}"/08/1/"
+    ${prefix}"/09/0/"
+    ${prefix}"/09/1/"
+    ${prefix}"/10/0/"
+    ${prefix}"/10/1/"
+    ${prefix}"/11/0/"
+    ${prefix}"/11/1/"
+    ${prefix}"/12/0/"
+    ${prefix}"/12/1/"
+    ${prefix}"/13/0/"
+    ${prefix}"/13/1/"
+    ${prefix}"/14/0/"
+    ${prefix}"/14/1/"
+    ${prefix}"/15/0/"
+    ${prefix}"/15/1/"
+    ${prefix}"/16/0/"
+    ${prefix}"/16/1/"
+    ${prefix}"/17/0/"
+    ${prefix}"/17/1/"
+    ${prefix}"/18/0/"
+    ${prefix}"/18/1/"
+    ${prefix}"/19/0/"
+    ${prefix}"/19/1/"
+    ${prefix}"/20/0/"
+    ${prefix}"/20/1/"
+    ${prefix}"/21/0/"
+    ${prefix}"/21/1/"
+    ${prefix}"/22/0/"
+    ${prefix}"/22/1/"
+    ${prefix}"/23/0/"
+    ${prefix}"/23/1/"
+)
+
+
+now_id=0021
+for idx in "${!sample_list[@]}";
+do
+    p00="${sample_list[$idx]}"
+    id="${id_list[$idx]}"
+
+    if [ "${id}" = "${now_id}"  ];then
+        echo "id = now_id, break..."
+        break
+    fi
+
+    is_new=${dir}/train_done_${curr_date}_${id}
+    if [[ -f "$is_new" ]]; then
+        continue
+    fi
+
+    file_count=${dir}/${curr_date}_${id}_count
+    file_part=${dir}/${curr_date}_${id}_part-10-0
+
+    if [[ ! -f ${file_count} ]]; then
+        hadoop fs -get ${p00}count ${file_count} &
+    fi
+    if [[ ! -f ${file_part} ]]; then
+        hadoop fs -get ${p00}part-10-0 ${file_part} &
+    fi
+done
+
+
+printf "waiting for downloading real-time data in parallel...\n"
+wait
+printf "downloaded real-time data file in parallel...\n"
+
+
+inc_data=()
+done_data=()
+all_data=()
+for idx in "${!sample_list[@]}";
+do
+    p00="${sample_list[$idx]}"
+    id="${id_list[$idx]}"
+
+    if [ "${id}" = "${now_id}"  ];then
+        echo "id = now_id, break..."
+        break
+    fi
+
+    is_new=${dir}/train_done_${curr_date}_${id}
+    if [[ -f "$is_new" ]]; then
+        printf "done with ${p00}, continuing\n"
+        done_data+=(${p00}${end})
+        all_data+=(${p00}${end})
+        continue
+    fi
+
+    file_count=${dir}/${curr_date}_${id}_count
+    file_part=${dir}/${curr_date}_${id}_part-10-0
+
+    if [[ ! -f ${file_count} ]]; then
+        printf "no ${file_count} file, continue...\n"
+        continue
+    fi
+    if [[ ! -f ${file_part} ]]; then
+        printf "no ${file_part} file, continue...\n"
+        continue
+    fi
+
+    file_size=`ls -l ${file_part} | awk '{ print $5 }'`
+    if [ ${file_size} -lt 1000 ]
+    then
+        printf "invalid ${file_part} file size:${file_size}, continue...\n"
+        continue
+    fi
+
+    touch ${is_new}
+    rm ${file_count}
+    rm ${file_part}
+
+    printf "new inc real-time file ${p00}\n"
+    inc_data+=(${p00}${end})
+    all_data+=(${p00}${end})
+    test_file=${p00}${end}
+    last_id=${id}
+done
+
+if [[ ${#inc_data[@]} -le 0 ]] ; then
+    printf "no incremental real-time training data file detected, existing...\n"
+    rm ${shell_in_run}
+    exit 0
+fi
+
+printf "got ${#inc_data[@]} new collect inc real-time training data file\n"
+
+train_file="$( IFS=$','; echo "${all_data[*]}" )"
+
+#test_file=${prefix}"/12/1/"${end}
+printf "train_file:%s\n" ${train_file}
+printf "test_file:%s\n" ${test_file}
+
+exit 0
+jarLib=hdfs://emr-cluster/warehouse/azkaban/lib/fhb_start_v1.jar
+queue=root.cpc.bigdata
+jars=("/home/cpc/anal/lib/spark-tensorflow-connector_2.11-1.10.0.jar" )
+
+randjar="fhb_start"`date +%s%N`".jar"
+hadoop fs -get ${jarLib} ${randjar}
+
+des_dir="hdfs://emr-cluster/user/cpc/fenghuabin/adlist-v4-transformer"
+
+spark-submit --master yarn --queue ${queue} \
+    --name "adlist-tf-make-sampling" \
+    --driver-memory 4g --executor-memory 4g \
+    --num-executors 100 --executor-cores 4 \
+    --conf spark.hadoop.fs.defaultFS=hdfs://emr-cluster2 \
+    --conf "spark.yarn.executor.memoryOverhead=4g" \
+    --conf "spark.sql.shuffle.partitions=500" \
+    --jars $( IFS=$','; echo "${jars[*]}" ) \
+    --class com.cpc.spark.ml.dnn.baseData.MakeSampling \
+    ${randjar} ${des_dir} ${train_file} ${test_file} ${curr_date} ${id}
+
+#chmod_des="hdfs://emr-cluster/user/cpc/fenghuabin/adlist-v4-sampled"
+#hadoop fs -chmod -R 0777 ${chmod_des}
+
+
+
+
+busy=${dir}/${busy_file}
+touch ${busy}
+hadoop fs -put ${busy} ${remote_busy}
+hadoop fs -chmod 0777 ${remote_busy}
+rm ${shell_in_run}
---
+++ src/main/scala/com/cpc/spark/ml/dnn/baseData/MakeAdListV4Samples.scala
@@ -0,0 +1,278 @@
+package com.cpc.spark.ml.dnn.baseData
+
+import java.io.{File, PrintWriter}
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import org.apache.commons.lang3.time.{DateFormatUtils, DateUtils}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.types.{StructField, _}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+
+import scala.collection.mutable.ArrayBuffer
+import scala.sys.process._
+import scala.util.Random
+
+/**
+  * 解析adlistv4tfrecord特征
+  * created time : 2019/09/19 23:31
+  * @author fenghuabin
+  * @version 1.0
+  *
+  */
+
+object MakeAdListV4Samples {
+
+
+  def delete_hdfs_path(path: String): Unit = {
+
+    val conf = new org.apache.hadoop.conf.Configuration()
+    val p = new org.apache.hadoop.fs.Path(path)
+    val hdfs = p.getFileSystem(conf)
+    val hdfs_path = new org.apache.hadoop.fs.Path(path.toString)
+
+    //val hdfs_path = new org.apache.hadoop.fs.Path(path.toString)
+    //val hdfs = org.apache.hadoop.fs.FileSystem.get(new org.apache.hadoop.conf.Configuration())
+    if (hdfs.exists(hdfs_path)) {
+      hdfs.delete(hdfs_path, true)
+    }
+  }
+
+  def exists_hdfs_path(path: String): Boolean = {
+
+    val conf = new org.apache.hadoop.conf.Configuration()
+    val p = new org.apache.hadoop.fs.Path(path)
+    val hdfs = p.getFileSystem(conf)
+    val hdfs_path = new org.apache.hadoop.fs.Path(path.toString)
+    //val hdfs = org.apache.hadoop.fs.FileSystem.get(new org.apache.hadoop.conf.Configuration())
+
+    if (hdfs.exists(hdfs_path)) {
+      true
+    } else {
+      false
+    }
+  }
+
+  def writeNum2File(file: String, num: Long): Unit = {
+    val writer = new PrintWriter(new File(file))
+    writer.write(num.toString)
+    writer.close()
+  }
+
+  def formatDate(date: Date, pattern: String = "yyyy-MM-dd"): String = {
+    val formatDate = DateFormatUtils.format(date, pattern)
+    formatDate
+  }
+
+  def GetDataRangeWithWeek(beginStr: String, endStr: String, format: String = "yyyy-MM-dd"): ArrayBuffer[String] = {
+    val ranges = ArrayBuffer[String]()
+    val sdf = new SimpleDateFormat(format)
+    var dateBegin = sdf.parse(beginStr)
+    val dateEnd = sdf.parse(endStr)
+    while (dateBegin.compareTo(dateEnd) <= 0) {
+      ranges += sdf.format(dateBegin) + ";" + DateFormatUtils.format(dateBegin, "E")
+      dateBegin = DateUtils.addDays(dateBegin, 1)
+    }
+    ranges
+  }
+
+  def main(args: Array[String]): Unit = {
+    if (args.length != 5) {
+      System.err.println(
+        """
+          |you have to input 8 parameters !!!
+        """.stripMargin)
+      System.exit(1)
+    }
+    //val Array(src, des_dir, des_date, des_map_prefix, numPartitions) = args
+    val Array(des_dir, train_files, test_file, curr_date, time_id) = args
+
+    println(args)
+
+    Logger.getRootLogger.setLevel(Level.WARN)
+
+    val sparkConf = new SparkConf()
+    sparkConf.set("spark.driver.maxResultSize", "5g")
+    val spark = SparkSession.builder().config(sparkConf).enableHiveSupport().getOrCreate()
+    val sc = spark.sparkContext
+
+    val idealIdMap = scala.collection.mutable.HashMap.empty[String, Int]
+    val unitIdMap = scala.collection.mutable.HashMap.empty[String, Int]
+    val planIdMap = scala.collection.mutable.HashMap.empty[String, Int]
+    val userIdMap = scala.collection.mutable.HashMap.empty[String, Int]
+
+    val idealIdMapBC = sc.broadcast(idealIdMap)
+    val unitIdMapBC = sc.broadcast(unitIdMap)
+    val planIdMapBC = sc.broadcast(planIdMap)
+    val userIdMapBC = sc.broadcast(userIdMap)
+
+    println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    println("Extract Test Examples' AD Info")
+    val text_test = des_dir + "/" + curr_date + "-" + time_id + "-test"
+
+    val importedDfTest: DataFrame = spark.read.format("tfrecords").option("recordType", "Example").load(test_file)
+    println("DF file count:" + importedDfTest.count().toString + " of file:" + test_file)
+    importedDfTest.printSchema()
+    importedDfTest.show(3)
+    importedDfTest.rdd.map(
+      rs => {
+        val idx2 = rs.getSeq[Long](0)
+        val idx1 = rs.getSeq[Long](1)
+        val idx_arr = rs.getSeq[Long](2)
+        val idx0 = rs.getSeq[Long](3)
+        val sample_idx = rs.getLong(4)
+        val label_arr = rs.getSeq[Long](5)
+        val dense = rs.getSeq[Long](6)
+
+        //11,12,13,14
+        //,ideaid,unitid,planid,userid
+        //(dense(11).toString, dense(12).toString, dense(13).toString, dense(14).toString)
+        val idealId = dense(11).toString
+        val unitId = dense(12).toString
+        val planId = dense(13).toString
+        val userId = dense(14).toString
+
+        if (idealIdMapBC.value.contains(idealId)) {
+          idealIdMapBC.value(idealId) = idealIdMapBC.value(idealId) + 1
+        } else {
+          idealIdMapBC.value(idealId) = 1
+        }
+
+        if (unitIdMapBC.value.contains(unitId)) {
+          unitIdMapBC.value(unitId) = unitIdMapBC.value(unitId) + 1
+        } else {
+          unitIdMapBC.value(unitId) = 1
+        }
+
+        if (planIdMapBC.value.contains(planId)) {
+          planIdMapBC.value(planId) = planIdMapBC.value(planId) + 1
+        } else {
+          planIdMapBC.value(planId) = 1
+        }
+
+        if (userIdMapBC.value.contains(userId)) {
+          userIdMapBC.value(userId) = userIdMapBC.value(userId) + 1
+        } else {
+          userIdMapBC.value(userId) = 1
+        }
+
+        val output = scala.collection.mutable.ArrayBuffer[String]()
+        output += sample_idx.toString
+        output += label_arr.map(_.toString).mkString(";")
+        output += dense.map(_.toString).mkString(";")
+        output += idx0.map(_.toString).mkString(";")
+        output += idx1.map(_.toString).mkString(";")
+        output += idx2.map(_.toString).mkString(";")
+        output += idx_arr.map(_.toString).mkString(";")
+        output.mkString("\t")
+      }
+    ).repartition(100).saveAsTextFile(text_test)
+
+    println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    println("Filter Train Examples")
+    val schema_new = StructType(List(
+      StructField("sample_idx", LongType, nullable = true),
+      StructField("label", ArrayType(LongType, containsNull = true)),
+      StructField("dense", ArrayType(LongType, containsNull = true)),
+      StructField("idx0", ArrayType(LongType, containsNull = true)),
+      StructField("idx1", ArrayType(LongType, containsNull = true)),
+      StructField("idx2", ArrayType(LongType, containsNull = true)),
+      StructField("id_arr", ArrayType(LongType, containsNull = true))
+    ))
+
+
+    val importedDf: DataFrame = spark.read.format("tfrecords").option("recordType", "Example").load(train_files)
+    println("DF file count:" + importedDf.count().toString + " of train files")
+    val text_train_rdd = importedDf.rdd.map(
+      rs => {
+        val idx2 = rs.getSeq[Long](0)
+        val idx1 = rs.getSeq[Long](1)
+        val idx_arr = rs.getSeq[Long](2)
+        val idx0 = rs.getSeq[Long](3)
+        val sample_idx = rs.getLong(4)
+        val label_arr = rs.getSeq[Long](5)
+        val dense = rs.getSeq[Long](6)
+
+        val idealId = dense(11).toString
+        val unitId = dense(12).toString
+        val planId = dense(13).toString
+        val userId = dense(14).toString
+
+        val output = scala.collection.mutable.ArrayBuffer[String]()
+        output += sample_idx.toString
+        output += label_arr.map(_.toString).mkString(";")
+        output += dense.map(_.toString).mkString(";")
+        output += idx0.map(_.toString).mkString(";")
+        output += idx1.map(_.toString).mkString(";")
+        output += idx2.map(_.toString).mkString(";")
+        output += idx_arr.map(_.toString).mkString(";")
+        (idealId, unitId, planId, userId, output.mkString("\t"))
+      }
+    ).filter(
+      rs => {
+        val idealId = rs._1
+        val unitId = rs._2
+        val planId = rs._3
+        val userId = rs._4
+        var filter = true
+        if (!idealIdMapBC.value.contains(idealId)) {
+          filter = false
+        }
+        if (!unitIdMapBC.value.contains(unitId)) {
+          filter = false
+        }
+        if (planIdMapBC.value.contains(planId)) {
+          filter = false
+        }
+        if (!userIdMapBC.value.contains(userId)) {
+          filter = false
+        }
+        filter
+      }
+    ).map(
+      rs => {
+        rs._5
+      }
+    )
+
+    val text_train_rdd_count = text_train_rdd.count
+    println(s"filter text_train_rdd_count is : $text_train_rdd_count")
+
+    val tf_train_rdd = text_train_rdd.map({
+      rs =>
+        val rs_list = rs.split("\t")
+        val sample_idx = rs_list(0).toLong
+        val label_arr = rs_list(2).split(";").map(_.toLong).toSeq
+        val dense = rs_list(3).split(";").map(_.toLong).toSeq
+        val idx0 = rs_list(4).split(";").map(_.toLong).toSeq
+        val idx1 = rs_list(5).split(";").map(_.toLong).toSeq
+        val idx2 = rs_list(6).split(";").map(_.toLong).toSeq
+        val idx_arr = rs_list(7).split(";").map(_.toLong).toSeq
+        Row(sample_idx, label_arr, dense, idx0, idx1, idx2, idx_arr)
+    })
+
+    val tf_train_rdd_count = tf_train_rdd.count
+    println(s"tf_train_rdd_count is : $tf_train_rdd_count")
+
+    val text_train = des_dir + "/" + curr_date + "-" + time_id + "-train-tf"
+    val tf_train = des_dir + "/" + curr_date + "-" + time_id + "-train-text"
+
+    val tf_df: DataFrame = spark.createDataFrame(tf_train_rdd, schema_new)
+    tf_df.repartition(100).write.format("tfrecords").option("recordType", "Example").save(tf_train)
+
+    //保存count文件
+    val fileName = "count_" + Random.nextInt(100000)
+    writeNum2File(fileName, tf_train_rdd_count)
+    s"hadoop fs -put $fileName $tf_train/count" !
+
+    s"hadoop fs -chmod -R 0777 $tf_train" !
+
+    text_train_rdd.repartition(100).saveAsTextFile(text_train)
+    s"hadoop fs -put $fileName $text_train/count" !
+
+    s"hadoop fs -chmod -R 0777 $text_train" !
+
+  }
+}
+
