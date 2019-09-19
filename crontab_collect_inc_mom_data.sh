420d1ae2c3eb80aef9cd9afaad08725f4c6a82ca
冯华宾 <fenghuabin@qutoutiao.net>
2019-09-20 00:40:24 +0800

    fhb_start

--- crontab_collect_inc_mom_data.sh
+++ crontab_collect_inc_mom_data.sh
@@ -210,7 +210,7 @@ des_dir="hdfs://emr-cluster/user/cpc/fenghuabin/adlist-v4-transformer"
 spark-submit --master yarn --queue ${queue} \
     --name "adlist-v4-make-samples" \
     --driver-memory 4g --executor-memory 4g \
-    --num-executors 100 --executor-cores 4 \
+    --num-executors 200 --executor-cores 2 \
     --conf spark.hadoop.fs.defaultFS=hdfs://emr-cluster2 \
     --conf "spark.yarn.executor.memoryOverhead=4g" \
     --conf "spark.sql.shuffle.partitions=500" \
