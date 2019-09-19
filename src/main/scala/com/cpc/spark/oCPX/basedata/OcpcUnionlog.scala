4f5b22d8c3c0e1727afea8d3b5b40943de5edbef
wangjun <wangjun02@qutoutiao.net>
2019-09-19 22:33:26 +0800

    fix bug

--- src/main/scala/com/cpc/spark/oCPX/basedata/OcpcUnionlog.scala
+++ src/main/scala/com/cpc/spark/oCPX/basedata/OcpcUnionlog.scala
@@ -21,10 +21,10 @@ object OcpcUnionlog {
 //      .write.mode("overwrite").insertInto("test.ocpc_base_unionlog")
       .write.mode("overwrite").insertInto("dl_cpc.ocpc_base_unionlog")
 
-    data
-      .repartition(100)
-//      .write.mode("overwrite").insertInto("test.ocpc_base_unionlog_hourly")
-      .write.mode("overwrite").insertInto("dl_cpc.ocpc_base_unionlog_hourly")
+//    data
+//      .repartition(100)
+////      .write.mode("overwrite").insertInto("test.ocpc_base_unionlog_hourly")
+//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_base_unionlog_hourly")
 
     println("successfully save data into table: dl_cpc.ocpc_base_unionlog")
 
@@ -35,10 +35,10 @@ object OcpcUnionlog {
 //      .write.mode("overwrite").insertInto("test.ocpc_filter_unionlog")
       .write.mode("overwrite").insertInto("dl_cpc.ocpc_filter_unionlog")
 
-    ocpcData
-      .repartition(50)
-//      .write.mode("overwrite").insertInto("test.ocpc_filter_unionlog_hourly")
-      .write.mode("overwrite").insertInto("dl_cpc.ocpc_filter_unionlog_hourly")
+//    ocpcData
+//      .repartition(50)
+////      .write.mode("overwrite").insertInto("test.ocpc_filter_unionlog_hourly")
+//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_filter_unionlog_hourly")
 
     println("successfully save data into table: dl_cpc.ocpc_filter_unionlog")
   }
