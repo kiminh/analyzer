53b73a9769d679fb3c52b58f331e89e69ddb40e0
冯华宾 <fenghuabin@qutoutiao.net>
2019-09-23 20:07:49 +0800

    fhb_start

--- crontab_collect_inc_mom_data.sh
+++ crontab_collect_inc_mom_data.sh
@@ -287,7 +287,7 @@ history_file="${p00},${p01},${p02},${p03},${p04},${p05},${p06}"
 spark-submit --master yarn --queue ${queue} \
     --name "adlist-v4-make-samples" \
     --driver-memory 4g --executor-memory 4g \
-    --num-executors 1000 --executor-cores 4 \
+    --num-executors 500 --executor-cores 4 \
     --conf spark.hadoop.fs.defaultFS=hdfs://emr-cluster2 \
     --conf "spark.yarn.executor.memoryOverhead=4g" \
     --conf "spark.sql.shuffle.partitions=500" \
