bab7790532e02c0c6aff1c61f3bf199ddc76725e
齐智 <qizhi@qutoutiao.net>
2019-09-24 11:19:56 +0800

    pass_back

--- src/main/scala/com/cpc/spark/ml/cvrmodel/daily/LREval.scala
+++ src/main/scala/com/cpc/spark/ml/cvrmodel/daily/LREval.scala
@@ -52,7 +52,7 @@ object LREval {
          | , planid
          | , unitid
          | , ideaid
-         | , IF(rank=1,1,0) as label from
+         | , IF(rank=1 and imeinotnull=1,1,0) as label from
          |(select searchid
          | , uid
          | , md5uid
@@ -72,7 +72,8 @@ object LREval {
          | , planid
          | , unitid
          | , ideaid
-         | , Row_Number() OVER (partition by uid ORDER BY `timestamp` desc) as rank from
+         | , imeinotnull
+         | , Row_Number() OVER (partition by uid,part ORDER BY `timestamp` desc) as rank from
          |(select searchid
          | , uid
          | , md5uid
@@ -92,9 +93,9 @@ object LREval {
          | , planid
          | , unitid
          | , ideaid
-         | , `timestamp` from
-         |(select imei,dt,date_add(dt,-7) as before7dt from dl_cpc.pass_back_test_qizhi)t1
-         |join
+         | , `timestamp`
+         | , IF(imei is not null,1,0) as imeinotnull
+         | , IF(imei is not null,10100,int(rand()*10000)) as part from
          |(select
          |   searchid
          | , uid
@@ -119,12 +120,11 @@ object LREval {
          |from dl_cpc.cpc_basedata_click_event
          |where day>='2019-06-01' and day<='2019-09-09'
          |  and media_appsid in ('80000001','80000002')
-         |  and adslot_type in (1, 2)
          |  and isclick = 1
-         |  and ideaid > 0
-         |  and unitid > 0
-         |  and userid in (1562482,1568203,1582093,1598145,1604411,1616036,1616821,1629979,1629982,1638653,1641461,1641463,1641469,1588335,1594022,1641470,1653235,1656197,1665043,1673378,1666928,1629538,1629537,1569962) )t2
-         |on t1.imei=t2.md5uid and dt>=day and before7dt<=day)t3)t4
+         |  and userid in (1562482,1568203,1582093,1598145,1604411,1616036,1616821,1629979,1629982,1638653,1641461,1641463,1641469,1588335,1594022,1641470,1653235,1656197,1665043,1673378,1666928,1629538,1629537,1569962))t1
+         |left outer join
+         |(select imei,dt,date_add(dt,-7) as before7dt from dl_cpc.pass_back_test_qizhi)t2
+         |on t1.md5uid=t2.imei and dt>=day and before7dt<=day)t3)t4
       """.stripMargin
     println("sql="+bs_sql)
     val bs_df=spark.sql(bs_sql)
