800770e00ea6290cb0c16dddedd35a0c823acf59
冯华宾 <fenghuabin@qutoutiao.net>
2019-09-26 01:53:11 +0800

    fhb_start

---
+++ crontab_base_daily_weight.sh
@@ -0,0 +1,157 @@
+#!/usr/bin/env bash
+#fenghuabin@qutoutiao.net
+
+source /etc/profile
+
+date_full=`date`
+printf "*****************************${date_full}********************************\n"
+
+now_hour=$(date "+%H")
+now_minutes=$(date "+%M")
+now_id="00"${now_hour}
+if [ ${now_minutes} -ge 30 ];then
+    now_id="30"${now_hour}
+fi
+printf "now id is:%s\n" ${now_id}
+
+dir=aggr_rock_samples
+if [[ ! -d "${dir}" ]]; then
+    mkdir ${dir}
+fi
+
+shell_in_run=${dir}/shell_in_busy
+if [[ -f "$shell_in_run" ]]; then
+    printf "shell are busy now, existing\n"
+    printf "*****************************${date_full}********************************\n"
+    exit 0
+fi
+touch ${shell_in_run}
+
+
+last_date=`date --date='1 days ago' +%Y-%m-%d`
+aggr_path="hdfs://emr-cluster/user/cpc/fenghuabin/rockefeller_backup/${last_date}-aggr"
+file_success=${dir}/${last_date}_aggr_success
+file_count=${dir}/${last_date}_aggr_count
+
+if [[ ! -f ${file_success} ]]; then
+    hadoop fs -get ${aggr_path}/_SUCCESS ${file_success} &
+fi
+if [[ ! -f ${file_count} ]]; then
+    hadoop fs -get ${aggr_path}/count ${file_count} &
+fi
+wait
+if [[ ! -f ${file_success} ]]; then
+    printf "no ${file_success}, exiting...\n"
+    rm ${shell_in_run}
+    exit 0
+fi
+if [[ ! -f ${file_count} ]]; then
+    printf "no ${file_count}, exiting...\n"
+    rm ${shell_in_run}
+    exit 0
+fi
+
+sample_list=(
+    `date --date='1 days ago' +%Y-%m-%d`
+    `date --date='2 days ago' +%Y-%m-%d`
+    `date --date='3 days ago' +%Y-%m-%d`
+    `date --date='4 days ago' +%Y-%m-%d`
+    `date --date='5 days ago' +%Y-%m-%d`
+    `date --date='6 days ago' +%Y-%m-%d`
+    `date --date='7 days ago' +%Y-%m-%d`
+    `date --date='8 days ago' +%Y-%m-%d`
+    `date --date='9 days ago' +%Y-%m-%d`
+    `date --date='10 days ago' +%Y-%m-%d`
+    `date --date='11 days ago' +%Y-%m-%d`
+    `date --date='12 days ago' +%Y-%m-%d`
+    `date --date='13 days ago' +%Y-%m-%d`
+    `date --date='14 days ago' +%Y-%m-%d`
+    `date --date='15 days ago' +%Y-%m-%d`
+    `date --date='16 days ago' +%Y-%m-%d`
+    `date --date='17 days ago' +%Y-%m-%d`
+    `date --date='18 days ago' +%Y-%m-%d`
+    `date --date='19 days ago' +%Y-%m-%d`
+    `date --date='20 days ago' +%Y-%m-%d`
+    `date --date='21 days ago' +%Y-%m-%d`
+)
+
+
+collect_file=()
+collect_date=()
+for idx in "${!sample_list[@]}";
+do
+    echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    curr_date="${sample_list[$idx]}"
+    echo "curr_date:${curr_date}"
+    aggr_path="hdfs://emr-cluster/user/cpc/fenghuabin/rockefeller_backup/${curr_date}-aggr"
+    echo ${aggr_path}
+    file_success=${dir}/${curr_date}_aggr_success
+    file_count=${dir}/${curr_date}_aggr_count
+    if [[ ! -f ${file_success} ]]; then
+        hadoop fs -get ${aggr_path}/_SUCCESS ${file_success} &
+    fi
+    if [[ ! -f ${file_count} ]]; then
+        hadoop fs -get ${aggr_path}/count ${file_count} &
+    fi
+    wait
+    if [[ ! -f ${file_success} ]]; then
+        printf "no ${file_success}, continue...\n"
+    fi
+    if [[ ! -f ${file_count} ]]; then
+        printf "no ${file_count}, continue...\n"
+    fi
+    collect_date+=(${curr_date})
+    collect_file+=("${aggr_path}/part-r-*")
+done
+
+if [[ ${#collect_file[@]} -le 0 ]] ; then
+    printf "no aggr training data file, existing...\n"
+    rm ${shell_in_run}
+    exit 0
+fi
+
+date_list="$( IFS=$';'; echo "${collect_date[*]}" )"
+file_list="$( IFS=$';'; echo "${collect_file[*]}" )"
+
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "${file_list}"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "${date_list}"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+#rm ${shell_in_run}
+#exit 0
+
+jarLib=hdfs://emr-cluster/warehouse/azkaban/lib/fhb_start_v1.jar
+queue=root.cpc.bigdata
+queue=root.cpc.develop
+jars=("/home/cpc/anal/lib/spark-tensorflow-connector_2.11-1.10.0.jar" )
+
+randjar="fhb_start"`date +%s%N`".jar"
+hadoop fs -get ${jarLib} ${randjar}
+
+des_dir="hdfs://emr-cluster/user/cpc/fenghuabin/adlist-v4-transformer"
+
+delete_old=true
+curr_date=`date --date='0 days ago' +%Y-%m-%d`
+
+spark-submit --master yarn --queue ${queue} \
+    --name "make-base-daily-samples" \
+    --driver-memory 8g --executor-memory 4g \
+    --num-executors 1000 --executor-cores 4 \
+    --conf spark.hadoop.fs.defaultFS=hdfs://emr-cluster2 \
+    --conf "spark.yarn.executor.memoryOverhead=4g" \
+    --conf "spark.sql.shuffle.partitions=500" \
+    --jars $( IFS=$','; echo "${jars[*]}" ) \
+    --class com.cpc.spark.ml.dnn.baseData.MakeBaseDailyWeight\
+    ${randjar} ${des_dir} ${file_list} ${curr_date} ${delete_old}
+
+#chmod_des="hdfs://emr-cluster/user/cpc/fenghuabin/adli}
+
+#busy=${dir}/${busy_file}
+#touch ${busy}
+#hadoop fs -put ${busy} ${remote_busy}
+#hadoop fs -chmod 0777 ${remote_busy}
+
+rm ${shell_in_run}
+
---
+++ src/main/scala/com/cpc/spark/ml/dnn/baseData/MakeBaseDailyWeight.scala
@@ -0,0 +1,232 @@
+package com.cpc.spark.ml.dnn.baseData
+
+import java.io.{File, PrintWriter}
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import org.apache.commons.lang3.time.{DateFormatUtils, DateUtils}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.types.{StructField, _}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+
+import scala.collection.mutable.ArrayBuffer
+import scala.sys.process._
+import scala.util.Random
+
+/**
+  * @author fenghuabin
+  * @version 1.0
+  *
+  */
+
+object MakeBaseDailyWeight {
+
+
+  def delete_hdfs_path(path: String): Unit = {
+
+    val conf = new org.apache.hadoop.conf.Configuration()
+    val p = new org.apache.hadoop.fs.Path(path)
+    val hdfs = p.getFileSystem(conf)
+    val hdfs_path = new org.apache.hadoop.fs.Path(path.toString)
+
+    //val hdfs_path = new org.apache.hadoop.fs.Path(path.toString)
+    //val hdfs = org.apache.hadoop.fs.FileSystem.get(new org.apache.hadoop.conf.Configuration())
+    if (hdfs.exists(hdfs_path)) {
+      hdfs.delete(hdfs_path, true)
+    }
+  }
+
+  def exists_hdfs_path(path: String): Boolean = {
+
+    val conf = new org.apache.hadoop.conf.Configuration()
+    val p = new org.apache.hadoop.fs.Path(path)
+    val hdfs = p.getFileSystem(conf)
+    val hdfs_path = new org.apache.hadoop.fs.Path(path.toString)
+    //val hdfs = org.apache.hadoop.fs.FileSystem.get(new org.apache.hadoop.conf.Configuration())
+
+    if (hdfs.exists(hdfs_path)) {
+      true
+    } else {
+      false
+    }
+  }
+
+  def writeNum2File(file: String, num: Long): Unit = {
+    val writer = new PrintWriter(new File(file))
+    writer.write(num.toString)
+    writer.close()
+  }
+
+  def formatDate(date: Date, pattern: String = "yyyy-MM-dd"): String = {
+    val formatDate = DateFormatUtils.format(date, pattern)
+    formatDate
+  }
+
+  def GetDataRangeWithWeek(beginStr: String, endStr: String, format: String = "yyyy-MM-dd"): ArrayBuffer[String] = {
+    val ranges = ArrayBuffer[String]()
+    val sdf = new SimpleDateFormat(format)
+    var dateBegin = sdf.parse(beginStr)
+    val dateEnd = sdf.parse(endStr)
+    while (dateBegin.compareTo(dateEnd) <= 0) {
+      ranges += sdf.format(dateBegin) + ";" + DateFormatUtils.format(dateBegin, "E")
+      dateBegin = DateUtils.addDays(dateBegin, 1)
+    }
+    ranges
+  }
+
+  def main(args: Array[String]): Unit = {
+    if (args.length != 4) {
+      System.err.println(
+        """
+          |you have to input 4 parameters !!!
+        """.stripMargin)
+      System.exit(1)
+    }
+    //val Array(src, des_dir, des_date, des_map_prefix, numPartitions) = args
+    val Array(des_dir, file_list, , curr_date, delete_old) = args
+
+    println(args)
+
+    Logger.getRootLogger.setLevel(Level.WARN)
+
+    val sparkConf = new SparkConf()
+    sparkConf.set("spark.driver.maxResultSize", "5g")
+    val spark = SparkSession.builder().config(sparkConf).enableHiveSupport().getOrCreate()
+    val sc = spark.sparkContext
+
+
+    val bid_mmh_map_file = des_dir + "/" + "bid_mmh_map.txt"
+
+    val bid_mmh_map = sc.textFile(bid_mmh_map_file).map({
+      rs =>
+        val line_list = rs.split("\t")
+        (line_list(0), line_list(1))
+    }).collectAsMap()
+    println("bid_mmh_map.size=" + bid_mmh_map.size)
+
+
+    println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+    val bid_cpm_file = des_dir + "/" + curr_date + "-" + "-bid-cpm-weight-info"
+    val weighted_file_collect = des_dir + "/" + curr_date + "-21days-weight"
+
+    if (delete_old == "true") {
+      delete_hdfs_path(bid_cpm_file)
+      delete_hdfs_path(weighted_file_collect)
+    }
+
+    if (!exists_hdfs_path(bid_cpm_file)) {
+      val df_train_files: DataFrame = spark.read.format("tfrecords").option("recordType", "Example").load(file_list)
+      //println("DF file count:" + df_train_files.count().toString + " of file:" + train_files)
+      df_train_files.printSchema()
+      df_train_files.show(3)
+      val info_rdd = df_train_files.rdd.map(
+        rs => {
+          val idx2 = rs.getSeq[Long](0)
+          val idx1 = rs.getSeq[Long](1)
+          val idx_arr = rs.getSeq[Long](2)
+          val idx0 = rs.getSeq[Long](3)
+          val sample_idx = rs.getLong(4)
+          val label_arr = rs.getSeq[Long](5)
+          val dense = rs.getSeq[Long](6)
+
+          var label = 0.0
+          if (label_arr.head == 1L) {
+            label = 1.0
+          }
+
+          val bid = dense(10).toString
+          val bid_ori = bid_mmh_map.getOrElse(bid, "-1")
+          (bid, bid_ori, label, 1.0)
+        }
+      ).map({
+        rs =>
+          (rs._1 + "\t" + rs._2, (rs._3, rs._4))
+      }).reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2)).map({
+        rs =>
+          val key_list = rs._1.split("\t")
+          val bid_ori = key_list(1).toFloat
+          val ctr = rs._2._1/rs._2._2
+          val cpm = ctr * bid_ori
+          (key_list(0), key_list(1), ctr, cpm, rs._2._1, rs._2._2)
+      })
+
+      val total_cpm_map = info_rdd.map({
+        rs =>
+          ("placeholder", rs._4)
+      }).reduceByKey(_ + _).collectAsMap()
+
+      val total_cpm = total_cpm_map.getOrElse("placeholder", 0.0).toFloat
+      println("total_cpm=" + total_cpm)
+
+      info_rdd.map({
+        rs =>
+          val weight = rs._4/total_cpm
+          (rs._1, rs._2, rs._3, rs._4, total_cpm, weight, rs._5, rs._6)
+      }).repartition(1).sortBy(_._6 * -1).map({
+        rs=>
+          rs._1 + "\t" + rs._2 + "\t" + rs._3 + "\t" + rs._4 + "\t" + rs._5 + "\t" + rs._6 + "\t" + rs._7 + "\t" + rs._8
+      }).saveAsTextFile(bid_cpm_file)
+
+
+      val sta_map = sc.textFile(bid_cpm_file).map({
+        rs =>
+          val line_list = rs.split("\t")
+          val bid_hash = line_list(0)
+          val bid_ori = line_list(1)
+          val ctr = line_list(2)
+          val cpm = line_list(3)
+          val total_cpm = line_list(4)
+          val weight = line_list(5)
+          val click = line_list(6)
+          val imp = line_list(7)
+          (bid_hash, weight)
+      }).collectAsMap()
+
+      println("sta_map.size=" + sta_map.size)
+
+      val schema_new = StructType(List(
+        StructField("sample_idx", LongType, nullable = true),
+        StructField("label", ArrayType(LongType, containsNull = true)),
+        StructField("weight", FloatType, nullable = true),
+        StructField("dense", ArrayType(LongType, containsNull = true)),
+        StructField("idx0", ArrayType(LongType, containsNull = true)),
+        StructField("idx1", ArrayType(LongType, containsNull = true)),
+        StructField("idx2", ArrayType(LongType, containsNull = true)),
+        StructField("id_arr", ArrayType(LongType, containsNull = true))
+      ))
+
+      val weighted_rdd = df_train_files.rdd.map(
+        rs => {
+          val idx2 = rs.getSeq[Long](0)
+          val idx1 = rs.getSeq[Long](1)
+          val idx_arr = rs.getSeq[Long](2)
+          val idx0 = rs.getSeq[Long](3)
+          val sample_idx = rs.getLong(4)
+          val label_arr = rs.getSeq[Long](5)
+          val dense = rs.getSeq[Long](6)
+
+          val bid = dense(10).toString
+          val weight = sta_map.getOrElse(bid, "1e-5").toFloat
+
+          Row(sample_idx, label_arr, weight, dense, idx0, idx1, idx2, idx_arr)
+        })
+
+      val weighted_rdd_count = weighted_rdd.count()
+      println(s"weighted_rdd_count is : $weighted_rdd_count")
+      println("DF file count:" + weighted_rdd_count.toString + " of file:" + file_list)
+
+      val tf_df: DataFrame = spark.createDataFrame(weighted_rdd, schema_new)
+      tf_df.repartition(12000).write.format("tfrecords").option("recordType", "Example").save(weighted_file_collect)
+
+      //保存count文件
+      var fileName = "count_" + Random.nextInt(100000)
+      writeNum2File(fileName, weighted_rdd_count)
+      s"hadoop fs -put $fileName $weighted_file_collect/count" !
+
+      s"hadoop fs -chmod -R 0777 $weighted_file_collect" !
+    }
+  }
+}
+
