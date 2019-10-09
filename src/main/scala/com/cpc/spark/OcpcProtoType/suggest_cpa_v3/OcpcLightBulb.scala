abf2f57b2f36c9f82ec3f7fcb54bb2f3531156e8
wangjun <wangjun02@qutoutiao.net>
2019-10-09 15:57:43 +0800

    准备上线

--- src/main/scala/com/cpc/spark/OcpcProtoType/suggest_cpa_v3/OcpcLightBulb.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/suggest_cpa_v3/OcpcLightBulb.scala
@@ -38,15 +38,15 @@ object OcpcLightBulb{
 
     currentLight
       .repartition(5)
-      .write.mode("overwrite").insertInto("test.ocpc_unit_light_control_hourly")
-//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_unit_light_control_hourly")
+//      .write.mode("overwrite").insertInto("test.ocpc_unit_light_control_hourly")
+      .write.mode("overwrite").insertInto("dl_cpc.ocpc_unit_light_control_hourly")
 
     currentLight
       .repartition(5)
       .select("unitid", "userid", "adclass", "media", "cpa", "version")
       .repartition(5)
-      .write.mode("overwrite").insertInto("test.ocpc_unit_light_control_version")
-//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_unit_light_control_version")
+//      .write.mode("overwrite").insertInto("test.ocpc_unit_light_control_version")
+      .write.mode("overwrite").insertInto("dl_cpc.ocpc_unit_light_control_version")
 
     // 根据上一个小时的灯泡数据，分别判断需要熄灭和点亮的灯泡
     // todo
@@ -58,8 +58,8 @@ object OcpcLightBulb{
       .withColumn("date", lit(date))
       .withColumn("hour", lit(hour))
       .repartition(1)
-      .write.mode("overwrite").insertInto("test.ocpc_auto_second_stage_hourly")
-//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_auto_second_stage_hourly")
+//      .write.mode("overwrite").insertInto("test.ocpc_auto_second_stage_hourly")
+      .write.mode("overwrite").insertInto("dl_cpc.ocpc_auto_second_stage_hourly")
 
 
 
@@ -88,8 +88,8 @@ object OcpcLightBulb{
 
     resultDF
       .repartition(5)
-      .write.mode("overwrite").insertInto("test.ocpc_light_api_control_hourly")
-//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_light_api_control_hourly")
+//      .write.mode("overwrite").insertInto("test.ocpc_light_api_control_hourly")
+      .write.mode("overwrite").insertInto("dl_cpc.ocpc_light_api_control_hourly")
 
     // 存入redis
 //    saveDataToRedis(version, date, hour, spark)
