ead33ccb5ec2adbbf503cd5ae5f66b39780be52a
wangjun <wangjun02@qutoutiao.net>
2019-10-10 11:44:19 +0800

    准备上线

--- src/main/scala/com/cpc/spark/oCPX/conversionMonitor/conversionMonitor.scala
+++ src/main/scala/com/cpc/spark/oCPX/conversionMonitor/conversionMonitor.scala
@@ -1,7 +1,11 @@
 package com.cpc.spark.oCPX.conversionMonitor
 
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
 
 /**
@@ -26,11 +30,94 @@ object conversionMonitor {
     println(s"date=$date")
 
     // todo 抽取点击数据和消费数据，按照unitid, userid, conversion_goal, conversion_from, is_ocpc做grouby
+    val clickData = getClickCost(date, dayCnt, spark)
     // todo 抽取转化数据，按照unitid, userid, conversion_goal, conversion_from做groupby
+    val cvData = getCVdata(date, dayCnt, spark)
     // todo 按照最小点击数限额，找到对应conversion_goal与conversion_from中无转化的单元
     // todo 根据是否是ocpc单元选择发送报警邮件
 
 
+  }
+
+  def getCVdata(date: String, dayCnt: Int, spark: SparkSession) = {
+    // 取历史数据
+    val dateConverter = new SimpleDateFormat("yyyy-MM-dd")
+    val today = dateConverter.parse(date)
+    val calendar = Calendar.getInstance
+    calendar.setTime(today)
+    calendar.add(Calendar.HOUR, -dayCnt)
+    val yesterday = calendar.getTime
+    val tmpDate = dateConverter.format(yesterday)
+    val date1 = tmpDate(0)
+    val selectCondition = s"`date` between '$date1' and '$date'"
+
+    // 收集转化数据
+    val sqlRequest =
+      s"""
+         |SELECT
+         |  searchid,
+         |  unitid,
+         |  userid,
+         |  conversion_goal,
+         |  conversion_from
+         |FROM
+         |  dl_cpc.ocpc_cvr_log_hourly
+         |WHERE
+         |  $selectCondition
+         |""".stripMargin
+    println(sqlRequest)
+    val rawData = spark.sql(sqlRequest)
+    val resultDF = rawData
+      .groupBy("unitid", "userid", "conversion_goal", "conversion_from")
+      .agg(
+        countDistinct(col("searchid")).alias("cv")
+      )
+      .select("unitid", "userid", "conversion_goal", "conversion_from", "cv")
+
+    resultDF
+  }
+
+  def getClickCost(date: String, dayCnt: Int, spark: SparkSession) = {
+    // 取历史数据
+    val dateConverter = new SimpleDateFormat("yyyy-MM-dd")
+    val today = dateConverter.parse(date)
+    val calendar = Calendar.getInstance
+    calendar.setTime(today)
+    calendar.add(Calendar.HOUR, -dayCnt)
+    val yesterday = calendar.getTime
+    val tmpDate = dateConverter.format(yesterday)
+    val date1 = tmpDate(0)
+    val selectCondition = s"`date` between '$date1' and '$date'"
+
+    // 收集点击和消费数据
+    val sqlRequest =
+      s"""
+         |SELECT
+         |  searchid,
+         |  unitid,
+         |  userid,
+         |  conversion_goal,
+         |  conversion_from,
+         |  is_ocpc,
+         |  isclick,
+         |  price * 0.01 as price
+         |FROM
+         |  dl_cpc.ocpc_base_unionlog
+         |WHERE
+         |  $selectCondition
+         |AND isclick = 1
+         |""".stripMargin
+    println(sqlRequest)
+    val rawData = spark.sql(sqlRequest)
+    val resultDF = rawData
+      .groupBy("unitid", "userid", "conversion_goal", "conversion_from", "is_ocpc")
+      .agg(
+        sum(col("isclick")).alias("click"),
+        sum(col("price")).alias("price")
+      )
+      .select("unitid", "userid", "conversion_goal", "conversion_from", "is_ocpc", "click", "price")
+
+    resultDF
   }
 
 
