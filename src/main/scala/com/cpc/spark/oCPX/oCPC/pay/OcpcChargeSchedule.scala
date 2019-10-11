e070f69f79edfaca5968707fa67ef375aa579fc8
wangjun <wangjun02@qutoutiao.net>
2019-10-11 22:26:30 +0800

    update

--- src/main/scala/com/cpc/spark/oCPX/oCPC/pay/OcpcChargeSchedule.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/pay/OcpcChargeSchedule.scala
@@ -78,7 +78,7 @@ object OcpcChargeSchedule {
          |  pay_cnt,
          |  pay_date,
          |  end_date,
-         |  (case when end_date < '$date' and pay_cnt < 4 then 1 else 0 end) as update_flag
+         |  (case when end_date <= '$date' and pay_cnt < 4 then 1 else 0 end) as update_flag
          |FROM
          |  raw_data
          |""".stripMargin
@@ -141,8 +141,6 @@ object OcpcChargeSchedule {
       .na.fill(0, Seq("pay_cnt"))
       .na.fill(date, Seq("pay_date"))
       .filter(s"pay_date <= '$date'")
-    //      .withColumn("pay_cnt", when(col("pay_cnt") < 5, col("pay_cnt") + 1).otherwise(col("pay_cnt")))
-//      .withColumn("flag", when(col("pay_cnt") < 5, 1).otherwise(0))
 
     data.printSchema()
     data
