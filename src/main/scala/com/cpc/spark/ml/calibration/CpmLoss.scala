9193907362a762df7e0a4689dfa24f58dd8ff79a
wangyao <wangyao@qutoutiao.net>
2019-10-11 19:46:33 +0800

    -

---
+++ src/main/scala/com/cpc/spark/ml/calibration/CpmLoss.scala
@@ -0,0 +1,135 @@
+package com.cpc.spark.ml.calibration
+
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+/**
+  * author: wangyao
+  * date: 9/30/19
+  */
+object CpmLoss {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder()
+      .appName("cpm loss")
+      .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+      .enableHiveSupport()
+      .getOrCreate()
+    val sql =
+      s"""
+         | select ctr_model_name,avg(sample_loss*(cpm+0.01)) as cpm_weight_loss
+         | from
+         | (
+         | select cpm,cast(isclick as int) as isclick,cast(raw_ctr as int) raw_ctr,
+         |if(isclick = 1, -log(raw_ctr/1000000),-log(1-raw_ctr/1000000)) as sample_loss, ctr_model_name
+         |from dl_cpc.slim_union_log a
+         |join
+         |(
+         |select ideaid, sum(if(isclick = 1,price,0))/sum(isshow)*10 cpm
+         |from dl_cpc.slim_union_log
+         |where dt = '2019-09-15'
+         |and media_appsid in ('80000001','80000002')
+         |and isshow = 1
+         |and adslot_type = 1
+         |and adtype != 15
+         |and adsrc in (1,28)
+         |group by ideaid
+         |) b
+         |    on a.ideaid = b.ideaid
+         |    where dt = '2019-09-15'
+         |    and media_appsid in ('80000001','80000002')
+         |    and isshow = 1
+         |    and adslot_type = 1
+         |    and adtype != 15
+         |    and adsrc in (1,28)
+         | )
+         | group by ctr_model_name
+         |""".stripMargin
+
+    println(sql)
+
+    val basedata = spark.sql(sql)
+      .withColumn("score",col("raw_ctr"))
+      .withColumn("label",col("isclick"))
+      .repartition(1000)
+
+    val result = getCpmAuc(spark,basedata)
+    result.show(10)
+    result.write.mode("overwrite").saveAsTable("dl_cpc.wy_test00")
+    println("job success")
+
+  }
+  case class LabeledPred(predict: Double, label: Int)
+
+  def getCpmAuc(spark:SparkSession, data:DataFrame, g:String = "ctr_model_name"):DataFrame = {
+    import spark.implicits._
+    val aucAndSum = data
+      .select("score","label","cpm",g)
+      .rdd
+      .map(x => (x.getAs[String](g),
+        (x.getAs[Int]("score"), x.getAs[Int]("label"),x.getAs[Double]("cpm"))))
+      .combineByKey(
+        x => List(x),
+        (x: List[(Int, Int, Double)], y: (Int, Int, Double)) => y :: x,
+        (x: List[(Int, Int, Double)], y: List[(Int, Int, Double)]) => x ::: y
+      )
+      .mapValues(x => {
+        val label = x.map(x => x._2)
+        val max = x.map(x => x._1).max + 2
+        val cpm = x.map(x => x._2)
+        val pos = Array.fill(max)(0.0)
+        val neg = Array.fill(max)(0.0)
+//        val n = label.sum //正样本数
+//        val m = x.length - n  //负样本数
+
+        for ((s,l,c) <- x){
+          if (l == 0) neg(s) += c
+          else pos(s) += c
+        }
+        val n = pos.sum
+        val m = neg.sum
+
+        var negSum = 0.0
+        var auc: Double = 0
+        for (i <- 0 to max - 1 ) {
+          auc += 1.0 * pos(i) * negSum + pos(i) * neg(i) * 0.5
+          negSum += neg(i)
+        }
+        val result = if (m <= 0.0 || n <= 0.0) (-1.0, 0.0 + m + n) else (auc / (1.0 * m * n), 0.0 + m + n)
+        result
+      }).map(x => (x._1,x._2._1,x._2._2))
+      .map(x => CpmAuc(model = x._1, auc = x._2, sum = x._3))
+      .toDF()
+    aucAndSum
+  }
+
+  case class CpmAuc(
+                     var model: String="",
+                     var auc: Double=0,
+                     var sum: Double=0
+                   )
+
+  def auc(points: Seq[LabeledPred]) = {
+    val posNum = points.count(_.label > 0)
+    val negNum = points.length - posNum
+
+    if (posNum == 0 || negNum == 0) {
+      println("Error: Lables of all samples are the same.")
+      0.0
+    } else {
+      val sorted = points.sortBy(_.predict)
+
+      var negSum = 0
+      var posGTNeg = 0
+      for (p <- sorted) {
+        if (p.label > 0) {
+          posGTNeg = posGTNeg + negSum
+        } else {
+          negSum = negSum + 1
+        }
+      }
+
+      posGTNeg.toDouble/(posNum * negNum).toDouble
+    }
+  }
+}
---
+++ src/main/scala/com/cpc/spark/ml/calibration/exp/LinearRegressionOnQttCvrCalibration.scala
@@ -0,0 +1,189 @@
+package com.cpc.spark.ml.calibration.exp
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import org.apache.spark.ml.feature.{OneHotEncoder, StringIndexer}
+import com.cpc.spark.common.Utils
+import com.cpc.spark.ml.calibration.exp.LrCalibrationOnQtt.calculateAuc
+import com.cpc.spark.ocpc.OcpcUtils._
+import com.typesafe.config.ConfigFactory
+import org.apache.spark.ml.regression.LinearRegression
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import java.time.LocalDateTime
+import org.apache.spark.ml.{Pipeline, PipelineStage}
+import java.time.format.DateTimeFormatter
+import scala.collection.mutable.ListBuffer
+import com.cpc.spark.common.Utils
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.sql.functions._
+
+import scala.collection.mutable
+
+object LinearRegressionOnQttCvrCalibration {
+  def main(args: Array[String]): Unit = {
+    // new calibration
+    val endDate = args(0)
+    val endHour = args(1)
+    val hourRange = args(2).toInt
+    val media = args(3)
+//    val model = args(4)
+//    val calimodel = args(5)
+    val k = args(6)
+    val conf = ConfigFactory.load("ocpc")
+    val conf_key = "medias." + media + ".media_selection"
+    val mediaSelection = conf.getString(conf_key)
+
+
+    val endTime = LocalDateTime.parse(s"$endDate-$endHour", DateTimeFormatter.ofPattern("yyyy-MM-dd-HH"))
+    val startTime = endTime.minusHours(Math.max(hourRange - 1, 0))
+
+    val startDate = startTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+    val startHour = startTime.format(DateTimeFormatter.ofPattern("HH"))
+
+    println(s"endDate=$endDate")
+    println(s"endHour=$endHour")
+    println(s"hourRange=$hourRange")
+    println(s"startDate=$startDate")
+    println(s"startHour=$startHour")
+    // parse and process input
+    val model = "qtt-cvr-dnn-rawid-v1wzjf-aibox"
+    val calimodel ="qtt-cvr-dnn-rawid-v1wzjf-aibox"
+
+    // build spark session
+    val spark = Utils.buildSparkSession("hourlyCalibration")
+    val selectCondition1 = getTimeRangeSql(startDate, startHour, endDate, endHour)
+    val selectCondition2 = getTimeRangeSql4(startDate, startHour, endDate, endHour)
+    import spark.implicits._
+
+    // get union log
+    val sql = s"""
+                      |select a.searchid, cast(b.raw_cvr as bigint) as rawcvr, substring(a.adclass,1,6) as adclass,
+                      |b.cvr_model_name as model, b.adslot_id as adslotid, a.ideaid,user_show_ad_num, exp_cvr,
+                      |if(c.iscvr is not null,1,0) iscvr
+                      |from
+                      |(select searchid,ideaid,unitid,userid,adclass,hour
+                      |  from dl_cpc.cpc_basedata_click_event
+                      |  where $selectCondition2
+                      |  and $mediaSelection and isclick = 1
+                      |  and adsrc in (1,28)
+                      |  and antispam_score = 10000
+                      |  )a
+                      |  join
+                      |  (select searchid,ideaid,user_show_ad_num
+                      |  from
+                      |  dl_cpc.cpc_basedata_adx_event
+                      |  where  $selectCondition2
+                      |  and $mediaSelection
+                      |  and cvr_model_name in ('$calimodel','$model')
+                      |  AND bid_mode = 0
+                      |  and conversion_goal>0) b
+                      |    on a.searchid = b.searchid and a.ideaid = b.ideaid
+                      | left join
+                      | (select distinct searchid,conversion_goal,1 as iscvr
+                      |  from dl_cpc.ocpc_quick_cv_log
+                      |  where  $selectCondition1) c
+                      |  on a.searchid = c.searchid and b.conversion_goal=c.conversion_goal
+       """.stripMargin
+
+    println(s"sql:\n$sql")
+    val data= spark.sql(sql)
+
+    val dataDF = data.groupBy("ideaid").count()
+      .withColumn("tag",when(col("count")>60,1).otherwise(0))
+      .withColumn("label",col("iscvr"))
+      .join(data,Seq("ideaid"),"left")
+      .withColumn("ideaid",when(col("label")===1,col("ideaid")).otherwise(9999999))
+      .select("searchid","ideaid","user_show_ad_num","adclass","adslotid")
+
+//    val f1 = new StringIndexer().setInputCol("ideaid").setOutputCol("ideaidIndex").fit(log).transform(log)
+//    val f2 = new StringIndexer().setInputCol("adclass").setOutputCol("adclassIndex").fit(f1).transform(f1)
+//    val f3 = new StringIndexer().setInputCol("adslotid").setOutputCol("adslotidIndex").fit(f2).transform(f2)
+//
+//    val one1 = new OneHotEncoder().setInputCol("ideaidIndex").setOutputCol("ideaidVec").setDropLast(false).transform(f3)
+//    val one2 = new OneHotEncoder().setInputCol("adclassIndex").setOutputCol("adclassVec").setDropLast(false).transform(one1)
+//    val one3 = new OneHotEncoder().setInputCol("adslotidIndex").setOutputCol("adslotidVec").setDropLast(false).transform(one2)
+
+
+    val categoricalColumns = Array("ideaid","adclass","adslotid")
+
+    val stagesArray = new ListBuffer[PipelineStage]()
+    for (cate <- categoricalColumns) {
+      val indexer = new StringIndexer().setInputCol(cate).setOutputCol(s"${cate}Index")
+      val encoder = new OneHotEncoder().setInputCol(indexer.getOutputCol).setOutputCol(s"${cate}classVec")
+      stagesArray.append(indexer,encoder)
+    }
+
+    val numericCols = Array("user_show_ad_num")
+    val assemblerInputs = categoricalColumns.map(_ + "classVec") ++ numericCols
+    /**使用VectorAssembler将所有特征转换为一个向量*/
+    val assembler = new VectorAssembler().setInputCols(assemblerInputs).setOutputCol("features")
+    stagesArray.append(assembler)
+
+    val pipeline = new Pipeline()
+    pipeline.setStages(stagesArray.toArray)
+    /**fit() 根据需要计算特征统计信息*/
+    val pipelineModel = pipeline.fit(dataDF)
+    /**transform() 真实转换特征*/
+    val dataset = pipelineModel.transform(dataDF)
+    dataset.show(false)
+
+    //test
+
+    val Array(trainingDF, testDF) = dataset.randomSplit(Array(0.6, 0.4), seed = 12345)
+    println(s"trainingDF size=${trainingDF.count()},testDF size=${testDF.count()}")
+    val lrModel = new LinearRegression().setFeaturesCol("feature")
+        .setLabelCol("label").setRegParam(1e-7).setElasticNetParam(0.1).fit(trainingDF)
+    val predictions = lrModel.transform(testDF).select("label", "features","rawPrediction", "probability", "prediction","ideaid")
+      predictions.show(5)
+
+    println("coefficients:" +lrModel.coefficients)
+    println("intercept:" +lrModel.intercept)
+
+    // 输出逻辑回归的系数和截距
+    //    println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
+    //获取训练模型的相关信息
+    val trainingSummary = lrModel.summary
+    //模型残差
+    trainingSummary.residuals.show()
+    //模型均方差
+    println("mse:" + trainingSummary.meanSquaredError)
+    //模型均方根误差
+    println("r-squared:" + trainingSummary.rootMeanSquaredError)
+
+    lrModel.transform(testDF).rdd.map{
+      x =>
+        val exp_ctr = x.getAs[Double]("prediction")
+        val raw_ctr = x(2).toString.toDouble
+        val coin_origin = x(3).toString.toInt
+        val isclick = x(0).toString.toInt
+        (exp_ctr,isclick,raw_ctr,coin_origin)
+    }.toDF("exp_ctr","isclick","raw_ctr","coin_origin").createOrReplaceTempView("result")
+
+
+
+
+
+    //    //   lr calibration
+//    calculateAuc(result2,"lr",spark)
+//    //    raw data
+//    val modelData = testsample.selectExpr("cast(isclick as Int) label","cast(raw_ctr as Int) prediction","ideaid")
+//    calculateAuc(modelData,"original",spark)
+//
+////    online calibration
+//    val calibData = testsample.selectExpr("cast(isclick as Int) label","cast(exp_ctr as Int) prediction","ideaid")
+//    calculateAuc(calibData,"online",spark)
+
+  }
+
+ def SparseFeature(profile_num: Int)
+  = udf {
+    els: Seq[Row] =>
+      val new_els: Seq[(Int, Double)] = els.map(x => {
+        (x.getInt(0), x.getDouble(1))
+      })
+      Vectors.sparse(profile_num, new_els)
+  }
+}
