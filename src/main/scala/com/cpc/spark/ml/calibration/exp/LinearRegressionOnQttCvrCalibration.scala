07413674b85bf10e0af32af08c2cba4132e69125
wangyao <wangyao@qutoutiao.net>
2019-10-14 14:09:50 +0800

    -

--- src/main/scala/com/cpc/spark/ml/calibration/exp/LinearRegressionOnQttCvrCalibration.scala
+++ src/main/scala/com/cpc/spark/ml/calibration/exp/LinearRegressionOnQttCvrCalibration.scala
@@ -295,6 +295,6 @@ object LinearRegressionOnQttCvrCalibration {
     val allnum = p3.count().toDouble
     val rightnum = p3.filter("pcoc<1.1 and pcoc>0.9").count()
     val greaternum = p3.filter("pcoc>1.1").count()
-    println("%s calibration by unitid: avgcvr:%f,avgecvr:%f,avgpcoc:%f,all:%f,right:%d,exceed 1.1:%d,ratio of pcoc in (0.9,1,1):%f,ratio of pcoc>1.1:%f".format(cate, cvr2, ecvr2, pcoc,allnum,rightnum,greaternum,rightnum/allnum,greaternum/allnum))
+    println("%s by unitid: avgcvr:%f,avgecvr:%f,avgpcoc:%f,all:%f,right:%d,exceed 1.1:%d,ratio of pcoc in (0.9,1,1):%f,ratio of pcoc>1.1:%f".format(cate, cvr2, ecvr2, pcoc,allnum,rightnum,greaternum,rightnum/allnum,greaternum/allnum))
   }
 }
