6fa7f534824da2ababb7bae4c969d34d459c467b
wangjun <wangjun02@qutoutiao.net>
2019-10-14 17:17:34 +0800

    测试代码

--- src/main/scala/com/cpc/spark/oCPX/deepOcpc/calibration/OcpcGetPb_baseline.scala
+++ src/main/scala/com/cpc/spark/oCPX/deepOcpc/calibration/OcpcGetPb_baseline.scala
@@ -75,8 +75,8 @@ object OcpcGetPb_baseline {
 
     resultDF
       .repartition(1)
-//      .write.mode("overwrite").insertInto("test.ocpc_deep_pb_data_hourly")
-      .write.mode("overwrite").insertInto("dl_cpc.ocpc_deep_pb_data_hourly")
+      .write.mode("overwrite").insertInto("test.ocpc_deep_pb_data_hourly")
+//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_deep_pb_data_hourly")
 
 
   }
