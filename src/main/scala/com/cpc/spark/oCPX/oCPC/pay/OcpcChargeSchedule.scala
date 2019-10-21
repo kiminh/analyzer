65ea3db2576f9f87c20cd63a5c47d67cbe7d7d40
wangjun <wangjun02@qutoutiao.net>
2019-10-21 10:58:46 +0800

    测试增加新的赔付行业

--- src/main/scala/com/cpc/spark/oCPX/oCPC/pay/OcpcChargeSchedule.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/pay/OcpcChargeSchedule.scala
@@ -51,8 +51,8 @@ object OcpcChargeSchedule {
       .withColumn("date", lit(date))
       .withColumn("version", lit(version))
       .repartition(1)
-//      .write.mode("overwrite").insertInto("test.ocpc_pay_cnt_daily_v2")
-      .write.mode("overwrite").insertInto("dl_cpc.ocpc_pay_cnt_daily_v2")
+      .write.mode("overwrite").insertInto("test.ocpc_pay_cnt_daily_v2")
+//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_pay_cnt_daily_v2")
 
 
 
@@ -162,10 +162,17 @@ object OcpcChargeSchedule {
       .filter(s"media in ('qtt', 'hottopic', 'novel')")
       .withColumn("industry", udfDeterminePayIndustry()(col("adslot_type"), col("adclass")))
 
+    newDataRaw
+      .write.mode("overwrite").saveAsTable("test.check_ocpc_pay_data20191021a")
+
     val newData = newDataRaw
       .filter(s"industry in ('feedapp', 'elds', 'pay_industry')")
       .select("unitid")
       .distinct()
+
+    newData
+      .write.mode("overwrite").saveAsTable("test.check_ocpc_pay_data20191021b")
+
 
     val data = prevData
       .join(newData, Seq("unitid"), "outer")
