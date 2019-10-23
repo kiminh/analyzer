0997c66ee4ae4e56818611817010951a7537cc59
wangjun <wangjun02@qutoutiao.net>
2019-10-23 22:47:30 +0800

    update

--- src/main/scala/com/cpc/spark/OcpcProtoType/suggest_cpa_v3/OcpcSuggestCpaRecordV2.scala
+++ src/main/scala/com/cpc/spark/OcpcProtoType/suggest_cpa_v3/OcpcSuggestCpaRecordV2.scala
@@ -50,20 +50,20 @@ object OcpcSuggestCpaRecordV2 {
       .withColumn("date", lit(date))
       .withColumn("hour", lit(hour))
       .withColumn("version", lit(version))
-//      .repartition(50)
-//      .cache()
-//
-//    resultDF.show(10)
+      .repartition(50)
+      .cache()
+
+    resultDF.show(10)
+
+//    resultDF
+//      .write.mode("overwrite").saveAsTable("test.ocpc_suggest_cpa20191023c")
 
     resultDF
-      .write.mode("overwrite").saveAsTable("test.ocpc_suggest_cpa20191023c")
-
-//    resultDF
-//      .write.mode("overwrite").insertInto("test.ocpc_history_suggest_cpa_hourly")
-////      .write.mode("overwrite").insertInto("dl_cpc.ocpc_history_suggest_cpa_hourly")
-//    resultDF
-//      .write.mode("overwrite").insertInto("test.ocpc_history_suggest_cpa_version")
-////      .write.mode("overwrite").insertInto("dl_cpc.ocpc_history_suggest_cpa_version")
+      .write.mode("overwrite").insertInto("test.ocpc_history_suggest_cpa_hourly")
+//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_history_suggest_cpa_hourly")
+    resultDF
+      .write.mode("overwrite").insertInto("test.ocpc_history_suggest_cpa_version")
+//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_history_suggest_cpa_version")
 
 
   }
@@ -86,6 +86,10 @@ object OcpcSuggestCpaRecordV2 {
     val data = spark
         .sql(sqlRequest)
         .filter(s"cpa_suggest is not null")
+        .groupBy("unitid", "media", "conversion_goal")
+        .agg(
+          avg(col("suggest_cpa")).alias("suggest_cpa")
+        )
         .cache()
 
     data.show(10)
