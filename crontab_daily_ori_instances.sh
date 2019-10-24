9a2c43ea626ff78814ce9dd39c46771e9339d2c2
冯华宾 <fenghuabin@qutoutiao.net>
2019-10-24 20:17:41 +0800

    fhb_start

---
+++ crontab_daily_ori_instances.sh
@@ -0,0 +1,186 @@
+#!/usr/bin/env bash
+#fenghuabin@qutoutiao.net
+
+source /etc/profile
+
+date_full=`date`
+printf "*****************************${date_full}********************************\n"
+
+dir=daily_ori_instances
+if [[ ! -d "${dir}" ]]; then
+    mkdir ${dir}
+fi
+
+shell_in_run=${dir}/shell_in_busy
+if [[ -f "$shell_in_run" ]]; then
+    printf "shell are busy now, existing\n"
+    exit 0
+fi
+touch ${shell_in_run}
+
+
+
+last_date=`date --date='1 days ago' +%Y-%m-%d`
+aggr_path="hdfs://emr-cluster/user/cpc/aiclk_dataflow/daily/adlist-v4/${last_date}"
+file_success=${dir}/${last_date}_aggr_success
+file_count=${dir}/${last_date}_aggr_count
+file_part=${dir}/${last_date}_aggr_part_r_00999
+
+if [[ ! -f ${file_success} ]]; then
+    hadoop fs -get ${aggr_path}/_SUCCESS ${file_success} &
+fi
+if [[ ! -f ${file_count} ]]; then
+    hadoop fs -get ${aggr_path}/count ${file_count} &
+fi
+if [[ ! -f ${file_part} ]]; then
+    hadoop fs -get ${aggr_path}/part-r-00999 ${file_part} &
+fi
+wait
+if [[ ! -f ${file_success} ]]; then
+    printf "no ${file_success}, exiting...\n"
+    rm ${shell_in_run}
+    exit 0
+fi
+if [[ ! -f ${file_count} ]]; then
+    printf "no ${file_count}, exiting...\n"
+    rm ${shell_in_run}
+    exit 0
+fi
+if [[ ! -f ${file_part} ]]; then
+    printf "no ${file_part}, exiting...\n"
+    rm ${shell_in_run}
+    exit 0
+fi
+
+file_size=`ls -l ${file_part} | awk '{ print $5 }'`
+if [ ${file_size} -lt 10000000 ]
+then
+    printf "invalid ${file_part} file size:${file_size}, exit...\n"
+    rm ${shell_in_run}
+    exit 0
+fi
+
+
+valid_data=()
+valid_date=()
+sample_list=(
+    `date --date='1 days ago' +%Y-%m-%d`
+    `date --date='2 days ago' +%Y-%m-%d`
+    `date --date='3 days ago' +%Y-%m-%d`
+    `date --date='4 days ago' +%Y-%m-%d`
+    `date --date='5 days ago' +%Y-%m-%d`
+    `date --date='6 days ago' +%Y-%m-%d`
+    `date --date='7 days ago' +%Y-%m-%d`
+    `date --date='8 days ago' +%Y-%m-%d`
+    `date --date='9 days ago' +%Y-%m-%d`
+    `date --date='10 days ago' +%Y-%m-%d`
+    `date --date='11 days ago' +%Y-%m-%d`
+    `date --date='12 days ago' +%Y-%m-%d`
+    `date --date='13 days ago' +%Y-%m-%d`
+    `date --date='14 days ago' +%Y-%m-%d`
+)
+
+for idx in "${!sample_list[@]}";
+do
+    curr_date="${sample_list[$idx]}"
+    echo "curr_date:${curr_date}"
+
+    prefix="hdfs://emr-cluster/user/cpc/aiclk_dataflow/daily/adlist-v4/${curr_date}"
+    end=part-*
+
+    file_count=${dir}/${curr_date}_${curr_date}_count
+    file_success=${dir}/${curr_date}_${curr_date}_success
+    file_part=${dir}/${curr_date}_${curr_date}_part-r-00099
+
+    if [[ ! -f ${file_count} ]]; then
+        hadoop fs -get ${prefix}/count ${file_count} &
+    fi
+    if [[ ! -f ${file_success} ]]; then
+        hadoop fs -get ${prefix}/_SUCCESS ${file_success} &
+    fi
+
+    printf "waiting for downloading real-time data in parallel...\n"
+    wait
+    printf "downloaded real-time data file in parallel...\n"
+
+
+    if [[ ! -f ${file_count} ]]; then
+        printf "no ${file_count} file, continue...\n"
+        continue
+    fi
+    if [[ ! -f ${file_success} ]]; then
+        printf "no ${file_success} file, continue...\n"
+        continue
+    fi
+
+    #file_size=`ls -l ${file_part} | awk '{ print $5 }'`
+    #if [ ${file_size} -lt 1000 ]
+    #then
+    #    printf "invalid ${file_part} file size:${file_size}, continue...\n"
+    #    continue
+    #fi
+
+    valid_data+=("${prefix}/${end}")
+    valid_date+=("${curr_date}")
+    rm ${file_count}
+    rm ${file_part}
+    if [[ ${#valid_data[@]} -eq 14 ]] ; then
+        break
+    fi
+done
+
+if [[ ${#valid_data[@]} -le 0 ]] ; then
+    printf "no daily training data file found, existing...\n"
+    rm ${shell_in_run}
+    exit 0
+fi
+
+date_list="$( IFS=$';'; echo "${valid_date[*]}" )"
+file_list="$( IFS=$';'; echo "${valid_data[*]}" )"
+
+echo "${file_list}"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "${date_list}"
+
+date_last=`date --date='1 days ago' +%Y-%m-%d`
+instances_all_list=(
+    `date --date='1 days ago' +%Y-%m-%d`
+    `date --date='3 days ago' +%Y-%m-%d`
+    `date --date='7 days ago' +%Y-%m-%d`
+    `date --date='14 days ago' +%Y-%m-%d`
+)
+instances_begin_date_list="$( IFS=$';'; echo "${instances_all_list[*]}" )"
+
+jarLib=hdfs://emr-cluster/warehouse/azkaban/lib/fhb_start_v1.jar
+queue=root.cpc.bigdata
+queue=root.cpc.develop
+jars=("/home/cpc/anal/lib/spark-tensorflow-connector_2.11-1.10.0.jar" )
+
+randjar="fhb_start"`date +%s%N`".jar"
+hadoop fs -get ${jarLib} ${randjar}
+
+des_dir="hdfs://emr-cluster/user/cpc/fenghuabin/adlist-v4-daily-instances"
+
+spark-submit --master yarn --queue ${queue} \
+    --name "adlist-v4-aggr-samples" \
+    --driver-memory 4g --executor-memory 4g \
+    --num-executors 1000 --executor-cores 4 \
+    --conf spark.hadoop.fs.defaultFS=hdfs://emr-cluster2 \
+    --conf "spark.yarn.executor.memoryOverhead=4g" \
+    --conf "spark.sql.shuffle.partitions=500" \
+    --jars $( IFS=$','; echo "${jars[*]}" ) \
+    --class com.cpc.spark.ml.dnn.baseData.MakeDailyInstances\
+    ${randjar} ${des_dir} ${date_list} ${file_list} ${date_last} ${instances_begin_date_list}
+
+
+rm ${shell_in_run}
+
+rm ${shell_in_run}
+
+
+#-p GIT_NAME="git@git.qutoutiao.net:fenghuabin/dl-demo-refactor.git" \
+#-p SCM_REVISION="master" \
+
+#-p GIT_NAME="git@git.qutoutiao.net:songchengru/dl-demo.git" \
+#-p SCM_REVISION="fhb_start" \
+
---
+++ src/main/scala/com/cpc/spark/ml/dnn/baseData/MakeDailyInstances.scala
@@ -0,0 +1,202 @@
+package com.cpc.spark.ml.dnn.baseData
+
+import java.io.{File, PrintWriter}
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import org.apache.commons.lang3.time.{DateFormatUtils, DateUtils}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.types.{StructField, _}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark.util.LongAccumulator
+
+import scala.collection.mutable.ArrayBuffer
+import scala.sys.process._
+import scala.util.Random
+
+/**
+  * 聚合adlistv4tfrecord特征
+  * created time : 2019/09/22 11:31
+  * @author fenghuabin
+  * @version 1.0
+  *
+  */
+
+object MakeDailyInstances {
+
+
+  def delete_hdfs_path(path: String): Unit = {
+
+    val conf = new org.apache.hadoop.conf.Configuration()
+    val p = new org.apache.hadoop.fs.Path(path)
+    val hdfs = p.getFileSystem(conf)
+    val hdfs_path = new org.apache.hadoop.fs.Path(path.toString)
+
+    //val hdfs_path = new org.apache.hadoop.fs.Path(path.toString)
+    //val hdfs = org.apache.hadoop.fs.FileSystem.get(new org.apache.hadoop.conf.Configuration())
+    if (hdfs.exists(hdfs_path)) {
+      hdfs.delete(hdfs_path, true)
+    }
+  }
+
+  def exists_hdfs_path(path: String): Boolean = {
+
+    val conf = new org.apache.hadoop.conf.Configuration()
+    val p = new org.apache.hadoop.fs.Path(path)
+    val hdfs = p.getFileSystem(conf)
+    val hdfs_path = new org.apache.hadoop.fs.Path(path.toString)
+    //val hdfs = org.apache.hadoop.fs.FileSystem.get(new org.apache.hadoop.conf.Configuration())
+
+    if (hdfs.exists(hdfs_path)) {
+      true
+    } else {
+      false
+    }
+  }
+
+  def writeNum2File(file: String, num: Long): Unit = {
+    val writer = new PrintWriter(new File(file))
+    writer.write(num.toString)
+    writer.close()
+  }
+
+  def formatDate(date: Date, pattern: String = "yyyy-MM-dd"): String = {
+    val formatDate = DateFormatUtils.format(date, pattern)
+    formatDate
+  }
+
+  def GetDateRange(beginStr: String, endStr: String, format : String = "yyyy-MM-dd"): ArrayBuffer[String] = {
+    val ranges = ArrayBuffer[String]()
+    val sdf = new SimpleDateFormat(format)
+    var dateBegin = sdf.parse(beginStr)
+    val dateEnd = sdf.parse(endStr)
+    while (dateBegin.compareTo(dateEnd) <= 0) {
+      ranges += sdf.format(dateBegin)
+      dateBegin = DateUtils.addDays(dateBegin, 1)
+    }
+    ranges
+  }
+
+  def main(args: Array[String]): Unit = {
+    if (args.length != 5) {
+      System.err.println(
+        """
+          |you have to input 5 parameters !!!
+        """.stripMargin)
+      System.exit(1)
+    }
+    //val Array(src, des_dir, des_date, des_map_prefix, numPartitions) = args
+    val Array(des_dir, src_date_list, src_file_list, date_last, date_begin_str) = args
+
+    Logger.getRootLogger.setLevel(Level.WARN)
+
+    val sparkConf = new SparkConf()
+    sparkConf.set("spark.driver.maxResultSize", "5g")
+    val spark = SparkSession.builder().config(sparkConf).enableHiveSupport().getOrCreate()
+    val sc = spark.sparkContext
+
+    val schema_new = StructType(List(
+      StructField("sample_idx", LongType, nullable = true),
+      StructField("label", ArrayType(LongType, containsNull = true)),
+      StructField("dense", ArrayType(LongType, containsNull = true)),
+      StructField("idx0", ArrayType(LongType, containsNull = true)),
+      StructField("idx1", ArrayType(LongType, containsNull = true)),
+      StructField("idx2", ArrayType(LongType, containsNull = true)),
+      StructField("id_arr", ArrayType(LongType, containsNull = true))
+    ))
+
+    println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    println("Aggr Examples")
+
+    val date_list = src_date_list.split(";")
+    val file_list = src_file_list.split(";")
+
+    for (list_idx <- date_list.indices) {
+
+      val curr_date = date_list(list_idx)
+      val curr_file = file_list(list_idx)
+      println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+      println(s"curr_date : $curr_date")
+      println(s"curr_file : $curr_file")
+
+      val importedDf: DataFrame = spark.read.format("tfrecords").option("recordType", "Example").load(curr_file)
+
+      val instances_file = des_dir + "/" + curr_date + "-instances"
+      if (!exists_hdfs_path(instances_file + "/_SUCCESS")) {
+        delete_hdfs_path(instances_file)
+        importedDf.rdd.map(
+          rs => {
+            val idx2 = rs.getSeq[Long](0)
+            val idx1 = rs.getSeq[Long](1)
+            val idx_arr = rs.getSeq[Long](2)
+            val idx0 = rs.getSeq[Long](3)
+            val sample_idx = rs.getLong(4)
+            val label_arr = rs.getSeq[Long](5)
+            val dense = rs.getSeq[Long](6)
+
+            val output: Array[String] = new Array[String](dense.length + idx_arr.length)
+            for (idx <- dense.indices) {
+              output(idx) = dense(idx).toString
+            }
+            for (idx <- idx_arr.indices) {
+              output(idx + dense.length) = idx_arr(idx).toString
+            }
+            output.mkString("\t")
+          }
+        ).flatMap(
+          rs => {
+            val line = rs.split("\t")
+            for (elem <- line)
+              yield (elem, 1L)
+          }
+        ).reduceByKey(_ + _).sortBy(_._2 * -1).map {
+          case (key, value) =>
+            key + "\t" + value.toString
+        }.repartition(1).saveAsTextFile(instances_file)
+      }
+    }
+
+    val date_begin_list = date_begin_str.split(";")
+    for (curr_begin_date <- date_begin_list) {
+      val instances_all = des_dir + "/" + date_last + "_" + curr_begin_date + "-instances-all"
+      val instances_map = des_dir + "/" + date_last + "_" + curr_begin_date + "-instances-map"
+      if (!exists_hdfs_path(instances_all + "/_SUCCESS") || !exists_hdfs_path(instances_map + "/_SUCCESS")) {
+        delete_hdfs_path(instances_all)
+        delete_hdfs_path(instances_map)
+        val instances_date_collect = GetDateRange(curr_begin_date, date_last)
+        val output = ArrayBuffer[String]()
+        for (curr_date <- instances_date_collect) {
+          val instances_file = des_dir + "/" + curr_date + "-instances"
+          if (exists_hdfs_path(instances_file + "/_SUCCESS")) {
+            output += instances_file + "/part-*"
+          }
+        }
+
+        sc.textFile(output.mkString(",")).map({
+          rs =>
+            val line_list = rs.split("\t")
+            (line_list(0), line_list(1).toLong)
+        }).reduceByKey(_ + _).repartition(1).sortBy(_._2 * -1).map({
+          case (key, value) =>
+            key + "\t" + value.toString
+        }).saveAsTextFile(instances_all)
+
+        val acc = new LongAccumulator
+        spark.sparkContext.register(acc)
+        sc.textFile(instances_all).coalesce(1, false).map{
+          rs => {
+            acc.add(1L)
+            val line = rs.split("\t")
+            val key = line(0)
+            (key, acc.count)
+          }
+        }.repartition(1).sortBy(_._2).map{
+          case (key, value) => key + "\t" + value.toString
+        }.saveAsTextFile(instances_map)
+      }
+    }
+
+  }
+}
+
