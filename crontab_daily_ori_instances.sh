d684aaaf5fc8bb8f5da5bcbf4d2201df7625b16a
冯华宾 <fenghuabin@qutoutiao.net>
2019-10-24 20:28:13 +0800

    fhb_start

--- crontab_daily_ori_instances.sh
+++ crontab_daily_ori_instances.sh
@@ -162,7 +162,7 @@ hadoop fs -get ${jarLib} ${randjar}
 des_dir="hdfs://emr-cluster/user/cpc/fenghuabin/adlist-v4-daily-instances"
 
 spark-submit --master yarn --queue ${queue} \
-    --name "adlist-v4-aggr-samples" \
+    --name "adlist-v4-daily-ori-instances" \
     --driver-memory 4g --executor-memory 4g \
     --num-executors 1000 --executor-cores 4 \
     --conf spark.hadoop.fs.defaultFS=hdfs://emr-cluster2 \
