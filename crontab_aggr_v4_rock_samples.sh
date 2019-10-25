d008235cceabec96d8f65b72b034ed75f1994f89
冯华宾 <fenghuabin@qutoutiao.net>
2019-10-26 00:05:30 +0800

    fhb_start

--- crontab_aggr_v4_rock_samples.sh
+++ crontab_aggr_v4_rock_samples.sh
@@ -295,6 +295,7 @@ echo "${date_list}"
 date_last=`date --date='1 days ago' +%Y-%m-%d`
 instances_all_list=(
     `date --date='1 days ago' +%Y-%m-%d`
+    `date --date='2 days ago' +%Y-%m-%d`
     `date --date='3 days ago' +%Y-%m-%d`
     `date --date='7 days ago' +%Y-%m-%d`
     `date --date='14 days ago' +%Y-%m-%d`
