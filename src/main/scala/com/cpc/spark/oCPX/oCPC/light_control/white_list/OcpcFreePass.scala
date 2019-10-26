288d66f20bd329a1955c71c56f423221f6e96919
wangjun <wangjun02@qutoutiao.net>
2019-10-26 13:37:58 +0800

    update

--- src/main/scala/com/cpc/spark/oCPX/oCPC/light_control/white_list/OcpcFreePass.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/light_control/white_list/OcpcFreePass.scala
@@ -72,8 +72,8 @@ object OcpcFreePass {
       .withColumn("hour", lit(hour))
       .withColumn("version", lit(version))
       .repartition(1)
-//      .write.mode("overwrite").insertInto("test.ocpc_auto_second_stage_light")
-      .write.mode("overwrite").insertInto("dl_cpc.ocpc_auto_second_stage_light")
+      .write.mode("overwrite").insertInto("test.ocpc_auto_second_stage_light")
+//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_auto_second_stage_light")
 
 
   }
