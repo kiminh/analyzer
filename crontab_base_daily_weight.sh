59dc85f16c4702d391ea80b58e46746bb764abd6
冯华宾 <fenghuabin@qutoutiao.net>
2019-10-28 18:22:58 +0800

    fhb_start

--- crontab_base_daily_weight.sh
+++ crontab_base_daily_weight.sh
@@ -29,7 +29,7 @@ touch ${shell_in_run}
 
 des_dir="hdfs://emr-cluster/user/cpc/fenghuabin/adlist-v4-transformer"
 last_date=`date --date='1 days ago' +%Y-%m-%d`
-des_file=${des_dir}/${last_date}-aggr-weight
+des_file=${des_dir}/${last_date}-weight-aggr
 #ctr_file=${des_dir}/${last_date}-23-ctr
 des_file_success=${dir}/${last_date}_weight_success
 des_file_count=${dir}/${last_date}_weight_count
