72b775f714dde31d9cfe7ab95f9b698fb2936b02
冯华宾 <fenghuabin@qutoutiao.net>
2019-10-29 20:25:39 +0800

    fhb_start

--- crontab_base_daily_weight.sh
+++ crontab_base_daily_weight.sh
@@ -112,6 +112,9 @@ fi
 
 sample_list=(
     `date --date='1 days ago' +%Y-%m-%d`
+    `date --date='2 days ago' +%Y-%m-%d`
+    `date --date='3 days ago' +%Y-%m-%d`
+    `date --date='4 days ago' +%Y-%m-%d`
 )
 
 
