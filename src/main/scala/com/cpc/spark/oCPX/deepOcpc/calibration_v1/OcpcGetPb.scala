9ca97ff7e3bdfb6ee7bcd49e55def05cf8fd8e02
wangjun <wangjun02@qutoutiao.net>
2019-10-29 22:43:37 +0800

    测试数据

--- src/main/scala/com/cpc/spark/oCPX/deepOcpc/calibration_v1/OcpcGetPb.scala
+++ src/main/scala/com/cpc/spark/oCPX/deepOcpc/calibration_v1/OcpcGetPb.scala
@@ -7,6 +7,7 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import com.cpc.spark.oCPX.OcpcTools._
+import com.cpc.spark.oCPX.deepOcpc.calibration.OcpcGetPb_daily.udfSetSmoothFactor
 import com.cpc.spark.oCPX.deepOcpc.calibration_v1.OcpcShallowFactor._
 import com.cpc.spark.oCPX.deepOcpc.calibration_v1.OcpcRetentionFactor._
 
@@ -43,16 +44,71 @@ object OcpcGetPb {
     val data = calculateCalibrationValue(data1, data2, spark)
 
     // 数据组装
+    val resultData = assemblyData(data, spark)
 
     // 输出到结果表 dl_cpc.ocpc_deep_pb_data_hourly
+    // 明投单元
+    val result = resultData
+      .withColumn("cpagiven", lit(1.0))
+      .withColumn("is_hidden", lit(0))
+      .withColumn("date", lit(date))
+      .withColumn("hour", lit(hour))
+      .withColumn("version", lit(version))
+      .select("identifier", "conversion_goal", "jfb_factor", "post_cvr", "smooth_factor", "cvr_factor", "high_bid_factor", "low_bid_factor", "cpagiven", "date", "hour", "exp_tag", "is_hidden", "version")
 
+    val resultDF = result
+      .select("identifier", "conversion_goal", "jfb_factor", "post_cvr", "smooth_factor", "cvr_factor", "high_bid_factor", "low_bid_factor", "cpagiven", "date", "hour", "exp_tag", "is_hidden", "version")
+
+
+    resultDF
+      .repartition(1)
+      .write.mode("overwrite").insertInto("test.ocpc_deep_pb_data_hourly")
+//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_deep_pb_data_hourly")
   }
+
+  def assemblyData(rawData: DataFrame, spark: SparkSession) = {
+    // 组装数据
+    // set some default value
+    // post_cvr: 0.0
+    // smooth_factor: 0.3
+    // high_bid_factor: 1.0
+    // low_bid_factor: 1.0
+    val data = rawData
+      .selectExpr("cast(unitid as string) identifier", "conversion_goal", "exp_tag", "jfb_factor", "cvr_factor")
+      .withColumn("post_cvr", lit(0.0))
+      .withColumn("smooth_factor", lit(0.3))
+      .withColumn("smooth_factor", udfSetSmoothFactor()(col("identifier"), col("smooth_factor")))
+      .withColumn("high_bid_factor", lit(1.0))
+      .withColumn("low_bid_factor", lit(1.0))
+      .select("identifier", "conversion_goal", "exp_tag", "jfb_factor", "post_cvr", "smooth_factor", "cvr_factor", "high_bid_factor", "low_bid_factor")
+      .na.fill(1.0, Seq("jfb_factor", "cvr_factor", "high_bid_factor", "low_bid_factor"))
+      .na.fill(0.0, Seq("post_cvr", "smooth_factor"))
+
+    data
+  }
+
 
   def calculateCalibrationValue(dataRaw1: DataFrame, dataRaw2: DataFrame, spark: SparkSession) = {
     val data1 = dataRaw1.filter(s"deep_conversion_goal = 3")
     val data = data1
       .join(dataRaw2, Seq("unitid", "media"), "inner")
+      .select("unitid", "deep_conversion_goal", "media", "exp_tag", "jfb", "pre_cvr", "post_cvr", "deep_cvr")
+      .withColumn("jfb_factor", lit(1.0) / col("jfb"))
+      .withColumn("cvr", col("deep_cvr") * col("post_cvr"))
+      .withColumn("cvr_factor", col("cvr") * 1.0 / col("pre_cvr"))
+      .withColumn("conversion_goal", col("deep_conversion_goal"))
+      .cache()
 
+    data
   }
+
+  def udfSetSmoothFactor() = udf((identifier: String, smoothFactor: Double) => {
+    val result = (identifier, smoothFactor) match {
+      case ("2399667", _) => 0.7
+      case (_, v) => v
+    }
+    result
+  })
+
 
 }
