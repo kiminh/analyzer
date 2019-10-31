c6a09d06aee24a9e1a8ad56000a8ba540c89ec4f
冯华宾 <fenghuabin@qutoutiao.net>
2019-11-01 00:22:14 +0800

    update

--- crontab_aggr_v4_rock_samples.sh
+++ crontab_aggr_v4_rock_samples.sh
@@ -294,6 +294,8 @@ echo "${date_list}"
 
 date_last=`date --date='1 days ago' +%Y-%m-%d`
 instances_all_list=(
+    `date --date='8 days ago' +%Y-%m-%d`
+    `date --date='7 days ago' +%Y-%m-%d`
     `date --date='4 days ago' +%Y-%m-%d`
     `date --date='1 days ago' +%Y-%m-%d`
     `date --date='3 days ago' +%Y-%m-%d`
