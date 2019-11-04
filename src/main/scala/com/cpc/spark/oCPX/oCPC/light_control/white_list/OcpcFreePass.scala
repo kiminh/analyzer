a1dc013d06668630ffec66f38a61473e4f0d6fa8
wangjun <wangjun02@qutoutiao.net>
2019-11-04 18:13:05 +0800

    准备测试

--- src/main/scala/com/cpc/spark/oCPX/oCPC/light_control/white_list/OcpcFreePass.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/light_control/white_list/OcpcFreePass.scala
@@ -63,21 +63,21 @@ object OcpcFreePass {
       .withColumn("hour", lit(hour))
       .withColumn("version", lit(version))
       .repartition(1)
-//      .write.mode("overwrite").insertInto("test.ocpc_auto_second_stage_light")
-      .write.mode("overwrite").insertInto("dl_cpc.ocpc_auto_second_stage_light")
+      .write.mode("overwrite").insertInto("test.ocpc_auto_second_stage_light")
+//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_auto_second_stage_light")
 
 
-    val resultDF = spark
-      .table("dl_cpc.ocpc_auto_second_stage_light")
-      .where(s"`date` = '$date' and `hour` = '$hour' and version = '$version' and flag = 1")
-
-    resultDF
-      .select("unitid", "userid", "conversion_goal", "media")
-      .withColumn("date", lit(date))
-      .withColumn("hour", lit(hour))
-      .repartition(1)
-//      .write.mode("overwrite").insertInto("test.ocpc_auto_second_stage_hourly")
-      .write.mode("overwrite").insertInto("dl_cpc.ocpc_auto_second_stage_hourly")
+//    val resultDF = spark
+//      .table("dl_cpc.ocpc_auto_second_stage_light")
+//      .where(s"`date` = '$date' and `hour` = '$hour' and version = '$version' and flag = 1")
+//
+//    resultDF
+//      .select("unitid", "userid", "conversion_goal", "media")
+//      .withColumn("date", lit(date))
+//      .withColumn("hour", lit(hour))
+//      .repartition(1)
+////      .write.mode("overwrite").insertInto("test.ocpc_auto_second_stage_hourly")
+//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_auto_second_stage_hourly")
 
 
 
