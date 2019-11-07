a8bebb13adb31d8803938835678aa8c8993e6a6d
wangyao <wangyao@qutoutiao.net>
2019-11-07 11:36:58 +0800

    -

--- src/main/scala/com/cpc/spark/ml/calibration/LinearRegressionOnQttCvrCalibration.scala
+++ src/main/scala/com/cpc/spark/ml/calibration/LinearRegressionOnQttCvrCalibration.scala
@@ -96,6 +96,7 @@ object LinearRegressionOnQttCvrCalibration {
       .select("searchid","ideaid","adclass","adslot_id","label","unitid","raw_cvr",
         "exp_cvr","sample","hourweight","userid","conversion_from","click_unit_count","hour")
     dataDF.show(10)
+    dataDF.persist()
 
     val categoricalColumns = Array("ideaid","adclass","adslot_id","unitid","userid","click_unit_count")
     val sampleidx = Map("ideaid" -> 11,"adclass" -> 16,"adslot_id" -> 5,"unitid" -> 12 ,"userid" -> 14,"conversion_from" -> 73,
@@ -123,6 +124,7 @@ object LinearRegressionOnQttCvrCalibration {
     dataset.show(10)
     dataset.select("label","features").show(10)
 
+    dataDF.unpersist()
     val trainingDF= dataset
     println(s"trainingDF size=${trainingDF.count()}")
     val lrModel = new LinearRegression().setFeaturesCol("features")
