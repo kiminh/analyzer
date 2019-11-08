bd00337ef81b793d9fb8962d1e92a3a2f9f152f8
wangjun <wangjun02@qutoutiao.net>
2019-11-08 20:08:35 +0800

    fix bug

--- scripts/oCPX/deepOcpc/permission/testOcpcDeepCalculateAUC.sh
+++ scripts/oCPX/deepOcpc/permission/testOcpcDeepCalculateAUC.sh
@@ -18,7 +18,7 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
     --class com.cpc.spark.oCPX.deepOcpc.calibration_v2.OcpcGetPb \
-    /home/cpc/wangjun/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $1 $2 $3 $4 
+    /home/cpc/wangjun/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $1 $2 $3 $4
 
 
 #val date = args(0).toString
