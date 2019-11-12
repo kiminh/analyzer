e7cf457123d6f177a94fdac38481bb367cf64bd8
wangjun <wangjun02@qutoutiao.net>
2019-11-12 22:01:57 +0800

    fix bug

--- src/main/scala/com/cpc/spark/oCPX/OcpcTools.scala
+++ src/main/scala/com/cpc/spark/oCPX/OcpcTools.scala
@@ -184,7 +184,7 @@ object OcpcTools {
          |  expids,
          |  exptags,
          |  ocpc_expand,
-         |  (case when hidden_tax is null then 0 else hidden_tax end) as hidden_tax
+         |  (case when hidden_tax is null then 0 else hidden_tax end) as hidden_tax,
          |  date,
          |  hour
          |FROM
