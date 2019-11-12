b74f0711655973041d7b3cb902990e568de2006e
wangyao <wangyao@qutoutiao.net>
2019-11-12 22:37:09 +0800

    -

--- src/main/scala/com/cpc/spark/ml/calibration/exp/LinearRegressionOnQttCvrCalibrationRotate.scala
+++ src/main/scala/com/cpc/spark/ml/calibration/exp/LinearRegressionOnQttCvrCalibrationRotate.scala
@@ -162,7 +162,7 @@ object LinearRegressionOnQttCvrCalibrationRotate {
       println(s"trainingDF size=${trainingDF.count()},validationDF size=${validationDF.count()}")
       val lrModel = new LinearRegression().setFeaturesCol("features")
         .setWeightCol("hourweight")
-        .setLabelCol("label").setRegParam(0.01).setElasticNetParam(0.1).fit(trainingDF)
+        .setLabelCol("label").setRegParam(0.01).setElasticNetParam(0.0).fit(trainingDF)
       val predictions = lrModel.transform(trainingDF).select("label", "features", "prediction","unitid")
       predictions.show(5)
 
