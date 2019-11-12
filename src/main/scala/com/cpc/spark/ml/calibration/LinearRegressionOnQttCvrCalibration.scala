9bd68ceb858c51323585f66353de8b6937d8afb4
wangyao <wangyao@qutoutiao.net>
2019-11-12 10:43:47 +0800

    -

--- src/main/scala/com/cpc/spark/ml/calibration/LinearRegressionOnQttCvrCalibration.scala
+++ src/main/scala/com/cpc/spark/ml/calibration/LinearRegressionOnQttCvrCalibration.scala
@@ -124,7 +124,7 @@ object LinearRegressionOnQttCvrCalibration {
     println(s"trainingDF size=${trainingDF.count()}")
     val lrModel = new LinearRegression().setFeaturesCol("features")
         .setWeightCol("hourweight")
-        .setLabelCol("label").setRegParam(0.001).setElasticNetParam(0.1).fit(trainingDF)
+        .setLabelCol("label").setRegParam(0.018).setElasticNetParam(0.1).fit(trainingDF)
 //    val predictions = lrModel.transform(trainingDF).select("label", "features", "prediction","unitid")
 
     // 输出逻辑回归的系数和截距
