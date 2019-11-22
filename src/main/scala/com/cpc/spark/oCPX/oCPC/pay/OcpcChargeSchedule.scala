cc7326e5c0ac3a47421185fd6dcd0a6d550fd422
wangjun <wangjun02@qutoutiao.net>
2019-11-22 14:45:17 +0800

    update

--- src/main/scala/com/cpc/spark/oCPX/oCPC/pay/OcpcChargeSchedule.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/pay/OcpcChargeSchedule.scala
@@ -36,23 +36,17 @@ object OcpcChargeSchedule {
     // 兼容逻辑：兼容老版本的逻辑
     // 抽取基本数据
     val scheduleData = getPaySchedule(date, version, spark)
-//    scheduleData
-//      .repartition(10)
-//      .write.mode("overwrite").saveAsTable("test.ocpc_pay_data20191010a")
 
     // 更新pay_cnt，pay_date
     val updateScheduleData = updatePaySchedule(date, dayCnt, scheduleData, spark)
-//    updateScheduleData
-//      .repartition(10)
-//      .write.mode("overwrite").saveAsTable("test.ocpc_pay_data20191010b")
 
     updateScheduleData
       .select("unitid", "pay_cnt", "pay_date", "flag", "update_flag", "prev_pay_cnt", "prev_pay_date")
       .withColumn("date", lit(date))
       .withColumn("version", lit(version))
       .repartition(1)
-//      .write.mode("overwrite").insertInto("test.ocpc_pay_cnt_daily_v2")
-      .write.mode("overwrite").insertInto("dl_cpc.ocpc_pay_cnt_daily_v2")
+      .write.mode("overwrite").insertInto("test.ocpc_pay_cnt_daily_v2")
+//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_pay_cnt_daily_v2")
 
 
 
@@ -75,9 +69,6 @@ object OcpcChargeSchedule {
     println(sqlRequest1)
     val rawData = spark.sql(sqlRequest1)
     rawData.createOrReplaceTempView("raw_data")
-//    rawData
-//      .repartition(10)
-//      .write.mode("overwrite").saveAsTable("test.ocpc_pay_data20191010c")
 
 
     val sqlRequest2 =
@@ -96,8 +87,6 @@ object OcpcChargeSchedule {
 
     val data = spark
       .sql(sqlRequest2)
-//      .withColumn("flag", when(col("pay_cnt") < 4, 1).otherwise(0))
-//      .withColumn("flag", when(col("pay_date") > date, 0).otherwise(col("flag")))
       .withColumn("prev_pay_cnt", col("pay_cnt"))
       .withColumn("prev_pay_date", col("pay_date"))
       .withColumn("pay_cnt", when(col("update_flag") === 1, col("pay_cnt") + 1).otherwise(col("pay_cnt")))
@@ -123,10 +112,6 @@ object OcpcChargeSchedule {
       .where(s"`date` = '$date1' and version = '$version'")
       .select("unitid", "pay_cnt", "pay_date")
       .distinct()
-
-//    val prevData = spark
-//      .table("dl_cpc.ocpc_pay_cnt_daily")
-//      .where(s"`date` = '2019-10-14'")
 
     // 抽取媒体id，获取当天的数据
     val conf = ConfigFactory.load("ocpc")
@@ -162,16 +147,14 @@ object OcpcChargeSchedule {
       .withColumn("industry", udfDeterminePayIndustry()(col("adslot_type"), col("adclass"), col("conversion_goal")))
       .distinct()
 
-//    newDataRaw
-//      .write.mode("overwrite").saveAsTable("test.check_ocpc_pay_data20191021a")
+    // todo 检查数据
+    newDataRaw
+      .write.mode("overwrite").saveAsTable("test.check_ocpc_pay_data20191021a")
 
     val newData = newDataRaw
       .filter(s"industry in ('feedapp', 'elds', 'pay_industry', 'siteform_pay_industry')")
       .select("unitid")
       .distinct()
-
-//    newData
-//      .write.mode("overwrite").saveAsTable("test.check_ocpc_pay_data20191021b")
 
 
     val data = prevData
@@ -179,7 +162,6 @@ object OcpcChargeSchedule {
       .select("unitid", "pay_cnt", "pay_date")
       .na.fill(0, Seq("pay_cnt"))
       .na.fill(date, Seq("pay_date"))
-//      .filter(s"pay_date <= '$date'")
 
     data.printSchema()
     data
@@ -198,7 +180,7 @@ object OcpcChargeSchedule {
       result = "yysc"
     } else if (adclass == 110110100 || adclass == 125100100) {
       result = "wzcp"
-    } else if (adclass == 103100100 || adclass == 111100100 || adclass == 104100100) {
+    } else if (adclass3 == 103 || adclass3 == 111 || adclass3 == 104) {
       result = "pay_industry"
     } else if (siteformPayAdclass.contains(adclass) && conversionGoal == 3) { // 【ID1091867】新增行业赔付规则-医护&医美&招商加盟
       result = "siteform_pay_industry"
