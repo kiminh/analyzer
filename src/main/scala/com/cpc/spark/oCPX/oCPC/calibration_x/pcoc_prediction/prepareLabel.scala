77022ecce0e8cdba87189ae1695c57b2a7d51e2b
wangjun <wangjun02@qutoutiao.net>
2019-11-22 19:47:20 +0800

    update

---
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/calibration_x/pcoc_prediction/prepareLabel.scala
@@ -0,0 +1,133 @@
+package com.cpc.spark.oCPX.oCPC.calibration_x.pcoc_prediction
+
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+import com.cpc.spark.oCPX.OcpcTools.{getTimeRangeSqlDate, udfConcatStringInt, udfDetermineMedia}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object prepareLabel {
+  def main(args: Array[String]): Unit = {
+    /*
+    采用拟合模型进行pcoc的时序预估
+     */
+    val spark = SparkSession.builder().enableHiveSupport().getOrCreate()
+    Logger.getRootLogger.setLevel(Level.WARN)
+
+    // 计算日期周期
+    // bash: 2019-01-02 12 1 qtt_demo qtt
+    val date = args(0).toString
+    val hour = args(1).toString
+    val hourInt = args(2).toInt
+
+
+    println("parameters:")
+    println(s"date=$date, hour=$hour, hourInt=$hourInt")
+
+    val rawData = getBaseData(date, hour, hourInt, spark).cache()
+    val baseData = calculateBaseData(rawData, spark).cache()
+    baseData
+      .write.mode("overwrite").saveAsTable("test.check_ocpc_result_data20191122a")
+
+  }
+
+  def getBaseData(date: String, hour: String, hourInt: Int, spark: SparkSession) = {
+    // 取历史数据
+    val dateConverter = new SimpleDateFormat("yyyy-MM-dd HH")
+    val newDate = date + " " + hour
+    val today = dateConverter.parse(newDate)
+    val calendar = Calendar.getInstance
+    calendar.setTime(today)
+    calendar.add(Calendar.HOUR, -hourInt)
+    val yesterday = calendar.getTime
+    val tmpDate = dateConverter.format(yesterday)
+    val tmpDateValue = tmpDate.split(" ")
+    val date1 = tmpDateValue(0)
+    val hour1 = tmpDateValue(1)
+    val selectCondition = getTimeRangeSqlDate(date1, hour1, date, hour)
+
+    val sqlRequest =
+      s"""
+         |SELECT
+         |  searchid,
+         |  cast(unitid as string) as identifier,
+         |  unitid,
+         |  userid,
+         |  adslot_type,
+         |  isshow,
+         |  isclick,
+         |  cast(exp_cvr as double) as exp_cvr,
+         |  media_appsid,
+         |  (case
+         |      when (cast(adclass as string) like '134%' or cast(adclass as string) like '107%') then "elds"
+         |      when (adslot_type<>7 and cast(adclass as string) like '100%') then "feedapp"
+         |      when (adslot_type=7 and cast(adclass as string) like '100%') then "yysc"
+         |      when adclass in (110110100, 125100100) then "wzcp"
+         |      else "others"
+         |  end) as industry,
+         |  conversion_goal,
+         |  cast(ocpc_log_dict['cvr_factor'] as double) as cvr_factor,
+         |  date,
+         |  hour
+         |FROM
+         |  dl_cpc.ocpc_filter_unionlog
+         |WHERE
+         |  $selectCondition
+         |AND
+         |  is_ocpc = 1
+         |AND
+         |  isclick = 1
+       """.stripMargin
+    println(sqlRequest)
+    val clickData = spark
+      .sql(sqlRequest)
+      .withColumn("cvr_goal", udfConcatStringInt("cvr")(col("conversion_goal")))
+      .withColumn("media", udfDetermineMedia()(col("media_appsid")))
+
+    // 抽取cv数据
+    val sqlRequest2 =
+      s"""
+         |SELECT
+         |  searchid,
+         |  label as iscvr,
+         |  cvr_goal
+         |FROM
+         |  dl_cpc.ocpc_label_cvr_hourly
+         |WHERE
+         |  $selectCondition
+       """.stripMargin
+    println(sqlRequest2)
+    val cvData = spark.sql(sqlRequest2).distinct()
+
+    // 数据关联
+    val resultDF = clickData
+      .join(cvData, Seq("searchid", "cvr_goal"), "left_outer")
+      .na.fill(0, Seq("iscvr"))
+
+    resultDF
+  }
+
+  def calculateBaseData(dataRaw: DataFrame, spark: SparkSession) = {
+    // 数据关联
+    val resultDF = dataRaw
+      .groupBy("identifier", "media", "conversion_goal", "date", "hour")
+      .agg(
+        sum(col("isclick")).alias("click"),
+        sum(col("iscvr")).alias("cv"),
+        sum(col("exp_cvr")).alias("total_pre_cvr")
+      )
+      .select("identifier", "media", "conversion_goal", "date", "hour", "click", "cv", "total_pre_cvr")
+      .withColumn("post _cvr", col("cv") * 1.0 / col("click"))
+      .withColumn("pre_cvr", col("total_pre_cvr") * 1.0 / col("click"))
+      .withColumn("pcoc", col("pre_cvr") * 1.0 / col("post_cvr"))
+      .select("identifier", "media", "conversion_goal", "date", "hour", "pcoc")
+
+    resultDF
+
+  }
+
+}
+
+
