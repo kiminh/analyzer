ada2725e3dd1515e17564e69ba18ed8532f5ac66
wangjun <wangjun02@qutoutiao.net>
2019-11-25 15:41:06 +0800

    update

--- src/main/scala/com/cpc/spark/oCPX/oCPC/calibration_x/pcoc_prediction/prepareTrainingSample.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/calibration_x/pcoc_prediction/prepareTrainingSample.scala
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat
 import java.util.Calendar
 
 import com.cpc.spark.oCPX.OcpcTools.{getTimeRangeSqlDate, udfConcatStringInt, udfDetermineMedia}
+import com.cpc.spark.oCPX.oCPC.calibration_x.pcoc_prediction.prepareLabel.prepareLabelMain
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SparkSession}
@@ -29,7 +30,7 @@ object prepareTrainingSample {
     println(s"date=$date, hour=$hour, hourInt=$hourInt, version=$version, expTag=$expTag")
 
     val data1 = getData(date, hour, hourInt, version, expTag, "dl_cpc.ocpc_pcoc_sample_part1_hourly", spark)
-    val data2 = getData(date, hour, hourInt, version, expTag, "dl_cpc.ocpc_pcoc_sample_part2_hourly", spark)
+    val data2 = prepareLabelMain(date, hour, hourInt, spark)
 
     val samples = assemblySample(data1, data2, spark)
     samples
