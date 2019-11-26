cdfe6c2fec65c61d02ff0443cb761c5958cb66db
wangjun <wangjun02@qutoutiao.net>
2019-11-26 12:12:12 +0800

    fix bug

--- src/main/scala/com/cpc/spark/oCPX/oCPC/calibration_x/pcoc_prediction/pcocModel_v2.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/calibration_x/pcoc_prediction/pcocModel_v2.scala
@@ -189,7 +189,7 @@ object pcocModel_v2 {
     val lrModel = new LinearRegression().setFeaturesCol("features").setLabelCol("label").setRegParam(0.001).setElasticNetParam(0.1).fit(dataset)
 
     val predictions = lrModel
-      .transform(predictData)
+      .transform(dataset)
       .withColumn("string_feature_list", udfStringFeatures()(col("hour")))
       .withColumn("double_feature_list", udfDoubleFeatures()(col("avg_pcoc"), col("diff1_pcoc"), col("diff2_pcoc"), col("recent_pcoc")))
       .select("identifier", "media", "conversion_goal", "conversion_from", "time", "hour", "avg_pcoc", "diff1_pcoc", "diff2_pcoc", "recent_pcoc", "features", "double_feature_list", "string_feature_list", "prediction", "label")
