617002a9570c10bf4217f8fff94e3f8701c0d272
wangjun <wangjun02@qutoutiao.net>
2019-11-26 22:41:48 +0800

    fix bug

--- src/main/scala/com/cpc/spark/oCPX/unittest/OcpcUnitTest.scala
+++ src/main/scala/com/cpc/spark/oCPX/unittest/OcpcUnitTest.scala
@@ -10,7 +10,7 @@ import com.cpc.spark.oCPX.oCPC.calibration_all.OcpcCalculateCalibrationValue._
 import com.cpc.spark.oCPX.oCPC.calibration_all.OcpcJFBfactor._
 import com.cpc.spark.oCPX.oCPC.calibration_all.OcpcSmoothfactor._
 import com.cpc.spark.oCPX.oCPC.calibration_by_tag.OcpcGetPb_baseline_others.getBaseDataDelayOther
-import com.cpc.spark.oCPX.oCPC.calibration_x.realtime.pcoc_calibration.OcpcGetPb_realtime.{OcpcCVRfactor, OcpcJFBfactor}
+import com.cpc.spark.oCPX.oCPC.calibration_x.realtime.pcoc_calibration.OcpcGetPb_realtime.{OcpcBIDfactor, OcpcCVRfactor, OcpcJFBfactor}
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SparkSession}
@@ -33,9 +33,9 @@ object OcpcUnitTest {
     println(s"date=$date, hour=$hour, hourInt=$hourInt")
 
     val version = "ocpctest"
-    val expTag = "realtime_v1"
+    val expTag = "realtimev1"
 
-    val baseDataRaw = OcpcCVRfactor(date, hour, hourInt, minCV, spark)
+    val baseDataRaw = OcpcBIDfactor(date, hour, version, expTag, 48, spark)
 
     baseDataRaw
       .repartition(1)
