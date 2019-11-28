1b50c360fb3bad45f3dbacba8b1bb1d488ae27fd
wangyao <wangyao@qutoutiao.net>
2019-11-28 13:32:48 +0800

    -

--- src/main/scala/com/cpc/spark/ml/calibration/SampleTempRead.scala
+++ src/main/scala/com/cpc/spark/ml/calibration/SampleTempRead.scala
@@ -1,32 +1,35 @@
 package com.cpc.spark.ml.calibration
 
+import com.cpc.spark.common.Murmur3Hash.stringHash64
+import com.cpc.spark.ml.calibration.MultiDimensionCalibOnQttCvrV3.LogToPb
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.{DataFrame, Row, SparkSession}
-import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
-import org.apache.spark.sql.functions._
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, udf}
 //import com.cpc.spark.ml.calibration.MultiDimensionCalibOnQtt.computeCalibration
 
 /**
   * author: wangyao
   * date: 5/14/19
   */
-object SampleTempRead {
+object SampleTempRead{
   def main(args: Array[String]): Unit = {
     Logger.getRootLogger.setLevel(Level.WARN)
 
     val spark = SparkSession.builder().enableHiveSupport().getOrCreate()
     import spark.implicits._
 
-
     val df = spark.read.format("com.databricks.spark.csv")
       .option("header", "false")
       .option("inferSchema", "false") //是否自动推到内容的类型
-      .option("delimiter", "\001") //分隔符，默认为 ,
-      .load("/home/cpc/wy/calibration_sample/cali_prediction-*.csv")
+      .option("delimiter","\001")  //分隔符，默认为 ,
+      .load("hdfs://emr-cluster/user/cpc/wy/tensorflow_cali_result.csv")
     df.show(10)
-    val resultDF = df.select($"_c0".alias("id"), $"_c1".alias("title"))
 
+    val resultDF= df.select($"_c0".alias("id"),$"_c1".alias("title"))
     resultDF.write.mode("overwrite").saveAsTable("dl_cpc.wy_tensorflow_cali_result")
   }
-}
 
+  def hash64(seed:Int)= udf {
+    x:String =>  stringHash64(x,seed)}
+
+}
