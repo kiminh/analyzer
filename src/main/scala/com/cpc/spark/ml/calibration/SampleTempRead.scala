c48bd6c6078e1ab934aebedd66bffcf91b859637
wangyao <wangyao@qutoutiao.net>
2019-11-28 14:17:19 +0800

    -

--- src/main/scala/com/cpc/spark/ml/calibration/SampleTempRead.scala
+++ src/main/scala/com/cpc/spark/ml/calibration/SampleTempRead.scala
@@ -19,14 +19,13 @@ object SampleTempRead{
     import spark.implicits._
 
     val df = spark.read.format("com.databricks.spark.csv")
-      .option("header", "false")
+      .option("header", "true")
       .option("inferSchema", "false") //是否自动推到内容的类型
       .option("delimiter","\001")  //分隔符，默认为 ,
       .load("hdfs://emr-cluster/user/cpc/wy/tensorflow_cali_result.csv")
     df.show(10)
 
-    val resultDF= df.select($"_c0".alias("id"),$"_c1".alias("title"))
-    resultDF.write.mode("overwrite").saveAsTable("dl_cpc.wy_tensorflow_cali_result")
+    df.write.mode("overwrite").saveAsTable("dl_cpc.wy_tensorflow_cali_result")
   }
 
   def hash64(seed:Int)= udf {
