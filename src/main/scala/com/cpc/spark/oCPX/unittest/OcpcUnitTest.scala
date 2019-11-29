bcb131b82ec5a499305e1cd88f5ed3b4a9a02b65
wangjun <wangjun02@qutoutiao.net>
2019-11-29 11:29:21 +0800

    fix bug

--- src/main/scala/com/cpc/spark/oCPX/unittest/OcpcUnitTest.scala
+++ src/main/scala/com/cpc/spark/oCPX/unittest/OcpcUnitTest.scala
@@ -1,7 +1,7 @@
 package com.cpc.spark.oCPX.unittest
 
 import com.cpc.spark.oCPX.oCPC.calibration_x.pcoc_prediction.prepareLabel.prepareLabelMain
-import com.cpc.spark.oCPX.oCPC.calibration_x.pcoc_prediction.v3.prepareTrainingSample.getFeatureData
+import com.cpc.spark.oCPX.oCPC.calibration_x.pcoc_prediction.v3.prepareTrainingSample.{getFeatureData, udfAddHour}
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SparkSession}
@@ -28,8 +28,12 @@ object OcpcUnitTest {
 
     val baseDataRaw = getFeatureData(date, hour, hourInt, version, expTag, spark)
 
-    baseDataRaw
-      .write.mode("overwrite").saveAsTable("test.check_ocpc_data20191128a")
+    val data = baseDataRaw
+      .withColumn("time", udfAddHour(4)(col("date"), col("hour")))
+      .withColumn("hour_diff", lit(4))
+      .select("identifier", "media", "conversion_goal", "conversion_from", "double_feature_list", "string_feature_list", "time", "date", "hour", "hour_diff")
+    data
+      .write.mode("overwrite").saveAsTable("test.check_ocpc_data20191129a")
 
   }
 
