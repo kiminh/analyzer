5de85459f8f9a9a237e994056b0e872c53ff90a8
樊一鸣 <fanyiming@qutoutiao.net>
2019-12-02 17:43:01 +0800

    Fym dev

--- src/main/scala/com/cpc/spark/qukan/userprofile/TopApps.scala
+++ src/main/scala/com/cpc/spark/qukan/userprofile/TopApps.scala
@@ -91,8 +91,8 @@ object TopApps {
       "zhangting@qutoutiao.net", "huxinjie@aiclk.com", "sujiaqi@qutoutiao.net", "weijinxian@qutoutiao.net",
       "yishaobin@qutoutiao.net", "yuxiaoyang@qutoutiao.net", "heting@qutoutiao.net",
       "liutianlin@qutoutiao.net", "baizhen@qutoutiao.net", "zhangzhiyang@qutoutiao.net",
-      "duruiyu@qutoutiao.net", "chenge@qutoutiao.net", "zhangxiaonian@qutoutiao.net", "zhangwei07@qutoutiao.net", "fanyiming@qutoutiao.net"))
-//    val qtt = sendMail(qtt_txt, "%s topApps 活跃用户DAU[仅趣头条]".format(date), Seq("zhangxiaonian@qutoutiao.net", "zhangwei07@qutoutiao.net", "fanyiming@qutoutiao.net"))
+      "duruiyu@qutoutiao.net", "chenge@qutoutiao.net", "zhangxiaonian@qutoutiao.net", "zhangwei07@qutoutiao.net", "fanyiming@qutoutiao.net", "zhuqiqi@qutoutiao.net"))
+    //val qtt = sendMail(qtt_txt, "%s topApps 活跃用户DAU[仅趣头条]".format(date), Seq("zhanghongyang@aiclk.com"))
     if (!qtt) {
       println("发送邮件失败")
     }
