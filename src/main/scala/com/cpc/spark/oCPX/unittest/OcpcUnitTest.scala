c7234fe997e6f1c968f7b1f1960862ac4bc786b2
wangjun <wangjun02@qutoutiao.net>
2019-12-02 20:19:01 +0800

    fix bug

--- src/main/scala/com/cpc/spark/oCPX/unittest/OcpcUnitTest.scala
+++ src/main/scala/com/cpc/spark/oCPX/unittest/OcpcUnitTest.scala
@@ -1,6 +1,7 @@
 package com.cpc.spark.oCPX.unittest
 
 import com.cpc.spark.oCPX.oCPC.calibration.OcpcCalibrationBase.OcpcCalibrationBaseMain
+import com.cpc.spark.oCPX.oCPC.calibration.OcpcJFBfactorV2.OcpcJFBfactorMain
 import com.cpc.spark.oCPX.oCPC.calibration_by_tag.OcpcGetPb_adtype15.{OcpcCalibrationBase, getDataByTimeSpan}
 import com.cpc.spark.oCPX.oCPC.calibration_x.pcoc_prediction.prepareLabel.prepareLabelMain
 import com.cpc.spark.oCPX.oCPC.calibration_x.pcoc_prediction.v3.prepareTrainingSample.{getFeatureData, udfAddHour, udfStringListAppend}
@@ -26,18 +27,24 @@ object OcpcUnitTest {
     println(s"date=$date, hour=$hour, hourInt=$hourInt1")
 
     val version = "ocpctest"
-    val expTag = "v3"
+    val expTag = "adtype15"
 
-    val dataRaw1 = OcpcCalibrationBaseMain(date, hour, hourInt1, spark).cache()
+    // 计算jfb_factor,cvr_factor,post_cvr
+    val dataRaw1 = OcpcCalibrationBaseMain(date, hour, 24, spark).cache()
+    dataRaw1.show(10)
+    val dataRaw2 = OcpcCalibrationBaseMain(date, hour, 48, spark).cache()
+    dataRaw2.show(10)
+    val dataRaw3 = OcpcCalibrationBaseMain(date, hour, 72, spark).cache()
+    dataRaw3.show(10)
 
-    val dataRaw2 = OcpcCalibrationBase(date, hour, 48, spark).cache()
-    val data2  = getDataByTimeSpan(dataRaw2, date, hour, hourInt1, spark)
+    val jfbDataRaw = OcpcJFBfactorMain(date, hour, version, expTag, dataRaw1, dataRaw2, dataRaw3, spark)
+    val jfbData = jfbDataRaw
+      .withColumn("jfb_factor", lit(1.0) / col("jfb"))
+      .select("unitid", "conversion_goal", "exp_tag", "jfb_factor")
+      .cache()
 
-    dataRaw1
+    jfbData
       .write.mode("overwrite").saveAsTable("test.check_ocpc_data20191202a")
-
-    data2
-      .write.mode("overwrite").saveAsTable("test.check_ocpc_data20191202b")
 
   }
 
