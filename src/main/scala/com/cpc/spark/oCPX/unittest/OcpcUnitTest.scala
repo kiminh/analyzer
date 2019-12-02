d1dff531f9dad280d85e9cd1b48efe786717f20b
wangjun <wangjun02@qutoutiao.net>
2019-12-02 20:20:31 +0800

    update

--- src/main/scala/com/cpc/spark/oCPX/unittest/OcpcUnitTest.scala
+++ src/main/scala/com/cpc/spark/oCPX/unittest/OcpcUnitTest.scala
@@ -2,7 +2,7 @@ package com.cpc.spark.oCPX.unittest
 
 import com.cpc.spark.oCPX.oCPC.calibration.OcpcCalibrationBase.OcpcCalibrationBaseMain
 import com.cpc.spark.oCPX.oCPC.calibration.OcpcJFBfactorV2.OcpcJFBfactorMain
-import com.cpc.spark.oCPX.oCPC.calibration_by_tag.OcpcGetPb_adtype15.{OcpcCalibrationBase, getDataByTimeSpan}
+import com.cpc.spark.oCPX.oCPC.calibration_by_tag.OcpcGetPb_adtype15.{OcpcCalibrationBase, OcpcJFBfactor, getDataByTimeSpan}
 import com.cpc.spark.oCPX.oCPC.calibration_x.pcoc_prediction.prepareLabel.prepareLabelMain
 import com.cpc.spark.oCPX.oCPC.calibration_x.pcoc_prediction.v3.prepareTrainingSample.{getFeatureData, udfAddHour, udfStringListAppend}
 import org.apache.log4j.{Level, Logger}
@@ -37,14 +37,27 @@ object OcpcUnitTest {
     val dataRaw3 = OcpcCalibrationBaseMain(date, hour, 72, spark).cache()
     dataRaw3.show(10)
 
-    val jfbDataRaw = OcpcJFBfactorMain(date, hour, version, expTag, dataRaw1, dataRaw2, dataRaw3, spark)
-    val jfbData = jfbDataRaw
+    val jfbDataRaw1 = OcpcJFBfactorMain(date, hour, version, expTag, dataRaw1, dataRaw2, dataRaw3, spark)
+    val jfbData1 = jfbDataRaw1
       .withColumn("jfb_factor", lit(1.0) / col("jfb"))
       .select("unitid", "conversion_goal", "exp_tag", "jfb_factor")
-      .cache()
 
-    jfbData
+    jfbData1
       .write.mode("overwrite").saveAsTable("test.check_ocpc_data20191202a")
+
+    /*****************************************/
+    // 基础数据
+    val dataRaw = OcpcCalibrationBase(date, hour, 72, spark).cache()
+    dataRaw.show(10)
+
+    // 计费比系数模块
+    val jfbDataRaw2 = OcpcJFBfactor(date, hour, expTag, dataRaw, 24, 48, 72, spark)
+    val jfbData2 = jfbDataRaw2
+      .withColumn("jfb_factor", lit(1.0) / col("jfb"))
+      .select("unitid", "conversion_goal", "exp_tag", "jfb_factor")
+
+    jfbData2
+      .write.mode("overwrite").saveAsTable("test.check_ocpc_data20191202b")
 
   }
 
