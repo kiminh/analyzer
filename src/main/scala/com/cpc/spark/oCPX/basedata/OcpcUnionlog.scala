c45832bde24f8938726dc951d6df7b322f9938c1
wangjun <wangjun02@qutoutiao.net>
2019-12-06 13:46:32 +0800

    update

--- src/main/scala/com/cpc/spark/oCPX/basedata/OcpcUnionlog.scala
+++ src/main/scala/com/cpc/spark/oCPX/basedata/OcpcUnionlog.scala
@@ -18,8 +18,8 @@ object OcpcUnionlog {
 
     data
       .repartition(100)
-//      .write.mode("overwrite").insertInto("test.ocpc_base_unionlog")
-      .write.mode("overwrite").insertInto("dl_cpc.ocpc_base_unionlog")
+      .write.mode("overwrite").insertInto("test.ocpc_base_unionlog")
+//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_base_unionlog")
 
     println("successfully save data into table: dl_cpc.ocpc_base_unionlog")
 
@@ -27,8 +27,8 @@ object OcpcUnionlog {
     val ocpcData = getOcpcUnionlog(data, date, hour, spark)
     ocpcData
       .repartition(50)
-//      .write.mode("overwrite").insertInto("test.ocpc_filter_unionlog")
-      .write.mode("overwrite").insertInto("dl_cpc.ocpc_filter_unionlog")
+      .write.mode("overwrite").insertInto("test.ocpc_filter_unionlog")
+//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_filter_unionlog")
 
     println("successfully save data into table: dl_cpc.ocpc_filter_unionlog")
   }
@@ -111,7 +111,8 @@ object OcpcUnionlog {
          |    ocpc_expand_tag,
          |    tuid,
          |    hidden_tax,
-         |    pure_deep_exp_cvr
+         |    pure_deep_exp_cvr,
+         |    deep_ocpc_step
          |from
          |    base_data
        """.stripMargin
@@ -213,7 +214,8 @@ object OcpcUnionlog {
          |    site_type,
          |    tuid,
          |    hidden_tax,
-         |    pure_deep_exp_cvr
+         |    pure_deep_exp_cvr,
+         |    deep_ocpc_step
          |from dl_cpc.cpc_basedata_union_events
          |where $selectWhere
          |and (isshow>0 or isclick>0)
