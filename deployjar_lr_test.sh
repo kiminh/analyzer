f68becf938a639475c5ed6e9d08fb44966015307
qizhi <qizhi@qutoutiao.net>
2019-12-09 21:09:59 +0800

    push

--- deployjar_lr_test.sh
+++ deployjar_lr_test.sh
@@ -4,3 +4,4 @@ git pull
 sbt assembly
 echo "cp target/scala-2.11/cpc-anal_2.11-0.1.jar /home/cpc/anal/lib/lr-train-test.jar"
 cp target/scala-2.11/cpc-anal_2.11-0.1.jar /home/cpc/anal/lib/lr-train-test.jar
+hadoop fs -put -f target/scala-2.11/cpc-anal_2.11-0.1.jar hdfs://emr-cluster/user/cpc/qizhi/lr-train-test.jar
