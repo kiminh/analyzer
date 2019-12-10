f50b4bcac9b93fef3f62f23b2e6b49ffeaa52880
wangjun <wangjun02@qutoutiao.net>
2019-12-10 17:34:27 +0800

    fix bug

--- src/main/scala/com/cpc/spark/oCPX/oCPC/pay/v2/OcpcChargeSchedule.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/pay/v2/OcpcChargeSchedule.scala
@@ -36,8 +36,8 @@ object OcpcChargeSchedule {
     // 获取前一天的ocpc_compensate线上数据（备份表），基于ocpc_charge_time和deep_ocpc_charge_time来判断周期开始日期以及分别需要计算深度还是浅层赔付
     val ocpcCompensate = getOcpcCompensate(date, spark)
 
-
     // 统计今天的分单元消耗和开始消费时间
+    
 
     // 更新赔付周期表
 
