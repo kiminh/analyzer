d8b3341ef4e2678721b38cb1d1b27a12f70d946f
wangjun <wangjun02@qutoutiao.net>
2019-12-16 20:42:49 +0800

    update

--- src/main/scala/com/cpc/spark/oCPX/oCPC/pay/v2/OcpcChargeSchedule.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/pay/v2/OcpcChargeSchedule.scala
@@ -47,15 +47,15 @@ object OcpcChargeSchedule {
       .repartition(1)
       .write.mode("overwrite").saveAsTable("test.ocpc_compensate_schedule_daily20191216a")
 
-//    val resultDF = result
-//      .select("unitid", "calc_dates", "date_diff", "pay_cnt", "last_ocpc_charge_time", "last_deep_ocpc_charge_time", "is_pay_flag", "is_deep_pay_flag", "first_charge_time", "final_charge_time", "recent_charge_time", "last_ocpc_charge_time_old", "last_deep_ocpc_charge_time_old")
-//      .withColumn("date", lit(date))
-//      .withColumn("version", lit(version))
-//
-//    resultDF
-//      .repartition(1)
-//      .write.mode("overwrite").insertInto("test.ocpc_compensate_schedule_daily")
-////      .write.mode("overwrite").insertInto("dl_cpc.ocpc_compensate_schedule_daily")
+    val resultDF = result
+      .select("unitid", "calc_dates", "date_diff", "pay_cnt", "current_ocpc_charge_time", "current_deep_ocpc_charge_time", "ocpc_charge_time", "deep_ocpc_charge_time", "is_pay_flag", "is_deep_pay_flag", "recent_charge_time")
+      .withColumn("date", lit(date))
+      .withColumn("version", lit(version))
+
+    resultDF
+      .repartition(1)
+      .write.mode("overwrite").insertInto("test.ocpc_compensate_schedule_daily")
+//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_compensate_schedule_daily")
 
   }
 
@@ -442,9 +442,9 @@ object OcpcChargeSchedule {
          |  base_data
          |""".stripMargin
     println(sqlRequest2)
-    val data2 = spark.sql(sqlRequest2)
+    val result = spark.sql(sqlRequest2)
 
-    data
+    result
   }
 
 
