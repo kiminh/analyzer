10abae7649d15956d5ab5d019d3a02d44694f812
wangjun <wangjun02@qutoutiao.net>
2019-12-17 10:25:43 +0800

    update

--- src/main/scala/com/cpc/spark/oCPX/sql/ocpc_compensate_backup_daily.sql
+++ src/main/scala/com/cpc/spark/oCPX/sql/ocpc_compensate_backup_daily.sql
@@ -10,11 +10,33 @@ create table if not exists test.ocpc_compensate_backup_daily(
     pay                     double,
     cpc_flag                int,
     logic_version           int,
-    create_time             string
+    create_time             string,
+    is_deep_ocpc            int,
+    deep_ocpc_charge_time   string,
+    is_pay                  int
 )
-partitioned by (`date` string)
+partitioned by (`date` string, version string)
 stored as parquet;
 
-alter table dl_cpc.ocpc_compensate_backup_daily add columns (is_deep_ocpc int);
-alter table dl_cpc.ocpc_compensate_backup_daily add columns (deep_ocpc_charge_time string);
-alter table dl_cpc.ocpc_compensate_backup_daily add columns (is_pay int);
+
+
+
+create table if not exists dl_cpc.ocpc_compensate_backup_daily_v2(
+    unitid                  int,
+    userid                  int,
+    compensate_key          string,
+    ocpc_charge_time        string,
+    cost                    double,
+    conversion              int,
+    cpareal                 double,
+    cpagiven                double,
+    pay                     double,
+    cpc_flag                int,
+    logic_version           int,
+    create_time             string,
+    is_deep_ocpc            int,
+    deep_ocpc_charge_time   string,
+    is_pay                  int
+)
+partitioned by (`date` string, version string)
+stored as parquet;
