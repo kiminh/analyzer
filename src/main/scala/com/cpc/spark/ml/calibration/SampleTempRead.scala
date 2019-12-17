1f0b2938c108f6c250c98c2e3f5717446b316f52
wangyao <wangyao@qutoutiao.net>
2019-12-17 10:24:35 +0800

    -

--- src/main/scala/com/cpc/spark/ml/calibration/SampleTempRead.scala
+++ src/main/scala/com/cpc/spark/ml/calibration/SampleTempRead.scala
@@ -22,7 +22,7 @@ object SampleTempRead{
       .option("header", "true")
       .option("inferSchema", "false") //是否自动推到内容的类型
       .option("delimiter","\001")  //分隔符，默认为 ,
-      .load("hdfs://emr-cluster/user/cpc/wy/tensorflow_cali_result_wzjf_load_r5.csv")
+      .load("hdfs://emr-cluster/user/cpc/wy/tensorflow_cali_result_novel_r5.csv")
     df.show(10)
 
     df.write.mode("overwrite").saveAsTable("dl_cpc.wy_tensorflow_cali_result")
