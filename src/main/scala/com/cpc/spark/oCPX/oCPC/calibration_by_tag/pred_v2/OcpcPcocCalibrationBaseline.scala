af15a0221e278dbd364d89d1d920adce77a3abd1
wangjun <wangjun02@qutoutiao.net>
2019-12-19 20:05:26 +0800

    fix bug

--- src/main/scala/com/cpc/spark/oCPX/oCPC/calibration_by_tag/pred_v2/OcpcPcocCalibrationBaseline.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/calibration_by_tag/pred_v2/OcpcPcocCalibrationBaseline.scala
@@ -67,8 +67,8 @@ object OcpcPcocCalibrationBaseline {
 
     resultDF
       .repartition(1)
-      .write.mode("overwrite").insertInto("test.ocpc_pb_data_hourly_exp")
-//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_pb_data_hourly_exp")
+//      .write.mode("overwrite").insertInto("test.ocpc_pb_data_hourly_exp")
+      .write.mode("overwrite").insertInto("dl_cpc.ocpc_pb_data_hourly_exp")
 
 
   }
