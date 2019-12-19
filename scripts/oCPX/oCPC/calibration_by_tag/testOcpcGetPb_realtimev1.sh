abd083a4c0429a2b5855a16197c3c9b6fdd0c1a4
高慧杰 <gaohuijie@qutoutiao.net>
2019-12-19 17:19:31 +0800

    1

--- scripts/oCPX/oCPC/calibration_by_tag/testOcpcGetPb_realtimev1.sh
+++ scripts/oCPX/oCPC/calibration_by_tag/testOcpcGetPb_realtimev1.sh
@@ -18,8 +18,8 @@ $SPARK_HOME/bin/spark-submit --master yarn --queue $queue \
     --conf 'spark.yarn.executor.memoryOverhead=4g'\
     --conf 'spark.dynamicAllocation.maxExecutors=50'\
     --jars $( IFS=$','; echo "${jars[*]}" ) \
-    --class com.cpc.spark.oCPX.oCPC.calibration_by_tag.OcpcGetPb_realtimev1 \
-    /home/cpc/wangjun/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $1 $2 $3 $4 $5 $6 $7 $8
+    --class com.cpc.spark.oCPX.oCPC.calibration_by_tag.OcpcGetPb_realtimev1_2 \
+    /home/cpc/ghj/cpc/analyzer/target/scala-2.11/cpc-anal_2.11-0.1.jar $1 $2 $3 $4 $5 $6 $7 $8
 
 #val date = args(0).toString
 #val hour = args(1).toString
