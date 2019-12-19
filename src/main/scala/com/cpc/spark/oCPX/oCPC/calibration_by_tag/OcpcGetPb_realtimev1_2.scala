9d6f5bb9c8850969ca10e9c98324cfcac4a17e43
高慧杰 <gaohuijie@qutoutiao.net>
2019-12-19 21:34:35 +0800

    13

--- src/main/scala/com/cpc/spark/oCPX/oCPC/calibration_by_tag/OcpcGetPb_realtimev1_2.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/calibration_by_tag/OcpcGetPb_realtimev1_2.scala
@@ -44,7 +44,7 @@ object OcpcGetPb_realtimev1_2 {
     // 计算pcoc
     val pcocDataRaw = OcpcCVRfactor(date1, hour1, hourInt, minCV, spark)
     val pcocData = pcocDataRaw
-      .selectExpr("identifier", "conversion_goal", "media", "cvr_factor as cvr_factor2" )
+      .selectExpr("identifier", "conversion_goal", "expTag", "cvr_factor as cvr_factor2" )
       .cache()
     pcocData.show(10)
 
@@ -52,14 +52,14 @@ object OcpcGetPb_realtimev1_2 {
     val pcocData_baseRaw = OcpcCVRfactor_base(date1, hour1, expTag, dataRaw, hourInt1, hourInt2, hourInt3, spark)
     val pcocData_base = pcocData_baseRaw
       .withColumn("cvr_factor1", lit(1.0) / col("pcoc"))
-      .selectExpr("cast(unitid as string) identifier", "conversion_goal", "media", "cvr_factor1")
+      .selectExpr("cast(unitid as string) identifier", "conversion_goal", "expTag", "cvr_factor1")
       .cache()
     pcocData_base.show(10)
 
 
     // 数据关联
     val pcocData_final = pcocData
-      .join(pcocData_base, Seq("identifier", "conversion_goal", "media" ), "outer")
+      .join(pcocData_base, Seq("identifier", "conversion_goal", "expTag" ), "outer")
       .withColumn("cvr_factor", when(col("cvr_factor2").isNull, col("cvr_factor1")).otherwise(col("cvr_factor2")))
 
     pcocData_final
