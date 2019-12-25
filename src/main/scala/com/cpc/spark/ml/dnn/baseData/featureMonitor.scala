5d12ec5b15a8badb4ce6f7494a980f0d6e83871d
duanguangdong <duanguangdong@qutoutiao.net>
2019-12-25 15:07:18 +0800

    delete

--- src/main/scala/com/cpc/spark/ml/dnn/baseData/featureMonitor.scala
+++ src/main/scala/com/cpc/spark/ml/dnn/baseData/featureMonitor.scala
@@ -155,7 +155,7 @@ object FeatureMonitor {
         s"""
            |select one_hot_feature_count, one_hot_feature_id_num, multi_hot_feature_count
            |from dl_cpc.cpc_feature_monitor where model_name="$model_name" and hour='$hour' and day in (
-           |select max(day) from dl_cpc.cpc_feature_monitor where model_name="$model_name" and day<"$curday")
+           |select max(day) from dl_cpc.cpc_feature_monitor where model_name="$model_name" and day<"$curday" and hour='$hour')
            |""".stripMargin)
       if(feature_value.count()==0){
         print("feature exception: Unable to find the historical information of this feature in the model")
