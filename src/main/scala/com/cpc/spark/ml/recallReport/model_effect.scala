850c3ed51df39b8702784828903dcd3a417b200a
duanguangdong <duanguangdong@qutoutiao.net>
2019-12-29 16:29:02 +0800

    delete

--- src/main/scala/com/cpc/spark/ml/recallReport/model_effect.scala
+++ src/main/scala/com/cpc/spark/ml/recallReport/model_effect.scala
@@ -97,7 +97,7 @@ object model_effect {
 
   def hash(seed:Int, dist_map:mutable.Map[Int, String])= udf {
     x:String => {
-      var hash_value = Murmur3Hash.stringHash32(x,seed).toDouble
+      var hash_value = Murmur3Hash.stringHash32(x,seed)
       if(hash_value<0){
         hash_value += scala.math.pow(2,32)
       }
