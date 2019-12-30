ae9baca463cbfd083cfb1d114b245ab416a5a591
wangyao <wangyao@qutoutiao.net>
2019-12-30 17:13:23 +0800

    -

--- src/main/scala/com/cpc/spark/ml/calibration/MultiDimensionCalibOnQttCvrwzjfnew.scala
+++ src/main/scala/com/cpc/spark/ml/calibration/MultiDimensionCalibOnQttCvrwzjfnew.scala
@@ -88,7 +88,7 @@ object MultiDimensionCalibOnQttCvrwzjfnew {
       s"""
          |select * from
          |(select
-         |  unitid, count(*) click, sum(isclck)/count(*) cvr
+         |  unitid, count(*) click, sum(isclick)/count(*) cvr
          |  from test
          |  group by unitid
          |)
