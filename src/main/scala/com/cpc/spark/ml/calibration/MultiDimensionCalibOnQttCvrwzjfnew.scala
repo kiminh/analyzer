b12b47dd4d62bd35a07772e85a226c94189d55cf
wangyao <wangyao@qutoutiao.net>
2019-12-31 14:13:42 +0800

    -

--- src/main/scala/com/cpc/spark/ml/calibration/MultiDimensionCalibOnQttCvrwzjfnew.scala
+++ src/main/scala/com/cpc/spark/ml/calibration/MultiDimensionCalibOnQttCvrwzjfnew.scala
@@ -101,6 +101,7 @@ object MultiDimensionCalibOnQttCvrwzjfnew {
         .filter("flag = 0")
 
     filter_data.show(10)
+    val tablename = model.split("-").mkString("_")
     filter_data.repartition(1).write.mode("overwrite").saveAsTable(s"dl_cpc.post_calibration_${model}")
 
     val data = session.sql(s"select * from dl_cpc.post_calibration_${model}")
