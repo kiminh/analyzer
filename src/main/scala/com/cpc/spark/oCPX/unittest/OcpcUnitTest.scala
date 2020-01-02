3b6e9c6a40f162ebfa48fb339557d61334004e24
wangjun <wangjun02@qutoutiao.net>
2020-01-02 22:51:13 +0800

    update

--- src/main/scala/com/cpc/spark/oCPX/unittest/OcpcUnitTest.scala
+++ src/main/scala/com/cpc/spark/oCPX/unittest/OcpcUnitTest.scala
@@ -1,7 +1,7 @@
 package com.cpc.spark.oCPX.unittest
 
 
-import com.cpc.spark.oCPX.deepOcpc.calibration_v7.OcpcGetPb_retention.{OcpcCalibrationBase, calculateCvrPart1, calculateCvrPart2, calculateDeepCvr}
+import com.cpc.spark.oCPX.deepOcpc.calibration_v7.OcpcGetPb_retention.{OcpcCalibrationBase, calculateCvrFactor, calculateCvrPart1, calculateCvrPart2, calculateDeepCvr}
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SparkSession}
@@ -22,21 +22,23 @@ object OcpcUnitTest {
     println(s"date=$date, hour=$hour")
 
     val dataRaw = OcpcCalibrationBase(date, hour, 96, spark)
-    // calculate deep_cvr
-    val deepCvr = calculateDeepCvr(date, 3, spark)
+//    // calculate deep_cvr
+//    val deepCvr = calculateDeepCvr(date, 3, spark)
+//
+//    // calculate cv2_t1
+//    val data1 = calculateCvrPart1(dataRaw, deepCvr, 10, spark)
+//
+//    // calculate cv2_t2 ~ cv2_t4
+//    val data2 = calculateCvrPart2(dataRaw, 20, spark)
+//
+//    // data join
+//    val data = data1.union(data2)
 
-    // calculate cv2_t1
-    val data1 = calculateCvrPart1(dataRaw, deepCvr, 10, spark)
-
-    // calculate cv2_t2 ~ cv2_t4
-    val data2 = calculateCvrPart2(dataRaw, 20, spark)
-
-    // data join
-    val data = data1.union(data2)
+    val cvrData = calculateCvrFactor(dataRaw, date, hour, spark)
 
 
-    data
-      .write.mode("overwrite").saveAsTable("test.check_ocpc_data20200102a")
+    cvrData
+      .write.mode("overwrite").saveAsTable("test.check_ocpc_data20200102b")
 
 
 
