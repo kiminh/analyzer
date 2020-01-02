0be38eae23c3718a2abfaa95a7b0935f7d263614
wangjun <wangjun02@qutoutiao.net>
2020-01-02 20:24:29 +0800

    fix bug

--- src/main/scala/com/cpc/spark/oCPX/oCPC/calibration_x/realtime/pcoc_calibration/OcpcGetPb_weightv2.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/calibration_x/realtime/pcoc_calibration/OcpcGetPb_weightv2.scala
@@ -68,8 +68,8 @@ object OcpcGetPb_weightv2{
 
     resultDF
       .repartition(1)
-      .write.mode("overwrite").insertInto("test.ocpc_pb_data_hourly_exp")
-//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_pb_data_hourly_exp")
+//      .write.mode("overwrite").insertInto("test.ocpc_pb_data_hourly_exp")
+      .write.mode("overwrite").insertInto("dl_cpc.ocpc_pb_data_hourly_exp")
 
 
   }
