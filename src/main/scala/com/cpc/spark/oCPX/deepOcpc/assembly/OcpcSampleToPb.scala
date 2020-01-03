e53a8612d2d2a3eb2f6f8d567232cd1d82d601f1
wangjun <wangjun02@qutoutiao.net>
2020-01-03 15:29:12 +0800

    fix bug

--- src/main/scala/com/cpc/spark/oCPX/deepOcpc/assembly/OcpcSampleToPb.scala
+++ src/main/scala/com/cpc/spark/oCPX/deepOcpc/assembly/OcpcSampleToPb.scala
@@ -43,8 +43,8 @@ object OcpcSampleToPb {
       .withColumn("hour", lit(hour))
       .withColumn("version", lit(version))
       .repartition(5)
-      .write.mode("overwrite").insertInto("test.ocpc_deep_param_pb_data_hourly")
-//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_deep_param_pb_data_hourly")
+//      .write.mode("overwrite").insertInto("test.ocpc_deep_param_pb_data_hourly")
+      .write.mode("overwrite").insertInto("dl_cpc.ocpc_deep_param_pb_data_hourly")
 
 
     val resultDF2 = getCalibrationData2(date, hour, hourInt, spark)
@@ -54,8 +54,8 @@ object OcpcSampleToPb {
       .withColumn("date", lit(date))
       .withColumn("hour", lit(hour))
       .repartition(5)
-      .write.mode("overwrite").insertInto("test.ocpc_deep_param_pb_data_hourly_baseline")
-    //      .write.mode("overwrite").insertInto("dl_cpc.ocpc_deep_param_pb_data_hourly_baseline")
+//      .write.mode("overwrite").insertInto("test.ocpc_deep_param_pb_data_hourly_baseline")
+      .write.mode("overwrite").insertInto("dl_cpc.ocpc_deep_param_pb_data_hourly_baseline")
 
   }
 
