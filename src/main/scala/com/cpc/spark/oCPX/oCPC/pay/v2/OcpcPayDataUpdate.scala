2a9fce12403a4f90ef507e455775f573aec3f068
wangjun <wangjun02@qutoutiao.net>
2020-01-08 21:48:54 +0800

    fix bug

--- src/main/scala/com/cpc/spark/oCPX/oCPC/pay/v2/OcpcPayDataUpdate.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/pay/v2/OcpcPayDataUpdate.scala
@@ -405,7 +405,6 @@ object OcpcPayDataUpdate {
 
     val resultDF = data
       .selectExpr("unitid",  "userid", "ocpc_charge_time", "deep_ocpc_charge_time", "cost", "conversion", "cpagiven", "pay", "is_deep_ocpc")
-      .filter(s"date(ocpc_charge_time) = '$date1'")
       .distinct()
       .cache()
 
