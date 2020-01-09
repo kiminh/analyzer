200dc3e5e18f8417a61ceeaeb4db4a7b9a3fda1a
wangjun <wangjun02@qutoutiao.net>
2020-01-09 18:55:21 +0800

    fix bug

--- src/main/scala/com/cpc/spark/oCPX/oCPC/pay/v2/OcpcChargeCost.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/pay/v2/OcpcChargeCost.scala
@@ -53,8 +53,8 @@ object OcpcChargeCost {
 
     resultDF
       .repartition(1)
-//      .write.mode("overwrite").insertInto("test.ocpc_compensate_result_daily")
-      .write.mode("overwrite").insertInto("dl_cpc.ocpc_compensate_result_daily")
+      .write.mode("overwrite").insertInto("test.ocpc_compensate_result_daily")
+//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_compensate_result_daily")
 
   }
 
