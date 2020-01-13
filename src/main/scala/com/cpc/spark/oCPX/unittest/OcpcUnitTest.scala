f33b9a30b425098bbe1f59ff1c4d33a2beb8cce6
wangjun <wangjun02@qutoutiao.net>
2020-01-13 10:24:53 +0800

    update

--- src/main/scala/com/cpc/spark/oCPX/unittest/OcpcUnitTest.scala
+++ src/main/scala/com/cpc/spark/oCPX/unittest/OcpcUnitTest.scala
@@ -1,7 +1,7 @@
 package com.cpc.spark.oCPX.unittest
 
 
-import com.cpc.spark.oCPX.oCPC.pay.v2.OcpcChargeSchedule.getTodayData
+import com.cpc.spark.oCPX.oCPC.pay.v2.OcpcChargeSchedule.{getTodayData, udfDeterminePayIndustry}
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SparkSession}
@@ -21,10 +21,32 @@ object OcpcUnitTest {
     println("parameters:")
     println(s"date=$date, hour=$hour")
 
-    val todayData = getTodayData(date, spark)
+    val sqlRequest1 =
+      s"""
+         |SELECT
+         |  searchid,
+         |  unitid,
+         |  adslot_type,
+         |  adclass,
+         |  conversion_goal,
+         |  timestamp,
+         |  from_unixtime(timestamp,'YYYY-MM-dd HH:mm:ss') as ocpc_charge_time,
+         |  row_number() over(partition by unitid order by timestamp) as seq
+         |FROM
+         |  dl_cpc.ocpc_filter_unionlog
+         |WHERE
+         |  date = '$date'
+         |""".stripMargin
+    println(sqlRequest1)
+    val data1 = spark
+      .sql(sqlRequest1)
+      .withColumn("industry", udfDeterminePayIndustry()(col("adslot_type"), col("adclass"), col("conversion_goal")))
+      .filter(s"industry in ('feedapp', 'elds', 'pay_industry', 'siteform_pay_industry')")
+      .filter(s"seq = 1")
+      .select("unitid", "timestamp", "ocpc_charge_time")
 
-    todayData
-      .write.mode("overwrite").saveAsTable("test.check_ocpc_data20200107d")
+    data1
+      .write.mode("overwrite").saveAsTable("test.check_ocpc_data20200113a")
 
 
 
