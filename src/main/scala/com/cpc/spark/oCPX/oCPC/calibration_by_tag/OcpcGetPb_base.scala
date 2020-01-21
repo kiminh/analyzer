c82a6943ec51f8b3b4ed7fb20444a6abb09b7dc5
wangjun <wangjun02@qutoutiao.net>
2020-01-21 20:20:13 +0800

    fix bug

--- src/main/scala/com/cpc/spark/oCPX/oCPC/calibration_by_tag/OcpcGetPb_base.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/calibration_by_tag/OcpcGetPb_base.scala
@@ -67,8 +67,8 @@ object OcpcGetPb_base {
 
     resultDF
       .repartition(1)
-//      .write.mode("overwrite").insertInto("test.ocpc_pb_data_hourly_exp")
-      .write.mode("overwrite").insertInto("dl_cpc.ocpc_pb_data_hourly_exp")
+      .write.mode("overwrite").saveAsTable("test.ocpc_pb_data_hourly_exp20200121")
+//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_pb_data_hourly_exp")
 
 
   }
