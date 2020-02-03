d47ff7f39a0163c81b57d30671c3ec211279cf99
wangjun <wangjun02@qutoutiao.net>
2020-02-03 18:47:11 +0800

    update

---
+++ src/main/scala/com/cpc/spark/oCPX/unittest/OcpcUnitTestNew.scala
@@ -0,0 +1,37 @@
+package com.cpc.spark.oCPX.unittest
+
+import com.cpc.spark.oCPX.cv_recall.deep_cv.OcpcDeepCVrecall_assessment.cvRecallAssessment
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+
+
+object OcpcUnitTestNew {
+  /*
+  新增部分媒体id采用暗投
+   */
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().enableHiveSupport().getOrCreate()
+    Logger.getRootLogger.setLevel(Level.WARN)
+
+    val date = args(0).toString
+    val hour = args(1).toString
+    val hourInt = 24
+
+    println("parameters:")
+    println(s"date=$date, hour=$hour")
+
+
+
+    val data = cvRecallAssessment(date, 1, spark)
+
+
+    data
+      .write.mode("overwrite").saveAsTable("test.check_ocpc_exp_data20200203h")
+
+
+
+  }
+
+}
+
+
