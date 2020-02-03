07f3c2d7386dad6e5906bcf2df9225c2c7f93511
wangjun <wangjun02@qutoutiao.net>
2020-02-03 19:36:32 +0800

    update

--- src/main/scala/com/cpc/spark/oCPX/unittest/OcpcUnitTestNew.scala
+++ src/main/scala/com/cpc/spark/oCPX/unittest/OcpcUnitTestNew.scala
@@ -1,6 +1,6 @@
 package com.cpc.spark.oCPX.unittest
 
-import com.cpc.spark.oCPX.deepOcpc.calibration_v9.OcpcGetPb_retention.{OcpcCalibrationBase, calculateCvrPart1, calculateDeepCvr, getDataByHourDiff, getRecallValue}
+import com.cpc.spark.oCPX.deepOcpc.calibration_v9.OcpcGetPb_retention.{OcpcCalibrationBase, calculateCvrPart1, calculateCvrPart2, calculateDeepCvr, getDataByHourDiff, getRecallValue}
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, lit, sum, when}
@@ -24,39 +24,13 @@ object OcpcUnitTestNew {
 
     val dataRaw = OcpcCalibrationBase(date, hour, 96, spark)
 
-    val minCV = 20
     // calculate deep_cvr
-    val deepCvrRaw = calculateDeepCvr(date, 3, spark)
+    val deepCvr = calculateDeepCvr(date, 3, spark)
 
-    val dataRaw1 = getDataByHourDiff(dataRaw, 24, 48, spark)
+    val data2 = calculateCvrPart2(dataRaw, deepCvr, 20, spark)
 
-    val recallValue = getRecallValue(spark).cache()
-    recallValue.show(10)
-
-    val deepCvr = deepCvrRaw
-      .groupBy("unitid")
-      .agg(
-        sum(col("cv1")).alias("cv1"),
-        sum(col("cv2")).alias("cv2")
-      )
-      .filter(s"cv2 >= 10")
-      .withColumn("deep_cvr", col("cv2") * 1.0 / col("cv1"))
-      .selectExpr("unitid", "cast(deep_cvr as double) as deep_cvr")
-
-
-    val data1 = dataRaw1
-      .withColumn("flag", when(col("cv2") >= minCV, 1).otherwise(0))
-      .withColumn("hour_diff", lit(24))
-      .join(recallValue, Seq("conversion_goal", "hour_diff"), "left_outer")
-      .na.fill(1.0, Seq("recall_value"))
-      .withColumn("cv2_recall1", col("cv2") * col("recall_value"))
-      .join(deepCvr, Seq("unitid"), "left_outer")
-      .withColumn("cv2_recall2", col("cv1") * col("deep_cvr"))
-      .withColumn("cv2_recall", when(col("deep_cvr").isNotNull, (col("cv2_recall1") + col("cv2_recall2")) * 1.0 / 2).otherwise(col("cv2")))
-
-
-    data1
-      .write.mode("overwrite").saveAsTable("test.check_ocpc_cali_exp_data20200203c")
+    data2
+      .write.mode("overwrite").saveAsTable("test.check_ocpc_cali_exp_data20200203d")
 
 
 
