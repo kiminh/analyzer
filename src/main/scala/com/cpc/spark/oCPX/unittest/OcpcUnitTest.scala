de91f3a799f491a44ff82ef5197061e77c0a0747
wangjun <wangjun02@qutoutiao.net>
2020-02-04 19:59:49 +0800

    fix bug

--- src/main/scala/com/cpc/spark/oCPX/unittest/OcpcUnitTest.scala
+++ src/main/scala/com/cpc/spark/oCPX/unittest/OcpcUnitTest.scala
@@ -1,6 +1,6 @@
 package com.cpc.spark.oCPX.unittest
 
-import com.cpc.spark.oCPX.oCPC.calibration_by_tag.OcpcGetPb_weightv6.{OcpcRealtimeCalibrationBase, cvRecallPredictV1}
+import com.cpc.spark.oCPX.oCPC.calibration_by_tag.OcpcGetPb_weightv6.{OcpcRealtimeCalibrationBase, cvRecallPredictV1, cvRecallPredictV2}
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 
@@ -22,10 +22,10 @@ object OcpcUnitTest {
 
 
 
-    val recallValue1 = cvRecallPredictV1(date, spark)
+    val recallValue2 = cvRecallPredictV2(date, spark)
 
-    recallValue1
-      .write.mode("overwrite").saveAsTable("test.check_ocpc_cali_exp_data20200204a")
+    recallValue2
+      .write.mode("overwrite").saveAsTable("test.check_ocpc_cali_exp_data20200204b")
 
 
 
