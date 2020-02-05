10d51d0727c46a4410169771ad34a8367cc32aca
wangjun <wangjun02@qutoutiao.net>
2020-02-05 14:52:45 +0800

    update

--- src/main/scala/com/cpc/spark/oCPX/unittest/OcpcUnitTest.scala
+++ src/main/scala/com/cpc/spark/oCPX/unittest/OcpcUnitTest.scala
@@ -1,7 +1,7 @@
 package com.cpc.spark.oCPX.unittest
 
 
-import com.cpc.spark.oCPX.oCPC.light_control.white_list.OcpcWhiteList.getUnitData
+import com.cpc.spark.oCPX.oCPC.light_control.white_list.OcpcFreePass.getUnitData
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 
@@ -22,7 +22,7 @@ object OcpcUnitTest {
     println(s"date=$date, hour=$hour")
 
 
-    val unit = getUnitData(spark)
+    val unit = getUnitData(date, hour, spark).cache()
 
     unit
       .write.mode("overwrite").saveAsTable("test.check_ocpc_cali_exp_data20200205a")
