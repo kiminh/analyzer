b509faa48d609dee152a45b77644f7ec6530055a
wangjun <wangjun02@qutoutiao.net>
2020-02-06 22:42:08 +0800

    update

--- src/main/scala/com/cpc/spark/oCPX/oCPC/calibration_by_tag/v1/OcpcGetPb_v1_part2.scala
+++ src/main/scala/com/cpc/spark/oCPX/oCPC/calibration_by_tag/v1/OcpcGetPb_v1_part2.scala
@@ -70,8 +70,8 @@ object OcpcGetPb_v1_part2{
 
     resultDF
       .repartition(1)
-      .write.mode("overwrite").insertInto("test.ocpc_pb_data_hourly_exp")
-//      .write.mode("overwrite").insertInto("dl_cpc.ocpc_pb_data_hourly_exp")
+//      .write.mode("overwrite").insertInto("test.ocpc_pb_data_hourly_exp")
+      .write.mode("overwrite").insertInto("dl_cpc.ocpc_pb_data_hourly_exp")
 
 
   }
