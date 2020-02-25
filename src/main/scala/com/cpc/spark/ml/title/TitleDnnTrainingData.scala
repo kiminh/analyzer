b04ebf72cb1d1f29cc973bc094d945d0118263cd
duanguangdong <duanguangdong@qutoutiao.net>
2020-02-25 22:56:28 +0800

    delete

--- src/main/scala/com/cpc/spark/ml/title/TitleDnnTrainingData.scala
+++ src/main/scala/com/cpc/spark/ml/title/TitleDnnTrainingData.scala
@@ -82,11 +82,11 @@ object TitleDnnTrainingData {
 
     val old_ideaid_data = spark.sql(
       s"""
-         |select t1.idx2, t1.idx1, t1.id_arr, t1.idx0, t1.sample_idx, t1.label, t1.dense, t2.title as floats
+         |select t1.idx2, t1.idx1, t1.id_arr, t1.idx0, t1.sample_idx, t1.label, t1.dense, t2.title as float
          |from training_data t1 left join idea_table t2 on t1.dense[11]=t2.ideaidhash
          |""".stripMargin)
 
-    old_ideaid_data.repartition(1000).select($"sample_idx",$"idx0",$"idx1",$"idx2",$"id_arr", $"label", $"dense", $"floats").write
+    old_ideaid_data.repartition(1000).select($"sample_idx",$"idx0",$"idx1",$"idx2",$"id_arr", $"label", $"dense", $"float").write
       .mode("overwrite")
       .format("tfrecords")
       .option("recordType", "Example")
